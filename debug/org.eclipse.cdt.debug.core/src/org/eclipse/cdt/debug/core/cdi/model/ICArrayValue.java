1b89c22226d5a3adf98f7a5af5d40f0f8e982df0
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2002-08-09 21:52:13 +0000

    Added interfaces for different types of variable's values.

---
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/model/ICArrayValue.java
@@ -0,0 +1,16 @@
+/*
+ *(c) Copyright QNX Software Systems Ltd. 2002.
+ * All Rights Reserved.
+ * 
+ */
+package org.eclipse.cdt.debug.core.cdi.model;
+
+/**
+ * 
+ * Represents a value of an array type.
+ * 
+ * @since Aug 9, 2002
+ */
+public interface ICArrayValue extends ICValue
+{
+}

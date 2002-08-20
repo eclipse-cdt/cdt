c1f1744c46b8a06d4780283f5d6910fdd7a61792
Alain Magloire <alain@qnx.com>
2002-08-20 04:26:17 +0000

    Implement wiht Variable Object.

--- debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/cdi/Argument.java
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/cdi/Argument.java
@@ -1,66 +1,13 @@
 package org.eclipse.cdt.debug.mi.core.cdi;
 
-import org.eclipse.cdt.debug.core.cdi.CDIException;
 import org.eclipse.cdt.debug.core.cdi.model.ICDIArgument;
-import org.eclipse.cdt.debug.core.cdi.model.ICDIObject;
-import org.eclipse.cdt.debug.core.cdi.model.ICDITarget;
-import org.eclipse.cdt.debug.core.cdi.model.ICDIValue;
-import org.eclipse.cdt.debug.mi.core.output.MIArg;
+import org.eclipse.cdt.debug.mi.core.output.MIVar;
 
 /**
- * @author alain
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
  */
-public class Argument extends CObject implements ICDIArgument {
+public class Argument extends Variable implements ICDIArgument {
 
-	MIArg arg;
-
-	public Argument(CTarget target, MIArg a) {
-		super(target);
-		arg = a;
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#getName()
-	 */
-	public String getName() throws CDIException {
-		return arg.getName();
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#getValue()
-	 */
-	public ICDIValue getValue() throws CDIException {
-		return new Value(getCTarget(), arg.getValue());
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#hasValueChanged()
-	 */
-	public boolean hasValueChanged() throws CDIException {
-		return false;
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#setValue(ICDIValue)
-	 */
-	public void setValue(ICDIValue value) throws CDIException {
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#setValue(String)
-	 */
-	public void setValue(String expression) throws CDIException {
-	}
-
-	/**
-	 * @see org.eclipse.cdt.debug.core.cdi.model.ICDIVariable#getTypeName()
-	 */
-	public String getTypeName() throws CDIException {
-		return "";
+	public Argument(StackFrame frame, String name, MIVar var) {
+		super(frame, name, var);
 	}
 }

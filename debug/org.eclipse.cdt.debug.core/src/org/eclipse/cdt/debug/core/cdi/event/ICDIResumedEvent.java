61b492d5c75717eff9b8d69783718f89eb8b0933
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2002-08-20 18:13:07 +0000

    Added new type to ICDIResumedEvent.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/event/ICDIResumedEvent.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/event/ICDIResumedEvent.java
@@ -17,11 +17,12 @@ package org.eclipse.cdt.debug.core.cdi.event;
  * @since Jul 10, 2002
  */
 public interface ICDIResumedEvent extends ICDIEvent {
-	final static public int STEP_OVER = 0;
-	final static public int STEP_INTO = 1;
-	final static public int STEP_OVER_INSTRUCTION = 2;
-	final static public int STEP_INTO_INSTRUCTION = 3;
-	final static public int STEP_RETURN = 4;
+	final static public int CONTINUE = 0;
+	final static public int STEP_OVER = 1;
+	final static public int STEP_INTO = 2;
+	final static public int STEP_OVER_INSTRUCTION = 3;
+	final static public int STEP_INTO_INSTRUCTION = 4;
+	final static public int STEP_RETURN = 5;
 
 	/**
 	 * Returns the stepping type.

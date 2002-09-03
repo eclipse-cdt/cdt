961a96a496b8296fe7c497feb3d9b05b01ffc73b
David Inglis <dinglis@qnx.com>
2002-09-03 17:52:57 +0000

    update qnx spawner

--- core/org.eclipse.cdt.core.qnx/library/common.mk
+++ core/org.eclipse.cdt.core.qnx/library/common.mk
@@ -3,12 +3,14 @@ QCONFIG=qconfig.mk
 endif
 include $(QCONFIG)
 
+NAME=spawner
+
 include $(MKFILES_ROOT)/qtargets.mk
 
 ifeq ($(OS),nto)
 ifeq ($(IVEHOME),)
-IVEHOME:=/opt/vame/ive
+IVEHOME:=/opt/vame/ive/bin
 endif
-EXTRA_INCVPATH+=$(IVEHOME)/bin/include
+EXTRA_INCVPATH+=$(IVEHOME)/include
 endif
 

5a8ae1ba8f8078f0bfcce2ca1d59e3988e80c6bf
Alain Magloire <alain@qnx.com>
2002-09-06 14:08:29 +0000

    code to get a master pseudo Terminal.

---
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/pty/PTY.java
@@ -0,0 +1,49 @@
+package org.eclipse.cdt.utils.pty;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @author alain
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class PTY {
+
+	String slave;
+	public int master;
+	InputStream in;
+	OutputStream out;
+	
+	public PTY() throws IOException {
+		slave= forkpty();
+		if (slave == null) {
+			throw new IOException("Can not create pty");
+		}
+		in = new PTYInputStream(master);
+		out = new PTYOutputStream(master);
+	}
+	
+	public String getSlaveName() {
+		return slave;
+	}
+	
+	public OutputStream getOutputStream() {
+		return out;
+	}
+	
+	public InputStream getInputStream() {
+		return in;
+	}
+	
+	native String forkpty();
+
+	static {
+		System.loadLibrary("pty");
+	}
+	
+}

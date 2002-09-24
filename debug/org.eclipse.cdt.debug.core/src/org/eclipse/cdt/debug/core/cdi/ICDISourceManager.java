3c63ff5d56b205e382bd4856f4b6465266352bc3
Alain Magloire <alain@qnx.com>
2002-09-24 19:07:37 +0000

    Remove addLibraryPath and getLibraryPaths() method it was consider to be to gdb specific.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/ICDISourceManager.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/ICDISourceManager.java
@@ -28,18 +28,6 @@ public interface ICDISourceManager extends ICDISessionObject
 	String[] getSourcePaths() throws CDIException;
 
 	/**
-	 * Set the shared library search paths for the debu session.
-	 * @param String
-	 */
-	void addLibraryPaths(String[] libPaths) throws CDIException;
-
-	/**
-	 * Return the array of shared libraries search paths
-	 * @return String array of search paths.
-	 */
-	String[] getLibraryPaths() throws CDIException;
-
-	/**
 	 * Returns an array of directories. Returns the empty array 
 	 * if the source path is empty.
 	 * 

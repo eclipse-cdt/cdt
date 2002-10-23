2f2b06a32096fc7c71656a29732fca8733ff02ca
Alain Magloire <alain@qnx.com>
2002-10-23 18:20:38 +0000

    New final static fields.

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/IMarkerGenerator.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/IMarkerGenerator.java
@@ -6,5 +6,10 @@ import org.eclipse.core.resources.IResource;
  * @author sam.robb
  */
 public interface IMarkerGenerator {
-	public void addMarker(IResource file, int lineNumber, String errorDesc, int severity, String errorVar);
+	int SEVERITY_INFO = 0;
+	int SEVERITY_WARNING = 1;
+	int SEVERITY_ERROR_RESOURCE = 2;
+	int SEVERITY_ERROR_BUILD = 3;
+
+	void addMarker(IResource file, int lineNumber, String errorDesc, int severity, String errorVar);
 }

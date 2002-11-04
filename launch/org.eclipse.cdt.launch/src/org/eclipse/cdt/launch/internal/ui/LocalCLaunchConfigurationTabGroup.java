194b8597561550b574252f0ee816b79534d3a01d
David Inglis <dinglis@qnx.com>
2002-11-04 20:01:11 +0000

    since config is shared we shouldn't filter that tab list

--- launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/ui/LocalCLaunchConfigurationTabGroup.java
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/ui/LocalCLaunchConfigurationTabGroup.java
@@ -1,12 +1,9 @@
 package org.eclipse.cdt.launch.internal.ui;
 
-import java.util.ArrayList;
-
 import org.eclipse.cdt.launch.ui.CArgumentsTab;
 import org.eclipse.cdt.launch.ui.CDebuggerTab;
 import org.eclipse.cdt.launch.ui.CEnvironmentTab;
 import org.eclipse.cdt.launch.ui.CMainTab;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
@@ -23,17 +20,14 @@ public class LocalCLaunchConfigurationTabGroup extends AbstractLaunchConfigurati
 	 * @see AbstractLaunchConfigurationTabGroup#createTabs
 	 */
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode)  {
-		ArrayList tabs = new ArrayList(5);
-
-		tabs.add(new CMainTab());
-		tabs.add(new CArgumentsTab());
-		tabs.add(new CEnvironmentTab());
-		if ( mode.equalsIgnoreCase(ILaunchManager.DEBUG_MODE) ) {
-			tabs.add(new CDebuggerTab() );
-		}
-		tabs.add(new CommonTab());
-
-		setTabs((ILaunchConfigurationTab[])tabs.toArray(new ILaunchConfigurationTab[tabs.size()]));
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+			new CMainTab(),
+			new CArgumentsTab(),
+			new CEnvironmentTab(),
+			new CDebuggerTab(),
+			new CommonTab() 
+		};
+		setTabs(tabs);
 	}
 	
 }

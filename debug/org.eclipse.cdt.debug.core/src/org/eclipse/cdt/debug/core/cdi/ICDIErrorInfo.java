3d26f7b862524e0ceafb7aba241281516d214ca8
Alain Magloire <alain@qnx.com>
2002-12-02 18:34:20 +0000

    New file for errors

---
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/ICDIErrorInfo.java
@@ -0,0 +1,21 @@
+/*
+ *(c) Copyright QNX Software Systems Ltd. 2002.
+ * All Rights Reserved.
+ * 
+ */
+package org.eclipse.cdt.debug.core.cdi;
+
+/**
+ * 
+ * Represents an information provided by the session when the program 
+ * exited.
+ * 
+ * @since Jul 10, 2002
+ */
+public interface ICDIErrorInfo extends ICDISessionObject {
+
+	/**
+	 * Returns the error message.
+	 */
+	public String getMessage();
+}

4a43f50ff80e98cfc82ee777edcf9436c799f429
Alain Magloire <alain@qnx.com>
2003-01-16 03:12:23 +0000

    place holder for the shared lib info

---
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/output/MIShared.java
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright QNX Software Systems Ltd. 2002.
+ * All Rights Reserved.
+ */
+package org.eclipse.cdt.debug.mi.core.output;
+
+
+/**
+ * GDB/MI shared information
+ */
+public class MIShared {
+
+	long from;
+	long to;
+	boolean isread;
+	String name;
+
+	public MIShared (long start, long end, boolean read, String location) {
+		from = start;
+		to = end;
+		isread = read;
+		name = location;
+	}
+
+	public long getFrom() {
+		return from;
+	}
+
+	public long getTo() {
+		return to;
+	}
+
+	public boolean isRead() {
+		return isread;
+	}
+
+	public String getName() {
+		return name;
+	}
+}

8e125546ce97a834e77cda61f3fe573948214a99
Alain Magloire <alain@qnx.com>
2003-01-27 03:10:36 +0000

    Change of the Constructor to use ArgumentObject

--- debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/cdi/model/Argument.java
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/cdi/model/Argument.java
@@ -5,13 +5,14 @@
 package org.eclipse.cdt.debug.mi.core.cdi.model;
 
 import org.eclipse.cdt.debug.core.cdi.model.ICDIArgument;
+import org.eclipse.cdt.debug.mi.core.cdi.ArgumentObject;
 import org.eclipse.cdt.debug.mi.core.output.MIVar;
 
 /**
  */
 public class Argument extends Variable implements ICDIArgument {
 
-	public Argument(StackFrame frame, String name, MIVar var) {
-		super(frame, name, var);
+	public Argument(ArgumentObject obj, MIVar var) {
+		super(obj, var);
 	}
 }

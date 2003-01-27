f60c2af5b28853a2f641041dc4c2586c2c0baa8c
Alain Magloire <alain@qnx.com>
2003-01-27 03:32:30 +0000

    destroyExpression() new method.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/ICDIExpressionManager.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/cdi/ICDIExpressionManager.java
@@ -11,27 +11,10 @@ import org.eclipse.cdt.debug.core.cdi.model.ICDIExpression;
  * 
  * Manages the collection of registered expressions in the 
  * debug session.
- * 
+ * Auto update is on by default.
  * @since Jul 9, 2002
  */
-public interface ICDIExpressionManager extends ICDISessionObject {
-
-	/**
-	 * Removes the given array of expressions from the expression 
-	 * manager.
-	 * 
-	 * @param expressions - the array of expressions to remove
-	 * @throws CDIException on failure. Reasons include:
-	 */
-	void removeExpressions(ICDIExpression[] expressions) throws CDIException;
-
-	/**
-	 * Removes the given expression from the expression manager.
-	 * 
-	 * @param expressions - the expression to remove
-	 * @throws CDIException on failure. Reasons include:
-	 */
-	void removeExpression(ICDIExpression expression) throws CDIException;
+public interface ICDIExpressionManager extends ICDIManager {
 
 	/**
 	 * Returns an expression specified by the given identifier.
@@ -49,5 +32,13 @@ public interface ICDIExpressionManager extends ICDISessionObject {
 	 * @throws CDIException on failure. Reasons include:
 	 */
 	ICDIExpression[] getExpressions() throws CDIException;
+
+	/**
+	 * Removes the given expression from the expression manager.
+	 * 
+	 * @param expressions - the expression to remove
+	 * @throws CDIException on failure. Reasons include:
+	 */
+	void destroyExpression(ICDIExpression expression) throws CDIException;
 
 }

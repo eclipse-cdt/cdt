6a38931f7e0985f3dbf2525d92e9e5876c14bc53
David Inglis <dinglis@qnx.com>
2003-02-07 14:38:34 +0000

    revert change since this new method does not exist in 2.0  (the new methods is not doc as 2.1!!!)

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dialogs/SelectionStatusDialog.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dialogs/SelectionStatusDialog.java
@@ -86,7 +86,7 @@ public abstract class SelectionStatusDialog extends SelectionDialog {
 	 * @return the first element of the initial selection.
 	 */
 	protected Object getPrimaryInitialSelection() {
-		List result= getInitialElementSelections();
+		List result= getInitialSelections();
 		if (result == null || result.size() == 0)
 			return null;
 		return result.get(0);	
@@ -124,7 +124,7 @@ public abstract class SelectionStatusDialog extends SelectionDialog {
 		fImage= image;
 	}
 	protected void setInitialSelection(int position, Object element) {
-		List l= getInitialElementSelections();
+		List l= getInitialSelections();
 		l.set(position, element);
 		fInitialSelectionSet= true;
 	}

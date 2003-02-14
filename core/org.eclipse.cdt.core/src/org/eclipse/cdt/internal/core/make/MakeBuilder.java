69f5cde8c4ecd0a671a5a3c64b45d2cea9b847ed
Alain Magloire <alain@qnx.com>
2003-02-14 01:17:11 +0000

    Comment out the references to the old build for now

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/make/MakeBuilder.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/make/MakeBuilder.java
@@ -6,14 +6,9 @@ package org.eclipse.cdt.internal.core.make;
 
 import org.eclipse.cdt.core.AbstractCExtension;
 import org.eclipse.cdt.core.CCorePlugin;
-import org.eclipse.cdt.core.ICExtensionReference;
-import org.eclipse.cdt.core.builder.CIncrementalBuilder;
-import org.eclipse.cdt.core.builder.ICBuilder;
-import org.eclipse.cdt.core.builder.IOptimization;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 
-public class MakeBuilder extends AbstractCExtension implements ICBuilder {
+public class MakeBuilder extends AbstractCExtension /*implements ICBuilder */ {
 
 	public IPath[] getIncludePaths() {
 		return new IPath[0];
@@ -36,21 +31,22 @@ public class MakeBuilder extends AbstractCExtension implements ICBuilder {
 	public void setLibraries(String[] libs) {
 	}
 
-	public IOptimization getOptimization() {
-		return null;
-	}
-
-	public IProject[] build(CIncrementalBuilder cbuilder) {
-		ICExtensionReference ref = getExtensionReference();
-		System.out.println("MakeBuilder!!!!\n Command is:" + ref.getExtensionData("command"));
-		return null;
-	}
+//	public IOptimization getOptimization() {
+//		return null;
+//	}
+//
+//	public IProject[] build(CIncrementalBuilder cbuilder) {
+//		ICExtensionReference ref = getExtensionReference();
+//		System.out.println("MakeBuilder!!!!\n Command is:" + ref.getExtensionData("command"));
+//		return null;
+//	}
+//
+//	public void setOptimization(IOptimization o) {
+//	}
 
 	public String getID() {
 		return CCorePlugin.PLUGIN_ID + ".makeBuilder";
 	}
 
-	public void setOptimization(IOptimization o) {
-	}
 
 }

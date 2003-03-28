b5c5fe52bef889237ab416682b8f13064a2989b1
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2003-03-28 19:19:33 +0000

    Added default format preference constants for variables, expressions and registers.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/ICDebugConstants.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/ICDebugConstants.java
@@ -25,4 +25,19 @@ public interface ICDebugConstants
 	 * disassembly mode.
 	 */
 	public static final String PREF_AUTO_DISASSEMBLY = PLUGIN_ID + "cDebug.auto_disassembly"; //$NON-NLS-1$
+
+	/**
+	 * The identifier of the default variable format to use in the variables view
+	 */
+	public static final String PREF_DEFAULT_VARIABLE_FORMAT = PLUGIN_ID + "cDebug.default_variable_format"; //$NON-NLS-1$
+
+	/**
+	 * The identifier of the default register format to use in the registers view
+	 */
+	public static final String PREF_DEFAULT_REGISTER_FORMAT = PLUGIN_ID + "cDebug.default_register_format"; //$NON-NLS-1$
+
+	/**
+	 * The identifier of the default expression format to use in the expressions views
+	 */
+	public static final String PREF_DEFAULT_EXPRESSION_FORMAT = PLUGIN_ID + "cDebug.default_expression_format"; //$NON-NLS-1$
 }

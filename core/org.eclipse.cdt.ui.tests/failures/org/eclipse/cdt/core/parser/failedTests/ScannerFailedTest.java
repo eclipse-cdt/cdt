85cb30cd545f8753d62656d1ce5d00fd62eb2604
John Camelon <jcamelon@ca.ibm.com>
2003-04-28 18:58:50 +0000

    Patch for Andrew Niefer     Added DOMFailedTest::testBug37019     Added DOMFailedTest::testBug36932     Added ScannerFailedTest::testBug37011

--- core/org.eclipse.cdt.ui.tests/failures/org/eclipse/cdt/core/parser/failedTests/ScannerFailedTest.java
+++ core/org.eclipse.cdt.ui.tests/failures/org/eclipse/cdt/core/parser/failedTests/ScannerFailedTest.java
@@ -18,6 +18,8 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.cdt.core.parser.tests.BaseScannerTest;
+import org.eclipse.cdt.internal.core.parser.ScannerException;
+import org.eclipse.cdt.internal.core.parser.Token;
 
 /**
  * @author aniefer
@@ -36,6 +38,7 @@ public class ScannerFailedTest extends BaseScannerTest  {
 		TestSuite suite = new TestSuite();
 		 
 		suite.addTest( new ScannerFailedTest( "testBug36701" ) );
+		suite.addTest( new ScannerFailedTest( "testBug37011" ) );
 
 		return suite;
 	}
@@ -57,6 +60,30 @@ public class ScannerFailedTest extends BaseScannerTest  {
 			//expected failure
 		}
 		
+		if( testPassed )
+			fail( "The expected error did not occur." );
+	}
+	
+	public void testBug37011() throws Exception
+	{
+		boolean testPassed = false;
+		try{
+			StringWriter writer = new StringWriter();
+			writer.write( "#define A \"//\"\n" );
+			writer.write( " { A };\n" );
+			
+			initializeScanner( writer.toString() );
+			validateToken( Token.tLBRACE );
+			validateString( "//" );
+			validateToken( Token.tRBRACE );
+			validateToken( Token.tSEMI );
+			validateEOF();
+			
+			testPassed = true;
+		} catch( ScannerException e ){
+			//expected failure
+		}
+				
 		if( testPassed )
 			fail( "The expected error did not occur." );
 	}

1ae15870b3bd55dc530f6e0142d9fd8693c19d31
Alain Magloire <alain@qnx.com>
2003-04-29 17:28:12 +0000

    Catch the case the path is null.

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/parser/ElfParser.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/parser/ElfParser.java
@@ -12,6 +12,7 @@ import org.eclipse.cdt.core.IBinaryParser;
 import org.eclipse.cdt.utils.elf.AR;
 import org.eclipse.cdt.utils.elf.Elf;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 
 /**
  */
@@ -21,6 +22,9 @@ public class ElfParser extends AbstractCExtension implements IBinaryParser {
 	 * @see org.eclipse.cdt.core.model.IBinaryParser#getBinary(IPath)
 	 */
 	public IBinaryFile getBinary(IPath path) throws IOException {
+		if (path == null ) {
+			path = new Path("");
+		}
 		try {
 			Elf e = new Elf(path.toOSString());
 			e.dispose();

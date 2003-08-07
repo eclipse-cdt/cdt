372a84559f099767fc4470676b44302010af054e
Alain Magloire <alain@qnx.com>
2003-08-07 03:29:26 +0000

    added new method for parsing.

--- debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/command/MIVarInfoExpression.java
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/command/MIVarInfoExpression.java
@@ -6,6 +6,11 @@
 
 package org.eclipse.cdt.debug.mi.core.command;
 
+import org.eclipse.cdt.debug.mi.core.MIException;
+import org.eclipse.cdt.debug.mi.core.output.MIInfo;
+import org.eclipse.cdt.debug.mi.core.output.MIOutput;
+import org.eclipse.cdt.debug.mi.core.output.MIVarInfoExpressionInfo;
+
 /**
  * 
  *     -var-info-expression NAME
@@ -22,4 +27,21 @@ public class MIVarInfoExpression extends MICommand
 	public MIVarInfoExpression(String name) {
 		super("-var-info-expression", new String[]{name});
 	}
+	
+	public MIVarInfoExpressionInfo getMIVarInfoExpressionInfo() throws MIException {
+		return (MIVarInfoExpressionInfo)getMIInfo();
+	}
+
+	public MIInfo getMIInfo() throws MIException {
+		MIInfo info = null;
+		MIOutput out = getMIOutput();
+		if (out != null) {
+			info = new MIVarInfoExpressionInfo(out);
+			if (info.isError()) {
+				throwMIException(info, out);
+			}
+		}
+		return info;
+	}
+
 }

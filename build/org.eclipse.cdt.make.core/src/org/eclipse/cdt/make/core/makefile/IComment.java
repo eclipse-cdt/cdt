3e86e80afd2c9858cf880c7bb5f257e09ae784d2
Alain Magloire <alain@qnx.com>
2003-09-07 19:42:06 +0000

    Interface of the Makefile parser

---
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/core/makefile/IComment.java
@@ -0,0 +1,22 @@
+/**********************************************************************
+ * Copyright (c) 2002,2003 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ * QNX Software Systems - Initial API and implementation
+***********************************************************************/
+package org.eclipse.cdt.make.core.makefile;
+
+/**
+ * IComment
+ */
+public interface IComment  extends IStatement {
+
+	final public static char POUND = '#';
+
+	final public static String POUND_STRING = "#";
+
+}

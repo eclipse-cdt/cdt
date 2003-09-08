ce59a25fa681dc96fe814a4737f87b6b7993c228
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2003-09-08 21:40:11 +0000

    Regrouping the launch configuration constants.

--- debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/IMILaunchConfigurationConstants.java
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/IMILaunchConfigurationConstants.java
@@ -14,21 +14,18 @@ public interface IMILaunchConfigurationConstants {
 	public static final String ATTR_DEBUG_NAME = MIPlugin.getUniqueIdentifier() + ".DEBUG_NAME"; //$NON-NLS-1$
 
 	/**
-	 * Launch configuration attribute key. The value is a List (array of String) directories for solib-search-path
-	 * the Debuger associated with a C/C++ launch configuration.
-	 */
-	public static final String ATTR_SOLIB_PATH = MIPlugin.getUniqueIdentifier() + ".SOLIB_PATH"; //$NON-NLS-1$
-
-	/**
-	 * Launch configuration attribute key. Boolean value to set the auto-solib-add
-	 * Debuger/gdb/MI property.
-	 */
-	public static final String ATTR_AUTO_SOLIB = MIPlugin.getUniqueIdentifier() + ".AUTO_SOLIB"; //$NON-NLS-1$
-
-	/**
-	 * Launch configuration attribute key. Boolean value to set the auto-solib-add
+	 * Launch configuration attribute key. Boolean value to set the gdb command file
 	 * Debuger/gdb/MI property.
 	 */
 	public static final String ATTR_GDB_INIT = MIPlugin.getUniqueIdentifier() + ".GDB_INIT"; //$NON-NLS-1$
 
+	/**
+	 * Launch configuration attribute key. Boolean value to set the 'automatically load shared library symbols' flag of the debugger.
+	 */
+	public static final String ATTR_DEBUGGER_AUTO_SOLIB = MIPlugin.getUniqueIdentifier() + ".AUTO_SOLIB"; //$NON-NLS-1$
+
+	/**
+	 * Launch configuration attribute key. Boolean value to set the 'stop on shared library events' flag of the debugger.
+	 */
+	public static final String ATTR_DEBUGGER_STOP_ON_SOLIB_EVENTS = MIPlugin.getUniqueIdentifier() + ".STOP_ON_SOLIB_EVENTS"; //$NON-NLS-1$
 }

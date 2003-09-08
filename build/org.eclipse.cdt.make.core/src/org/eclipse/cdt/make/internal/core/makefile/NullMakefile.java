a8edb98ca73d25c094f3b93898060b23d237a944
Alain Magloire <alain@qnx.com>
2003-09-08 04:57:30 +0000

    Makefile adapter.

---
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/makefile/NullMakefile.java
@@ -0,0 +1,48 @@
+/**********************************************************************
+ * Copyright (c) 2002,2003 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ * QNX Software Systems - Initial API and implementation
+***********************************************************************/
+package org.eclipse.cdt.make.internal.core.makefile;
+
+import org.eclipse.cdt.make.core.makefile.IStatement;
+
+/**
+ * Makefile : ( statement ) *
+ * statement :   rule | macro_definition | comments | empty
+ * rule :  inference_rule | target_rule
+ * inference_rule : target ':' <nl> ( <tab> command <nl> ) +
+ * target_rule : target [ ( target ) * ] ':' [ ( prerequisite ) * ] [ ';' command ] <nl> 
+                 [ ( command ) * ]
+ * macro_definition : string '=' (string)* 
+ * comments : ('#' (string) <nl>) *
+ * empty : <nl>
+ * command : <tab> prefix_command string <nl>
+ * target : string
+ * prefix_command : '-' | '@' | '+'
+ * internal_macro :  "$<" | "$*" | "$@" | "$?" | "$%" 
+ */
+
+public class NullMakefile extends AbstractMakefile {
+
+	public static IStatement[] empty = new IStatement[0];
+
+	public NullMakefile() {
+	}
+
+	public IStatement[] getStatements() {
+		return empty;
+	}
+
+	public IStatement[] getBuiltins() {
+		return empty;
+	}
+
+	public void addStatement(IStatement statement) {
+	}
+}

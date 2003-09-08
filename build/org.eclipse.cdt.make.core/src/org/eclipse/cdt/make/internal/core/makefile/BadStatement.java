3638c876a55f1ffb127495e4f6ff9466fb6c9b4f
Alain Magloire <alain@qnx.com>
2003-09-08 04:57:16 +0000

    new File BadStatement

---
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/makefile/BadStatement.java
@@ -0,0 +1,25 @@
+/**********************************************************************
+ * Copyright (c) 2002,2003 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ * QNX Software Systems - Initial API and implementation
+***********************************************************************/
+package org.eclipse.cdt.make.internal.core.makefile;
+
+public class BadStatement extends Statement {
+
+	String line;
+
+	public BadStatement(String s) {
+		line = s;
+	}
+
+	public String toString() {
+		return line;
+	}
+
+}

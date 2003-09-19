313f632a5d6cd694a47eb7e0dbe7274918dd3a83
Doug Schaefer <doug.schaefer@windriver.com>
2003-09-19 13:50:48 +0000

    Fix the build.

--- build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/makefile/Util.java
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/makefile/Util.java
@@ -46,7 +46,7 @@ public class Util {
 	}
 
 	public static int indexOf(String line, char c) {
-		return indexOf(line, Character.toString(c));
+		return indexOf(line, new Character(c).toString());
 	}
 
 	/**

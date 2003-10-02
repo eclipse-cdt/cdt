7bda679ef01f5e670fdec6e2521e957d17e10dde
cvs2svn <cdt-dev@eclipse.org>
2003-10-02 02:56:47 +0000

    This commit was manufactured by cvs2svn to create tag 'CDT_1_2_RC0'.
    Sprout from master 2003-10-02 02:56:46 UTC Doug Schaefer <doug.schaefer@windriver.com> 'Patch for Sean Evoy:'
    Delete:
        core/org.eclipse.cdt.core.aix/.classpath
        core/org.eclipse.cdt.core.aix/.cvsignore
        core/org.eclipse.cdt.core.aix/.project
        core/org.eclipse.cdt.core.aix/.template
        core/org.eclipse.cdt.core.aix/build.properties
        core/org.eclipse.cdt.core.aix/fragment.properties
        core/org.eclipse.cdt.core.aix/fragment.xml
        core/org.eclipse.cdt.core.aix/library/.cvsignore
        core/org.eclipse.cdt.core.aix/library/Makefile
        core/org.eclipse.cdt.core.aix/library/PTY.h
        core/org.eclipse.cdt.core.aix/library/PTYInputStream.h
        core/org.eclipse.cdt.core.aix/library/PTYOutputStream.h
        core/org.eclipse.cdt.core.aix/library/Spawner.h
        core/org.eclipse.cdt.core.aix/library/SpawnerInputStream.h
        core/org.eclipse.cdt.core.aix/library/SpawnerOutputStream.h
        core/org.eclipse.cdt.core.aix/library/exec0.h
        core/org.eclipse.cdt.core.aix/library/exec_unix.c
        core/org.eclipse.cdt.core.aix/library/io.c
        core/org.eclipse.cdt.core.aix/library/openpty.c
        core/org.eclipse.cdt.core.aix/library/openpty.h
        core/org.eclipse.cdt.core.aix/library/pfind.c
        core/org.eclipse.cdt.core.aix/library/pty.c
        core/org.eclipse.cdt.core.aix/library/ptyio.c
        core/org.eclipse.cdt.core.aix/library/spawner.c
        core/org.eclipse.cdt.core.aix/src/org/eclipse/cdt/internal/core/aix/ProcessInfo.java
        core/org.eclipse.cdt.core.aix/src/org/eclipse/cdt/internal/core/aix/ProcessList.java
        releng/org.eclipse.cdt.releng/.cvsignore
        releng/org.eclipse.cdt.releng/.project
        releng/org.eclipse.cdt.releng/build.sh
        releng/org.eclipse.cdt.releng/build.xml
        releng/org.eclipse.cdt.releng/genscripts.xsl
        releng/org.eclipse.cdt.releng/index.xsl
        releng/org.eclipse.cdt.releng/junit.xsl
        releng/org.eclipse.cdt.releng/main.xsl
        releng/org.eclipse.cdt.releng/manifest.xml
        releng/org.eclipse.cdt.releng/plugin.xsl
        releng/org.eclipse.cdt.releng/site.xsl

--- releng/org.eclipse.cdt.releng/build.sh
+++
@@ -1,1 +0,0 @@
-java -classpath $JAVA_HOME/lib/tools.jar:jars/ant.jar:jars/optional.jar:jars/xalan.jar:jars/xml-apis.jar:jars/xercesImpl.jar:jars/NetComponents.jar:jars/activation.jar:jars/mail.jar org.apache.tools.ant.Main $* 2>&1 | tee build.log

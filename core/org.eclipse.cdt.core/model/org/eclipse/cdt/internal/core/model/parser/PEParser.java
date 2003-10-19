d06d62c1c20f90862cd3cc8f095c34c0485f6365
Alain Magloire <alain@qnx.com>
2003-10-19 01:13:53 +0000

    The real implementation of the parser been move to utils/org/eclipse/cdt/utils/coff/parser and utils/org/eclipse/cdt/utils/elf/parser

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/parser/PEParser.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/parser/PEParser.java
@@ -5,43 +5,10 @@ package org.eclipse.cdt.internal.core.model.parser;
  * All Rights Reserved.
  */
 
-import java.io.IOException;
-
-import org.eclipse.cdt.core.AbstractCExtension;
-import org.eclipse.cdt.core.IBinaryParser;
-import org.eclipse.cdt.utils.coff.PE;
-import org.eclipse.cdt.utils.coff.PEArchive;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 
 /**
+ * Implementation moved to org.eclipse.cdt.utils.coff.parser.PEParser
+ * But this class is still provided for backward compatibility.
  */
-public class PEParser extends AbstractCExtension implements IBinaryParser {
-
-	/**
-	 * @see org.eclipse.cdt.core.model.IBinaryParser#getBinary(IFile)
-	 */
-	public IBinaryFile getBinary(IPath path) throws IOException {
-		if (path == null) {
-			path = new Path("");
-		}
-		try {
-			PE pe = new PE(path.toOSString());
-			pe.dispose();
-			return new PEBinaryFile(path);
-		} catch (IOException e) {
-		}
-		// Is it an Archive.
-		PEArchive ar = new PEArchive(path.toOSString());
-		ar.dispose();
-		return new PEBinaryArchive(path);
-	}
-
-	/**
-	 * @see org.eclipse.cdt.core.model.IBinaryParser#getFormat()
-	 */
-	public String getFormat() {
-		return "PE";
-	}
-
+public class PEParser extends org.eclipse.cdt.utils.coff.parser.PEParser {
 }

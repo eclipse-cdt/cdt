e63680bf70cd076a8f5c1a83d0fe1ca8f5466f49
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2004-01-06 20:45:35 +0000

    Fix for bug 49587: Unable to set breakpoints in the editors that extend CEditor.

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/actions/ManageBreakpointRulerActionDelegate.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/actions/ManageBreakpointRulerActionDelegate.java
@@ -6,12 +6,8 @@
 
 package org.eclipse.cdt.debug.internal.ui.actions;
 
-import org.eclipse.cdt.debug.internal.ui.editors.CDebugEditor;
-import org.eclipse.cdt.debug.internal.ui.editors.DisassemblyEditor;
-import org.eclipse.cdt.ui.CUIPlugin;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.texteditor.AbstractRulerActionDelegate;
 import org.eclipse.ui.texteditor.ITextEditor;
 
@@ -23,25 +19,6 @@ import org.eclipse.ui.texteditor.ITextEditor;
  */
 public class ManageBreakpointRulerActionDelegate extends AbstractRulerActionDelegate
 {
-	static final private String ASM_EDITOR_ID = "org.eclipse.cdt.ui.editor.asm.AsmEditor"; //$NON-NLS-1$
-
-	/**
-	 * @see IEditorActionDelegate#setActiveEditor(IAction, IEditorPart)
-	 */
-	public void setActiveEditor( IAction callerAction, IEditorPart targetEditor )
-	{
-		if ( targetEditor != null )
-		{
-			String id = targetEditor.getSite().getId();
-			if ( !id.equals( CDebugEditor.EDITOR_ID ) && 
-				 !id.equals( CUIPlugin.EDITOR_ID ) && 
-				 !id.equals( ASM_EDITOR_ID ) && 
-				 !id.equals( DisassemblyEditor.DISASSEMBLY_EDITOR_ID ) )
-				targetEditor = null;
-		}
-		super.setActiveEditor( callerAction, targetEditor );
-	}
-
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.texteditor.AbstractRulerActionDelegate#createAction(ITextEditor, IVerticalRulerInfo)
 	 */

fc5179e4770e34807a79d2a627410021a347567a
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2004-03-31 23:05:05 +0000

    The Registers view has been contibuted and moved to the Eclipse platform. To enable updating set the default value of the auto-refresh flag of the registers to true. Remove it when the "Refresh" and "Auto-Refresh" actions are contributed.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/CDebugCorePlugin.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/CDebugCorePlugin.java
@@ -218,6 +218,7 @@ public class CDebugCorePlugin extends Plugin
 	public void startup() throws CoreException
 	{
 		super.startup();
+		initDefaultPreferences();
 		resetBreakpointsInstallCount();
 		setSessionManager( new SessionManager() );
 	}
@@ -273,5 +274,11 @@ public class CDebugCorePlugin extends Plugin
 	public ICSourceLocation[] getCommonSourceLocations()
 	{
 		return SourceUtils.getCommonSourceLocationsFromMemento( CDebugCorePlugin.getDefault().getPluginPreferences().getString( ICDebugConstants.PREF_SOURCE_LOCATIONS ) );
+	}
+
+	private void initDefaultPreferences()
+	{
+		// for now
+		CDebugCorePlugin.getDefault().getPluginPreferences().setDefault( ICDebugConstants.PREF_REGISTERS_AUTO_REFRESH, true );		
 	}
 }

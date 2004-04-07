98b86d1d78b0778ef6954e26710e9bff093f4d73
Alain Magloire <alain@qnx.com>
2004-04-07 04:06:15 +0000

    typo in the string.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/cview/LibraryRefContainer.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/cview/LibraryRefContainer.java
@@ -68,7 +68,7 @@ public class LibraryRefContainer implements IAdaptable, IWorkbenchAdapter {
 	 * @see org.eclipse.ui.model.IWorkbenchAdapter#getLabel(java.lang.Object)
 	 */
 	public String getLabel(Object o) {
-		return "Library References"; //$NON-NLS-1
+		return "Libraries"; //$NON-NLS-1
 	}
 
 	/* (non-Javadoc)

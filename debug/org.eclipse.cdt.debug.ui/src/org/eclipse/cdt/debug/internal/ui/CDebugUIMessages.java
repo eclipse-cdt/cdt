41a3d5d9ff18cf8f6e73be91ead7e4aa7b35bb7b
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2004-04-20 16:02:49 +0000

    Changed copyright header.

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/CDebugUIMessages.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/CDebugUIMessages.java
@@ -1,19 +1,18 @@
-/*
- *(c) Copyright QNX Software Systems Ltd. 2002.
- * All Rights Reserved.
+/**********************************************************************
+ * Copyright (c) 2004 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
- */
-
+ * Contributors: 
+ * QNX Software Systems - Initial API and implementation
+ ***********************************************************************/
 package org.eclipse.cdt.debug.internal.ui;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-/**
- * Enter type comment.
- *
- * @since: Feb 23, 2004
- */
 public class CDebugUIMessages {
 
 	private static final String BUNDLE_NAME = "org.eclipse.cdt.debug.internal.ui.CDebugUIMessages"; //$NON-NLS-1$

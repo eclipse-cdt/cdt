83ff50fe2776892277a0a2087406e1d5b6eaedab
Alain Magloire <alain@qnx.com>
2004-05-07 18:21:22 +0000

    PR 57127. Pass the stream verbatim 	even if it has spaces. 	This what gdb expects.

--- debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIEnvironmentCD.java
+++ debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIEnvironmentCD.java
@@ -20,4 +20,17 @@ public class MIEnvironmentCD extends MICommand
 	public MIEnvironmentCD(String path) {
 		super("-environment-cd", new String[]{path}); //$NON-NLS-1$
 	}
+
+	/**
+	 * !@*^%^$( sigh ... gdb for this command does not make any interpretation
+	 * So we must past the command verbatim without any changes. 
+	 * (non-Javadoc)
+	 * @see org.eclipse.cdt.debug.mi.core.command.MICommand#parametersToString()
+	 */
+	protected String parametersToString() {
+		if (parameters != null && parameters.length == 1) {
+			return parameters[0];
+		}
+		return super.parametersToString();		
+	}
 }

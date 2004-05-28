cda816bba3c965312953d10246e825bda9534f17
cvs2svn <cdt-dev@eclipse.org>
2004-05-28 23:14:16 +0000

    This commit was manufactured by cvs2svn to create tag 'CDT_2_0_M9'.
    Sprout from master 2004-05-28 23:14:15 UTC Alain Magloire <alain@qnx.com> 'Protect agaist NPE'
    Delete:
        doc/org.eclipse.cdt.doc.isv/.project
        releng/org.eclipse.cdt.releng/.cvsignore
        releng/org.eclipse.cdt.releng/.project
        releng/org.eclipse.cdt.releng/build.sh
        releng/org.eclipse.cdt.releng/build.xml
        releng/org.eclipse.cdt.releng/cdt-map.in
        releng/org.eclipse.cdt.releng/index.xsl
        releng/org.eclipse.cdt.releng/maps/cdt.map
        releng/org.eclipse.cdt.releng/message.in
        releng/org.eclipse.cdt.releng/platform/.cvsignore
        releng/org.eclipse.cdt.releng/platform/build.properties
        releng/org.eclipse.cdt.releng/platform/customTargets.xml
        releng/org.eclipse.cdt.releng/plugin.xsl
        releng/org.eclipse.cdt.releng/sdk/build.properties
        releng/org.eclipse.cdt.releng/sdk/customTargets.xml
        releng/org.eclipse.cdt.releng/site.in
        releng/org.eclipse.cdt.releng/site.xsl
        releng/org.eclipse.cdt.releng/testing/build.properties
        releng/org.eclipse.cdt.releng/testing/customTargets.xml
        releng/org.eclipse.cdt.testing-feature/.project
        releng/org.eclipse.cdt.testing-feature/build.properties
        releng/org.eclipse.cdt.testing-feature/cpl-v10.html
        releng/org.eclipse.cdt.testing-feature/eclipse_update_120.jpg
        releng/org.eclipse.cdt.testing-feature/feature.properties
        releng/org.eclipse.cdt.testing-feature/feature.xml
        releng/org.eclipse.cdt.testing-feature/license.html
        releng/org.eclipse.cdt.testing/.project
        releng/org.eclipse.cdt.testing/about.html
        releng/org.eclipse.cdt.testing/about.ini
        releng/org.eclipse.cdt.testing/about.mappings
        releng/org.eclipse.cdt.testing/about.properties
        releng/org.eclipse.cdt.testing/build.properties
        releng/org.eclipse.cdt.testing/cpl-v10.html
        releng/org.eclipse.cdt.testing/eclipse32.gif
        releng/org.eclipse.cdt.testing/notice.html
        releng/org.eclipse.cdt.testing/plugin.properties
        releng/org.eclipse.cdt.testing/plugin.xml
        releng/org.eclipse.cdt.testing/test.xml

--- releng/org.eclipse.cdt.releng/build.sh
+++
@@ -1,9 +0,0 @@
-# Treat this is an example build script
-# Please adjust paths as necessary for your build machine
-
-export JAVA_HOME=/opt/java/j2sdk1.4.2_03
-export PATH=$JAVA_HOME/bin:$PATH
-
-cd `dirname $0`
-
-java -cp ../org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner $* 2>&1 | tee build.log

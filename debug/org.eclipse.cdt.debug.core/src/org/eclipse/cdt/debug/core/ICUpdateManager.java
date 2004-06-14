8ce08923feeca7ed8e7c127369b007bbea2e291e
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2004-06-14 01:31:35 +0000

    Changed the comments and copyright text.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/ICUpdateManager.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/ICUpdateManager.java
@@ -1,24 +1,27 @@
-/*
- *(c) Copyright QNX Software Systems Ltd. 2002.
- * All Rights Reserved.
+/**********************************************************************
+ * Copyright (c) 2004 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
- */
+ * Contributors: 
+ * QNX Software Systems - Initial API and implementation
+ ***********************************************************************/
 package org.eclipse.cdt.debug.core;
 
 import org.eclipse.debug.core.DebugException;
 
 /**
- * Enter type comment.
- * 
- * @since: Feb 10, 2003
+ * Indicates the support of update and auto-update functions.
  */
-public interface ICUpdateManager
-{
+public interface ICUpdateManager {
+
 	void setAutoModeEnabled( boolean enable );
 
 	boolean getAutoModeEnabled();
 
 	void update() throws DebugException;
-	
+
 	boolean canUpdate();
 }

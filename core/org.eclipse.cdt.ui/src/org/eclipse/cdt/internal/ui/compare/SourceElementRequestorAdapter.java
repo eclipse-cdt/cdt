7cee0b70c6a97325f11ced02716e0d1cac64547e
John Camelon <jcamelon@ca.ibm.com>
2004-06-24 21:29:20 +0000

    Applied yet more copyrights for Dave Daoust.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/compare/SourceElementRequestorAdapter.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/compare/SourceElementRequestorAdapter.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
  * 
+ * Copyright (c) 2002, 2004 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v0.5 
+ * are made available under the terms of the Common Public License v1.0 
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
- *     QnX Software Systems - initial implementation base on code from rational/IBM
+ *     QNX Software Systems - initial implementation base on code from rational/IBM
  ******************************************************************************/
 
 package org.eclipse.cdt.internal.ui.compare;

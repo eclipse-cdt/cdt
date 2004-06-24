da3ce3ef51a50bbb2b1c41e50c4d35c74287d861
John Camelon <jcamelon@ca.ibm.com>
2004-06-24 19:29:42 +0000

    Applied copyright notices for David Daoust on core and ui packages.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/CUIPlugin.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/CUIPlugin.java
@@ -1,16 +1,16 @@
-package org.eclipse.cdt.ui;
-
-/***********************************************************************
- * Copyright (c) 2003 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v0.5 
+/*******************************************************************************
+ * Copyright (c) 2003, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v05.html
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
- * QNX Software Systems - Initial API and implementation
- * IBM Corp. - Rational Software 
-***********************************************************************/
+ *     QNX Software Systems - Initial API and implementation
+ *     IBM Corp. - Rational Software
+ *******************************************************************************/
+package org.eclipse.cdt.ui;
+
 
 import java.text.MessageFormat;
 import java.util.ArrayList;

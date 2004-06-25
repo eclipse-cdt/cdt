ca2e5ce5aea2f37cba0901aa8e16c3cfd8f0f971
David Inglis <dinglis@qnx.com>
2004-06-25 14:56:35 +0000

    update copyright

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/cview/DefaultAction.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/cview/DefaultAction.java
@@ -1,9 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     QNX Software Systems - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.internal.ui.cview;
 
-/*
- * (c) Copyright QNX Software Systems Ltd. 2002.
- * All Rights Reserved.
- */
 
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/ICPathContainerPageExtension.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/ICPathContainerPageExtension.java
@@ -1,11 +1,13 @@
-/*
- * Created on Apr 14, 2004
- *
- * Copyright (c) 2002,2003 QNX Software Systems Ltd.
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
  * 
- * Contributors: 
- * QNX Software Systems - Initial API and implementation
-***********************************************************************/
+ * Contributors:
+ *     QNX Software Systems - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.ui.wizards;
 
 import org.eclipse.cdt.core.model.IPathEntry;

c5102cafc424cef4cbe5e89f4874456a35fc61ed
Doug Schaefer <doug.schaefer@windriver.com>
2004-07-16 18:12:17 +0000

    Updated the build scripts to clean things up a bit and to get rid of the nightly build update site.

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -3,7 +3,8 @@
 
 export JAVA_HOME=/opt/java/j2sdk1.4.2_03
 export PATH=$JAVA_HOME/bin:$PATH
+export ECLIPSE_HOME=~/eclipse/3.0/eclipse
 
 cd `dirname $0`
 
-java -cp ../org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner $* 2>&1 | tee build.log
+java -cp $ECLIPSE_HOME/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner $* 2>&1 | tee build.log

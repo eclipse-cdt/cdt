022ed4b8ebe0b258992e78bbc0e2ff2b381d32fd
David Inglis <dinglis@qnx.com>
2004-08-23 20:09:09 +0000

    - remove unused i/f - fix container page i/f (use new IPathEntryContainerPage deprecated ICPathContainerPage)

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/IPathEntryContainerPage.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/IPathEntryContainerPage.java
@@ -10,6 +10,7 @@
 package org.eclipse.cdt.ui.wizards;
 
 import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.core.model.IContainerEntry;
 import org.eclipse.cdt.core.model.IPathEntry;
 import org.eclipse.jface.wizard.IWizardPage;
 
@@ -26,26 +27,24 @@ import org.eclipse.jface.wizard.IWizardPage;
  * Clients implementing this interface may subclass from 
  * <code>org.eclipse.jface.wizard.WizardPage</code>.
  * </p>
- * 
- * @deprecated - use IPathEntryContainerPage 
  */
 
-public interface ICPathContainerPage extends IWizardPage {
+public interface IPathEntryContainerPage extends IWizardPage {
 
 	/**
 	 * Method <code>initialize()</code> is called before  <code>ICPathContainerPage.setSelection</code>
 	 * to give additional information about the context the path container entry is configured in. This information
 	 * only reflects the underlying dialogs current selection state. The user still can make changes after the
-	 * the classpath container pages has been closed or decide to cancel the operation.
-	 * @param project The project the new or modified entry is added to. The project does not have to exist. 
+	 * the path container pages has been closed or decide to cancel the operation.
+	 * @param project - The project the new or modified entry is added to. The project does not have to exist. 
 	 * Project can be <code>null</code>.
-	 * @param currentEntries The class path entries currently selected to be set as the projects classpath. This can also
+	 * @param currentEntries - The path entries currently selected to be set as the projects path. This can also
 	 * include the entry to be edited.
 	 */
 	public void initialize(ICProject project, IPathEntry[] currentEntries);
 
 	/**
-	 * Called when the classpath container wizard is closed by selecting 
+	 * Called when the path container wizard is closed by selecting 
 	 * the finish button. Implementers typically override this method to 
 	 * store the page result (new/changed path entry returned in 
 	 * getSelection) into its model.
@@ -54,24 +53,20 @@ public interface ICPathContainerPage extends IWizardPage {
 	 * <code>true</code>, the wizard will close.
 	 */
 	public boolean finish();
-	
-	
+
 	/**
-	 * Method {@link #getNewContainers()} is called instead of {@link IClasspathContainerPage#getSelection() }
-	 * to get the the newly added containers. {@link IClasspathContainerPage#getSelection() } is still used
-	 * to get the edited elements.
-	 * @return the classpath entries created on the page. All returned entries must be {@link
-	 * IClasspathEntry#CPE_CONTAINER}
+	 * Method {@link #getNewContainers()} is called to get the the newly added containers. 
+	 * @return the path entries created on this page. 
 	 */
-	public IPathEntry[] getContainerEntries();
-	
+	public IContainerEntry[] getNewContainers();
+
 	/**
 	 * Sets the path container entry to be edited or <code>null</code> 
 	 * if a new entry should be created.
 	 * 
-	 * @param containerEntry the classpath entry to edit or <code>null</code>.
-	 * If unequals <code>null</code> then the classpath entry must be of
-	 * kind <code>IClasspathEntry.CPE_CONTAINER</code>
+	 * @param containerEntry the path entry to edit or <code>null</code>.
+	 * If unequals <code>null</code> then the path entry must be of
+	 * kind <code>IPathEntry.CDT_CONTAINER</code>
 	 */
-	public void setSelection(IPathEntry containerEntry);
+	public void setSelection(IContainerEntry containerEntry);
 }

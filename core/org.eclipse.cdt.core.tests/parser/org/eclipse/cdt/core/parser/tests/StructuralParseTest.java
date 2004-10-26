e58b686575c4b0803c69ad4cd57e627b125a344a
Andrew Niefer <aniefer@ca.ibm.com>
2004-10-26 15:50:51 +0000

    77010: IASTClassSpecifier#getDeclarations() returns incomplete information

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/StructuralParseTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/StructuralParseTest.java
@@ -13,6 +13,8 @@
  */
 package org.eclipse.cdt.core.parser.tests;
 
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Iterator;
 
 import junit.framework.TestCase;
@@ -25,9 +27,12 @@ import org.eclipse.cdt.core.parser.ParserFactoryError;
 import org.eclipse.cdt.core.parser.ParserLanguage;
 import org.eclipse.cdt.core.parser.ParserMode;
 import org.eclipse.cdt.core.parser.ScannerInfo;
+import org.eclipse.cdt.core.parser.ast.IASTAbstractTypeSpecifierDeclaration;
+import org.eclipse.cdt.core.parser.ast.IASTClassSpecifier;
 import org.eclipse.cdt.core.parser.ast.IASTCompilationUnit;
 import org.eclipse.cdt.core.parser.ast.IASTFunction;
 import org.eclipse.cdt.core.parser.ast.IASTLinkageSpecification;
+import org.eclipse.cdt.core.parser.ast.IASTMethod;
 import org.eclipse.cdt.core.parser.ast.IASTTemplateDeclaration;
 import org.eclipse.cdt.core.parser.ast.IASTVariable;
 import org.eclipse.cdt.internal.core.parser.ParserException;
@@ -93,4 +98,44 @@ public class StructuralParseTest extends TestCase {
     	IASTFunction foo = (IASTFunction) template.getOwnedDeclaration();
     	assertEquals( foo.getName(), "foo" ); //$NON-NLS-1$
 	}
+    
+    public void testBug77010() throws Exception
+    {
+        Writer writer = new StringWriter();
+        writer.write(" struct Example{                                \n"); //$NON-NLS-1$
+        writer.write("    int                *deref();                \n"); //$NON-NLS-1$
+        writer.write("    int const          *deref() const;          \n"); //$NON-NLS-1$
+        writer.write("    int       volatile *deref()       volatile; \n"); //$NON-NLS-1$
+        writer.write("    int const volatile *deref() const volatile; \n"); //$NON-NLS-1$
+        writer.write(" };                                             \n"); //$NON-NLS-1$
+        
+        Iterator i = parse( writer.toString() ).getDeclarations();
+        IASTClassSpecifier Ex = (IASTClassSpecifier) ((IASTAbstractTypeSpecifierDeclaration)i.next()).getTypeSpecifier();
+        
+        i = Ex.getDeclarations();
+        IASTMethod deref = (IASTMethod) i.next();
+        assertFalse( deref.getReturnType().isConst() );
+        assertFalse( deref.getReturnType().isVolatile() );
+        assertFalse( deref.isConst() );
+        assertFalse( deref.isVolatile() );
+        
+        deref = (IASTMethod) i.next();
+        assertTrue( deref.getReturnType().isConst() );
+        assertFalse( deref.getReturnType().isVolatile() );
+        assertTrue( deref.isConst() );
+        assertFalse( deref.isVolatile() );
+        
+        deref = (IASTMethod) i.next();
+        assertFalse( deref.getReturnType().isConst() );
+        assertTrue( deref.getReturnType().isVolatile() );
+        assertFalse( deref.isConst() );
+        assertTrue( deref.isVolatile() );
+        
+        deref = (IASTMethod) i.next();
+        assertTrue( deref.getReturnType().isConst() );
+        assertTrue( deref.getReturnType().isVolatile() );
+        assertTrue( deref.isConst() );
+        assertTrue( deref.isVolatile() );
+        assertFalse( i.hasNext() );
+    }
 }

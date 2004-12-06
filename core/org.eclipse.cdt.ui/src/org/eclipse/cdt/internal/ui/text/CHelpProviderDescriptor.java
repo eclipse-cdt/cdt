f75ccc0e6f0540fd12a852ff25743e46fe58b563
Alain Magloire <alain@qnx.com>
2004-12-06 23:07:39 +0000

    2004-12-06 Alain Magloire 	Fix for 79865 	* src/org/eclipse/cdt/internal/ui/text/CHelpProviderDescriptor.java

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CHelpProviderDescriptor.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CHelpProviderDescriptor.java
@@ -128,9 +128,9 @@ public class CHelpProviderDescriptor {
 	}
 	
 	public CHelpBookDescriptor [] getCHelpBookDescriptors(Element projectElement){
-		if(fHelpBookDescriptors == null || projectElement != null){
+		if (fHelpBookDescriptors == null || projectElement != null){
 			ICHelpProvider provider = getCHelpProvider();
-			if(provider != null){
+			if (provider != null && fProject != null) {
 				ICHelpBook books[] = provider.getCHelpBooks();
 				if(books != null){
 					List descriptorList = new ArrayList();

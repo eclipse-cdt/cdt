495d10915b8febac355ff90933b07484656b0bff
Andrew Niefer <aniefer@ca.ibm.com>
2004-12-15 17:57:28 +0000

    This someone got left out of the last commit

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/ITypeContainer.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+/*
+ * Created on Dec 13, 2004
+ */
+package org.eclipse.cdt.internal.core.dom.parser;
+
+import org.eclipse.cdt.core.dom.ast.IType;
+
+/**
+ * @author aniefer
+ */
+public interface ITypeContainer extends IType{
+    IType getType();
+}

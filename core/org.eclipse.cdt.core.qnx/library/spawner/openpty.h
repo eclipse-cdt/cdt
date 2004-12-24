e20f39193f9093965a715ff8a23da02fe5a670ae
David Inglis <dinglis@qnx.com>
2004-12-24 17:26:22 +0000

    fixed qnx spawner

---
+++ core/org.eclipse.cdt.core.qnx/library/spawner/openpty.h
@@ -0,0 +1,10 @@
+/*
+ * (c) Copyright QNX Software Systems Ltd. 2002.
+ * All Rights Reserved.
+ *
+ */
+#ifndef _OPENPTY_H
+#define _OPENPTY_H
+int ptym_open (char *pts_name);
+int ptys_open (int fdm, char * pts_name);
+#endif

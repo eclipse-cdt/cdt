db7c82e88bf75701f670ae246eac9336cac3865a
Alain Magloire <alain@qnx.com>
2005-02-15 18:07:45 +0000

    2005-02-15 Alain Magloire 	Fix PR 82856 	* src/org/eclipse/cdt/internal/ui/editor/ExternalSearchAnnotationModel.java 	* src/org/eclipse/cdt/internal/ui/editor/ExternalSearchDocumentProvider.java 	* src/org/eclipse/cdt/internal/ui/editor/ExternalSearchEditor.java 	* src/org/eclipse/cdt/internal/ui/editor/ExternalSearchFile.java 	* src/org/eclipse/cdt/internal/ui/preferences/CSearchPreferencePage.java

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/ExternalSearchFile.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/ExternalSearchFile.java
@@ -1,12 +1,12 @@
 /**********************************************************************
 Copyright (c) 2002, 2004 IBM Rational Software and others.
-All rights reserved.   This program and the accompanying materials
+All rights reserved.  This program and the accompanying materials
 are made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
 http://www.eclipse.org/legal/cpl-v10.html
- 
+
 Contributors:
-    IBM Rational Software - Initial Contribution
+ IBM Rational Software - Initial Contribution
 **********************************************************************/
 
 package org.eclipse.cdt.internal.ui.editor;

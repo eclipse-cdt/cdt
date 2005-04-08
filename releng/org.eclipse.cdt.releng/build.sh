1b9b0cdcac4f2913ed786e8373d888e8e6220b9c
Doug Schaefer <doug.schaefer@windriver.com>
2005-04-08 18:23:12 +0000

    Still trying to get M6 building.

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -1,15 +1,9 @@
-# JAVA_HOME must be set to the root of your JRE directory (JDK if you want to do javadoc)
-# ECLIPSE_HOME must be set to the root of you Eclipse install that you want to do the build with
-#    right now, this must be a 3.1.0 install (for PDE build scripts)
-
-die() {
-	echo $*
-	exit 1
-}
-
-#[ -n "$ECLIPSE_HOME" ] || die ECLIPSE_HOME not set
-ECLIPSE_HOME=../org.eclipse.releng.basebuilder
-
 cd `dirname $0`
 
-java -cp $ECLIPSE_HOME/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner $* 2>&1 | tee build.log
+mkdir -p tools
+cd tools
+cvs -d:pserver:anonymous@dev.eclipse.org:/home/eclipse checkout org.eclipse.releng.basebuilder
+cd ..
+
+java -cp tools/org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main \
+	-ws gtk -application org.eclipse.ant.core.antRunner $* 2>&1 | tee build.log

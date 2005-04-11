db68d203486f68f0841cc0f1309813eec432673a
David Inglis <dinglis@qnx.com>
2005-04-11 00:56:03 +0000

    added some useful methods for creating UI controls

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/dialogs/AbstractCOptionPage.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/dialogs/AbstractCOptionPage.java
@@ -10,10 +10,15 @@
  *******************************************************************************/
 package org.eclipse.cdt.ui.dialogs;
 
+import org.eclipse.cdt.internal.ui.util.SWTUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.DialogPage;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 public abstract class AbstractCOptionPage extends DialogPage implements ICOptionPage {
@@ -21,6 +26,33 @@ public abstract class AbstractCOptionPage extends DialogPage implements ICOption
 	private boolean bIsValid = true;
 	private ICOptionContainer fContainer;
 	
+
+	protected Button createPushButton(Composite parent, String label, Image image) {
+		Button button = new Button(parent, SWT.PUSH);
+		button.setFont(parent.getFont());
+		if (image != null) {
+			button.setImage(image);
+		}
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);
+		SWTUtil.setButtonDimensionHint(button);
+		return button;
+	}
+
+	protected Button createRadioButton(Composite parent, String label) {
+		Button button = new Button(parent, SWT.RADIO);
+		button.setFont(parent.getFont());
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);
+		SWTUtil.setButtonDimensionHint(button);
+		return button;
+	}
 
 	protected AbstractCOptionPage() {
 		super();

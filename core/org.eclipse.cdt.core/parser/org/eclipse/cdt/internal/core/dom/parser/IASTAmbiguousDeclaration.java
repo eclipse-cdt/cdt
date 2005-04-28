f74fc1c0ac42a60241445319d134fcd801f2089e
John Camelon <jcamelon@ca.ibm.com>
2005-04-28 13:45:05 +0000

    Add Declaration Ambiguity interface.

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/IASTAmbiguousDeclaration.java
@@ -0,0 +1,22 @@
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.cdt.internal.core.dom.parser;
+
+import org.eclipse.cdt.core.dom.ast.ASTNodeProperty;
+import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
+
+public interface IASTAmbiguousDeclaration extends IASTDeclaration {
+
+    public static final ASTNodeProperty SUBDECLARATION = new ASTNodeProperty( "IASTAmbiguousDeclaration.SUBDECLARATION"); //$NON-NLS-1$
+    public void addDeclaration( IASTDeclaration d );
+    public IASTDeclaration [] getDeclarations();
+
+}

3784acdac1533f784f466e9205516ef11ff2b07c
David Inglis <dinglis@qnx.com>
2005-05-24 14:15:42 +0000

    fixed bug  https://bugs.eclipse.org/bugs/show_bug.cgi?id=76821

--- build/org.eclipse.cdt.make.ui/src/org/eclipse/cdt/make/ui/dialogs/SettingsBlock.java
+++ build/org.eclipse.cdt.make.ui/src/org/eclipse/cdt/make/ui/dialogs/SettingsBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 QNX Software Systems and others. All rights
+ * Copyright (c) 2000, 2005 QNX Software Systems and others. All rights
  * reserved. This program and the accompanying materials are made available
  * under the terms of the Common Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/cpl-v10.html
@@ -517,15 +517,25 @@ public class SettingsBlock extends AbstractCOptionPage {
 			argumentVariablesButton.setEnabled(true);
 			stopOnErrorButton.setEnabled(false);
 		}
+		
 		defButton.setSelection(info.isDefaultBuildCmd());
+		
 		autoButton.setSelection(info.isAutoBuildEnable());
+		autoVariableButton.setEnabled(info.isAutoBuildEnable());
+		targetAuto.setEnabled(info.isAutoBuildEnable());
 		targetAuto.setText(info.getAutoBuildTarget());
 		incrButton.setSelection(info.isIncrementalBuildEnabled());
+		incrVariableButton.setEnabled(info.isIncrementalBuildEnabled());
 		targetIncr.setText(info.getIncrementalBuildTarget());
+		targetIncr.setEnabled(info.isIncrementalBuildEnabled());
 		fullButton.setSelection(info.isFullBuildEnabled());
+		fullVariableButton.setEnabled(info.isFullBuildEnabled());
 		targetFull.setText(info.getFullBuildTarget());
+		targetFull.setEnabled(info.isFullBuildEnabled());
 		cleanButton.setSelection(info.isCleanBuildEnabled());
+		cleanVariableButton.setEnabled(info.isCleanBuildEnabled());
 		targetClean.setText(info.getCleanBuildTarget());
+		targetClean.setEnabled(info.isCleanBuildEnabled());
 	}
 
 	boolean isStopOnError() {

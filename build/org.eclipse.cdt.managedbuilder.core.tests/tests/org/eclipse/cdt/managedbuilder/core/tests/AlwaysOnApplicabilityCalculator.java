a3ca580f440dba885be6308fa972bc32c6caeff5
Leo Treggiari <leo.treggiari@intel.com>
2005-05-26 21:18:54 +0000

    Update tests for IOptionApplicability callback

---
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/AlwaysOnApplicabilityCalculator.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Texas Instruments Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Texas Instruments Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.core.tests;
+
+import org.eclipse.cdt.managedbuilder.core.IOptionApplicability;
+import org.eclipse.cdt.managedbuilder.core.ITool;
+
+/**
+ * 
+ */
+public class AlwaysOnApplicabilityCalculator implements IOptionApplicability {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionUsedInCommandLine()
+	 */
+	public boolean isOptionUsedInCommandLine(ITool tool) {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionVisible()
+	 */
+	public boolean isOptionVisible(ITool tool) {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionEnabled()
+	 */
+	public boolean isOptionEnabled(ITool tool) {
+		return false;
+	}
+
+}

7475da7bda2a778663b568bf6c463839cbbef581
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2005-06-20 19:43:42 +0000

    Bug 93855: Changing values in detailed pane doesn't work the first time after the view is loaded.

---
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/DetailsViewerConfiguration.java
@@ -0,0 +1,20 @@
+/**********************************************************************
+ * Copyright (c) 2004 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ * QNX Software Systems - Initial API and implementation
+ ***********************************************************************/ 
+package org.eclipse.cdt.debug.internal.ui; 
+
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
+ 
+/**
+ * The source viewer configuration used in the Details pane of the variables, 
+ * registers and expressions views.
+ */
+public class DetailsViewerConfiguration extends SourceViewerConfiguration {
+}

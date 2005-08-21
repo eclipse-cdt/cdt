ef4e5c166a699f92ca2662430582a05c90e3f090
Alain Magloire <alain@qnx.com>
2005-08-21 02:48:27 +0000

    2005-08-20 Alain Magloire 	Fix PR 86802 106911 87497 : Assembly editor fixes. 	* src/org/eclipse/cdt/internal/ui/editor/asm/ASMEditorActionContributor.java 	* src/org/eclipse/cdt/internal/ui/editor/asm/AsmSourceViewerConfiguration.java 	* src/org/eclipse/cdt/internal/ui/editor/asm/AsmTextEditor.java 	* plugin.xml

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/asm/ASMEditorActionContributor.java
@@ -0,0 +1,9 @@
+package org.eclipse.cdt.internal.ui.editor.asm;
+
+import org.eclipse.ui.editors.text.TextEditorActionContributor;
+
+public class ASMEditorActionContributor extends
+		TextEditorActionContributor {
+
+		// nothing for now
+}

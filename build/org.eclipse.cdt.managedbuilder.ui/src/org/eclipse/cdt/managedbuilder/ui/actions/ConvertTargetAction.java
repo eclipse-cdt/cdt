9439b53c3b9d269b001ee6413f0da72b11847a0e
Leo Treggiari <leo.treggiari@intel.com>
2005-09-20 03:37:24 +0000

    Add content menu item for invoking project converters

--- build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/actions/ConvertTargetAction.java
+++ build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/actions/ConvertTargetAction.java
@@ -10,10 +10,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.ui.actions;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.cdt.managedbuilder.core.IConvertManagedBuildObject;
 import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
 import org.eclipse.cdt.managedbuilder.core.IManagedProject;
 import org.eclipse.cdt.managedbuilder.core.IProjectType;
@@ -22,11 +18,6 @@ import org.eclipse.cdt.managedbuilder.internal.ui.ManagedBuilderUIMessages;
 import org.eclipse.cdt.managedbuilder.internal.ui.ManagedBuilderUIPlugin;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -40,93 +31,29 @@ public class ConvertTargetAction
 	extends ActionDelegate
 	implements IObjectActionDelegate {
 
-	private String  converterId = null;
-	private String	fromId = null;
-	private String	toId = null;
-	private IConvertManagedBuildObject  convertBuildObject = null;
 	private IProject selectedProject = null;
+	
+	public static final String PREFIX = "ProjectConvert";	//$NON-NLS-1$
+	public static final String PROJECT_CONVERTER_DIALOG = PREFIX + ".title";	//$NON-NLS-1$
 	
 	public static void initStartup() {
 		return;
 	}
 	
-	public void initConvertAction(IAction action) {
-		convertBuildObject = null;
-		String id = action.getId();
-		try {
-			
-			// Get the Converter Extension Point
-			IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
-					.getExtensionPoint("org.eclipse.cdt.managedbuilder.core",	//$NON-NLS-1$
-							"projectConverter");	//$NON-NLS-1$
-			if (extensionPoint != null) {
-				// Get the Converter Extensions
-				IExtension[] extensions = extensionPoint.getExtensions();
-				List list = new ArrayList(extensions.length);
-				for (int i = 0; i < extensions.length; i++) {
-					
-					// Get the configuration elements for each extension
-					IConfigurationElement[] configElements = extensions[i]
-							.getConfigurationElements();
-
-					for (int j = 0; j < configElements.length; j++) {
-						IConfigurationElement element = configElements[j];
-
-						if (element.getName().equals("converter")) {	//$NON-NLS-1$
-							// Get the converter 'id'
-							String tmpConverterID = element.getAttribute("id"); //$NON-NLS-1$
-							// If the converter 'id' and action 'id' are same.
-							if (id.equals(tmpConverterID)) {
-
-								convertBuildObject = (IConvertManagedBuildObject) element
-									.createExecutableExtension("class"); //$NON-NLS-1$
-								fromId = element.getAttribute("fromId"); //$NON-NLS-1$
-								toId = element.getAttribute("toId"); //$NON-NLS-1$
-								return;
-							}
-						}
-					}
-				}
-			}
-		} catch (CoreException e) {
-		}
-	}
-	
 	public void selectionChanged(IAction action, ISelection selection) {
-		initConvertAction(action);
 		if (selection instanceof IStructuredSelection) {
 			IStructuredSelection sel = (IStructuredSelection) selection;
 			Object obj = sel.getFirstElement();
 			if (obj instanceof IProject) {
-				 IProject project = (IProject)obj;
-				 
+				 IProject project = (IProject)obj;				 
 				 // Save the selected project.
 				 setSelectedProject(project);
-				 
-				 // If the project does not have managed build nature then disable the action.
-				 try {
-					if(!project.hasNature("org.eclipse.cdt.managedbuilder.core.managedBuildNature")) {	//$NON-NLS-1$
-						 action.setEnabled(false);
-						 return;
-					 }
-				} catch (CoreException e) {
-//					 e.printStackTrace();
-				}
-					 
-				// Get the projectType of the project.
-				IProjectType projectType = getProjectType(project);
-
-				// Check whether the Converter can convert the selected project.
-				 if( isProjectConvertable(projectType))
-					 action.setEnabled(true);
-				 else
-					 action.setEnabled(false);
-			} else {
-				action.setEnabled(false);
+				 return;
 			}
 		}
+		setSelectedProject(null);
 	}
-	
+
 	private IProjectType getProjectType(IProject project) {
 		IProjectType projectType = null;
 
@@ -134,76 +61,48 @@ public class ConvertTargetAction
 		IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project);
 		if (info != null) {
 			IManagedProject managedProject = info.getManagedProject();
-			projectType = managedProject.getProjectType();
+			if ( managedProject != null )
+				projectType = managedProject.getProjectType();
 		}
 		return projectType;
 	}
 	
-	
-	private boolean isProjectConvertable(IProjectType projectType) {
-		
-		IProjectType  tmpProjectType = projectType;
-		
-//		Check whether the converter can convert the given projectType
-		if(fromId == null)
-			return false;
 
-		while( tmpProjectType != null) {
-			String id = tmpProjectType.getId();
-			
-			if (fromId.equals(id))
-				return true;
-			else
-				tmpProjectType = tmpProjectType.getSuperClass();
+	public void run(IAction action) {
+		Shell shell = ManagedBuilderUIPlugin.getDefault().getShell();
+		
+		// Check whether the converters available for the selected project
+		// If there are no converters display error dialog otherwise display converters list
+		
+		if( ManagedBuildManager.hasTargetConversionElements(getProjectType(getSelectedProject())) == true ) {			
+			handleConvertTargetAction();
+		} else {
+			MessageDialog.openError(shell,ManagedBuilderUIMessages.getResourceString("ProjectConvert.noConverterErrordialog.title"), //$NON-NLS-1$
+			        ManagedBuilderUIMessages.getFormattedString("ProjectConvert.noConverterErrordialog.message", new String[] {getSelectedProject().getName()}) );  //$NON-NLS-1$
 		}
-		return false;
 	}
 
-
-	
-	public void run(IAction action) {
-		if( convertBuildObject != null) {
-//			 Get the confirmation from user before converting the selected project
-			Shell shell = ManagedBuilderUIPlugin.getDefault().getShell();
-			boolean shouldConvert = MessageDialog.openQuestion(shell,
-			        ManagedBuilderUIMessages.getResourceString("ProjectConvert.confirmdialog.title"), //$NON-NLS-1$
-			        ManagedBuilderUIMessages.getFormattedString("ProjectConvert.confirmdialog.message",  //$NON-NLS-1$
-			                new String[] {getSelectedProject().getName()}));
-			
-			if (shouldConvert) {
-				convertBuildObject.convert( getProjectType(getSelectedProject()), getFromId(), getToId(), true);
+	private void handleConvertTargetAction() {
+		Shell shell = ManagedBuilderUIPlugin.getDefault().getShell();
+		
+		String title = ManagedBuilderUIMessages.getFormattedString(PROJECT_CONVERTER_DIALOG, new String(getSelectedProject().getName())); 
+		ConvertTargetDialog dialog = new ConvertTargetDialog(shell, getSelectedProject(), title);
+		if ( dialog.open() == ConvertTargetDialog.OK ) {
+			if ( ConvertTargetDialog.isConversionSuccessful() == false) {									
+				MessageDialog.openError(
+								shell,
+								ManagedBuilderUIMessages
+										.getResourceString("ProjectConvert.conversionErrordialog.title"), //$NON-NLS-1$
+								ManagedBuilderUIMessages
+										.getFormattedString(
+												"ProjectConvert.conversionErrordialog.message", new String[] { getSelectedProject().getName() })); //$NON-NLS-1$
 			}
 		}
-		
+		return;
 	}
-
+		
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private IConvertManagedBuildObject getConvertBuildObject() {
-		return convertBuildObject;
-	}
-
-	private void setConvertBuildObject(IConvertManagedBuildObject convertBuildObject) {
-		this.convertBuildObject = convertBuildObject;
-	}
-
-	private String getConverterId() {
-		return converterId;
-	}
-
-	private void setConverterId(String converterId) {
-		this.converterId = converterId;
-	}
-
-	private String getFromId() {
-		return fromId;
-	}
-
-	private void setFromId(String fromId) {
-		this.fromId = fromId;
+		// TODO Auto-generated method stub	
 	}
 
 	/**
@@ -218,19 +117,5 @@ public class ConvertTargetAction
 	 */
 	private void setSelectedProject(IProject selectedProject) {
 		this.selectedProject = selectedProject;
-	}
-
-	/**
-	 * @return Returns the toId.
-	 */
-	private String getToId() {
-		return toId;
-	}
-
-	/**
-	 * @param toId The toId to set.
-	 */
-	private void setToId(String toId) {
-		this.toId = toId;
 	}
 }

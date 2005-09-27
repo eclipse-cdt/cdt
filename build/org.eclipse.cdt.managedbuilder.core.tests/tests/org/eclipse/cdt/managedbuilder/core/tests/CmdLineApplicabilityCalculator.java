b53866dccfc8e7e5e02fbac7f5f1685f9514a328
cvs2svn <cdt-dev@eclipse.org>
2005-09-27 23:35:15 +0000

    This commit was manufactured by cvs2svn to create branch 'cdt_3_0'.
    Cherrypick from master 2005-09-27 23:35:14 UTC Leo Treggiari <leo.treggiari@intel.com> 'Add test for InputType, assignToOption attribute':
        build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/TestATO/TestATO.zip
        build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/CmdLineApplicabilityCalculator.java

---
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/CmdLineApplicabilityCalculator.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Texas Instruments Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Texas Instruments Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.core.tests;
+
+import org.eclipse.cdt.managedbuilder.core.IHoldsOptions;
+import org.eclipse.cdt.managedbuilder.core.IBuildObject;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.IOptionApplicability;
+
+/**
+ * 
+ */
+public class CmdLineApplicabilityCalculator implements IOptionApplicability {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionUsedInCommandLine()
+	 */
+	public boolean isOptionUsedInCommandLine(
+			IBuildObject config,
+			IHoldsOptions holder,
+			IOption option) {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionVisible()
+	 */
+	public boolean isOptionVisible(
+			IBuildObject config,
+			IHoldsOptions holder,
+			IOption option) {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IOptionApplicability#isOptionEnabled()
+	 */
+	public boolean isOptionEnabled(
+			IBuildObject config,
+			IHoldsOptions holder,
+			IOption option) {
+		return false;
+	}
+
+}

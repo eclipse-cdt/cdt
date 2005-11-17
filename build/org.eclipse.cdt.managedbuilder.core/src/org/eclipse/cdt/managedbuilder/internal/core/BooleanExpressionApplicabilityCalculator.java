afb233e645e4bc3caf4065205467f2a624854b61
cvs2svn <cdt-dev@eclipse.org>
2005-11-17 14:16:59 +0000

    This commit was manufactured by cvs2svn to create branch 'cdt_3_0'.
    Cherrypick from master 2005-11-17 14:16:58 UTC Mikhail Sennikovsky <mikhail.sennikovskiy@gmail.com> 'The initial check-in for the option enablement expressions':
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/core/BooleanExpressionApplicabilityCalculator.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/AndExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/CheckOptionExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/CheckStringExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/CompositeExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/FalseExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/IBooleanExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/NotExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/OptionEnablementExpression.java
        build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/OrExpression.java

---
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/core/BooleanExpressionApplicabilityCalculator.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Intel Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Intel Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.internal.core;
+
+import org.eclipse.cdt.managedbuilder.core.IBuildObject;
+import org.eclipse.cdt.managedbuilder.core.IHoldsOptions;
+import org.eclipse.cdt.managedbuilder.core.IManagedConfigElement;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.IOptionApplicability;
+import org.eclipse.cdt.managedbuilder.internal.enablement.OptionEnablementExpression;
+
+public class BooleanExpressionApplicabilityCalculator implements IOptionApplicability {
+	private OptionEnablementExpression fExpressions[];
+
+	public BooleanExpressionApplicabilityCalculator(IManagedConfigElement optionElement){
+		this(optionElement.getChildren(OptionEnablementExpression.NAME));
+	}
+
+	public BooleanExpressionApplicabilityCalculator(IManagedConfigElement enablementElements[]){
+		fExpressions = new OptionEnablementExpression[enablementElements.length];
+		
+		for(int i = 0; i < enablementElements.length; i++){
+			fExpressions[i] = new OptionEnablementExpression(enablementElements[i]);
+		}
+	}
+	
+	public boolean isOptionVisible(IBuildObject configuration, 
+            IHoldsOptions holder, 
+            IOption option){
+		return evaluate(configuration, holder, option, OptionEnablementExpression.FLAG_UI_VISIBILITY);
+	}
+
+	public boolean isOptionEnabled(IBuildObject configuration, 
+            IHoldsOptions holder, 
+            IOption option){
+		return evaluate(configuration, holder, option, OptionEnablementExpression.FLAG_UI_ENABLEMENT);
+	}
+
+	public boolean isOptionUsedInCommandLine(IBuildObject configuration, 
+            IHoldsOptions holder, 
+            IOption option){
+		return evaluate(configuration, holder, option, OptionEnablementExpression.FLAG_CMD_USAGE);
+	}
+
+	public boolean evaluate(IBuildObject configuration,
+			IHoldsOptions holder, IOption option, int flags){
+		for(int i = 0; i < fExpressions.length; i++){
+			if(!fExpressions[i].evaluate(configuration, holder, option, flags))
+				return false;
+		}
+		return true;
+	}
+}
---
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/enablement/IBooleanExpression.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Intel Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Intel Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.internal.enablement;
+
+import org.eclipse.cdt.managedbuilder.core.IBuildObject;
+import org.eclipse.cdt.managedbuilder.core.IHoldsOptions;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+
+public interface IBooleanExpression {
+	public boolean evaluate(IBuildObject configuration, 
+            IHoldsOptions holder, 
+            IOption option);
+}

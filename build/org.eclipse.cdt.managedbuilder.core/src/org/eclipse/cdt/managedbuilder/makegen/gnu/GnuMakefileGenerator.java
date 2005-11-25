9d274d09abb68ea7eb3d04fb779aa58db327ef76
Chris Recoskie <recoskie@ca.ibm.com>
2005-11-25 19:29:13 +0000

    crecoskie Nov 25, 2005 - support macros in the clean command

--- build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
@@ -1110,7 +1110,19 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator {
 
 		// Get the clean command from the build model
 		buffer.append("RM := "); //$NON-NLS-1$
-		buffer.append(info.getCleanCommand() + NEWLINE);
+        
+        // support macros in the clean command
+        String cleanCommand = info.getCleanCommand();
+        
+        try {
+            cleanCommand = ManagedBuildManager.getBuildMacroProvider()
+                    .resolveValueToMakefileFormat(info.getCleanCommand(),
+                            EMPTY_STRING, WHITESPACE,
+                            IBuildMacroProvider.CONTEXT_CONFIGURATION, config);
+        } catch (BuildMacroException e) {
+        }
+        
+        buffer.append(cleanCommand + NEWLINE);
 		buffer.append(NEWLINE);
 		
 		// Now add the source providers

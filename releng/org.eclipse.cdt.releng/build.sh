e38c5cfc70e383da80dbe73a4b0e6fbb8788c8ba
Doug Schaefer <doug.schaefer@windriver.com>
2006-01-03 17:13:29 +0000

    Updating the build script to the latest basebuilder.

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -5,8 +5,8 @@ cd `dirname $0`
 mkdir -p tools
 cd tools
 cvs -d:pserver:anonymous@dev.eclipse.org:/home/eclipse \
-	checkout -rv20050405 org.eclipse.releng.basebuilder
+	checkout org.eclipse.releng.basebuilder
 cd ..
 
-java -cp tools/org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main \
+java -jar tools/org.eclipse.releng.basebuilder/startup.jar \
 	-ws gtk -application org.eclipse.ant.core.antRunner $*

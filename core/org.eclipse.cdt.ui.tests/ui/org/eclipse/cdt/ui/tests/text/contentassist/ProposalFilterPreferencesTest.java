1849e12a3803a18c0c3bab670df2f05f9d8cbe2c
Doug Schaefer <doug.schaefer@windriver.com>
2006-01-18 15:44:24 +0000

    Bug 104792 - Content proposal filtering feature.

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist/ProposalFilterPreferencesTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist/ProposalFilterPreferencesTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Norbert Plött and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Norbert Plött (Seimens) - Initial Contribution
+ *******************************************************************************/
 package org.eclipse.cdt.ui.tests.text.contentassist;
 
 import org.eclipse.cdt.internal.ui.preferences.ProposalFilterPreferencesUtil;

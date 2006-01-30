4771ebbe8d17c875e99f3055b768e43a188fcb9c
Doug Schaefer <doug.schaefer@windriver.com>
2006-01-30 15:08:21 +0000

    See if the new basebuilder works for this branch.

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 mkdir -p tools
 cd tools
 cvs -d:pserver:anonymous@dev.eclipse.org:/home/eclipse \
-	checkout -rv20050405 org.eclipse.releng.basebuilder
+	checkout org.eclipse.releng.basebuilder
 cd ..
 
 java -cp tools/org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main \

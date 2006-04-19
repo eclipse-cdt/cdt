93740b531682ce9662b5d12f830e6ec3f84c72b9
Thomas Fletcher <thomasf@qnx.com>
2006-04-19 21:19:27 +0000

    Updated reconciler usage to make use of new MonoReconciler class and removed CReconciler. Patch contributed by Anton Leherbauer PR 130089

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CSourceViewerConfiguration.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CSourceViewerConfiguration.java
@@ -44,7 +44,7 @@ import org.eclipse.jface.text.information.InformationPresenter;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.reconciler.IReconciler;
-import org.eclipse.jface.text.reconciler.Reconciler;
+import org.eclipse.jface.text.reconciler.MonoReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.source.IAnnotationHover;
@@ -222,21 +222,10 @@ public class CSourceViewerConfiguration extends TextSourceViewerConfiguration {
 	 */
 	public IReconciler getReconciler(ISourceViewer sourceViewer) {
 		if (fEditor != null && fEditor.isEditable()) {
-			Reconciler reconciler= new CReconciler() {
-				protected void initialProcess() {
-					// prevent case where getDocument() returns null
-					// and causes exception in initialProcess()
-					// PR 63890
-					IDocument doc = getDocument();
-					if (doc != null)
-						super.initialProcess();
-				}
-			};
 			//Delay changed and non-incremental reconciler used due to 
 			//PR 130089
+			MonoReconciler reconciler= new MonoReconciler(new CReconcilingStrategy(fEditor), false);
 			reconciler.setDelay(500);
-			reconciler.setIsIncrementalReconciler(false);
-			reconciler.setReconcilingStrategy(new CReconcilingStrategy(fEditor), IDocument.DEFAULT_CONTENT_TYPE);
 			return reconciler;
 		}
 		return null;

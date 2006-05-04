a994bf6631bd33163d2b82b382070d525c9efed4
David Dykstal <david_dykstal@us.ibm.com>
2006-05-04 20:54:28 +0000

    Updating build to use stock build.xml and genericTargets.xml from org.eclipse.pde.build.

--- releng/org.eclipse.rse.build/build.rb
+++ releng/org.eclipse.rse.build/build.rb
@@ -6,25 +6,54 @@
 
 require "ftools"
 
+def ask(question, default)
+	message = "#{question} (default is #{default})" 
+	STDERR.print message
+	answer = readline()
+	answer = answer.strip.empty? ? default : answer
+	return answer
+end
+
 # "eclipse" is the location of the basic PDE and plugins to compile against
-# "builder" is the location of the build scripts (i.e. the contents of org.eclipse.rse.build)
-# "working" is where the build is actually done, does not need to exist
+# This should include the org.eclipse.pde.build project
 eclipse	= "c:/work/targets/OpenRSE-1.0/eclipse" 
-builder	= "." 
+
+# "builder" is the location of the custom build scripts customTargets.xml and build.properties
+# (i.e. the contents of org.eclipse.rse.build)
+builder	= File.expand_path(".") # needs to be an absolute path
+
+# "working" is where the build is actually done, does not need to exist
 working = "c:/temp/build"
+
+# Find the base build scripts
+candidates = Dir["#{eclipse}/plugins/org.eclipse.pde.build*"]
+if (candidates.size == 0) then 
+	raise("PDE Build was not found.")
+end
+if (candidates.size > 1) then
+	raise("Too many versions of PDE Build were found.")
+end
+baseBuilder = candidates[0]
+
+buildDirectory = "#{working}/build"
+packageDirectory = "#{working}/package"
+publishDirectory = "#{working}/publish"
+
+tag = ask("Enter tag to fetch from CVS", "HEAD")
+buildType = ask("Enter build type (N=Nightly, I=Integration, M=Milestone)", "N")
+buildId = ask("Enter the build id", Time.now.strftime("%Y%m%d-%H%M"))
 
 command = "java -cp #{eclipse}/startup.jar org.eclipse.core.launcher.Main "
 command += "-application org.eclipse.ant.core.antRunner "
-command += "-buildfile #{builder}/build.xml "
-command += "-DbuildDirectory=#{working}/build "
-command += "-DpackageDirectory=#{working}/package "
-command += "-DpublishDirectory=#{working}/publish "
+command += "-buildfile #{baseBuilder}/scripts/build.xml "
+command += "-DbuildDirectory=#{buildDirectory} "
+command += "-DpackageDirectory=#{packageDirectory} "
+command += "-DpublishDirectory=#{publishDirectory} "
 command += "-Dbuilder=#{builder} "
 command += "-DbaseLocation=#{eclipse} "
-command += "-Dbld_do_extract=yes "
-command += "-Dbld_do_build=yes "
-command += "-Dbld_do_package=yes "
-command += "-Dbld_do_publish=yes "
+command += "-DbuildType=#{buildType} "
+command += "-DbuildId=#{buildId} "
+command += "-DmapVersionTag=#{tag} "
 
 puts(command)
 

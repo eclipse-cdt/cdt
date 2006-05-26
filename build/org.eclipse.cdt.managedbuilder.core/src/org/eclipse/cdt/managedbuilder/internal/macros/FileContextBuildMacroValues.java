f4fc7d4d272fe39eca2a7c99d10b28d8d76a07b5
Mikhail Sennikovsky <mikhail.sennikovskiy@gmail.com>
2006-05-26 16:34:09 +0000

    Fix for 141001: FileContextBuildMacroValues now implements Cloneable

--- build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/macros/FileContextBuildMacroValues.java
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/internal/macros/FileContextBuildMacroValues.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 Intel Corporation and others.
+ * Copyright (c) 2005, 2006 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,7 +25,7 @@ import org.eclipse.cdt.managedbuilder.macros.IFileContextBuildMacroValues;
  * @since 3.0
  */
 public class FileContextBuildMacroValues implements
-		IFileContextBuildMacroValues {
+		IFileContextBuildMacroValues, Cloneable {
 	
 	private IBuilder fBuilder;
 	private IFileContextBuildMacroValues fSupperClassValues;

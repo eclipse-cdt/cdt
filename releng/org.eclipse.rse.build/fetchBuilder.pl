52c43ecf8a0e8cb272c5a5e797c2713d1bbae6c3
David Dykstal <david_dykstal@us.ibm.com>
2006-06-07 01:42:00 +0000

    Moving from ruby to perl so that this can be run on build.eclipse.org

--- releng/org.eclipse.rse.build/fetchBuilder.pl
+++ releng/org.eclipse.rse.build/fetchBuilder.pl
@@ -7,14 +7,14 @@ chomp($answer);
 $tag = $answer ? $answer : "HEAD";
 
 $incantation = "cvs ";
-$incantation .= "-d :pserver:anonymous:none@dev.eclipse.org:/cvsroot/dsdp ";
+$incantation .= '-d :pserver:anonymous:none@dev.eclipse.org:/cvsroot/dsdp ';
 $incantation .= "checkout ";
 $incantation .= "-r ${tag} ";
 $incantation .= "-d builder ";
 $incantation .= "org.eclipse.tm.rse/releng/org.eclipse.rse.build ";
 
 print($incantation);
-#system($incantation);
+system($incantation);
 
 print("\n");
 print("Builder has been fetched and is in the builder subdirectory\n");

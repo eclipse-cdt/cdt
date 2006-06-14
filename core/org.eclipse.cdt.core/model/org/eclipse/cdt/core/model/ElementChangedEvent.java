4836857ef3de2510e9d82982f4fef118ade4cbe5
Chris Recoskie <recoskie@ca.ibm.com>
2006-06-14 12:16:17 +0000

    update copyrights

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/ElementChangedEvent.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/ElementChangedEvent.java
@@ -1,7 +1,7 @@
 package org.eclipse.cdt.core.model;
 
 /*******************************************************************************
- * Copyright (c) 2001 - 2002 IBM Corporation and others.
+ * Copyright (c) 2001, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/Flags.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/Flags.java
@@ -1,7 +1,7 @@
 package org.eclipse.cdt.core.model;
 
 /*******************************************************************************
- * Copyright (c) 2000 - 2001 IBM Corporation and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

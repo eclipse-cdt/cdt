e539d8c06dc8ba94f04f2226af82a1f837099a96
Ken Ryall <ken.ryall@nokia.com>
2006-06-15 15:36:43 +0000

    Added missing copyright notices.

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/ui/importexecutable/Messages.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/ui/importexecutable/Messages.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Nokia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nokia - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.cdt.debug.ui.importexecutable;
 
 import org.eclipse.osgi.util.NLS;

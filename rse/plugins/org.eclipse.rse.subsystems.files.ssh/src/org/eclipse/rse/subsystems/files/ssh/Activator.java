a698ade0f7fde856e695eab9d5fead438ba92a81
David McKnight <dmcknigh@ca.ibm.com>
2006-06-29 20:17:49 +0000

    Fix for 148971.  Need to activate files.ui plugin to get adapter for files

--- rse/plugins/org.eclipse.rse.subsystems.files.ssh/src/org/eclipse/rse/subsystems/files/ssh/Activator.java
+++ rse/plugins/org.eclipse.rse.subsystems.files.ssh/src/org/eclipse/rse/subsystems/files/ssh/Activator.java
@@ -28,6 +28,9 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		
+		// make sure files.ui is activated
+		org.eclipse.rse.files.ui.Activator.getDefault();
 	}
 
 	/*

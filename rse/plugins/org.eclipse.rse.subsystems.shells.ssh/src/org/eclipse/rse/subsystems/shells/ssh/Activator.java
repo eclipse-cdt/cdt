108fd36c65e84942f48ab084a2f67dc3dbc1e908
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-06-30 08:52:11 +0000

    Bug 149284 - shells not properly initialized when only ssh is installed

--- rse/plugins/org.eclipse.rse.subsystems.shells.ssh/src/org/eclipse/rse/subsystems/shells/ssh/Activator.java
+++ rse/plugins/org.eclipse.rse.subsystems.shells.ssh/src/org/eclipse/rse/subsystems/shells/ssh/Activator.java
@@ -28,6 +28,9 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+
+		// shells ui needs to be started for proper UI init
+		org.eclipse.rse.shells.ui.Activator.getDefault();
 	}
 
 	/*

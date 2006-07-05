da21b108846414112f482bc83d071fba5b66087a
Chris Recoskie <recoskie@ca.ibm.com>
2006-07-05 18:06:57 +0000

    fix for 40101

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/c/hover/AbstractCEditorTextHover.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/c/hover/AbstractCEditorTextHover.java
@@ -23,7 +23,9 @@ import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextHoverExtension;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Region;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.commands.ICommand;
@@ -31,89 +33,110 @@ import org.eclipse.ui.commands.IKeySequenceBinding;
 import org.eclipse.ui.keys.KeySequence;
 
 /**
- * AbstractCEditorTextHover
- * Abstract class for providing hover information for C elements.
+ * AbstractCEditorTextHover Abstract class for providing hover information for C
+ * elements.
  * 
  */
-public class AbstractCEditorTextHover implements ICEditorTextHover, ITextHoverExtension {
+public class AbstractCEditorTextHover implements ICEditorTextHover,
+		ITextHoverExtension {
 
 	private IEditorPart fEditor;
+
 	private ICommand fCommand;
-//	{
-//		ICommandManager commandManager= PlatformUI.getWorkbench().getCommandSupport().getCommandManager();
-//		fCommand= commandManager.getCommand(ICEditorActionDefinitionIds.SHOW_JAVADOC);
-//		if (!fCommand.isDefined())
-//			fCommand= null;
-//	}
+
+	// {
+	// ICommandManager commandManager=
+	// PlatformUI.getWorkbench().getCommandSupport().getCommandManager();
+	// fCommand=
+	// commandManager.getCommand(ICEditorActionDefinitionIds.SHOW_JAVADOC);
+	// if (!fCommand.isDefined())
+	// fCommand= null;
+	// }
 
 	/*
 	 * @see IJavaEditorTextHover#setEditor(IEditorPart)
 	 */
 	public void setEditor(IEditorPart editor) {
-		fEditor= editor;
+		fEditor = editor;
 	}
 
 	protected IEditorPart getEditor() {
 		return fEditor;
 	}
-	
+
 	/*
 	 * @see ITextHover#getHoverRegion(ITextViewer, int)
 	 */
 	public IRegion getHoverRegion(ITextViewer textViewer, int offset) {
-		return CWordFinder.findWord(textViewer.getDocument(), offset);
+		if (textViewer != null) {
+			/*
+			 * If the hover offset falls within the selection range return the
+			 * region for the whole selection.
+			 */
+			Point selectedRange = textViewer.getSelectedRange();
+			if (selectedRange.x >= 0 && selectedRange.y > 0
+					&& offset >= selectedRange.x
+					&& offset <= selectedRange.x + selectedRange.y)
+				return new Region(selectedRange.x, selectedRange.y);
+			else {
+				return CWordFinder.findWord(textViewer.getDocument(), offset);
+			}
+		}
+		return null;
 	}
 
-//	protected ICodeAssist getCodeAssist() {
-//		if (fEditor != null) {
-//			IEditorInput input= fEditor.getEditorInput();
-//			if (input instanceof IClassFileEditorInput) {
-//				IClassFileEditorInput cfeInput= (IClassFileEditorInput) input;
-//				return cfeInput.getClassFile();
-//			}
-//			
-//			IWorkingCopyManager manager= CUIPlugin.getDefault().getWorkingCopyManager();				
-//			return manager.getWorkingCopy(input);
-//		}
-//		
-//		return null;
-//	}
-	
+	// protected ICodeAssist getCodeAssist() {
+	// if (fEditor != null) {
+	// IEditorInput input= fEditor.getEditorInput();
+	// if (input instanceof IClassFileEditorInput) {
+	// IClassFileEditorInput cfeInput= (IClassFileEditorInput) input;
+	// return cfeInput.getClassFile();
+	// }
+	//			
+	// IWorkingCopyManager manager=
+	// CUIPlugin.getDefault().getWorkingCopyManager();
+	// return manager.getWorkingCopy(input);
+	// }
+	//		
+	// return null;
+	// }
 
 	/*
 	 * @see ITextHover#getHoverInfo(ITextViewer, IRegion)
 	 */
 	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
-	
-//		ICodeAssist resolve= getCodeAssist();
-//		if (resolve != null) {
-//			try {
-//				ICElement[] result= null;
-//				
-//				synchronized (resolve) {
-//					result= resolve.codeSelect(hoverRegion.getOffset(), hoverRegion.getLength());
-//				}
-//				
-//				if (result == null)
-//					return null;
-//				
-//				int nResults= result.length;	
-//				if (nResults == 0)
-//					return null;
-//				
-//				return getHoverInfo(result);
-//				
-//			} catch (CModelException x) {
-//				CUIPlugin.log(x.getStatus());
-//			}
-//		}
+
+		// ICodeAssist resolve= getCodeAssist();
+		// if (resolve != null) {
+		// try {
+		// ICElement[] result= null;
+		//				
+		// synchronized (resolve) {
+		// result= resolve.codeSelect(hoverRegion.getOffset(),
+		// hoverRegion.getLength());
+		// }
+		//				
+		// if (result == null)
+		// return null;
+		//				
+		// int nResults= result.length;
+		// if (nResults == 0)
+		// return null;
+		//				
+		// return getHoverInfo(result);
+		//				
+		// } catch (CModelException x) {
+		// CUIPlugin.log(x.getStatus());
+		// }
+		// }
 		return null;
 	}
 
 	/**
 	 * Provides hover information for the given C elements.
 	 * 
-	 * @param cElements the C elements for which to provide hover information
+	 * @param cElements
+	 *            the C elements for which to provide hover information
 	 * @return the hover information string
 	 */
 	protected String getHoverInfo(ICElement[] cElements) {
@@ -127,33 +150,39 @@ public class AbstractCEditorTextHover implements ICEditorTextHover, ITextHoverEx
 	public IInformationControlCreator getHoverControlCreator() {
 		return new IInformationControlCreator() {
 			public IInformationControl createInformationControl(Shell parent) {
-				return new DefaultInformationControl(parent, SWT.NONE, new HTMLTextPresenter(true), getTooltipAffordanceString());
+				return new DefaultInformationControl(parent, SWT.NONE,
+						new HTMLTextPresenter(true),
+						getTooltipAffordanceString());
 			}
 		};
 	}
-	
+
 	/**
 	 * Returns the tool tip affordance string.
 	 * 
-	 * @return the affordance string or <code>null</code> if disabled or no key binding is defined
+	 * @return the affordance string or <code>null</code> if disabled or no
+	 *         key binding is defined
 	 * @since 3.0
 	 */
 	protected String getTooltipAffordanceString() {
-//		if (!CUIPlugin.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.EDITOR_SHOW_TEXT_HOVER_AFFORDANCE))
-//			return null;
-//		
-//		KeySequence[] sequences= getKeySequences();
-//		if (sequences == null)
-//			return null;
-//		
-//		String keySequence= sequences[0].format();
-//		return CHoverMessages.getFormattedString("JavaTextHover.makeStickyHint", keySequence); //$NON-NLS-1$
+		// if
+		// (!CUIPlugin.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.EDITOR_SHOW_TEXT_HOVER_AFFORDANCE))
+		// return null;
+		//		
+		// KeySequence[] sequences= getKeySequences();
+		// if (sequences == null)
+		// return null;
+		//		
+		// String keySequence= sequences[0].format();
+		// return
+		// CHoverMessages.getFormattedString("JavaTextHover.makeStickyHint",
+		// keySequence); //$NON-NLS-1$
 		return null;
 	}
 
 	/**
-	 * Returns the array of valid key sequence bindings for the
-	 * show tool tip description command.
+	 * Returns the array of valid key sequence bindings for the show tool tip
+	 * description command.
 	 * 
 	 * @return the array with the {@link KeySequence}s
 	 * 
@@ -161,14 +190,15 @@ public class AbstractCEditorTextHover implements ICEditorTextHover, ITextHoverEx
 	 */
 	private KeySequence[] getKeySequences() {
 		if (fCommand != null) {
-			List list= fCommand.getKeySequenceBindings();
+			List list = fCommand.getKeySequenceBindings();
 			if (!list.isEmpty()) {
-				KeySequence[] keySequences= new KeySequence[list.size()];
-				for (int i= 0; i < keySequences.length; i++) {
-					keySequences[i]= ((IKeySequenceBinding) list.get(i)).getKeySequence();
+				KeySequence[] keySequences = new KeySequence[list.size()];
+				for (int i = 0; i < keySequences.length; i++) {
+					keySequences[i] = ((IKeySequenceBinding) list.get(i))
+							.getKeySequence();
 				}
 				return keySequences;
-			}		
+			}
 		}
 		return null;
 	}

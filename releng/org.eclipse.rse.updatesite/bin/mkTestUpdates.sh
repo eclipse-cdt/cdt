3930ee45ca1c9ed53ae4551473913de9137959b3
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-07-26 11:51:48 +0000

    Support pack200 and digest for TM update site

--- releng/org.eclipse.rse.updatesite/bin/mkTestUpdates.sh
+++ releng/org.eclipse.rse.updatesite/bin/mkTestUpdates.sh
@@ -13,19 +13,26 @@ umask 002
 
 # patch site.xml
 cd ..
-TAG=`ls plugins | head -1 | sed -e 's,[^_]*_[0-9.]*\([^.]*\).jar,\1,'`
+SITE=`pwd`
+TAG=`ls features | head -1 | sed -e 's,[^_]*_[0-9.]*\([^.]*\).jar,\1,'`
 rm site.xml
 cvs -q update -d
-sed -e 's,/dsdp/tm/updates,/dsdp/tm/testUpdates,g' \
-    -e "s,200607201800,$TAG,g" \
+if [ `basename $SITE` = testUpdates ]; then
+    echo "Working on test update site"
+    sed -e 's,/dsdp/tm/updates,/dsdp/tm/testUpdates,g' \
+        -e 's,Project Update Site,Project Test Update Site,g' \
+        site.xml > site.xml.new
+    mv -f site.xml.new site.xml
+else
+    echo "Working on official update site"
+fi
+sed -e "s,200607201800,$TAG,g" \
     site.xml > site.xml.new
-chgrp dsdp-tm-rse site.xml.new
 mv -f site.xml.new site.xml
+chgrp dsdp-tm-rse site.xml
 
 # optimize the site
 # see http://wiki.eclipse.org/index.php/Platform-releng-faq
-cd ..
-site=`pwd`
 cd $HOME/ws/eclipse
 #Use Java5 on build.eclipse.org
 export PATH=/shared/common/ibm-java2-ppc64-50/bin:$PATH
@@ -33,17 +40,17 @@ export PATH=/shared/common/ibm-java2-ppc64-50/bin:$PATH
 echo "Packing the site..."
 java -jar startup.jar \
     -application org.eclipse.update.core.siteOptimizer \
-    -jarProcessor -outputDir $site \
-    -processAll -pack $site
+    -jarProcessor -outputDir $SITE \
+    -processAll -pack $SITE
 
 #Create the digest
 echo "Creating digest..."
 java -jar startup.jar \
     -application org.eclipse.update.core.siteOptimizer \
-    -digestBuilder -digestOutputDir=$site \
-    -siteXML=$site/site.xml
+    -digestBuilder -digestOutputDir=$SITE \
+    -siteXML=$SITE/site.xml
 
-cd $site
+cd $SITE
 chown -R dsdp-tm-rse .
 chmod -R g+w .
 cd $curdir

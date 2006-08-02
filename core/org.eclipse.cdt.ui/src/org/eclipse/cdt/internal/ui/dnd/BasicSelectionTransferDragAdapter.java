732d9a063f6bc9561b13ba56fc49bca66b10423a
Markus Schorn <markus.schorn@windriver.com>
2006-08-02 09:10:28 +0000

    Fixes wrong assertion.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dnd/BasicSelectionTransferDragAdapter.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dnd/BasicSelectionTransferDragAdapter.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2005 QNX Software Systems and others.
+ * Copyright (c) 2002, 2006 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * QNX Software Systems - Initial API and implementation
+ *    QNX Software Systems - Initial API and implementation
+ *    Markus Schorn (Wind River Systems)
  *******************************************************************************/
 
 package org.eclipse.cdt.internal.ui.dnd;
@@ -74,9 +75,8 @@ public class BasicSelectionTransferDragAdapter extends DragSourceAdapter impleme
 	 * @see org.eclipse.swt.dnd.DragSourceListener#dragFinished
 	 */	
 	public void dragFinished(DragSourceEvent event) {
-		// We assume that the drop target listener has done all
-		// the work.
-		Assert.isTrue(event.detail == DND.DROP_NONE);
+		// Make sure we don't have to do any remaining work
+		Assert.isTrue(event.detail != DND.DROP_MOVE);
 		LocalSelectionTransfer.getInstance().setSelection(null);
 		LocalSelectionTransfer.getInstance().setSelectionSetTime(0);
 	}	

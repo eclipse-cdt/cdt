7827b8d4a1979e41e67bb0c5b6b62052b161b7bf
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2006-09-11 17:45:54 +0000

    Bug 156114: GDB options layout problem. Applied patch from Vladimir Prus (vladimir@codesourcery.com).

--- debug/org.eclipse.cdt.debug.mi.ui/src/org/eclipse/cdt/debug/mi/internal/ui/StandardGDBDebuggerPage.java
+++ debug/org.eclipse.cdt.debug.mi.ui/src/org/eclipse/cdt/debug/mi/internal/ui/StandardGDBDebuggerPage.java
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *     QNX Software Systems - Initial API and implementation
+ *     Vadimir Prus (vladimir@codesourcery.com) - bug 156114: GDB options layout 
+ *     problem
  *******************************************************************************/
 package org.eclipse.cdt.debug.mi.internal.ui;
 
@@ -36,6 +38,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -177,6 +180,11 @@ public class StandardGDBDebuggerPage extends AbstractCDebuggerPage implements Ob
 			// use default
 		}
 		fVerboseModeButton.setSelection( verboseMode );
+		
+		// We've populated combos, which affects their preferred size, and so must relayout things.
+		Control changed[] = { fCommandFactoryCombo, fProtocolCombo };
+		((Composite) getControl()).layout( changed );
+
 		setInitializing( false ); 
 	}
 

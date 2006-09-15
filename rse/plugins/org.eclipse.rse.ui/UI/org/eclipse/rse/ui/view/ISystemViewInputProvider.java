bfd81e0d2c192bebc702373a8a6887f641191246
David Dykstal <david_dykstal@us.ibm.com>
2006-09-15 19:36:18 +0000

    [cleanup] format and javadoc

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemViewInputProvider.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemViewInputProvider.java
@@ -15,83 +15,99 @@
  ********************************************************************************/
 
 package org.eclipse.rse.ui.view;
+
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.rse.core.model.IHost;
 import org.eclipse.swt.widgets.Shell;
 
-
 /**
  * Abstraction for any object that wishes to be a roots-provider for the SystemView tree viewer.
- * 
  */
-public interface ISystemViewInputProvider extends IAdaptable
-{
+public interface ISystemViewInputProvider extends IAdaptable {
+	
 	/**
-	 * Return the children objects to consistute the root elements in the system view tree
+	 * @return the children objects to consistute the root elements in the system view tree
 	 */
 	public Object[] getSystemViewRoots();
+
 	/**
-	 * Return true if {@link #getSystemViewRoots()} will return a non-empty list
+	 * @return true if {@link #getSystemViewRoots()} will return a non-empty list
 	 */
 	public boolean hasSystemViewRoots();
+
 	/**
-	 * Return true if we are listing connections or not, so we know whether we are interested in 
-	 *  connection-add events
+	 * @return true if we are listing connections or not, so we know whether we are interested in 
+	 * connection-add events
 	 */
 	public boolean showingConnections();
+
 	/**
 	 * This method is called by the connection adapter when the user expands
-	 *  a connection. This method must return the child objects to show for that
-	 *  connection.
+	 * a connection. This method must return the child objects to show for that
+	 * connection.
+	 * @param selectedConnection the connection undergoing expansion
+	 * @return the list of objects under the connection
 	 */
 	public Object[] getConnectionChildren(IHost selectedConnection);
+
 	/**
 	 * This method is called by the connection adapter when deciding to show a plus-sign
-	 * or not beside a connection. Return true if this connection has children to be shown.
+	 * or not beside a connection.
+	 * @param selectedConnection the connection being shown in the viewer 
+	 * @return true if this connection has children to be shown.
 	 */
 	public boolean hasConnectionChildren(IHost selectedConnection);
-    /**
-	 * This is the method required by the IAdaptable interface.
-	 * Given an adapter class type, return an object castable to the type, or
-	 *  null if this is not possible.
-	 */
-    public Object getAdapter(Class adapterType);
-    
-    /**
-     * Set the shell in case it is needed for anything.
-     * The label and content provider will call this.
-     */
-    public void setShell(Shell shell);
-    /**
-     * Return the shell of the viewer we are currently associated with
-     */
-    public Shell getShell();
-    /**
-     * Set the viewer in case it is needed for anything.
-     * The label and content provider will call this.
-     */
-    public void setViewer(Viewer viewer);
-    /**
-     * Return the viewer we are currently associated with
-     */
-    public Viewer getViewer();
 
-    /**
-     * Return true to show the action bar (ie, toolbar) above the viewer.
-     * The action bar contains connection actions, predominantly.
-     */
-    public boolean showActionBar();	
-    /**
-     * Return true to show the button bar above the viewer.
-     * The tool bar contains "Get List" and "Refresh" buttons and is typicall
-     * shown in dialogs that list only remote system objects.
-     */
-    public boolean showButtonBar();	
-    /**
-     * Return true to show right-click popup actions on objects in the tree.
-     */
-    public boolean showActions();
-    
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
+	 */
+	public Object getAdapter(Class adapterType);
+
+	/**
+	 * Set the shell in case it is needed for anything.
+	 * The label and content provider will call this.
+	 * @param shell the shell being used by the viewer using this provider
+	 */
+	// TODO should possibly be deprecated or removed
+	public void setShell(Shell shell);
+
+	/**
+	 * @return the shell of the viewer we are currently associated with
+	 */
+	// TODO should possibly be deprecated or removed
+	public Shell getShell();
+
+	/**
+	 * Set the viewer in case it is needed for anything.
+	 * The label and content provider will call this.
+	 * @param viewer the viewer that uses this provider
+	 */
+	// TODO should possibly be deprecated or removed
+	public void setViewer(Viewer viewer);
+
+	/**
+	 * @return the viewer we are currently associated with
+	 */
+	// TODO should possibly be deprecated or removed
+	public Viewer getViewer();
+
+	/**
+	 * @return true to show the action bar (ie, toolbar) above the viewer.
+	 * The action bar contains connection actions, predominantly.
+	 */
+	public boolean showActionBar();
+
+	/**
+	 * @return true to show the button bar above the viewer.
+	 * The tool bar contains "Get List" and "Refresh" buttons and is typicall
+	 * shown in dialogs that list only remote system objects.
+	 */
+	public boolean showButtonBar();
+
+	/**
+	 * @return true to show right-click popup actions on objects in the tree.
+	 */
+	public boolean showActions();
 
 }

22cae6ea2d824b0ad116d34ef5e4940d9425e848
David Dykstal <david_dykstal@us.ibm.com>
2006-09-20 19:05:50 +0000

    [cleanup] format and javadoc

--- rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/filters/ISystemFilterPoolReference.java
+++ rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/filters/ISystemFilterPoolReference.java
@@ -15,67 +15,61 @@
  ********************************************************************************/
 
 package org.eclipse.rse.core.filters;
+
 import org.eclipse.rse.core.model.IRSEModelObject;
 import org.eclipse.rse.core.references.IRSEPersistableReferencingObject;
 
-
 /**
  * Interface implemented by references to filter pools. Filter pools are stored at the profile
- *  level, while subsystems contain references to one or more pools. A pool can be referenced
- *  by multiple connections. Pools don't go away until explicitly deleted by the user, regardless
- *  of their reference count.
+ * level, while subsystems contain references to one or more pools. A pool can be referenced
+ * by multiple connections. Pools don't go away until explicitly deleted by the user, regardless
+ * of their reference count.
  */
-/** 
- * @lastgen interface SystemFilterPoolReference extends SystemPersistableReferencingObject, IRSEPersistableReferencingObject, SystemFilterContainerReference  {}
- */
-public interface ISystemFilterPoolReference extends IRSEPersistableReferencingObject, ISystemFilterContainerReference, IRSEModelObject
-{
+public interface ISystemFilterPoolReference extends IRSEPersistableReferencingObject, ISystemFilterContainerReference, IRSEModelObject {
 	/**
-	 * Return the reference manager which is managing this filter reference
-	 * framework object.
+	 * @return the reference manager which is managing this filter pool reference
 	 */
 	public ISystemFilterPoolReferenceManager getFilterPoolReferenceManager();
-	
+
 	/**
-	 * Return the object which instantiated the pool reference manager object.
-	 * Makes it easy to get back to the point of origin, given any filter reference
-	 * framework object
+	 * @return the object which instantiated the pool reference manager object.
+	 * Makes it easy to get back to the point of origin, given any filter pool reference.
 	 */
-    public ISystemFilterPoolReferenceManagerProvider getProvider();    
+	public ISystemFilterPoolReferenceManagerProvider getProvider();
 
-    /**
-     * Return name of the filter pool we reference
-     * The pool name is stored qualified by the manager name,
-     *  so we first have to strip it off.
-     */
-    public String getReferencedFilterPoolName();
-    /**
-     * Return name of the filter pool manager containing the pool we reference.
-     * The pool name is stored qualified by the manager name,
-     *  so we get it from there.
-     */
-    public String getReferencedFilterPoolManagerName();
-
-    /**
-     * Reset the name of the filter pool we reference. 
-     * Called on filter pool rename operations
-     */
-    public void resetReferencedFilterPoolName(String newName);
-
-    
-    /**
-     * Set the filter pool that we reference.
-     * This also calls addReference(this) on that pool!
-     */
-    public void setReferenceToFilterPool(ISystemFilterPool pool);
-    
 	/**
-	 * Return referenced filter pool object
+	 * @return the simple name of the filter pool we reference. Not qualified by the manager name.
 	 */
-	public ISystemFilterPool getReferencedFilterPool();    
-	
-    /**
-     * Return fully qualified name that includes the filter pool managers name
-     */
-    public String getFullName();	
+	public String getReferencedFilterPoolName();
+
+	/**
+	 * @return name of the filter pool manager containing the pool we reference.
+	 */
+	public String getReferencedFilterPoolManagerName();
+
+	/**
+	 * Reset the name of the filter pool we reference. 
+	 * Called on filter pool rename operations.
+	 * @param newName the new name of the filer pool
+	 */
+	public void resetReferencedFilterPoolName(String newName);
+
+	/**
+	 * Set the filter pool that we reference.
+	 * This should also call addReference(this) on that pool.
+	 * @param pool the pool to which this reference refers.
+	 */
+	public void setReferenceToFilterPool(ISystemFilterPool pool);
+
+	/**
+	 * @return referenced filter pool object. 
+	 * This may be null if the reference is broken
+	 * or is yet to be resolved.
+	 */
+	public ISystemFilterPool getReferencedFilterPool();
+
+	/**
+	 * @return the fully qualified name that includes the name of the filter pool manager
+	 */
+	public String getFullName();
 }

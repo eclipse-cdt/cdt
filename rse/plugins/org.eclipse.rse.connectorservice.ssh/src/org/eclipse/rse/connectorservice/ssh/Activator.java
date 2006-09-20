18057c62e3810be638eb3ee72353511b7085ee7d
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-09-20 11:19:58 +0000

    Add missing copyrights

--- rse/plugins/org.eclipse.rse.connectorservice.ssh/src/org/eclipse/rse/connectorservice/ssh/Activator.java
+++ rse/plugins/org.eclipse.rse.connectorservice.ssh/src/org/eclipse/rse/connectorservice/ssh/Activator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Wind River Systems, Inc.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Martin Oberhuber (Wind River) - initial API and implementation 
+ *******************************************************************************/
 package org.eclipse.rse.connectorservice.ssh;
 
 import java.text.DateFormat;

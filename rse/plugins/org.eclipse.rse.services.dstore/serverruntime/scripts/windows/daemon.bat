079bc09b83b218492216517cd1db83cdd4b75120
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-09-21 21:13:24 +0000

    Fix bug 142952 windows daemon.bat calls setup.bat

--- rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/windows/daemon.bat
+++ rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/windows/daemon.bat
@@ -1,2 +1,12 @@
+@ECHO OFF
+IF NOT "%A_PLUGIN_PATH%"=="" GOTO DoneSetup
+IF EXIST setup.bat GOTO HaveSetup
+ECHO.
+ECHO Please run setup.bat before running daemon.bat
+PAUSE
+GOTO Done
+:HaveSetup
+CALL setup.bat
+:DoneSetup
 java -DA_PLUGIN_PATH=%A_PLUGIN_PATH% org.eclipse.dstore.core.server.ServerLauncher
-
+:Done

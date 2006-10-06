67eaded88ad3e6f4fb253e430c8e3265849b5225
Javier Montalvo Orus <javier.montalvoorus@gmail.com>
2006-10-06 10:06:54 +0000

    Fix Copyrights

--- discovery/org.eclipse.rse.discovery/src/org/eclipse/rse/discovery/Messages.java
+++ discovery/org.eclipse.rse.discovery/src/org/eclipse/rse/discovery/Messages.java
@@ -9,15 +9,6 @@
  ********************************************************************************/
 
 package org.eclipse.rse.discovery;
-/********************************************************************************
- * Copyright (c) 2006 Symbian Software Ltd. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *   Javier Montalvo Orús (Symbian) - initial API and implementation
- ********************************************************************************/
 
 import org.eclipse.osgi.util.NLS;
 

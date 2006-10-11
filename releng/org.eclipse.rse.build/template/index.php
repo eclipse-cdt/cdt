abad039f473e3ee17f1cb58365b6faa2fe45da7e
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-10-11 01:24:35 +0000

    Adapt downloads template to Platform 3.2.1, 3.3M2

--- releng/org.eclipse.rse.build/template/index.php
+++ releng/org.eclipse.rse.build/template/index.php
@@ -47,9 +47,13 @@
 </table>
 <table>
   <tbody>
-    <tr><td><b>RSE requires Eclipse-3.2.<b> (<a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.2-200606291905/index.php">download page</a>)</td></tr>
+    <tr><td><b>RSE requires Eclipse-3.2
+       (<a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.2-200606291905/index.php">
+       download page</a>) or later, e.g. 
+       <a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.2.1-200609210945/index.php">
+       3.2.1</a>.</b></td></tr>
     <tr><td>Earlier versions will <b>not</b> work!</td></tr>
-    <tr><td>3.3M1 has been reported to work, but has not undergone any formal testing.</td></tr>
+    <tr><td>3.3M1 and 3.3M2 have been reported to work, but have not undergone any formal testing.</td></tr>
     <!-- <tr>
       <td>We've been testing with Eclipse-3.2, built 29-Jun-2006 (<a href="http://download.eclipse.org/eclipse/downloads/drops/R-3.2-200606291905/index.php">download page</a>)</td>
     </tr> -->

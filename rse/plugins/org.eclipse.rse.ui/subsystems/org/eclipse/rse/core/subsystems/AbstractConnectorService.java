a18756b306ea8c1fd2a5bd5481a3b18e977d8297
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-10-24 13:41:38 +0000

    [cleanup] IBM-supplied --> predefined

--- rse/plugins/org.eclipse.rse.ui/subsystems/org/eclipse/rse/core/subsystems/AbstractConnectorService.java
+++ rse/plugins/org.eclipse.rse.ui/subsystems/org/eclipse/rse/core/subsystems/AbstractConnectorService.java
@@ -1027,7 +1027,7 @@ public abstract class AbstractConnectorService extends RSEModelObject implements
 	 * <p>
 	 * You normally do not call this! Rather, your subsystem factory class will override
 	 * {@link org.eclipse.rse.core.subsystems.SubSystemConfiguration#supportsServerLaunchType(ServerLaunchType)}.
-	 * However, this method is needed by ISVs that re-use IBM-supplied subsystem factories,
+	 * However, this method is needed by ISVs that re-use predefined subsystem factories,
 	 * and merely supply their own ISystem object via the "systemClass" attribute of the
 	 * subsystemConfigurations extension point.
 	 * 

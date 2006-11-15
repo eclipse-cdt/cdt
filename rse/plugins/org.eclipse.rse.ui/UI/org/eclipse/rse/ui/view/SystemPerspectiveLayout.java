efc2a3dbca5893c9d0799772f0d6961dbc8c028f
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-11-15 12:42:56 +0000

    Fix 164628: get rid of dependency to debug.ui

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/SystemPerspectiveLayout.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/SystemPerspectiveLayout.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2006 IBM Corporation. All rights reserved.
+ * Copyright (c) 2006 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -11,14 +11,13 @@
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
- * {Name} (company) - description of contribution.
+ * Martin Oberhuber (Wind River) - Fix 164628: get rid of dependency to debug.ui
  ********************************************************************************/
 
 package org.eclipse.rse.ui.view;
 
 
 
-import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.rse.ui.view.scratchpad.SystemScratchpadViewPart;
 import org.eclipse.rse.ui.view.team.SystemTeamViewPart;
 import org.eclipse.ui.IFolderLayout;
@@ -32,26 +31,26 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class SystemPerspectiveLayout implements IPerspectiveFactory
 {
 
-	public static final String ID = "org.eclipse.rse.ui.view.SystemPerspective"; // matches id in plugin.xml, layout tag
+	public static final String ID = "org.eclipse.rse.ui.view.SystemPerspective"; // matches id in plugin.xml, layout tag //$NON-NLS-1$
 	/**
 	 * Defines the initial layout for a perspective.
 	 * This method is only called when a new perspective is created.  If
 	 * an old perspective is restored from a persistence file then
 	 * this method is not called.
 	 *
-	 * @param factory the factory used to add views to the perspective
+	 * @param layout the page layout
 	 */
 	public void createInitialLayout(IPageLayout layout) 
 	{
         String editorArea = layout.getEditorArea();
 		
-		IFolderLayout folder= layout.createFolder("org.eclipse.rse.ui.view.NavFolder", IPageLayout.LEFT, 
+		IFolderLayout folder= layout.createFolder("org.eclipse.rse.ui.view.NavFolder", IPageLayout.LEFT, //$NON-NLS-1$
 		                                   (float)0.25, editorArea);
 		//folder.addView(IPageLayout.ID_RES_NAV);
 		folder.addView(SystemViewPart.ID);
 		folder.addView(SystemTeamViewPart.ID);
 						           
-		folder= layout.createFolder("org.eclipse.rse.ui.view.MiscFolder", IPageLayout.BOTTOM, 
+		folder= layout.createFolder("org.eclipse.rse.ui.view.MiscFolder", IPageLayout.BOTTOM,  //$NON-NLS-1$
 		                            (float).60, editorArea);		           
 
 		folder.addView(SystemTableViewPart.ID);
@@ -59,14 +58,14 @@ public class SystemPerspectiveLayout implements IPerspectiveFactory
 		folder.addView(IPageLayout.ID_TASK_LIST); // put in the desktop-supplied task list view
 
 		
-		folder= layout.createFolder("org.eclipse.rse.ui.view.OutlineFolder", IPageLayout.RIGHT, 
+		folder= layout.createFolder("org.eclipse.rse.ui.view.OutlineFolder", IPageLayout.RIGHT,  //$NON-NLS-1$
 		                            (float).80, editorArea);		           
 	
 		folder.addView(IPageLayout.ID_OUTLINE); // put in desktop-supplied outline view
 		// unfortunately we can't do the following as snippets aren't in wswb, according to DKM
 
-		folder= layout.createFolder("org.eclipse.rse.ui.view.PropertiesFolder", IPageLayout.BOTTOM, 
-                (float).75, "org.eclipse.rse.ui.view.NavFolder");	
+		folder= layout.createFolder("org.eclipse.rse.ui.view.PropertiesFolder", IPageLayout.BOTTOM, //$NON-NLS-1$ 
+                (float).75, "org.eclipse.rse.ui.view.NavFolder"); //$NON-NLS-1$
 		//layout.addView(IPageLayout.ID_PROP_SHEET, IPageLayout.BOTTOM, 
 		  //     (float)0.75, "org.eclipse.rse.ui.view.NavFolder"); // put in desktop-supplied property sheet view         
 		folder.addView(IPageLayout.ID_PROP_SHEET);
@@ -83,8 +82,12 @@ public class SystemPerspectiveLayout implements IPerspectiveFactory
 		layout.addPerspectiveShortcut(ID);
 		
 		// Add action sets to the tool bar.
-		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
-		layout.addActionSet(IDebugUIConstants.DEBUG_ACTION_SET);
+		// Fix 164628: get rid of dependency to debug.ui
+		//layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
+		//layout.addActionSet(IDebugUIConstants.DEBUG_ACTION_SET);
+		layout.addActionSet("org.eclipse.debug.ui.launchActionSet"); //$NON-NLS-1$
+		layout.addActionSet("org.eclipse.debug.ui.debugActionSet"); //$NON-NLS-1$
+		
 
 	}
 }

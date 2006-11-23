d4dc19649e14d59679404233545538431278b7a6
Andrew Ferguson <andrew.ferguson@symbian.com>
2006-11-23 16:36:01 +0000

    add javadoc

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/db/IString.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/db/IString.java
@@ -20,17 +20,67 @@ import org.eclipse.core.runtime.CoreException;
  * @author Doug Schaefer
  */
 public interface IString {
-
+	/**
+	 * Get the offset of this IString record in the PDOM
+	 * @return
+	 */
 	public int getRecord();
 	
 	// strcmp equivalents
+	/**
+	 * Compare this IString record and the specified IString record
+	 * @param chars
+	 * @return <ul><li> -1 if this &lt; string
+	 * <li> 0 if this == string
+	 * <li> 1 if this &gt; string
+	 * </ul>
+	 * @throws CoreException
+	 */
 	public int compare(IString string) throws CoreException;
+	
+	/**
+	 * Compare this IString record and the specified String object
+	 * @param chars
+	 * @return <ul><li> -1 if this &lt; string
+	 * <li> 0 if this == string
+	 * <li> 1 if this &gt; string
+	 * </ul>
+	 * @throws CoreException
+	 */
 	public int compare(String string) throws CoreException;
+	
+	/**
+	 * Compare this IString record and the specified character array
+	 * @param chars
+	 * @return <ul><li> -1 if this &lt; chars
+	 * <li> 0 if this == chars
+	 * <li> 1 if this &gt; chars
+	 * </ul>
+	 * @throws CoreException
+	 */
 	public int compare(char[] chars) throws CoreException;
-
-	// use sparingly, these can be expensive
+	
+	/**
+	 * Get an equivalent character array to this IString record<p>
+	 * <b>N.B. This method can be expensive: compare and equals can be used for
+	 * efficient comparisons</b>
+	 * @return an equivalent character array to this IString record
+	 * @throws CoreException
+	 */
 	public char[] getChars() throws CoreException;
+	
+	/**
+	 * Get an equivalent String object to this IString record<p>
+	 * <b>N.B. This method can be expensive: compare and equals can be used for
+	 * efficient comparisons</b>
+	 * @return an equivalent String object to this IString record
+	 * @throws CoreException
+	 */
 	public String getString() throws CoreException;
 	
+	/**
+	 * Free the associated record in the PDOM
+	 * @throws CoreException
+	 */
 	public void delete() throws CoreException;
 }

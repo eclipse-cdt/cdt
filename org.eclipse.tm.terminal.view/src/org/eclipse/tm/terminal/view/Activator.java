2e8951426c60519df6777e18bac69f3f0bc9e181
Martin Oberhuber <martin.oberhuber@windriver.com>
2006-12-04 10:37:14 +0000

    Fix PLUGIN_ID in com.windriver.ide.tm.terminal.view

--- org.eclipse.tm.terminal.view/src/org/eclipse/tm/terminal/view/Activator.java
+++ org.eclipse.tm.terminal.view/src/org/eclipse/tm/terminal/view/Activator.java
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.tm.terminal.internal.view";
+	public static final String PLUGIN_ID = "org.eclipse.tm.terminal.view"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;

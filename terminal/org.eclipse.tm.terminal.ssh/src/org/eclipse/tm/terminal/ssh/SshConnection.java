9d59d4087afc2eaeba38279a3634cc799cad1e10
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-12-07 10:26:20 +0000

    [cleanup] fix terminal copyright headers and beautify

--- terminal/org.eclipse.tm.terminal.ssh/src/org/eclipse/tm/terminal/ssh/SshConnection.java
+++ terminal/org.eclipse.tm.terminal.ssh/src/org/eclipse/tm/terminal/ssh/SshConnection.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2006 Wind River Systems, Inc. and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
  * 
- * Contributors:
- *     Michael Scharf (Wind River) - initial implementation
- *     
+ * Contributors: 
+ * Michael Scharf (Wind River) - initial API and implementation
+ * Martin Oberhuber (Wind River) - fixed copyright headers and beautified
  *******************************************************************************/
 package org.eclipse.tm.terminal.ssh;
 
@@ -84,6 +84,7 @@ class SshConnection extends Thread {
 			
 		}
 	}
+
 	/**
 	 * Read the data from the ssh connection and display it in the terminal.
 	 * @param in
@@ -135,7 +136,8 @@ class SshConnection extends Thread {
 			});
 		}
     }
-	private void connectFailed(String terminalText, String msg) {
+
+    private void connectFailed(String terminalText, String msg) {
 		Logger.log(terminalText);
 		fControl.displayTextInTerminal(terminalText);
 		fControl.setState(TerminalState.CLOSED);

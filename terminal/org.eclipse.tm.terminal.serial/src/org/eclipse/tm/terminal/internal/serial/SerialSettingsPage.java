c0ecadde5652ba0f08d5ef866c7c6532e79b6868
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-12-07 10:19:22 +0000

    [cleanup] fix terminal copyright headers and beautify

--- terminal/org.eclipse.tm.terminal.serial/src/org/eclipse/tm/terminal/internal/serial/SerialSettingsPage.java
+++ terminal/org.eclipse.tm.terminal.serial/src/org/eclipse/tm/terminal/internal/serial/SerialSettingsPage.java
@@ -1,15 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2006 Wind River Systems, Inc. and others.
+ * Copyright (c) 2003, 2006 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
+ * Initial Contributors:
+ * The following Wind River employees contributed to the Terminal component
+ * that contains this file: Chris Thew, Fran Litterio, Stephen Lamb,
+ * Helmut Haigermoser and Ted Williams.
+ *
  * Contributors:
- *     Wind River Systems, Inc. - initial implementation
- *     
+ * Michael Scharf (Wind River) - extracted from TerminalSettingsDlg 
+ * Martin Oberhuber (Wind River) - fixed copyright headers and beautified
  *******************************************************************************/
-
 package org.eclipse.tm.terminal.internal.serial;
 
 import java.util.Iterator;

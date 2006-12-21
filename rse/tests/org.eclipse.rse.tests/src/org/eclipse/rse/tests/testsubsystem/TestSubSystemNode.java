5deb2dcf1e324d3e66c2848ba289c9be732765e7
Martin Oberhuber < martin.oberhuber@windriver.com>
2006-12-21 10:58:48 +0000

    Fix Copyright Headers for Tests

--- rse/tests/org.eclipse.rse.tests/src/org/eclipse/rse/tests/testsubsystem/TestSubSystemNode.java
+++ rse/tests/org.eclipse.rse.tests/src/org/eclipse/rse/tests/testsubsystem/TestSubSystemNode.java
@@ -1,12 +1,13 @@
-/* *******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
- * available at http://www.eclipse.org/legal/epl-v10.html
+/*******************************************************************************
+ * Copyright (c) 2006 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
  * 
- * Contributors:
- * Tobias Schwarz (Wind River) - initial contribution.
- * *******************************************************************************/
+ * Contributors: 
+ * Tobias Schwarz (Wind River) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.rse.tests.testsubsystem;
 
 import org.eclipse.rse.core.subsystems.AbstractResource;

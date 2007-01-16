e221ecb948734b8820ff61bb978b924f5de8a954
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-01-16 15:20:33 +0000

    Fix copyright years

--- rse/tests/org.eclipse.rse.tests/src/org/eclipse/rse/tests/internal/RSEConnectionManager.java
+++ rse/tests/org.eclipse.rse.tests/src/org/eclipse/rse/tests/internal/RSEConnectionManager.java
@@ -1,14 +1,15 @@
-/* *******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+/*******************************************************************************
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
  * Don Yantzi (IBM) - initial contribution.
  * David Dykstal (IBM) - initial contribution.
  * Uwe Stieber (Wind River) - refactoring and cleanup.
- * *******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.rse.tests.internal;
 
 import java.io.FileInputStream;

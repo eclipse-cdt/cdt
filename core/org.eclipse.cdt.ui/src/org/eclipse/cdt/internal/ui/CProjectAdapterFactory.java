1c2f9bf300685417d5d41769c458d0581b0b31a1
Anton Leherbauer <anton.leherbauer@windriver.com>
2007-01-18 13:01:51 +0000

    Fix plug-in manifest warnings

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/CProjectAdapterFactory.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Anton Leherbauer (Wind River Systems) - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.internal.ui;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdapterFactory;
+
+import org.eclipse.cdt.core.model.ICProject;
+
+/**
+ * Adapter factory to adapt <code>ICProject</code> to <code>IProject</code>.
+ *
+ * @since 4.0
+ */
+public class CProjectAdapterFactory implements IAdapterFactory {
+
+	private static final Class[] ADAPTERS = { IProject.class };
+
+	/*
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
+	 */
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (IProject.class.equals(adapterType)) {
+			return ((ICProject)adaptableObject).getProject();
+		}
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	public Class[] getAdapterList() {
+		return ADAPTERS;
+	}
+
+}

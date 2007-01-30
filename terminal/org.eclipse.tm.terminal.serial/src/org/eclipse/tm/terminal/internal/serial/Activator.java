b174869998b89acfbb7e8ef266a2c6bb36d0aa3b
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-01-30 18:06:57 +0000

    [cleanup] compiler warnings

--- terminal/org.eclipse.tm.terminal.serial/src/org/eclipse/tm/terminal/internal/serial/Activator.java
+++ terminal/org.eclipse.tm.terminal.serial/src/org/eclipse/tm/terminal/internal/serial/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.tm.terminal.serial";
+	public static final String PLUGIN_ID = "org.eclipse.tm.terminal.serial"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;

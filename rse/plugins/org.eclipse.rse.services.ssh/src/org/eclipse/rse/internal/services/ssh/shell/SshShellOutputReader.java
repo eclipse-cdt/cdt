f6f19182d79c1bbe34378b878f25c18a7b6ddcfc
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-02-15 10:01:26 +0000

    [cleanup] update copyright year 2007

--- rse/plugins/org.eclipse.rse.services.ssh/src/org/eclipse/rse/internal/services/ssh/shell/SshShellOutputReader.java
+++ rse/plugins/org.eclipse.rse.services.ssh/src/org/eclipse/rse/internal/services/ssh/shell/SshShellOutputReader.java
@@ -1,9 +1,10 @@
-/********************************************************************************
- * Copyright (c) 2006 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+/*******************************************************************************
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
  * component that contains this file: David McKnight, Kushal Munir, 
@@ -13,7 +14,7 @@
  * Contributors:
  * Martin Oberhuber (Wind River) - Adapted from LocalShellOutputReader. 
  * Martin Oberhuber (Wind River) - Added vt100 escape sequence ignoring. 
- ********************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.rse.internal.services.ssh.shell;
 

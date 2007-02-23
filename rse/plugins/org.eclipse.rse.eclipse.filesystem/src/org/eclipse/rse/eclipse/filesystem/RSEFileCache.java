98929a2c3c74f5798c008e9c1355e0814a639880
Kushal Munir < kmunir@ca.ibm.com>
2007-02-23 20:19:24 +0000

    [170916] [efs] Implementation for SSH should work. FTP and Dstore will not work for EFS. Those implementations will be ready in next milestone.

--- rse/plugins/org.eclipse.rse.eclipse.filesystem/src/org/eclipse/rse/eclipse/filesystem/RSEFileCache.java
+++ rse/plugins/org.eclipse.rse.eclipse.filesystem/src/org/eclipse/rse/eclipse/filesystem/RSEFileCache.java
@@ -1,3 +1,13 @@
+/********************************************************************************
+ * Copyright (c) 2007 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * {Kushal Munir} (IBM) - initial implementation.
+ ********************************************************************************/
+
 package org.eclipse.rse.eclipse.filesystem;
 
 import java.io.BufferedOutputStream;

6a376a5c6c0bb90c3b8f5363f9fbb8e5e0403a28
Oleg Krasilnikov <oleg.krasilnikov@intel.com>
2007-03-12 17:30:52 +0000

    warning eliminated

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/AbstractLangsListTab.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/AbstractLangsListTab.java
@@ -12,7 +12,6 @@ package org.eclipse.cdt.ui.newui;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;

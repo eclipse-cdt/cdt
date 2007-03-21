6f9fcdf81f1df8b0ca2810c443aa6810e007d6b3
Vivian Kong <vivkong@ca.ibm.com>
2007-03-21 19:24:35 +0000

    fix for bug 178369 - externalize include grouping label in Outline view

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/CElementGrouping.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/CElementGrouping.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2000, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     QNX Software Systems - Initial API and implementation
+ *     IBM Corporation
  *******************************************************************************/
 package org.eclipse.cdt.ui;
 
@@ -42,7 +43,7 @@ public abstract class CElementGrouping extends WorkbenchAdapter implements IAdap
 	public String getLabel(Object object) {
 		switch (type) {
 			case INCLUDES_GROUPING:
-				return "include directives"; //$NON-NLS-1$
+				return Messages.CElementGrouping_includeGroupingLabel; 
 		}
 		return super.getLabel(object);
 	}

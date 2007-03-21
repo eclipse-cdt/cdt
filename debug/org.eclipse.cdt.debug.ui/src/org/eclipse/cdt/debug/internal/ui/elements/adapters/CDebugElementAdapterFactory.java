5bc8da81cd9e300444ac5e0e6ecf53e38f9bee29
Vivian Kong <vivkong@ca.ibm.com>
2007-03-21 18:03:22 +0000

    fix for bug 175338 - changing all references to IModelProxyFactoryAdapter to IModelProxyFactory

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/elements/adapters/CDebugElementAdapterFactory.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/elements/adapters/CDebugElementAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 QNX Software Systems and others.
+ * Copyright (c) 2004, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  * QNX Software Systems - Initial API and implementation
+ * IBM Corporation
  *******************************************************************************/
 package org.eclipse.cdt.debug.internal.ui.elements.adapters; 
 
@@ -21,7 +22,7 @@ import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IElementContentProvider;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IElementLabelProvider;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IElementMementoProvider;
-import org.eclipse.debug.internal.ui.viewers.model.provisional.IModelProxyFactoryAdapter;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IModelProxyFactory;
  
 /**
  * Comment for .
@@ -30,7 +31,7 @@ public class CDebugElementAdapterFactory implements IAdapterFactory {
 
 	private static IElementLabelProvider fgModuleLabelProvider = new ModuleLabelProvider();
     private static IElementContentProvider fgModuleContentProvider = new ModuleContentProvider();
-	private static IModelProxyFactoryAdapter fgModuleProxyFactory = new ModuleProxyFactory();
+	private static IModelProxyFactory fgModuleProxyFactory = new ModuleProxyFactory();
     private static IElementMementoProvider fgModuleMementoProvider = new ModuleMementoProvider();
 
 	/* (non-Javadoc)
@@ -59,7 +60,7 @@ public class CDebugElementAdapterFactory implements IAdapterFactory {
 				return fgModuleContentProvider;
 			}
 		}
-		if ( adapterType.equals( IModelProxyFactoryAdapter.class ) ) {
+		if ( adapterType.equals( IModelProxyFactory.class ) ) {
 			if ( adaptableObject instanceof IModuleRetrieval ) {
 				return fgModuleProxyFactory;
 			}
@@ -79,7 +80,7 @@ public class CDebugElementAdapterFactory implements IAdapterFactory {
 		return new Class[] {
 				IElementLabelProvider.class,
 				IElementContentProvider.class,
-				IModelProxyFactoryAdapter.class,
+				IModelProxyFactory.class,
         		IElementMementoProvider.class,
 			};
 	}
--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/views/modules/ModuleProxyFactory.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/views/modules/ModuleProxyFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 QNX Software Systems and others.
+ * Copyright (c) 2004, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,19 +7,20 @@
  *
  * Contributors:
  * QNX Software Systems - Initial API and implementation
+ * IBM Corporation
  *******************************************************************************/
 package org.eclipse.cdt.debug.internal.ui.views.modules; 
 
 import org.eclipse.cdt.debug.core.model.IModuleRetrieval;
 import org.eclipse.cdt.debug.ui.ICDebugUIConstants;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IModelProxy;
-import org.eclipse.debug.internal.ui.viewers.model.provisional.IModelProxyFactoryAdapter;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IModelProxyFactory;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
  
-public class ModuleProxyFactory implements IModelProxyFactoryAdapter {
+public class ModuleProxyFactory implements IModelProxyFactory {
 
 	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.viewers.provisional.IModelProxyFactoryAdapter#createModelProxy(java.lang.Object, org.eclipse.debug.internal.ui.viewers.provisional.IPresentationContext)
+	 * @see org.eclipse.debug.internal.ui.viewers.provisional.IModelProxyFactory#createModelProxy(java.lang.Object, org.eclipse.debug.internal.ui.viewers.provisional.IPresentationContext)
 	 */
 	public IModelProxy createModelProxy( Object element, IPresentationContext context ) {
 		if ( ICDebugUIConstants.ID_MODULES_VIEW.equals( context.getId() ) ) {

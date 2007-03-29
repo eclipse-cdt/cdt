51b4189f67707a08c66db5cb539733998895ed52
Andrew Ferguson <andrew.ferguson@symbian.com>
2007-03-29 16:50:41 +0000

    add class template tests

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/pdom/tests/PDOMTests.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/pdom/tests/PDOMTests.java
@@ -44,6 +44,7 @@ public class PDOMTests extends TestSuite {
 		suite.addTest(CPPFieldTests.suite());
 		suite.addTest(CPPFunctionTests.suite());
 		suite.addTest(CPPVariableTests.suite());
+		suite.addTest(CPPClassTemplateTests.suite());
 		suite.addTest(MethodTests.suite());
 		suite.addTest(NamespaceTests.suite());
 		

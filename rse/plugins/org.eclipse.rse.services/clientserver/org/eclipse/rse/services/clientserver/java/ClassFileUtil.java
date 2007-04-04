910cb15848690fe612655473ead55d8a1f2aadd4
Kushal Munir < kmunir@ca.ibm.com>
2007-04-04 12:56:39 +0000

    [180606] [api] Add javadoc to indicate that BasicClassFileParser and ClassFileUtil should not be subclassed.

--- rse/plugins/org.eclipse.rse.services/clientserver/org/eclipse/rse/services/clientserver/java/ClassFileUtil.java
+++ rse/plugins/org.eclipse.rse.services/clientserver/org/eclipse/rse/services/clientserver/java/ClassFileUtil.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2006 IBM Corporation. All rights reserved.
+ * Copyright (c) 2006, 2007 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -11,7 +11,7 @@
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
- * {Name} (company) - description of contribution.
+ * Kushal Munir (IBM) - Update javadoc for class.
  ********************************************************************************/
 
 package org.eclipse.rse.services.clientserver.java;
@@ -22,7 +22,8 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Singleton class for obtaining various information about class files.
+ * Singleton class for obtaining various information about class files. This class is not
+ * intended to be subclassed by clients.
  */
 public class ClassFileUtil {
 	

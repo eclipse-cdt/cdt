8c367212a0ca40b6836ece2210d7577f0cd687fd
Andrew Ferguson <andrew.ferguson@symbian.com>
2007-04-24 16:08:23 +0000

    181936: upversion PDOM

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/PDOM.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/PDOM.java
@@ -75,7 +75,7 @@ import org.eclipse.core.runtime.Status;
 public class PDOM extends PlatformObject implements IIndexFragment, IPDOM {
 	protected Database db;
 
-	public static final int VERSION = 31;
+	public static final int VERSION = 32;
 	// 0 - the beginning of it all
 	// 1 - first change to kick off upgrades
 	// 2 - added file inclusions
@@ -108,6 +108,7 @@ public class PDOM extends PlatformObject implements IIndexFragment, IPDOM {
 	// 29 - includes: fixed modelling of unresolved includes (180159)
 	// 30 - templates: method/constructor templates, typedef specializations
 	// 31 - macros: added file locations
+	// 32 - support standalone function types (181936)
 
 	public static final int LINKAGES = Database.DATA_AREA;
 	public static final int FILE_INDEX = Database.DATA_AREA + 4;

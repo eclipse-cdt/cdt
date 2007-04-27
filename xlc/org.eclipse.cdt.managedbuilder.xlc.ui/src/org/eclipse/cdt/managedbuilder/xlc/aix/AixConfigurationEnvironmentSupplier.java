bd759a1e82e49f2f5410183b68897e4b84a46388
Chris Recoskie <recoskie@ca.ibm.com>
2007-04-27 02:19:35 +0000

    initial xlc integration

---
+++ xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/aix/AixConfigurationEnvironmentSupplier.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.xlc.aix;
+
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.envvar.IBuildEnvironmentVariable;
+import org.eclipse.cdt.managedbuilder.envvar.IConfigurationEnvironmentVariableSupplier;
+import org.eclipse.cdt.managedbuilder.envvar.IEnvironmentVariableProvider;
+import org.eclipse.cdt.managedbuilder.internal.envvar.BuildEnvVar;
+
+public class AixConfigurationEnvironmentSupplier implements
+		IConfigurationEnvironmentVariableSupplier {
+	static final String VARNAME = "PATH";
+	static final String BINPATH = "/usr/vac/bin";     
+	static final String DELIMITER_AIX = ":";    
+	static final String PROPERTY_DELIMITER = "path.separator"; 
+	static final String PROPERTY_OSNAME    = "os.name"; 
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.envvar.IConfigurationEnvironmentVariableSupplier#getVariable(java.lang.String, org.eclipse.cdt.managedbuilder.core.IConfiguration, org.eclipse.cdt.managedbuilder.envvar.IEnvironmentVariableProvider)
+	 */
+	public IBuildEnvironmentVariable getVariable(String variableName,
+			IConfiguration configuration, IEnvironmentVariableProvider provider) {
+		
+		if (variableName == null) return null;
+		if (!VARNAME.equalsIgnoreCase(variableName)) return null;
+		return new BuildEnvVar(VARNAME, BINPATH, 
+			IBuildEnvironmentVariable.ENVVAR_PREPEND, 
+			System.getProperty(PROPERTY_DELIMITER, DELIMITER_AIX)); 
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.envvar.IConfigurationEnvironmentVariableSupplier#getVariables(org.eclipse.cdt.managedbuilder.core.IConfiguration, org.eclipse.cdt.managedbuilder.envvar.IEnvironmentVariableProvider)
+	 */
+	public IBuildEnvironmentVariable[] getVariables(
+			IConfiguration configuration, IEnvironmentVariableProvider provider) {
+		
+		IBuildEnvironmentVariable[] tmp = new IBuildEnvironmentVariable[1];   
+		tmp[0] = getVariable(VARNAME, configuration, provider);
+		if (tmp[0] != null) return tmp; 
+		return null;
+	}
+}
---
+++ xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/ui/HiddenOptionApplicabilityCalculator.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.xlc.ui;
+
+import org.eclipse.cdt.managedbuilder.core.IBuildObject;
+import org.eclipse.cdt.managedbuilder.core.IHoldsOptions;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.IOptionApplicability;
+
+/**
+	This class decides whether the make shared object
+	option is visible,enabled or used in command line or not */
+
+public class HiddenOptionApplicabilityCalculator implements
+		IOptionApplicability {
+
+	/**
+	 * This function decides if the option for which this class 
+	 * is option applicability calculator is enabled or not.
+	 */
+	public boolean isOptionEnabled(IBuildObject configuration,
+			IHoldsOptions holder, IOption option) {
+		return false;
+	}
+
+	/**
+	 * This function decides if the option for which this class 
+	 * is option applicability calculator is used in command line 
+	 * or not.
+	 */
+
+	public boolean isOptionUsedInCommandLine(IBuildObject configuration,
+			IHoldsOptions holder, IOption option) {
+		return true;
+	}
+
+	/**
+	 * This function decides if the option for which this class 
+	 * is option applicability calculator is visible or not.
+	 */
+
+	public boolean isOptionVisible(IBuildObject configuration,
+			IHoldsOptions holder, IOption option) {
+
+		return false;
+	}
+
+}

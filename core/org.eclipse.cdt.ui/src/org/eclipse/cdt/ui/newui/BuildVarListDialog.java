5afe61ff79da2e307c0b6e0aa280e824b6f610a5
Oleg Krasilnikov <oleg.krasilnikov@intel.com>
2007-04-27 08:06:18 +0000

    Copyright update

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/BuildVarListDialog.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/BuildVarListDialog.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Intel Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Intel Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.ui.newui;
 
 import org.eclipse.core.variables.IStringVariable;
@@ -11,11 +21,13 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
-
 import org.eclipse.cdt.core.cdtvariables.ICdtVariable;
 
 import org.eclipse.cdt.internal.ui.wizards.indexwizards.Messages;
 
+/**
+ * Displays CDT variables dialog with ability to filter
+ */
 public class BuildVarListDialog extends ElementListSelectionDialog {
 
 	private static final String TYPE = UIMessages.getString("BuildVarListDialog_0"); //$NON-NLS-1$

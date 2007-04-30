13fc99911bd3ed32758edadfc7c6b66a44969c12
Doug Schaefer <doug.schaefer@windriver.com>
2007-04-30 15:17:10 +0000

    Use the bin's project as a mapped resource too.

--- launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/CApplicationLaunchShortcut.java
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/CApplicationLaunchShortcut.java
@@ -182,7 +182,7 @@ public class CApplicationLaunchShortcut implements ILaunchShortcut {
 				configType.newInstance(null, getLaunchManager().generateUniqueLaunchConfigurationNameFrom(bin.getElementName()));
 			wc.setAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_NAME, projectName);
 			wc.setAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, bin.getCProject().getElementName());
-			wc.setMappedResources(new IResource[] {bin.getResource()});
+			wc.setMappedResources(new IResource[] {bin.getResource(), bin.getResource().getProject()});
 			wc.setAttribute(ICDTLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY, (String) null);
 			wc.setAttribute(ICDTLaunchConfigurationConstants.ATTR_DEBUGGER_STOP_AT_MAIN, true);
 			wc.setAttribute(

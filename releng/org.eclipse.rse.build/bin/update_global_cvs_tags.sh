832e9f80328187a4c112d0f8164087ee3a3b364c
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-05-03 09:02:59 +0000

    Adding a script to update global CVS tags

---
+++ releng/org.eclipse.rse.build/bin/update_global_cvs_tags.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# This script can be used to apply a release tag or branch tag globally
+# to those files of the TM CVS Repository that are usually not seen. It
+# needs to run from command-line because under Eclipse, these top-level
+# modules and files would require checking out the entire TM module tree;
+# from commandline, this works with checking out the top-level directories
+# only.
+#
+# These files are named "readme.txt" in the top-level CVS modules, and
+# contribute to the tags shown in the Eclipse CVS Repository Explorer
+# when expanding the "Branches" or the "Tags" node.
+#
+# Before running this script, customize the "cvs tag" commands below.
+#
+curdir=`pwd`
+TOPMODULES="\
+  org.eclipse.tm.rse \
+  org.eclipse.tm.rse/doc \
+  org.eclipse.tm.rse/examples \
+  org.eclipse.tm.rse/features \
+  org.eclipse.tm.rse/plugins \
+  org.eclipse.tm.rse/releng \
+  org.eclipse.tm.rse/tests \
+  org.eclipse.tm.core \
+  org.eclipse.tm.core/discovery \
+  org.eclipse.tm.core/terminal \
+  org.eclipse.tm.core/thirdparty \
+"
+cvs co -l $TOPMODULES
+##exit 0
+for topmod in $TOPMODULES ; do
+  cd $topmod
+  #cvs tag R1_0 readme.txt
+  #cvs update -l -r R1_0 .
+  #cvs tag -b R1_0_maintenance readme.txt
+  #cvs tag -d v20060630 readme.txt
+  #cvs tag R2_0 readme.txt
+  cd $curdir
+done

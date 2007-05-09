ebaacc4b256fe83d1a1e51f95c5f614a8292d860
Randy Rohrbach <Randy.Rohrbach@Windriver.com>
2007-05-09 21:24:17 +0000

    Applied patch from Fran Litterio for Bugzilla 186256.
    Initial implementation of the variables view for Open Source.

--- plugins/org.eclipse.dd.dsf.debug.ui/src/org/eclipse/dd/dsf/debug/ui/viewmodel/variable/VariableVMProvider.java
+++ plugins/org.eclipse.dd.dsf.debug.ui/src/org/eclipse/dd/dsf/debug/ui/viewmodel/variable/VariableVMProvider.java
@@ -1,13 +1,11 @@
-/*******************************************************************************
- * Copyright (c) 2006 Wind River Systems and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+/**
+ * Copyright (c) 2006 Wind River Systems and others. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public
+ * License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Wind River Systems - initial API and implementation
- *******************************************************************************/
+ * Contributors: Wind River Systems - initial API and implementation
+ */
 package org.eclipse.dd.dsf.debug.ui.viewmodel.variable;
 
 import org.eclipse.dd.dsf.debug.ui.viewmodel.DebugViewSelectionRootLayoutNode;
@@ -17,19 +15,37 @@ import org.eclipse.dd.dsf.ui.viewmodel.IVMLayoutNode;
 import org.eclipse.dd.dsf.ui.viewmodel.IVMRootLayoutNode;
 import org.eclipse.dd.dsf.ui.viewmodel.dm.AbstractDMVMProvider;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IColumnPresentation;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IColumnPresentationFactory;
 import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
 
-/**
- * 
- */
 @SuppressWarnings("restriction")
-public class VariableVMProvider extends AbstractDMVMProvider {
-    public VariableVMProvider(AbstractVMAdapter adapter, IPresentationContext context, DsfSession session) {
+public class VariableVMProvider extends AbstractDMVMProvider implements
+		IColumnPresentationFactory {
+
+	public VariableVMProvider(AbstractVMAdapter adapter, IPresentationContext context, DsfSession session) {
         super(adapter, context, session);
-        IVMRootLayoutNode debugViewSelectionNode = new DebugViewSelectionRootLayoutNode(this); 
-        IVMLayoutNode localsNode = new LocalsLayoutNode(this, getSession());
-        debugViewSelectionNode.setChildNodes(new IVMLayoutNode[] { localsNode });
-        setRootLayoutNode(debugViewSelectionNode);
+        
+        /*
+         *  Create the top level node to deal with the root selection.
+         */
+        IVMRootLayoutNode debugViewSelection = new DebugViewSelectionRootLayoutNode(this);
+        
+        /*
+         *  Create the local variables nodes next. They represent the first level shown in the view.
+         */
+        IVMLayoutNode localsNode = new VariableLocalsLayoutNode(this, getSession());
+        debugViewSelection.setChildNodes(new IVMLayoutNode[] { localsNode });
+        
+        /*
+         * Create the next level which represents members of structs/unions/enums and elements of arrays.
+         */
+        IVMLayoutNode subExpressioNode = new VariableSubExpressionsLayoutNode(this, getSession());
+        localsNode.setChildNodes(new IVMLayoutNode[] { subExpressioNode });
+        
+        /*
+         *  Now set this schema set as the layout set.
+         */
+        setRootLayoutNode(debugViewSelection);
     }
 
     @Override

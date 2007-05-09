d5aad42f7056c96c2b00a4358210d1a7fcfb3f51
Anton Leherbauer <anton.leherbauer@windriver.com>
2007-05-09 11:04:18 +0000

    Undo bogus fix

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/contentassist/CContentAssistInvocationContext.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/contentassist/CContentAssistInvocationContext.java
@@ -109,9 +109,6 @@ public class CContentAssistInvocationContext extends ContentAssistInvocationCont
 		
 		fCNComputed = true;
 		
-		if (fTU == null) {
-			return null;
-		}
 		int offset = getParseOffset();
 		if (offset < 0) return null;
 		

9e9213cb95d815b27cd459ad5e9b491ec0488213
David McKnight <dmcknigh@ca.ibm.com>
2007-05-23 16:03:08 +0000

    [187732] if file doesn't exist, don't  try to get the parent

--- rse/plugins/org.eclipse.rse.files.ui/src/org/eclipse/rse/internal/files/ui/view/SystemViewRemoteFileAdapter.java
+++ rse/plugins/org.eclipse.rse.files.ui/src/org/eclipse/rse/internal/files/ui/view/SystemViewRemoteFileAdapter.java
@@ -569,11 +569,15 @@ public class SystemViewRemoteFileAdapter
 	{
 		//System.out.println("Inside getParent for: "+element);
 		IRemoteFile file = (IRemoteFile) element;
-		IRemoteFile parent = file.getParentRemoteFile();
-		if ((parent != null) && parent.getAbsolutePath().equals(file.getAbsolutePath()))
-			// should never happen but sometimes it does, leading to infinite loop.
-			parent = null;
-		return parent;
+		if (file.exists())
+		{
+			IRemoteFile parent = file.getParentRemoteFile();
+			if ((parent != null) && parent.getAbsolutePath().equals(file.getAbsolutePath()))
+				// should never happen but sometimes it does, leading to infinite loop.
+				parent = null;
+			return parent;
+		}
+		return null;
 	}
 
 	/**

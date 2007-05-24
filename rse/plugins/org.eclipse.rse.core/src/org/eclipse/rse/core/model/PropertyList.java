6e3638f499da0f8f53340e318604c282a9ce26a8
David Dykstal <david_dykstal@us.ibm.com>
2007-05-24 15:29:50 +0000

    [cleanup] copyrights

--- rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/model/PropertyList.java
+++ rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/model/PropertyList.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * David Dykstal (IBM) - initial API and implementation.
+ *******************************************************************************/
 package org.eclipse.rse.core.model;
 
 import java.util.ArrayList;

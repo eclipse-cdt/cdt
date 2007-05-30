afcbc59d64afac66593132043430a252f24fbfdf
Kushal Munir < kmunir@ca.ibm.com>
2007-05-30 12:43:40 +0000

    [189506] [api][breaking] Remove showActionBar(), showButtonBar() and showActions() in ISystemViewInputProvider.

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemViewInputProvider.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemViewInputProvider.java
@@ -92,23 +92,4 @@ public interface ISystemViewInputProvider extends IAdaptable {
 	 */
 	// TODO should possibly be deprecated or removed
 	public Viewer getViewer();
-
-	/**
-	 * @return true to show the action bar (ie, toolbar) above the viewer.
-	 * The action bar contains connection actions, predominantly.
-	 */
-	public boolean showActionBar();
-
-	/**
-	 * @return true to show the button bar above the viewer.
-	 * The tool bar contains "Get List" and "Refresh" buttons and is typicall
-	 * shown in dialogs that list only remote system objects.
-	 */
-	public boolean showButtonBar();
-
-	/**
-	 * @return true to show right-click popup actions on objects in the tree.
-	 */
-	public boolean showActions();
-
 }

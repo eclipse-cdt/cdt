0200895010b6d7cedd153fd5b0c7b8796bd0361f
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-05-31 15:47:14 +0000

    [190231] Move AbstractCacheManager from org.eclipse.rse.ui/subsystems to org.eclipse.rse.core

--- rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/internal/core/subsystems/AbstractCacheManager.java
+++ rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/internal/core/subsystems/AbstractCacheManager.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation. All rights reserved.
+ * Copyright (c) 2002, 2007 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -11,10 +11,10 @@
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
- * {Name} (company) - description of contribution.
- ********************************************************************************/
+ * Martin Oberhuber (Wind River) - [190231] Moved from org.eclipse.rse.ui/subsystems to core
+ *********************************************************************************/
 
-package org.eclipse.rse.core.internal.subsystems;
+package org.eclipse.rse.internal.core.subsystems;
 
 import org.eclipse.rse.core.subsystems.ICacheManager;
 

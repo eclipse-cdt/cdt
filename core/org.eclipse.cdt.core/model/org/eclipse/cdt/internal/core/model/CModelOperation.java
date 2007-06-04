f00fd4cd95bd35506858a846e71f04de834960fe
Anton Leherbauer <anton.leherbauer@windriver.com>
2007-06-04 10:20:52 +0000

    Fix for 190664: Change "(anon)" to "(anonymous)" in outline view Patch by Sergey Prigogin (Google)

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CModelOperation.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CModelOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2000, 2007 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -212,12 +212,12 @@ public abstract class CModelOperation implements IWorkspaceRunnable, IProgressMo
 	 * should be canceled. An operation should regularly call this method
 	 * during its operation so that the user can cancel it.
 	 *
-	 * @exception OperationCanceledException if cancelling the operation has been requested
+	 * @exception OperationCanceledException if canceling the operation has been requested
 	 * @see IProgressMonitor#isCanceled
 	 */
 	protected void checkCanceled() {
 		if (isCanceled()) {
-			throw new OperationCanceledException("operation.cancelled"); //$NON-NLS-1$
+			throw new OperationCanceledException("operation.canceled"); //$NON-NLS-1$
 		}
 	}
 

0d0ee5ddf5ef716ce26c4f0f76ecaaa7291d3029
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-06-05 11:36:59 +0000

    [cleanup] Update Copyrights

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/RSEAdapter.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/RSEAdapter.java
@@ -1,8 +1,9 @@
-/********************************************************************************
- * Copyright (c) 2006 IBM Corporation. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+/*******************************************************************************
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
@@ -12,7 +13,7 @@
  *
  * Contributors:
  * {Name} (company) - description of contribution.
- ********************************************************************************/
+ *******************************************************************************/
 package org.eclipse.rse.internal.ui;
 
 import org.eclipse.rse.ui.IRSEAdapter;
--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/view/SystemViewForm.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/view/SystemViewForm.java
@@ -1,9 +1,10 @@
-/********************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation. All rights reserved.
- * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+/*******************************************************************************
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
  * component that contains this file: David McKnight, Kushal Munir, 
@@ -12,7 +13,7 @@
  * 
  * Contributors:
  * Kevin Doyle (IBM) - [187553] - Removed code and related methods for toolbar/button bar.
- ********************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.rse.internal.ui.view;
 import java.util.Vector;

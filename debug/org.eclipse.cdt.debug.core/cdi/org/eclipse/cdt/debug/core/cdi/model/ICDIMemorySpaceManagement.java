3ab17f6cf87fa5f8849c8c3d245b1a5c975c5d75
Doug Schaefer <doug.schaefer@windriver.com>
2007-06-14 18:19:40 +0000

    Fix up copyrights.

--- debug/org.eclipse.cdt.debug.core/cdi/org/eclipse/cdt/debug/core/cdi/model/ICDIMemorySpaceManagement.java
+++ debug/org.eclipse.cdt.debug.core/cdi/org/eclipse/cdt/debug/core/cdi/model/ICDIMemorySpaceManagement.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
+ * Copyright (c) 2005, 2007 Freescale, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Freescale Semiconductor - Initial API
+ *     Freescale, Inc. - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.cdt.debug.core.cdi.model;

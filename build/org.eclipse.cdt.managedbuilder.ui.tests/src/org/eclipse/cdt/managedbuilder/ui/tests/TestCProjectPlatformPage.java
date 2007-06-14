81bcb26800d58eab415ea9121a67a00e3c45f644
Doug Schaefer <doug.schaefer@windriver.com>
2007-06-14 20:10:17 +0000

    Fix up copyrights.

--- build/org.eclipse.cdt.managedbuilder.ui.tests/src/org/eclipse/cdt/managedbuilder/ui/tests/TestCProjectPlatformPage.java
+++ build/org.eclipse.cdt.managedbuilder.ui.tests/src/org/eclipse/cdt/managedbuilder/ui/tests/TestCProjectPlatformPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 Innoopract Informationssysteme GmbH and others.
+ * Copyright (c) 2005, 2007 Innoopract Informationssysteme GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

5bb6232e141711d7aa80d932ba3fd3c90b8882fd
Doug Schaefer <doug.schaefer@windriver.com>
2007-06-14 19:36:57 +0000

    Fix up copyrights.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/preferences/CFileTypesPreferencePage.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/preferences/CFileTypesPreferencePage.java
@@ -1,14 +1,14 @@
-/**********************************************************************
- * Copyright (c) 2004, 2005 TimeSys Corporation and others.
+/*******************************************************************************
+ * Copyright (c) 2004, 2006 TimeSys Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
+ *
+ * Contributors:
  *     TimeSys Corporation - Initial implementation
  *     Qnx Software System
- **********************************************************************/
+ *******************************************************************************/
 package org.eclipse.cdt.internal.ui.preferences;
 
 
--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/util/StatusLineHandler.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/util/StatusLineHandler.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
- * Copyright (c) 2007 Nokia, Inc.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +9,7 @@
  *     IBM Corp. - Rational Software - initial implementation
  *     Markus Schorn (Wind River Systems)
  *     Ed Swartz (Nokia)
- *******************************************************************************/ 
+ *******************************************************************************/
 package org.eclipse.cdt.internal.ui.util;
 
 import org.eclipse.jface.action.IStatusLineManager;

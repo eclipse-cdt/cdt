5103a1e38b234cfd950a06b8a60bb799325dfc76
David McKnight <dmcknigh@ca.ibm.com>
2007-06-19 20:35:57 +0000

    [188889] apidoc change

--- rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/events/ISystemRemoteChangeEvents.java
+++ rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/events/ISystemRemoteChangeEvents.java
@@ -18,27 +18,72 @@ package org.eclipse.rse.core.events;
 
 /**
  * The event IDs sent when remote resources in the model change
+ * These IDs are used when creating SystemRemoteChangeEvent objects.  
+ * 
  */
 public interface ISystemRemoteChangeEvents 
 {
-	
 	/**
 	 * Event Type: a remote resource was added
+	 * 
+	 * The event stores the following event parameters:
+	 * <ul> 
+	 *  <li>resource - the remote resource object, or absolute name of the resource as would be given by calling getAbsoluteName on its remote adapter
+	 *  <li>resourceParent - the remote resource's parent object, or absolute name, if that is known. If it is non-null, this will aid in refreshing occurrences of that parent.
+	 *  <li>subsystem - the subsystem which contains this remote resource. This allows the search for impacts to be 
+	 *   limited to subsystems of the same parent factory, and to connections with the same hostname as the subsystem's connection.
+	 *  <li>oldName - on a rename operation, this is the absolute name of the resource prior to the rename
+	 *  <li>originatingViewer - optional. If set, this gives the viewer a clue that it should select the affected resource after refreshing its parent. 
+	 *    This saves sending a separate event to reveal and select the new created resource on a create event, for example.
+	 * </ul>
 	 */
 	public static final int SYSTEM_REMOTE_RESOURCE_CREATED = 1;	
 
 	/**
 	 * Event Type: a remote resource was removed
+	 * 
+	 *  The event stores the following event parameters:
+	 * <ul> 
+	 *  <li>resource - the remote resource object, or absolute name of the resource as would be given by calling getAbsoluteName on its remote adapter
+	 *  <li>resourceParent - the remote resource's parent object, or absolute name, if that is known. If it is non-null, this will aid in refreshing occurrences of that parent.
+	 *  <li>subsystem - the subsystem which contains this remote resource. This allows the search for impacts to be 
+	 *   limited to subsystems of the same parent factory, and to connections with the same hostname as the subsystem's connection.
+	 *  <li>oldName - on a rename operation, this is the absolute name of the resource prior to the rename
+	 *  <li>originatingViewer - optional. If set, this gives the viewer a clue that it should select the affected resource after refreshing its parent. 
+	 *    This saves sending a separate event to reveal and select the new created resource on a create event, for example.
+	 * </ul>
 	 */
 	public static final int SYSTEM_REMOTE_RESOURCE_DELETED = 2;	
 
 	/**
 	 * Event Type: a remote resource was changed
+	 * 
+	 *  The event stores the following event parameters:
+	 * <ul> 
+	 *  <li>resource - the remote resource object, or absolute name of the resource as would be given by calling getAbsoluteName on its remote adapter
+	 *  <li>resourceParent - the remote resource's parent object, or absolute name, if that is known. If it is non-null, this will aid in refreshing occurrences of that parent.
+	 *  <li>subsystem - the subsystem which contains this remote resource. This allows the search for impacts to be 
+	 *   limited to subsystems of the same parent factory, and to connections with the same hostname as the subsystem's connection.
+	 *  <li>oldName - on a rename operation, this is the absolute name of the resource prior to the rename
+	 *  <li>originatingViewer - optional. If set, this gives the viewer a clue that it should select the affected resource after refreshing its parent. 
+	 *    This saves sending a separate event to reveal and select the new created resource on a create event, for example.
+	 * </ul>
 	 */
 	public static final int SYSTEM_REMOTE_RESOURCE_CHANGED = 4;	
 
 	/**
 	 * Event Type: a remote resource was renamed
+	 * 
+	 * The event stores the following event parameters:
+	 * <ul> 
+	 *  <li>resource - the remote resource object, or absolute name of the resource as would be given by calling getAbsoluteName on its remote adapter
+	 *  <li>resourceParent - the remote resource's parent object, or absolute name, if that is known. If it is non-null, this will aid in refreshing occurrences of that parent.
+	 *  <li>subsystem - the subsystem which contains this remote resource. This allows the search for impacts to be 
+	 *   limited to subsystems of the same parent factory, and to connections with the same hostname as the subsystem's connection.
+	 *  <li>oldName - on a rename operation, this is the absolute name of the resource prior to the rename
+	 *  <li>originatingViewer - optional. If set, this gives the viewer a clue that it should select the affected resource after refreshing its parent. 
+	 *    This saves sending a separate event to reveal and select the new created resource on a create event, for example.
+	 * </ul>
 	 */
 	public static final int SYSTEM_REMOTE_RESOURCE_RENAMED = 8;
 	

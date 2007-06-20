7b4ffeff15f6bf71f6814803cb88ef0942704902
cvs2svn <cdt-dev@eclipse.org>
2007-06-20 18:58:26 +0000

    This commit was manufactured by cvs2svn to create branch 'cdt_3_1'.
    Cherrypick from master 2007-06-20 18:58:24 UTC Chris Recoskie <recoskie@ca.ibm.com> 'adding built-in macros and includes to xlc toolchain':
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/.classpath
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/.project
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/META-INF/MANIFEST.MF
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/about.html
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/about.ini
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/about.mappings
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/about.properties
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/build.properties
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/eclipse32.gif
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/plugin.properties
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/plugin.xml
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/aix/AixConfigurationEnvironmentSupplier.java
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/aix/AixPathResolver.java
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/ui/HiddenOptionApplicabilityCalculator.java
        xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/ui/XlcUIPlugin.java
        xlc/org.eclipse.cdt.xlc.feature/build.properties
        xlc/org.eclipse.cdt.xlc.feature/feature.xml
        xlc/org.eclipse.cdt.xlc.feature/rootfiles/epl-v10.html
        xlc/org.eclipse.cdt.xlc.feature/rootfiles/notice.html

---
+++ xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/aix/AixPathResolver.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.xlc.aix;
+
+import org.eclipse.cdt.managedbuilder.core.IBuildPathResolver;
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+
+public class AixPathResolver implements IBuildPathResolver {
+	static final String DELIMITER_AIX = ":";
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.managedbuilder.core.IBuildPathResolver#resolveBuildPaths(int, java.lang.String, java.lang.String, org.eclipse.cdt.managedbuilder.core.IConfiguration)
+	 */
+	public String[] resolveBuildPaths(int pathType, String variableName,
+			String variableValue, IConfiguration configuration) {
+		return variableValue.split(DELIMITER_AIX);
+	}
+}
---
+++ xlc/org.eclipse.cdt.managedbuilder.xlc.ui/src/org/eclipse/cdt/managedbuilder/xlc/ui/XlcUIPlugin.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.xlc.ui;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import java.util.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class XlcUIPlugin extends Plugin {
+	//The shared instance.
+	private static XlcUIPlugin plugin;
+	//Resource bundle.
+	private ResourceBundle resourceBundle;
+	
+	/**
+	 * The constructor.
+	 */
+	public XlcUIPlugin() {
+		super();
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+		resourceBundle = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static XlcUIPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = XlcUIPlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null) ? bundle.getString(key) : key;
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		try {
+			if (resourceBundle == null)
+				resourceBundle = ResourceBundle.getBundle("org.eclipse.cdt.managedbuilder.gnu.ui.XlcUIPluginResources"); //$NON-NLS-1$
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+		return resourceBundle;
+	}
+
+}

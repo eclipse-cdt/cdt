e8659be28ea073cccae69ce1e883d48d41fa9435
David Dykstal <david_dykstal@us.ibm.com>
2007-06-27 01:52:12 +0000

    [194215] formatted code in popup example, updated tutorial from example, tested

--- rse/examples/org.eclipse.rse.examples.tutorial/src/samples/ui/actions/ShowJarContents.java
+++ rse/examples/org.eclipse.rse.examples.tutorial/src/samples/ui/actions/ShowJarContents.java
@@ -12,6 +12,7 @@
  * 
  * Contributors:
  * Martin Oberhuber (Wind River) - Adapted original tutorial code to Open RSE.
+ * David Dykstal (IBM) - formatting for tutorial
  ********************************************************************************/
 
 package samples.ui.actions;
@@ -47,49 +48,42 @@ import org.eclipse.ui.IWorkbenchPart;
  * An action that runs a command to display the contents of a Jar file.
  * The plugin.xml file restricts this action so it only appears for .jar files.
  */
-public class ShowJarContents implements IObjectActionDelegate 
-{
+public class ShowJarContents implements IObjectActionDelegate {
 	private List _selectedFiles;
-	
+
 	/**
 	 * Constructor for ShowJarContents.
 	 */
-	public ShowJarContents() 
-	{
+	public ShowJarContents() {
 		_selectedFiles = new ArrayList();
 	}
 
-	protected Shell getShell()
-	{
+	protected Shell getShell() {
 		return SystemBasePlugin.getActiveWorkbenchShell();
 	}
-	
-	protected IRemoteFile getFirstSelectedRemoteFile()
-	{
-		if (_selectedFiles.size() > 0)
-		{
-			return (IRemoteFile)_selectedFiles.get(0);
+
+	protected IRemoteFile getFirstSelectedRemoteFile() {
+		if (_selectedFiles.size() > 0) {
+			return (IRemoteFile) _selectedFiles.get(0);
 		}
 		return null;
 	}
-	
-	protected ISubSystem getSubSystem()
-	{
+
+	protected ISubSystem getSubSystem() {
 		return getFirstSelectedRemoteFile().getParentRemoteFileSubSystem();
 	}
-	
-	
+
 	/* (non-Javadoc)
-	 * @see org.eclipse.rse.ui.actions.SystemAbstractPopupMenuExtensionAction#run()
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
 	public void run(IAction action) {
 		IRemoteFile selectedFile = getFirstSelectedRemoteFile();
 		String cmdToRun = "jar -tvf " + selectedFile.getAbsolutePath(); //$NON-NLS-1$
 		try {
 			runCommand(cmdToRun);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			String excType = e.getClass().getName();
-			MessageDialog.openError(getShell(), excType, excType+": "+e.getLocalizedMessage()); //$NON-NLS-1$
+			MessageDialog.openError(getShell(), excType, excType + ": " + e.getLocalizedMessage()); //$NON-NLS-1$
 			e.printStackTrace();
 		}
 	}
@@ -98,20 +92,17 @@ public class ShowJarContents implements IObjectActionDelegate
 		//get the Command subsystem associated with the current host
 		IHost myHost = getSubSystem().getHost();
 		IRemoteCmdSubSystem[] subsys = RemoteCommandHelpers.getCmdSubSystems(myHost);
-		for (int i=0; i<subsys.length; i++) {
+		for (int i = 0; i < subsys.length; i++) {
 			if (subsys[i].getSubSystemConfiguration().supportsCommands()) {
 				return subsys[i];
 			}
 		}
 		return null;
 	}
-	
-	
-	
-	public void runCommand(String command) throws Exception
-	{
+
+	public void runCommand(String command) throws Exception {
 		IRemoteCmdSubSystem cmdss = getRemoteCmdSubSystem();
-		if (cmdss!=null && cmdss.isConnected()) {
+		if (cmdss != null && cmdss.isConnected()) {
 			//Option A: run the command invisibly through SubSystem API
 			//runCommandInvisibly(cmdss, command);
 			//Option B: run the command invisibly through Service API
@@ -126,71 +117,63 @@ public class ShowJarContents implements IObjectActionDelegate
 	public static class StdOutOutputListener implements IHostShellOutputListener {
 		public void shellOutputChanged(IHostShellChangeEvent event) {
 			IHostOutput[] lines = event.getLines();
-			for(int i=0; i<lines.length; i++) {
+			for (int i = 0; i < lines.length; i++) {
 				System.out.println(lines[i]);
 			}
 		}
 	}
-	
-	/** New version of running commands through IShellService / IHostShell */
-	public void runCommandInvisiblyService(IRemoteCmdSubSystem cmdss, String command) throws Exception
-	{
-		if (cmdss instanceof IShellServiceSubSystem) {
-			IShellService shellService = ((IShellServiceSubSystem)cmdss).getShellService();
-	        String [] environment = new String[1];
-	        environment[0] = "AAA=BBB"; //$NON-NLS-1$
-	        String initialWorkingDirectory = "."; //$NON-NLS-1$
 
-	        IHostShell hostShell = shellService.launchShell(initialWorkingDirectory, environment, new NullProgressMonitor());
+	/** New version of running commands through IShellService / IHostShell */
+	public void runCommandInvisiblyService(IRemoteCmdSubSystem cmdss, String command) throws Exception {
+		if (cmdss instanceof IShellServiceSubSystem) {
+			IShellService shellService = ((IShellServiceSubSystem) cmdss).getShellService();
+			String[] environment = new String[1];
+			environment[0] = "AAA=BBB"; //$NON-NLS-1$
+			String initialWorkingDirectory = "."; //$NON-NLS-1$
+
+			IHostShell hostShell = shellService.launchShell(initialWorkingDirectory, environment, new NullProgressMonitor());
 			hostShell.addOutputListener(new StdOutOutputListener());
-	        //hostShell.writeToShell("pwd"); //$NON-NLS-1$
-	        //hostShell.writeToShell("echo ${AAA}"); //$NON-NLS-1$
-	        //hostShell.writeToShell("env"); //$NON-NLS-1$
-	        hostShell.writeToShell(command);
-	        hostShell.writeToShell("exit"); //$NON-NLS-1$
+			//hostShell.writeToShell("pwd"); //$NON-NLS-1$
+			//hostShell.writeToShell("echo ${AAA}"); //$NON-NLS-1$
+			//hostShell.writeToShell("env"); //$NON-NLS-1$
+			hostShell.writeToShell(command);
+			hostShell.writeToShell("exit"); //$NON-NLS-1$
 		}
 	}
-	
+
 	/** Old version of running commands through the command subsystem */
-	public void runCommandInvisibly(IRemoteCmdSubSystem cmdss, String command) throws Exception
-	{
+	public void runCommandInvisibly(IRemoteCmdSubSystem cmdss, String command) throws Exception {
 		command = command + cmdss.getParentRemoteCmdSubSystemConfiguration().getCommandSeparator() + "exit"; //$NON-NLS-1$
 		Object[] result = cmdss.runCommand(command, null, false, new NullProgressMonitor());
-		if (result.length>0 && result[0] instanceof IRemoteCommandShell) {
-			IRemoteCommandShell cs = (IRemoteCommandShell)result[0];
+		if (result.length > 0 && result[0] instanceof IRemoteCommandShell) {
+			IRemoteCommandShell cs = (IRemoteCommandShell) result[0];
 			while (cs.isActive()) {
 				Thread.sleep(1000);
 			}
 			Object[] output = cs.listOutput();
-			for (int i=0; i<output.length; i++) {
+			for (int i = 0; i < output.length; i++) {
 				if (output[i] instanceof IRemoteOutput) {
-					System.out.println(((IRemoteOutput)output[i]).getText());
+					System.out.println(((IRemoteOutput) output[i]).getText());
 				} else if (output[i] instanceof IRemoteError) {
-					System.err.println(((IRemoteError)output[i]).getText());
+					System.err.println(((IRemoteError) output[i]).getText());
 				}
 			}
 			cmdss.removeShell(cs);
 		}
 	}
 
-    public void selectionChanged(org.eclipse.jface.action.IAction action,
-            org.eclipse.jface.viewers.ISelection selection) 
-    { 
-    	_selectedFiles.clear();
-    	// store the selected jars to be used when running
-    	Iterator theSet = ((IStructuredSelection)selection).iterator();
-    	while (theSet.hasNext())
-    	 {
-    		 Object obj = theSet.next();
-    		 if (obj instanceof IRemoteFile)
-    		 {
-    			 _selectedFiles.add(obj);
-    		 }
-    	 }
-    }
+	public void selectionChanged(org.eclipse.jface.action.IAction action, org.eclipse.jface.viewers.ISelection selection) {
+		_selectedFiles.clear();
+		// store the selected jars to be used when running
+		Iterator theSet = ((IStructuredSelection) selection).iterator();
+		while (theSet.hasNext()) {
+			Object obj = theSet.next();
+			if (obj instanceof IRemoteFile) {
+				_selectedFiles.add(obj);
+			}
+		}
+	}
 
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		// TODO Auto-generated method stub
-		
 	}
 }

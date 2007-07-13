23375e6f89adf6795554c8763333a0273b82dee8
David McKnight <dmcknigh@ca.ibm.com>
2007-07-13 19:44:49 +0000

    [190010] search cancellation issues -best to have incomplete results

--- rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/internal/dstore/universal/miners/filesystem/UniversalSearchHandler.java
+++ rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/internal/dstore/universal/miners/filesystem/UniversalSearchHandler.java
@@ -122,8 +122,7 @@ public class UniversalSearchHandler extends Thread implements ICancellableHandle
 		_isDone = true;
 		
 		if (_isCancelled) {
-			_status.removeNestedData();
-			_dataStore.createObject(_status,"error", "cancelled");
+
 			_miner.statusCancelled(_status);
 		}
 		else {

79c143bd481a666031d8b28a423290c9a26e3123
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2007-07-25 11:45:42 +0000

    Possible NPE in "getUniqueIdentifier" if the plugin's bundle hasn't been loaded. Replaced by a constant string for the plugin id.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/CDebugCorePlugin.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/CDebugCorePlugin.java
@@ -122,13 +122,7 @@ public class CDebugCorePlugin extends Plugin {
 	 * @return the unique identifier of this plugin
 	 */
 	public static String getUniqueIdentifier() {
-		if ( getDefault() == null ) {
-			// If the default instance is not yet initialized,
-			// return a static identifier. This identifier must
-			// match the plugin id defined in plugin.xml
-			return PLUGIN_ID;
-		}
-		return getDefault().getBundle().getSymbolicName();
+		return PLUGIN_ID;
 	}
 
 	/**
--- debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/MIPlugin.java
+++ debug/org.eclipse.cdt.debug.mi.core/src/org/eclipse/cdt/debug/mi/core/MIPlugin.java
@@ -492,13 +492,7 @@ public class MIPlugin extends Plugin {
 	 * Convenience method which returns the unique identifier of this plugin.
 	 */
 	public static String getUniqueIdentifier() {
-		if (getDefault() == null) {
-			// If the default instance is not yet initialized,
-			// return a static identifier. This identifier must
-			// match the plugin id defined in plugin.xml
-			return PLUGIN_ID;
-		}
-		return getDefault().getBundle().getSymbolicName();
+		return PLUGIN_ID;
 	}
 
 	public void debugLog(String message) {

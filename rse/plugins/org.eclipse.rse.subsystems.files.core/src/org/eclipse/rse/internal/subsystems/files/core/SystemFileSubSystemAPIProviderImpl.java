68862c619ec5226bc53dcd8fa6b2bd48ecdab26f
Martin Oberhuber < martin.oberhuber@windriver.com>
2007-08-08 12:20:09 +0000

    Fix typo

--- rse/plugins/org.eclipse.rse.subsystems.files.core/src/org/eclipse/rse/internal/subsystems/files/core/SystemFileSubSystemAPIProviderImpl.java
+++ rse/plugins/org.eclipse.rse.subsystems.files.core/src/org/eclipse/rse/internal/subsystems/files/core/SystemFileSubSystemAPIProviderImpl.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2002, 2006 IBM Corporation. All rights reserved.
+ * Copyright (c) 2002, 2007 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -58,7 +58,7 @@ public class SystemFileSubSystemAPIProviderImpl
     // SYSTEMVIEWINPUTPROVIDER METHODS...
     // ----------------------------------
 	/**
-	 * Return the children objects to consistute the root elements in the system view tree.
+	 * Return the children objects to constitute the root elements in the system view tree.
 	 * We return all filter pools for this subsystem
 	 */
 	public Object[] getSystemViewRoots()

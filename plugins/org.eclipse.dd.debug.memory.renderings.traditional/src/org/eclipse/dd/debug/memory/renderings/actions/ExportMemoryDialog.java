f9da220e1b9a8ace2f1397f4b3ddcca7f1ef17ac
Ted Williams <ted@ted.net>
2007-08-27 20:26:38 +0000

    fixed copyrights

--- plugins/org.eclipse.dd.debug.memory.renderings.traditional/src/org/eclipse/dd/debug/memory/renderings/actions/ExportMemoryDialog.java
+++ plugins/org.eclipse.dd.debug.memory.renderings.traditional/src/org/eclipse/dd/debug/memory/renderings/actions/ExportMemoryDialog.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ted R Williams (Wind River Systems, Inc.) - initial implementation
+ *******************************************************************************/
+
 package org.eclipse.dd.debug.memory.renderings.actions;
 
 import java.io.File;

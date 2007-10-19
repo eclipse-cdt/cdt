58150201b7b7bd008e7622437de50a138989ee8a
Ken Ryall <ken.ryall@nokia.com>
2007-10-19 20:44:46 +0000

    Fix typo in previous commit.

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/CDebugUIUtils.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/CDebugUIUtils.java
@@ -127,10 +127,8 @@ public class CDebugUIUtils {
 							valueString = CDebugUIMessages.getString( "CDTDebugModelPresentation.24" ); //$NON-NLS-1$
 						label.append( valueString );
 					}
-					else if ( type == null ) {
-						if ( valueString.length() > 0 ) {
+					else if ( valueString.length() > 0 ) {
 							label.append( valueString );
-						}
 					}
 				}
 			}

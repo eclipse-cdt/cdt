ad73d6a16148b2a92cfc8c1042339f714ee23a8d
Pawel Piech <pawel.piech@windriver.com>
2007-12-11 20:05:07 +0000

    [209045] Added IDsfDebugUIConstants and a reference to it.

---
+++ plugins/org.eclipse.dd.dsf.debug.ui/src/org/eclipse/dd/dsf/debug/ui/IDsfDebugUIConstants.java
@@ -0,0 +1,16 @@
+package org.eclipse.dd.dsf.debug.ui;
+
+public interface IDsfDebugUIConstants {
+
+	/**
+	 * Debug UI plug-in identifier (value <code>"org.eclipse.debug.ui"</code>).
+	 */
+	public static final String PLUGIN_ID = "org.eclipse.dd.dsf.debug.ui"; //$NON-NLS-1$;
+	
+	/** Loaded shared library symbols image identifier. */
+	public static final String IMG_OBJS_SHARED_LIBRARY_SYMBOLS_LOADED= "icons/library_syms_obj.gif"; //$NON-NLS-1$
+	
+	/** Unloaded Shared library symbols image identifier. */
+	public static final String IMG_OBJS_SHARED_LIBRARY_SYMBOLS_UNLOADED= "icons/library_obj.gif"; //$NON-NLS-1$
+
+}

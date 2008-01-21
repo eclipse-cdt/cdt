c2d448fd860b5bf3f334b3b47762391ffdc0a517
Chris Recoskie <recoskie@ca.ibm.com>
2008-01-21 20:16:41 +0000

    Initial commit.  Patch from Leonardo Garcia (IBM)

---
+++ xlc/org.eclipse.cdt.make.xlc.core/src/org/eclipse/cdt/make/xlc/core/scannerconfig/DefaultXlCScannerInfoCollector.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial Implementation
+ *
+ */
+package org.eclipse.cdt.make.xlc.core.scannerconfig;
+
+import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector3;
+import org.eclipse.cdt.make.internal.core.scannerconfig2.PerProjectSICollector;
+import org.eclipse.cdt.managedbuilder.scannerconfig.IManagedScannerInfoCollector;
+
+/**
+ * @author laggarcia
+ *
+ */
+public class DefaultXlCScannerInfoCollector extends PerProjectSICollector
+		implements IScannerInfoCollector3, IManagedScannerInfoCollector {
+
+}

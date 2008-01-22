6c58a960298f56e66a553c61b6bbac8ae4bcf829
Chris Recoskie <recoskie@ca.ibm.com>
2008-01-22 18:46:42 +0000

    fix copyrights

--- xlc/org.eclipse.cdt.make.xlc.core/src/org/eclipse/cdt/make/xlc/core/scannerconfig/xl/XlCSpecsConsoleParser.java
+++ xlc/org.eclipse.cdt.make.xlc.core/src/org/eclipse/cdt/make/xlc/core/scannerconfig/xl/XlCSpecsConsoleParser.java
@@ -1,14 +1,13 @@
-/**
- * Copyright (c) 2006 IBM Corporation.
+/*******************************************************************************
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - Initial Implementation
  *
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.make.xlc.core.scannerconfig.xl;
 
 import java.util.List;

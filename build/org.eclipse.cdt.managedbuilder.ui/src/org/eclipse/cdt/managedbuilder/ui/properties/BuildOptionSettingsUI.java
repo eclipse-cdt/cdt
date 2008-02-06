622b2739c31ba837fb1c852077c75b4fe4e8deb9
Oleg Krasilnikov <oleg.krasilnikov@intel.com>
2008-02-06 11:47:46 +0000

    Check for isOptionVisible() is added to update procedure.

--- build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/properties/BuildOptionSettingsUI.java
+++ build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/properties/BuildOptionSettingsUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -415,8 +415,14 @@ public class BuildOptionSettingsUI extends AbstractToolSettingUI {
 		if (optionApplicability == null)
 			return;
 
-		// if the option is not enabled then disable it
 		IBuildObject config = fInfo;
+
+		if (!optionApplicability.isOptionVisible(config, holder, option )) {
+			fieldEditor.setEnabled(false, parent); // temporary: instead of setVisible
+			return;
+		} 
+		
+		// if the option is not enabled then disable it
 		if (!optionApplicability.isOptionEnabled(config, holder, option )) {
 			fieldEditor.setEnabled(false, parent);
 		} else {

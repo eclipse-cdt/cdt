65ff00c6073da67ab0ae5e474dea87c9acf25a93
Andrew Ferguson <andrew.ferguson@symbian.com>
2008-02-21 17:19:45 +0000

    Fix documentation, schema fix is contributed by Tim Kelly (Nokia)

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/index/URIRelativeLocationConverter.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/index/URIRelativeLocationConverter.java
@@ -17,20 +17,23 @@ import org.eclipse.core.filesystem.URIUtil;
 
 /**
  * A IIndexLocationConverter for converting relative paths within an index, by prefixing them
- * with the supplied base URI
+ * with the supplied base URI.<p>
+ * <em>Note: The supplied base URI must end with a forward slash</em>
  * <br>
  * This location converter is internal-representation-compatible with ResourceContainerRelativeLocationConverter
+ * @since 4.0
  */
 /*
- * Internal representation is uri relative path (non encoded form)
+ * The associated internal PDOM representation is the relative path (non encoded form)
  */
 public class URIRelativeLocationConverter implements IIndexLocationConverter {
 	private URI baseURI;
 	
 	/**
 	 * Constructs an URIRelativeLocationConverter which will relative paths
-	 * by prefixing the supplied base URI
-	 * @param baseURI
+	 * by prefixing the supplied base URI.
+	 * @param baseURI the URI which will form the absolute base that relative paths
+	 * are concatenated to. <em>Note: It must end with a forward slash</em>.
 	 */
 	public URIRelativeLocationConverter(URI baseURI) {
 		this.baseURI = baseURI;

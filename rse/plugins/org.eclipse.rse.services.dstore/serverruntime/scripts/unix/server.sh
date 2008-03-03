f015f165f597b2f3a2ca8ddb52a94c26bf23d54f
David McKnight <dmcknigh@ca.ibm.com>
2008-03-03 15:59:45 +0000

    [164300] Patrick Juhl - Fix for unix (Parameters for port selection)

--- rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/unix/server.sh
+++ rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/unix/server.sh
@@ -9,6 +9,7 @@
 # Contributors:
 # IBM Corporation - initial API and implementation
 # Martin Oberhuber (Wind River) - Fix for solaris (Bourne Shell export statement)
+# Patrick Juhl - Fix for unix (Parameters for port selection)
 #*******************************************************************************
 # Shell script to start an RSE communications server
 # This script will start the datastore server listening on an available socket
@@ -16,6 +17,6 @@ serverpath=.;
 CLASSPATH=.:dstore_extra_server.jar:dstore_core.jar:dstore_miners.jar:clientserver.jar:$CLASSPATH;
 export serverpath CLASSPATH
 if [ $1 ]                     
-then java -DA_PLUGIN_PATH=$serverpath -DDSTORE_TRACING_ON=false -Dclient.username=$1 -DDSTORE_SPIRIT_ON=true org.eclipse.dstore.core.server.Server 0 60000 &
+then java -DA_PLUGIN_PATH=$serverpath -DDSTORE_TRACING_ON=false -Dclient.username=$1 -DDSTORE_SPIRIT_ON=true org.eclipse.dstore.core.server.Server $1 &
 else java -DA_PLUGIN_PATH=$serverpath -DDSTORE_TRACING_ON=false -DDSTORE_SPIRIT_ON=true org.eclipse.dstore.core.server.Server 0 60000 &
 fi

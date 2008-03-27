e2578a62329bb0c81c4f1a6174e943018c509411
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-03-27 22:07:33 +0000

    [cleanup] Fix copyright headers

--- discovery/org.eclipse.rse.discovery/src/org/eclipse/rse/internal/discovery/Messages.java
+++ discovery/org.eclipse.rse.discovery/src/org/eclipse/rse/internal/discovery/Messages.java
@@ -1,7 +1,7 @@
 /********************************************************************************
- * Copyright (c) 2006, 2007 Symbian Software Ltd. All rights reserved.
+ * Copyright (c) 2006, 2008 Symbian Software Ltd. All rights reserved.
  * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
--- rse/plugins/org.eclipse.rse.services.files.ftp/src/org/eclipse/rse/internal/services/files/ftp/FTPServiceResources.java
+++ rse/plugins/org.eclipse.rse.services.files.ftp/src/org/eclipse/rse/internal/services/files/ftp/FTPServiceResources.java
@@ -1,13 +1,13 @@
 /********************************************************************************
- * Copyright (c) 2006, 2007, 2008 IBM Corporation. All rights reserved.
+ * Copyright (c) 2006, 2008 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
- * component that contains this file: David McKnight, Kushal Munir, 
- * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson, 
+ * component that contains this file: David McKnight, Kushal Munir,
+ * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson,
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
@@ -19,14 +19,14 @@ package org.eclipse.rse.internal.services.files.ftp;
 
 import org.eclipse.osgi.util.NLS;
 
-public class FTPServiceResources extends NLS 
+public class FTPServiceResources extends NLS
 {
 	private static String BUNDLE_NAME = "org.eclipse.rse.internal.services.files.ftp.FTPServiceResources";//$NON-NLS-1$
 	public static String FTP_File_Service_Deleting_Task;
 	public static String FTP_File_Service_Monitor_Format;
 	public static String FTP_File_Service_Name;
 	public static String FTP_File_Service_Description;
-	
+
 	static {
 		// load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, FTPServiceResources.class);

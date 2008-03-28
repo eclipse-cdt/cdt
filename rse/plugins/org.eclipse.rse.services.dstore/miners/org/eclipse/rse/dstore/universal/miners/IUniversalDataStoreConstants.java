b91376bf099e99fc9205295ea7bcd38974147b40
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-03-28 17:57:32 +0000

    [cleanup] Add API "since" Javadoc tags

--- rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/dstore/universal/miners/IUniversalDataStoreConstants.java
+++ rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/dstore/universal/miners/IUniversalDataStoreConstants.java
@@ -7,14 +7,15 @@
  *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
- * component that contains this file: David McKnight, Kushal Munir, 
- * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson, 
+ * component that contains this file: David McKnight, Kushal Munir,
+ * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson,
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
  * Martin Oberhuber (Wind River) - [183165] Do not implement constant interfaces
- * David McKnight   (IBM)        - [196624] dstore miner IDs should be String constants rather than dynamic lookup
- * David McKnight   (IBM)        - [209593] [api] add support for "file permissions" and "owner" properties for unix files
+ * David McKnight   (IBM) - [196624] dstore miner IDs should be String constants rather than dynamic lookup
+ * David McKnight   (IBM) - [209593] [api] add support for "file permissions" and "owner" properties for unix files
+ * Martin Oberhuber (Wind River) - [cleanup] Add API "since" Javadoc tags
  *******************************************************************************/
 
 package org.eclipse.rse.dstore.universal.miners;
@@ -25,33 +26,57 @@ public interface IUniversalDataStoreConstants
 	 * Miner names, used for logging
 	 */
 	public static final String UNIVERSAL_FILESYSTEM_MINER = "UniversalFileSystemMiner"; //$NON-NLS-1$
-	
-	
+
+
 	/*
 	 * Miner IDs
 	 */
+	/**
+	 * Universal Filesystem miner ID. Value =
+	 * "org.eclipse.rse.dstore.universal.miners.UniversalFileSystemMiner".
+	 * 
+	 * @since org.eclipse.rse.services.dstore 3.0
+	 */
 	public static final String UNIVERSAL_FILESYSTEM_MINER_ID = "org.eclipse.rse.dstore.universal.miners.UniversalFileSystemMiner"; //$NON-NLS-1$
+	/**
+	 * Universal Command miner ID. Value =
+	 * "org.eclipse.rse.dstore.universal.miners.CommandMiner".
+	 * 
+	 * @since org.eclipse.rse.services.dstore 3.0
+	 */
 	public static final String UNIVERSAL_COMMAND_MINER_ID = "org.eclipse.rse.dstore.universal.miners.CommandMiner"; //$NON-NLS-1$
+	/**
+	 * Universal Environment miner ID. Value =
+	 * "org.eclipse.rse.dstore.universal.miners.EnvironmentMiner".
+	 * 
+	 * @since org.eclipse.rse.services.dstore 3.0
+	 */
 	public static final String UNIVERSAL_ENVIRONMENT_MINER_ID = "org.eclipse.rse.dstore.universal.miners.EnvironmentMiner"; //$NON-NLS-1$
+	/**
+	 * Universal Process miner ID. Value =
+	 * "org.eclipse.rse.dstore.universal.miners.UniversalProcessMiner".
+	 * 
+	 * @since org.eclipse.rse.services.dstore 3.0
+	 */
 	public static final String UNIVERSAL_PROCESS_MINER_ID = "org.eclipse.rse.dstore.universal.miners.UniversalProcessMiner"; //$NON-NLS-1$
-	
-	
+
+
 	//
 	// Universal File descriptors for DataStore DataElements
 	//
 	public static final String MESSAGE_DESCRIPTOR = "universal.message"; //$NON-NLS-1$
 
-    public static final String UNIVERSAL_NODE_DESCRIPTOR = "universal.node"; //$NON-NLS-1$
-    public static final String UNIVERSAL_TEMP_DESCRIPTOR = "universal.temp"; //$NON-NLS-1$
+	public static final String UNIVERSAL_NODE_DESCRIPTOR = "universal.node"; //$NON-NLS-1$
+	public static final String UNIVERSAL_TEMP_DESCRIPTOR = "universal.temp"; //$NON-NLS-1$
 	public static final String UNIVERSAL_FILTER_DESCRIPTOR = "universal.FilterObject"; //$NON-NLS-1$
 	public static final String UNIVERSAL_FILE_DESCRIPTOR = "universal.FileObject"; //$NON-NLS-1$
 	public static final String UNIVERSAL_FOLDER_DESCRIPTOR = "universal.FolderObject"; //$NON-NLS-1$
-	
+
 	public static final String UNIVERSAL_ARCHIVE_FILE_DESCRIPTOR = "universal.ArchiveFileObject"; //$NON-NLS-1$
 	public static final String UNIVERSAL_VIRTUAL_FILE_DESCRIPTOR = "universal.VirtualFileObject"; //$NON-NLS-1$
 	public static final String UNIVERSAL_VIRTUAL_FOLDER_DESCRIPTOR = "universal.VirtualFolderObject"; //$NON-NLS-1$
 
-	
+
 	//
 	// Universal File Miner Commands
 	//
@@ -63,16 +88,16 @@ public interface IUniversalDataStoreConstants
 	public static final String C_CREATE_FILE = "C_CREATE_FILE"; //$NON-NLS-1$
 	public static final String C_CREATE_FOLDER = "C_CREATE_FOLDER"; //$NON-NLS-1$
 	public static final String C_QUERY_GET_REMOTE_OBJECT="C_QUERY_GET_REMOTE_OBJECT"; //$NON-NLS-1$
-	
+
 	public static final String C_DELETE = "C_DELETE"; //$NON-NLS-1$
 	public static final String C_DELETE_BATCH = "C_DELETE_BATCH"; //$NON-NLS-1$
 	public static final String C_RENAME = "C_RENAME"; //$NON-NLS-1$
 	public static final String C_COPY = "C_COPY"; //$NON-NLS-1$
 	public static final String C_COPY_BATCH = "C_COPY_BATCH"; //$NON-NLS-1$
-	
+
 	public static final String C_SEARCH = "C_SEARCH"; //$NON-NLS-1$
 	public static final String C_CANCEL = "C_CANCEL"; //$NON-NLS-1$
-	
+
 	public static final String C_SET_READONLY = "C_SET_READONLY";//$NON-NLS-1$
 	public static final String C_SET_LASTMODIFIED = "C_SET_LASTMODIFIED";//$NON-NLS-1$
 	public static final String C_QUERY_BASIC_PROPERTY = "C_QUERY_BASIC_PROPERTY";//$NON-NLS-1$
@@ -84,30 +109,31 @@ public interface IUniversalDataStoreConstants
 	public static final String C_GET_OSTYPE = "C_GET_OSTYPE";//$NON-NLS-1$
 	public static final String C_QUERY_CLASSNAME = "C_QUERY_CLASSNAME"; //$NON-NLS-1$
 	public static final String C_CREATE_TEMP = "C_CREATE_TEMP"; //$NON-NLS-1$
-	
+
 	// Download file command
 	public static final String C_DOWNLOAD_FILE = "C_DOWNLOAD_FILE"; //$NON-NLS-1$
-	
+
 	// Query system encoding command
 	public static final String C_SYSTEM_ENCODING = "C_SYSTEM_ENCODING"; //$NON-NLS-1$
-	
+
 	// Query unused port
 	public static final String C_QUERY_UNUSED_PORT = "C_QUERY_UNUSED_PORT"; //$NON-NLS-1$
-	
+
 	// Qualified class name command and return type
 	public static final String C_QUERY_QUALIFIED_CLASSNAME = "C_QUERY_QUALIFIED_CLASSNAME"; //$NON-NLS-1$
 	public static final String TYPE_QUALIFIED_CLASSNAME = "fullClassName"; //$NON-NLS-1$
-	
+
 	// permissions commands
 	public static final String C_QUERY_FILE_PERMISSIONS = "C_QUERY_FILE_PERMISSIONS"; //$NON-NLS-1$
+	/** @since org.eclipse.rse.services.dstore 3.0 */
 	public static final String C_SET_FILE_PERMISSIONS = "C_SET_FILE_PERMISSIONS"; //$NON-NLS-1$
-	
+
 
 	// Mode of transfer: text or binary
 	public static final int TEXT_MODE = -1;
 	public static final int BINARY_MODE = -2;
-	
-	
+
+
 	// Download result types and download messages
 	public static final String DOWNLOAD_RESULT_SUCCESS_TYPE = "universal.download.success"; //$NON-NLS-1$
 	public static final String DOWNLOAD_RESULT_SUCCESS_MESSAGE = "successful"; //$NON-NLS-1$
@@ -116,14 +142,14 @@ public interface IUniversalDataStoreConstants
 	public static final String DOWNLOAD_RESULT_IO_EXCEPTION = "IOException"; //$NON-NLS-1$
 	public static final String DOWNLOAD_RESULT_EXCEPTION = "Exception"; //$NON-NLS-1$
 	public static final String DOWNLOAD_RESULT_UNEXPECTED_ERROR = "UnexpectedError"; //$NON-NLS-1$
-	
-	
+
+
 	// Number of bytes in a kilobyte
 	public static final int KB_IN_BYTES = 1024;
-	
+
 	// Number of kilobytes we want
 	public static final int NUM_OF_KB = 10;
-	
+
 	// The size of file segments to read and send across connection (in bytes)
 	public static final int BUFFER_SIZE = NUM_OF_KB * KB_IN_BYTES;
 }
--- rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/dstore/universal/miners/UniversalServerUtilities.java
+++ rse/plugins/org.eclipse.rse.services.dstore/miners/org/eclipse/rse/dstore/universal/miners/UniversalServerUtilities.java
@@ -1,25 +1,31 @@
 /********************************************************************************
  * Copyright (c) 2002, 2008 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
- * component that contains this file: David McKnight, Kushal Munir, 
- * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson, 
+ * component that contains this file: David McKnight, Kushal Munir,
+ * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson,
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
  * 
  * Contributors:
- * Noriaki Takatsu (IBM)  - [220126] [dstore][api][breaking] Single process server for multiple clients
+ * Noriaki Takatsu (IBM) - [220126] [dstore][api][breaking] Single process server for multiple clients
+ * Martin Oberhuber (Wind River) - [cleanup] Add API "since" Javadoc tags
  ********************************************************************************/
 
 package org.eclipse.rse.dstore.universal.miners;
 
 import org.eclipse.dstore.core.model.DataStore;
 
+/**
+ * Utilities for dstore servers.
+ * 
+ * @noextend This class is not intended to be subclassed by clients.
+ * @noinstantiate This class is not intended to be instantiated by clients.
+ */
 public class UniversalServerUtilities {
-
 
 	/**
 	 * logInfo
@@ -27,8 +33,9 @@ public class UniversalServerUtilities {
 	 * @param minerName
 	 * 
 	 * @param message Message text to be logged.
+	 * @since org.eclipse.rse.services.dstore 3.0
 	 */
-	public static void logInfo(String minerName, String message, DataStore dataStore) 
+	public static void logInfo(String minerName, String message, DataStore dataStore)
 	{
 		dataStore.getClient().getLogger().logInfo(minerName, message);
 	}
@@ -37,24 +44,24 @@ public class UniversalServerUtilities {
 	 * logWarning
 	 * 
 	 * @param minerName
-	 * 
 	 * @param message Message text to be logged.
+	 * @since org.eclipse.rse.services.dstore 3.0
 	 */
-	public static void logWarning(String minerName, String message, DataStore dataStore) 
+	public static void logWarning(String minerName, String message, DataStore dataStore)
 	{
 		dataStore.getClient().getLogger().logWarning(minerName, message);
 	}
-	
+
 	/**
 	 * logError
 	 * 
 	 * @param minerName
-	 * 
 	 * @param message Message text to be logged.
-	 * 
-	 * @param exception Exception that generated the error.  Used to print a stack trace.
+	 * @param exception Exception that generated the error. Used to print a
+	 *            stack trace.
+	 * @since org.eclipse.rse.services.dstore 3.0
 	 */
-	public static void logError(String minerName, String message, Throwable exception, DataStore dataStore) 
+	public static void logError(String minerName, String message, Throwable exception, DataStore dataStore)
 	{
 		dataStore.getClient().getLogger().logError(minerName, message, exception);
 	}
@@ -63,10 +70,10 @@ public class UniversalServerUtilities {
 	 * logDebugMessage
 	 * 
 	 * @param minerName
-	 * 
 	 * @param message Message text to be logged.
+	 * @since org.eclipse.rse.services.dstore 3.0
 	 */
-	public static void logDebugMessage(String minerName, String message, DataStore dataStore) 
+	public static void logDebugMessage(String minerName, String message, DataStore dataStore)
 	{
 		dataStore.getClient().getLogger().logDebugMessage(minerName, message);
 	}

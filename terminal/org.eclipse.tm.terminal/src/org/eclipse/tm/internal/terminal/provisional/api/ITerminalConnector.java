1692d941ab0dd0f3741041be39a7b82bc9797c11
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-04-04 15:23:10 +0000

    [200541][api][breaking] Rename terminalConnector -> terminalConnectors and add some Javadoc and @since tags

--- terminal/org.eclipse.tm.terminal/src/org/eclipse/tm/internal/terminal/provisional/api/ITerminalConnector.java
+++ terminal/org.eclipse.tm.terminal/src/org/eclipse/tm/internal/terminal/provisional/api/ITerminalConnector.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 Wind River Systems, Inc. and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
- * http://www.eclipse.org/legal/epl-v10.html 
- * 
- * Contributors: 
+ * Copyright (c) 2006, 2008 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  * Michael Scharf (Wind River) - initial API and implementation
  * Martin Oberhuber (Wind River) - fixed copyright headers and beautified
  *******************************************************************************/
@@ -17,19 +17,21 @@ import org.eclipse.core.runtime.IAdaptable;
 
 
 /**
- * Manage a single connection. Implementations of this class are contributed 
- * via <code>org.eclipse.tm.terminal.terminalConnector</code> extension point.
- * This class is a handle to a {@link ITerminalConnector connector} that comes from an
- * extension. It maintains {@link TerminalConnectorImpl} to the connector to allow lazy initialization of the
- * real {@link ITerminalConnector connector} that comes from an extension.
-
+ * Manage a single connection. Implementations of this class are contributed via
+ * <code>org.eclipse.tm.terminal.terminalConnectors</code> extension point.
+ * This class is a handle to a {@link ITerminalConnector connector} that comes
+ * from an extension. It maintains {@link TerminalConnectorImpl} to the
+ * connector to allow lazy initialization of the real
+ * {@link ITerminalConnector connector} that comes from an extension.
+ *
  * @author Michael Scharf
- * <p>
- * <strong>EXPERIMENTAL</strong>. This class or interface has been added as
- * part of a work in progress. There is no guarantee that this API will
- * work or that it will remain the same. Please do not use this API without
- * consulting with the <a href="http://www.eclipse.org/dsdp/tm/">Target Management</a> team.
- * </p>
+ *         <p>
+ *         <strong>EXPERIMENTAL</strong>. This class or interface has been
+ *         added as part of a work in progress. There is no guarantee that this
+ *         API will work or that it will remain the same. Please do not use this
+ *         API without consulting with the <a
+ *         href="http://www.eclipse.org/dsdp/tm/">Target Management</a> team.
+ *         </p>
  */
 public interface ITerminalConnector extends IAdaptable {
 	/**
@@ -48,7 +50,7 @@ public interface ITerminalConnector extends IAdaptable {
 	 * returns the error message.
 	 */
 	boolean isInitialized();
-	
+
 	/**
 	 * This method initializes the connector if it is not initialized!
 	 * If the connector was initialized successfully, <code>null</code> is
@@ -89,16 +91,16 @@ public interface ITerminalConnector extends IAdaptable {
     OutputStream getTerminalToRemoteStream();
 
 	/**
-	 * Load the state of this connection. Is typically called before 
+	 * Load the state of this connection. Is typically called before
 	 * {@link #connect(ITerminalControl)}.
-	 * 
-	 * @param store a string based data store. Short keys like "foo" can be used to 
+	 *
+	 * @param store a string based data store. Short keys like "foo" can be used to
 	 * store the state of the connection.
 	 */
 	void load(ISettingsStore store);
-	
+
 	/**
-	 * When the view or dialog containing the terminal is closed, 
+	 * When the view or dialog containing the terminal is closed,
 	 * the state of the connection is saved into the settings store <code>store</code>
 	 * @param store
 	 */
@@ -108,13 +110,13 @@ public interface ITerminalConnector extends IAdaptable {
 	 * @return a new page that can be used in a dialog to setup this connection.
 	 * The dialog should persist its settings with the {@link #load(ISettingsStore)}
 	 * and {@link #save(ISettingsStore)} methods.
-	 *  
+	 *
 	 */
 	ISettingsPage makeSettingsPage();
 
 	/**
 	 * @return A string that represents the settings of the connection. This representation
-	 * may be shown in the status line of the terminal view. 
+	 * may be shown in the status line of the terminal view.
 	 */
 	String getSettingsSummary();
 

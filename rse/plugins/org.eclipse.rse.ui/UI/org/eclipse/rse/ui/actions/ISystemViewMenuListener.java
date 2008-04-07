3c0b5335c32d66f070d592123b8bd165aab6d16e
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-04-07 10:12:15 +0000

    [cleanup] Adding @since tag

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/actions/ISystemViewMenuListener.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/actions/ISystemViewMenuListener.java
@@ -1,15 +1,15 @@
 /********************************************************************************
  * Copyright (c) 2008 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
  * component that contains this file: David McKnight.
- * 
+ *
  * Contributors:
- * David McKnight   (IBM)        - [225506] [api][breaking] RSE UI leaks non-API types
+ * David McKnight (IBM) - [225506] [api][breaking] RSE UI leaks non-API types
  ********************************************************************************/
 package org.eclipse.rse.ui.actions;
 
@@ -18,8 +18,10 @@ import org.eclipse.swt.events.ArmListener;
 import org.eclipse.swt.events.MenuListener;
 
 /**
- * An interface to be implemented for listening for menu show events for 
- * the purpose of applying mnemonics to the menu items.
+ * An interface to be implemented for listening for menu show events for the
+ * purpose of applying mnemonics to the menu items.
+ *
+ * @since 3.0
  */
 public interface ISystemViewMenuListener extends IMenuListener, MenuListener, ArmListener
 {

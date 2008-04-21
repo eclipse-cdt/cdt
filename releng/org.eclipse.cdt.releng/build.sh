d69ff9d58d5690231cb60443639ef24cbd58992b
Doug Schaefer <doug.schaefer@windriver.com>
2008-04-21 17:31:23 +0000

    *** empty log message ***

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -12,11 +12,34 @@ umask 0022
 
 cd `dirname $0`
 
+# Checkout basebuilder to run the build
 mkdir -p tools
 cd tools
 cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse \
 	checkout -r M5_34 org.eclipse.releng.basebuilder
 cd ..
+
+# Mylyn build dependencies
+set mylynTag=R_2_3_2
+mkdir -p mylyn
+cd mylyn
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.context.core
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.context.ui
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.monitor.core
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.monitor.ui
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.resources.ui
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.tasks.core
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/tools \
+	checkout -r $mylynTag org.eclipse.mylyn/org.eclipse.mylyn.tasks.ui
+cd ..
+mkdir -p results/plugins
+mv mylyn/org.eclipse/mylyn/* results/plugins
 
 java -jar tools/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar \
 	-ws gtk -arch ppc -os linux -application org.eclipse.ant.core.antRunner $*

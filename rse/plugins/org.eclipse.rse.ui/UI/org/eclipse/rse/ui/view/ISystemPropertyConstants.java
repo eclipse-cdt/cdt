6535099c751bce0c84a090b6d9308068fb8c911e
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-05-05 21:16:10 +0000

    [230298][api][breaking] ISystemPropertyConstants should not extend IBasicPropertyConstants

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemPropertyConstants.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/ui/view/ISystemPropertyConstants.java
@@ -7,23 +7,23 @@
  *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
- * component that contains this file: David McKnight, Kushal Munir, 
- * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson, 
+ * component that contains this file: David McKnight, Kushal Munir,
+ * Michael Berger, David Dykstal, Phil Coulthard, Don Yantzi, Eric Simpson,
  * Emily Bruner, Mazen Faraj, Adrian Storisteanu, Li Ding, and Kent Hawley.
- * 
+ *
  * Contributors:
- * David McKnight   (IBM)        - [209593] [api] add support for "file permissions" and "owner" properties for unix files
+ * David McKnight (IBM) - [209593] [api] add support for "file permissions" and "owner" properties for unix files
+ * Martin Oberhuber (Wind River) - [230298][api][breaking] ISystemPropertyConstants should not extend IBasicPropertyConstants
  *******************************************************************************/
 
 package org.eclipse.rse.ui.view;
-import org.eclipse.jface.viewers.IBasicPropertyConstants;
 /**
  * Constants that are the key values used to identify properties that populate the
  * Property Sheet viewer.
  */
-public interface ISystemPropertyConstants extends IBasicPropertyConstants
+public interface ISystemPropertyConstants
 {
-	public static final String P_PREFIX = org.eclipse.rse.ui.ISystemIconConstants.PREFIX;
+	public static final String P_PREFIX = "org.eclipse.rse.ui."; //$NON-NLS-1$
 	// GENERIC / COMMON
 	public static final String P_TYPE     = P_PREFIX+"type";	 //$NON-NLS-1$
 	public static final String P_NEWNAME  = P_PREFIX+"newName";		 //$NON-NLS-1$
@@ -38,9 +38,9 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 	public static final String P_HOSTNAME      = P_PREFIX+"hostname"; //$NON-NLS-1$
 	public static final String P_DEFAULTUSERID = P_PREFIX+"defaultuserid";	 //$NON-NLS-1$
 	public static final String P_DESCRIPTION   = P_PREFIX+"description";		 //$NON-NLS-1$
-	
+
 	// FILTER POOL PROPERTIES
-	//public static final String P_IS_SHARABLE = P_PREFIX+"sharable"; // transient	
+	//public static final String P_IS_SHARABLE = P_PREFIX+"sharable"; // transient
 
 	// FILTER PROPERTIES
 	public static final String P_FILTERSTRINGS = P_PREFIX+"filterstrings"; //$NON-NLS-1$
@@ -49,7 +49,7 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 	public static final String P_PARENT_FILTERPOOL   = P_PREFIX+"filterParentPool";			 //$NON-NLS-1$
 	public static final String P_RELATED_CONNECTION  = P_PREFIX+"filterRelatedConnection"; //$NON-NLS-1$
 	public static final String P_IS_CONNECTION_PRIVATE  = P_PREFIX+"filterConnectionPrivate"; //$NON-NLS-1$
-		
+
 	// FILE PROPERTIES
 	public static final String P_FILE_LASTMODIFIED = P_PREFIX+"file.lastmodified"; //$NON-NLS-1$
 	public static final String P_FILE_SIZE         = P_PREFIX+"file.size";	 //$NON-NLS-1$
@@ -64,7 +64,7 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 	public static final String P_FILE_READABLE     = P_PREFIX+"file.readable"; //$NON-NLS-1$
 	public static final String P_FILE_WRITABLE     = P_PREFIX+"file.writable"; //$NON-NLS-1$
 	public static final String P_FILE_HIDDEN       = P_PREFIX+"file.hidden"; //$NON-NLS-1$
-	
+
 	/**
 	 * @since 3.0
 	 */
@@ -77,10 +77,10 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 	 * @since 3.0
 	 */
 	public static final String P_FILE_GROUP        = P_PREFIX+"file.group"; //$NON-NLS-1$
-	
-	
-	
-	
+
+
+
+
 	// SEARCH LOCATION PROPERTIES
 	public static final String P_SEARCH_LINE  = P_PREFIX+"search.line"; //$NON-NLS-1$
 	//public static final String P_SEARCH_CHAR_END    = P_PREFIX+"search.char.end";
@@ -88,17 +88,17 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 	// ARCHIVE FILE PROPERTIES
 	public static final String P_ARCHIVE_EXPANDEDSIZE 	= P_PREFIX+"archive.expandedsize"; //$NON-NLS-1$
 	public static final String P_ARCHIVE_COMMENT 		= P_PREFIX+"archive.comment"; //$NON-NLS-1$
-	
+
 	// VIRTUAL FILE PROPERTIES
 	public static final String P_VIRTUAL_COMPRESSEDSIZE 	= P_PREFIX+"virtual.compressedsize"; //$NON-NLS-1$
 	public static final String P_VIRTUAL_COMMENT 			= P_PREFIX+"virtual.comment"; //$NON-NLS-1$
 	public static final String P_VIRTUAL_COMPRESSIONRATIO 	= P_PREFIX+"virtual.compressionratio"; //$NON-NLS-1$
 	public static final String P_VIRTUAL_COMPRESSIONMETHOD 	= P_PREFIX+"virtual.compressionmethod"; //$NON-NLS-1$
-	
+
 	// SHELL PROPERTIES
 	public static final String P_SHELL_STATUS = P_PREFIX+"shell.status"; //$NON-NLS-1$
 	public static final String P_SHELL_CONTEXT = P_PREFIX+"shell.context"; //$NON-NLS-1$
-	
+
 	// ERROR PROPERTIES
 	public static final String P_ERROR_FILENAME    = P_PREFIX+"error.filename"; //$NON-NLS-1$
 	public static final String P_ERROR_LINENO      = P_PREFIX+"error.lineno"; //$NON-NLS-1$
@@ -108,8 +108,8 @@ public interface ISystemPropertyConstants extends IBasicPropertyConstants
 
 	// COMPILE TYPE PROPERTIES
 	public static final String P_COMPILETYPE_TYPES = P_PREFIX+"compiletypes.types"; //$NON-NLS-1$
-				
-	// MISCELLANEOUS PROPERTIES	
+
+	// MISCELLANEOUS PROPERTIES
 	public static final String P_USERID   = P_PREFIX+"userid"; //$NON-NLS-1$
 	public static final String P_PASSWORD = P_PREFIX+"password"; //$NON-NLS-1$
 	public static final String P_CCSID    = P_PREFIX+"ccsid"; //$NON-NLS-1$

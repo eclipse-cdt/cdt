03d512d2efb38951acc68f1a8539db796dbf0079
Marc Khouzam <marc.khouzam@ericsson.com>
2008-05-16 16:53:34 +0000

    Bug 232357 The CDT code we re-use uses the CDT constants that I copied to DSF, but uses them with a CDT prefix.  I had changed that prefix for DSF.  I put the prefix back so that the constants would match.

--- plugins/org.eclipse.dd.gdb/src/org/eclipse/dd/gdb/internal/provisional/IGDBLaunchConfigurationConstants.java
+++ plugins/org.eclipse.dd.gdb/src/org/eclipse/dd/gdb/internal/provisional/IGDBLaunchConfigurationConstants.java
@@ -10,18 +10,21 @@
  *******************************************************************************/
 package org.eclipse.dd.gdb.internal.provisional;
 
-import org.eclipse.dd.gdb.internal.GdbPlugin;
 
 public class IGDBLaunchConfigurationConstants {
+
+	// This is the ID used by CDT.  We must use the same because we still use some CDT code
+	// and we must be consistent, or else these values will not be found in the Launch
+    private static final String CDT_CONSTS_ID = "org.eclipse.cdt.debug.mi.core" ; //$NON-NLS-1$
 
 	//
 	// Taken from org.eclipse.cdt.debug.mi.core.IGDBServerMILaunchConfigurationConstants
 	//
-	public static final String ATTR_REMOTE_TCP = GdbPlugin.PLUGIN_ID + ".REMOTE_TCP"; //$NON-NLS-1$
-	public static final String ATTR_HOST = GdbPlugin.PLUGIN_ID + ".HOST"; //$NON-NLS-1$
-	public static final String ATTR_PORT = GdbPlugin.PLUGIN_ID + ".PORT"; //$NON-NLS-1$
-	public static final String ATTR_DEV = GdbPlugin.PLUGIN_ID + ".DEV"; //$NON-NLS-1$
-	public static final String ATTR_DEV_SPEED = GdbPlugin.PLUGIN_ID + ".DEV_SPEED"; //$NON-NLS-1$
+	public static final String ATTR_REMOTE_TCP = CDT_CONSTS_ID + ".REMOTE_TCP"; //$NON-NLS-1$
+	public static final String ATTR_HOST = CDT_CONSTS_ID + ".HOST"; //$NON-NLS-1$
+	public static final String ATTR_PORT = CDT_CONSTS_ID + ".PORT"; //$NON-NLS-1$
+	public static final String ATTR_DEV = CDT_CONSTS_ID + ".DEV"; //$NON-NLS-1$
+	public static final String ATTR_DEV_SPEED = CDT_CONSTS_ID + ".DEV_SPEED"; //$NON-NLS-1$
 	//
 	//
 	
@@ -39,33 +42,33 @@ public class IGDBLaunchConfigurationConstants {
 	 * Launch configuration attribute key. The value is the name of
 	 * the Debuger associated with a C/C++ launch configuration.
 	 */
-	public static final String ATTR_DEBUG_NAME = GdbPlugin.PLUGIN_ID + ".DEBUG_NAME"; //$NON-NLS-1$
+	public static final String ATTR_DEBUG_NAME = CDT_CONSTS_ID + ".DEBUG_NAME"; //$NON-NLS-1$
 
 	/**
 	 * Launch configuration attribute key. Boolean value to set the gdb command file
 	 * Debuger/gdb/MI property.
 	 */
-	public static final String ATTR_GDB_INIT = GdbPlugin.PLUGIN_ID + ".GDB_INIT"; //$NON-NLS-1$
+	public static final String ATTR_GDB_INIT = CDT_CONSTS_ID + ".GDB_INIT"; //$NON-NLS-1$
 
 	/**
 	 * Launch configuration attribute key. Boolean value to set the 'automatically load shared library symbols' flag of the debugger.
 	 */
-	public static final String ATTR_DEBUGGER_AUTO_SOLIB = GdbPlugin.PLUGIN_ID + ".AUTO_SOLIB"; //$NON-NLS-1$
+	public static final String ATTR_DEBUGGER_AUTO_SOLIB = CDT_CONSTS_ID + ".AUTO_SOLIB"; //$NON-NLS-1$
 
 //	/**
 //	 * Launch configuration attribute key. Boolean value to set the 'stop on shared library events' flag of the debugger.
 //	 */
-//	public static final String ATTR_DEBUGGER_STOP_ON_SOLIB_EVENTS = GdbPlugin.PLUGIN_ID + ".STOP_ON_SOLIB_EVENTS"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_STOP_ON_SOLIB_EVENTS = CDT_CONSTS_ID + ".STOP_ON_SOLIB_EVENTS"; //$NON-NLS-1$
 //
 	/**
 	 * Launch configuration attribute key. The value is a List (array of String) of directories for the search path of shared libraries.
 	 */
-	public static final String ATTR_DEBUGGER_SOLIB_PATH = GdbPlugin.PLUGIN_ID + ".SOLIB_PATH"; //$NON-NLS-1$
+	public static final String ATTR_DEBUGGER_SOLIB_PATH = CDT_CONSTS_ID + ".SOLIB_PATH"; //$NON-NLS-1$
 
 //	/**
 //	 * Launch configuration attribute key. The value is a List (array of String) of shared libraries to load symbols automatically.
 //	 */
-//	public static final String ATTR_DEBUGGER_AUTO_SOLIB_LIST = GdbPlugin.PLUGIN_ID + ".AUTO_SOLIB_LIST"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_AUTO_SOLIB_LIST = CDT_CONSTS_ID + ".AUTO_SOLIB_LIST"; //$NON-NLS-1$
 //
 	/**
 	 * Launch configuration attribute value. The key is ATTR_DEBUG_NAME.
@@ -90,18 +93,18 @@ public class IGDBLaunchConfigurationConstants {
 //	/**
 //	 * Launch configuration attribute key.  The value is a string specifying the identifier of the command factory to use.
 //	 */
-//	public static final String ATTR_DEBUGGER_COMMAND_FACTORY = GdbPlugin.PLUGIN_ID + ".commandFactory"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_COMMAND_FACTORY = CDT_CONSTS_ID + ".commandFactory"; //$NON-NLS-1$
 //
 //	/**
 //	 * Launch configuration attribute key.  The value is a string specifying the protocol to
 //	 * use.  For now only "mi", "mi1", "m2", "mi3" are supported.
 //	 */
-//	public static final String ATTR_DEBUGGER_PROTOCOL = GdbPlugin.PLUGIN_ID + ".protocol"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_PROTOCOL = CDT_CONSTS_ID + ".protocol"; //$NON-NLS-1$
 //
 //	/**
 //	 * Launch configuration attribute key.  The value is a boolean specifying the mode of the gdb console.
 //	 */
-//	public static final String ATTR_DEBUGGER_VERBOSE_MODE = GdbPlugin.PLUGIN_ID + ".verboseMode"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_VERBOSE_MODE = CDT_CONSTS_ID + ".verboseMode"; //$NON-NLS-1$
 //
 //	/**
 //	 * Launch configuration attribute value. The key is ATTR_DEBUGGER_VERBOSE_MODE.
@@ -110,7 +113,7 @@ public class IGDBLaunchConfigurationConstants {
 //	/**
 //	 * Launch configuration attribute key.  The value is a boolean specifying is debugger should use full pathname to set breakpoints.
 //	 */
-//	public static final String ATTR_DEBUGGER_FULLPATH_BREAKPOINTS = GdbPlugin.PLUGIN_ID + ".breakpointsFullPath"; //$NON-NLS-1$
+//	public static final String ATTR_DEBUGGER_FULLPATH_BREAKPOINTS = CDT_CONSTS_ID + ".breakpointsFullPath"; //$NON-NLS-1$
 //
 //	/**
 //	 * Launch configuration default attribute value. The key is ATTR_DEBUGGER_FULLPATH_BREAKPOINTS.

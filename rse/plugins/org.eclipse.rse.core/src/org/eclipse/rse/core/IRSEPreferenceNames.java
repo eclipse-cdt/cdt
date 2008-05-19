3a8b16dc1173007b54e6243f5e3ceb0d8383e204
Martin Oberhuber < martin.oberhuber@windriver.com>
2008-05-19 19:30:20 +0000

    [cleanup] Get rid of invalid @noreference on final fields

--- rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/IRSEPreferenceNames.java
+++ rse/plugins/org.eclipse.rse.core/src/org/eclipse/rse/core/IRSEPreferenceNames.java
@@ -14,7 +14,7 @@ package org.eclipse.rse.core;
 
 /**
  * These constants define the set of preference names that the RSE core uses.
- * 
+ *
  * @noimplement This interface is not intended to be implemented by clients.
  */
 
@@ -29,8 +29,8 @@ public interface IRSEPreferenceNames {
 	 * The key for the value that specifies that queries should be "deferred",
 	 * that is, run when needed and in the background, as nodes are asked for
 	 * their children. This value is not part of the API.
-	 * 
-	 * @noreference This field is not intended to be referenced by clients.
+	 *
+	 * This field is not intended to be referenced by clients.
 	 */
 	public static final String USE_DEFERRED_QUERIES = "useDeferredQueries"; //$NON-NLS-1$
 
@@ -38,8 +38,8 @@ public interface IRSEPreferenceNames {
 	 * The key for the default system type. Used when a system type is needed
 	 * but not declared when creating new connections (hosts) and for password
 	 * determination. This value is not part of the API.
-	 * 
-	 * @noreference This field is not intended to be referenced by clients.
+	 *
+	 * This field is not intended to be referenced by clients.
 	 */
 	public static final String SYSTEMTYPE = "systemtype"; //$NON-NLS-1$
 
@@ -48,8 +48,8 @@ public interface IRSEPreferenceNames {
 	 * values keyed by some key - usually a system type, a connection name, or a
 	 * combination of a connection name and subsystem. This value is not part of
 	 * the API.
-	 * 
-	 * @noreference This field is not intended to be referenced by clients.
+	 *
+	 * This field is not intended to be referenced by clients.
 	 */
 	public static final String USERIDPERKEY = "useridperkey"; //$NON-NLS-1$
 
@@ -58,7 +58,7 @@ public interface IRSEPreferenceNames {
 	 * alphabetical order. As profiles are activated, deactivated, or renamed
 	 * this string must be modified. This value is not part of the API.
 	 * 
-	 * @noreference This field is not intended to be referenced by clients.
+	 * This field is not intended to be referenced by clients.
 	 */
 	public static final String ACTIVEUSERPROFILES = "activeuserprofiles"; //$NON-NLS-1$
 
@@ -73,7 +73,7 @@ public interface IRSEPreferenceNames {
 	 * The key of the string containing the id of the boolean value to create a
 	 * local connection. Value is "CREATE_LOCAL_CONNECTION". This value is part
 	 * of the API and may be used to customize products.
-	 * 
+	 *
 	 * @since org.eclipse.rse.core 3.0
 	 */
 	public static final String CREATE_LOCAL_CONNECTION = "CREATE_LOCAL_CONNECTION"; //$NON-NLS-1$

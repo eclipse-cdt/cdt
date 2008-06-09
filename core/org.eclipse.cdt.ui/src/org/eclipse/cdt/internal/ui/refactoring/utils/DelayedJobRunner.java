92b04e261e8e4d66cf78fff98e887568e1a104e2
Markus Schorn <markus.schorn@windriver.com>
2008-06-09 13:19:40 +0000

    Fix copy-right notice.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/refactoring/utils/DelayedJobRunner.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/refactoring/utils/DelayedJobRunner.java
@@ -1,5 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Institute for Software, HSR Hochschule fuer Technik  
+ * Rapperswil, University of applied sciences and others
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *  
+ * Contributors: 
+ * Institute for Software - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.internal.ui.refactoring.utils;
-
 
 /**
  * Runs a job delayed by a given time when <code>runJob</code> gets called. <br>

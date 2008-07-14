ec6e6b4dc84fb7d60978c39e77d515ed9e6a381e
Marc Khouzam <marc.khouzam@ericsson.com>
2008-07-14 19:47:08 +0000

    Bug 240525 New MI context to represent thread-groups

---
+++ plugins/org.eclipse.dd.mi/src/org/eclipse/dd/mi/service/IMIExecutionGroupDMContext.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ericsson - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.dd.mi.service;
+
+import org.eclipse.dd.dsf.debug.service.IRunControl.IContainerDMContext;
+
+/**
+ * An execution group context object.  In the GDB/MI protocol, thread groups
+ * are represented by a string identifier, which is the basis for this context.
+ */
+public interface IMIExecutionGroupDMContext extends IContainerDMContext 
+{
+    /**
+     * Returns the GDB/MI thread group identifier of this context.
+     */
+    public String getGroupId();
+}

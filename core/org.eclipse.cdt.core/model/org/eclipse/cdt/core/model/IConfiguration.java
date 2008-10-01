8da6bfb0f0cc700de4b117f4da29b491ae3a2f44
Doug Schaefer <doug.schaefer@windriver.com>
2008-10-01 17:02:10 +0000

    CModel element to model a build/indexer config.

---
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/IConfiguration.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Wind River Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Doug Schaefer (Wind River Systems)
+ *******************************************************************************/
+package org.eclipse.cdt.core.model;
+
+import org.eclipse.cdt.core.parser.IScannerInfo;
+import org.eclipse.core.resources.IResource;
+
+/**
+ * Represents a build configuration.
+ * 
+ * @author Doug Schaefer
+ */
+public interface IConfiguration {
+
+	/**
+	 * Get the scanner info for the given resource in this configuration.
+	 * 
+	 * @param resource
+	 * @return scanner info for this resource in this configuration
+	 */
+	IScannerInfo getScannerInfo(IResource resource);
+
+}

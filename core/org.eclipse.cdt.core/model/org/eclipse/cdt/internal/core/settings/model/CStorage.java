43ee5141f2c1542cc158e73ff6ab7a95363ac01f
Anton Leherbauer <anton.leherbauer@windriver.com>
2008-11-07 10:23:49 +0000

    Fix Copyright dates

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/CStorage.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/CStorage.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2007 Intel Corporation and others.
+ * Copyright (c) 2007 2008 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Intel Corporation - Initial API and implementation
+ *     Intel Corporation - Initial API and implementation
+ *     Anton Leherbauer (Wind River Systems) - Bug 253911
  *******************************************************************************/
 package org.eclipse.cdt.internal.core.settings.model;
 

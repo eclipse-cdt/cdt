cf7a0d9b03ca595d1e7a26e45fd587cd65d029a1
David McKnight <dmcknigh@ca.ibm.com>
2008-11-07 19:17:31 +0000

    David McKnight   (IBM)        - [254614] Promptable filter's shouldn't require supportsCommands on the subsystem to be false

--- rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/view/SystemViewFilterReferenceAdapter.java
+++ rse/plugins/org.eclipse.rse.ui/UI/org/eclipse/rse/internal/ui/view/SystemViewFilterReferenceAdapter.java
@@ -31,6 +31,7 @@
  * David McKnight   (IBM)        - [238507] Promptable Filters refreshed after modifying filter strings
  * David McKnight   (IBM)        - [244824] filter not refreshed if child is "empty list" or system message node
  * David McKnight   (IBM)        - [249245] not showing inappropriate popup actions for: Refresh, Show In Table, Go Into, etc. 
+ * David McKnight   (IBM)        - [254614] Promptable filter's shouldn't require supportsCommands on the subsystem to be false
  *******************************************************************************/
 
 package org.eclipse.rse.internal.ui.view;
@@ -316,7 +317,7 @@ public class SystemViewFilterReferenceAdapter
 		final ISubSystemConfiguration ssf = SubSystemHelpers.getParentSubSystemConfiguration(referencedFilter);
 
 		// PROMPTING FILTER?...
-		if (promptable && !ssf.supportsCommands())
+		if (promptable)
 		{
 			final Object[] pchildren = new SystemMessageObject[1];
 			final Object pelement = element;

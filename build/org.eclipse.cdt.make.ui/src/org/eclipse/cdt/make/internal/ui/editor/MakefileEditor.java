2529e886bb3be760ea2282e6e5246cd9ce070576
Anton Leherbauer <anton.leherbauer@windriver.com>
2008-12-18 13:39:50 +0000

    Bug 172331 - Keyboard shortcuts don't work in Makefile Editor, patch by  Olexiy Buyanskyy

--- build/org.eclipse.cdt.make.ui/src/org/eclipse/cdt/make/internal/ui/editor/MakefileEditor.java
+++ build/org.eclipse.cdt.make.ui/src/org/eclipse/cdt/make/internal/ui/editor/MakefileEditor.java
@@ -424,4 +424,13 @@ public class MakefileEditor extends TextEditor implements ISelectionChangedListe
 		// see http://bugs.eclipse.org/186106
 		return false;
 	}
+	
+	/*
+	 * @see org.eclipse.ui.editors.text.TextEditor#initializeKeyBindingScopes()
+	 * @see http://bugs.eclipse.org/172331
+	 */
+	protected void initializeKeyBindingScopes() {
+		setKeyBindingScopes(new String [] { "org.eclipse.cdt.make.ui.makefileEditorScope" } ); //$NON-NLS-1$
+	}
+
 }

504ceda709d4c532b167e541ee920ad0c2d40de2
Mike Kucera <mkucera@ca.ibm.com>
2009-01-07 19:40:52 +0000

    reverting accidental commit

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/AbstractGNUSourceCodeParser.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/AbstractGNUSourceCodeParser.java
@@ -488,7 +488,6 @@ public abstract class AbstractGNUSourceCodeParser implements ISourceCodeParser {
     }
 
     public IASTTranslationUnit parse() {
-    	if(1==1) throw new RuntimeException("KABOOM");
         long startTime = System.currentTimeMillis();
         translationUnit();
         log.traceLog("Parse " //$NON-NLS-1$

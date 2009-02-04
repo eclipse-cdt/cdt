65ae93ef1c5a740e0051434edbea6817df673751
Anton Leherbauer <anton.leherbauer@windriver.com>
2009-02-04 08:02:35 +0000

    API cleanup - bug 260830

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/ICPathContainerPage.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/wizards/ICPathContainerPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2006 QNX Software Systems and others.
+ * Copyright (c) 2002, 2009 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,6 +29,8 @@ import org.eclipse.jface.wizard.IWizardPage;
  * </p>
  * 
  * @deprecated - use IPathEntryContainerPage 
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 
 @Deprecated

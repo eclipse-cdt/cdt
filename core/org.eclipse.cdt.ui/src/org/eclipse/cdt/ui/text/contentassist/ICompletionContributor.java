c012aa7daec941804c16b72143e151baaacfcf28
Anton Leherbauer <anton.leherbauer@windriver.com>
2009-02-04 07:48:54 +0000

    API cleanup - bug 260830

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/text/contentassist/ICompletionContributor.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/text/contentassist/ICompletionContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,8 @@ import org.eclipse.cdt.core.model.IWorkingCopy;
  * @deprecated Clients should extend the new extension point
  *             <tt>completionProprosalComputer</tt> and implement interface
  *             {@link ICompletionProposalComputer}
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 @Deprecated
 public interface ICompletionContributor {

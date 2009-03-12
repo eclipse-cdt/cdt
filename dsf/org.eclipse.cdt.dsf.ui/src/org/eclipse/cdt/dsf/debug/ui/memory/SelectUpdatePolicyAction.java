39de0e3a61bc90a159f272c14af86fa40e44bce3
Anton Leherbauer <anton.leherbauer@windriver.com>
2009-03-12 13:04:42 +0000

    [268339] Fix since tags

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/memory/SelectUpdatePolicyAction.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/memory/SelectUpdatePolicyAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Wind River Systems and others.
+ * Copyright (c) 2008, 2009 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,7 +34,7 @@ import org.eclipse.ui.IViewPart;
 
 /**
  * 
- * @since 1.0
+ * @since 2.0  Moved to different package, exists since 1.0.
  */
 public class SelectUpdatePolicyAction implements IMenuCreator, IViewActionDelegate, IDebugContextListener, IActionDelegate2 {
 
--- dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/debug/service/IFormattedValues.java
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/debug/service/IFormattedValues.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 Wind River Systems and others.
+ * Copyright (c) 2007, 2009 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,6 +78,9 @@ public interface IFormattedValues extends IDsfService {
     {
         private final String fFormatID;
         
+        /**
+		 * @since 2.0
+		 */
         public FormattedValueDMContext(IDsfService service, IDMContext parent, String formatId) {
             super(service, new IDMContext[] { parent });
             fFormatID = formatId;

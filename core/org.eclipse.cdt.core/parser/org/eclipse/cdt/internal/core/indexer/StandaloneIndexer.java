eb6a81c9f3cb0997a922b99ac74437bb62504262
Mike Kucera <mkucera@ca.ibm.com>
2009-03-26 18:09:59 +0000

    Bug 270160 - need StandaloneIndexer.setLanguageMapper()

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/indexer/StandaloneIndexer.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/indexer/StandaloneIndexer.java
@@ -244,6 +244,11 @@ public abstract class StandaloneIndexer {
 		return fMapper;
 	}
 	
+	
+	public void setLanguageMapper(ILanguageMapper mapper) {
+		fMapper = mapper;
+	}
+	
 	/**
 	 * Returns the logger.
 	 */

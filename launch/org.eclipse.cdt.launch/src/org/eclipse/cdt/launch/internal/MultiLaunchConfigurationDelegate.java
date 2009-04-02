4e2bb4061d6ca399c9b87e41241e8ccd2bdbd8da
Marc Khouzam <marc.khouzam@ericsson.com>
2009-04-02 19:56:55 +0000

    Remove unused import

--- launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/MultiLaunchConfigurationDelegate.java
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/internal/MultiLaunchConfigurationDelegate.java
@@ -3,7 +3,6 @@ package org.eclipse.cdt.launch.internal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.cdt.launch.internal.ui.LaunchMessages;

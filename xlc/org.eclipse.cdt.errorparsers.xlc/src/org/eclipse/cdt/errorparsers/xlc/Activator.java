b849a3be0627510aafba5fb926be7c3c97f5da54
Vivian Kong <vivkong@ca.ibm.com>
2009-04-09 19:51:46 +0000

    bug 268581 individual source bundles

--- xlc/org.eclipse.cdt.errorparsers.xlc/src/org/eclipse/cdt/errorparsers/xlc/Activator.java
+++ xlc/org.eclipse.cdt.errorparsers.xlc/src/org/eclipse/cdt/errorparsers/xlc/Activator.java
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.cdt.errorparsers.xlc";
+	public static final String PLUGIN_ID = "org.eclipse.cdt.errorparsers.xlc"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;

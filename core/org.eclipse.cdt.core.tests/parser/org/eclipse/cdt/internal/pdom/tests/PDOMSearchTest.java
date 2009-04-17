41cbe5212b47579389ad68e5a30c9041f5c2cb17
Markus Schorn <markus.schorn@windriver.com>
2009-04-17 13:54:39 +0000

    Adjust testcase for optimized C/C++ search.

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/pdom/tests/PDOMSearchTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/pdom/tests/PDOMSearchTest.java
@@ -128,8 +128,8 @@ public class PDOMSearchTest extends PDOMTestBase {
 		assertEquals(4, class1s.length);
 		assertTrue(class1s[0] instanceof ICPPClassType);
 		assertTrue(class1s[1] instanceof ICPPClassType);
-		assertTrue(class1s[2] instanceof ICPPMethod);
-		assertTrue(class1s[3] instanceof ICPPClassType);
+		assertTrue(class1s[2] instanceof ICPPClassType);
+		assertTrue(class1s[3] instanceof ICPPMethod);
 
 		/** result #1 * */
 		ICPPClassType class1 = (ICPPClassType) class1s[0];
@@ -165,12 +165,12 @@ public class PDOMSearchTest extends PDOMTestBase {
 		assertEquals("class1y", fields[1].getName());
 
 		/** result #3 * */
-		ICPPMethod method3 = (ICPPMethod) class1s[2];
+		ICPPMethod method3 = (ICPPMethod) class1s[3];
 		assertEquals("namespace1::Class1::Class1", getBindingQualifiedName(pdom.getLinkageImpls()[0].adaptBinding(method3)));
 		assertEquals(method3, methods[0]);
 
 		/** result #4 * */
-		ICPPClassType class4 = (ICPPClassType) class1s[3];
+		ICPPClassType class4 = (ICPPClassType) class1s[2];
 		assertEquals("namespace1::namespace2::Class1", getBindingQualifiedName(pdom.getLinkageImpls()[0].adaptBinding(class4)));
 		methods = class4.getDeclaredMethods();
 		assertEquals(0, methods.length);
@@ -179,9 +179,9 @@ public class PDOMSearchTest extends PDOMTestBase {
 		IBinding[] class2s = pdom.findBindings(Pattern.compile("Class2"), false, INDEX_FILTER, NULL_MONITOR);
 		assertEquals(4, class2s.length);
 		assertTrue(class2s[0] instanceof ICPPClassType);
-		assertTrue(class2s[1] instanceof ICPPMethod);
+		assertTrue(class2s[1] instanceof ICPPClassType);
 		assertTrue(class2s[2] instanceof ICPPClassType);
-		assertTrue(class2s[3] instanceof ICPPClassType);
+		assertTrue(class2s[3] instanceof ICPPMethod);
 
 		/** result #1 * */
 		ICPPClassType cls1 = (ICPPClassType) class2s[0];
@@ -194,16 +194,16 @@ public class PDOMSearchTest extends PDOMTestBase {
 		assertEquals("foo", methods[1].getName());
 
 		/** result #2 * */
-		ICPPMethod meth2 = (ICPPMethod) class2s[1];
+		ICPPMethod meth2 = (ICPPMethod) class2s[3];
 		assertEquals("Class2::Class2", getBindingQualifiedName(pdom.getLinkageImpls()[0].adaptBinding(meth2)));
 		assertEquals(meth2, methods[0]);
 
 		/** result #3 * */
-		ICPPClassType cls3 = (ICPPClassType) class2s[2];
+		ICPPClassType cls3 = (ICPPClassType) class2s[1];
 		assertEquals("namespace1::Class1::Class2", getBindingQualifiedName(pdom.getLinkageImpls()[0].adaptBinding(cls3)));
 
 		/** result #3 * */
-		ICPPClassType cls4 = (ICPPClassType) class2s[3];
+		ICPPClassType cls4 = (ICPPClassType) class2s[2];
 		assertEquals("namespace1::Class2", getBindingQualifiedName(pdom.getLinkageImpls()[0].adaptBinding(cls4)));
 		
 		/* Nested class references - namespace1::Class1::Class2 */

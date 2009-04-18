8950983042c9335f0bcf4bd7f3515177f1372910
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2009-04-18 02:11:07 +0000

    plugin for checkers ui elements

---
+++ codan/org.eclipse.cdt.codan.checkers.ui/src/org/eclipse/cdt/codan/checkers/ui/quickfix/CodanProblemMarkerResolutionGenerator.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Andrew Gvozdev
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrew Gvozdev  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.codan.checkers.ui.quickfix;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.ui.IMarkerResolution;
+import org.eclipse.ui.IMarkerResolutionGenerator;
+
+public class CodanProblemMarkerResolutionGenerator implements IMarkerResolutionGenerator {
+	public IMarkerResolution[] getResolutions(IMarker marker) {
+		return new IMarkerResolution[] { new QuickFixAssignmentInCondition() };
+	}
+}

d6767953a968d788f5c34706a2fdae70f618b4c1
Anton Leherbauer <anton.leherbauer@windriver.com>
2009-05-04 10:42:26 +0000

    Bug 269590 - Format error for global const typename

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/formatter/CodeFormatterVisitor.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/formatter/CodeFormatterVisitor.java
@@ -1358,14 +1358,14 @@ public class CodeFormatterVisitor extends CPPASTVisitor {
 	}
 
 	private int visit(IASTNamedTypeSpecifier node) {
+		if (scribe.printModifiers()) {
+			scribe.space();
+		}
 		if (node instanceof ICPPASTNamedTypeSpecifier) {
 			if (((ICPPASTNamedTypeSpecifier)node).isTypename()) {
 				scribe.printNextToken(Token.t_typename);
 				scribe.space();
 			}
-		}
-		if (scribe.printModifiers()) {
-			scribe.space();
 		}
 		node.getName().accept(this);
 		return PROCESS_SKIP;

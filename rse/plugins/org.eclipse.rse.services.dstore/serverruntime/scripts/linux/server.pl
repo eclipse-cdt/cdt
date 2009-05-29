52c7f69242f4059c7291be6fe797af6c3cd97a77
Martin Oberhuber < martin.oberhuber@windriver.com>
2009-05-29 19:19:25 +0000

    [cleanup] Fix copyright dates in .pl files

--- rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/linux/server.pl
+++ rse/plugins/org.eclipse.rse.services.dstore/serverruntime/scripts/linux/server.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #*******************************************************************************
-# Copyright (c) 2005, 2006 IBM Corporation, Wind River Systems, Inc. and others.
+# Copyright (c) 2005, 2008 IBM Corporation, Wind River Systems, Inc. and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at

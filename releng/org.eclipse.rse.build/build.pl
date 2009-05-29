a02763db00a5d4ee88d9d1aca0004fd4eab6de6e
Martin Oberhuber < martin.oberhuber@windriver.com>
2009-05-29 21:22:38 +0000

    [cleanup] Fix copyright dates in .pl files

--- releng/org.eclipse.rse.build/build.pl
+++ releng/org.eclipse.rse.build/build.pl
@@ -1,4 +1,15 @@
 #!/usr/bin/perl
+#*******************************************************************************
+# Copyright (c) 2006, 2007 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# David Dykstal (IBM) - initial API and implementation
+# Martin Oberhuber (Wind River) - ongoing maintenance
+#*******************************************************************************
 
 # Build script for Remote System Explorer
 # Authors: Dave Dykstal, Kushal Munir
--- releng/org.eclipse.rse.build/fetchBuilder.pl
+++ releng/org.eclipse.rse.build/fetchBuilder.pl
@@ -1,4 +1,15 @@
 #!/usr/bin/perl
+#*******************************************************************************
+# Copyright (c) 2006 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# David Dykstal (IBM) - initial API and implementation
+# Martin Oberhuber (Wind River) - ongoing maintenance
+#*******************************************************************************
 use warnings;
 
 print STDERR "Which tag do you want to fetch? (default is HEAD): ";

76c13438d753f3d460a1c90c9cbfbe5437949a46
Martin Oberhuber < martin.oberhuber@windriver.com>
2009-07-10 12:48:08 +0000

    [282996] Fix bundle version dependencies

--- rse/plugins/org.eclipse.rse.terminals.ui/src/org/eclipse/rse/internal/terminals/ui/views/RSETerminalConnector.java
+++ rse/plugins/org.eclipse.rse.terminals.ui/src/org/eclipse/rse/internal/terminals/ui/views/RSETerminalConnector.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2008, 2009 MontaVista Software, Inc and others
+ * Copyright (c) 2008, 2009 MontaVista Software, Inc. and others
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -31,7 +31,7 @@ public class RSETerminalConnector extends RSETerminalConnectorImpl implements IT
     public String getName() {
         return "rse_internal_connector";
     }
-    
+
     public boolean isHidden() {
     	return true;
     }

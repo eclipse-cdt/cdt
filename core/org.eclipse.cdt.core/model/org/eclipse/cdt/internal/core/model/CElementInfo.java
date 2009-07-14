e106e552683c0b005e515a0c6db6bb0be788b801
Vivian Kong <vivkong@ca.ibm.com>
2009-07-14 21:31:00 +0000

    backport bug 277410

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CElementInfo.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CElementInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2000, 2009 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ import org.eclipse.core.resources.IResource;
  * Holds cached structure and properties for a C element.
  * Subclassed to carry properties for specific kinds of elements.
  */
-class CElementInfo {
+public class CElementInfo {
 	/**
 	 * Shared empty collection used for efficiency.
 	 */

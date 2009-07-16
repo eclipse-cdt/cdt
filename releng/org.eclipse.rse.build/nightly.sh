9264b12c0ee548f6c6b59508f16fce028af36e4b
Martin Oberhuber < martin.oberhuber@windriver.com>
2009-07-16 18:41:04 +0000

    [283482] Provide execution environments for nightly builds (CDC-1.1, J2SE-1.4, J2SE-1.5)

--- releng/org.eclipse.rse.build/nightly.sh
+++ releng/org.eclipse.rse.build/nightly.sh
@@ -59,8 +59,9 @@ if [ "$3" != "" ]; then
 fi
 rm -rf "${buildDirectory}"
 
-#default value of the bootclasspath attribute used in ant javac calls.  
-bootclasspath="/shared/dsdp/JDKs/win32/j2sdk1.4.2_19/jre/lib/rt.jar:/shared/dsdp/JDKs/j2sdk1.4.2_19/jre/lib/jsse.jar"
+#default value of the bootclasspath attribute used in ant javac calls.
+#these pathes are valid on build.eclipse.org  
+bootclasspath="/shared/dsdp/JDKs/win32/j2sdk1.4.2_19/jre/lib/rt.jar:/shared/dsdp/JDKs/win32/j2sdk1.4.2_19/jre/lib/jsse.jar"
 bootclasspath_15="/shared/common/jdk-1.5.0_16/jre/lib/rt.jar"
 #bootclasspath_16="$builderDir/jdk/win32_16/jdk6/jre/lib/rt.jar"
 #bootclasspath_foundation="/shared/common/Java_ME_platform_SDK_3.0_EA/runtimes/cdc-hi/lib/rt.jar"

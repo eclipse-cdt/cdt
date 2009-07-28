3561a3e22b9f670b4d5a3d85460e7afd26de1c4b
Markus Schorn <markus.schorn@windriver.com>
2009-07-28 09:01:14 +0000

    Instantiation of default template argument, bug 284683.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPTemplateNonTypeParameter.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPTemplateNonTypeParameter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,7 +63,7 @@ public class CPPTemplateNonTypeParameter extends CPPTemplateParameter implements
 			return null;
 		
 		IValue val= Value.create(d, Value.MAX_RECURSION_DEPTH);
-		IType t= CPPVisitor.createType(d);
+		IType t= getType();
 		return new CPPTemplateArgument(val, t);
 	}
 

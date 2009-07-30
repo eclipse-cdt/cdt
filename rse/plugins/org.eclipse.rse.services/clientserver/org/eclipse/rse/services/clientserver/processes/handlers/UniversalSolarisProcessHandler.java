45a2400a0b232af66b972bf3fdb878f18794d91a
Martin Oberhuber < martin.oberhuber@windriver.com>
2009-07-30 07:59:59 +0000

    [releng] Fix version and api tooling filters for 3.1.1

--- rse/plugins/org.eclipse.rse.services/clientserver/org/eclipse/rse/services/clientserver/processes/handlers/UniversalSolarisProcessHandler.java
+++ rse/plugins/org.eclipse.rse.services/clientserver/org/eclipse/rse/services/clientserver/processes/handlers/UniversalSolarisProcessHandler.java
@@ -1,13 +1,13 @@
 /********************************************************************************
  * Copyright (c) 2009 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and is 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Initial Contributors:
  * The following IBM employees contributed to the Remote System Explorer
  * component that contains this file: David McKnight.
- * 
+ *
  * Contributors:
  * David McKnight      (IBM) - [175293] [dstore] Processes do not work on Dstore-UNIX connection to Solaris
  ********************************************************************************/
@@ -23,14 +23,21 @@ import org.eclipse.rse.services.clientserver.processes.IHostProcess;
 import org.eclipse.rse.services.clientserver.processes.IHostProcessFilter;
 
 /**
- * @since 3.1
+ * ProcessHandler implementation for Solaris. This is part of internal
+ * implementation, and should not be used by clients directly. Use
+ * {@link ProcessHandlerManager#getNewProcessHandler()} on a Solaris system
+ * instead.
+ * 
+ * @since 3.1.1
+ * @noextend This class is not intended to be subclassed by clients.
+ * @noinstantiate This class is not intended to be instantiated by clients.
  */
 public class UniversalSolarisProcessHandler extends UniversalAIXProcessHandler {
 
 	private static final String[] processAttributes = {"pid","ppid","comm","uid","user","gid","vsz","s"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
 	private static final String firstColumnHeader = "PID"; //$NON-NLS-1$
 
-	
+
 	public IHostProcess kill(IHostProcess process, String type)
 			throws Exception {
 		return super.kill(process, type);
@@ -43,7 +50,7 @@ public class UniversalSolarisProcessHandler extends UniversalAIXProcessHandler {
 	throws Exception
 	{
 		SortedSet results = new TreeSet(new ProcessComparator());
-		
+
 		// create the remote command with the UNIX specific attributes
 		String cmdLine = "/usr/bin/ps -A -o "; //$NON-NLS-1$
 		for (int i = 0; i < processAttributes.length; i++)
@@ -69,59 +76,59 @@ public class UniversalSolarisProcessHandler extends UniversalAIXProcessHandler {
 				nextLine = reader.readLine();
 				continue;
 			}
-		
+
 			String pid = (String) psLineContents.get("pid"); //$NON-NLS-1$
 			statusLine = pid + "|"; //$NON-NLS-1$
-			
+
 			// add the name to the status string
 			String name = (String) psLineContents.get("comm"); //$NON-NLS-1$
 			if (name == null) name = " "; //$NON-NLS-1$
 			statusLine = statusLine + name + "|"; //$NON-NLS-1$
-		
+
 			// add the status letter to the status string
 			String state = (String) psLineContents.get("s"); //$NON-NLS-1$
 			if (state == null) state = " "; //$NON-NLS-1$
 			String stateCode = convertToStateCode(state);
 			statusLine = statusLine + stateCode + "|"; //$NON-NLS-1$
-			
+
 			// add the Tgid
 			String tgid = (String) psLineContents.get("tgid"); //$NON-NLS-1$
 			if (tgid == null) tgid = " "; //$NON-NLS-1$
 			statusLine = statusLine + tgid + "|";				 //$NON-NLS-1$
-			
+
 			// add the Ppid
 			String pPid = (String) psLineContents.get("ppid"); //$NON-NLS-1$
 			if (pPid == null) pPid = " "; //$NON-NLS-1$
 			statusLine = statusLine + pPid + "|"; //$NON-NLS-1$
-		
+
 			// add the TracerPid
 			String tracerpid = (String) psLineContents.get("tracerpid"); //$NON-NLS-1$
 			if (tracerpid == null) tracerpid = " "; //$NON-NLS-1$
 			statusLine = statusLine + tracerpid + "|"; //$NON-NLS-1$
-			
+
 			String uid = (String) psLineContents.get("uid"); //$NON-NLS-1$
 			if (uid == null) uid = " "; //$NON-NLS-1$
 			statusLine = statusLine + uid + "|"; // add the uid to the status string //$NON-NLS-1$
-		
+
 			String username = (String) psLineContents.get("user"); //$NON-NLS-1$
 			if (username == null) username = " "; //$NON-NLS-1$
 			statusLine = statusLine + username + "|"; // add the username to the status string //$NON-NLS-1$
-			
+
 			// add the gid to the status string
 			String gid = (String) psLineContents.get("gid"); //$NON-NLS-1$
 			if (gid == null) gid = " "; //$NON-NLS-1$
 			statusLine = statusLine + gid + "|"; //$NON-NLS-1$
-			
+
 			// add the VmSize to the status string
 			String vmsize = (String) psLineContents.get("vsz"); //$NON-NLS-1$
 			if (vmsize == null) vmsize = " "; //$NON-NLS-1$
 			statusLine = statusLine + vmsize +"|"; //$NON-NLS-1$
-			
+
 			// add a dummy vmrss to the status string
 			// vmRss is not available on ZOS
 			String vmrss = " "; //$NON-NLS-1$
 			statusLine = statusLine + vmrss;
-			
+
 			if (rpfs.allows(statusLine))
 			{
 				UniversalServerProcessImpl usp = new UniversalServerProcessImpl(statusLine);

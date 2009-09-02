0df87d142eedc8bc1bca9852f016b85a7ef697fd
John Cortell <john.cortell@freescale.com>
2009-09-02 15:15:41 +0000

    Fixed typos in comment

--- dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/ThreadSafeAndProhibitedFromDsfExecutor.java
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/ThreadSafeAndProhibitedFromDsfExecutor.java
@@ -18,11 +18,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation idicating that given package, class, method, can be accessed on
+ * Annotation indicating that given package, class, method, can be accessed on
  * any thread, except on the dispatch thread of given DsfExecutor. 
  * <br> This restriction is desirable if it is expected that the implementation 
  * behavior is to block the calling thread and execute a transaction using an 
- * executor.  In this situation, if the call is made on the executor's dispach
+ * executor.  In this situation, if the call is made on the executor's dispatch
  * thread, the execution would dead-lock.
  * <br> 
  * If declared on package or type, a field or method could still be declared 

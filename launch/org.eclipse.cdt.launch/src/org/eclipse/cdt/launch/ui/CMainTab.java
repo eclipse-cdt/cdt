c0121fbe5fe0d0924aecc999724247d2e335ae95
Ken Ryall <ken.ryall@nokia.com>
2009-09-05 20:03:28 +0000

    Bug 286359. Executable not auto-selected

--- launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/ui/CMainTab.java
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/ui/CMainTab.java
@@ -718,6 +718,7 @@ public class CMainTab extends CLaunchConfigurationTab {
 	 * the specified project.
 	 */
 	protected void handleProjectButtonSelected() {
+		String currentProjectName = fProjText.getText();
 		ICProject project = chooseCProject();
 		if (project == null) {
 			return;
@@ -725,6 +726,15 @@ public class CMainTab extends CLaunchConfigurationTab {
 
 		String projectName = project.getElementName();
 		fProjText.setText(projectName);
+		if (currentProjectName.length() == 0)
+		{
+			// New project selected for the first time, set the program name default too.
+			IBinary[] bins = getBinaryFiles(project);
+			if (bins != null && bins.length == 1) {				
+				fProgText.setText(bins[0].getResource().getProjectRelativePath().toOSString());
+			}
+		
+		}
 	}
 
 	/**

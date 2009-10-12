9ffb4cb9f785a4243cc35973c1caaf60dce8ad23
Ken Ryall <ken.ryall@nokia.com>
2009-10-12 13:53:53 +0000

    Added "since" tag.

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/launching/GdbLaunchDelegate.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/launching/GdbLaunchDelegate.java
@@ -211,6 +211,7 @@ public class GdbLaunchDelegate extends LaunchConfigurationDelegate
 	/**
 	 * Method used to check that the project, program and binary are correct.
 	 * Can be overridden to avoid checking certain things.
+	 * @since 2.1
 	 */
 	protected IPath checkBinaryDetails(final ILaunchConfiguration config) throws CoreException {
 		// First verify we are dealing with a proper project.

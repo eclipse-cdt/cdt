15bf4874779af7a091d8af0af5b096e89dc30e61
David McKnight <dmcknigh@ca.ibm.com>
2009-11-09 20:52:09 +0000

    [291738] [efs] repeated queries to RSEFileStoreImpl.fetchInfo() in short time-span should be reduced

--- rse/plugins/org.eclipse.rse.efs/src/org/eclipse/rse/internal/efs/RSEFileStoreImpl.java
+++ rse/plugins/org.eclipse.rse.efs/src/org/eclipse/rse/internal/efs/RSEFileStoreImpl.java
@@ -83,7 +83,7 @@ public class RSEFileStoreImpl extends FileStore
 
 	// to help with with performance issues when eclipse makes excessing fetchInfo calls
 	private long _lastFetch = 0;
-	private int _fetchWaitThreshold = 5000;
+	private int _fetchWaitThreshold = 1000;
 	
 	//cached IRemoteFile object: an Object to avoid early class loading
 	private transient volatile IRemoteFile _remoteFile;
@@ -104,7 +104,7 @@ public class RSEFileStoreImpl extends FileStore
 				_fetchWaitThreshold = Integer.parseInt(waitStr);
 			}
 			catch (Exception e){
-				_fetchWaitThreshold = 5000;
+				_fetchWaitThreshold = 1000;
 			}
 		}
 	}

107aabe04c272ea5a9e30d8aa49f445cb63130b0
Andrew Gvozdev <angvoz.dev@gmail.com>
2009-11-20 04:46:14 +0000

    bug 292003: JUnit failure: managedbuilder testLinkedFolder intermittently fails Benchmarks updated to 6.0.0 level

--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/linkedFolder/Benchmarks/sources.mk
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/linkedFolder/Benchmarks/sources.mk
@@ -2,11 +2,11 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-C_SRCS := 
 O_SRCS := 
+C_SRCS := 
 OBJS := 
+C_DEPS := 
 ARCHIVES := 
-DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/linkedFolder/Benchmarks/subdir.mk
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/linkedFolder/Benchmarks/subdir.mk
@@ -4,30 +4,25 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-$(ROOT)/f1.c \
-$(ROOT)/f2.c 
+../f1.c \
+../f2.c 
 
 OBJS += \
-${addprefix ./, \
-f1.o \
-f2.o \
-}
+./f1.o \
+./f2.o 
 
-DEPS += \
-${addprefix ./, \
-f1.d \
-f2.d \
-}
+C_DEPS += \
+./f1.d \
+./f2.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MBS30.compiler.gnu.c'
-	@echo gcc -O0 -g3 -Wall -c -fmessage-length=0 -o$@ $<
-	@gcc -O0 -g3 -Wall -c -fmessage-length=0 -o$@ $< && \
-	echo -n $(@:%.o=%.d) $(dir $@) > $(@:%.o=%.d) && \
-	gcc -MM -MG -P -w -O0 -g3 -Wall -c -fmessage-length=0  $< >> $(@:%.o=%.d)
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -v -o"$@" "$<" && \
+	echo -n '$(@:%.o=%.d)' $(dir $@) > '$(@:%.o=%.d)' && \
+	gcc -MM -MG -P -w -O0 -g3 -Wall -c -fmessage-length=0 -v  "$<" >> '$(@:%.o=%.d)'
 	@echo 'Finished building: $<'
 	@echo ' '
 

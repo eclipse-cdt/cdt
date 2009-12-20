6fa46c7bb1b51e16477285aecf9f15ffe847cfb3
Andrew Gvozdev <angvoz.dev@gmail.com>
2009-12-20 04:39:51 +0000

    cleanup: renamed private fields, reformatted

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/parser/ExtendedScannerInfo.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/parser/ExtendedScannerInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,37 +14,36 @@ package org.eclipse.cdt.core.parser;
 import java.util.Map;
 
 /**
- * Implementation for the {@link IExtendedScannerInfo} interface. Allows to configure
- * the preprocessor.
+ * Implementation for the {@link IExtendedScannerInfo} interface. Allows to configure the preprocessor.
  */
 public class ExtendedScannerInfo extends ScannerInfo implements IExtendedScannerInfo {
 
-	private static final String [] EMPTY_STRING_ARRAY = new String[ 0 ];
-	private String [] m, i, localIncludePaths;
-	
-	public ExtendedScannerInfo()
-	{
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+	private String[] macroFiles;
+	private String[] includeFiles;
+	private String[] localIncludePaths;
+
+	public ExtendedScannerInfo() {
 	}
-	
-	public ExtendedScannerInfo( Map<String, String> d, String [] incs )
-	{
-		super(d,incs);
+
+	public ExtendedScannerInfo(Map<String, String> definedSymbols, String[] includePaths) {
+		super(definedSymbols, includePaths);
 	}
-	
-	public ExtendedScannerInfo( Map<String, String> d, String [] incs, String [] macros, String [] includes )
-	{
-		super(d,incs);
-		m = macros;
-		i = includes;
+
+	public ExtendedScannerInfo(Map<String, String> definedSymbols, String[] includePaths,
+			String[] macroFiles, String[] includeFiles) {
+
+		super(definedSymbols, includePaths);
+		this.macroFiles = macroFiles;
+		this.includeFiles = includeFiles;
 	}
-	
-	public ExtendedScannerInfo( IScannerInfo info )
-	{
+
+	public ExtendedScannerInfo(IScannerInfo info) {
 		super(info.getDefinedSymbols(), info.getIncludePaths());
 		if (info instanceof IExtendedScannerInfo) {
-			IExtendedScannerInfo einfo = (IExtendedScannerInfo)info;
-			m = einfo.getMacroFiles();
-			i = einfo.getIncludeFiles();
+			IExtendedScannerInfo einfo = (IExtendedScannerInfo) info;
+			macroFiles = einfo.getMacroFiles();
+			includeFiles = einfo.getIncludeFiles();
 			localIncludePaths = einfo.getLocalIncludePath();
 		}
 	}
@@ -53,23 +52,26 @@ public class ExtendedScannerInfo extends ScannerInfo implements IExtendedScanner
 	 * @see org.eclipse.cdt.core.parser.IExtendedScannerInfo#getMacroFiles()
 	 */
 	public String[] getMacroFiles() {
-		if( m == null ) return EMPTY_STRING_ARRAY;
-		return m;
+		if (macroFiles == null)
+			return EMPTY_STRING_ARRAY;
+		return macroFiles;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.cdt.core.parser.IExtendedScannerInfo#getIncludeFiles()
 	 */
 	public String[] getIncludeFiles() {
-		if( i == null ) return EMPTY_STRING_ARRAY;
-		return i;
+		if (includeFiles == null)
+			return EMPTY_STRING_ARRAY;
+		return includeFiles;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.cdt.core.parser.IExtendedScannerInfo#getLocalIncludePath()
 	 */
 	public String[] getLocalIncludePath() {
-		if ( localIncludePaths == null ) return EMPTY_STRING_ARRAY;
+		if (localIncludePaths == null)
+			return EMPTY_STRING_ARRAY;
 		return localIncludePaths;
 	}
 }

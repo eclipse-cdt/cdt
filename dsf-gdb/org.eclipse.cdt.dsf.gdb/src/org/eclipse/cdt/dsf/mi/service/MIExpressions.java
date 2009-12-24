9f337b74172d2845767867449f247cfc57f59d47
Marc Khouzam <marc.khouzam@ericsson.com>
2009-12-24 19:00:49 +0000

    [297798] Fix classCastException

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIExpressions.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIExpressions.java
@@ -11,7 +11,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.dsf.mi.service;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
@@ -747,24 +746,53 @@ public class MIExpressions extends AbstractDsfService implements IExpressions, I
 	 *            should be retrieved.
 	 * @param startIndex
 	 *            The starting index within the list of all children of the parent
-	 *            expression.
+	 *            expression.  Must be a positive integer.  
 	 * @param length
-	 *            The length or number of elements of the range requested
+	 *            The length or number of elements of the range requested.  
+	 *            Must be a positive integer.
 	 * @param rm
 	 *            The data request monitor that will contain the requested data
 	 */
 	public void getSubExpressions(IExpressionDMContext exprCtx, final int startIndex,
 			final int length, final DataRequestMonitor<IExpressionDMContext[]> rm) {
-		getSubExpressions(
-				exprCtx,
-				new DataRequestMonitor<IExpressionDMContext[]>(getExecutor(), rm) {
-					@Override
-					protected void handleSuccess() {
-						rm.setData((IExpressionDMContext[])Arrays.asList(getData()).subList(startIndex, startIndex + length).toArray());
-						rm.done();
-					}
-				});
+
+		if (startIndex < 0 || length < 0) {
+			rm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, INTERNAL_ERROR, "Invalid range for evaluating sub expressions.", null)); //$NON-NLS-1$
+			rm.done();
+			return;
+		}
+		
+		if (exprCtx instanceof MIExpressionDMC) {
+			getSubExpressions(
+					exprCtx,
+					new DataRequestMonitor<IExpressionDMContext[]>(getExecutor(), rm) {
+						@Override
+						protected void handleSuccess() {
+							IExpressionDMContext[] subExpressions = getData();
+
+							if (startIndex >= subExpressions.length || startIndex + length > subExpressions.length) {
+								rm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, REQUEST_FAILED, "Invalid range for evaluating sub expressions.", null)); //$NON-NLS-1$
+								rm.done();
+								return;
+							}
+							
+							IExpressionDMContext[] subRange = new IExpressionDMContext[length];
+							for (int i=0; i<subRange.length; i++) {
+								subRange[i] = subExpressions[i+startIndex]; 
+							}
+							rm.setData(subRange);
+							rm.done();
+						}
+					});
+		} else if (exprCtx instanceof InvalidContextExpressionDMC) {
+			rm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, INVALID_HANDLE, "Invalid context for evaluating expressions.", null)); //$NON-NLS-1$
+			rm.done();
+		} else {
+			rm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, INTERNAL_ERROR, "Invalid expression context.", null)); //$NON-NLS-1$
+			rm.done();
+		}		
 	}
+
 	
 	/**
 	 * Retrieves the count of children expressions of the specified expression

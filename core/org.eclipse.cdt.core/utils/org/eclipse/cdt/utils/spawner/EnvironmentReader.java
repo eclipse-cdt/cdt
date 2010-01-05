152e38ddb0b55ae0609b802bc168fa610eba3991
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-01-05 02:35:39 +0000

    bug 285349: JUnit failure: ManagedBuildEnvironmentTests.testEnvGetParams()

--- core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/EnvironmentReader.java
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/EnvironmentReader.java
@@ -10,44 +10,69 @@
  *******************************************************************************/
 package org.eclipse.cdt.utils.spawner;
 
-
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Properties;
 
+import org.eclipse.core.runtime.Platform;
+
 /**
+ * This class provides environment variables supplied as {@link Properties} class.
+ * 
  * @noextend This class is not intended to be subclassed by clients.
  * @noinstantiate This class is not intended to be instantiated by clients.
  */
 public class EnvironmentReader {
 	private static Properties envVars = null;
+	private static Properties envVarsNormalized = null;
 	private static ArrayList<String> rawVars = null;
 
-	public static synchronized Properties getEnvVars() {
-		if (null != envVars) {
-			return (Properties)envVars.clone();
+	private static synchronized void init() {
+		if (envVars==null) {
+			envVars = new Properties();
+			// on Windows environment variable names are case-insensitive
+			if (Platform.getOS().equals(Platform.OS_WIN32)) {
+				envVarsNormalized = new Properties();
+			} else {
+				envVarsNormalized = envVars;
+			}
+			rawVars = new ArrayList<String>();
+			Map<String, String> envMap = System.getenv();
+			for (String var : envMap.keySet()) {
+				String value = envMap.get(var);
+				envVars.setProperty(var, value);
+				if (envVarsNormalized!=envVars) {
+					envVarsNormalized.setProperty(var.toUpperCase(), value);
+				}
+				rawVars.add(var + "=" + value); //$NON-NLS-1$
+			}
+			rawVars.trimToSize();
 		}
-
-		envVars = new Properties();
-		rawVars = new ArrayList<String>();
-		Map<String,String> envMap = System.getenv();
-		for (String var : envMap.keySet()) {
-	        String value = envMap.get(var);
-			envVars.setProperty(var, value);
-			rawVars.add(var + "=" + value); //$NON-NLS-1$
-        }
-		rawVars.trimToSize();
-		return (Properties)envVars.clone();
 	}
 
+	/**
+	 * @return list of environment variables.
+	 */
+	public static Properties getEnvVars() {
+		init();
+		return (Properties) envVars.clone();
+	}
+
+	/**
+	 * @param key - name of environment variable (without $ sign).
+	 * @return value of environment variable.
+	 */
 	public static String getEnvVar(String key) {
-		Properties p = getEnvVars();
-		return p.getProperty(key);
+		init();
+		return envVarsNormalized.getProperty(key);
 	}
 
-	public static synchronized String[] getRawEnvVars() {
-		if (rawVars==null) 
-			getEnvVars();
+	/**
+	 * @deprecated since CDT 6.1. {@link #getEnvVars()} provides all the data.
+	 */
+	@Deprecated
+	public static String[] getRawEnvVars() {
+		init();
 		return rawVars.toArray(new String[rawVars.size()]);
 	}
 }

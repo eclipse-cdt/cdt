5110be1b2c17812b269bea2a5a41208249d674e3
Anton Leherbauer <anton.leherbauer@windriver.com>
2010-01-13 13:15:21 +0000

    Fix code formatter test case

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 Wind River Systems, Inc. and others.
+ * Copyright (c) 2007, 2010 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -523,10 +523,10 @@ public class CodeFormatterTest extends BaseUITestCase {
 		assertFormatterResult();
 	}
 
-	//class Example: { void foo() throw(int); };
+	//class Example { void foo() throw(int); };
 	//void Example::foo()throw(int){}
 
-	//class Example: {
+	//class Example {
 	//	void foo()
 	//		throw (int);
 	//};

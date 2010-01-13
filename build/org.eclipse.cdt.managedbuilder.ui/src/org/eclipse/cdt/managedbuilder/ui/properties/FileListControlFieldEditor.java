2ef049ea2889660e585070ef0dae6c388579ce72
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-01-13 02:54:50 +0000

    bug 237115: "Other objects" list does not resize Patch from Marc-Andre Laperle

--- build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/properties/FileListControlFieldEditor.java
+++ build/org.eclipse.cdt.managedbuilder.ui/src/org/eclipse/cdt/managedbuilder/ui/properties/FileListControlFieldEditor.java
@@ -150,7 +150,7 @@ public class FileListControlFieldEditor extends FieldEditor {
 		layout.marginHeight = 5;
 		layout.makeColumnsEqualWidth = false;
 		topLayout.setLayout(layout);
-		GridData gddata = new GridData(GridData.FILL_HORIZONTAL);
+		GridData gddata = new GridData(GridData.FILL_BOTH);
 		gddata.horizontalSpan = 2;
 		topLayout.setLayoutData(gddata);
 		// file list control

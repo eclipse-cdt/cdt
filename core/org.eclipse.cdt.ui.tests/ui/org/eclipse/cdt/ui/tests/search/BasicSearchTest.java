a3f2dca46467ddd2511e1a5ba207b840223af8c1
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2010-01-18 01:03:10 +0000

    Fixed a typo.

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/search/BasicSearchTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/search/BasicSearchTest.java
@@ -308,8 +308,8 @@ public class BasicSearchTest extends BaseUITestCase {
 	// }
 	public void testNewResultsOnSearchAgainA() throws Exception {
 		PDOMSearchQuery query= makeProjectQuery("foo");
-		assertOccurences(query, 2);
-		assertOccurences(query, 2);
+		assertOccurrences(query, 2);
+		assertOccurrences(query, 2);
 		
 		String newContent= "void bar() {}";
 		IFile file = fCProject.getProject().getFile(new Path("references.cpp"));
@@ -317,7 +317,7 @@ public class BasicSearchTest extends BaseUITestCase {
 		Job.getJobManager().join(ResourcesPlugin.FAMILY_AUTO_REFRESH, null);
 		assertTrue(CCorePlugin.getIndexManager().joinIndexer(360000, new NullProgressMonitor()));
 
-		assertOccurences(query, 1);
+		assertOccurrences(query, 1);
 	}
 	
 	// void foo() {}
@@ -325,8 +325,8 @@ public class BasicSearchTest extends BaseUITestCase {
 	// void bar() {foo();foo();foo();}
 	public void testNewResultsOnSearchAgainB() throws Exception {
 		PDOMSearchQuery query= makeProjectQuery("foo");
-		assertOccurences(query, 4);
-		assertOccurences(query, 4);
+		assertOccurrences(query, 4);
+		assertOccurrences(query, 4);
 		
 		// whitespace s.t. new match offset is same as older 
 		String newContent= "void bar() {      foo();      }";
@@ -337,22 +337,22 @@ public class BasicSearchTest extends BaseUITestCase {
 		indexManager.update(new ICElement[] {fCProject}, IIndexManager.UPDATE_ALL);
 		assertTrue(indexManager.joinIndexer(360000, new NullProgressMonitor()));
 
-		assertOccurences(query, 2);
+		assertOccurrences(query, 2);
 		
 		String newContent2= "void bar() {foo(); foo();}";
 		file.setContents(new ByteArrayInputStream(newContent2.getBytes()), IResource.FORCE, NPM);
 		Job.getJobManager().join(ResourcesPlugin.FAMILY_AUTO_REFRESH, null);
 		assertTrue(indexManager.joinIndexer(360000, new NullProgressMonitor()));
 
-		assertOccurences(query, 3);
+		assertOccurrences(query, 3);
 	}
 	
-	protected PDOMSearchQuery makeProjectQuery(String pattern) {
+	private PDOMSearchQuery makeProjectQuery(String pattern) {
 		String scope1= "Human Readable Description";
 		return new PDOMSearchPatternQuery(new ICElement[] {fCProject}, scope1, pattern, true, PDOMSearchQuery.FIND_ALL_OCCURRENCES | PDOMSearchPatternQuery.FIND_ALL_TYPES);
 	}
 	
-	protected void assertOccurences(PDOMSearchQuery query, int expected) {
+	private void assertOccurrences(PDOMSearchQuery query, int expected) {
 		query.run(NPM);
 		PDOMSearchResult result= (PDOMSearchResult) query.getSearchResult();
 		assertEquals(expected, result.getMatchCount());

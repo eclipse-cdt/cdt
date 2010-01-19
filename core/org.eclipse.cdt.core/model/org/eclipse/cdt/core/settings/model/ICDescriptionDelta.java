f068e6da17f9f4461e1e5a723d12562247ccf249
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-01-19 15:32:21 +0000

    cosmetics: JavaDoc tag fixed

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/settings/model/ICDescriptionDelta.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/settings/model/ICDescriptionDelta.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Intel Corporation and others.
+ * Copyright (c) 2007, 2010 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -94,9 +94,7 @@ public interface ICDescriptionDelta {
 	int getDeltaKind();
 	
 	/**
-	 * returns ored delta flags
-	 * 
-	 * @return
+	 * @return ored delta flags
 	 */
 	int getChangeFlags();
 	

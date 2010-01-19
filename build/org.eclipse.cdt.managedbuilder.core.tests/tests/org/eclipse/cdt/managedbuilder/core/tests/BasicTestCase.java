9f09d045669cd96f0854a870034057c23dce1caa
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-01-19 15:00:06 +0000

    cosmetics: @Override, generics etc

--- build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/BasicTestCase.java
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/BasicTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Intel Corporation and others.
+ * Copyright (c) 2010 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,19 +20,21 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 
 public class BasicTestCase extends TestCase {
-	private List fProjList = new LinkedList();
+	private List<IProject> fProjList = new LinkedList<IProject>();
 	
 	protected void addProject(IProject project){
 		fProjList.add(project);
 	}
 
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
 
+	@Override
 	protected void tearDown() throws Exception {
-		for(Iterator iter = fProjList.iterator(); iter.hasNext();){
-			IProject proj = (IProject)iter.next();
+		for(Iterator<IProject> iter = fProjList.iterator(); iter.hasNext();){
+			IProject proj = iter.next();
 			try {
 				proj.delete(true, null);
 			} catch (CoreException e){

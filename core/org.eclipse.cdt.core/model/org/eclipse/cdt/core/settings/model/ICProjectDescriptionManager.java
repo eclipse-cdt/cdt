2fa73f9d8f296b838b9f1fc9ea16e626962cd685
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-02-16 15:27:11 +0000

    cleanup: JavaDoc warnings + extra spaces removed

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/settings/model/ICProjectDescriptionManager.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/settings/model/ICProjectDescriptionManager.java
@@ -41,9 +41,9 @@ public interface ICProjectDescriptionManager {
 	public static final int GET_WRITABLE = 1 << 2;
 	/** Return the project description <b>only</b> if it's already loaded */
 	public static final int GET_IF_LOADDED = 1 << 3;
-	/** 
-	 * Flag indicating that a new empty ICProjectDescription should be created and returned 
-	 * (irrespective of whether one already exists) 
+	/**
+	 * Flag indicating that a new empty ICProjectDescription should be created and returned
+	 * (irrespective of whether one already exists)
 	 * @since 5.1
 	 */
 	public static final int GET_EMPTY_PROJECT_DESCRIPTION = 1 << 4;
@@ -55,13 +55,13 @@ public interface ICProjectDescriptionManager {
 	 * @since 5.1
 	 */
 	public static final int GET_CREATE_DESCRIPTION = 1 << 5;
-	/** 
+	/**
 	 * Flag indicating that the Project is in the process of being created (i.e.
 	 * the user is working through the new project dialog...) This flag doesn't
 	 * affect whether a description should or shouldn't be created.
-	 * 
+	 *
  	 * @see #GET_CREATE_DESCRIPTION
-	 * @see ICProjectDescription#isCdtProjectCreating() 
+	 * @see ICProjectDescription#isCdtProjectCreating()
 	 * @since 5.1
 	 */
 	public static final int PROJECT_CREATING = 1 << 6;
@@ -75,10 +75,10 @@ public interface ICProjectDescriptionManager {
 	 * @see #createProjectDescription(IProject, boolean, boolean)
 	 */
 	ICProjectDescription createProjectDescription(IProject project, boolean loadIfExists) throws CoreException;
-	
+
 	/**
 	 * the method creates and returns a writable project description
-	 * 
+	 *
 	 * @param project project for which the project description is requested
 	 * @param loadIfExists if true the method first tries to load and return the project description
 	 * from the settings file (.cproject)
@@ -86,33 +86,33 @@ public interface ICProjectDescriptionManager {
 	 * @param creating if true the created project description will be contain the true "isCdtProjectCreating" state.
 	 * NOTE: in case the project already contains the project description AND its "isCdtProjectCreating" is false
 	 * the resulting description will be created with the false "isCdtProjectCreating" state
-	 * 
-	 * NOTE: changes made to the returned project description will not be applied until the {@link #setProjectDescription(IProject, ICProjectDescription)} is called 
+	 *
+	 * NOTE: changes made to the returned project description will not be applied until the {@link #setProjectDescription(IProject, ICProjectDescription)} is called
 	 * @return {@link ICProjectDescription}
 	 * @throws CoreException
 	 */
 	ICProjectDescription createProjectDescription(IProject project, boolean loadIfExists, boolean creating) throws CoreException;
-	
+
 	/**
 	 * This method is called to save/apply the project description
 	 * the method should be called to apply changes made to the project description
-	 * returned by the {@link #getProjectDescription(IProject, boolean)} or {@link #createProjectDescription(IProject, boolean)} 
-	 * 
-	 * Note that having persisted changes to the description, the passed in ICProjectDescription is read-only 
+	 * returned by the {@link #getProjectDescription(IProject, boolean)} or {@link #createProjectDescription(IProject, boolean)}
+	 *
+	 * Note that having persisted changes to the description, the passed in ICProjectDescription is read-only
 	 * and shouldn't be used.  If the user wishes to continue editing the ICProjectDescription they must ensure
-	 * they getProjectDescription again. 
-	 * 
+	 * they getProjectDescription again.
+	 *
 	 * @param project
 	 * @param des
 	 * @throws CoreException
-	 * 
+	 *
 	 * @see {@link #getProjectDescription(IProject, boolean)}
 	 * @see #createProjectDescription(IProject, boolean)
 	 */
 	void setProjectDescription(IProject project, ICProjectDescription des) throws CoreException;
 
 	/**
-	 * This method is called to 
+	 * This method is called to
 	 * @param project
 	 * @param des
 	 * @param force
@@ -122,7 +122,7 @@ public interface ICProjectDescriptionManager {
 	void setProjectDescription(IProject project, ICProjectDescription des, boolean force, IProgressMonitor monitor) throws CoreException;
 
 	/**
-	 * 
+	 *
 	 * @param project
 	 * @param des
 	 * @param flags
@@ -133,86 +133,85 @@ public interface ICProjectDescriptionManager {
 
 	/**
 	 * returns the project description associated with this project or null if the project does not contain the
-	 * CDT data associated with it. 
-	 * 
+	 * CDT data associated with it.
+	 *
 	 * this is a convenience method fully equivalent to getProjectDescription(project, true)
 	 * see {@link #getProjectDescription(IProject, boolean)} for more detail
 	 * @param project
 	 * @return a writable copy of the ICProjectDescription or null if the project does not contain the
-	 * CDT data associated with it. 
+	 * CDT data associated with it.
 	 * Note: changes to the project description will not be reflected/used by the core
 	 * until the {@link #setProjectDescription(IProject, ICProjectDescription)} is called
-	 * 
+	 *
 	 * @see #getProjectDescription(IProject, boolean)
 	 */
 	ICProjectDescription getProjectDescription(IProject project);
 
 	/**
 	 * returns the project description associated with this project or null if the project does not contain the
-	 * CDT data associated with it. 
-	 * 
+	 * CDT data associated with it.
+	 *
 	 * @param project project for which the description is requested
-	 * @param write if true, the writable description copy is returned. 
+	 * @param write if true, the writable description copy is returned.
 	 * If false the cached read-only description is returned.
-	 * 
+	 *
 	 * CDT core maintains the cached project description settings. If only read access is needed to description,
 	 * then the read-only project description should be obtained.
 	 * This description always operates with cached data and thus it is better to use it for performance reasons
 	 * All set* calls to the read-only description result in the {@link WriteAccessException}
-	 * 
+	 *
 	 * When the writable description is requested, the description copy is created.
 	 * Changes to this description will not be reflected/used by the core and Build System until the
 	 * {@link #setProjectDescription(IProject, ICProjectDescription)} is called
 	 *
-	 * Each getProjectDescription(project, true) returns a new copy of the project description 
-	 * 
+	 * Each getProjectDescription(project, true) returns a new copy of the project description
+	 *
 	 * The writable description uses the cached data until the first set call
 	 * after that the description communicates directly to the Build System
 	 * i.e. the implementer of the org.eclipse.cdt.core.CConfigurationDataProvider extension
 	 * This ensures the Core<->Build System settings integrity
-	 * 
+	 *
 	 * @return {@link ICProjectDescription} or null if the project does not contain the
-	 * CDT data associated with it. 
+	 * CDT data associated with it.
 	 */
 	ICProjectDescription getProjectDescription(IProject project, boolean write);
-	
+
 	/**
 	 * @see ICProjectDescriptionManager#createProjectDescription(IProject, boolean)
 	 * @param project
 	 * @param flags
-	 * @return
+	 * @return {@link ICProjectDescription} or null if the project does not contain the
+	 * CDT data associated with it.
 	 */
 	ICProjectDescription getProjectDescription(IProject project, int flags);
 
 	/**
 	 * forces the cached data of the specified projects to be re-loaded.
-	 * if the <code>projects</code> argument is <code>null</code> all projects 
+	 * if the <code>projects</code> argument is <code>null</code> all projects
 	 * within the workspace are updated
-	 * 
+	 *
 	 * @param projects
 	 * @param monitor
-	 * @throws CoreException 
+	 * @throws CoreException
 	 */
 	void updateProjectDescriptions(IProject projects[], IProgressMonitor monitor) throws CoreException;
-	
+
 	/**
-	 * answers whether the given project is a new-style project, i.e. CConfigurationDataProvider-driven
 	 * @param project
-	 * @return
+	 * @return whether the given project is a new-style project, i.e. CConfigurationDataProvider-driven
 	 */
 	boolean isNewStyleProject(IProject project);
 
 	/**
-	 * answers whether the given project is a new-style project, i.e. CConfigurationDataProvider-driven
 	 * @param des
-	 * @return
+	 * @return whether the given project is a new-style project, i.e. CConfigurationDataProvider-driven
 	 */
 	boolean isNewStyleProject(ICProjectDescription des);
-	
+
 	/**
 	 * Register a listener for changes on the set of known ICProjectDescriptions for the specified set
 	 * of events
-	 * 
+	 *
 	 * @param listener
 	 * @param eventTypes see the eventTypes in {@link CProjectDescriptionEvent}
 	 * @see CProjectDescriptionEvent#ABOUT_TO_APPLY
@@ -229,47 +228,46 @@ public interface ICProjectDescriptionManager {
 	 * @param listener
 	 */
 	void removeCProjectDescriptionListener(ICProjectDescriptionListener listener);
-	
+
 	/**
-	 * returns the workspace project description preferences.
+	 * Returns the workspace project description preferences.
 	 * if the <code>write</code> argument is <code>false</code>, the returned preferences are read-only
 	 * otherwise the preferences are writable.
-	 * NOTE: the changes made to the preferences will NOT get applied untill the preferences are set via the {@link #setProjectDescriptionWorkspacePreferences(ICProjectDescriptionWorkspacePreferences, boolean, IProgressMonitor)}  
+	 * NOTE: the changes made to the preferences will NOT get applied until the preferences are set via the {@link #setProjectDescriptionWorkspacePreferences(ICProjectDescriptionWorkspacePreferences, boolean, IProgressMonitor)}
 	 * method
-	 * @param write
-	 * @return
-	 * 
+	 * @param write if true, the writable preferences copy is returned.
+	 * @return the workspace project description preferences
+	 *
 	 * @see #setProjectDescriptionWorkspacePreferences(ICProjectDescriptionWorkspacePreferences, boolean, IProgressMonitor)
 	 */
 	ICProjectDescriptionWorkspacePreferences getProjectDescriptionWorkspacePreferences(boolean write);
 
 	/**
 	 * used to apply the project description workspace preferences
-	 * 
-	 * @param prefs - preferences to be applied 
-	 * @param updateProjects - if <code>true</code> all project descriptions within the workspace will be updated 
+	 *
+	 * @param prefs - preferences to be applied
+	 * @param updateProjects - if <code>true</code> all project descriptions within the workspace will be updated
 	 * to reflect/use the settings specified with the given preferences
-	 *   
 	 * @param monitor
-	 * @return
+	 * @return {@code true} if new {@code prefs} differ from the old ones, i.e. preferences changed
 	 */
 	boolean setProjectDescriptionWorkspacePreferences(ICProjectDescriptionWorkspacePreferences prefs, boolean updateProjects, IProgressMonitor monitor);
-	
+
 	/**
 	 * forces the external settings providers of the specified IDs to be rescanned
 	 * and all configurations referencing the specified providers to be updated
-	 * 
+	 *
 	 * @param ids the ids of externalSettinsProvider extensions
-	 * 
+	 *
 	 * @see ICConfigurationDescription#getExternalSettingsProviderIds()
 	 * @see ICConfigurationDescription#setExternalSettingsProviderIds(String[])
 	 * @see ICConfigurationDescription#updateExternalSettingsProviders(String[])
 	 */
 	void updateExternalSettingsProviders(String[] ids, IProgressMonitor monitor);
-	
+
 	ICConfigurationDescription getPreferenceConfiguration(String buildSystemId) throws CoreException;
 
 	ICConfigurationDescription getPreferenceConfiguration(String buildSystemId, boolean write) throws CoreException;
-	
+
 	void setPreferenceConfiguration(String buildSystemId, ICConfigurationDescription des) throws CoreException;
 }

be13e38e00c991d284bf3701fa9c78e3adf43c07
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-03-04 15:02:56 +0000

    - remove unused includes

--- codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/test/CheckerTestCase.java
+++ codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/test/CheckerTestCase.java
@@ -13,8 +13,6 @@ package org.eclipse.cdt.codan.core.test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;

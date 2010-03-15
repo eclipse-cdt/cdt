54460ccbce096a2adc322ca862460c45a8c71852
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-03-15 18:54:35 +0000

    cleanup: deprecated class used for CDT 3.X style projects

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dialogs/cpaths/CPathContainerDescriptor.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/dialogs/cpaths/CPathContainerDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 QNX Software Systems and others.
+ * Copyright (c) 2004, 2010 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,6 +38,11 @@ import org.eclipse.cdt.ui.wizards.IPathEntryContainerPage;
 
 import org.eclipse.cdt.internal.ui.util.CoreUtility;
 
+/**
+ * @deprecated as of CDT 4.0. This wizard was used in property pages/wizards
+ * for 3.X style projects.
+ */
+@Deprecated
 public class CPathContainerDescriptor implements IContainerDescriptor {
 
 

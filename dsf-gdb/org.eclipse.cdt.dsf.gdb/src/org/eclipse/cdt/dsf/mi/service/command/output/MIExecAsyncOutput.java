b32ebda2f8b523e2f40070d3036e17cdfa89c555
John Cortell <john.cortell@freescale.com>
2010-03-17 22:37:04 +0000

    Additional documentation

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIExecAsyncOutput.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIExecAsyncOutput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 QNX Software Systems and others.
+ * Copyright (c) 2000, 2010 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,6 +13,12 @@
 package org.eclipse.cdt.dsf.mi.service.command.output;
 
 /**
+ * Represents an asynchronous OOB record from gdb that notifies the client of
+ * state changes on the target (stopped, started, etc).
+ * 
+ * <p>
+ * All such records are prefixed by *.
+ * 
  * @see MIAsyncRecord
  */
 public class MIExecAsyncOutput extends MIAsyncRecord {
--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIStatusAsyncOutput.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIStatusAsyncOutput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 QNX Software Systems and others.
+ * Copyright (c) 2000, 2010 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,13 @@
 
 package org.eclipse.cdt.dsf.mi.service.command.output;
 
-
 /**
+ * Represents an asynchronous OOB record from gdb that provides status on an
+ * ongoing time consuming operation.
+ * 
+ * <p>
+ * All such output is prefixed by `+'.
+ * 
  * @see MIAsyncRecord
  */
 public class MIStatusAsyncOutput extends MIAsyncRecord {

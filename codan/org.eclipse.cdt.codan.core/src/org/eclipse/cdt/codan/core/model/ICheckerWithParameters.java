3ff7fd3ecd77348b388bc98353ef7e1bd6466788
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-03-18 14:36:12 +0000

    - fixed interface comment

--- codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/core/model/ICheckerWithParameters.java
+++ codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/core/model/ICheckerWithParameters.java
@@ -11,17 +11,24 @@
 package org.eclipse.cdt.codan.core.model;
 
 /**
- * Interface for checker with parameters, if checker implements this
- * interface method would be called on initialization so checker has
- * a chance to set default values for its parameters
+ * Interface for checker with parameters, if checker implements this interface
+ * method would be called on initialization so checker has a chance to set
+ * default values for its parameters
+ * <p>
+ * <strong>EXPERIMENTAL</strong>. This class or interface has been added as part
+ * of a work in progress. There is no guarantee that this API will work or that
+ * it will remain the same.
+ * </p>
  * 
  * @noextend This interface is not intended to be extended by clients.
- * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ICheckerWithParameters {
 	/**
-	 * Implement this method to set default parameters for checkers with parameters. 
-	 * @param problem - instance of problem working copy
+	 * Implement this method to set default parameters for checkers with
+	 * parameters.
+	 * 
+	 * @param problem
+	 *            - instance of problem working copy
 	 */
 	void initParameters(IProblemWorkingCopy problem);
 }

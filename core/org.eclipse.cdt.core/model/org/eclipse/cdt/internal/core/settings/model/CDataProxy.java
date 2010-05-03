89f528fac70eb6079ac169a440339d94f1df52f7
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-05-03 20:51:15 +0000

    toString() added

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/CDataProxy.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/settings/model/CDataProxy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Intel Corporation and others.
+ * Copyright (c) 2007, 2010 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -169,5 +169,14 @@ public abstract class CDataProxy implements ICSettingObject {
 			return null;
 		
 		return projDes.getProject();
+	}
+	
+	/**
+	 * This method is intended for debugging purpose only.
+	 */
+	@SuppressWarnings("nls")
+	@Override
+	public String toString() {
+		return "name=["+getName()+"], id=["+getId()+"]";
 	}
 }

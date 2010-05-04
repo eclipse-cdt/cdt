09e271729815fedc124d04b3c502dd64a636729d
Andrew Gvozdev <angvoz.dev@gmail.com>
2010-05-04 04:24:40 +0000

    compilation warnings

--- build/org.eclipse.cdt.make.core.tests/src/org/eclipse/cdt/make/scannerdiscovery/TestScannerInfoCollector.java
+++ build/org.eclipse.cdt.make.core.tests/src/org/eclipse/cdt/make/scannerdiscovery/TestScannerInfoCollector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 Wind River Systems, Inc. and others.
+ * Copyright (c) 2007, 2010 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,30 +12,32 @@
 package org.eclipse.cdt.make.scannerdiscovery;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector;
 import org.eclipse.cdt.make.core.scannerconfig.ScannerInfoTypes;
 import org.eclipse.cdt.make.internal.core.scannerconfig.util.CCommandDSC;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
 final class TestScannerInfoCollector implements IScannerInfoCollector {
-	private HashMap fInfoMap = new HashMap();
-	private HashMap fResourceToInfoMap = new HashMap();
+	private HashMap<ScannerInfoTypes, List> fInfoMap = new HashMap<ScannerInfoTypes, List>();
+	private HashMap<Object, Map<ScannerInfoTypes, List>> fResourceToInfoMap = new HashMap<Object, Map<ScannerInfoTypes, List>>();
 	
-	public void contributeToScannerConfig(Object resource, Map scannerInfo) {
-		for (Iterator iterator = scannerInfo.entrySet().iterator(); iterator.hasNext();) {
-			Map.Entry entry = (Map.Entry) iterator.next();
-			ScannerInfoTypes key = (ScannerInfoTypes) entry.getKey();
-			List value = (List) entry.getValue();
+	public void contributeToScannerConfig(Object resource, Map scannerInfo0) {
+		Map<ScannerInfoTypes, List> scannerInfo = scannerInfo0;
+		Set<Entry<ScannerInfoTypes, List>> entrySet = scannerInfo.entrySet();
+		for (Entry<ScannerInfoTypes, List> entry : entrySet) {
+			ScannerInfoTypes key = entry.getKey();
+			List value = entry.getValue();
 			addTo(key, value);
 			if (ScannerInfoTypes.COMPILER_COMMAND.equals(key)) {
-				for (Iterator iterator2 = value.iterator(); iterator2.hasNext();) {
-					CCommandDSC cdsc= (CCommandDSC) iterator2.next();
+				List<CCommandDSC> cdscs = value;
+				for (CCommandDSC cdsc : cdscs) {
 					cdsc.resolveOptions(null);
 					addTo(ScannerInfoTypes.INCLUDE_PATHS, cdsc.getIncludes());
 					addTo(ScannerInfoTypes.QUOTE_INCLUDE_PATHS, cdsc.getQuoteIncludes());
@@ -48,10 +50,10 @@ final class TestScannerInfoCollector implements IScannerInfoCollector {
 		}
 	}
 
-	private void addTo(ScannerInfoTypes type, List col) {
-		Collection target= (Collection) fInfoMap.get(type);
+	private void addTo(ScannerInfoTypes type, List<String> col) {
+		List<String> target = fInfoMap.get(type);
 		if (target == null) {
-			target= new ArrayList();
+			target= new ArrayList<String>();
 			fInfoMap.put(type, target);
 		}
 		target.addAll(col);
@@ -59,12 +61,12 @@ final class TestScannerInfoCollector implements IScannerInfoCollector {
 
 	public List getCollectedScannerInfo(Object resource, ScannerInfoTypes type) {
 		if (resource == null) {
-			List result= (List) fInfoMap.get(type);
+			List result= fInfoMap.get(type);
 			return result == null ? Collections.EMPTY_LIST : result;
 		}
-		Map scannerInfo= (Map)fResourceToInfoMap.get(resource);
+		Map<ScannerInfoTypes, List> scannerInfo= fResourceToInfoMap.get(resource);
 		if (scannerInfo != null) {
-			List result= (List) scannerInfo.get(type);
+			List result= scannerInfo.get(type);
 			return result == null ? Collections.EMPTY_LIST : result;
 		}
 		return Collections.EMPTY_LIST;

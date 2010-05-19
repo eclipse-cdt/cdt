85d6bff98e4c292ff6599beecd7d6dc163ae86ff
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-05-19 02:41:12 +0000

    Added method to check that node belongs to macro expansion

--- codan/org.eclipse.cdt.codan.core.cxx/src/org/eclipse/cdt/codan/core/cxx/CxxAstUtils.java
+++ codan/org.eclipse.cdt.codan.core.cxx/src/org/eclipse/cdt/codan/core/cxx/CxxAstUtils.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.cdt.codan.core.cxx;
 
+import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTNodeSelector;
+import org.eclipse.cdt.core.dom.ast.IASTPreprocessorMacroExpansion;
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.IType;
 import org.eclipse.cdt.core.dom.ast.ITypedef;
@@ -25,23 +29,38 @@ public final class CxxAstUtils {
 	}
 
 	public synchronized static CxxAstUtils getInstance() {
-		if (instance == null) instance = new CxxAstUtils();
+		if (instance == null)
+			instance = new CxxAstUtils();
 		return instance;
 	}
 
 	public IType unwindTypedef(IType type) {
-		if (!(type instanceof IBinding)) return type;
+		if (!(type instanceof IBinding))
+			return type;
 		IBinding typeName = (IBinding) type;
 		// unwind typedef chain
 		try {
 			while (typeName instanceof ITypedef) {
 				IType t = ((ITypedef) typeName).getType();
-				if (t instanceof IBinding) typeName = (IBinding) t;
-				else return t;
+				if (t instanceof IBinding)
+					typeName = (IBinding) t;
+				else
+					return t;
 			}
 		} catch (Exception e) { // in CDT 6.0 getType throws DOMException
 			Activator.log(e);
 		}
 		return (IType) typeName;
+	}
+
+	public boolean isInMacro(IASTNode node) {
+		IASTNodeSelector nodeSelector = node.getTranslationUnit()
+				.getNodeSelector(node.getTranslationUnit().getFilePath());
+		IASTFileLocation fileLocation = node.getFileLocation();
+	
+		IASTPreprocessorMacroExpansion macro = nodeSelector
+				.findEnclosingMacroExpansion(fileLocation.getNodeOffset(),
+						fileLocation.getNodeLength());
+		return macro != null;
 	}
 }

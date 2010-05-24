a835558a3b66305d1df849a69071a0fb8b6e4210
Martin Oberhuber < martin.oberhuber@windriver.com>
2010-05-24 07:55:42 +0000

    TM 3.2RC2 build notes

--- releng/org.eclipse.rse.build/template/buildNotes.php
+++ releng/org.eclipse.rse.build/template/buildNotes.php
@@ -38,7 +38,7 @@
 </ul>
 </li>
 -->
-<li>At least 7 bugs were resolved: Use 
+<li>At least 9 bugs were resolved: Use 
   <!-- <a href="https://bugs.eclipse.org/bugs/buglist.cgi?negate0=1;field0-0-0=target_milestone;type0-0-1=regexp;field0-0-1=target_milestone;resolution=FIXED;resolution=WONTFIX;resolution=WORKSFORME;classification=DSDP;chfieldto=2010-05-26;chfield=resolution;query_format=advanced;chfieldfrom=2010-05-03;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;value0-0-1=3.2%20M[67];type0-0-0=regexp;value0-0-0=[23]\.[01].*;component=Core;component=RSE;component=Terminal;product=Target%20Management"> -->
   <a href="https://bugs.eclipse.org/bugs/buglist.cgi?resolution=FIXED;resolution=WONTFIX;resolution=WORKSFORME;classification=DSDP;query_format=advanced;bug_status=RESOLVED;bug_status=VERIFIED;bug_status=CLOSED;component=Core;component=RSE;component=Terminal;target_milestone=3.2%20RC1;target_milestone=3.2%20RC2;product=Target%20Management">
   this query</a> to show the list of bugs fixed since

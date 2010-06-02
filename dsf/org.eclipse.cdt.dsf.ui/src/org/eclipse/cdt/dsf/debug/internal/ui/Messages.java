43e8a91e3ad7804116bddcc7da6d394e336bb7ef
Marc Khouzam <marc.khouzam@ericsson.com>
2010-06-02 19:01:56 +0000

    Bug 315446: Wrong bundle name makes NLS strings fail to be found.

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/Messages.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Wind River Systems, Inc. and others.
+ * Copyright (c) 2009, 2010 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,13 +7,14 @@
  *
  * Contributors:
  *     Wind River Systems - initial API and implementation
+ *     Patrick Chuong (Texas Instruments) - Bug 315446: Invalid event breakpoint type (group) name 
  *******************************************************************************/
 package org.eclipse.cdt.dsf.debug.internal.ui;
 
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME= "org.eclipse.cdt.dsf.internal.ui.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME= "org.eclipse.cdt.dsf.debug.internal.ui.messages"; //$NON-NLS-1$
 
 	public static String ToggleBreakpointsTargetFactory_description;
 	public static String ToggleBreakpointsTargetFactory_name;

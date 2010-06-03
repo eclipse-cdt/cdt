70237c3d811c01948827dd306222f5200ae837a9
Vivian Kong <vivkong@ca.ibm.com>
2010-06-03 00:12:05 +0000

    Bug 315439 Fix up copyright

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/breakpoints/DataCache.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/breakpoints/DataCache.java
@@ -1,7 +1,7 @@
 package org.eclipse.cdt.dsf.debug.ui.viewmodel.breakpoints;
 
 /******************************************************************************* 
- * Copyright (c) 2008 Wind River Systems, Inc. and others. 
+ * Copyright (c) 2008, 2010 Wind River Systems, Inc. and others. 
  * All rights reserved. This program and the accompanying materials  
  * are made available under the terms of the Eclipse Public License v1.0  
  * which accompanies this distribution, and is available at  
--- dsf/org.eclipse.cdt.examples.dsf.pda/src/org/eclipse/cdt/examples/dsf/pda/service/PDABackend.java
+++ dsf/org.eclipse.cdt.examples.dsf.pda/src/org/eclipse/cdt/examples/dsf/pda/service/PDABackend.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Nokia Corporation.
+ * Copyright (c) 2008, 2009 Nokia Corporation.
  * All rights reserved. This fProgram and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

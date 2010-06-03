b2370eeea77670a7ace4e28a4575910c391dff32
Vivian Kong <vivkong@ca.ibm.com>
2010-06-03 14:12:21 +0000

    Bug 315439 Fix up copyright

--- memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/TraditionalMemoryByte.java
+++ memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/TraditionalMemoryByte.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Wind River Systems, Inc. - initial implementation
+ *******************************************************************************/
 package org.eclipse.cdt.debug.ui.memory.traditional;
 
 import org.eclipse.debug.core.model.MemoryByte;

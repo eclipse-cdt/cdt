078c2fd1f8e9a2a52cc5575863f48e8ea5b25ee2
Vivian Kong <vivkong@ca.ibm.com>
2010-06-03 17:36:02 +0000

    Bug 315439 Fix up copyright

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/ProblemMarkerInfo.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/ProblemMarkerInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 Siemens AG.
+ * Copyright (c) 2006, 2009 Siemens AG.
  * All rights reserved. This content and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

b01f608abbd12a8844aa4b5802228981b44dc0a9
Vivian Kong <vivkong@ca.ibm.com>
2010-06-03 13:14:13 +0000

    Bug 315237 Make ImportExportWizardButtons public

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/ImportExportWizardButtons.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/newui/ImportExportWizardButtons.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,7 +34,7 @@ import org.eclipse.cdt.internal.ui.wizards.settingswizards.ProjectSettingsWizard
  * 
  * @since 5.2
  */
-class ImportExportWizardButtons {
+public class ImportExportWizardButtons {
 	
 	private ImportExportWizardButtons() {}
 	

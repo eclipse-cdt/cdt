dbaa2242285261d7c953aea54eaafb3f20b2c229
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-06-17 00:20:17 +0000

    Bug 316154: support processing of inner functions

--- codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/internal/checkers/ReturnCheckerTest.java
+++ codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/internal/checkers/ReturnCheckerTest.java
@@ -18,72 +18,91 @@ import org.eclipse.cdt.codan.core.test.CheckerTestCase;
  * 
  */
 public class ReturnCheckerTest extends CheckerTestCase {
-
-
-//	dummy() {
-//	  return; // error here on line 2
-//	}
+	//	dummy() {
+	//	  return; // error here on line 2
+	//	}
 	public void testDummyFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkNoErrors(); // because return type if not defined, usually people don't care
 	}
 
-
-//	void void_function(void) {
-//	  return; // no error here
-//	}
+	//	void void_function(void) {
+	//	  return; // no error here
+	//	}
 	public void testVoidFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkNoErrors();
 	}
 
-
-//	int integer_return_function(void) {
-//	  if (global) {
-//		if (global == 100) {
-//			return; // error here on line 4
-//		}
-//	  }
-//	}	 
+	//	int integer_return_function(void) {
+	//	  if (global) {
+	//		if (global == 100) {
+	//			return; // error here on line 4
+	//		}
+	//	  }
+	//	}	 
 	public void testBasicTypeFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkErrorLine(4);
 	}
 
-//
-//	struct My_Struct {
-//	int a;
-//	};
-//
-//	 struct My_Struct struct_return_function(void) {
-//	return; // error here on line 6
-//	}
+	//
+	//	struct My_Struct {
+	//	int a;
+	//	};
+	//
+	//	 struct My_Struct struct_return_function(void) {
+	//	return; // error here on line 6
+	//	}
 	public void testUserDefinedFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkErrorLine(6);
 	}
 
-
-//	 typedef unsigned int uint8_t;
-//	 
-//	uint8_t return_typedef(void) {
-//	return; // error here on line 4
-//	}
+	//	 typedef unsigned int uint8_t;
+	//	 
+	//	uint8_t return_typedef(void) {
+	//	return; // error here on line 4
+	//	}
 	public void testTypedefReturnFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkErrorLine(4);
 	}
 
-
-//	typedef unsigned int uint8_t;
-//	 	 
-//	uint8_t (*return_fp_no_typedef(void))(void)
-//	{
-//			return; // error here on line 5
-//	}
+	//	typedef unsigned int uint8_t;
+	//	 	 
+	//	uint8_t (*return_fp_no_typedef(void))(void)
+	//	{
+	//			return; // error here on line 5
+	//	}
 	public void testFunctionPointerReturnFunction() {
 		loadCodeAndRun(getAboveComment());
 		checkErrorLine(5);
 	}
 
+	//	void test() {
+	//		  class A {
+	//		   public:
+	//		    void m() {
+	//		      return; // should not be an error here
+	//		    }
+	//		  };
+	//		}
+	public void testInnerFunction_Bug315525() {
+		loadCodeAndRunCpp(getAboveComment());
+		checkNoErrors();
+	}
+
+	//	void test() {
+	//		  class A {
+	//		   public:
+	//		    int m() {
+	//		      return; // should be an error here
+	//		    }
+	//		  };
+	//		}
+	public void testInnerFunction_Bug316154() {
+		loadCodeAndRunCpp(getAboveComment());
+		checkErrorLine(5);
+	}
 }

469f8fbc8780253cb209b06a9c10a5a65ca4378e
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2010-07-17 18:21:07 +0000

    Added noimplement and noextend tags.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/ICEditorActionDefinitionIds.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/ICEditorActionDefinitionIds.java
@@ -18,13 +18,12 @@ import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
 
 /**
  * Defines the definition IDs for the C editor actions.
- * 
- * <p>
- * This interface is not intended to be implemented or extended.
- * </p>.
- * 
+ *
  * @since 2.1
- */
+ *
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
+*/
 public interface ICEditorActionDefinitionIds extends ITextEditorActionDefinitionIds {
 
 	/**

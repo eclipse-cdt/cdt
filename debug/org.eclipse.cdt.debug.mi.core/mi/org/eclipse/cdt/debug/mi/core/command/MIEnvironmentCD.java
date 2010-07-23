1aeb6dc884b92beda5e092e2c8ca739c423333b1
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-07-23 14:50:06 +0000

    Bug 80700:  modern gdb does need argument be quoted for environment-cd,  let super class take care of it

--- debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIEnvironmentCD.java
+++ debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIEnvironmentCD.java
@@ -25,18 +25,4 @@ public class MIEnvironmentCD extends MICommand
 	public MIEnvironmentCD(String miVersion, String path) {
 		super(miVersion, "-environment-cd", new String[]{path}); //$NON-NLS-1$
 	}
-
-	/**
-	 * !@*^%^$( sigh ... gdb for this command does not make any interpretation
-	 * So we must past the command verbatim without any changes. 
-	 * (non-Javadoc)
-	 * @see org.eclipse.cdt.debug.mi.core.command.MICommand#parametersToString()
-	 */
-	protected String parametersToString() {
-		String[] parameters = getParameters();
-		if (parameters != null && parameters.length == 1) {
-			return parameters[0];
-		}
-		return super.parametersToString();		
-	}
 }

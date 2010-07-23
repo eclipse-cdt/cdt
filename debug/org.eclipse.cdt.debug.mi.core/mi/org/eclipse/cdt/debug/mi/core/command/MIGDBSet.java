664e416fa2b78ece798ab8f6b345299748958f2d
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2010-07-23 14:48:49 +0000

    Bug 320629 -  MI command -gdb-set does not properly escape argument

--- debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIGDBSet.java
+++ debug/org.eclipse.cdt.debug.mi.core/mi/org/eclipse/cdt/debug/mi/core/command/MIGDBSet.java
@@ -23,4 +23,14 @@ public class MIGDBSet extends MICommand
 	public MIGDBSet(String miVersion, String[] params) {
 		super(miVersion, "-gdb-set", params); //$NON-NLS-1$
 	}
+	@Override
+	protected String parametersToString() {
+		/* gdb (at least up to 6.8) does not correctly process escaping for arguments.
+		 * pass argument without escaping. Just in case only do it for simple cases only like -gdb-set variable value.
+		 * For example set solib-search-path */
+	    if (fParameters!=null && fParameters.length==2 && (fOptions==null || fOptions.length==0)) {
+	    	return fParameters[0]+" "+fParameters[1]; //$NON-NLS-1$
+	    }
+	    return super.parametersToString();
+	}
 }

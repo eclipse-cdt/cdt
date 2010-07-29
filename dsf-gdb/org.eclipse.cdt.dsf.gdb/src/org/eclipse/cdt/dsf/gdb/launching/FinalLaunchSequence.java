48b6e9207fbee6977cdc7d170825dc4ed5f51398
Marc Khouzam <marc.khouzam@ericsson.com>
2010-07-29 18:55:54 +0000

    Bug 321259: Need to set environment dir before sourcing init file, so that we look for the init file in the right place

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/launching/FinalLaunchSequence.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/launching/FinalLaunchSequence.java
@@ -103,6 +103,28 @@ public class FinalLaunchSequence extends Sequence {
             requestMonitor.done();
         }},
     	/*
+    	 * Specify GDB's working directory
+    	 */
+        new Step() { @Override
+        public void execute(final RequestMonitor requestMonitor) {
+   			IPath dir = null;
+       		try {
+       			dir = fGDBBackend.getGDBWorkingDirectory();
+       		} catch (CoreException e) {
+       			requestMonitor.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, -1, "Cannot get working directory", e)); //$NON-NLS-1$
+       			requestMonitor.done();
+       			return;
+      		}
+
+        	if (dir != null) {
+        		fCommandControl.queueCommand(
+        				fCommandFactory.createMIEnvironmentCD(fCommandControl.getContext(), dir.toPortableString()), 
+        				new DataRequestMonitor<MIInfo>(getExecutor(), requestMonitor));
+        	} else {
+        		requestMonitor.done();
+        	}
+        }},
+    	/*
     	 * Source the gdbinit file specified in the launch
     	 */
         new Step() { @Override
@@ -131,28 +153,6 @@ public class FinalLaunchSequence extends Sequence {
         		}
         	} catch (CoreException e) {
         		requestMonitor.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, -1, "Cannot get gdbinit option", e)); //$NON-NLS-1$
-        		requestMonitor.done();
-        	}
-        }},
-    	/*
-    	 * Specify GDB's working directory
-    	 */
-        new Step() { @Override
-        public void execute(final RequestMonitor requestMonitor) {
-   			IPath dir = null;
-       		try {
-       			dir = fGDBBackend.getGDBWorkingDirectory();
-       		} catch (CoreException e) {
-       			requestMonitor.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, -1, "Cannot get working directory", e)); //$NON-NLS-1$
-       			requestMonitor.done();
-       			return;
-      		}
-
-        	if (dir != null) {
-        		fCommandControl.queueCommand(
-        				fCommandFactory.createMIEnvironmentCD(fCommandControl.getContext(), dir.toPortableString()), 
-        				new DataRequestMonitor<MIInfo>(getExecutor(), requestMonitor));
-        	} else {
         		requestMonitor.done();
         	}
         }},

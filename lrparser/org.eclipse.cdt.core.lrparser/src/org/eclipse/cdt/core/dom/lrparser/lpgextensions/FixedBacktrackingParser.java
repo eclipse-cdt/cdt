d79a8c8f70830a0c92ea87b94f93e167bb5c1456
Vivian Kong <vivkong@ca.ibm.com>
2010-08-09 17:57:20 +0000

    Bug 315826  Update LR Parser for the template arguments rules - patch by John Liu

--- lrparser/org.eclipse.cdt.core.lrparser/src/org/eclipse/cdt/core/dom/lrparser/lpgextensions/FixedBacktrackingParser.java
+++ lrparser/org.eclipse.cdt.core.lrparser/src/org/eclipse/cdt/core/dom/lrparser/lpgextensions/FixedBacktrackingParser.java
@@ -305,14 +305,11 @@ public class FixedBacktrackingParser extends Stacks
             current_kind = tokStream.getKind(curtok),
             act = tAction(stateStack[stateStackTop], current_kind);
 
-        int count = 0;
-
         //
         // The main driver loop
         //
         for (;;)
         {
-        	count++;
             //
             // if the parser needs to stop processing,
             // it may do so here.
@@ -393,8 +390,6 @@ public class FixedBacktrackingParser extends Stacks
         //System.out.println("****Number of actions: " + action.size());
         //System.out.println("****Max Stack Size = " + maxStackTop);
         //System.out.flush();
-        System.out.println("The backtrace parser count is: " + count );
-
         return (act == ERROR_ACTION ? error_token : 0);
     }
 

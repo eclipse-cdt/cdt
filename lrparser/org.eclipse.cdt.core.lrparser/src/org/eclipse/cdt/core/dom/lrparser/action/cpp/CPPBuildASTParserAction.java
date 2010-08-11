0acbd73fadf99fdad81896e75def2b85631766db
Vivian Kong <vivkong@ca.ibm.com>
2010-08-11 21:08:29 +0000

    Bug 322426 Add copy implementation to CPPASTAmbiguousTemplateArgument class for John Liu

--- lrparser/org.eclipse.cdt.core.lrparser/src/org/eclipse/cdt/core/dom/lrparser/action/cpp/CPPBuildASTParserAction.java
+++ lrparser/org.eclipse.cdt.core.lrparser/src/org/eclipse/cdt/core/dom/lrparser/action/cpp/CPPBuildASTParserAction.java
@@ -478,16 +478,7 @@ public class CPPBuildASTParserAction extends BuildASTParserAction {
 		IASTExpression expr = (IASTExpression) astStack.peek();
 		
 		if(expr instanceof IASTIdExpression) {
-			IASTName orgName =((IASTIdExpression)expr).getName();
-			IASTName name = null;
-			try{
-			 name = orgName.copy();
-			 //if there is node throws UnsupportedOperationException in copy, just use the original node
-			} catch(UnsupportedOperationException ue){
-				name = orgName;
-			}
-			
-			ParserUtil.setOffsetAndLength(name, expr);
+			IASTName name = ((IASTIdExpression)expr).getName().copy();
 			
 			IASTNamedTypeSpecifier declSpec = nodeFactory.newTypedefNameSpecifier(name);
 			ParserUtil.setOffsetAndLength(declSpec, name);

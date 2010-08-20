eabf8400b2c01370d133923bc8adc340a8b55168
Anton Leherbauer <anton.leherbauer@windriver.com>
2010-08-20 08:21:32 +0000

    Bug 323206 - [Code templates] Pop up error when using code formatter option and ${line_selection}

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/corext/template/c/ExclusivePositionUpdater.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/corext/template/c/ExclusivePositionUpdater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -77,7 +77,7 @@ final class ExclusivePositionUpdater implements IPositionUpdater {
 					// offset becomes end of event, length adjusted accordingly
 					int newOffset= eventOffset + eventNewLength;
 					position.setOffset(newOffset);
-					position.setLength(end - newOffset);
+					position.setLength(end - newOffset + deltaLength);
 				} else {
 					// event consumes the position - delete it
 					position.delete();

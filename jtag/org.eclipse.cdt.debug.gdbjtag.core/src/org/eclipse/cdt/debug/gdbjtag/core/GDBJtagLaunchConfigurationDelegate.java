fff45080d07fea3b16b47f0c2f68ed9b1ad5362a
John Cortell <john.cortell@freescale.com>
2010-09-02 16:35:56 +0000

    Fix potential null pointer use

--- jtag/org.eclipse.cdt.debug.gdbjtag.core/src/org/eclipse/cdt/debug/gdbjtag/core/GDBJtagLaunchConfigurationDelegate.java
+++ jtag/org.eclipse.cdt.debug.gdbjtag.core/src/org/eclipse/cdt/debug/gdbjtag/core/GDBJtagLaunchConfigurationDelegate.java
@@ -62,7 +62,7 @@ public class GDBJtagLaunchConfigurationDelegate extends AbstractCLaunchDelegate 
 					Process process = targets[i].getProcess();
 					IProcess iprocess = null;
 					if ( process != null ) {
-						iprocess = DebugPlugin.newProcess(launch, process, renderProcessLabel(exePath.toOSString()),
+						iprocess = DebugPlugin.newProcess(launch, process, renderProcessLabel(exePath != null ? exePath.toOSString() : "???"),
 								getDefaultProcessMap() );
 					}
 					CDIDebugModel.newDebugTarget(launch, project.getProject(), targets[i],

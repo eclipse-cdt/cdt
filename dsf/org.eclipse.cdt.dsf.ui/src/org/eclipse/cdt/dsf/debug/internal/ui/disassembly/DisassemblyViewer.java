d862d98eff85d7028105c91951dd96d736b1269f
Anton Leherbauer <anton.leherbauer@windriver.com>
2010-09-07 08:27:11 +0000

    Bug 324597 - [disassembly] maximizing/resizing view sometimes leaves "........" lines Patch from Kirk Beitz (Nokia)

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/DisassemblyViewer.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/DisassemblyViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 Wind River Systems and others.
+ * Copyright (c) 2007, 2010 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -277,6 +277,9 @@ public class DisassemblyViewer extends SourceViewer {
 	protected void updateViewportListeners(int origin) {
 		fCachedLastTopPixel = fLastTopPixel;
 		fUserTriggeredScrolling = origin != INTERNAL && origin != RESIZE;
+		if (origin == RESIZE) {
+			fLastTopPixel = -1;
+		}
 		super.updateViewportListeners(origin);
 	}
 	

e19da248043bad0fc6394aa5ca99d105351d8991
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2010-12-01 22:51:10 +0000

    Cosmetics.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/CSourcePathComputerDelegate.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/CSourcePathComputerDelegate.java
@@ -6,8 +6,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * QNX Software Systems - Initial API and implementation
- * Nokia - Added support for AbsoluteSourceContainer( 159833 ) 
+ *     QNX Software Systems - Initial API and implementation
+ *     Nokia - Added support for AbsoluteSourceContainer(159833) 
  *******************************************************************************/
 package org.eclipse.cdt.debug.internal.core.sourcelookup; 
 
@@ -43,35 +43,35 @@ public class CSourcePathComputerDelegate implements ISourcePathComputerDelegate 
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate#computeSourceContainers(org.eclipse.debug.core.ILaunchConfiguration, org.eclipse.core.runtime.IProgressMonitor)
 	 */
-	public ISourceContainer[] computeSourceContainers( ILaunchConfiguration configuration, IProgressMonitor monitor ) throws CoreException {
+	public ISourceContainer[] computeSourceContainers(ILaunchConfiguration configuration, IProgressMonitor monitor) throws CoreException {
 		// First, get all the the containers in the global preferences (but add them last)
 		ISourceContainer[] common = CDebugCorePlugin.getDefault().getCommonSourceLookupDirector().getSourceContainers();
 
-		List<ISourceContainer> containers = new ArrayList<ISourceContainer>( common.length + 2 );
+		List<ISourceContainer> containers = new ArrayList<ISourceContainer>(common.length + 2);
 		
 		// Add a container that fetches files that are specified with an absolute path
-		containers.add(new AbsolutePathSourceContainer() );
+		containers.add(new AbsolutePathSourceContainer());
 
 		// Add a container that fetches files that are specified with a program relative path
 		containers.add(new ProgramRelativePathSourceContainer());
 
 		// Add a container that looks in the project specified in the configuration
-		String projectName = configuration.getAttribute( ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, (String)null );
+		String projectName = configuration.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, (String) null);
 		if (projectName != null && projectName.length() > 0) {
-			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject( projectName );
-			if ( project.exists() ) {
-				containers.add(new ProjectSourceContainer( project, true ) );
+			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+			if (project.exists()) {
+				containers.add(new ProjectSourceContainer(project, true));
 			}
 		}
 
 		// Finally, add the common (global) containers
-		for ( ISourceContainer sc : common ) {
+		for (ISourceContainer sc : common) {
 			// If the container is a path-mapper, use a copy (why?)
-			if ( sc.getType().getId().equals( MappingSourceContainer.TYPE_ID ) )
+			if (sc.getType().getId().equals(MappingSourceContainer.TYPE_ID))
 				sc = ((MappingSourceContainer)sc).copy();
-			containers.add( sc );
+			containers.add(sc);
 		}
 		
-		return containers.toArray( new ISourceContainer[containers.size()] );
+		return containers.toArray(new ISourceContainer[containers.size()]);
 	}
 }

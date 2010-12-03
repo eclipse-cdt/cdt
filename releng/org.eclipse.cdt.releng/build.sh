93c55fd9d53f006d78f496351b41392bbe139a36
Vivian Kong <vivkong@ca.ibm.com>
2010-12-03 18:40:56 +0000

    update subversive version in build script

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -26,7 +26,7 @@ cd `dirname $0`
 mkdir -p tools
 cd tools
 cvs -d /cvsroot/eclipse	co -r R36_RC4 org.eclipse.releng.basebuilder
-cp /home/data/httpd/download.eclipse.org/technology/subversive/0.7/pde-update-site/plugins/org.eclipse.team.svn.pde.build_0.7.8.I20090525-1500.jar \
+cp /home/data/httpd/download.eclipse.org/technology/subversive/0.7/pde-update-site/plugins/org.eclipse.team.svn.pde.build_0.7.9.I20101203-1700.jar \
 	org.eclipse.releng.basebuilder/plugins
 cd ..
 

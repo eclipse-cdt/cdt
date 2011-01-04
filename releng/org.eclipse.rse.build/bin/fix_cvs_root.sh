c0565b2adb3e4539c4b1d8076fc11a43d4a90707
Martin Oberhuber < martin.oberhuber@windriver.com>
2011-01-04 21:48:54 +0000

    [releng] Build on tools - branding - update file copyright dates 2011

--- releng/org.eclipse.rse.build/bin/fix_cvs_root.sh
+++ releng/org.eclipse.rse.build/bin/fix_cvs_root.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#*******************************************************************************
+#  Copyright (c) 2011 Wind River Systems, Inc. and others.
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+# 
+#  Contributors:
+#  Martin Oberhuber (Wind River) - initial API and implementation
+#*******************************************************************************
 echo ""
 echo "Replace /cvsroot/dsdp -> /cvsroot/tools in all CVS/Root files below current directory"
 echo ""

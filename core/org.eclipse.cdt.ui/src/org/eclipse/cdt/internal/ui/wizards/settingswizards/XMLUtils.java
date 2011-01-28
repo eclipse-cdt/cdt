c62c6e671eb788de195c486d61f29f483d43b347
Vivian Kong <vivkong@ca.ibm.com>
2011-01-28 16:36:58 +0000

    Bug 335512 - Update XLC scanning discovery parser and marcos and include path import XML utility accessible scope (for John Liu)

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/wizards/settingswizards/XMLUtils.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/wizards/settingswizards/XMLUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
-class XMLUtils {
+public class XMLUtils {
 
 	private XMLUtils() {}
 	

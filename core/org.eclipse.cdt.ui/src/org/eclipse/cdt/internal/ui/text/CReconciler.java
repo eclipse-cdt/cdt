624470f48bb85f394919cadef7d231d98bbff254
Anton Leherbauer <anton.leherbauer@windriver.com>
2011-02-25 08:07:45 +0000

    Bug 338045 - AST should start building when an editor becomes active

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CReconciler.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/text/CReconciler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 Wind River Systems, Inc. and others.
+ * Copyright (c) 2006, 2011 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -144,8 +144,7 @@ public class CReconciler extends MonoReconciler {
 		 */
 		public void partVisible(IWorkbenchPartReference partRef) {
 			if (partRef.getPart(false) == fTextEditor) {
-				if (hasCModelChanged())
-					CReconciler.this.scheduleReconciling();
+				CReconciler.this.scheduleReconciling();
 				setEditorActive(true);
 			}
 		}

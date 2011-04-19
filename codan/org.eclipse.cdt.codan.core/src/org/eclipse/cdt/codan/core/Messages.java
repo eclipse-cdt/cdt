30fc84b911da4ee6d1d5a9b0262650d17173697c
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2011-04-19 17:00:58 +0000

    Rename messages.properties to Messages.properties.

--- codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/core/Messages.java
+++ codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/core/Messages.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009,2010 Alena Laskavaia 
+ * Copyright (c) 2009,2010 Alena Laskavaia
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Alena Laskavaia  - initial API and implementation
+ *     Alena Laskavaia  - initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.codan.core;
 
@@ -16,7 +16,6 @@ import org.eclipse.osgi.util.NLS;
  * Core Messages
  */
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.cdt.codan.core.messages"; //$NON-NLS-1$
 	public static String CodanApplication_all_option;
 	public static String CodanApplication_Error_ProjectDoesNotExists;
 	public static String CodanApplication_LogRunProject;
@@ -26,11 +25,12 @@ public class Messages extends NLS {
 	public static String CodanApplication_verbose_option;
 	public static String CodanBuilder_Code_Analysis_On;
 	public static String FileScopeProblemPreference_Label;
+
 	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+		NLS.initializeMessages(Messages.class.getName(), Messages.class);
 	}
 
+	// Do not instantiate
 	private Messages() {
 	}
 }

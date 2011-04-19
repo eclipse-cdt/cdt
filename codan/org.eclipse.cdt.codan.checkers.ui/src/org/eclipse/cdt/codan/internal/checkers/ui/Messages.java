3ed57b3aa3f5019129ed35d81f0ec753df957e1a
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2011-04-19 17:09:39 +0000

    Renamed messages.properties to Messages.properties.

--- codan/org.eclipse.cdt.codan.checkers.ui/src/org/eclipse/cdt/codan/internal/checkers/ui/Messages.java
+++ codan/org.eclipse.cdt.codan.checkers.ui/src/org/eclipse/cdt/codan/internal/checkers/ui/Messages.java
@@ -6,25 +6,25 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Alena Laskavaia  - initial API and implementation
+ *     Alena Laskavaia  - initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.codan.internal.checkers.ui;
 
 import org.eclipse.osgi.util.NLS;
 
 /**
- * TODO: add description
+ * Localizable messages.
  */
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.cdt.codan.internal.checkers.ui.messages"; //$NON-NLS-1$
 	public static String CatchByReferenceQuickFix_Message;
 	public static String QuickFixAssignmentInCondition_Message;
 	public static String SuggestedParenthesisQuickFix_Message;
+
 	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+		NLS.initializeMessages(Messages.class.getName(), Messages.class);
 	}
 
+	// Do not instantiate
 	private Messages() {
 	}
 }

6fe37c004ee22bd7b20c3073de5354f9abcb47f2
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2011-04-22 01:20:11 +0000

    Cosmetics.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/corext/util/Messages.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/corext/util/Messages.java
@@ -20,7 +20,7 @@ import com.ibm.icu.text.MessageFormat;
 public class Messages {
 
 	public static String format(String message, Object object) {
-		return MessageFormat.format(message, new Object[] { object});
+		return MessageFormat.format(message, new Object[] { object });
 	}
 
 	public static String format(String message, Object[] objects) {

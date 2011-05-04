28c0d195e911fee3ff6834809bbf34d8868403c2
Vivian Kong <vivkong@ca.ibm.com>
2011-05-04 21:27:19 +0000

    update releng builder

--- releng/org.eclipse.cdt.releng/build.sh
+++ releng/org.eclipse.cdt.releng/build.sh
@@ -25,8 +25,8 @@ cd `dirname $0`
 # Checkout basebuilder to run the build
 mkdir -p tools
 cd tools
-cvs -d /cvsroot/eclipse	co -r R36_RC4 org.eclipse.releng.basebuilder
-cp /home/data/httpd/download.eclipse.org/technology/subversive/0.7/pde-update-site/plugins/org.eclipse.team.svn.pde.build_0.7.9.I20101203-1700.jar \
+cvs -d /cvsroot/eclipse	co -r r36x_v20101125 org.eclipse.releng.basebuilder
+cp /home/data/httpd/download.eclipse.org/technology/subversive/0.7/pde-update-site/plugins/org.eclipse.team.svn.pde.build_*.jar \
 	org.eclipse.releng.basebuilder/plugins
 cd ..
 

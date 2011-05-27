a2c86151ad17bf4e5a256be24b9474d7496d22de
Vivian Kong <vivkong@ca.ibm.com>
2011-05-27 18:18:43 +0000

    Bug 347492 - raise the visibility of getEnvironment(Object context) in the class of UserDefinedEnvironmentSupplier - for John Liu

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/envvar/UserDefinedEnvironmentSupplier.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/envvar/UserDefinedEnvironmentSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2010 Intel Corporation and others.
+ * Copyright (c) 2005, 2011 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -145,7 +145,7 @@ public class UserDefinedEnvironmentSupplier extends
 		}
 		
 	}
-	protected StorableEnvironment getEnvironment(Object context){
+	public StorableEnvironment getEnvironment(Object context){
 		return getEnvironment(context,true);
 	}
 	

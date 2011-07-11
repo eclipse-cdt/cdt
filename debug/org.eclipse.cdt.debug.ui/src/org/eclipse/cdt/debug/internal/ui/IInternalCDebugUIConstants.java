745c9d7cec751867b746a66946a633636cabc535
Anton Leherbauer <anton.leherbauer@windriver.com>
2011-07-11 15:10:13 +0200

    Remove obsolete internal constant, preventing potential illegal thread access during startup

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/IInternalCDebugUIConstants.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/IInternalCDebugUIConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2010 QNX Software Systems and others.
+ * Copyright (c) 2004, 2011 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,9 +12,6 @@
 package org.eclipse.cdt.debug.internal.ui;
 
 import org.eclipse.cdt.debug.ui.CDebugUIPlugin;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Display;
 
 public interface IInternalCDebugUIConstants {
 
@@ -35,14 +32,6 @@ public interface IInternalCDebugUIConstants {
 	 * the workbench font preference page.
 	 */ 
 	public static final String DISASSEMBLY_FONT = PREFIX + "disassemblyFont"; //$NON-NLS-1$	
-
-	/**
-	 * The color id to highlight the source lines in disassembly view. This color is managed via
-	 * the workbench font preference page.
-	 */ 
-	public static final String DISASSEMBLY_SOURCE_LINE_COLOR = PREFIX + "disassembly.sourceLineColor"; //$NON-NLS-1$	
-	
-	public static final RGB DEFAULT_DISASSEMBLY_SOURCE_LINE_RGB = Display.getDefault().getSystemColor( SWT.COLOR_DARK_BLUE ).getRGB();
 
 	// Current stack frame instruction pointer
 	public static final String DISASM_INSTRUCTION_POINTER = PREFIX + "disassemblyInstructionPointer"; //$NON-NLS-1$

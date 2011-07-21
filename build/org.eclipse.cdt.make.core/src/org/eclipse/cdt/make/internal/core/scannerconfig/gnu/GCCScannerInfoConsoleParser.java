f32349a738936db399fe64cfbf9097f8707e2d34
John Liu <jwsliu@ca.ibm.com>
2011-07-21 13:32:29 -0400

    Bug 352758 - Update some private fields in some scanner parsers to protected
    Change-Id: Iea5c586088fa8045e9eb43dff7d43909c0181bd8

--- build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/gnu/GCCScannerInfoConsoleParser.java
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/gnu/GCCScannerInfoConsoleParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2010 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ import org.eclipse.core.runtime.Path;
  */
 public class GCCScannerInfoConsoleParser extends AbstractGCCBOPConsoleParser {
 
-	private ScannerInfoConsoleParserUtility fUtil = null;
+	protected ScannerInfoConsoleParserUtility fUtil = null;
 	private String fDefaultMacroDefinitionValue= "1"; //$NON-NLS-1$
 	
     /* (non-Javadoc)

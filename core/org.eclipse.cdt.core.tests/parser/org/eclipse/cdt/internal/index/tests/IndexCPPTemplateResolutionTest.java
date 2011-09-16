b9e57bcf8c1aa970a02e72c7dc9d566de6854ee1
Markus Schorn <markus.schorn@windriver.com>
2011-09-16 12:44:46 +0200

    Bug 357320: Class template usage in method definded outside of class body.

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/index/tests/IndexCPPTemplateResolutionTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/index/tests/IndexCPPTemplateResolutionTest.java
@@ -1897,4 +1897,15 @@ public class IndexCPPTemplateResolutionTest extends IndexBindingResolutionTestBa
 		getBindingFromASTName("f(2,1)", 1, ICPPMethod.class);
 	}
 
+	//	template<class T> struct C1 {
+	//	    typedef int iterator;
+	//	    iterator m1();
+	//	};
+
+	//	template<class T> typename C1<T>::iterator C1<T>::m1() {
+	//		return 0;
+	//	}
+	public void testUsageOfClassTemplateOutsideOfClassBody_357320() throws Exception {
+		getBindingFromASTName("m1", 0, ICPPMethod.class);
+	}
 }

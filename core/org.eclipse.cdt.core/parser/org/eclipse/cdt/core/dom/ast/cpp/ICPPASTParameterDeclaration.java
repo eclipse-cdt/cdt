a85d7aea25a37d12360055b4cf936539b8649cac
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2011-11-01 20:50:41 -0700

    Added missing @Override annotations.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTParameterDeclaration.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTParameterDeclaration.java
@@ -6,8 +6,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    John Camelon (IBM) - Initial API and implementation
- *    Markus Schorn (Wind River Systems)
+ *     John Camelon (IBM) - Initial API and implementation
+ *     Markus Schorn (Wind River Systems)
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast.cpp;
 
@@ -18,7 +18,6 @@ import org.eclipse.cdt.core.dom.ast.IASTParameterDeclaration;
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ICPPASTParameterDeclaration extends ICPPASTTemplateParameter, IASTParameterDeclaration {
-
 	/**
 	 * @since 5.2
 	 */
@@ -27,15 +26,18 @@ public interface ICPPASTParameterDeclaration extends ICPPASTTemplateParameter, I
 	/**
 	 * @since 5.1
 	 */
+	@Override
 	public ICPPASTParameterDeclaration copy();
 	
 	/**
 	 * @since 5.3
 	 */
+	@Override
 	public ICPPASTParameterDeclaration copy(CopyStyle style);
 
 	/**
 	 * @since 5.2
 	 */
+	@Override
 	public ICPPASTDeclarator getDeclarator();
 }

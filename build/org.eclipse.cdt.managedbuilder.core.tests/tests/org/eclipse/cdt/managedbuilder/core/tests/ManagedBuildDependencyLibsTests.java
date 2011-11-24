6e57986e89b7b9a571058932622daae7b4c985d1
Andrew Gvozdev <angvoz.dev@gmail.com>
2011-11-24 12:30:37 -0500

    Bug 210248 - test projects update, revert sample projects to MinGW toolchain
    This reverts commit 8cc8d861c8b0a8c839a59655719466d1fbc4e275.

--- build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/ManagedBuildDependencyLibsTests.java
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/ManagedBuildDependencyLibsTests.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     QNX Software Systems - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.core.tests;
 
 import java.util.ArrayList;
@@ -43,7 +53,6 @@ public class ManagedBuildDependencyLibsTests extends TestCase {
 	
 	public ManagedBuildDependencyLibsTests(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	public static Test suite() {

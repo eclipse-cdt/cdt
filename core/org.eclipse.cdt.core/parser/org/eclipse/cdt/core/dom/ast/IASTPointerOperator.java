b140bf8ba6e96869a322826338b86beb777db5b5
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2011-12-05 19:40:53 -0800

    Override annotations.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/IASTPointerOperator.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/IASTPointerOperator.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Doug Schaefer (IBM) - Initial API and implementation
+ *     Doug Schaefer (IBM) - Initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast;
 
@@ -15,19 +15,20 @@ package org.eclipse.cdt.core.dom.ast;
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface IASTPointerOperator extends IASTNode {
-
 	/**
 	 * Constant/sentinel.
 	 */
-	public static final IASTPointerOperator[] EMPTY_ARRAY = new IASTPointerOperator[0];
+	public static final IASTPointerOperator[] EMPTY_ARRAY = {};
 
 	/**
 	 * @since 5.1
 	 */
+	@Override
 	public IASTPointerOperator copy();
 
 	/**
 	 * @since 5.3
 	 */
+	@Override
 	public IASTPointerOperator copy(CopyStyle style);
 }

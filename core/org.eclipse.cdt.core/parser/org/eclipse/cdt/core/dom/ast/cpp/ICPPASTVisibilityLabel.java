76e962ee9b55f4f365d3146f8cd63d8607e82963
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2012-01-05 20:21:29 -0800

    @Override annotations.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTVisibilityLabel.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTVisibilityLabel.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    John Camelon (IBM) - Initial API and implementation
+ *     John Camelon (IBM) - Initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast.cpp;
 
@@ -20,7 +20,6 @@ import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ICPPASTVisibilityLabel extends IASTDeclaration {
-
 	/**
 	 * <code>v_public</code> == public:
 	 */
@@ -37,28 +36,28 @@ public interface ICPPASTVisibilityLabel extends IASTDeclaration {
 	public static final int v_private = 3;
 
 	/**
-	 * Get the visibility.
+	 * Returns the visibility.
 	 * 
 	 * @return int
 	 */
 	public int getVisibility();
 
 	/**
-	 * Set visibility.
+	 * Sets visibility.
 	 * 
-	 * @param visibility
-	 *            int
+	 * @param visibility one of v_public, v_protected, v_private
 	 */
 	public void setVisibility(int visibility);
 
-	
 	/**
 	 * @since 5.1
 	 */
+	@Override
 	public ICPPASTVisibilityLabel copy();
 
 	/**
 	 * @since 5.3
 	 */
+	@Override
 	public ICPPASTVisibilityLabel copy(CopyStyle style);
 }

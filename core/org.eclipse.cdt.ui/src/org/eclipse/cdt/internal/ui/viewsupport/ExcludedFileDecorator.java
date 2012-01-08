e62f6b43d8b36679705c14f2c78cc400fa2f30fa
Andrew Gvozdev <angvoz.dev@gmail.com>
2012-01-08 16:13:45 -0500

    bug 312165: Viewers should clearly indicate whether a folder is excluded from build
    Conflicts:
    	core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/viewsupport/ExcludedFileDecorator.java

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/viewsupport/ExcludedFileDecorator.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/viewsupport/ExcludedFileDecorator.java
@@ -11,6 +11,8 @@
 package org.eclipse.cdt.internal.ui.viewsupport;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.preference.JFacePreferences;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.IDecoration;
@@ -31,43 +33,46 @@ import org.eclipse.cdt.internal.ui.CPluginImages;
  * renders the label using the qualifier (gray) color.
  */
 public class ExcludedFileDecorator implements ILightweightLabelDecorator {
-
+	@Override
 	public void decorate(Object element, IDecoration decoration) {
-		if (element instanceof IFile) {
-			IFile resource = (IFile) element;
+		if (element instanceof IFile || element instanceof IFolder) {
+			IResource resource = (IResource) element;
 			ICProjectDescriptionManager mngr = CoreModel.getDefault().getProjectDescriptionManager();
 			ICProjectDescription desc = mngr.getProjectDescription(resource.getProject(), false);
 			if (desc == null)
 				return;
 			ICConfigurationDescription conf = desc.getDefaultSettingConfiguration();
 			ICSourceEntry[] entries = conf.getSourceEntries();
-			boolean isSource = false;
+			boolean isUnderSourceRoot = false;
 			for (ICSourceEntry icSourceEntry : entries) {
 				if (icSourceEntry.getFullPath().isPrefixOf(resource.getFullPath())) {
-					isSource = true;
+					isUnderSourceRoot = true;
 					break;
 				}
 			}
 			// Only bother to mark items that would be included otherwise
-			if (isSource && CDataUtil.isExcluded(resource.getFullPath(), entries)) {
+			if (isUnderSourceRoot && CDataUtil.isExcluded(resource.getFullPath(), entries)) {
 				decoration.addOverlay(CPluginImages.DESC_OVR_INACTIVE);
-				decoration.setForegroundColor(JFaceResources.getColorRegistry().get(
-						JFacePreferences.QUALIFIER_COLOR));
+				decoration.setForegroundColor(JFaceResources.getColorRegistry().get(JFacePreferences.QUALIFIER_COLOR));
 			}
 		}
 	}
 
+	@Override
 	public void addListener(ILabelProviderListener listener) {
 		// We don't track state changes
 	}
 
+	@Override
 	public void dispose() {
 	}
 
+	@Override
 	public boolean isLabelProperty(Object element, String property) {
 		return false;
 	}
 
+	@Override
 	public void removeListener(ILabelProviderListener listener) {
 		// We don't track state changes
 	}

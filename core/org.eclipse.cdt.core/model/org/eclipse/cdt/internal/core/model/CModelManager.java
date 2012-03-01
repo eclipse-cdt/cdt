9ecb8e0c6e8fd33b4b0ed5d37826ea44bbd951e5
Vivian Kong <vivkong@ca.ibm.com>
2012-03-01 15:54:22 -0500

    Bug 372675 - Change CModelManager.getElementsOutOfSynchWithBuffers() to public

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CModelManager.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/CModelManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 QNX Software Systems and others.
+ * Copyright (c) 2000, 2012 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1162,7 +1162,7 @@ public class CModelManager implements IResourceChangeListener, IContentTypeChang
 	/**
 	 * Returns the set of elements which are out of synch with their buffers.
 	 */
-	protected Map<ICElement,ICElement> getElementsOutOfSynchWithBuffers() {
+	public Map<ICElement,ICElement> getElementsOutOfSynchWithBuffers() {
 		return this.elementsOutOfSynchWithBuffers;
 	}
 

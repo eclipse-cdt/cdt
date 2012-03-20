8f2332179b82a79000bcf04402d08ebae9125a2e
David McKnight <dmcknigh@ca.ibm.com>
2012-03-20 19:27:57 +0000

    [performance] DefaultFileServiceCodePageConverter is wasteful with main memory

--- rse/plugins/org.eclipse.rse.services/src/org/eclipse/rse/services/files/DefaultFileServiceCodePageConverter.java
+++ rse/plugins/org.eclipse.rse.services/src/org/eclipse/rse/services/files/DefaultFileServiceCodePageConverter.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2007, 2011 IBM Corporation. All rights reserved.
+ * Copyright (c) 2007, 2012 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -95,6 +95,10 @@ public class DefaultFileServiceCodePageConverter implements
 					}
 					inputStream.close();	
 					outStream.close();
+					if(!altFile.renameTo(file) && file.exists()) {
+						file.delete();
+						altFile.renameTo(file);
+					}
 					altFile.renameTo(file);
 				}
 

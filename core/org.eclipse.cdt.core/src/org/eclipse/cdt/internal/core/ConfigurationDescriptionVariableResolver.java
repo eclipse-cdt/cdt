f1a62f0fe3948a2a2f3b45cb315d67fbb0104442
Anton Gorenkov <xgsa@yandex.ru>
2012-04-02 10:23:31 -0700

    Bug 375814 - Implement the Eclipse variables to obtain CDT configuration name and description for the specified project

---
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/ConfigurationDescriptionVariableResolver.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Anton Gorenkov and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Anton Gorenkov - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.internal.core;
+
+import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
+
+public class ConfigurationDescriptionVariableResolver extends ConfigurationInfoVariableResolver {
+	@Override
+	protected String fetchConfigurationInfo(ICConfigurationDescription configuration) {
+		return configuration.getDescription();
+	}
+}

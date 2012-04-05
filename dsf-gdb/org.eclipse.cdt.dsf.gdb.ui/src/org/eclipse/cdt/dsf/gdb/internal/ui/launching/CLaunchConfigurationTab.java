1a4b2a06b5e6b02fcc07c3f26f01edb6cbb8b68f
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2012-04-04 17:33:36 -0700

    Cosmetics.

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/launching/CLaunchConfigurationTab.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/launching/CLaunchConfigurationTab.java
@@ -38,7 +38,6 @@ import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 
 public abstract class CLaunchConfigurationTab extends AbstractLaunchConfigurationTab {
-
 	/**
 	 * Returns the current C element context from which to initialize default
 	 * settings, or <code>null</code> if none. Note, if possible we will
@@ -60,6 +59,7 @@ public abstract class CLaunchConfigurationTab extends AbstractLaunchConfiguratio
 	        }
 		} catch (CoreException e) {
 		}
+
 		if (projectName != null && !projectName.equals("")) { //$NON-NLS-1$
 			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
 			ICProject cProject = CCorePlugin.getDefault().getCoreModel().create(project);
@@ -144,11 +144,10 @@ public abstract class CLaunchConfigurationTab extends AbstractLaunchConfiguratio
 		if (cProject != null && cProject.exists()) {
 			name = cProject.getElementName();
 			config.setMappedResources(new IResource[] {cProject.getProject()});
-			
+
 			ICProjectDescription projDes = CCorePlugin.getDefault().getProjectDescription(cProject.getProject());
 			String buildConfigID = projDes.getActiveConfiguration().getId();
 			config.setAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_BUILD_CONFIG_ID, buildConfigID);
-			
 		}
 		config.setAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, name);
 	}

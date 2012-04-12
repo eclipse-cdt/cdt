8a7ff9cd2dcff3088fb1357dbfa9343e74536c5f
Marc Khouzam <marc.khouzam@ericsson.com>
2012-04-12 13:34:04 -0400

    Bug 373707: [tracepoints] Make use of the new 'collect /s' command
    Change-Id: I60689b73dd1f081fa3e3b53807e52800c9c03006

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/tracepointactions/CollectActionPage.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/tracepointactions/CollectActionPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Ericsson and others.
+ * Copyright (c) 2010, 2012 Ericsson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Ericsson - initial API and implementation
+ *     Marc Khouzam (Ericsson) - Added support for collecting char pointers as strings (bug 373707)
  *******************************************************************************/
 package org.eclipse.cdt.dsf.gdb.internal.ui.tracepointactions;
 
@@ -15,8 +16,11 @@ import org.eclipse.cdt.debug.ui.breakpointactions.IBreakpointActionPage;
 import org.eclipse.cdt.dsf.gdb.internal.tracepointactions.CollectAction;
 import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -27,6 +31,9 @@ import org.eclipse.swt.widgets.Text;
 public class CollectActionPage extends PlatformObject implements IBreakpointActionPage {
 
 	private Text fCollectString;
+	private Button fTreatCharPtrAsStrings;
+	private Text fTreatCharPtrAsStringsLimit;
+
 	private CollectAction fCollectAction;
 
 	/**
@@ -36,14 +43,51 @@ public class CollectActionPage extends PlatformObject implements IBreakpointActi
 		Composite composite = new Composite(parent, style);
 		composite.setLayout(new GridLayout(2, false));
 
+		// The label asking for what to collect
 		final Label collectLabel = new Label(composite, SWT.NONE);
 		collectLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 		collectLabel.setText(MessagesForTracepointActions.TracepointActions_Collect_Label);
 
+		// The user-specified string of what to collect
 		fCollectString = new Text(composite, SWT.BORDER);
 		fCollectString.setText(fCollectAction.getCollectString());
-		fCollectString.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		fCollectString.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		
+		// An option to collect character pointers as strings
+		fTreatCharPtrAsStrings = new Button(composite, SWT.CHECK);
+		GridData gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1);
+		gd.verticalIndent = 15;
+		// Store the button width before we add the text as we only care about the checkbox width
+		int buttonWidth = fTreatCharPtrAsStrings.computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
+		fTreatCharPtrAsStrings.setText(MessagesForTracepointActions.TracepointActions_Collect_Strings_Label);
+		fTreatCharPtrAsStrings.setLayoutData(gd);
+		fTreatCharPtrAsStrings.setSelection(fCollectAction.getCharPtrAsStrings());
+		fTreatCharPtrAsStrings.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// Disable/enable the limit field
+				fTreatCharPtrAsStringsLimit.setEnabled(fTreatCharPtrAsStrings.getSelection());
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// Disable/enable the limit field
+				fTreatCharPtrAsStringsLimit.setEnabled(fTreatCharPtrAsStrings.getSelection());
+			}
+		});
+		
+		// A label asking for an optional limit of bytes of collected strings 
+		final Label limitLabel = new Label(composite, SWT.NONE);
+		gd = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+		gd.horizontalIndent = buttonWidth;
+		limitLabel.setLayoutData(gd);
+		limitLabel.setText(MessagesForTracepointActions.TracepointActions_Collect_Strings_Limit_Label);
+
+		// A user-specified limit of bytes
+		fTreatCharPtrAsStringsLimit = new Text(composite, SWT.BORDER);
+		fTreatCharPtrAsStringsLimit.setText(getCharPtrAsStringLimit(fCollectAction.getCharPtrAsStringsLimit()));
+		fTreatCharPtrAsStringsLimit.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+		fTreatCharPtrAsStringsLimit.setEnabled(fTreatCharPtrAsStrings.getSelection());
+
 		return composite;
 	}
 
@@ -58,6 +102,10 @@ public class CollectActionPage extends PlatformObject implements IBreakpointActi
     @Override
 	public void actionDialogOK() {
 		fCollectAction.setCollectString(fCollectString.getText());
+		fCollectAction.setCharPtrAsStrings(fTreatCharPtrAsStrings.getSelection());
+		
+		Integer limit = getCharPtrAsStringLimit(fTreatCharPtrAsStringsLimit.getText());
+		fCollectAction.setCharPtrAsStringsLimit(limit);
 	}
 
     @Override
@@ -66,4 +114,33 @@ public class CollectActionPage extends PlatformObject implements IBreakpointActi
 		return createCollectActionComposite(composite, style);
 	}
 
+    /**
+     * Convert the user-specified string into an integer.
+     * If the string is not valid, disable the limit by using null.
+     * @param limitStr The string provided by the user
+     * @return An non-negative integer limit, or null for no limit.
+     */
+    private Integer getCharPtrAsStringLimit(String limitStr) {
+		Integer limit = null;
+		try {
+			limit = Integer.parseInt(limitStr);
+			if (limit < 0) {
+				limit = null;
+			}
+		} catch (NumberFormatException e) {
+			limit = null;
+		}
+		return limit;
+    }
+    
+    /**
+     * Convert the integer limit into a string.
+     * If the string is not valid, disable the limit by using null.
+     * @param limit The integer limit to convert.  Can be null for no limit.
+     * @return The limit as a string, where no limit or a negative limit is the empty string.
+     */
+    private String getCharPtrAsStringLimit(Integer limit) {
+    	if (limit == null || limit < 0) return ""; //$NON-NLS-1$
+		return Integer.toString(limit);
+    }
 }

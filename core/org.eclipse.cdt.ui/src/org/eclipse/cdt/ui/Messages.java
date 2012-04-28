82fb03e1a065fc13a9dfa7dd2baa150beacbb985
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2012-04-28 16:57:39 -0700

    Messages class is not API.

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/Messages.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/Messages.java
@@ -1,28 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others.
+ * Copyright (c) 2007, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    IBM - Initial API and implementation
- *    Markus Schorn (Wind River Systems)
+ *     IBM - Initial API and implementation
+ *     Markus Schorn (Wind River Systems)
  *******************************************************************************/
-
 package org.eclipse.cdt.ui;
 
 import org.eclipse.osgi.util.NLS;
 
-public class Messages extends NLS {
-	/** @since 5.3 */
+class Messages extends NLS {
 	public static String CDTSharedImages_MissingImage;
-
 	public static String CElementGrouping_includeGroupingLabel;
-	
-	/** @since 5.2 */
 	public static String CElementGrouping_macroGroupingLabel;
-	
 	public static String CUIPlugin_jobStartMakeUI;
 
 	static {

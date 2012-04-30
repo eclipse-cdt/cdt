f3588cf4b04f33bd939b727c209b8c02aa7549fb
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2012-04-29 19:01:22 -0700

    Fixed a typo.

--- codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/internal/core/CodeAnalysisNature.java
+++ codan/org.eclipse.cdt.codan.core/src/org/eclipse/cdt/codan/internal/core/CodeAnalysisNature.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 Alena Laskavaia 
+ * Copyright (c) 2009, 2010 Alena Laskavaia
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Alena Laskavaia  - initial API and implementation
+ *     Alena Laskavaia  - initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.codan.internal.core;
 
@@ -19,7 +19,7 @@ import org.eclipse.core.runtime.CoreException;
 /**
  * Code Analysis Nature for builder
  */
-public class CodeAnlysisNature implements IProjectNature {
+public class CodeAnalysisNature implements IProjectNature {
 	/**
 	 * ID of this project nature
 	 */

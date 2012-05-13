de2ec5102b69a4e45a7cdc0ae04f27a018923229
Andrew Gvozdev <angvoz.dev@gmail.com>
2012-05-13 08:37:02 -0400

    Revert "bug 378978: [sd90] Reduce scope of scheduling rule by creating language.settings.xml in advance"
    This reverts commit e9d7946b5a914eba0e625538edcefc95e1c9b7e4.

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/language/settings/providers/LanguageSettingsProvidersSerializer.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/language/settings/providers/LanguageSettingsProvidersSerializer.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.core.language.settings.providers;
 
-import java.io.ByteArrayInputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -917,19 +916,7 @@ public class LanguageSettingsProvidersSerializer {
 				}
 			}
 			if (folder.isAccessible()) {
-				// Create dummy file to reduce scheduling to the file itself
-				if (!fileStorePrj.exists()) {
-					try {
-						fileStorePrj.create(new ByteArrayInputStream("<project/>".getBytes()), true, null); //$NON-NLS-1$
-					} catch (CoreException e) {
-						CCorePlugin.log(e);
-					}
-				}
-				if (fileStorePrj.exists()) {
-					rule = fileStorePrj;
-				} else {
-					rule = folder;
-				}
+				rule = folder;
 			}
 		}
 		if (rule == null) {

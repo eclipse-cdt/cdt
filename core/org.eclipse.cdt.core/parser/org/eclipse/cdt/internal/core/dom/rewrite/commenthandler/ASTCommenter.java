bd376d124bbf3dd09fb7b61a382793a8821a4bb9
Lukas Felber <l.felber@gmx.ch>
2012-05-25 09:49:52 +0200

    Bug 380615 - Assign comments belonging to preprocessor directives to them in NodeCommentMap instead of ignoring them.
    Change-Id: Idabbe95ff7dd4e11b173474b708e622252264adb

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/rewrite/commenthandler/ASTCommenter.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/rewrite/commenthandler/ASTCommenter.java
@@ -12,9 +12,9 @@
 package org.eclipse.cdt.internal.core.dom.rewrite.commenthandler;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-import java.util.TreeMap;
 
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
 import org.eclipse.cdt.core.dom.ast.IASTArrayModifier;
@@ -22,6 +22,7 @@ import org.eclipse.cdt.core.dom.ast.IASTComment;
 import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTEnumerationSpecifier.IASTEnumerator;
 import org.eclipse.cdt.core.dom.ast.IASTExpression;
 import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
 import org.eclipse.cdt.core.dom.ast.IASTInitializer;
@@ -33,15 +34,10 @@ import org.eclipse.cdt.core.dom.ast.IASTPreprocessorStatement;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.IASTTypeId;
-import org.eclipse.cdt.core.dom.ast.IASTEnumerationSpecifier.IASTEnumerator;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompositeTypeSpecifier.ICPPASTBaseSpecifier;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTNamespaceDefinition;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTTemplateParameter;
-import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompositeTypeSpecifier.ICPPASTBaseSpecifier;
 import org.eclipse.cdt.internal.core.dom.parser.ASTNode;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 
 /**
  * This is the starting point of the entire comment handling  process. The creation of the 
@@ -58,29 +54,35 @@ public class ASTCommenter {
 	
 	private static final class PPRangeChecker extends ASTVisitor {
 		int ppOffset;
-		int commentOffset;
+		IASTFileLocation commentNodeLocation;
 		boolean isPrePPComment = true;
 		
-		private PPRangeChecker(boolean visitNodes, int nextPPOfset, int commentNodeOffset) {
-			super(visitNodes);
-			ppOffset = nextPPOfset;
-			commentOffset = commentNodeOffset;
+		private PPRangeChecker(int ppOffset, IASTFileLocation commentNodeLocation) {
+			super(true);
+			this.ppOffset = ppOffset;
+			this.commentNodeLocation = commentNodeLocation;
 		}
 
 		private int checkOffsets(IASTNode node) {
 			int offset = ((ASTNode)node).getOffset();
-			int status = ASTVisitor.PROCESS_CONTINUE;
+			int status = PROCESS_CONTINUE;
 			
-			if (offset > commentOffset && offset < ppOffset) {
+			if (isCommentOnSameLine(node) || offset > commentNodeLocation.getNodeOffset()
+					&& offset < ppOffset) {
 				isPrePPComment = false;
-				status = ASTVisitor.PROCESS_ABORT;
-			} else if ((offset + ((ASTNode)node).getLength() < commentOffset)) {
-				status = ASTVisitor.PROCESS_SKIP;
+				status = PROCESS_ABORT;
+			} else if ((offset + ((ASTNode) node).getLength() < commentNodeLocation.getNodeOffset())) {
+				status = PROCESS_SKIP;
 			} else if (offset > ppOffset) {
-				status = ASTVisitor.PROCESS_ABORT;
+				status = PROCESS_ABORT;
 			}
 			
 			return status;
+		}
+
+		private boolean isCommentOnSameLine(IASTNode node) {
+			return commentNodeLocation.getStartingLineNumber() == node.getFileLocation()
+					.getEndingLineNumber();
 		}
 
 		@Override
@@ -172,137 +174,99 @@ public class ASTCommenter {
 	 * @return NodeCommentMap
 	 */
 	public static NodeCommentMap getCommentedNodeMap(IASTTranslationUnit transUnit){
-		if (transUnit == null) {
-			return new NodeCommentMap();
+		NodeCommentMap commentMap = new NodeCommentMap();
+		if (transUnit== null) {
+			return commentMap;
 		}
-		List<IASTComment> comments = removeNotNeededComments(transUnit);		
-		if (comments == null || comments.isEmpty()) {
-			return new NodeCommentMap();
+		IASTComment[] commentsArray = transUnit.getComments();
+		if (commentsArray == null) {
+			return commentMap;
 		}
+		ArrayList<IASTComment> comments = new ArrayList<IASTComment>(Arrays.asList(commentsArray));
+		filterNonTuComments(comments);
 		return addCommentsToCommentMap(transUnit, comments);
 	}
 
-	private static List<IASTComment> removeNotNeededComments(IASTTranslationUnit transUnit) {
-		List<IASTComment> comments = getCommentsInWorkspace(transUnit);
-		if (comments == null || comments.isEmpty()) {
-			return null;
+	private static void filterNonTuComments(ArrayList<IASTComment> comments) {
+		Iterator<IASTComment> iterator = comments.iterator();
+		while (iterator.hasNext()) {
+			if (!iterator.next().isPartOfTranslationUnitFile()) {
+				iterator.remove();
+			}
 		}
-		return removeAllPreprocessorComments(transUnit, comments);
 	}
 
-	private static List<IASTComment> getCommentsInWorkspace(IASTTranslationUnit tu) {
-		IASTComment[] comments = tu.getComments();
-		ArrayList<IASTComment> commentsInWorkspace = new ArrayList<IASTComment>();
-
-		if (comments == null || comments.length == 0) {
-			return null;
-		}
-
-		for (IASTComment comment : comments) {
-			if (isInWorkspace(comment)) {
-				commentsInWorkspace.add(comment);
-			}
-		}
-		return commentsInWorkspace;
-	}
-
-	private static List<IASTComment> removeAllPreprocessorComments(IASTTranslationUnit tu,
-			List<IASTComment> comments) {
-		IASTPreprocessorStatement[] preprocessorStatements = tu.getAllPreprocessorStatements();
-		TreeMap<Integer, String> treeOfPreProcessorLines = new TreeMap<Integer,String>();
-		TreeMap<String, ArrayList<Integer>> ppOffsetForFiles = new TreeMap<String, ArrayList<Integer>>();
-
-		for (IASTPreprocessorStatement statement : preprocessorStatements) {
-			if (isInWorkspace(statement)) {
-				String fileName = statement.getFileLocation().getFileName();
-				treeOfPreProcessorLines.put(statement.getFileLocation().getStartingLineNumber(), fileName);
-				ArrayList<Integer> offsetList = ppOffsetForFiles.get(fileName);
-				if (offsetList == null) {
-					offsetList = new ArrayList<Integer>();
-					ppOffsetForFiles.put(fileName, offsetList);
-				}
-				offsetList.add(((ASTNode)statement).getOffset());
-			}
-		}
-
-		ArrayList<IASTComment> commentsInCode = new ArrayList<IASTComment>();
-		for (IASTComment comment : comments) {
-			IASTFileLocation commentFileLocation = comment.getFileLocation();
-			int comStartLineNumber = commentFileLocation.getStartingLineNumber();
-			String fileName = commentFileLocation.getFileName();
-			if (treeOfPreProcessorLines.containsKey(comStartLineNumber)
-					&& treeOfPreProcessorLines.get(comStartLineNumber).equals(fileName)) {
-				continue;
-			}
-			if (commentIsAtTheBeginningBeforePreprocessorStatements(comment,
-					ppOffsetForFiles.get(fileName), tu)) {
-				continue;
-			}
-			commentsInCode.add(comment);
-		}
-		return commentsInCode;
-	}
-
-	private static boolean commentIsAtTheBeginningBeforePreprocessorStatements(IASTComment comment, 
-			ArrayList<Integer> listOfPreProcessorOffset, IASTTranslationUnit tu) {
-		if (listOfPreProcessorOffset == null) {
-			return false;
-		}
-		
-		if (comment.getTranslationUnit() == null || comment.getTranslationUnit().getDeclarations().length < 1) {
+	private static boolean isCommentDirectlyBeforePreprocessorStatement(IASTComment comment,
+			IASTPreprocessorStatement ppStatement, IASTTranslationUnit tu) {
+		if (tu == null || tu.getDeclarations().length < 1) {
 			return true;
 		}
-		IASTDeclaration decl = comment.getTranslationUnit().getDeclarations()[0];
-		String commentFileName = comment.getFileLocation().getFileName();
-		boolean sameFile = decl.getFileLocation().getFileName().equals(commentFileName);
-		int commentNodeOffset = ((ASTNode)comment).getOffset();
-		if (sameFile) {
-			if (decl.getFileLocation().getNodeOffset() < commentNodeOffset) {
-				return false;
-			}
+		IASTFileLocation commentLocation = comment.getFileLocation();
+		int pPOffset = ppStatement.getFileLocation().getNodeOffset();
+		if (pPOffset > commentLocation.getNodeOffset()) {
+			PPRangeChecker visti = new PPRangeChecker(pPOffset, commentLocation);
+			tu.accept(visti);
+			return visti.isPrePPComment;
 		}
-		Collections.sort(listOfPreProcessorOffset);
-		int nextPPOfset = -1;
-		for (Integer integer : listOfPreProcessorOffset) {
-			if (integer > commentNodeOffset) {
-				nextPPOfset = integer;
-				PPRangeChecker visti = new PPRangeChecker(true, nextPPOfset, commentNodeOffset);
-				tu.accept(visti);
-				if (visti.isPrePPComment) {
-					return true;
-				}
-			}
-		}
-			
 		return false;
 	}
 
-	private static boolean isInWorkspace(IASTNode node) {
-		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		IPath nodePath = new Path(node.getContainingFilename());
-		for (IProject project : projects) {
-			if (project.getLocation().isPrefixOf(nodePath)) return true;
-		}
-		return false;
+	public static boolean isInWorkspace(IASTNode node) {
+		return node.isPartOfTranslationUnitFile();
 	}
 	
-	private static NodeCommentMap addCommentsToCommentMap(IASTTranslationUnit rootNode,
-			List<IASTComment> comments){
+	private static NodeCommentMap addCommentsToCommentMap(IASTTranslationUnit tu,
+			ArrayList<IASTComment> comments){
 		NodeCommentMap commentMap = new NodeCommentMap();
 		CommentHandler commHandler = new CommentHandler(comments);
 
-		IASTDeclaration[] declarations = rootNode.getDeclarations();
-		for (int i = 0; i < declarations.length; i++) {
-			if (isInWorkspace(declarations[i])) {
-				ASTCommenterVisitor commenter = new ASTCommenterVisitor(commHandler, commentMap);
-				declarations[i].accept(commenter);
-				
-				// Add the remaining comments to the last declaration to prevent comment loss.
-				if (i == declarations.length - 1) {
-					commenter.addRemainingComments(declarations[i]);
-				}
+		assignPreprocessorComments(commentMap, comments, tu);
+		ASTCommenterVisitor commenter = new ASTCommenterVisitor(commHandler, commentMap);
+		tu.accept(commenter);
+		return commentMap;
+	}
+
+	private static void assignPreprocessorComments(NodeCommentMap commentMap,
+			ArrayList<IASTComment> comments, IASTTranslationUnit tu) {
+		IASTPreprocessorStatement[] preprocessorStatementsArray = tu.getAllPreprocessorStatements();
+		if (preprocessorStatementsArray == null) {
+			return;
+		}
+		List<IASTPreprocessorStatement> preprocessorStatements = Arrays
+				.asList(preprocessorStatementsArray);
+
+		if (preprocessorStatements.isEmpty() || comments.isEmpty()) {
+			return;
+		}
+
+		Iterator<IASTPreprocessorStatement> ppIter = preprocessorStatements.iterator();
+		Iterator<IASTComment> commentIter = comments.iterator();
+		IASTPreprocessorStatement curPPStatement = getNextNodeInTu(ppIter);
+		IASTComment curComment = getNextNodeInTu(commentIter);
+		while (curPPStatement != null && curComment != null) {
+			int ppLineNr = curPPStatement.getFileLocation().getStartingLineNumber();
+			int commentLineNr = curComment.getFileLocation().getStartingLineNumber();
+			if (commentLineNr == ppLineNr) {
+				commentMap.addTrailingCommentToNode(curPPStatement, curComment);
+				commentIter.remove();
+				curComment = getNextNodeInTu(commentIter);
+			} else if (commentLineNr > ppLineNr) {
+				curPPStatement = getNextNodeInTu(ppIter);
+			} else if (isCommentDirectlyBeforePreprocessorStatement(curComment, curPPStatement, tu)) {
+				commentMap.addLeadingCommentToNode(curPPStatement, curComment);
+				commentIter.remove();
+				curComment = getNextNodeInTu(commentIter);
+			} else {
+				curComment = getNextNodeInTu(commentIter);
 			}
 		}
-		return commentMap;
-	}	
+	}
+
+	private static <T extends IASTNode> T getNextNodeInTu(Iterator<T> iter) {
+		if (!iter.hasNext()) {
+			return null;
+		}
+		T next = iter.next();
+		return next.isPartOfTranslationUnitFile() ? next : getNextNodeInTu(iter);
+	}
 }

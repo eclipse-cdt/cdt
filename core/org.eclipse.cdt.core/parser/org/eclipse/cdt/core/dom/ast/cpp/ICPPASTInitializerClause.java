c676ddf34e0c5775bec0ed1da10c89e3d8f3e23c
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2012-06-29 15:39:12 -0700

    Cosmetics.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTInitializerClause.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTInitializerClause.java
@@ -6,22 +6,20 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Markus Schorn - initial API and implementation
+ *     Markus Schorn - initial API and implementation
  *******************************************************************************/ 
-
 package org.eclipse.cdt.core.dom.ast.cpp;
 
 import org.eclipse.cdt.core.dom.ast.IASTInitializerClause;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.ICPPEvaluation;
 
 /**
- * C++ specific initializer clause
+ * C++ specific initializer clause.
  * @noextend This interface is not intended to be extended by clients.
  * @noimplement This interface is not intended to be implemented by clients.
  * @since 5.5
  */
 public interface ICPPASTInitializerClause extends IASTInitializerClause {
-
 	/**
 	 * Returns the evaluation object for this expression.
 	 * @noreference This method is not intended to be referenced by clients.

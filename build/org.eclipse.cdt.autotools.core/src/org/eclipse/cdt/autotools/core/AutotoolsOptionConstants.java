4a091991108a0f59d904994b6021aca2ef1a9988
Jeff Johnston <jjohnstn@redhat.com>
2012-07-23 15:43:28 -0400

    Added constants to use when updating Autotools Configure options.
    Change-Id: I18c923f176dbd36d99d8a3f8de72334c8f3c3f9d
    Reviewed-on: https://git.eclipse.org/r/6943
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    IP-Clean: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

---
+++ build/org.eclipse.cdt.autotools.core/src/org/eclipse/cdt/autotools/core/AutotoolsOptionConstants.java
@@ -0,0 +1,51 @@
+package org.eclipse.cdt.autotools.core;
+
+/**
+ * @since 1.2
+ */
+public class AutotoolsOptionConstants {
+	// IAutotoolOption Names
+	public final static String TOOL_CONFIGURE = "configure"; // $NON-NLS-1$
+	public final static String CATEGORY_GENERAL = "general"; // $NON-NLS-1$
+	public final static String OPT_CONFIGDIR = "configdir"; // $NON-NLS-1$
+	public final static String OPT_CACHE_FILE = "cache-file"; // $NON-NLS-1$
+	public final static String OPT_HELP = "help"; // $NON-NLS-1$
+	public final static String OPT_NO_CREATE = "no-create"; // $NON-NLS-1$
+	public final static String OPT_QUIET = "quiet"; // $NON-NLS-1$
+	public final static String OPT_VERSION = "version"; // $NON-NLS-1$
+	public final static String CATEGORY_PLATFORM = "platform"; // $NON-NLS-1$
+	public final static String OPT_HOST = "host"; // $NON-NLS-1$
+	public final static String OPT_BUILD = "build"; // $NON-NLS-1$
+	public final static String OPT_TARGET = "target"; // $NON-NLS-1$
+	public final static String CATEGORY_DIRECTORIES = "directories"; // $NON-NLS-1$
+	public final static String OPT_PREFIX = "prefix"; // $NON-NLS-1$
+	public final static String OPT_EXEC_PREFIX = "exec-prefix"; // $NON-NLS-1$
+	public final static String OPT_LIBDIR = "libdir"; // $NON-NLS-1$
+	public final static String OPT_BINDIR = "bindir"; // $NON-NLS-1$
+	public final static String OPT_SBINDIR = "sbindir"; // $NON-NLS-1$
+	public final static String OPT_INCLUDEDIR = "includedir"; // $NON-NLS-1$
+	public final static String OPT_DATADIR = "datadir"; // $NON-NLS-1$
+	public final static String OPT_SYSCONFDIR = "sysconfdir"; // $NON-NLS-1$
+	public final static String OPT_INFODIR = "infodir"; // $NON-NLS-1$
+	public final static String OPT_MANDIR = "mandir"; // $NON-NLS-1$
+	public final static String OPT_SRCDIR = "srcdir"; // $NON-NLS-1$
+	public final static String OPT_LOCALSTATEDIR = "localstatedir"; // $NON-NLS-1$
+	public final static String OPT_SHAREDSTATEDIR = "sharedstatedir"; // $NON-NLS-1$
+	public final static String OPT_LIBEXECDIR = "libexecdir"; // $NON-NLS-1$
+	public final static String OPT_OLDINCLUDEDIR = "oldincludedir"; // $NON-NLS-1$
+	public final static String CATEGORY_FILENAMES = "filenames"; // $NON-NLS-1$
+	public final static String OPT_PROGRAM_PREFIX = "program-prefix"; // $NON-NLS-1$
+	public final static String OPT_PROGRAM_SUFFIX = "program-suffix"; // $NON-NLS-1$
+	public final static String OPT_PROGRAM_TRANSFORM_NAME = "program-transform-name"; // $NON-NLS-1$
+	public final static String CATEGORY_FEATURES = "features"; // $NON-NLS-1$
+	public final static String OPT_ENABLE_MAINTAINER_MODE = "enable-maintainer-mode"; // $NON-NLS-1$
+	public final static String FLAG_CFLAGS = "CFLAGS"; // $NON-NLS-1$
+	public final static String OPT_CFLAGS_DEBUG = "cflags-debug"; // $NON-NLS-1$
+	public final static String OPT_CFLAGS_GPROF = "cflags-gprof"; // $NON-NLS-1$ 
+	public final static String OPT_CFLAGS_GCOV = "cflags-gcov"; // $NON-NLS-1$ 
+	public final static String OPT_USER = "user"; // $NON-NLS-1$
+	public final static String TOOL_AUTOGEN = "autogen"; // $NON-NLS-1$
+	public final static String CATEGORY_OPTIONS = "options"; // $NON-NLS-1$
+	public final static String OPT_AUTOGENOPTS = "autogenOpts"; // $NON-NLS-1$
+
+}

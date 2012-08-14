9dd70eb4bb483044ad231c037eac45722bae163c
Markus Schorn <markus.schorn@windriver.com>
2012-08-14 14:16:19 -0700

    Bug 299911: Increment plugin and pdom version.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTInitializerClause.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTInitializerClause.java
@@ -18,7 +18,7 @@ import org.eclipse.cdt.internal.core.dom.parser.cpp.ICPPEvaluation;
  * C++ specific initializer clause
  * @noextend This interface is not intended to be extended by clients.
  * @noimplement This interface is not intended to be implemented by clients.
- * @since 5.4
+ * @since 5.5
  */
 public interface ICPPASTInitializerClause extends IASTInitializerClause {
 

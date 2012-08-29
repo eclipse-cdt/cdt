94bc2d793032fa62c459dadc86b16d71ce898bd6
Vivian Kong <vivkong@ca.ibm.com>
2012-08-28 20:35:12 -0400

    Revert "Bug 387191 - ARA: Incorrect mirrored strings occurred on panels of right click menu selection of Invoke Autotools"
    This reverts commit f9a91f9eaeaa37f1cce06d6ebd08be56d2d3744e.

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/InvokeMessages.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/InvokeMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 Red Hat Inc..
+ * Copyright (c) 2006, 2007, 2009 Red Hat Inc..
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,15 +7,12 @@
  *
  * Contributors:
  *     Red Hat Incorporated - initial API and implementation
- *     IBM Corporation
  *******************************************************************************/
 package org.eclipse.cdt.internal.autotools.ui.actions;
 
 import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-
-import org.eclipse.osgi.util.TextProcessor;
 
 public class InvokeMessages {
 	private static final String BUNDLE_NAME = InvokeMessages.class.getName();
@@ -35,7 +32,7 @@ public class InvokeMessages {
 	 */
 	public static String getString(String key) {
 		try {
-			return TextProcessor.process(RESOURCE_BUNDLE.getString(key));
+			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
 		}
@@ -50,7 +47,7 @@ public class InvokeMessages {
 	 * @return the resource bundle message
 	 */
 	public static String getFormattedString(String key, String[] args) {
-		return TextProcessor.process(MessageFormat.format(getString(key), (Object[])args));
+		return MessageFormat.format(getString(key), (Object[])args);
 	}
 
 }

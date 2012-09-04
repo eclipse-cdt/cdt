c8075218df87be17329a0291b87fa45247660701
Andrew Gvozdev <angvoz.dev@gmail.com>
2012-09-04 17:34:05 -0400

    bug 388755: Fix EFSExtensionProvider to return conventional path on Windows as C:/path (without extra leading slash)
    Change-Id: I82507744f069d774579f0bdd301e210df8157305

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/EFSExtensionProvider.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/EFSExtensionProvider.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.URIUtil;
 
 /**
@@ -22,25 +23,25 @@ import org.eclipse.core.runtime.URIUtil;
  * operate on virtual EFS filesystems (where IFileStores are just links to other IFileStores), or that operate
  * on the physical file backed by an IFileStore, without having to know the implementation details of a given
  * EFS filesystem.
- * 
+ *
  * Provides a default implementation that assumes that URIs for the given filesystem map directly to resources
  * in the physical filesystem, and that the path component of the URI is a direct representation of the
  * absolute path to the file in the physical filesystem.
- * 
+ *
  * Clients wishing to support a filesystem with different behaviour should extend this class and override its
  * methods where appropriate.
- * 
+ *
  * Clients should not typically call methods on this class or its descendants directly. Instead, they should
  * call the appropriate method in FileSystemUtilityManager so that said manager can properly route calls to
  * the proper utility, depending on the filesystem.
- * 
+ *
  * <strong>EXPERIMENTAL</strong>. This class or interface has been added as part of a work in progress. There
  * is no guarantee that this API will work or that it will remain the same. Please do not use this API without
  * consulting with the CDT team.
- * 
+ *
  * @author crecoskie
  * @since 5.2
- * 
+ *
  */
 public abstract class EFSExtensionProvider {
 
@@ -48,20 +49,25 @@ public abstract class EFSExtensionProvider {
 	 * If the EFS store represented by locationURI is backed by a physical file, gets the path corresponding
 	 * to the underlying file as the operating system on hosting machine would see it. In the future, it would
 	 * be better if EFS had an API for this.
-	 * 
+	 *
 	 * @param locationURI
 	 * @return String representing the path, or <code>null</code> if there is an error or if there is no such
 	 *         physical file.
 	 */
 	public String getPathFromURI(URI locationURI) {
-		return locationURI.getPath();
+		String path = locationURI.getPath();
+		// URI path on Windows is represented as "/C:/path"
+		if (path != null && Platform.getOS().equals(Platform.WS_WIN32) && path.matches("/[A-Za-z]:.*")) { //$NON-NLS-1$
+			path = path.substring(1);
+		}
+		return path;
 	}
 
 	/**
 	 * In the case of a virtual filesystem, where URIs in the given filesystem are just soft links in EFS to
 	 * URIs in other filesystems, returns the URI that this URI links to. If the filesystem is not virtual,
 	 * then this method acts as an identity mapping.
-	 * 
+	 *
 	 * @param locationURI
 	 * @return A URI corresponding to the linked store, or <code>null</code> on error.
 	 */
@@ -73,25 +79,25 @@ public abstract class EFSExtensionProvider {
 	 * Creates a new URI which clones the contents of the original URI, but with the path replaced by the
 	 * given absolute path, such that calling getPathFromURI() on the returned URI will return the given path. Returns
 	 * null on error.
-	 * 
+	 *
 	 * The default implementation places the path in the path field of the URI, ensuring that there is a leading slash.
 	 * It also determines whether or not to convert backslashes in the provided path based on whether or not the
 	 * local operating system's file separator is a backslash, thus ensuring proper behaviour for URIs corresponding
 	 * to the local filesystem.
-	 * 
+	 *
 	 * @param locationOnSameFilesystem
 	 * @param path An absolute path.
 	 * @return URI
 	 */
 	public URI createNewURIFromPath(URI locationOnSameFilesystem, String path) {
 		URI uri = locationOnSameFilesystem;
-		
+
 		Path p = new Path(path);
 		String pathString = p.toString(); // to convert any backslashes to slashes if we are on Windows
 		final int length = pathString.length();
 		StringBuffer pathBuf = new StringBuffer(length + 1);
 
-		// force the path to be absolute
+		// force the path to be absolute including Windows where URI path is represented as "/C:/path"
 		if (length > 0 && (pathString.charAt(0) != '/')) {
 			pathBuf.append('/');
 		}
@@ -99,9 +105,9 @@ public abstract class EFSExtensionProvider {
 		if (pathString.startsWith("//")) //$NON-NLS-1$
 			pathBuf.append('/').append('/');
 		pathBuf.append(pathString);
-		
+
 		try {
-			//Bug 326957 - EFSExtensionProvider does not handle URI's correctly 
+			//Bug 326957 - EFSExtensionProvider does not handle URI's correctly
 			return new URI(uri.getScheme(), uri.getAuthority(), pathBuf.toString(), // replaced!
 					uri.getQuery(), uri.getFragment());
 		} catch (URISyntaxException e) {
@@ -115,11 +121,11 @@ public abstract class EFSExtensionProvider {
 	 * another path (say, on the local machine), this method returns the path that the store maps to. I.e., it
 	 * returns the path that the path returned by getPathFromURI(URI locationURI) maps to. If there is no such
 	 * mapping, then an identity mapping of the paths is assumed.
-	 * 
+	 *
 	 * Typically if a filesystem maps one filesytem to another, it will place the mapped path in the path
 	 * field of its URIs (which the default implementation assumes), but this is not guaranteed to be so for
 	 * all filesystem implementations.
-	 * 
+	 *
 	 * @return String representing the path, or <code>null</code> on error.
 	 */
 	public String getMappedPath(URI locationURI) {
@@ -129,7 +135,7 @@ public abstract class EFSExtensionProvider {
 	/**
 	 * Returns true if the given URI is part of a virtual filesystem and thus points to another underlying
 	 * URI. Returns false otherwise. By default, filesystems are assumed to be non-virtual.
-	 * 
+	 *
 	 * @param locationURI
 	 * @return boolean
 	 */
@@ -139,12 +145,12 @@ public abstract class EFSExtensionProvider {
 
 	/**
 	 * Creates a new URI with the same components as the baseURI, except that calling
-	 * getPathFromURI() on the new URI will return a path that has the extension appended to 
+	 * getPathFromURI() on the new URI will return a path that has the extension appended to
 	 * the path returned by baseURI.getPathFromURI()
-	 * 
+	 *
 	 * The default implementation assumes that the path component of the URI is used
 	 * to store the path.
-	 * 
+	 *
 	 * @param baseURI
 	 * @param extension
 	 * @return the new URI, or <code>null</code> on error.

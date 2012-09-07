edd32c480d16febf5b2131f7e12b2b2f8e7d18e6
Marc Khouzam <marc.khouzam@ericsson.com>
2012-09-06 21:11:57 -0400

    Invalid @since tag

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPClassType.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPClassType.java
@@ -104,7 +104,7 @@ public interface ICPPClassType extends ICompositeType, ICPPBinding {
 	/**
 	 * Returns whether this type is declared final.
 	 * 
-	 * @since 5.5
+	 * @since 5.4
 	 */
 	public boolean isFinal();
 }

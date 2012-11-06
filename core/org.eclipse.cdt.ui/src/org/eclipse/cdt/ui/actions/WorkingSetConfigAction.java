c1210c5d8b44253456c5b818e81d7aa5d91e592b
Tom Hochstein <tom.hochstein@freescale.com>
2012-11-06 13:25:02 -0600

    Bug 392954 - Manage Working Sets dialog can be opened when all working sets are empty

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/actions/WorkingSetConfigAction.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/actions/WorkingSetConfigAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Intel Corporation, QNX Software Systems, and others.
+ * Copyright (c) 2008, 2009, 2012 Intel Corporation, QNX Software Systems, and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     Intel Corporation - initial API and implementation
  *     QNX Software Systems - [272416] Rework the config sets dialog
+ *     Freescale Semiconductor - [392954] disable the action if only empty working sets exist
  *******************************************************************************/
 package org.eclipse.cdt.ui.actions;
 
@@ -27,40 +28,53 @@ import org.eclipse.cdt.internal.ui.workingsets.WorkingSetConfigurationDialog;
 /**
  */
 public class WorkingSetConfigAction implements IWorkbenchWindowActionDelegate, IPropertyChangeListener {
+	
 	private static final IWorkingSetManager wsm = CUIPlugin.getDefault().getWorkbench().getWorkingSetManager();
 	private boolean enabled = true;
-
 	private IWorkbenchWindow window;
+	private IAction action;
 
 	@Override
 	public void run(IAction action) {
-		new WorkingSetConfigurationDialog(window.getShell()).open();
+		this.action = action;
+		checkWS();
+		if (enabled) {
+			new WorkingSetConfigurationDialog(window.getShell()).open();
+		}
 	}
 
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
+		this.action = action;
 		checkWS();
-		if (action.isEnabled() != enabled)
-			action.setEnabled(enabled);
 	}
+
 	@Override
 	public void dispose() {
 		wsm.removePropertyChangeListener(this);
-
 	}
+
 	@Override
 	public void init(IWorkbenchWindow window) {
 		this.window = window;
 		wsm.addPropertyChangeListener(this);
 		checkWS();
 	}
-
-	private IWorkingSet[] checkWS() {
+	
+	private void checkWS() {
+		enabled = false;
 		IWorkingSet[] w = wsm.getWorkingSets();
 		if (w == null)
 			w = new IWorkingSet[0];
-		enabled = w.length > 0;
-		return w;
+		for (IWorkingSet ws : w) {
+			if (!ws.isEmpty()) {
+				enabled = true;
+				break;
+			}
+		}
+		if (action != null) {
+			action.setEnabled(enabled);
+		}
 	}
 
 	@Override

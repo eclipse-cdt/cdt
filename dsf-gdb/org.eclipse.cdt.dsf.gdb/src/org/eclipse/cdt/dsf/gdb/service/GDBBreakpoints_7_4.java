991bb3f62c5ebfc2898f1d726fbfc164ba5fa1a6
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2012-11-13 17:00:00 -0500

    Bug 392512 - Support GDB breakpoint notifications Change-Id: I244ba995392806a56a52852e0d0d9ea72f87edfa

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/service/GDBBreakpoints_7_4.java
@@ -0,0 +1,252 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Mentor Graphics and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mentor Graphics - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.dsf.gdb.service;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.concurrent.ImmediateRequestMonitor;
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
+import org.eclipse.cdt.dsf.datamodel.IDMContext;
+import org.eclipse.cdt.dsf.debug.service.IBreakpoints;
+import org.eclipse.cdt.dsf.debug.service.IBreakpointsExtension;
+import org.eclipse.cdt.dsf.debug.service.command.ICommandControl;
+import org.eclipse.cdt.dsf.debug.service.command.IEventListener;
+import org.eclipse.cdt.dsf.gdb.internal.GdbPlugin;
+import org.eclipse.cdt.dsf.mi.service.IMICommandControl;
+import org.eclipse.cdt.dsf.mi.service.MIBreakpointDMData;
+import org.eclipse.cdt.dsf.mi.service.MIBreakpoints;
+import org.eclipse.cdt.dsf.mi.service.MIBreakpointsSynchronizer;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIBreakpoint;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIConst;
+import org.eclipse.cdt.dsf.mi.service.command.output.MINotifyAsyncOutput;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIOOBRecord;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIOutput;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIResult;
+import org.eclipse.cdt.dsf.mi.service.command.output.MITuple;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIValue;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+/**
+ * Breakpoints service for GDB 7.4.
+ * Using breakpoint notifications introduced in 7.4 supports synchronization between the breakpoints 
+ * set from the GDB console and the Breakpoints view as well as the tracepoints reported form trace files.
+ * 
+ * @since 4.2
+ */
+public class GDBBreakpoints_7_4 extends GDBBreakpoints_7_2 implements IEventListener {
+
+    // Breakpoint notifications
+    private static final String BREAKPOINT_PREFIX = "breakpoint-"; //$NON-NLS-1$
+    private static final String BREAKPOINT_CREATED = BREAKPOINT_PREFIX + "created"; //$NON-NLS-1$
+    private static final String BREAKPOINT_MODIFIED = BREAKPOINT_PREFIX + "modified"; //$NON-NLS-1$
+    private static final String BREAKPOINT_DELETED = BREAKPOINT_PREFIX + "deleted"; //$NON-NLS-1$
+
+	private IMICommandControl fConnection;
+
+	public GDBBreakpoints_7_4(DsfSession session) {
+		super(session);
+	}
+
+	@Override
+	public void initialize(final RequestMonitor rm) {
+		super.initialize(new ImmediateRequestMonitor(rm) {
+			@Override
+			protected void handleSuccess() {
+				doInitialize(rm);
+			}
+		});
+	}
+
+	private void doInitialize(final RequestMonitor rm) {
+		fConnection = getServicesTracker().getService(IMICommandControl.class);
+		if (fConnection == null) {
+			rm.setStatus(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, "Service is not available")); //$NON-NLS-1$
+			rm.done();
+			return;
+		}
+		fConnection.addEventListener(this);
+
+		// Register this service
+		register(new String[] { IBreakpoints.class.getName(),
+		                        IBreakpointsExtension.class.getName(),
+								MIBreakpoints.class.getName(),
+								GDBBreakpoints_7_0.class.getName(),
+								GDBBreakpoints_7_2.class.getName(),
+								GDBBreakpoints_7_4.class.getName() },
+				new Hashtable<String, String>());
+
+		rm.done();
+	}
+
+	@Override
+	public void shutdown(RequestMonitor requestMonitor) {
+		ICommandControl control = getCommandControl();
+		if (control != null) {
+			control.removeEventListener(this);
+		}
+        unregister();
+		super.shutdown(requestMonitor);
+	}
+
+	@Override
+	public void eventReceived(Object output) {
+		if (output instanceof MIOutput) {
+			MIBreakpointsSynchronizer bs = getServicesTracker().getService(MIBreakpointsSynchronizer.class);
+			if (bs != null) {	
+				MIOOBRecord[] records = ((MIOutput)output).getMIOOBRecords();
+				for(MIOOBRecord r : records) {
+					if (r instanceof MINotifyAsyncOutput) {
+						MINotifyAsyncOutput notifyOutput = (MINotifyAsyncOutput)r;
+						String asyncClass = notifyOutput.getAsyncClass();
+						if (BREAKPOINT_CREATED.equals(asyncClass)) {
+							MIBreakpoint bpt = getMIBreakpointFromOutput(notifyOutput);
+							if (bpt != null)
+								bs.targetBreakpointCreated(bpt);
+						}
+						else if (BREAKPOINT_DELETED.equals(asyncClass)) {
+							int id = getMIBreakpointIdFromOutput(notifyOutput);
+							if (id != 0)
+								bs.targetBreakpointDeleted(id);
+						}
+						else if (BREAKPOINT_MODIFIED.equals(asyncClass)) {
+							MIBreakpoint bpt = getMIBreakpointFromOutput(notifyOutput);
+							if (bpt != null)
+								bs.targetBreakpointModified(bpt);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private IMICommandControl getCommandControl() {
+		return fConnection;
+	}
+	
+	private MIBreakpoint getMIBreakpointFromOutput(MINotifyAsyncOutput notifyOutput) {
+		MIBreakpoint bpt = null;
+		MIResult[] results = notifyOutput.getMIResults();
+		for(int i = 0; i < results.length; i++) {
+			String var = results[i].getVariable();
+			MIValue val = results[i].getMIValue();
+			if (var.equals("bkpt")) { //$NON-NLS-1$
+				if (val instanceof MITuple) {
+					bpt = new MIBreakpoint((MITuple)val);
+				}
+			}
+		}
+		return bpt;
+	}
+
+	private int getMIBreakpointIdFromOutput(MINotifyAsyncOutput notifyOutput) {
+		MIResult[] results = notifyOutput.getMIResults();
+		for(int i = 0; i < results.length; i++) {
+			String var = results[i].getVariable();
+			MIValue val = results[i].getMIValue();
+			if (var.equals("id") && val instanceof MIConst) { //$NON-NLS-1$
+            	try {
+					return Integer.parseInt(((MIConst)val).getCString().trim());
+				}
+				catch(NumberFormatException e) {
+					GdbPlugin.log(new Status(IStatus.ERROR, GdbPlugin.PLUGIN_ID, "Invalid breakpoint id")); //$NON-NLS-1$
+				}
+			}
+		}
+		return 0;
+	}
+
+	@Override
+	protected void addBreakpoint(IBreakpointsTargetDMContext context, Map<String, Object> attributes, DataRequestMonitor<IBreakpointDMContext> finalRm) {
+		MIBreakpointsSynchronizer bs = getServicesTracker().getService(MIBreakpointsSynchronizer.class);
+		if (bs != null) {
+			// Skip the breakpoints set from the console or from outside of Eclipse 
+			// because they are already installed on the target.
+			MIBreakpoint miBpt = bs.getTargetBreakpoint(context, attributes);
+			if (miBpt != null) {
+				bs.removeCreatedTargetBreakpoint(context, miBpt);
+				MIBreakpointDMData newBreakpoint = new MIBreakpointDMData(miBpt);
+				getBreakpointMap(context).put(newBreakpoint.getNumber(), newBreakpoint);
+				IBreakpointDMContext dmc = new MIBreakpointDMContext(this, new IDMContext[] { context }, newBreakpoint.getNumber());
+				finalRm.setData(dmc);
+
+				getSession().dispatchEvent(new BreakpointAddedEvent(dmc), getProperties());
+
+				finalRm.done();
+				return;
+			}
+		}
+		super.addBreakpoint(context, attributes, finalRm);
+	}
+
+	@Override
+	protected void addTracepoint(IBreakpointsTargetDMContext context, Map<String, Object> attributes, DataRequestMonitor<IBreakpointDMContext> drm) {
+		MIBreakpointsSynchronizer bs = getServicesTracker().getService(MIBreakpointsSynchronizer.class);
+		if (bs != null) {
+			// Skip the breakpoints set from the console or from outside of Eclipse 
+			// because they are already installed on the target.
+			MIBreakpoint miBpt = bs.getTargetBreakpoint(context, attributes);
+			if (miBpt != null) {
+				bs.removeCreatedTargetBreakpoint(context, miBpt);
+				MIBreakpointDMData newBreakpoint = new MIBreakpointDMData(miBpt);
+				getBreakpointMap(context).put(newBreakpoint.getNumber(), newBreakpoint);
+				IBreakpointDMContext dmc = new MIBreakpointDMContext(this, new IDMContext[] { context }, newBreakpoint.getNumber());
+				drm.setData(dmc);
+	
+				getSession().dispatchEvent(new BreakpointAddedEvent(dmc), getProperties());
+	
+				drm.done();
+				return;
+			}
+		}
+		super.addTracepoint(context, attributes, drm);
+	}
+
+	@Override
+	protected void addWatchpoint(IBreakpointsTargetDMContext context, Map<String, Object> attributes, DataRequestMonitor<IBreakpointDMContext> drm) {
+		MIBreakpointsSynchronizer bs = getServicesTracker().getService(MIBreakpointsSynchronizer.class);
+		if (bs != null) {
+			// Skip the breakpoints set from the console or from outside of Eclipse 
+			// because they are already installed on the target.
+			MIBreakpoint miBpt = bs.getTargetBreakpoint(context, attributes);
+			if (miBpt != null) {
+				bs.removeCreatedTargetBreakpoint(context, miBpt);
+				MIBreakpointDMData newBreakpoint = new MIBreakpointDMData(miBpt);
+				getBreakpointMap(context).put(newBreakpoint.getNumber(), newBreakpoint);
+				IBreakpointDMContext dmc = new MIBreakpointDMContext(this, new IDMContext[] { context }, newBreakpoint.getNumber());
+				drm.setData(dmc);
+	
+				getSession().dispatchEvent(new BreakpointAddedEvent(dmc), getProperties());
+	
+				drm.done();
+				return;
+			}
+		}
+		super.addWatchpoint(context, attributes, drm);
+	}
+
+	@Override
+	protected void deleteBreakpointFromTarget(IBreakpointsTargetDMContext context, int reference, RequestMonitor finalRm) {
+		MIBreakpointsSynchronizer bs = getServicesTracker().getService(MIBreakpointsSynchronizer.class);
+		if (bs != null) {
+			// Do nothing if the breakpoint is deleted from the console.
+			if (bs.isTargetBreakpointDeleted(context, reference, true)) {
+				finalRm.done();
+				return;
+			}
+		}
+		super.deleteBreakpointFromTarget(context, reference, finalRm);
+	}
+}
--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIBreakpointsManager.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIBreakpointsManager.java
@@ -41,6 +41,7 @@ import org.eclipse.cdt.debug.internal.core.breakpoints.BreakpointProblems;
 import org.eclipse.cdt.dsf.concurrent.CountingRequestMonitor;
 import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
 import org.eclipse.cdt.dsf.concurrent.DsfRunnable;
+import org.eclipse.cdt.dsf.concurrent.ImmediateExecutor;
 import org.eclipse.cdt.dsf.concurrent.ImmediateRequestMonitor;
 import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
 import org.eclipse.cdt.dsf.concurrent.ThreadSafe;
@@ -82,6 +83,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
@@ -104,7 +106,19 @@ import com.ibm.icu.text.MessageFormat;
  */
 public class MIBreakpointsManager extends AbstractDsfService implements IBreakpointManagerListener, IBreakpointListener
 {
-    // Note: Find a way to import this (careful of circular dependencies)
+    /**
+     * A listener is notified by {@link MIBreakpointsManager} when 
+     * the breakpoints tracking starts or stops. 
+     * @since 4.2
+     */
+    public interface IMIBreakpointsTrackingListener {
+
+    	public void breakpointTrackingStarted(IBreakpointsTargetDMContext bpTargetDMC);
+    	
+    	public void breakpointTrackingStopped(IBreakpointsTargetDMContext bpTargetDMC);
+	}
+
+	// Note: Find a way to import this (careful of circular dependencies)
     public final static String GDB_DEBUG_MODEL_ID = "org.eclipse.cdt.dsf.gdb"; //$NON-NLS-1$
 
     // Extra breakpoint attributes
@@ -176,6 +190,8 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
 
     private Map<ICBreakpoint, IMarker> fBreakpointMarkerProblems =
         new HashMap<ICBreakpoint, IMarker>();
+
+    private ListenerList fTrackingListeners = new ListenerList();
 
     ///////////////////////////////////////////////////////////////////////////
     // String constants
@@ -284,6 +300,7 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
         getSession().removeServiceEventListener(this);
         fBreakpointManager.removeBreakpointListener(this);
         fBreakpointManager.removeBreakpointManagerListener(this);
+        fTrackingListeners.clear();
 
         // Cleanup the breakpoints that are still installed by the service.
         // Use a counting monitor which will call mom to complete the shutdown
@@ -369,7 +386,16 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
                 getExecutor().submit(new Runnable() {
                 	@Override
                 	public void run() {
-                        installInitialBreakpoints(dmc, rm);
+                        installInitialBreakpoints(dmc, new RequestMonitor(ImmediateExecutor.getInstance(), rm) {
+                        	@Override
+                        	protected void handleSuccess() {
+                        		// Notify breakpoints tracking listeners that the tracking is started.
+                        		for (Object o : fTrackingListeners.getListeners()) {
+                        			((IMIBreakpointsTrackingListener)o).breakpointTrackingStarted(dmc);
+                        		}
+                    			rm.done();
+                        	};
+                        });
                     }
                 });
 
@@ -491,6 +517,10 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
                 fBreakpointIDs.remove(dmc);
                 fTargetBPs.remove(dmc);
                 fBreakpointThreads.remove(dmc);
+        		// Notify breakpoints tracking listeners that the tracking is stopped.
+        		for (Object o : fTrackingListeners.getListeners()) {
+        			((IMIBreakpointsTrackingListener)o).breakpointTrackingStopped(dmc);
+        		}
                 rm.done();
             }
         };
@@ -719,8 +749,10 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
      * @param dmc
      * @param breakpoint
      * @param rm
+     * 
+     * @since 4.2
      */
-    private void uninstallBreakpoint(final IBreakpointsTargetDMContext dmc,
+    public void uninstallBreakpoint(final IBreakpointsTargetDMContext dmc,
             final ICBreakpoint breakpoint, final RequestMonitor rm)
     {
         // Retrieve the breakpoint maps
@@ -1927,5 +1959,19 @@ public class MIBreakpointsManager extends AbstractDsfService implements IBreakpo
 		catch(CoreException e) {
 		}
     	return true;
+    }
+
+    /**
+	 * @since 4.2
+	 */
+    public void addBreakpointsTrackingListener(IMIBreakpointsTrackingListener listener) {
+    	fTrackingListeners.add(listener);
+    }
+
+    /**
+	 * @since 4.2
+	 */
+    public void removeBreakpointsTrackingListener(IMIBreakpointsTrackingListener listener) {
+    	fTrackingListeners.remove(listener);
     }
 }

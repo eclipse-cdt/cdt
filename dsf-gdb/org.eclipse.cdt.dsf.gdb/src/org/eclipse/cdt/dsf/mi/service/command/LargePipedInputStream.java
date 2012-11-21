17f5758af3e7030ce3f98bff1407a5dc4901c1d8
Marc Khouzam <marc.khouzam@ericsson.com>
2012-11-21 10:22:38 -0500

    Bug 394797 - LargePipedInputStream can use java 6's new constructor for PipedInputStream
    Change-Id: I416c3c877fcae238b8faaff16937ef7dac378f35
    Reviewed-on: https://git.eclipse.org/r/8787
    Reviewed-by: Marc Khouzam <marc.khouzam@ericsson.com>
    IP-Clean: Marc Khouzam <marc.khouzam@ericsson.com>
    Tested-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/LargePipedInputStream.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/LargePipedInputStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 Wind River Systems and others.
+ * Copyright (c) 2006, 2012 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  * 
  * Contributors:
  *     Wind River Systems - initial API and implementation
+ *     Marc Khouzam (Ericsson) - Specify the larger size in the new constructor
+ *                               of PipedInputStream provided by Java 6
  *******************************************************************************/
 package org.eclipse.cdt.dsf.mi.service.command;
 
@@ -17,13 +19,12 @@ import java.io.PipedOutputStream;
 
 class LargePipedInputStream extends PipedInputStream {
 	
-	private final int LARGE_BUF_SIZE = 1024 * 1024; // 1 megs
+	private static final int LARGE_BUF_SIZE = 1024 * 1024; // 1M
 	
 	public LargePipedInputStream(PipedOutputStream pipedoutputstream)
         throws IOException
     {
-		super(pipedoutputstream);
-		buffer = new byte[LARGE_BUF_SIZE];
+		super(pipedoutputstream, LARGE_BUF_SIZE);
     }
 
 }

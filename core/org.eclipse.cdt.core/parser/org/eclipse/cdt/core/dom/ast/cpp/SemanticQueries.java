1194093e73002cad90d7746c22a19efdd92ca6e0
Nathan Ridge <zeratul976@hotmail.com>
2012-12-06 23:17:52 -0500

    Bug 395018 - False 'member was not initalized in this constructor' warning for defaulted copy/move constructor
    Change-Id: Ib7800e46174b195fd15daef923abfff482fd3aff

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/SemanticQueries.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Nathan Ridge.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nathan Ridge - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.dom.ast.cpp;
+
+import static org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.SemanticUtil.CVTYPE;
+import static org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.SemanticUtil.TDEF;
+
+import org.eclipse.cdt.core.dom.ast.IType;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.semantics.SemanticUtil;
+
+/**
+ * @since 5.5
+ */
+public class SemanticQueries {
+
+	public static boolean isCopyOrMoveConstructor(ICPPConstructor constructor) {
+		return isCopyOrMoveConstructor(constructor, CopyOrMoveConstructorKind.COPY_OR_MOVE);
+	}
+
+	public static boolean isMoveConstructor(ICPPConstructor constructor) {
+		return isCopyOrMoveConstructor(constructor, CopyOrMoveConstructorKind.MOVE);
+	}
+
+	public static boolean isCopyConstructor(ICPPConstructor constructor) {
+		return isCopyOrMoveConstructor(constructor, CopyOrMoveConstructorKind.COPY);
+	}
+
+	private enum CopyOrMoveConstructorKind { COPY, MOVE, COPY_OR_MOVE }
+
+	private static boolean isCopyOrMoveConstructor(ICPPConstructor constructor, CopyOrMoveConstructorKind kind) {
+		// 12.8/2-3 [class.copy]:
+		// "A non-template constructor for class X is a copy [move] constructor
+		//  if its first parameter is of type X&[&], const X&[&], volatile X&[&]
+		//  or const volatile X&[&], and either there are no other parametrs or
+		//  else all other parametrs have default arguments."
+		if (constructor instanceof ICPPFunctionTemplate)
+			return false;
+		if (!isCallableWithNumberOfArguments(constructor, 1))
+			return false;
+		IType firstArgumentType = constructor.getType().getParameterTypes()[0];
+		firstArgumentType = SemanticUtil.getNestedType(firstArgumentType, TDEF);
+		if (!(firstArgumentType instanceof ICPPReferenceType))
+			return false;
+		ICPPReferenceType firstArgReferenceType = (ICPPReferenceType) firstArgumentType;
+		boolean isRvalue = firstArgReferenceType.isRValueReference();
+		if (isRvalue && kind == CopyOrMoveConstructorKind.COPY)
+			return false;
+		if (!isRvalue && kind == CopyOrMoveConstructorKind.MOVE)
+			return false;
+		firstArgumentType = firstArgReferenceType.getType();
+		firstArgumentType = SemanticUtil.getNestedType(firstArgumentType, CVTYPE);
+		return firstArgumentType.isSameType(constructor.getClassOwner());
+	}
+
+	private static boolean isCallableWithNumberOfArguments(ICPPFunction function, int numArguments) {
+		return function.getParameters().length >= numArguments
+			&& function.getRequiredArgumentCount() <= numArguments;
+	}
+}

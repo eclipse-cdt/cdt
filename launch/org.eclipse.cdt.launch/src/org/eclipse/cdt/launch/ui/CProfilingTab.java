01ab56ccb5d0ebb9aeae84bd0a62d411c1ac18a7
Jeff Johnston <jjohnstn@redhat.com>
2012-12-14 19:08:29 -0500

    Bug 391608: RFE: Extend CDT launch to allow external profiling tool providers for "profile" short-cut mode https://bugs.eclipse.org/bugs/show_bug.cgi?id=391608
    - Add a new profilingProvider extension to org.eclipse.cdt.launch
      to allow specification of an external profiling provider for
      local C/C++ launch which supplies a launch delegate and a C tab for
      showing in the Profiling Launch Configurations dialog
    - Add code to check for the extension in the CApplicationLaunchShortcut
      to use the extension to set profile launch attributes
    - Add code in LocalCDILaunchDelegate to look at the extension and
      use the delegate for profiling
    - Add a proxy tab for Profiling which uses the extension's tab for
      underlying methods
    - Enable the Local C/C++ application for profiling support
    
    Change-Id: I1bbc43656bb222320b6b095387fc4efdc227189c

---
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/ui/CProfilingTab.java
@@ -0,0 +1,184 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.launch.ui;
+
+import org.eclipse.cdt.launch.internal.ui.LaunchImages;
+import org.eclipse.cdt.launch.internal.ui.LaunchMessages;
+import org.eclipse.cdt.launch.internal.ui.LaunchUIPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @since 7.2
+ */
+public class CProfilingTab extends CLaunchConfigurationTab {
+
+	private ILaunchConfigurationTab tab;
+	private Composite profilingComp;
+	
+	public CProfilingTab() {
+		super();
+		IExtensionPoint extPoint = Platform.getExtensionRegistry()
+				.getExtensionPoint(LaunchUIPlugin.PLUGIN_ID,
+						"profilingProvider"); //$NON-NLS-1$
+		IConfigurationElement[] configs = extPoint.getConfigurationElements();
+		for (IConfigurationElement config : configs) {
+			if (config.getName().equals("provider")) { //$NON-NLS-1$
+				try {
+					Object obj = config
+							.createExecutableExtension("tab"); //$NON-NLS-1$
+					if (obj instanceof ILaunchConfigurationTab) {
+						tab = (ILaunchConfigurationTab)obj;
+						break;
+					}
+				} catch (CoreException e) {
+					// continue, perhaps another configuration will succeed
+				}
+			}
+		}
+
+	}
+	
+	@Override
+	public void createControl(Composite parent) {
+		if (tab != null)
+			tab.createControl(parent);
+		else {
+			profilingComp = new Composite(parent, SWT.NONE);
+			setControl(profilingComp);
+			GridLayout profilingLayout = new GridLayout();
+			profilingLayout.numColumns = 1;
+			profilingLayout.marginHeight = 5;
+			profilingLayout.marginWidth = 5;
+			profilingComp.setLayout(profilingLayout);
+			GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+			profilingComp.setLayoutData(gd);
+			Label label = new Label(profilingComp, SWT.WRAP);
+			label.setText(LaunchMessages.CProfilingTab_No_Profiling_Providers);
+			label.setEnabled(true);
+		}
+	}
+
+	@Override
+	public Control getControl() {
+		if (tab != null)
+			return tab.getControl();
+		return super.getControl();
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		if (tab != null)
+			tab.setDefaults(configuration);
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		if (tab != null)
+			tab.initializeFrom(configuration);
+	}
+
+	@Override
+	public void dispose() {
+		if (tab != null)
+			tab.dispose();
+		super.dispose();
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		if (tab != null)
+			tab.performApply(configuration);
+	}
+
+	@Override
+	public String getErrorMessage() {
+		if (tab != null)
+			return tab.getErrorMessage();
+		return LaunchMessages.CProfilingTab_No_Profiling_Providers;
+	}
+
+	@Override
+	public String getMessage() {
+		if (tab != null)
+			return tab.getMessage();
+		return null;
+	}
+
+	@Override
+	public boolean isValid(ILaunchConfiguration launchConfig) {
+		if (tab != null)
+			return tab.isValid(launchConfig);
+		return false;
+	}
+
+	@Override
+	public boolean canSave() {
+		if (tab != null)
+			return tab.canSave();
+		return false;
+	}
+
+	@Override
+	public void setLaunchConfigurationDialog(ILaunchConfigurationDialog dialog) {
+		if (tab != null)
+			tab.setLaunchConfigurationDialog(dialog);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void launched(ILaunch launch) {
+		if (tab != null)
+			tab.launched(launch);
+	}
+
+	@Override
+	public String getName() {
+		return LaunchMessages.CProfilingTab_Profiling;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#getImage()
+	 */
+	@Override
+	public Image getImage() {
+		return LaunchImages.get(LaunchImages.IMG_VIEW_TIME_TAB);
+	}
+
+	@Override
+	public void activated(ILaunchConfigurationWorkingCopy workingCopy) {
+		if (tab != null)
+			tab.activated(workingCopy);
+	}
+
+	@Override
+	public void deactivated(ILaunchConfigurationWorkingCopy workingCopy) {
+		if (tab != null)
+			tab.deactivated(workingCopy);
+	}
+
+}

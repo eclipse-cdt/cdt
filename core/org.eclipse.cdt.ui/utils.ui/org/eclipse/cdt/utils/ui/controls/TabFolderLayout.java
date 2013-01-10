b9dd91b39f3459fb6caf569e0c23cbf2c6dbf343
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2013-01-10 14:41:10 -0800

    Cosmetics

--- core/org.eclipse.cdt.ui/utils.ui/org/eclipse/cdt/utils/ui/controls/TabFolderLayout.java
+++ core/org.eclipse.cdt.ui/utils.ui/org/eclipse/cdt/utils/ui/controls/TabFolderLayout.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.utils.ui.controls;
 
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -22,34 +21,33 @@ import org.eclipse.swt.widgets.Layout;
  * @noextend This class is not intended to be subclassed by clients.
  */
 public class TabFolderLayout extends Layout {
-
 	@Override
-	protected Point computeSize (Composite composite, int wHint, int hHint, boolean flushCache) {
+	protected Point computeSize(Composite composite, int wHint, int hHint, boolean flushCache) {
 		if (wHint != SWT.DEFAULT && hHint != SWT.DEFAULT)
 			return new Point(wHint, hHint);
-			
-		Control [] children = composite.getChildren ();
+
+		Control[] children = composite.getChildren();
 		int count = children.length;
 		int maxWidth = 0, maxHeight = 0;
-		for (int i=0; i<count; i++) {
+		for (int i = 0; i < count; i++) {
 			Control child = children [i];
-			Point pt = child.computeSize (SWT.DEFAULT, SWT.DEFAULT, flushCache);
-			maxWidth = Math.max (maxWidth, pt.x);
-			maxHeight = Math.max (maxHeight, pt.y);
+			Point pt = child.computeSize(SWT.DEFAULT, SWT.DEFAULT, flushCache);
+			maxWidth = Math.max(maxWidth, pt.x);
+			maxHeight = Math.max(maxHeight, pt.y);
 		}
-		
+
 		if (wHint != SWT.DEFAULT)
 			maxWidth= wHint;
 		if (hHint != SWT.DEFAULT)
 			maxHeight= hHint;
-		
-		return new Point(maxWidth, maxHeight);	
-		
+
+		return new Point(maxWidth, maxHeight);
 	}
+
 	@Override
-	protected void layout (Composite composite, boolean flushCache) {
+	protected void layout(Composite composite, boolean flushCache) {
 		Rectangle rect= composite.getClientArea();
-	
+
 		Control[] children = composite.getChildren();
 		for (int i = 0; i < children.length; i++) {
 			children[i].setBounds(rect);

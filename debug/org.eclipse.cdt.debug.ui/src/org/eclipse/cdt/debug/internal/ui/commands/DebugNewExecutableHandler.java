6f37c7a91426eddcfa8afd7b7e4b41a5daa522be
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2013-01-15 16:59:30 -0500

    Bug 344890 - [remote] Cannot start a new process on a remote target with extended-remote
    Change-Id: Iae6f29a8a549cc57577911bdc84cc95f6ba983df

---
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/commands/DebugNewExecutableHandler.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Mentor Graphics and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mentor Graphics - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.debug.internal.ui.commands;
+
+import org.eclipse.cdt.debug.core.model.IDebugNewExecutableHandler;
+import org.eclipse.debug.ui.actions.DebugCommandHandler;
+
+public class DebugNewExecutableHandler extends DebugCommandHandler {
+
+	@Override
+	protected Class<?> getCommandType() {
+		return IDebugNewExecutableHandler.class;
+	}
+}

ad4a7c41c416ef22e8553b47bb554c331ee19703
Andrew Gvozdev <angvoz.dev@gmail.com>
2013-01-21 14:58:13 -0500

    bug 398681: Cosmetics - Renamed a few of tests

--- xlc/org.eclipse.cdt.errorparsers.xlc.tests/src/org/eclipse/cdt/errorparsers/xlc/tests/TestSevereError_4.java
+++ xlc/org.eclipse.cdt.errorparsers.xlc.tests/src/org/eclipse/cdt/errorparsers/xlc/tests/TestSevereError_4.java
@@ -8,14 +8,14 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.cdt.errorparsers.xlc.tests;
 
 import junit.framework.TestCase;
 
 import org.eclipse.cdt.core.IMarkerGenerator;
 
-
-public class TestOperModi extends TestCase {
+public class TestSevereError_4 extends TestCase {
 	String err_msg;
 	/**
 	 * This function tests parseLine function of the
@@ -28,14 +28,17 @@ public class TestOperModi extends TestCase {
 		XlcErrorParserTester aix = new XlcErrorParserTester();
 		aix.parseLine(err_msg);
 		assertEquals("temp9.c", aix.getFileName(0));
-		assertEquals(13, aix.getLineNumber(0));
+		assertEquals(12, aix.getLineNumber(0));
 		assertEquals(IMarkerGenerator.SEVERITY_ERROR_RESOURCE, aix.getSeverity(0));
-		assertEquals("Operand must be a modifiable lvalue.",aix.getMessage(0));
+		assertEquals("Function argument assignment between types " +
+				"\"int\" and \"char*\" is not allowed.",
+					aix.getMessage(0));
 	}
-	public TestOperModi( String name)
+	public TestSevereError_4( String name)
 	{
 		super(name);
-		err_msg = "\"temp9.c\", line 13.9: 1506-025 (S) " + 
-					"Operand must be a modifiable lvalue.";
+		err_msg = "\"temp9.c\", line 12.18: 1506-280 (S) " +
+				"Function argument assignment between types " + 
+				"\"int\" and \"char*\" is not allowed.";
 	}
 }

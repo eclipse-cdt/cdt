6eb85e5008c03367ff2d23d69d7b3bab69914d8b
Marc Khouzam <marc.khouzam@ericsson.com>
2013-01-26 08:12:42 -0500

    Bug 324101 - [launch] Have the JTag launch extend FinalLaunchSequence instead of copying it
    Change-Id: Ia65064a38cff1aa1a0ccff3dbb33c4baa92bccac

--- jtag/org.eclipse.cdt.debug.gdbjtag.core/src/org/eclipse/cdt/debug/gdbjtag/core/dsf/gdb/service/GDBJtagControl_7_4.java
+++ jtag/org.eclipse.cdt.debug.gdbjtag.core/src/org/eclipse/cdt/debug/gdbjtag/core/dsf/gdb/service/GDBJtagControl_7_4.java
@@ -1,21 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2011 Ericsson and others.
+ * Copyright (c) 2013 Ericsson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Ericsson - initial API and implementation
+ *     Marc Khouzam (Ericsson) - initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service;
 
-import java.util.Map;
-
-import org.eclipse.cdt.debug.gdbjtag.core.GDBJtagDSFFinalLaunchSequence_7_2;
-import org.eclipse.cdt.dsf.concurrent.RequestMonitorWithProgress;
-import org.eclipse.cdt.dsf.concurrent.Sequence;
-import org.eclipse.cdt.dsf.gdb.service.command.GDBControl_7_2;
+import org.eclipse.cdt.dsf.gdb.service.command.GDBControl_7_4;
 import org.eclipse.cdt.dsf.mi.service.command.CommandFactory;
 import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -25,14 +20,9 @@ import org.eclipse.debug.core.ILaunchConfiguration;
  * Jtag control service which selects the Jtag CompleteInitializationSequence.
  * Use for GDB >= 7.2
  */
-public class GDBJtagControl_7_2 extends GDBControl_7_2 {
+public class GDBJtagControl_7_4 extends GDBControl_7_4 {
 
-	public GDBJtagControl_7_2(DsfSession session, ILaunchConfiguration config, CommandFactory factory) {
+	public GDBJtagControl_7_4(DsfSession session, ILaunchConfiguration config, CommandFactory factory) {
 		super(session, config, factory);
-	}
-
-	@Override
-	protected Sequence getCompleteInitializationSequence(Map<String,Object> attributes, RequestMonitorWithProgress rm) {
-		return new GDBJtagDSFFinalLaunchSequence_7_2(getSession(), attributes, rm);
 	}
 }

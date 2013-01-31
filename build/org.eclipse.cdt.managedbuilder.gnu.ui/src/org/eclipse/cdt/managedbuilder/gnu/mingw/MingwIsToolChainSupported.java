b09a3880fd91ece29369e4616371cadace1b628f
Andrew Gvozdev <angvoz.dev@gmail.com>
2013-01-31 11:01:49 -0500

    bug 403257: MinGW toolchain detection does not consider configuration-specific settings.

--- build/org.eclipse.cdt.managedbuilder.gnu.ui/src/org/eclipse/cdt/managedbuilder/gnu/mingw/MingwIsToolChainSupported.java
+++ build/org.eclipse.cdt.managedbuilder.gnu.ui/src/org/eclipse/cdt/managedbuilder/gnu/mingw/MingwIsToolChainSupported.java
@@ -1,30 +1,35 @@
 /**********************************************************************
- * Copyright (c) 2007, 2010 QNX Software Systems and others.
+ * Copyright (c) 2007, 2013 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     QNX Software Systems - Initial API and implementation
+ *     Doug Schaefer (QNX Software Systems) - Initial API and implementation
+ *     Andrew Gvozdev                       - Ability to use different MinGW versions in different cfg
  **********************************************************************/
 
 package org.eclipse.cdt.managedbuilder.gnu.mingw;
 
+import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
+import org.eclipse.cdt.internal.core.MinGW;
 import org.eclipse.cdt.managedbuilder.core.IManagedIsToolChainSupported;
 import org.eclipse.cdt.managedbuilder.core.IToolChain;
+import org.eclipse.cdt.managedbuilder.internal.envvar.EnvironmentVariableManagerToolChain;
 import org.osgi.framework.Version;
 
 /**
- * @author Doug Schaefer
- *
  * @noextend This class is not intended to be subclassed by clients.
  */
 public class MingwIsToolChainSupported implements IManagedIsToolChainSupported {
+	private static final String ENV_PATH = "PATH"; //$NON-NLS-1$
+
 	@Override
 	public boolean isSupported(IToolChain toolChain, Version version, String instance) {
-		// Only supported if we can find the mingw bin dir to run the compiler
-		return MingwEnvironmentVariableSupplier.getBinDir() != null;
+		IEnvironmentVariable var = new EnvironmentVariableManagerToolChain(toolChain).getVariable(ENV_PATH, true);
+		String envPath = var != null ? var.getValue() : null;
+		return MinGW.isAvailable(envPath);
 	}
 
 }

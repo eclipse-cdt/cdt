771f202f55932b45b761738f5ad5fbbc22f9aaad
Andrew Eidsness <eclipse@jfront.com>
2013-01-31 11:41:44 -0500

    Bug 363406: extensible semantic highlighting - introduced extension point to allow plugins to add new semantic highlighters - see bug for more details
    Change-Id: Ic019e8a3c483fcfc42344fe4ef2139cc933c0301

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/SemanticToken.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/SemanticToken.java
@@ -15,14 +15,15 @@ import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.IBinding;
+import org.eclipse.cdt.ui.text.ISemanticToken;
 
 /**
  * Semantic token.
  * Cloned from JDT.
- * 
+ *
  * @since 4.0
  */
-public final class SemanticToken {
+public final class SemanticToken implements ISemanticToken {
 	/** AST node */
 	private IASTNode fNode;
 
@@ -38,26 +39,29 @@ public final class SemanticToken {
 	/**
 	 * @return Returns the binding, can be <code>null</code>.
 	 */
+	@Override
 	public IBinding getBinding() {
 		if (!fIsBindingResolved) {
 			fIsBindingResolved= true;
 			if (fNode instanceof IASTName)
 				fBinding= ((IASTName) fNode).resolveBinding();
 		}
-		
+
 		return fBinding;
 	}
 
 	/**
 	 * @return the AST node
 	 */
+	@Override
 	public IASTNode getNode() {
 		return fNode;
 	}
-	
+
 	/**
 	 * @return the AST root
 	 */
+	@Override
 	public IASTTranslationUnit getRoot() {
 		if (!fIsRootResolved) {
 			fIsRootResolved= true;

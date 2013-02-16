7ae029c6e36cbf6b0226503409acd17cac3fbbac
Andrew Eidsness <andrewe@jfront.com>
2013-02-16 11:40:15 -0500

    Bug 400020: Allow tagging of IBindings
    This new extension point allows contributors to put their own
    information into the PDOM and to later retrieve it for their own
    purposes.
    
    There are many details in the bug.  The idea is that contributors
    provide an implementation of IBindingTagger, which is given a chance to
    examine IBindings when they are created.  The ITagWriter interface
    allows the contributor to create a new tag which can then have data
    written to it.
    
    The ITagService interface (accessible from CCorePlugin.getTagService()
    provides a way for the contributor to later get an instance of
    ITagReader to retrieve tags from bindings.
    
    ITags are copied to the PDOM when the associated binding is persisteed.
    
    Contributors use a unique id (based on their plugin id), so that
    multiple contributors are able to independently tag a given binding.
    
    In-memory tags are not cached.  I've done some timing tests using my
    sample implementation and found no measurable difference.  The full log
    lines look like:
    
    	!MESSAGE Indexed 'simple-01' (2 sources, 184 headers) in <see below>
    sec: 21,550 declarations; 35,394 references; 0 unresolved inclusions; 1
    syntax errors; 0 unresolved names (0.00%)
    
    I did 5 tests using the current master (no tagging-related code), the
    times were:
    	18.86 sec
    	 9.17 sec
    	 5.91 sec
    	 4.79 sec
    	 4.83 sec
    
    And then I ran the same sequence of tests using the code in this
    commit:
    	18.73 sec
    	 9.39 sec
    	 6.50 sec
    	 4.78 sec
    	 5.27 sec
    
    If performance does become a problem, then caching could be introduced
    with a new implementation of ITaggableService.  The two problems are
    finding a key other than the identity of the IBinding (since IBindings
    are re-created often) and properly evicting stale entries when the
    binding is no longer valid.
    
    The process of copying tags from an in-memory IBinding to a PDOMBinding,
    is a synchronization.  This means that tags that are no longer
    applicable, will be removed from the persistent store.
    
    While developing this I found that PDOMBindings are not deleted from the
    Database (only the names that reference them are deleted), so there is
    no provision for deleting all tags at once.
    
    New database locks are not needed.  By the time the persistent tags are
    accessed, higher levels of code have already taken a read or write lock
    as appropriate.
    
    There are new unit tests covering the changes to the PDOM.
               
    Change-Id: I8da1bf5eeba7e1fc2ca7ec308ed8e212629986a4

---
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/pdom/tests/PDOMTagIndexTests.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2013 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.cdt.internal.pdom.tests;
+
+import java.util.Arrays;
+
+import junit.framework.Test;
+
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.dom.IPDOMManager;
+import org.eclipse.cdt.core.dom.ast.tag.ITag;
+import org.eclipse.cdt.core.index.IIndex;
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.core.testplugin.CProjectHelper;
+import org.eclipse.cdt.core.testplugin.util.BaseTestCase;
+import org.eclipse.cdt.internal.core.dom.ast.tag.Tag;
+import org.eclipse.cdt.internal.core.index.CIndex;
+import org.eclipse.cdt.internal.core.index.IIndexFragment;
+import org.eclipse.cdt.internal.core.pdom.PDOM;
+import org.eclipse.cdt.internal.core.pdom.tag.PDOMTag;
+import org.eclipse.cdt.internal.core.pdom.tag.PDOMTagIndex;
+
+// copy/pasted from BTreeTests
+public class PDOMTagIndexTests extends BaseTestCase
+{
+    private ICProject cProject;
+	private PDOM pdom;
+
+	public static Test suite()
+	{
+		return suite( PDOMTagIndexTests.class );
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+	    super.setUp();
+
+	    int attempts = 10;
+	    IIndexFragment[] fragments = null;
+	    do
+	    {
+            // If this test is run too quickly after the previous, then the old project is still being
+            // deleted and this one gets a PDOMProxy.  So, if we don't get what we want, just wait and
+            // try again.
+            if( fragments != null )
+            {
+                //CProjectHelper.delete( cProject );
+                Thread.sleep( 1000 );
+            }
+
+	        cProject = CProjectHelper.createCCProject( "PDOMTagIndexTests." + Double.toString( Math.random() ).substring( 2 ), "bin", IPDOMManager.ID_FAST_INDEXER );
+    		IIndex index = CCorePlugin.getIndexManager().getIndex(  cProject );
+    		assertTrue( index instanceof CIndex );
+
+    		fragments = ( (CIndex)index ).getFragments();
+    		assertTrue( fragments.length > 0 );
+	    }
+    	while( ! ( fragments[0] instanceof PDOM ) && --attempts >= 0 );
+
+		assertTrue( fragments[0] instanceof PDOM );
+		pdom = (PDOM)fragments[0];
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+	    // Don't delete the project because there are timing issues with recreating the project on the
+	    // next test case.  A new handle is created for every test case, and they are all cleaned up
+	    // when the suite is done.
+
+        super.tearDown();
+	}
+
+	// return the nearest valid record that is less than the specified base
+	private static long lastRecordBase = 1000;
+	private static long computeValidRecord()
+	{
+	    lastRecordBase += 1000;
+	    return ( lastRecordBase & ~7L ) | 2;
+	}
+
+    // A quick sanity test to confirm basic functionality.
+    public void testSimple() throws Exception
+    {
+        String tagger = "tagger_a";
+        long rec = computeValidRecord();
+
+        assertNotNull( PDOMTagIndex.createTag( pdom, rec, tagger, 1 ) );
+        assertNotNull( PDOMTagIndex.getTag( pdom, rec, tagger ) );
+    }
+
+    public void testMultipleTaggers() throws Exception
+    {
+        String tagger_a = "tagger_a";
+        String tagger_b = "tagger_b";
+        long rec1 = computeValidRecord();
+        long rec2 = computeValidRecord();
+
+        assertNotNull( PDOMTagIndex.createTag( pdom, rec1, tagger_a, 1 ) );
+        assertNotNull( PDOMTagIndex.createTag( pdom, rec1, tagger_b, 1 ) );
+        assertNotNull( PDOMTagIndex.createTag( pdom, rec2, tagger_a, 1 ) );
+
+        assertNotNull( PDOMTagIndex.getTag( pdom, rec2, tagger_a ) );
+        assertNull(    PDOMTagIndex.getTag( pdom, rec2, tagger_b ) );
+
+        Iterable<ITag> tags1 = PDOMTagIndex.getTags( pdom, rec1 );
+        int tag_count = 0;
+        for( ITag tag : tags1 )
+        {
+            ++tag_count;
+            assertTrue( tag.getTaggerId().equals( tagger_a ) || tag.getTaggerId().equals( tagger_b ) );
+            assertEquals( 1, tag.getDataLen() );
+        }
+        assertEquals( 2, tag_count );
+    }
+
+    public void testReplaceTags() throws Exception
+    {
+        String tagger_a = "tagger_a";
+        String tagger_b = "tagger_b";
+        long rec = computeValidRecord();
+
+        ITag taga = PDOMTagIndex.createTag( pdom, rec, tagger_a, 2 );
+        assertNotNull( taga );
+        assertTrue( taga instanceof PDOMTag );
+        PDOMTag taga_pdom = (PDOMTag)taga;
+        ITag tagb = PDOMTagIndex.createTag( pdom, rec, tagger_a, 2 );
+        assertNotNull( tagb );
+
+        // replacement should delete tags for taggers that are no longer present and shorter tags
+        // should be modified in place
+        PDOMTagIndex.setTags( pdom, rec, Arrays.<ITag>asList( new Tag( tagger_a, 1 ) ) );
+        assertNull( PDOMTagIndex.getTag( pdom, rec, tagger_b ) );
+        ITag shorter_ = PDOMTagIndex.getTag( pdom, rec, tagger_a );
+        assertNotNull( shorter_ );
+        assertTrue( shorter_ instanceof PDOMTag );
+        PDOMTag shorter_pdom = (PDOMTag)shorter_;
+        assertEquals( taga_pdom.getRecord(), shorter_pdom.getRecord() );
+
+        // longer tags should create a new record
+        PDOMTagIndex.setTags( pdom, rec, Arrays.<ITag>asList( new Tag( tagger_a, 4 ) ) );
+        ITag longer_ = PDOMTagIndex.getTag( pdom, rec, tagger_a );
+        assertNotNull( longer_ );
+        assertTrue( longer_ instanceof PDOMTag );
+        PDOMTag longer_pdom = (PDOMTag)longer_;
+        assertTrue( taga_pdom.getRecord() != longer_pdom.getRecord() );
+
+        // TODO figure out how to tell if the original tag was free'd
+    }
+}

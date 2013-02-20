2279927623d6d393db81fba7ceca12f047935bf8
Andrew Eidsness <andrewe@jfront.com>
2013-02-19 20:40:15 -0500

    Bug 400020: Allow tagging of IBindings
    This new extension point allows contributors to put their own
    information into the PDOM and to later retrieve it for their own
    purposes.
    
    There are many details in the bug.  The idea is that contributors
    provide an implementation of IBindingTagger, which is given a chance to
    examine IBindings when they are created.  The ITagWriter interface
    allows the contributor to create a new tag which can then have data
    written to it.
    
    The ITagService interface (accessible from CCorePlugin.getTagService()
    provides a way for the contributor to later get an instance of
    ITagReader to retrieve tags from bindings.
    
    ITags are copied to the PDOM when the associated binding is persisteed.
    
    Contributors use a unique id (based on their plugin id), so that
    multiple contributors are able to independently tag a given binding.
    
    In-memory tags are not cached.  I've done some timing tests using my
    sample implementation and found no measurable difference.  The full log
    lines look like:
    
    	!MESSAGE Indexed 'simple-01' (2 sources, 184 headers) in <see below>
    sec: 21,550 declarations; 35,394 references; 0 unresolved inclusions; 1
    syntax errors; 0 unresolved names (0.00%)
    
    I did 5 tests using the current master (no tagging-related code), the
    times were:
    	18.86 sec
    	 9.17 sec
    	 5.91 sec
    	 4.79 sec
    	 4.83 sec
    
    And then I ran the same sequence of tests using the code in this
    commit:
    	18.73 sec
    	 9.39 sec
    	 6.50 sec
    	 4.78 sec
    	 5.27 sec
    
    If performance does become a problem, then caching could be introduced
    with a new implementation of ITaggableService.  The two problems are
    finding a key other than the identity of the IBinding (since IBindings
    are re-created often) and properly evicting stale entries when the
    binding is no longer valid.
    
    The process of copying tags from an in-memory IBinding to a PDOMBinding,
    is a synchronization.  This means that tags that are no longer
    applicable, will be removed from the persistent store.
    
    While developing this I found that PDOMBindings are not deleted from the
    Database (only the names that reference them are deleted), so there is
    no provision for deleting all tags at once.
    
    New database locks are not needed.  By the time the persistent tags are
    accessed, higher levels of code have already taken a read or write lock
    as appropriate.
    
    There are new unit tests covering the changes to the PDOM.
    
    Change-Id: I8da1bf5eeba7e1fc2ca7ec308ed8e212629986a4
    Reviewed-on: https://git.eclipse.org/r/10407
    IP-Clean: Doug Schaefer <dschaefer@qnx.com>
    Tested-by: Doug Schaefer <dschaefer@qnx.com>
    Reviewed-by: Doug Schaefer <dschaefer@qnx.com>

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/tag/ITagService.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2013 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.cdt.core.dom.ast.tag;
+
+import org.eclipse.cdt.core.dom.ast.IBinding;
+
+/**
+ * Provides ITagReaders for specific bindings.  The kind of the reader will vary based on
+ * the kind of the input binding.
+ *
+ * @see ITag
+ * @see ITagReader
+ * @since 5.5
+ */
+public interface ITagService
+{
+	/**
+	 * Finds or creates a tag reader for the specified binding or null if a reader cannot
+	 * be associated with this binding.
+	 *
+	 * @param binding could be null
+	 */
+	public ITagReader findTagReader( IBinding binding );
+}

885d140319b130d65713861d7a0d8f11b3498ed5
Nathan Ridge <zeratul976@hotmail.com>
2013-02-21 05:03:49 -0500

    Bug 401400 - Another error involving variadic non-type template parameters
    Change-Id: Ic5c64de6d32a203d13274053da61923d9d8e2ad3

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPTemplateParameterMap.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPTemplateParameterMap.java
@@ -45,6 +45,14 @@ public interface ICPPTemplateParameterMap {
 	public ICPPTemplateArgument[] getPackExpansion(ICPPTemplateParameter param);
 
 	/**
+	 * If 'param' is not a parameter pack, returns the value for 'param' in the map.
+	 * If 'param' is a parmameter pack, returns the value at position 'packOffset'
+	 * in the pack of values for 'param' in the map.
+	 * Returns <code>null</code> is 'param' is not mapped.
+	 */
+	public ICPPTemplateArgument getArgument(ICPPTemplateParameter param, int packOffset);
+	
+	/**
 	 * Returns the array of template parameter positions, for which a mapping exists.
 	 */
 	Integer[] getAllParameterPositions();

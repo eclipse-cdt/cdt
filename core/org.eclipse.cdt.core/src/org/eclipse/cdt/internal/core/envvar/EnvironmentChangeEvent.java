993e26b0f91abe75d51e6e0a446cf34262050926
Andrew Gvozdev <angvoz.dev@gmail.com>
2013-03-15 14:46:38 -0400

    bug 403406: Notification mechanism for changes to environment variables.

--- core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/envvar/EnvironmentChangeEvent.java
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/internal/core/envvar/EnvironmentChangeEvent.java
@@ -1,37 +1,47 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 Intel Corporation and others.
+ * Copyright (c) 2005, 2013 Intel Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Intel Corporation - Initial API and implementation
+ *    Intel Corporation - Initial API and implementation
+ *    Andrew Gvozdev    - Implementation of notification mechanism including changes to this API
  *******************************************************************************/
 package org.eclipse.cdt.internal.core.envvar;
 
+import java.util.Collection;
+
 import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
+import org.eclipse.cdt.utils.envvar.IEnvironmentChangeEvent;
 
-public class EnvironmentChangeEvent {
-	private static final IEnvironmentVariable[] EMPTY_VAR_ARRAY = new IEnvironmentVariable[0];
-	
-	private IEnvironmentVariable[] fAddedVars, fRemovedVars, fChangedVars;
-	
-	EnvironmentChangeEvent(IEnvironmentVariable[] addedVars, IEnvironmentVariable[] removedVars, IEnvironmentVariable[] changedVars){
-		fAddedVars = addedVars != null ? (IEnvironmentVariable[])addedVars.clone() : null;
-		fRemovedVars = removedVars != null ? (IEnvironmentVariable[])removedVars.clone() : null;
-		fChangedVars = changedVars != null ? (IEnvironmentVariable[])changedVars.clone() : null;
-	}
-	public IEnvironmentVariable[] getAddedVariables(){
-		return fAddedVars != null ? (IEnvironmentVariable[])fAddedVars.clone() : EMPTY_VAR_ARRAY;
-	}
+/**
+ * Concrete implementation of event describing changes to environment variables defined by user
+ * on CDT Environment page in Preferences.
+ */
+public class EnvironmentChangeEvent implements IEnvironmentChangeEvent {
+	private IEnvironmentVariable[] oldVariables;
+	private IEnvironmentVariable[] newVariables;
 
-	public IEnvironmentVariable[] getRemovedVariables(){
-		return fRemovedVars != null ? (IEnvironmentVariable[])fRemovedVars.clone() : EMPTY_VAR_ARRAY;
-	}
-
-	public IEnvironmentVariable[] getChangedVariables(){
-		return fChangedVars != null ? (IEnvironmentVariable[])fChangedVars.clone() : EMPTY_VAR_ARRAY;
+	/**
+	 * Constructor.
+	 * 
+	 * @param oldVars - set of environment variables before the change.
+	 * @param newVars - set of environment variables after the change.
+	 */
+	public EnvironmentChangeEvent(Collection<IEnvironmentVariable> oldVars, Collection<IEnvironmentVariable> newVars) {
+		oldVariables = oldVars.toArray(new IEnvironmentVariable[oldVars.size()]);
+		newVariables = newVars.toArray(new IEnvironmentVariable[newVars.size()]);
 	}
 
+	@Override
+	public IEnvironmentVariable[] getOldVariables() {
+		return oldVariables;
+	}
+
+	@Override
+	public IEnvironmentVariable[] getNewVariables() {
+		return newVariables;
+	}
 }

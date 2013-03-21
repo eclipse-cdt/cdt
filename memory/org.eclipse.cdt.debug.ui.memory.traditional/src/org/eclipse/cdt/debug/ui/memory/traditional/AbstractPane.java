cc1464212d3b7da5d46e62ad0ce9fbe540c1a705
congwang <congwang@ca.ibm.com>
2013-03-21 12:55:04 -0400

    Bug 341632 - Trad mem rendering keyboard access
    Change-Id: Iae09285e88f76d1155fd28a6aa25b818ae0164dd

--- memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/AbstractPane.java
+++ memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/AbstractPane.java
@@ -229,6 +229,14 @@ public abstract class AbstractPane extends Canvas
             	
                 editCell(fCaretAddress, fSubCellCaretPosition, ke.character);
             }
+            else if (ke.keyCode == SWT.TAB && (ke.stateMask & SWT.SHIFT) != 0) {
+                // move backward cursor to the first position in the following pane
+                handleSHIFTTabKey();
+            }
+            else if (ke.keyCode == SWT.TAB) {
+                // move forward cursor to the first position in the following pane
+                handleTabKey();
+            }
 
             if((ke.stateMask & SWT.SHIFT) != 0)
             {
@@ -434,6 +442,33 @@ public abstract class AbstractPane extends Canvas
     	}
     }
     
+    /**
+     * Use TAB to shift cursor to the same address in next pane.
+     */
+    protected void handleTabKey()
+    {
+        switchTo(fRendering.nextPane(this));
+    }
+
+    /**
+     * Use SHIFT + TAB to shift cursor back to the same address in previous
+     * pane.
+     */
+    protected void handleSHIFTTabKey()
+    {
+        switchTo(fRendering.prevPane(this));
+    }
+
+    private void switchTo(AbstractPane pane)
+    {
+        pane.setCaretAddress(this.fCaretAddress);
+        pane.fOldSubCellCaretPosition = 0;
+        pane.fSubCellCaretPosition = 0;
+        pane.updateCaret();
+        pane.ensureCaretWithinViewport();
+        pane.setFocus();
+    }
+
     protected boolean isPaneVisible()
     {
     	return fPaneVisible;

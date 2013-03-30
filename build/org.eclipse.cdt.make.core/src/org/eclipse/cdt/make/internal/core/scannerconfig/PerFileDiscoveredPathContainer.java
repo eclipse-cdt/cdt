e14728c2c4dbc9b1767d7b43cce2bd8755b3e631
Andrew Gvozdev <angvoz.dev@gmail.com>
2013-03-30 07:17:37 -0400

    bug 401961: Regression: pathEntryContainers entries are no longer honored when importing a project into cdt-8.1.2, due to LSP's being enabled

--- build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/PerFileDiscoveredPathContainer.java
+++ build/org.eclipse.cdt.make.core/src/org/eclipse/cdt/make/internal/core/scannerconfig/PerFileDiscoveredPathContainer.java
@@ -22,88 +22,86 @@ import org.eclipse.cdt.make.core.MakeCorePlugin;
 import org.eclipse.cdt.make.core.scannerconfig.IDiscoveredPathManager.IDiscoveredPathInfo;
 import org.eclipse.cdt.make.core.scannerconfig.IDiscoveredPathManager.IPerFileDiscoveredPathInfo;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
-public class PerFileDiscoveredPathContainer extends DiscoveredPathContainer
-                                            implements IPathEntryContainerExtension {
+public class PerFileDiscoveredPathContainer extends DiscoveredPathContainer implements IPathEntryContainerExtension {
+	public PerFileDiscoveredPathContainer(IProject project) {
+		super(project);
+	}
 
-    public PerFileDiscoveredPathContainer(IProject project) {
-        super(project);
-    }
-
-    /* (non-Javadoc)
-     * @see org.eclipse.cdt.core.model.IPathEntryContainerExtension#getPathEntries(org.eclipse.core.runtime.IPath, int)
-     */
-    @Override
+	@Override
 	public IPathEntry[] getPathEntries(IPath path, int mask) {
 		ArrayList<IPathEntry> entries = new ArrayList<IPathEntry>();
-        try {
-            IDiscoveredPathInfo info = MakeCorePlugin.getDefault().getDiscoveryManager().getDiscoveredInfo(fProject);
-            if (info instanceof IPerFileDiscoveredPathInfo) {
-                IPerFileDiscoveredPathInfo filePathInfo = (IPerFileDiscoveredPathInfo) info;
+		try {
+			IDiscoveredPathInfo info = MakeCorePlugin.getDefault().getDiscoveryManager().getDiscoveredInfo(fProject);
+			if (info instanceof IPerFileDiscoveredPathInfo) {
+				IResource rc = fProject.getWorkspace().getRoot().findMember(path);
+				if (rc.getType() == IResource.FOLDER || rc.getType() == IResource.PROJECT) {
+					return new IPathEntry[0];
+				}
 
-        		if ((mask & IPathEntry.CDT_INCLUDE) != 0) {
-    				IPath[] includes = filePathInfo.getIncludePaths(path);
-    				for (int i = 0; i < includes.length; i++) {
-                        // add as a system include path
-    					entries.add(CoreModel.newIncludeEntry(path, Path.EMPTY, includes[i], true));
-    				}
-                    includes = filePathInfo.getQuoteIncludePaths(path);
-                    for (int i = 0; i < includes.length; i++) {
-                        // add as a local include path
-                        entries.add(CoreModel.newIncludeEntry(path, Path.EMPTY, includes[i], false));
-                    }
-        		}
-        		if ((mask & IPathEntry.CDT_MACRO) != 0) {
-    				Map<String, String> syms = filePathInfo.getSymbols(path);
-    				Set<Entry<String, String>> entrySet = syms.entrySet();
-    				for (Entry<String, String> entry : entrySet) {
-    					entries.add(CoreModel.newMacroEntry(path, entry.getKey(), entry.getValue()));
-    				}
-        		}
-        		// compare the resource with include and macros files
-            	IPath fullResPath = fProject.getWorkspace().getRoot().getFile(path).getLocation();
-            	if (fullResPath == null) {
-            		fullResPath = path;
-            	}
-        		if ((mask & IPathEntry.CDT_INCLUDE_FILE) != 0) {
-                    IPath[] includeFiles = filePathInfo.getIncludeFiles(path);
-                    for (int i = 0; i < includeFiles.length; i++) {
-                    	if (!includeFiles[i].equals(fullResPath)) {
-                            entries.add(CoreModel.newIncludeFileEntry(path, includeFiles[i]));
-                    	}
-                    }
-                }
-                if ((mask & IPathEntry.CDT_MACRO_FILE) != 0) {
-                    IPath[] imacrosFiles = filePathInfo.getMacroFiles(path);
-                    for (int i = 0; i < imacrosFiles.length; i++) {
-                    	if (!imacrosFiles[i].equals(fullResPath)) {
-                    		entries.add(CoreModel.newMacroFileEntry(path, imacrosFiles[i]));
-                    	}
-                    }
-                }
-            }
-        }
-        catch (CoreException e) {
-            //
-        }
+				IPerFileDiscoveredPathInfo filePathInfo = (IPerFileDiscoveredPathInfo) info;
+
+				if ((mask & IPathEntry.CDT_INCLUDE) != 0) {
+					IPath[] includes = filePathInfo.getIncludePaths(path);
+					for (int i = 0; i < includes.length; i++) {
+						// add as a system include path
+						entries.add(CoreModel.newIncludeEntry(path, Path.EMPTY, includes[i], true));
+					}
+					includes = filePathInfo.getQuoteIncludePaths(path);
+					for (int i = 0; i < includes.length; i++) {
+						// add as a local include path
+						entries.add(CoreModel.newIncludeEntry(path, Path.EMPTY, includes[i], false));
+					}
+				}
+				if ((mask & IPathEntry.CDT_MACRO) != 0) {
+					Map<String, String> syms = filePathInfo.getSymbols(path);
+					Set<Entry<String, String>> entrySet = syms.entrySet();
+					for (Entry<String, String> entry : entrySet) {
+						entries.add(CoreModel.newMacroEntry(path, entry.getKey(), entry.getValue()));
+					}
+				}
+				// compare the resource with include and macros files
+				IPath fullResPath = fProject.getWorkspace().getRoot().getFile(path).getLocation();
+				if (fullResPath == null) {
+					fullResPath = path;
+				}
+				if ((mask & IPathEntry.CDT_INCLUDE_FILE) != 0) {
+					IPath[] includeFiles = filePathInfo.getIncludeFiles(path);
+					for (int i = 0; i < includeFiles.length; i++) {
+						if (!includeFiles[i].equals(fullResPath)) {
+							entries.add(CoreModel.newIncludeFileEntry(path, includeFiles[i]));
+						}
+					}
+				}
+				if ((mask & IPathEntry.CDT_MACRO_FILE) != 0) {
+					IPath[] imacrosFiles = filePathInfo.getMacroFiles(path);
+					for (int i = 0; i < imacrosFiles.length; i++) {
+						if (!imacrosFiles[i].equals(fullResPath)) {
+							entries.add(CoreModel.newMacroFileEntry(path, imacrosFiles[i]));
+						}
+					}
+				}
+			}
+		}
+		catch (CoreException e) {
+			//
+		}
 		return entries.toArray(new IPathEntry[entries.size()]);
-    }
+	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.cdt.core.model.IPathEntryContainerExtension#isEmpty(org.eclipse.core.runtime.IPath)
-	 */
 	@Override
 	public boolean isEmpty(IPath path) {
-        IDiscoveredPathInfo info;
+		IDiscoveredPathInfo info;
 		try {
 			info = MakeCorePlugin.getDefault().getDiscoveryManager().getDiscoveredInfo(fProject);
-	        if (info instanceof IPerFileDiscoveredPathInfo) {
-	            IPerFileDiscoveredPathInfo filePathInfo = (IPerFileDiscoveredPathInfo) info;
-	            return filePathInfo.isEmpty(path);
-	        }
+			if (info instanceof IPerFileDiscoveredPathInfo) {
+				IPerFileDiscoveredPathInfo filePathInfo = (IPerFileDiscoveredPathInfo) info;
+				return filePathInfo.isEmpty(path);
+			}
 		} catch (CoreException e) {
 		}
 		return false;

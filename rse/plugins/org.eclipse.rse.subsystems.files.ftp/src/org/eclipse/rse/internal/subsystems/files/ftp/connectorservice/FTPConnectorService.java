03f94f01c9181a95562628a88cb1d26c583d46c0
Martin Oberhuber <martin.oberhuber@windriver.com>
2013-05-15 23:19:40 +0200

    Bug 269442 - [ftp] FTP connections should use passive mode by default
    Change-Id: Ica81e2fe2afbbb84c4dac6218717c2c2e92c28dd
    Signed-off-by: Martin Oberhuber <martin.oberhuber@windriver.com>

--- rse/plugins/org.eclipse.rse.subsystems.files.ftp/src/org/eclipse/rse/internal/subsystems/files/ftp/connectorservice/FTPConnectorService.java
+++ rse/plugins/org.eclipse.rse.subsystems.files.ftp/src/org/eclipse/rse/internal/subsystems/files/ftp/connectorservice/FTPConnectorService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@
  * Martin Oberhuber (Wind River) - [204669] Fix ftp path concatenation on systems using backslash separator
  * Martin Oberhuber (Wind River) - [203500] Support encodings for FTP paths
  * Martin Oberhuber (Wind River) - [235463][ftp][dstore] Incorrect case sensitivity reported on windows-remote
+ * Martin Oberhuber (Wind River) - [269442][ftp] Set passive mode by default
  *******************************************************************************/
 
 package org.eclipse.rse.internal.subsystems.files.ftp.connectorservice;
@@ -78,7 +79,7 @@ public class FTPConnectorService extends StandardConnectorService
 
 			//Active - passive mode
 			propertySet = createPropertySet("FTP Settings"); //$NON-NLS-1$
-			propertySet.addProperty("passive","false",PropertyType.getEnumPropertyType(new String[]{"true","false"})); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			propertySet.addProperty("passive","true",PropertyType.getEnumPropertyType(new String[]{"true","false"})); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 			// FTP List parser
 			String[] keys = FTPClientConfigFactory.getParserFactory().getKeySet();

1e52117031ea0fef823bb58aba41dd40b9b851b3
Marc Khouzam <marc.khouzam@ericsson.com>
2013-05-28 16:24:46 -0400

    Bug 409186 - Memory tests had failures.
    First failure was because we now know the endianess but the test was not
    updated.  The second failure was because different GDB versions gave
    different error messages for memory write failures.
    
    
    Change-Id: Idbac275a5de3a0af77d030474fea60808ca9a54c

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_7_6/MIMemoryTest_7_6.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_7_6/MIMemoryTest_7_6.java
@@ -10,9 +10,17 @@
  *******************************************************************************/
 package org.eclipse.cdt.tests.dsf.gdb.tests.tests_7_6;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.cdt.dsf.debug.service.IRunControl.StepType;
+import org.eclipse.cdt.tests.dsf.gdb.framework.AsyncCompletionWaitor;
 import org.eclipse.cdt.tests.dsf.gdb.framework.BackgroundRunner;
+import org.eclipse.cdt.tests.dsf.gdb.framework.SyncUtil;
 import org.eclipse.cdt.tests.dsf.gdb.tests.ITestConstants;
 import org.eclipse.cdt.tests.dsf.gdb.tests.tests_7_5.MIMemoryTest_7_5;
+import org.eclipse.cdt.utils.Addr64;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(BackgroundRunner.class)
@@ -21,4 +29,35 @@ public class MIMemoryTest_7_6 extends MIMemoryTest_7_5 {
 	protected void setGdbVersion() {
 		setGdbProgramNamesLaunchAttributes(ITestConstants.SUFFIX_GDB_7_6);
 	}
+	
+
+	// Error message has changed back with GDB 7.6
+	@Override
+	@Test
+	public void writeWithInvalidAddress() throws Throwable {
+		// Run to the point where the variable is initialized
+		SyncUtil.addBreakpoint("MemoryTestApp.cc:zeroBlocks", true);
+		SyncUtil.resumeUntilStopped();
+		SyncUtil.step(StepType.STEP_RETURN);
+
+		// Setup call parameters
+		long offset = 0;
+		int word_size = 1;
+		int count = 1;
+		byte[] buffer = new byte[count];
+		fBaseAddress = new Addr64("0");
+
+		// Perform the test
+		fWait.waitReset();
+		writeMemory(fMemoryDmc, fBaseAddress, offset, word_size, count, buffer);
+		fWait.waitUntilDone(AsyncCompletionWaitor.WAIT_FOREVER);
+		assertFalse(fWait.getMessage(), fWait.isOK());
+		String expected = "Cannot access memory at address";	// Error msg returned by gdb
+		assertTrue("Wrong error message: expected '" + expected + "', received '" + fWait.getMessage() + "'",
+				fWait.getMessage().contains(expected));
+
+		// Ensure no MemoryChangedEvent event was received
+		assertTrue("MemoryChangedEvent problem: expected " + 0 + ", received " + getEventCount(), getEventCount() == 0);
+	}
+
 }

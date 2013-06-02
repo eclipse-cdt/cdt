5bb17fc715d52e191c6f38037838f50456747b2c
Marc-Andre Laperle <malaperle@gmail.com>
2013-06-01 20:07:10 -0400

    Bug 405909 - LLVM plugin refreshes resources when bp is inserted in java
    Change-Id: Iad9082d59d188714bed6f6e110c32a631e65450f
    Signed-off-by: Marc-Andre Laperle <malaperle@gmail.com>

--- llvm/org.eclipse.cdt.managedbuilder.llvm.ui/src/org/eclipse/cdt/managedbuilder/llvm/util/LlvmResourceListener.java
+++ llvm/org.eclipse.cdt.managedbuilder.llvm.ui/src/org/eclipse/cdt/managedbuilder/llvm/util/LlvmResourceListener.java
@@ -6,17 +6,15 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *      Nokia Siemens Networks - initial implementation
- *      Petri Tuononen - Initial implementation
+ *     Nokia Siemens Networks - initial implementation
+ *     Petri Tuononen - Initial implementation
+ *     Marc-Andre Laperle (Individual)
  *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.llvm.util;
 
 import org.eclipse.cdt.managedbuilder.llvm.ui.preferences.LlvmPreferenceStore;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.runtime.CoreException;
 
 /**
  * Implements Resource listener.
@@ -31,17 +29,26 @@ public class LlvmResourceListener implements IResourceChangeListener {
 	public void resourceChanged(IResourceChangeEvent event) {
 
 		if (event.getType() == IResourceChangeEvent.POST_BUILD) { //refresh every project after build
-			//get all projects
-			IProject[] projects = LlvmToolOptionPathUtil.getProjectsInWorkspace();
 
-			//refresh the projects
-			for (IProject proj : projects) {
-				try {
-					proj.refreshLocal(IResource.DEPTH_INFINITE, null);
-				} catch (CoreException e) {
-					e.printStackTrace();
-				}
-			}
+			/*
+			 * FIXME: M-A.L: (Bug 405909) I commented out the refresh code because it was a
+			 * major performance issue even without LLVM projects in the
+			 * workspace. Unfortunately, I could not track down the reason as to
+			 * why there was a refresh in the first the place by looking at the
+			 * history (git and svn) and by manual testing
+			 */
+
+//			//get all projects
+//			IProject[] projects = LlvmToolOptionPathUtil.getProjectsInWorkspace();
+//
+//			//refresh the projects
+//			for (IProject proj : projects) {
+//				try {
+//					proj.refreshLocal(IResource.DEPTH_INFINITE, null);
+//				} catch (CoreException e) {
+//					e.printStackTrace();
+//				}
+//			}
 		} else if (event.getType() == IResourceChangeEvent.PRE_BUILD) {
 			String os = System.getProperty("os.name").toLowerCase(); //$NON-NLS-1$
 			if (os.indexOf("win") >= 0) { //$NON-NLS-1$

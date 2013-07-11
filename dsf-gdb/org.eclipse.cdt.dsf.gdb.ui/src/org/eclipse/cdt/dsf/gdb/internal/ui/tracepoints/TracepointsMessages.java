30c37f7e3f512fdf508b7e8f3dc92ca2e54953c3
Dmitry Kozlov <ddk@codesourcery.com>
2013-07-11 21:00:34 +0400

    Bug 390827 - Trace View enhancement
    	Enhance Trace Control View UI.
    
    Change-Id: I3ae49dece2056c8e8fd9eb21118926b039a5cdf0
    Signed-off-by: Dmitry Kozlov <ddk@codesourcery.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/tracepoints/TracepointsMessages.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/tracepoints/TracepointsMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Wind River Systems and others.
+ * Copyright (c) 2010, 2013 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     Wind River Systems - initial API and implementation
+ *     Dmitry Kozlov (Mentor Graphics) - trace control view enhancements (Bug 390827)
  *******************************************************************************/
 package org.eclipse.cdt.dsf.gdb.internal.ui.tracepoints;
 
@@ -20,11 +21,23 @@ public final class TracepointsMessages extends NLS {
 	private TracepointsMessages() {
 		// Do not instantiate
 	}
-
+	public static String TraceControlView_buffer_label;
+	public static String TraceControlView_buffer_circular_button_label;
+	public static String TraceControlView_buffer_circular_on_tooltip;
+	public static String TraceControlView_buffer_circular_off_tooltip;
+	public static String TraceControlView_buffer_frames_collected;
+	public static String TraceControlView_trace_status_offline;
+	public static String TraceControlView_tracing_stopped_user_request;
+	public static String TraceControlView_tracing_stopped_passcount;
+	public static String TraceControlView_tracing_stopped_tracepoint_number;
+	public static String TraceControlView_tracing_stopped_buffer_full;
+	public static String TraceControlView_tracing_stopped_disconnection;
+	public static String TraceControlView_tracing_stopped_error;
+	public static String TraceControlView_tracing_stopped_unknown;
 	public static String TraceControlView_action_Refresh_label;
-	public static String TraceControlView_trace_view_content_updated_label;
+	public static String TraceControlView_auto_refresh_action_label;
 	public static String TraceControlView_action_trace_variable_details;
-	public static String TraceControlView_trace_variable_invalid_value;
+	public static String TraceControlView_action_Disconnected_tracing_label;
 	public static String TraceControlView_trace_variable_tracing_unavailable;
 	public static String TraceControlView_trace_variable_details_dialog_title;
 	public static String TraceControlView_trace_variable_details_column_name;
@@ -38,6 +51,44 @@ public final class TracepointsMessages extends NLS {
 	public static String TraceControlView_create_variable_empty_name_error;
 	public static String TraceControlView_action_exit_visualization_mode;
 	public static String TraceControlView_refresh_variable_error;
+	public static String TraceControlView_trace_status_not_supported;
+	public static String TraceControlView_trace_status_inactive;
+	public static String TraceControlView_trace_status_no_debug_session;
+	public static String TraceControlView_trace_status_not_started;
+	public static String TraceControlView_trace_status_in_progress;
+	public static String TraceControlView_trace_status_visualization;
+	public static String TraceControlView_trace_status_stopped;
+	public static String TraceControlView_frame_label;
+	public static String TraceControlView_frame_looking;
+	public static String TraceControlView_frame_dragging;
+	public static String TraceControlView_frame_not_looking;
+	public static String TraceControlView_trace_notes_edit_tooltip;
+	public static String TraceControlView_trace_notes_save_tooltip;
+	public static String TraceControlView_trace_notes_not_set;
+	public static String TraceControlView_trace_notes_label;
+	public static String TraceControlView_trace_status_secondary_stopped;
+	public static String TraceControlView_trace_status_secondary_running;
+	public static String TraceControlView_trace_status_secondary_user;
+	public static String TraceControlView_trace_status_secondary_refresh_time;
+	public static String TraceControlView_trace_status_secondary_offline;
+	public static String TraceControlView_action_start;
+	public static String TraceControlView_action_stop;
+	public static String TraceControlView_action_restart;
+	public static String TraceControlView_action_finish_visualization;
+	public static String TraceControlView_today;
+	public static String TraceControlView_yesterday;
+	public static String TraceControlView_date_days;
+	public static String TraceControlView_date_hours; 
+	public static String TraceControlView_date_minutes; 
+	public static String TraceControlView_date_seconds;
+	public static String TraceControlView_date_zero;
+	public static String TraceControlView_date_short_days;
+	public static String TraceControlView_date_short_hours; 
+	public static String TraceControlView_date_short_minutes; 
+	public static String TraceControlView_date_short_seconds;
+	public static String TraceControlView_date_short_zero;
+	// Not used
+	public static String TraceControlView_tracing_stopped_at;
 	
 	static {
 		NLS.initializeMessages(TracepointsMessages.class.getName(), TracepointsMessages.class);
---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/MIGDBSetTraceUser.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Mentor Graphics and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Dmitry Kozlov (Mentor Graphics) - Initial API and implementation
+*******************************************************************************/
+
+package org.eclipse.cdt.dsf.mi.service.command.commands;
+
+import org.eclipse.cdt.dsf.gdb.service.IGDBTraceControl.ITraceTargetDMContext;
+
+/**
+ * -gdb-set trace-user "user"
+ * 
+ * Sets trace user
+ * @since 4.3
+ */
+public class MIGDBSetTraceUser extends MIGDBSet {
+	public MIGDBSetTraceUser(ITraceTargetDMContext ctx, String userName) {
+		super(ctx, new String[] {"trace-user", userName}); //$NON-NLS-1$
+	}
+}

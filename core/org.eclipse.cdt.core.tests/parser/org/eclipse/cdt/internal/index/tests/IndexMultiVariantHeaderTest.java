919e6c258652136d1fad774fef0fea63314947d3
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2013-08-30 16:37:44 -0700

    Cosmetics.

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/index/tests/IndexMultiVariantHeaderTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/internal/index/tests/IndexMultiVariantHeaderTest.java
@@ -183,5 +183,4 @@ public class IndexMultiVariantHeaderTest extends IndexBindingResolutionTestBase 
 		assertEquals("{}", inc.getSignificantMacros().toString());
 		assertNotNull(inc.getImportedIndexFile());
 	}
-
 }

a0a2059fdd2296fe9fd9c269e129d24fe9b9bf48
Andrew Eidsness <eclipse@jfront.com>
2013-09-10 21:33:19 -0400

    Bug 416471: Support for extra ILanguageSettingsProviders in new projects
    This change introduces three new ProcessRunners that can be used in the
    New Project wizard's template.xml files.  These will be used by two new
    Qt project wizards that I will introduce in a second patch.
    
    The three new rules are:
    
    1) "AddMakeTarget" which creates new Make Targets (in the Make Targets
    view) for the new projects.
    
    2) "SetEnvironmentVariable" which sets an environment variable in all of
    the new project's build configurations.
    
    3) "ExtraLanguageSettingsProvider" which modifies the new project's
    build configurations to include a new ILanguageSettingsProvider.
    
    The first two are straightforward, the third is a bit different.
    Instead of creating a new Toolchain or Configuration it modifies the
    Configurations that were created for the new project.  In this case the
    only modification is to add the extra ILanguageSettingsProvider, but it
    might be useful to extend this to other customizations as well.
    
    Change-Id: I30710400e9b0dffcbe6e8965ce7ce2078c1c99ca
    Signed-off-by: Andrew Eidsness <eclipse@jfront.com>
    Reviewed-on: https://git.eclipse.org/r/16817
    Reviewed-by: Andrew Gvozdev <angvoz.dev@gmail.com>
    IP-Clean: Andrew Gvozdev <angvoz.dev@gmail.com>
    Tested-by: Andrew Gvozdev <angvoz.dev@gmail.com>

---
+++ core/org.eclipse.cdt.core/templateengine/org/eclipse/cdt/core/templateengine/process/processes/SetEnvironmentVariable.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2013 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     QNX Software Systems - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.templateengine.process.processes;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.envvar.IContributedEnvironment;
+import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
+import org.eclipse.cdt.core.model.CoreModel;
+import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
+import org.eclipse.cdt.core.settings.model.ICProjectDescription;
+import org.eclipse.cdt.core.settings.model.ICProjectDescriptionManager;
+import org.eclipse.cdt.core.templateengine.TemplateCore;
+import org.eclipse.cdt.core.templateengine.process.ProcessArgument;
+import org.eclipse.cdt.core.templateengine.process.ProcessFailureException;
+import org.eclipse.cdt.core.templateengine.process.ProcessRunner;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * A template process for setting an environment variable in all of the new project's
+ * build configurations.  E.g.,
+ * <pre>
+ *  &lt;process type="org.eclipse.cdt.core.SetEnvironmentVariable">
+ *      &lt;simple name="projectName" value="$(projectName)"/>
+ *      &lt;complex-array name="variables">
+ *          &lt;element>
+ *              &lt;simple name="name" value="QMAKE"/>
+ *              &lt;simple name="value" value="$(qmake)"/>
+ *          &lt;/element>
+ *      &lt;/complex-array>
+ *  &lt;/process>
+ * </pre>
+ * This will create an environment variable called "QMAKE" and will set the value to
+ * be the value entered in a field (called qmake) in the New Project wizard.
+ *
+ * @since 5.6
+ */
+public class SetEnvironmentVariable extends ProcessRunner {
+
+	private static final String PROJECTNAME_VARNAME = "projectName"; //$NON-NLS-1$
+	private static final String VARIABLES_VARNAME = "variables"; //$NON-NLS-1$
+	private static final String VARIABLES_NAME_VARNAME = "name"; //$NON-NLS-1$
+	private static final String VARIABLES_VALUE_VARNAME = "value"; //$NON-NLS-1$
+
+	@Override
+	public void process(TemplateCore template, ProcessArgument[] args, String processId, IProgressMonitor monitor) throws ProcessFailureException {
+
+		IProject project = null;
+		Map<String, String> envVars = new LinkedHashMap<String, String>();
+
+		for (ProcessArgument arg : args) {
+			String argName = arg.getName();
+			if (PROJECTNAME_VARNAME.equals(argName))
+				project = ResourcesPlugin.getWorkspace().getRoot().getProject(arg.getSimpleValue());
+			else if(VARIABLES_VARNAME.equals(argName)) {
+
+				for(ProcessArgument[] envVarArgs : arg.getComplexArrayValue()) {
+					String name = null;
+					String value = null;
+
+					for(ProcessArgument varArg : envVarArgs) {
+						String varArgName = varArg.getName();
+						if (VARIABLES_NAME_VARNAME.equals(varArgName))
+							name = varArg.getSimpleValue();
+						else if(VARIABLES_VALUE_VARNAME.equals(varArgName))
+							value = varArg.getSimpleValue();
+					}
+
+					if (name == null)
+						throw missingArgException(processId, VARIABLES_NAME_VARNAME);
+					if (value == null)
+						throw missingArgException(processId, VARIABLES_VALUE_VARNAME);
+
+					envVars.put(name, value);
+				}
+			}
+		}
+
+		if (project == null)
+			throw missingArgException(processId, PROJECTNAME_VARNAME);
+
+		ICProjectDescriptionManager mngr = CoreModel.getDefault().getProjectDescriptionManager();
+		ICProjectDescription des = mngr.getProjectDescription(project, true);
+		ICConfigurationDescription[] configDescs = des.getConfigurations();
+		IContributedEnvironment ice = CCorePlugin.getDefault().getBuildEnvironmentManager().getContributedEnvironment();
+
+		for(Map.Entry<String, String> envVar : envVars.entrySet()) {
+			String name = envVar.getKey();
+			String value = envVar.getValue();
+
+			for (ICConfigurationDescription configDesc : configDescs)
+				ice.addVariable(name, value, IEnvironmentVariable.ENVVAR_REPLACE, null, configDesc);
+		}
+
+		try {
+			mngr.setProjectDescription(project, des);
+		} catch(CoreException e) {
+			CCorePlugin.log(e);
+		}
+	}
+}

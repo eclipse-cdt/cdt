f21f8f8c588e160ee1d8f4dc9bc63b9ab054d9bd
Andrew Eidsness <eclipse@jfront.com>
2013-09-10 22:14:30 -0400

    Bug 418406: Qt4 and Qt5 specific HelloWorld project wizards
    This replaces the QtQuick2 project wizards with a Qt5 wizard that
    includes the old content as well as:
    
    1) Make targets to build and clean the project
    2) Sample interaction between C++ and QML (a trivial clock)
    3) A Qt header path provider so that paths from the Qt installation are
    properly resolved in the project.
    
    Item 3 is particularly important for using the 'New Class' wizard with
    QObject as the base class.  Unless the Qt paths are pre-populated the
    QObject base class will not be found and the New Class wizard's Finish
    button will be disabled.
    
    The Qt headers are resolved by running `qmake -query QT_INSTALL_HEADERS`
    and then creating IncludePath entries for all sub-folders.  This list of
    include paths is stored in-memory which means they are computed once
    (for each version of qmake) in each workbench session.
    
    Change-Id: Ic82fdb147e6a69060f93e2e9aed2e919139a0ae9
    Signed-off-by: Andrew Eidsness <eclipse@jfront.com>

--- qt/org.eclipse.cdt.qt.core/src/org/eclipse/cdt/qt/core/QtPlugin.java
+++ qt/org.eclipse.cdt.qt.core/src/org/eclipse/cdt/qt/core/QtPlugin.java
@@ -1,9 +1,12 @@
 package org.eclipse.cdt.qt.core;
 
-import org.osgi.framework.BundleActivator;
+import org.eclipse.cdt.core.model.CModelException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
-public class QtPlugin implements BundleActivator {
+public class QtPlugin extends Plugin {
 
     public static final String ID = "org.eclipse.cdt.qt.core";
     public static final String SIGNAL_SLOT_TAGGER_ID = ID + ".signalslot.tagger";
@@ -11,10 +14,19 @@ public class QtPlugin implements BundleActivator {
     public static final int SignalSlot_Mask_signal = 1;
     public static final int SignalSlot_Mask_slot = 2;
 
+	private static QtPlugin plugin;
     private static BundleContext context;
 
 	static BundleContext getContext() {
 		return context;
+	}
+
+	public QtPlugin() {
+		plugin = this;
+	}
+
+	public static QtPlugin getDefault() {
+		return plugin;
 	}
 
 	/*
@@ -35,4 +47,29 @@ public class QtPlugin implements BundleActivator {
 		QtPlugin.context = null;
 	}
 
+	public static void log(String e) {
+		log(IStatus.INFO, e, null);
+	}
+
+	public static void log(Throwable e) {
+		String msg= e.getMessage();
+		if (msg == null) {
+			log("Error", e); //$NON-NLS-1$
+		} else {
+			log("Error: " + msg, e); //$NON-NLS-1$
+		}
+	}
+
+	public static void log(String message, Throwable e) {
+		Throwable nestedException;
+		if (e instanceof CModelException
+				&& (nestedException = ((CModelException)e).getException()) != null) {
+			e = nestedException;
+		}
+		log(IStatus.ERROR, message, e);
+	}
+
+	public static void log(int code, String msg, Throwable e) {
+		getDefault().getLog().log(new Status(code, ID, msg, e));
+	}
 }

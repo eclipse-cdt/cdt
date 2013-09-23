7569b2522fa31f636714f4c49aff2e829bae2544
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2013-09-23 11:00:24 -0700

    Cosmetics.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/gnu/cpp/GPPLanguage.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/gnu/cpp/GPPLanguage.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 QNX Software Systems and others.
+ * Copyright (c) 2005, 2013 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    QNX - Initial API and implementation
- *    Markus Schorn (Wind River Systems)
- *    IBM Corporation
- *    Anton Leherbauer (Wind River Systems)
- *    Mike Kucera - IBM
+ *     QNX - Initial API and implementation
+ *     Markus Schorn (Wind River Systems)
+ *     IBM Corporation
+ *     Anton Leherbauer (Wind River Systems)
+ *     Mike Kucera - IBM
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast.gnu.cpp;
 
@@ -40,11 +40,10 @@ public class GPPLanguage extends AbstractCLikeLanguage {
 	protected static final GPPParserExtensionConfiguration CPP_GNU_PARSER_EXTENSION= GPPParserExtensionConfiguration.getInstance();
 	public static final String ID = CCorePlugin.PLUGIN_ID + ".g++"; //$NON-NLS-1$
 
-	/**
-	 * @since 5.6
-	 */
-	public static final int GNU_LATEST_VERSION_MAJOR = 4,
-						    GNU_LATEST_VERSION_MINOR = 7;
+	/** @since 5.6 */
+	public static final int GNU_LATEST_VERSION_MAJOR = 4;
+	/** @since 5.6 */
+	public static final int GNU_LATEST_VERSION_MINOR = 7;
 
 	private static final GPPLanguage DEFAULT_INSTANCE = new GPPLanguage();
 	

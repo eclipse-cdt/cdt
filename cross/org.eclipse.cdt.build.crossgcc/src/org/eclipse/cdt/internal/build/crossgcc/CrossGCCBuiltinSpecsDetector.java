333a490e6899f53a47d999f626db8b4f67a7de86
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2013-10-09 00:21:36 -0400

    Bug 392404 - Change Cross GCC toolchain to use language setting provider
    Add cross toolchain prefix to the command macro.
    
    Change-Id: I3188d21050b84fdc6d6a4b8d3b6aa540710f1790
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

---
+++ cross/org.eclipse.cdt.build.crossgcc/src/org/eclipse/cdt/internal/build/crossgcc/CrossGCCBuiltinSpecsDetector.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marc-Andre Laperle (Ericsson) - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.cdt.internal.build.crossgcc;
+
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.IToolChain;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
+import org.eclipse.cdt.managedbuilder.language.settings.providers.GCCBuiltinSpecsDetector;
+
+public class CrossGCCBuiltinSpecsDetector extends GCCBuiltinSpecsDetector {
+
+	@Override
+	protected String getCompilerCommand(String languageId) {
+		// Include the cross command prefix (tool option) in the ${COMMAND} macro
+		// For example: "arch-os-" + "gcc"
+		String prefix = "";
+		IToolChain toolchain = null;
+		if (currentCfgDescription != null) {
+			IConfiguration cfg = ManagedBuildManager.getConfigurationForDescription(currentCfgDescription);
+			toolchain = cfg != null ? cfg.getToolChain() : null;
+			if (toolchain != null) {
+				IOption option = toolchain.getOptionBySuperClassId("cdt.managedbuild.option.gnu.cross.prefix"); //$NON-NLS-1$
+				prefix = (String)option.getValue();
+			}
+		}
+		return prefix + super.getCompilerCommand(languageId);
+	}
+
+}

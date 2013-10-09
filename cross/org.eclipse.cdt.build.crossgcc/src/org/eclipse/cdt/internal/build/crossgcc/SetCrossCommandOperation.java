c644a857ba56c32171d4f5a738e19aea858f7faf
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2013-10-09 00:21:36 -0400

    Bug 392404 - Change Cross GCC toolchain to use language setting provider
    Add cross toolchain prefix to the command macro.
    
    Change-Id: I3188d21050b84fdc6d6a4b8d3b6aa540710f1790
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/17189

--- cross/org.eclipse.cdt.build.crossgcc/src/org/eclipse/cdt/internal/build/crossgcc/SetCrossCommandOperation.java
+++ cross/org.eclipse.cdt.build.crossgcc/src/org/eclipse/cdt/internal/build/crossgcc/SetCrossCommandOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Wind River Systems, Inc. and others.
+ * Copyright (c) 2009, 2013 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,22 +11,8 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.build.crossgcc;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
 
-import org.eclipse.cdt.build.core.scannerconfig.CfgInfoContext;
-import org.eclipse.cdt.build.core.scannerconfig.ICfgScannerConfigBuilderInfo2Set;
-import org.eclipse.cdt.build.core.scannerconfig.ScannerConfigBuilder;
-import org.eclipse.cdt.build.internal.core.scannerconfig.CfgDiscoveredPathManager;
-import org.eclipse.cdt.build.internal.core.scannerconfig2.CfgScannerConfigProfileManager;
 import org.eclipse.cdt.core.templateengine.SharedDefaults;
-import org.eclipse.cdt.make.core.scannerconfig.IScannerConfigBuilderInfo2;
-import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollector;
-import org.eclipse.cdt.make.core.scannerconfig.IScannerInfoCollectorCleaner;
-import org.eclipse.cdt.make.core.scannerconfig.InfoContext;
-import org.eclipse.cdt.make.internal.core.scannerconfig.DiscoveredPathInfo;
-import org.eclipse.cdt.make.internal.core.scannerconfig.DiscoveredScannerInfoStore;
-import org.eclipse.cdt.make.internal.core.scannerconfig2.SCProfileInstance;
-import org.eclipse.cdt.make.internal.core.scannerconfig2.ScannerConfigProfileManager;
 import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
 import org.eclipse.cdt.managedbuilder.core.IOption;
@@ -35,9 +21,7 @@ import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPageManager;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 
 /**
@@ -74,60 +58,9 @@ public class SetCrossCommandOperation implements IRunnableWithProgress {
 			ManagedBuildManager.setOption(config, toolchain, option, prefix);
 			option = toolchain.getOptionBySuperClassId("cdt.managedbuild.option.gnu.cross.path"); //$NON-NLS-1$
 			ManagedBuildManager.setOption(config, toolchain, option, path);
-			
-			ICfgScannerConfigBuilderInfo2Set cbi = CfgScannerConfigProfileManager.getCfgScannerConfigBuildInfo(config);
-			Map<CfgInfoContext, IScannerConfigBuilderInfo2> map = cbi.getInfoMap();
-			for (CfgInfoContext cfgInfoContext : map.keySet()) {
-				IScannerConfigBuilderInfo2 bi = map.get(cfgInfoContext);
-				String providerId = "specsFile"; //$NON-NLS-1$
-				String runCommand = bi.getProviderRunCommand(providerId);
-				bi.setProviderRunCommand(providerId, prefix + runCommand);
-				try {
-					bi.save();
-				} catch (CoreException e) {
-					Activator.log(e);
-				}
-				
-				// Clear the path info that was captured at project creation time
-				// TODO we need an API to do this to avoid the discouraged access warnings.
-				
-				DiscoveredPathInfo pathInfo = new DiscoveredPathInfo(project);
-				InfoContext infoContext = cfgInfoContext.toInfoContext();
-				
-				// 1. Remove scanner info from .metadata/.plugins/org.eclipse.cdt.make.core/Project.sc
-				DiscoveredScannerInfoStore dsiStore = DiscoveredScannerInfoStore.getInstance();
-				try {
-					dsiStore.saveDiscoveredScannerInfoToState(project, infoContext, pathInfo);
-				} catch (CoreException e) {
-					e.printStackTrace();
-				}
-				
-				// 2. Remove scanner info from CfgDiscoveredPathManager cache and from the Tool
-				CfgDiscoveredPathManager cdpManager = CfgDiscoveredPathManager.getInstance();
-				cdpManager.removeDiscoveredInfo(project, cfgInfoContext);
-
-				// 3. Remove scanner info from SI collector
-				IScannerConfigBuilderInfo2 buildInfo2 = map.get(cfgInfoContext);
-				if (buildInfo2!=null) {
-					ScannerConfigProfileManager scpManager = ScannerConfigProfileManager.getInstance();
-					String selectedProfileId = buildInfo2.getSelectedProfileId();
-					SCProfileInstance profileInstance = scpManager.getSCProfileInstance(project, infoContext, selectedProfileId);
-					
-					IScannerInfoCollector collector = profileInstance.getScannerInfoCollector();
-					if (collector instanceof IScannerInfoCollectorCleaner) {
-						((IScannerInfoCollectorCleaner) collector).deleteAll(project);
-					}
-					buildInfo2 = null;
-				}
-			}
 		}
 		
 		ManagedBuildManager.saveBuildInfo(project, true);
-		
-		for (IConfiguration config : configs) {
-			ScannerConfigBuilder.build(config, ScannerConfigBuilder.PERFORM_CORE_UPDATE, new NullProgressMonitor());	
-		}
-
 	}
 
 }

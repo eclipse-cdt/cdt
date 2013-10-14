25ea069927e7d217c2671cf09d9497d9391fe977
Anton Leherbauer <anton.leherbauer@windriver.com>
2013-10-14 11:03:36 +0200

    Bug 418797 - Spawner closes file descriptor it didn't open when process cannot be started

--- core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/Spawner.java
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/Spawner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 QNX Software Systems and others.
+ * Copyright (c) 2000, 2013 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,7 @@ public class Spawner extends Process {
 
 	int pid = 0;
 	int status;
-	final int[] fChannels = new int[3];
+	final int[] fChannels = { -1, -1, -1 };
 	boolean isDone;
 	OutputStream out;
 	InputStream in;
@@ -400,9 +400,6 @@ public class Spawner extends Process {
 		if (pid == -1) {
 			throw new IOException("Exec error"); //$NON-NLS-1$
 		}
-		fChannels[0] = -1;
-		fChannels[1] = -1;
-		fChannels[2] = -1;
 	}
 
 	/**

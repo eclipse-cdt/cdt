3b2b514224ad1732514bf28ed776350e918e2ce7
Zhuohui Qiu <zqiu@blackberry.com>
2013-11-20 16:40:00 -0500

    Bug 410112 - Core file can be used with Debug as... but fails to launch
    Change-Id: Iad51a39f54f37aeb93d40480c3b8fb9ad1a61dcb
    Signed-off-by: Zhuohui Qiu <zqiu@blackberry.com>

---
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/launch/CApplicationLaunchShortcutPMD.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BlackBerry Ltd and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Raymond Qiu (BlackBerry Ltd) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.debug.internal.ui.launch;
+
+import org.eclipse.cdt.core.model.CoreModel;
+import org.eclipse.cdt.core.model.IBinary;
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
+import org.eclipse.cdt.debug.ui.CDebugUIPlugin;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+
+/**
+ * This is the launch shortcut for postmortem debugging
+ * 
+ */
+public class CApplicationLaunchShortcutPMD extends CApplicationLaunchShortcut {
+
+	@Override
+	protected void searchAndLaunch(final Object[] elements, String mode) {
+		if (elements != null && elements.length > 0) {
+			if (elements[0] instanceof IResource) {
+				// for postmortem debugging, we always want to convert the selection
+				// to the IBinary
+				ICElement cElement = CoreModel.getDefault().create(
+						(IResource) elements[0]);
+				if (cElement != null) {
+					super.searchAndLaunch(new Object[] { cElement }, mode);
+					return;
+				}
+			}
+		}
+		super.searchAndLaunch(elements, mode);
+	}
+
+	@Override
+	protected ILaunchConfigurationType getCLaunchConfigType() {
+		return getLaunchManager().getLaunchConfigurationType(
+				ICDTLaunchConfigurationConstants.ID_LAUNCH_C_POST_MORTEM);
+	}
+
+	@Override
+	protected ILaunchConfiguration findLaunchConfiguration(IBinary bin,
+			String mode) {
+		ILaunchConfiguration lc = super.findLaunchConfiguration(bin, mode);
+		IResource resource = (IResource) bin.getAdapter(IResource.class);
+		if (resource instanceof IFile) {
+			try {
+				ILaunchConfigurationWorkingCopy wc = lc.getWorkingCopy();
+				// set the core file path, for now, it is for core file only. Trace file is not supported.
+				wc.setAttribute(
+						ICDTLaunchConfigurationConstants.ATTR_COREFILE_PATH,
+						resource.getLocation().toOSString());
+				wc.doSave();
+			} catch (CoreException e) {
+				CDebugUIPlugin.log(e);
+			}
+		}
+		return lc;
+	}
+}

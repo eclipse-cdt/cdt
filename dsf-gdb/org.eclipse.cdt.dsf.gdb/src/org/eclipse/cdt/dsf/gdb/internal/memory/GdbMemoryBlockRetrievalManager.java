914f12179fd50c52cb61f44e7747a9723048fa15
Alvaro Sanchez-Leon <alvsan09@gmail.com>
2013-12-05 15:20:44 -0500

    Bug 250323 - Each memory context needs a different memory retrieval
    Change-Id: I7874d84e5831aeb36be508a5b6b83c1623fcc5ab
    Signed-off-by: Alvaro Sanchez-Leon <alvsan09@gmail.com>

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/internal/memory/GdbMemoryBlockRetrievalManager.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Alvaro Sanchez-Leon (Ericsson AB) - Each memory context needs a different MemoryRetrieval (Bug 250323)
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.gdb.internal.memory;
+
+import org.eclipse.cdt.dsf.debug.internal.provisional.model.MemoryBlockRetrievalManager;
+import org.eclipse.cdt.dsf.debug.model.DsfMemoryBlockRetrieval;
+import org.eclipse.cdt.dsf.gdb.internal.GdbPlugin;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.IMemoryBlockRetrieval;
+
+public class GdbMemoryBlockRetrievalManager extends MemoryBlockRetrievalManager {
+
+	public GdbMemoryBlockRetrievalManager(String modelId, ILaunchConfiguration config, DsfSession session) {
+		super(modelId, config, session);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.dsf.debug.internal.provisional.model.MemoryBlockRetrievalManager#createMemoryBlockRetrieval(java.lang.String, org.eclipse.debug.core.ILaunchConfiguration, org.eclipse.cdt.dsf.service.DsfSession)
+	 */
+	@Override
+	protected IMemoryBlockRetrieval createMemoryBlockRetrieval(String model, ILaunchConfiguration config, DsfSession session) {
+		DsfMemoryBlockRetrieval memRetrieval = null;
+	
+		try {
+			memRetrieval = new GdbMemoryBlockRetrieval(model, config, session);
+		} catch (DebugException e) {
+			GdbPlugin.getDefault().getLog().log(e.getStatus());
+		}
+		
+		return memRetrieval;
+	}
+	
+}

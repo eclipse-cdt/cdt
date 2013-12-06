a19fec95898389b896c11051cedaadeed1ed9374
David Kaspar <dkaspar@blackberry.com>
2013-12-06 14:02:21 +0100

    Bug 422797 - Improved JavaDoc for IQMakeProjectInfoListener.qmakeChanged()
    Change-Id: I36ad640cefae08d5ebb2b7e6a566e7e385d9036a
    Signed-off-by: David Kaspar <dkaspar@blackberry.com>

--- qt/org.eclipse.cdt.qt.core/src/org/eclipse/cdt/qt/core/index/IQMakeProjectInfoListener.java
+++ qt/org.eclipse.cdt.qt.core/src/org/eclipse/cdt/qt/core/index/IQMakeProjectInfoListener.java
@@ -15,6 +15,10 @@ public interface IQMakeProjectInfoListener {
 	/**
 	 * Notifies that a QMake information provided by IQMakeProjectInfo might have changed.
 	 * A new QMake information can be read via IQMakeProjectInfo.getQMakeInfo() method.
+	 *
+	 * Note that this method might be called even after the listener is removed from
+	 * IQMakeProjectInfo. To prevent this, your implementation should maintain a flag
+	 * representing whether qmakeInfoChanged() notifications should be processed or not.
 	 */
 	void qmakeInfoChanged();
 

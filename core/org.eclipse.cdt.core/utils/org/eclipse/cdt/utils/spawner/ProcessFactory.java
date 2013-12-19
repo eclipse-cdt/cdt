80d5db77489dd8d0e6f1e5c6ab6fb9ad788c353a
Marc Khouzam <marc.khouzam@ericsson.com>
2013-12-19 04:40:41 +0200

    Bug 424397 - Don't print full stack trace in case spawner native is not available
    Change-Id: Id950622f6ca39639addeedc33e8d1c3fadd611e0
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/20028

--- core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/ProcessFactory.java
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/spawner/ProcessFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 QNX Software Systems and others.
+ * Copyright (c) 2000, 2013 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,7 +42,7 @@ public class ProcessFactory {
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (UnsatisfiedLinkError e) {
-			e.printStackTrace();
+			CCorePlugin.log(e.getMessage());
 		}
 	}
 

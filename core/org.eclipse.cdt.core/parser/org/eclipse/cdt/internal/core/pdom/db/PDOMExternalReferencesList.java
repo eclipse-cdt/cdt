6de9f5288e678d920bb16386ecd81f2835734ac0
Andrew Eidsness <eclipse@jfront.com>
2014-01-22 11:18:13 -0500

    Bug 426238: Update head of external references list when needed
    When a PDOMName is deleted and that name is the head of an external
    references list, the list's head must be changed.
    
    The Qt plugin is the only user of the external reference list.  One case
    is the link from a SIGNAL or SLOT expansion to the C++ method binding
    for the corresponding function.  In this case, the problem will appear
    when all of the following are true:
    
    1) The file containing the SIGNAL/SLOT expansion is changed and the
       index updated
    2) The corresponding function is declared in a different file
    3) The the function is the first entry in the external references list
    
    When #2 is false, the binding (and the entire list) is removed as part
    of updating the file containing the name.
    
    When #3 is false, the list is updated with existing code using the
    PDOMName's {next|prev}InBinding pointers.
    
    Change-Id: I1e27c7c2356ca1fb68f57d69c40728289288ed66
    Signed-off-by: Andrew Eidsness <eclipse@jfront.com>
    Reviewed-on: https://git.eclipse.org/r/20972
    Tested-by: Hudson CI
    Reviewed-by: Doug Schaefer <dschaefer@qnx.com>
    IP-Clean: Doug Schaefer <dschaefer@qnx.com>

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/db/PDOMExternalReferencesList.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/db/PDOMExternalReferencesList.java
@@ -18,24 +18,41 @@ import org.eclipse.core.runtime.CoreException;
 
 /**
  * A utility class for storing a list of external references.  An external reference is
- * a PDOMName that references a PDOMBinding in a different linkage.
- * <p>
- * External references are stored in a singly linked list with one node for each linkage.
- * Each node stores a list of PDOMNames.  The PDOMName.BindingList is used to store the
- * list.
- * <p>
- * Each node has three fields:
- * {
- *     INT_SIZE linkageId;
- *     PTR_SIZE nextNode;
- *     PTR_SIZE nameListHead;
- * }
- * <p>
- * An iterator is provided to examine all names in the list.  An iterator is needed (instead
- * of a simple list) so that we have a chance to move to the next linkage's node when we get
- * to the end of a name list.
+ * a PDOMName that references a PDOMBinding in a different linkage.  This list can be
+ * examined using {@link #getIterator()}.
  */
 public class PDOMExternalReferencesList {
+
+	/*
+	 * This data structure is a list of lists.  The inner lists are names that should that
+	 * should be created in the same linkage.  The outer list collects the heads of the
+	 * inner lists.
+	 *
+	 * This example shows this storage structure for three names in two linkages.
+	 *
+	 *             NameA2
+	 *               ^
+	 *               |
+	 *             NameA1       NameB
+	 *               ^            ^
+	 *               |            |
+	 * record --> LinkageA --> LinkageB
+	 *
+	 * NameA1 and NameA2 should both be created in LinkageA, NameB should be created in LinkageB.
+	 *
+	 * The interface to this class flattens this storage structure so it appears as a simple
+	 * list that can be iterated over.
+	 *
+	 * Continuing with the same example, the iterator behaves as though the list were:
+	 *
+	 *     { NameA1, NameA2, NameB }
+	 *
+	 * This class mostly doesn't care about the inner lists.  They are implemented using the
+	 * #getNextInBinding attribute of PDOMName.
+	 *
+	 * This class implements the outer list as a singly linked list of "nodes".  Each node stores
+	 * the linkageId, the record of the first PDOMName in the list, and the record of the next node.
+	 */
 
 	private final PDOM pdom;
 	private final long record;
@@ -71,7 +88,7 @@ public class PDOMExternalReferencesList {
 		// needed.
 		long lastAddr = record;
 		long nodeRec = 0;
-		while((nodeRec = pdom.getDB().getRecPtr(lastAddr)) != 0) {
+		while ((nodeRec = pdom.getDB().getRecPtr(lastAddr)) != 0) {
 			// Each node looks like { int linkageID; recPtr nextNode; recPtr headOfList; }
 			int linkageID = pdom.getDB().getInt(nodeRec);
 			if (linkageID == nameLinkageID)
@@ -104,6 +121,35 @@ public class PDOMExternalReferencesList {
 
 		// Finally, make the new name the first element in the list.
 		pdom.getDB().putRecPtr(nodeRec + Database.INT_SIZE + Database.PTR_SIZE, name.getRecord());
+	}
+
+	/**
+	 * This function must be used during PDOMName deletion when the list head is being deleted.
+	 */
+	public void setFirstReference(PDOMLinkage linkage, PDOMName name) throws CoreException {
+		// Find the head of this linkage's list.
+		PDOMName head = null;
+		Iterator iterator = new Iterator(record);
+		while (head == null) {
+			if (iterator.next == null)
+				break;
+
+			if (!linkage.equals(iterator.next.getLinkage())) {
+				iterator.next = iterator.advance();
+			} else {
+				head = iterator.next;
+			}
+		}
+
+		// If there isn't already a node for this name's linkage then the new name can be inserted
+		// in the normal way.
+		if (head == null) {
+			add(name);
+		} else {
+			// Otherwise update the node's head field to point to the given name.
+			long nextNameRec = iterator.node + Database.INT_SIZE + Database.PTR_SIZE;
+			pdom.getDB().putRecPtr(nextNameRec, name == null ? 0 : name.getRecord());
+		}
 	}
 
 	private class Iterator implements IPDOMIterator<PDOMName> {

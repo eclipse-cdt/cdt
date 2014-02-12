12a2ba4b3fe350e40cc8f1ff7e51f66dda9cd770
Jeff Johnston <jjohnstn@redhat.com>
2014-02-12 17:57:17 -0500

    Add GNU debug-info support to DwarfReader/Dwarf
    - Add support to find separate debug info file using
      the .gnu_debuglink section
    - Add support to also find the GNU alt debug info file
      created by the DWZ utility
    - Add additional DW_FORMs to support alt data
    - Add support to DwarfReader to find the special note section
      containing the build-id and then see if it can find the
      debug-info with that build-id
    
    Change-Id: I0e43ba8af12396cdab4e085ad0c20fdec8c1d83e
    Reviewed-on: https://git.eclipse.org/r/24034
    Tested-by: Hudson CI
    Reviewed-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Tested-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

---
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/ICompileOptionsFinder.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core;
+
+/**
+ * A class that can find compiler options for a given file name.
+ * 
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @since 5.7
+ */
+public interface ICompileOptionsFinder {
+
+	/**
+	 * Get compiler options for a given file name.
+	 * 
+	 * @param fileName - absolute source file name
+	 * @return a String containing the compiler options used or null.
+	 * 
+	 */
+	public String getCompileOptions(String fileName);
+}

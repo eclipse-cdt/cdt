8402df62e30ccb30d57ff33d38a773f0db9df5a2
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2014-02-14 13:58:53 -0500

    Bug 424947 - Resource Configurations > Reset to Default cannot reset language settings
    Change-Id: Ie0e8aeec49ec2868841970858e3cd2735ec1a2af
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/viewsupport/CustomBuildSettingsDecorator.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/viewsupport/CustomBuildSettingsDecorator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2013 Andrew Gvozdev and others.
+ * Copyright (c) 2013, 2014 Andrew Gvozdev and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.ui.viewsupport;
 
-import java.util.List;
-
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
@@ -20,18 +17,13 @@ import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
 
-import org.eclipse.cdt.core.language.settings.providers.ILanguageSettingsProvider;
-import org.eclipse.cdt.core.language.settings.providers.ILanguageSettingsProvidersKeeper;
-import org.eclipse.cdt.core.language.settings.providers.LanguageSettingsManager;
-import org.eclipse.cdt.core.language.settings.providers.ScannerDiscoveryLegacySupport;
 import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
 import org.eclipse.cdt.core.settings.model.ICProjectDescription;
 import org.eclipse.cdt.core.settings.model.ICProjectDescriptionManager;
-import org.eclipse.cdt.core.settings.model.ICResourceDescription;
 
 import org.eclipse.cdt.internal.ui.CPluginImages;
+import org.eclipse.cdt.internal.ui.util.ResourceConfigurationUtil;
 
 /**
  * Determines if a file or folder got customized build settings and if so decorates with the "wrench" overlay.
@@ -46,32 +38,11 @@ public class CustomBuildSettingsDecorator implements ILightweightLabelDecorator 
 			if (prjDescription != null) {
 				ICConfigurationDescription cfgDescription = prjDescription.getDefaultSettingConfiguration();
 				if (cfgDescription != null) {
-					if (isCustomizedResource(cfgDescription, rc))
+					if (ResourceConfigurationUtil.isCustomizedResource(cfgDescription, rc))
 						decoration.addOverlay(CPluginImages.DESC_OVR_SETTING);
 				}
 			}
 		}
-	}
-
-	private static boolean isCustomizedResource(ICConfigurationDescription cfgDescription, IResource rc) {
-		if (ScannerDiscoveryLegacySupport.isLanguageSettingsProvidersFunctionalityEnabled(rc.getProject()) && cfgDescription instanceof ILanguageSettingsProvidersKeeper) {
-				IContainer parent = rc.getParent();
-				List<String> languages = LanguageSettingsManager.getLanguages(rc, cfgDescription);
-				for (ILanguageSettingsProvider provider: ((ILanguageSettingsProvidersKeeper) cfgDescription).getLanguageSettingProviders()) {
-					for (String languageId : languages) {
-						List<ICLanguageSettingEntry> list = provider.getSettingEntries(cfgDescription, rc, languageId);
-						if (list != null) {
-							List<ICLanguageSettingEntry> listDefault = provider.getSettingEntries(cfgDescription, parent, languageId);
-							// != is OK here due as the equal lists will have the same reference in WeakHashSet
-							if (list != listDefault)
-								return true;
-						}
-					}
-				}
-		}
-
-		ICResourceDescription rcDescription = cfgDescription.getResourceDescription(rc.getProjectRelativePath(), true);
-		return rcDescription != null;
 	}
 
 	@Override

851dec94b329ad69c6b7c1308e8f2b458bdef046
Alvaro Sanchez-Leon <alvsan09@gmail.com>
2014-03-18 10:01:27 -0400

    Bug 235747: [registers] Allow user to edit the register groups. Bug 235747: Move register group actions to the command framework.
    Change-Id: Ife5aefc1a1609309724db01d92a35750e25def24
    
    Signed-off-by: Alvaro Sanchez-Leon <alvsan09@gmail.com>
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/internal/ui/DsfUIPlugin.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/internal/ui/DsfUIPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 Wind River Systems and others.
+ * Copyright (c) 2006, 2014 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     Wind River Systems - initial API and implementation
+ *     Alvaro Sanchez-Leon (Ericsson) - Allow user to edit the register groups (Bug 235747)
  *******************************************************************************/
 package org.eclipse.cdt.dsf.internal.ui;
 
@@ -22,6 +23,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -168,5 +171,19 @@ public class DsfUIPlugin extends AbstractUIPlugin {
     public static IStatus newErrorStatus(int code, String message, Throwable exception) {
         return new Status(IStatus.ERROR, DsfUIPlugin.PLUGIN_ID, code, message, exception);
     }
+	
+	/**
+	 * Returns the active workbench shell or <code>null</code> if none
+	 * 
+	 * @return the active workbench shell or <code>null</code> if none
+	 */
+	public static Shell getActiveWorkbenchShell() {
+		IWorkbenchWindow window = getDefault().getWorkbench().getActiveWorkbenchWindow();
+		if (window != null) {
+			return window.getShell();
+		}
+		
+		return null;
+	}
 
 }

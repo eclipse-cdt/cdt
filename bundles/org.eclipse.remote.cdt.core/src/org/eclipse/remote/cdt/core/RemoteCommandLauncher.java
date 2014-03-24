6f3c30a61308c2bfba6a19dc157a75536870d23a
Greg Watson <g.watson@computer.org>
2014-03-24 09:22:07 -0400

    Add CDT-specific remote support.
    Signed-off-by: Greg Watson <g.watson@computer.org>

---
+++ bundles/org.eclipse.remote.cdt.core/src/org/eclipse/remote/cdt/core/RemoteCommandLauncher.java
@@ -0,0 +1,234 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.remote.cdt.core;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Map;
+import java.util.Properties;
+
+import org.eclipse.cdt.core.CommandLauncher;
+import org.eclipse.cdt.core.ICommandLauncher;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.remote.core.IRemoteConnection;
+import org.eclipse.remote.core.IRemoteProcess;
+import org.eclipse.remote.core.IRemoteProcessBuilder;
+import org.eclipse.remote.core.IRemoteResource;
+import org.eclipse.remote.core.IRemoteServices;
+import org.eclipse.remote.core.RemoteProcessAdapter;
+import org.eclipse.remote.core.RemoteServices;
+import org.eclipse.remote.internal.cdt.core.messages.Messages;
+
+public class RemoteCommandLauncher implements ICommandLauncher {
+	private final ICommandLauncher fLocalLauncher = new CommandLauncher();
+	private boolean fShowCommand;
+	private String[] fCommandArgs;
+	private IRemoteConnection fConnection;
+	private IRemoteProcess fRemoteProcess;
+	private final Properties fEnvironment = new Properties();
+
+	/**
+	 * The number of milliseconds to pause between polling.
+	 */
+	private static final long DELAY = 50L;
+
+	/**
+	 * Constructs a command array that will be passed to the process
+	 */
+	private String[] constructCommandArray(String command, String[] commandArgs) {
+		String[] args = new String[1 + commandArgs.length];
+		args[0] = command;
+		System.arraycopy(commandArgs, 0, args, 1, commandArgs.length);
+		return args;
+	}
+
+	@Override
+	public Process execute(IPath commandPath, String[] args, String[] env, IPath workingDirectory, IProgressMonitor monitor)
+			throws CoreException {
+		if (fLocalLauncher.getProject() != null) {
+			IRemoteResource remRes = (IRemoteResource) fLocalLauncher.getProject().getAdapter(IRemoteResource.class);
+			if (remRes != null) {
+				URI uri = remRes.getActiveLocationURI();
+				IRemoteServices remServices = RemoteServices.getRemoteServices(uri);
+				if (remServices != null) {
+					fConnection = remServices.getConnectionManager().getConnection(uri);
+					if (fConnection != null) {
+						parseEnvironment(env);
+						fCommandArgs = constructCommandArray(commandPath.toOSString(), args);
+						IRemoteProcessBuilder processBuilder = fConnection.getProcessBuilder(fCommandArgs);
+						Map<String, String> processEnv = processBuilder.environment();
+						for (String key : fEnvironment.stringPropertyNames()) {
+							processEnv.put(key, fEnvironment.getProperty(key));
+						}
+						try {
+							fRemoteProcess = processBuilder.start();
+							return new RemoteProcessAdapter(fRemoteProcess);
+						} catch (IOException e) {
+							fLocalLauncher.setErrorMessage(e.getMessage());
+							return null;
+						}
+					}
+				}
+			}
+		}
+		return fLocalLauncher.execute(commandPath, args, env, workingDirectory, monitor);
+	}
+
+	@Override
+	public String[] getCommandArgs() {
+		return fCommandArgs;
+	}
+
+	@Override
+	public String getCommandLine() {
+		return getCommandLine(fCommandArgs);
+	}
+
+	protected String getCommandLine(String[] commandArgs) {
+		return getCommandLineQuoted(commandArgs, false);
+	}
+
+	@SuppressWarnings("nls")
+	private String getCommandLineQuoted(String[] commandArgs, boolean quote) {
+		String nl = System.getProperty("line.separator", "\n");
+		if (fConnection != null) {
+			nl = fConnection.getProperty(IRemoteConnection.LINE_SEPARATOR_PROPERTY);
+		}
+		StringBuffer buf = new StringBuffer();
+		if (commandArgs != null) {
+			for (String commandArg : commandArgs) {
+				if (quote && (commandArg.contains(" ") || commandArg.contains("\"") || commandArg.contains("\\"))) {
+					commandArg = '"' + commandArg.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\"") + '"';
+				}
+				buf.append(commandArg);
+				buf.append(' ');
+			}
+			buf.append(nl);
+		}
+		return buf.toString();
+	}
+
+	@Override
+	public Properties getEnvironment() {
+		return fEnvironment;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return fLocalLauncher.getErrorMessage();
+	}
+
+	@Override
+	public IProject getProject() {
+		return fLocalLauncher.getProject();
+	}
+
+	/**
+	 * Parse array of "ENV=value" pairs to Properties.
+	 */
+	private void parseEnvironment(String[] env) {
+		if (env != null) {
+			fEnvironment.clear();
+			for (String envStr : env) {
+				// Split "ENV=value" and put in Properties
+				int pos = envStr.indexOf('=');
+				if (pos < 0) {
+					pos = envStr.length();
+				}
+				String key = envStr.substring(0, pos);
+				String value = envStr.substring(pos + 1);
+				fEnvironment.put(key, value);
+			}
+		}
+	}
+
+	private void printCommandLine(OutputStream os) {
+		if (os != null) {
+			try {
+				os.write(getCommandLineQuoted(getCommandArgs(), true).getBytes());
+				os.flush();
+			} catch (IOException e) {
+				// ignore;
+			}
+		}
+	}
+
+	@Override
+	public void setErrorMessage(String error) {
+		fLocalLauncher.setErrorMessage(error);
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		fLocalLauncher.setProject(project);
+	}
+
+	@Override
+	public void showCommand(boolean show) {
+		fLocalLauncher.showCommand(show);
+		fShowCommand = show;
+	}
+
+	@Override
+	public int waitAndRead(OutputStream out, OutputStream err) {
+		if (fShowCommand) {
+			printCommandLine(out);
+		}
+
+		if (fRemoteProcess == null) {
+			return ILLEGAL_COMMAND;
+		}
+
+		RemoteProcessClosure closure = new RemoteProcessClosure(fRemoteProcess, out, err);
+		closure.runBlocking();
+		return OK;
+	}
+
+	@Override
+	public int waitAndRead(OutputStream out, OutputStream err, IProgressMonitor monitor) {
+		if (fShowCommand) {
+			printCommandLine(out);
+		}
+		if (fRemoteProcess == null) {
+			return ILLEGAL_COMMAND;
+		}
+
+		RemoteProcessClosure closure = new RemoteProcessClosure(fRemoteProcess, out, err);
+		closure.runNonBlocking();
+		while (!monitor.isCanceled() && closure.isAlive()) {
+			try {
+				Thread.sleep(DELAY);
+			} catch (InterruptedException ie) {
+				// ignore
+			}
+		}
+
+		int state = OK;
+
+		// Operation canceled by the user, terminate abnormally.
+		if (monitor.isCanceled()) {
+			closure.terminate();
+			state = COMMAND_CANCELED;
+			setErrorMessage(Messages.RemoteCommandLauncher_Command_canceled);
+		}
+
+		try {
+			fRemoteProcess.waitFor();
+		} catch (InterruptedException e) {
+			// ignore
+		}
+		return state;
+	}
+}

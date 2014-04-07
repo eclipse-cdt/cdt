033ab31bfeee6aef5f8bfba0b913c8930119ae29
Marc Dumais <marc.dumais@ericsson.com>
2014-04-07 07:41:49 -0400

    Bug 432151 - [visualizer] Thread id no longer shown in multicore visualizer
    Change-Id: Ie88b23ccd0b34343d208b576324cc570ce1568fd
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.multicorevisualizer.ui/src/org/eclipse/cdt/dsf/gdb/multicorevisualizer/internal/ui/view/MulticoreVisualizerThread.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.multicorevisualizer.ui/src/org/eclipse/cdt/dsf/gdb/multicorevisualizer/internal/ui/view/MulticoreVisualizerThread.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     William R. Swanson (Tilera Corporation) - initial API and implementation
  *     Xavier Raynaud (Kalray) - Bug 431690
+ *     Marc Dumais (Ericsson) - Bug 432151
  *******************************************************************************/
 
 package org.eclipse.cdt.dsf.gdb.multicorevisualizer.internal.ui.view;
@@ -177,7 +178,7 @@ public class MulticoreVisualizerThread extends MulticoreVisualizerGraphicObject
 			
 			// draw TID
 			String displayTID = Integer.toString(m_thread.getTID());
-			GUIUtils.drawText(gc, displayTID, m_bounds, x + w + 4, y + 2);
+			GUIUtils.drawText(gc, displayTID, x + w + 4, y + 2);
 			
 			// draw selection marker, if any
 			if (m_selected)

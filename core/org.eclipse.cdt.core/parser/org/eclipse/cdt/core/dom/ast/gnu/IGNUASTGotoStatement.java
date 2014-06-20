8b7cc144628c698bfab50d8587d30c5c68d6e886
qdagans <anders.xb.dahlberg@ericsson.com>
2014-06-20 03:09:30 +0200

    Bug 84144 - [Parser2] GCC: labels as values
    Add support for GNU goto label references.
    
    "http://gcc.gnu.org/onlinedocs/gcc-3.3.2/gcc/Labels-as-Values.html#Labels%20as%20Values
    GCC extensions to C allow taking the address of labels. These addresses can be used in a goto statement where any expression of type void * is allowed:
    
    foo:
      void* labelPtr = &&foo;
      goto *labelPtr;", comment from Andrew Niefer
    
    Add new classes and necessary changes in existing classes to support the above.
    
    Change-Id: I60b64957af3fdfd5463e89c2a36eea13bd280887
    Signed-off-by: qdagans <anders.xb.dahlberg@ericsson.com>

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/gnu/IGNUASTGotoStatement.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Anders Dahlberg (Ericsson) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.dom.ast.gnu;
+
+import org.eclipse.cdt.core.dom.ast.ASTNodeProperty;
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTNameOwner;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
+
+/**
+ * Represents a GNU goto expression.
+ * 
+ * <code>
+ * foo:
+ *   void *labelPtr = &&foo;
+ *   goto *labelPtr;
+ * </code>
+ * 
+ * @since 8.4
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ */
+public interface IGNUASTGotoStatement extends IASTStatement, IASTNameOwner {
+	public static final ASTNodeProperty LABEL_NAME =
+			new ASTNodeProperty("IASTGotoExpression.LABEL_NAME [IASTExpression]"); //$NON-NLS-1$
+
+	/**
+	 * Returns the label-name expression. The expression resolves to a ILabel binding.
+	 * 
+	 * @return <code>IASTExpression</code>
+	 */
+	public IASTExpression getLabelNameExpression();
+
+	/**
+	 * Set the label-name expression.
+	 * 
+	 * @param expression
+	 *            <code>IASTExpression</code>
+	 */
+	public void setLabelNameExpression(IASTExpression expression);
+	
+	/**
+	 * @since 5.1
+	 */
+	@Override
+	public IGNUASTGotoStatement copy();
+
+	/**
+	 * @since 5.3
+	 */
+	@Override
+	public IGNUASTGotoStatement copy(CopyStyle style);
+}
--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/CPPVisitor.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/CPPVisitor.java
@@ -13,6 +13,7 @@
  *     Thomas Corbat (IFS)
  *     Nathan Ridge
  *     Marc-Andre Laperle
+ *     Anders Dahlberg (Ericsson) - bug 84144
  *******************************************************************************/
 package org.eclipse.cdt.internal.core.dom.parser.cpp.semantics;
 
@@ -178,6 +179,7 @@ import org.eclipse.cdt.internal.core.dom.parser.SizeofCalculator;
 import org.eclipse.cdt.internal.core.dom.parser.Value;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFieldReference;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionCallExpression;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTIdExpression;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTName;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTTranslationUnit;
@@ -299,7 +301,18 @@ public class CPPVisitor extends ASTQueries {
 			id.resolveBinding();
 			return name.getBinding();
 		}
-		if (parent instanceof IASTIdExpression) {
+		
+		// GNU Goto label reference
+		//
+		//   void* labelPtr = &&foo; <-- label reference
+		// foo:
+		//
+		boolean labelReference = isLabelReference(parent);
+		
+		if (labelReference) {
+			IASTUnaryExpression expression = (IASTUnaryExpression) parent.getParent();
+			return createLabelReferenceBinding(name, expression);
+		} else if (parent instanceof IASTIdExpression) {
 			return resolveBinding(parent);
 		} else if (parent instanceof ICPPASTFieldReference) {
 			return resolveBinding(parent);
@@ -374,6 +387,55 @@ public class CPPVisitor extends ASTQueries {
 		}
 
 		return scope == inScope;
+	}
+
+	private static boolean isLabelReference(IASTNode node) {
+		boolean labelReference = false;
+		IASTNode parent = node.getParent();
+		
+		if (parent instanceof IASTUnaryExpression) {
+			int operator = ((IASTUnaryExpression) parent).getOperator();
+			labelReference = operator == IASTUnaryExpression.op_labelReference; 
+		}
+		
+		return labelReference;
+	}
+
+	private static IBinding createLabelReferenceBinding(IASTName name, IASTUnaryExpression expression) {
+		IBinding binding = null;
+		
+		// Find function scope for r-value expression
+		//   void* labelPtr = &&foo;
+		// foo:                 ^^^
+		//   return
+	    IScope scope = getContainingScope(name);
+	    IASTInternalScope s = (IASTInternalScope) scope;
+	    IASTNode node = s.getPhysicalNode();
+	    
+	    while (node != null && !(node instanceof IASTFunctionDefinition)) {
+	    	node = node.getParent();
+	    }
+	    	
+	    if (node != null) {
+		    IASTFunctionDefinition definition = (IASTFunctionDefinition) node;
+		    CPPASTFunctionDeclarator declarator = (CPPASTFunctionDeclarator) definition.getDeclarator();
+		    scope = declarator.getFunctionScope();
+		    
+	        if (scope != null) {
+			    binding = scope.getBinding(name, false);
+				if (!(binding instanceof ILabel)) {
+				    binding = new CPPLabel(name);
+				    ASTInternal.addName(scope, name);
+				}
+		    } 
+	    }
+	    
+        if (binding == null) {
+	    	binding = new CPPScope.CPPScopeProblem(expression, IProblemBinding.SEMANTIC_BAD_SCOPE,
+		    		expression.getRawSignature().toCharArray());
+	    }
+    
+	    return binding;
 	}
 
 	private static IBinding createBinding(IASTGotoStatement gotoStatement) {

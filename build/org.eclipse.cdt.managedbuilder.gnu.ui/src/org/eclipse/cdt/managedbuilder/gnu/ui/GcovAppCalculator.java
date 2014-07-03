17cc0188ffbed9b3fd87db51cf92f64f4251d73a
Leo Ufimtsev <lufimtse@redhat.com>
2014-07-03 10:40:45 -0400

    Bug 408545 - Added single checkbox to enable gcov. (edit commit msg) 	I added a checkbox to enable gCov via a single checkbox to         enable gcov in the compiler. It then uses the 	applicability calculator to automatically enable the linker if 	the compiler option is set.
    Signed-off-by: Leo Ufimtsev <lufimtse@redhat.com>

---
+++ build/org.eclipse.cdt.managedbuilder.gnu.ui/src/org/eclipse/cdt/managedbuilder/gnu/ui/GcovAppCalculator.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat, Inc.
+ *
+ *   *Based on implementation of GprofAppCalculator*
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    lufimtse :  Leo Ufimtsev lufimtse@redhat.com
+ *******************************************************************************/
+
+package org.eclipse.cdt.managedbuilder.gnu.ui;
+
+public class GcovAppCalculator extends ProfAppCalculator {
+	@Override
+	protected String getOptionIdPattern() {
+		return ".compiler.option.debugging.codecov"; //$NON-NLS-1$
+	}
+}

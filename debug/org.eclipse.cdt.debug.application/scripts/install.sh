402176c141371516e32f7771b4c1a2968c94e7a5
Marc Khouzam <marc.khouzam@ericsson.com>
2014-07-11 14:15:58 -0400

    Improve Standalone Debugger scripts
    - Fix indentation
    - Call install.sh from cdtdebug.sh if installation needed.
      This should help the user realize they can run the script
      outside if the eclipse installation directory after the first time.
    - Use a subshell to run pwd to avoid having to actually change directory.
    - Tag the line that needs to be modified with a marker to make it future-proof.
    - Use find instead of ls which is easier to parse.
    - Allow to run install.sh without doing a cd to its location
    - Allow to run original cdtdebug.sh without doing a cd to its location
    
    Change-Id: I3f63dcf8a307784b0c38a286e645ea962ce62dc4
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- debug/org.eclipse.cdt.debug.application/scripts/install.sh
+++ debug/org.eclipse.cdt.debug.application/scripts/install.sh
@@ -9,35 +9,35 @@
 # Contributors:
 #    Red Hat Inc. - initial API and implementation
 ###############################################################################
-olddir=`pwd`
-cd ../..
-PLUGINS_DIR=`pwd`
+
 # Verify that the install script is being run from a plug-ins folder of a
 # downloaded Eclipse and not in a local user .eclipse folder.
-if [ ! -f ../eclipse ]; then
-echo "$0: error: eclipse executable not found in expected location"
-echo " "
-echo "This can occur if you are running this script from your local .eclipse directory"
-echo "which would mean you are running a shared instance of the Eclipse platform for"
-echo "your distro and have installed the Stand-alone Debugger from an eclipse.org"
-echo "download repo.  Downloading the Stand-alone Debugger feature on top of a distro"
-echo "version of Eclipse Debugger is not supported.  If you are using a distro version"
-echo "of the Eclipse platform, you should not use this script.  Instead, install the"
-echo "corresponding Eclipse CDT package for your distro (e.g. eclipse-cdt package)"
-echo "which will install the Stand-alone Debugger for you."
-exit 1
+
+SCRIPT_DIR=`dirname $0`
+
+if [ ! -f $SCRIPT_DIR/../../../eclipse ]; then
+  echo "$0: error: eclipse executable not found in expected location"
+  echo " "
+  echo "This can occur if you are running this script from your local .eclipse directory"
+  echo "which would mean you are running a shared instance of the Eclipse platform for"
+  echo "your distro and have installed the Stand-alone Debugger from an eclipse.org"
+  echo "download repo.  Downloading the Stand-alone Debugger feature on top of a distro"
+  echo "version of Eclipse Debugger is not supported.  If you are using a distro version"
+  echo "of the Eclipse platform, you should not use this script.  Instead, install the"
+  echo "corresponding Eclipse CDT package for your distro (e.g. eclipse-cdt package)"
+  echo "which will install the Stand-alone Debugger for you."
+  exit 1
 fi
-cd $olddir
+
 if [ ! -d $HOME/cdtdebugger ]; then
-mkdir -p $HOME/cdtdebugger
+  mkdir -p $HOME/cdtdebugger
 fi
-cp config.ini $HOME/cdtdebugger
-cp dev.properties $HOME/cdtdebugger
-cp cdtdebug.sh $HOME/cdtdebugger
+cp $SCRIPT_DIR/config.ini $HOME/cdtdebugger
+cp $SCRIPT_DIR/dev.properties $HOME/cdtdebugger
+cp $SCRIPT_DIR/cdtdebug.sh $HOME/cdtdebugger
 chmod +x $HOME/cdtdebugger/cdtdebug.sh
-olddir=`pwd`
-cd ../..
-PLUGINS_DIR=`pwd`
-cd $olddir
-sed -i -e "s,cd ../..,cd $PLUGINS_DIR," $HOME/cdtdebugger/cdtdebug.sh
+
+ECLIPSE_HOME=$(cd $SCRIPT_DIR/../../.. && pwd)
+# Replace the entire line with tag @#@# by the actual location of the eclipse installation
+sed -i -e "s,^.*@#@#.*$,ECLIPSE_HOME=$ECLIPSE_HOME," $HOME/cdtdebugger/cdtdebug.sh
 echo "Installation complete"

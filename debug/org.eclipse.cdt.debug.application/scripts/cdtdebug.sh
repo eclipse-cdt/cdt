8fd34dcf10aa81138b6f8da3ffbccd5100147769
Marc Khouzam <marc.khouzam@ericsson.com>
2014-07-11 14:15:58 -0400

    Improve Standalone Debugger scripts
    - Fix indentation
    - Call install.sh from cdtdebug.sh if installation needed.
      This should help the user realize they can run the script
      outside if the eclipse installation directory after the first time.
    - Use a subshell to run pwd to avoid having to actually change directory.
    - Tag the line that needs to be modified with a marker to make it future-proof.
    - Use find instead of ls which is easier to parse.
    
    Change-Id: I3f63dcf8a307784b0c38a286e645ea962ce62dc4
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- debug/org.eclipse.cdt.debug.application/scripts/cdtdebug.sh
+++ debug/org.eclipse.cdt.debug.application/scripts/cdtdebug.sh
@@ -70,28 +70,25 @@ done
 
 # Make sure local directory exists and has contents initialized
 if [ ! -d $HOME/cdtdebugger ]; then
-mkdir -p $HOME/cdtdebugger
-cp config.ini $HOME/cdtdebugger
-cp dev.properties $HOME/cdtdebugger
+  /bin/sh install.sh
 fi
 
 # Calculate platform-specific jar file names
-olddir=`pwd`
-cd ../..
-OSGI_JAR=`ls org.eclipse.osgi_*.jar`
-SWT_JAR=`ls org.eclipse.swt.*.jar`
+ECLIPSE_HOME=$(cd ../../.. && pwd)  # install.sh will modify this line.  DO NOT REMOVE THE FOLLOWING MARKER: @#@#
+PLUGIN_DIR=$ECLIPSE_HOME/plugins
+
+OSGI_JAR=`find $PLUGIN_DIR -maxdepth 1 -name 'org.eclipse.osgi_*.jar' -printf "%f\n"`
+SWT_JAR=`find $PLUGIN_DIR -maxdepth 1 -name 'org.eclipse.swt.*.jar' -printf "%f\n"`
 SWT_PLUGIN=`echo $SWT_JAR | sed -e "s/_[0-9]*\..*.jar//"`
-FS_JAR=`ls org.eclipse.core.filesystem.*.jar | grep -v java7`
+FS_JAR=`find $PLUGIN_DIR -maxdepth 1 -name 'org.eclipse.core.filesystem.*.jar' -printf "%f\n" | grep -v java7`
 FS_PLUGIN=`echo $FS_JAR | sed -e "s/_[0-9]*\..*.jar//"`
-LINUX_JAR=`ls org.eclipse.cdt.core.linux.*.jar`
+LINUX_JAR=`find $PLUGIN_DIR -maxdepth 1 -name 'org.eclipse.cdt.core.linux.*.jar' -printf "%f\n"`
 LINUX_PLUGIN=`echo $LINUX_JAR | sed -e "s/_[0-9]*\..*.jar//"`
-cd ..; ECLIPSE_HOME=`pwd`
-cd $olddir
 
 # Run eclipse with the Stand-alone Debugger product specified
 $ECLIPSE_HOME/eclipse -clean -product org.eclipse.cdt.debug.application.product \
--data $HOME/workspace-cdtdebug -configuration file\:$HOME/cdtdebugger \
--dev file\:$HOME/cdtdebugger/dev.properties $options \
--vmargs -Dosgi.jar=$OSGI_JAR -Dswt.plugin=$SWT_PLUGIN -Dfs.plugin=$FS_PLUGIN \
--Dlinux.plugin=$LINUX_PLUGIN -Declipse.home=$ECLIPSE_HOME
+                      -data $HOME/workspace-cdtdebug -configuration file\:$HOME/cdtdebugger \
+                      -dev file\:$HOME/cdtdebugger/dev.properties $options \
+                      -vmargs -Dosgi.jar=$OSGI_JAR -Dswt.plugin=$SWT_PLUGIN -Dfs.plugin=$FS_PLUGIN \
+                      -Dlinux.plugin=$LINUX_PLUGIN -Declipse.home=$ECLIPSE_HOME
 

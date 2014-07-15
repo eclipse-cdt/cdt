16c28bc16091ba424f19ab9a676b40b82daf91dc
Marc Khouzam <marc.khouzam@ericsson.com>
2014-07-15 09:34:38 -0400

    Bug 439624 - Per-element formatting for expressions and variables
    Change-Id: I25d2c0656d9a21b74693d459e66c0fbe4131038d
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

---
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/numberformat/AbstractElementVMProvider.java
@@ -0,0 +1,100 @@
+/*****************************************************************
+ * Copyright (c) 2012, 2014 Texas Instruments and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marc Khouzam (Ericsson) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.debug.ui.viewmodel.numberformat;
+
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.datamodel.IDMContext;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.cdt.dsf.ui.viewmodel.AbstractVMAdapter;
+import org.eclipse.cdt.dsf.ui.viewmodel.IVMContext;
+import org.eclipse.cdt.dsf.ui.viewmodel.IVMNode;
+import org.eclipse.cdt.dsf.ui.viewmodel.datamodel.AbstractDMVMProvider;
+import org.eclipse.debug.internal.ui.viewers.model.provisional.IPresentationContext;
+import org.eclipse.jface.viewers.TreePath;
+
+/**
+ * Base class for view model providers that can support individual element formatting.
+ * Extending classes should override {@link #supportFormat(IVMContext)} and {@link #getElementKey(IDMContext)}
+ * to enable individual element formatting.
+ */
+public class AbstractElementVMProvider extends AbstractDMVMProvider implements IElementFormatProvider
+{
+	private static String ELEMENT_FORMAT_PERSISTABLE_PROPERTY = "org.eclipse.cdt.dsf.ui.elementFormatPersistable"; //$NON-NLS-1$
+
+	public AbstractElementVMProvider(AbstractVMAdapter adapter,  IPresentationContext context, DsfSession session) {
+		super(adapter, context, session);
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.eclipse.cdt.dsf.debug.ui.viewmodel.numberformat.IElementFormatProvider#supportFormat(org.eclipse.cdt.dsf.ui.viewmodel.IVMContext)
+	 * 
+	 * Keep individual element formatting off by default. Extending classes should override to enable for specific contexts.
+	 */
+	@Override
+	public boolean supportFormat(IVMContext context) {
+		return false;
+	}
+	
+	/**
+	 * Provides a key that will be used to uniquely identify the element for which formatting has been set.
+	 * 
+	 * @param context The element for which formatting has or is being set.
+	 * @return The key to be used to retrieve the element format.
+	 */
+	protected String getElementKey(IVMContext context) {
+		return null;
+	}
+
+	@Override
+	public void getActiveFormat(IPresentationContext context, IVMNode node, Object viewerInput, TreePath elementPath,
+			DataRequestMonitor<String> rm) {
+
+		Object p = context.getProperty(ELEMENT_FORMAT_PERSISTABLE_PROPERTY);
+		String format = null;
+		if (p instanceof ElementFormatPersistable) {
+			ElementFormatPersistable persistable = (ElementFormatPersistable) p;
+			Object x = elementPath.getLastSegment();
+			if (x instanceof IVMContext) {
+				format = persistable.getFormat(getElementKey((IVMContext)x));
+			}
+		}
+
+		rm.done(format);
+	}
+
+	@Override
+	public void setActiveFormat(IPresentationContext context, IVMNode[] node, Object viewerInput, TreePath[] elementPath, String format) {
+		Object p = context.getProperty(ELEMENT_FORMAT_PERSISTABLE_PROPERTY);
+		ElementFormatPersistable persistable = null;
+		if (p instanceof ElementFormatPersistable) {
+			persistable = (ElementFormatPersistable) p;
+		} else {
+			persistable = new ElementFormatPersistable();
+			context.setProperty(ELEMENT_FORMAT_PERSISTABLE_PROPERTY, persistable);
+		}
+
+		boolean changed = false;
+		for (int i = 0; i < elementPath.length; i++) {
+			Object x = elementPath[i].getLastSegment();
+			if (x instanceof IVMContext) {
+				String key = getElementKey((IVMContext)x);
+				if (key != null) {
+					persistable.setFormat(key, format);
+					changed = true;
+				}
+			}
+		}
+		if (changed) {
+			refresh();
+		}
+	}
+}
--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/numberformat/ElementFormatPersistableFactory.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/numberformat/ElementFormatPersistableFactory.java
@@ -1,31 +1,32 @@
 /*****************************************************************
- * Copyright (c) 2011, 2012 Texas Instruments and others
+ * Copyright (c) 2011, 2014 Texas Instruments and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     John Dallaway - DSF-GDB register format persistence (bug 395909)
+ *     John Dallaway - initial API and implementation
+ *     Marc Khouzam (Ericsson) - Create generic element factory for format persistence (bug 439624) 
  *****************************************************************/
-package org.eclipse.cdt.dsf.gdb.internal.ui.viewmodel;
+package org.eclipse.cdt.dsf.debug.ui.viewmodel.numberformat;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.ui.IElementFactory;
 import org.eclipse.ui.IMemento;
 
 /**
- * Register persistable factory for ElementFormatPersistable
+ * Element format persistable factory for ElementFormatPersistable
  */
-public class RegisterPersistableFactory implements IElementFactory {
+public class ElementFormatPersistableFactory implements IElementFactory {
 
-	public static String FACTORY_ID = "org.eclipse.cdt.dsf.gdb.ui.registerPersistableFactory"; //$NON-NLS-1$
+	// Must match id in plugin.xml
+	public static String FACTORY_ID = "org.eclipse.cdt.dsf.ui.elementFormatPersistableFactory"; //$NON-NLS-1$
 	
 	@Override
 	public IAdaptable createElement(IMemento memento) {
-		ElementFormatPersistable x = new ElementFormatPersistable(FACTORY_ID);
+		ElementFormatPersistable x = new ElementFormatPersistable();
 		x.restore(memento);
 		return x;
 	}
-
 }

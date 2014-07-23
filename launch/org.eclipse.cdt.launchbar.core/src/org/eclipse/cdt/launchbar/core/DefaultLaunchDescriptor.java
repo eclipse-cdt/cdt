a4a50f33a162aecd9cdff89146c2ee10c37d6ff9
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2014-07-23 11:41:13 -0400

    LaunchBar - use (more) unique descriptor id, not the name
    Change-Id: I90674ab2e8d72a351be2f717c839e9305bdf2cbe
    Signed-off-by: Alena Laskavaia <elaskavaia.cdt@gmail.com>

--- launch/org.eclipse.cdt.launchbar.core/src/org/eclipse/cdt/launchbar/core/DefaultLaunchDescriptor.java
+++ launch/org.eclipse.cdt.launchbar.core/src/org/eclipse/cdt/launchbar/core/DefaultLaunchDescriptor.java
@@ -13,12 +13,11 @@ package org.eclipse.cdt.launchbar.core;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 public class DefaultLaunchDescriptor implements ILaunchDescriptor {
-
-	private final ILaunchDescriptorType type;
+	private final ILaunchDescriptorType id;
 	private final ILaunchConfiguration config;
 	
 	public DefaultLaunchDescriptor(ILaunchDescriptorType type, ILaunchConfiguration config) {
-		this.type = type;
+		this.id = type;
 		this.config = config;
 	}
 
@@ -29,11 +28,33 @@ public class DefaultLaunchDescriptor implements ILaunchDescriptor {
 
 	@Override
 	public ILaunchDescriptorType getType() {
-		return type;
+		return id;
 	}
 
 	public ILaunchConfiguration getConfig() {
 		return config;
 	}
 
+	public String getId() {
+		return config.getName() + "." + id.getId();
+	}
+
+	@Override
+	public int hashCode() {
+		return 17 + getId().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof DefaultLaunchDescriptor))
+			return false;
+		DefaultLaunchDescriptor other = (DefaultLaunchDescriptor) obj;
+		if (!getId().equals(other.getId()))
+			return false;
+		return true;
+	}
 }

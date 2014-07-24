e916dc56c677ca2a3171e67d089836ca9aa1a5d8
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2014-07-24 16:50:54 -0400

    LaunchBar - added test plugin and refactoring
    Initial tests and refactoring to support tests
    
    
    Change-Id: Ia14961b4a13ad7e8f2e1d75be264243f7432fe2d
    Signed-off-by: Alena Laskavaia <elaskavaia.cdt@gmail.com>

---
+++ launch/org.eclipse.cdt.launchbar.core.tests/src/org/eclipse/cdt/launchbar/core/tests/AutomatedIntegrationSuite.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2011 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     QNX Software Systems - initial API and implementation
+ *     Markus Schorn (Wind River Systems)
+ *******************************************************************************/
+package org.eclipse.cdt.launchbar.core.tests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.cdt.launchbar.core.internal.LaunchBarManagerTest;
+
+public class AutomatedIntegrationSuite extends TestSuite {
+	public AutomatedIntegrationSuite() {
+	}
+
+	public AutomatedIntegrationSuite(Class<? extends TestCase> theClass, String name) {
+		super(theClass, name);
+	}
+
+	public AutomatedIntegrationSuite(Class<? extends TestCase> theClass) {
+		super(theClass);
+	}
+
+	public AutomatedIntegrationSuite(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		final AutomatedIntegrationSuite suite = new AutomatedIntegrationSuite();
+		// tests
+		suite.addTestSuite(LaunchBarManagerTest.class);
+		return suite;
+	}
+}

7f2b0101becad3e3c0986063f4d48e1dfbb77703
Thomas Corbat <tcorbat@hsr.ch>
2014-07-25 17:05:27 +0200

    Bug 440447 - ASTRewrite Loses Nested Modifications
    Modified ChangeGeneratorWriterVisitor to cope with nested modifications
    as described in the ticket.
    
    Also added some related change generator tests to the corresponding
    tests suites.
    
    Refined change to cope with sibling changes too.
    
    Change-Id: Ia04f3d01e50375828588e7fa65e2ec3782c58e79
    Signed-off-by: Thomas Corbat <tcorbat@hsr.ch>

---
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/rewrite/changegenerator/replace/NestedReplaceTest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Institute for Software, HSR Hochschule fuer Technik
+ * Rapperswil, University of applied sciences and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Thomas Corbat (IFS) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.parser.tests.rewrite.changegenerator.replace;
+
+import junit.framework.Test;
+
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTCompoundStatement;
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTExpressionStatement;
+import org.eclipse.cdt.core.dom.ast.IASTName;
+import org.eclipse.cdt.core.dom.ast.IASTNullStatement;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.ast.IASTUnaryExpression;
+import org.eclipse.cdt.core.dom.ast.INodeFactory;
+import org.eclipse.cdt.core.parser.tests.rewrite.changegenerator.ChangeGeneratorTest;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTModification;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTModification.ModificationKind;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTModificationStore;
+
+public class NestedReplaceTest extends ChangeGeneratorTest {
+
+	public NestedReplaceTest() {
+		super("Nested Replace"); //$NON-NLS-1$
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		source =
+				"void foo(int x) {\n" +
+				"	x += 1;\n" +
+				"}";
+		expectedSource = 
+				"void foo(int x) {\n" +
+				"	x++;\n" +
+				"}";
+		super.setUp();
+	}
+
+	public static Test suite() {
+		return new NestedReplaceTest();
+	}
+
+	@Override
+	protected ASTVisitor createModificator(final ASTModificationStore modStore) {
+		return new ASTVisitor() {
+			{
+				shouldVisitStatements = true;
+			}
+
+			@Override
+			public int visit(IASTStatement statement) {
+				if (statement instanceof IASTCompoundStatement) {
+					IASTCompoundStatement compoundStatement = (IASTCompoundStatement) statement;
+					INodeFactory factory = statement.getTranslationUnit().getASTNodeFactory();
+
+					IASTCompoundStatement newCompoundStatement = factory.newCompoundStatement();
+					IASTNullStatement dummyStatement = factory.newNullStatement();
+					newCompoundStatement.addStatement(dummyStatement);
+					ASTModification compoundReplacement = new ASTModification(ModificationKind.REPLACE, compoundStatement, newCompoundStatement, null);
+					modStore.storeModification(null, compoundReplacement);
+
+					IASTName emptyName = factory.newName();
+					IASTExpression idExpression = factory.newIdExpression(emptyName);
+					IASTExpression incrementExpression = factory.newUnaryExpression(IASTUnaryExpression.op_postFixIncr, idExpression);
+					IASTExpressionStatement newStatement = factory.newExpressionStatement(incrementExpression);
+					IASTStatement replacedStatement = compoundStatement.getStatements()[0];
+					ASTModification statementModification = new ASTModification(ModificationKind.REPLACE, dummyStatement, newStatement, null);
+					modStore.storeModification(compoundReplacement, statementModification);
+
+					IASTName xName = factory.newName("x".toCharArray());
+					ASTModification nameModification = new ASTModification(ModificationKind.REPLACE, emptyName, xName, null);
+					modStore.storeModification(statementModification, nameModification);
+				}
+				return PROCESS_ABORT;
+			}
+		};
+	}
+}
--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/rewrite/changegenerator/replace/ReplaceTestSuite.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/rewrite/changegenerator/replace/ReplaceTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Institute for Software, HSR Hochschule fuer Technik  
+ * Copyright (c) 2008, 2014 Institute for Software, HSR Hochschule fuer Technik  
  * Rapperswil, University of applied sciences and others
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0 
@@ -22,20 +22,25 @@ public class ReplaceTestSuite {
 	public static Test suite() throws Exception {
 		TestSuite suite = new TestSuite("ReplaceTestSuite");
 	
-		suite.addTest(NameTest.suite());
-		suite.addTest(MoveTest.suite());
-		suite.addTest(MoveRenameTest.suite());
-		suite.addTest(SameNameTest.suite());
-		suite.addTest(IdenticalTest.suite());
-		suite.addTest(PointerInParameterTest.suite());
-		suite.addTest(ExceptionTest.suite());
 		suite.addTest(ArrayModifierTest.suite());
-		suite.addTest(InitializerTest.suite());
-		suite.addTest(ExpressionTest.suite());
 		suite.addTest(ArraySizeExpressionTest.suite());
+		suite.addTest(CtorChainInitializerTest.suite());
+		suite.addTest(ExceptionTest.suite());
+		suite.addTest(ExpressionTest.suite());
+		suite.addTest(IdenticalTest.suite());
+		suite.addTest(InitializerTest.suite());
+		suite.addTest(MoveRenameTest.suite());
+		suite.addTest(MoveTest.suite());
+		suite.addTest(MultilineWhitespaceHandlingTest.suite());
+		suite.addTest(NameTest.suite());
+		suite.addTest(NestedReplaceTest.suite());
 		suite.addTest(NewInitializerExpressionTest.suite());
-		suite.addTest(StatementTest.suite());
+		suite.addTest(PointerInParameterTest.suite());
+		suite.addTest(ReplaceForLoopBodyTest.suite());
 		suite.addTest(ReplaceInsertStatementTest.suite());
+		suite.addTest(SameNameTest.suite());
+		suite.addTest(StatementTest.suite());
+		suite.addTest(WhitespaceHandlingTest.suite());
 		return suite;
 	}
 }

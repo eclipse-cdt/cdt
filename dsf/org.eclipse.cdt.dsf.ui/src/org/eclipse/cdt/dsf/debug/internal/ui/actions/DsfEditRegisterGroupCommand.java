6182fc469fd1eead25c1f8827f8c7ff56bcb14cf
Marc Khouzam <marc.khouzam@ericsson.com>
2014-07-31 09:15:55 -0400

    Bug 235747: Move register group actions to the command framework.
    Change-Id: I11623e2e057a4c726c0ce3cb101ee0113de6a296
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

---
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/actions/DsfEditRegisterGroupCommand.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marc Khouzam (Ericsson) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.debug.internal.ui.actions;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISources;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Command performing adding a register group.
+ * @since 2.4
+ */
+public class DsfEditRegisterGroupCommand extends AbstractDsfRegisterGroupActions {
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		boolean state = false;
+	    if (evaluationContext instanceof IEvaluationContext) {
+	        Object s = ((IEvaluationContext) evaluationContext).getVariable(ISources.ACTIVE_MENU_SELECTION_NAME);
+	        Object p = ((IEvaluationContext) evaluationContext).getVariable(ISources.ACTIVE_PART_NAME);
+	        if (s instanceof IStructuredSelection && p instanceof IWorkbenchPart) {
+	        	state = canEditRegisterGroup((IWorkbenchPart)p, (IStructuredSelection)s);
+	        }
+	    }
+		setBaseEnabled(state);
+	}
+	
+	@Override
+	public Object execute(ExecutionEvent event) {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		IWorkbenchPart part = HandlerUtil.getActivePart(event);
+		if (selection instanceof IStructuredSelection) {
+			editRegisterGroup(part, (IStructuredSelection)selection);
+		}
+    	return null;
+    }
+}

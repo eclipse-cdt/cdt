de6b33924c249c0db1187ffa88f92eb22b9ffde8
Nathan Ridge <zeratul976@hotmail.com>
2014-08-10 04:02:08 -0400

    Bug 437675 - Handle PDOM function instances in CPPTemplates.instantiateBinding()
    Change-Id: I3bf9ee9f5db0521e60c302b96db39513873aeb1b
    Signed-off-by: Nathan Ridge <zeratul976@hotmail.com>
    Reviewed-on: https://git.eclipse.org/r/31347
    Reviewed-by: Sergey Prigogin <eclipse.sprigogin@gmail.com>
    Tested-by: Sergey Prigogin <eclipse.sprigogin@gmail.com>

---
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPFunctionInstance.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Nathan Ridge.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Nathan Ridge - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.dom.ast.cpp;
+
+/**
+ * This interface represents an instantiation of a function template.
+ *
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
+ * @since 5.8
+ */
+public interface ICPPFunctionInstance extends ICPPTemplateInstance, ICPPFunction {
+}
--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/index/composite/cpp/CompositeCPPFunctionInstance.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/index/composite/cpp/CompositeCPPFunctionInstance.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 Symbian Software Systems and others.
+ * Copyright (c) 2007, 2014 Symbian Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,12 +8,14 @@
  * Contributors:
  *    Andrew Ferguson (Symbian) - Initial implementation
  *    Markus Schorn (Wind River Systems)
+ *    Nathan Ridge
  *******************************************************************************/
 package org.eclipse.cdt.internal.core.index.composite.cpp;
 
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.IType;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPFunction;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPFunctionInstance;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateArgument;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateDefinition;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateInstance;
@@ -21,7 +23,7 @@ import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateParameterMap;
 import org.eclipse.cdt.core.parser.util.ObjectMap;
 import org.eclipse.cdt.internal.core.index.composite.ICompositesFactory;
 
-public class CompositeCPPFunctionInstance extends CompositeCPPFunction implements ICPPTemplateInstance {
+public class CompositeCPPFunctionInstance extends CompositeCPPFunction implements ICPPFunctionInstance {
 
 	public CompositeCPPFunctionInstance(ICompositesFactory cf, ICPPFunction rbinding) {
 		super(cf, rbinding);

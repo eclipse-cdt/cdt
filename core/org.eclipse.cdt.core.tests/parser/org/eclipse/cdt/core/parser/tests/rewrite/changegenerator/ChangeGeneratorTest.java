29064721eb3f4302bbbfaa30ad424bd1e01954fd
Thomas Corbat <tcorbat@hsr.ch>
2014-08-22 12:22:56 +0200

    Refurbished change generator tests: - Moved tests from separate files to categorized files. - Introduced node factory in base class. - Removed use of specific classes, replaced with interaces. - Moved some tests to apropriate suites.
    Change-Id: I71557c0132b888bdc22788f545fec3b03aa732c2
    Signed-off-by: Thomas Corbat <tcorbat@hsr.ch>

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/rewrite/changegenerator/ChangeGeneratorTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/rewrite/changegenerator/ChangeGeneratorTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Institute for Software, HSR Hochschule fuer Technik
+ * Copyright (c) 2008, 2014 Institute for Software, HSR Hochschule fuer Technik
  * Rapperswil, University of applied sciences and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,13 +11,22 @@
  *******************************************************************************/
 package org.eclipse.cdt.core.parser.tests.rewrite.changegenerator;
 
+import java.io.IOException;
+
 import org.eclipse.cdt.core.CCorePlugin;
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPNodeFactory;
 import org.eclipse.cdt.core.index.IIndexManager;
 import org.eclipse.cdt.core.model.CoreModelUtil;
 import org.eclipse.cdt.core.parser.tests.rewrite.TestHelper;
+import org.eclipse.cdt.core.testplugin.CTestPlugin;
+import org.eclipse.cdt.core.testplugin.util.TestSourceReader;
 import org.eclipse.cdt.core.tests.BaseTestFramework;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPNodeFactory;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTModification;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTModification.ModificationKind;
 import org.eclipse.cdt.internal.core.dom.rewrite.ASTModificationStore;
 import org.eclipse.cdt.internal.core.dom.rewrite.changegenerator.ChangeGenerator;
 import org.eclipse.cdt.internal.core.dom.rewrite.commenthandler.ASTCommenter;
@@ -31,8 +40,8 @@ import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
 
 public abstract class ChangeGeneratorTest extends BaseTestFramework {
-	protected String source;
-	protected String expectedSource;
+	protected ASTModificationStore modStore;
+	protected final ICPPNodeFactory factory = CPPNodeFactory.getDefault();
 
 	public ChangeGeneratorTest() {
 		super();
@@ -44,16 +53,28 @@ public abstract class ChangeGeneratorTest extends BaseTestFramework {
 
 	@Override
 	protected void setUp() throws Exception {
+		modStore = new ASTModificationStore();
 		CCorePlugin.getIndexManager().joinIndexer(IIndexManager.FOREVER, new NullProgressMonitor());
 		super.setUp();
 	}
 
 	@Override
-	public void runTest() throws Exception {
-		final ASTModificationStore modStore = new ASTModificationStore();
-		IFile testFile = importFile("source.h", source); //$NON-NLS-1$
+	protected void tearDown() throws Exception {
+		System.gc();
+		fileManager.closeAllFiles();
+		super.tearDown();
+	}
 
-		ASTVisitor visitor = createModificator(modStore);
+	protected StringBuilder[] getTestSource(int sections) throws IOException {
+		return TestSourceReader.getContentsForTest(CTestPlugin.getDefault().getBundle(), "parser",
+				getClass(), getName(), sections);
+	}
+
+	protected void compareResult(ASTVisitor visitor) throws Exception {
+		final StringBuilder[] testSources = getTestSource(2);
+		final String source = testSources[0].toString();
+		final String expected = testSources[1].toString();
+		final IFile testFile = importFile("source.h", source); //$NON-NLS-1$
 
 		CCorePlugin.getIndexManager().reindex(cproject);
 
@@ -61,28 +82,25 @@ public abstract class ChangeGeneratorTest extends BaseTestFramework {
 
 		waitForIndexer(cproject);
 
-		IASTTranslationUnit unit = CoreModelUtil.findTranslationUnit(testFile).getAST();
+		final IASTTranslationUnit unit = CoreModelUtil.findTranslationUnit(testFile).getAST();
 		final ChangeGenerator changeGenerator =
 				new ChangeGenerator(modStore, ASTCommenter.getCommentedNodeMap(unit));
 		unit.accept(visitor);
 
 		changeGenerator.generateChange(unit);
-		Document doc = new Document(source);
+		final Document doc = new Document(source);
 		for (Change change : ((CompositeChange) changeGenerator.getChange()).getChildren()) {
 			if (change instanceof TextFileChange) {
 				TextFileChange textChange = (TextFileChange) change;
 				textChange.getEdit().apply(doc);
 			}
 		}
-		assertEquals(TestHelper.unifyNewLines(expectedSource), TestHelper.unifyNewLines(doc.get()));
+		assertEquals(TestHelper.unifyNewLines(expected), TestHelper.unifyNewLines(doc.get()));
 	}
 
-	protected abstract ASTVisitor createModificator(ASTModificationStore modStore);
-
-	@Override
-	protected void tearDown() throws Exception {
-		System.gc();
-		fileManager.closeAllFiles();
-		super.tearDown();
+	protected ASTModification addModification(ASTModification parentMod, ModificationKind kind, IASTNode targetNode, IASTNode newNode) {
+		ASTModification mod = new ASTModification(kind, targetNode, newNode, null);
+		modStore.storeModification(parentMod, mod);
+		return mod;
 	}
 }

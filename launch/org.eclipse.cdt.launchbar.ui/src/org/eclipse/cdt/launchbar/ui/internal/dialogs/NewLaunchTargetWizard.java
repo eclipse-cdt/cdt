ea50eb632b54e15c728b9e9ef8cb28c544cbfb68
Doug Schaefer <dschaefer@qnx.com>
2014-09-28 15:52:22 -0400

    Launch Bar - New Target Wizard somewhat like the new resource wizards.
    Has a page to select the target type. The INewWizard for the target
    type is registered in the launchBarUIContributions extension point.
    When selected and nextPage is hit, the wizard starts up.
    
    Also some minor cleanup to the new config wizard.
    
    Change-Id: Ic2bb5164d531a21b7a2a9dd7ecd109e18f2411cd

---
+++ launch/org.eclipse.cdt.launchbar.ui/src/org/eclipse/cdt/launchbar/ui/internal/dialogs/NewLaunchTargetWizard.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2014 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Doug Schaefer
+ *******************************************************************************/
+package org.eclipse.cdt.launchbar.ui.internal.dialogs;
+
+import org.eclipse.cdt.launchbar.ui.internal.LaunchBarUIManager;
+import org.eclipse.jface.wizard.Wizard;
+
+public class NewLaunchTargetWizard extends Wizard {
+
+	private final NewLaunchTargetTypePage typePage;
+	
+	public NewLaunchTargetWizard(LaunchBarUIManager uiManager) {
+		typePage = new NewLaunchTargetTypePage(uiManager);
+		setForcePreviousAndNextButtons(true);
+	}
+
+	@Override
+	public void addPages() {
+		addPage(typePage);
+	}
+	
+	@Override
+	public boolean performFinish() {
+		return true;
+	}
+
+	@Override
+	public boolean canFinish() {
+		// Need to move onto the new target wizard
+		return false;
+	}
+
+}

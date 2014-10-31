1a7a2368b30267f27548d748bf76c156bb0e06be
Simon Marchi <simon.marchi@polymtl.ca>
2014-10-31 15:12:44 -0400

    Use consistent indentation in BreakpointTestApp.cc
    Change-Id: I753591b8465ec57477989fee7bbaa0537a9d7dc7
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/BreakpointTestApp.cc
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/BreakpointTestApp.cc
@@ -33,15 +33,15 @@ void setBlocks()
 void loop()
 {
 	int j = 10;
-    int i = 0;
+	int i = 0;
 	for (i = 0; i < ARRAY_SIZE; i++)
 		j = i;
 }
 
 int main()
 {
-    int a = 10;
-    
+	int a = 10;
+
 	zeroBlocks(1);
 	loop();
 	setBlocks();

c845e09950c40d6107111e7e22feb3147979c60d
Simon Marchi <simon.marchi@polymtl.ca>
2014-11-20 12:46:13 -0500

    debug tests: Add thread compatibility layer and use it
    Having a compatibility layer for threading operations, like the one we
    have for sleep, will allow removing a lot of platform dependent code in
    the test sources, therefore simplifying the tests themselves.
    
    The Windows part is not implemented, but I'll do it if the preliminary
    reviews are positive.
    
    I changed MultiThread.cc and MultiThreadRunControl.cc as examples, but
    there are other tests files that could benefit from it.
    
    I also changed MultiThread.cc to remove all the synchronization based on
    sleeps. It now works using thread barriers, which should make the tests
    less prone to random failure (although I don't think these ones were
    particularly flaky) and run faster (since we don't wait for nothing).
    
    The fallouts of that change on the Java part of the tests are taken care
    of as well.
    
    Change-Id: I7be86a5727877638c0ff0a489d263ee6bbe84764
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

---
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/ThreadPthread.h
@@ -0,0 +1,39 @@
+#ifndef THREADPTHREAD_H
+#define THREADPTHREAD_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Thread functions */
+
+static int StartThread(ThreadFunc func, void *arg, ThreadHandle *handle) {
+	return pthread_create(handle, NULL, func, arg) == 0;
+}
+
+static int JoinThread(ThreadHandle handle, ThreadRet *ret)
+{
+	return pthread_join(handle, ret) == 0;
+}
+
+
+/* Barrier functions */
+
+static int ThreadBarrierInit(ThreadBarrier *barrier, unsigned int count)
+{
+	return pthread_barrier_init(barrier, NULL, count) == 0;
+}
+
+static int ThreadBarrierDestroy(ThreadBarrier *barrier)
+{
+	return pthread_barrier_destroy(barrier) == 0;
+}
+
+static int ThreadBarrierWait(ThreadBarrier *barrier)
+{
+	int ret;
+	ret = pthread_barrier_wait(barrier);
+	return ret == 0 || ret == PTHREAD_BARRIER_SERIAL_THREAD;
+}
+
+#endif // THREADPTHREAD_H

0f84a6e04a88daf94c3edf6927e19ca21eeccc70
Simon Marchi <simon.marchi@polymtl.ca>
2014-11-23 08:26:53 -0500

    debug tests: Temporary fix for setting thread name
    This is not intended to be committed.
    
    It would be nice to use pthread_setname_np to set the thread name, but
    it doesn't seem to work on the test machines. This is just an alternative
    to test the Java code.
    
    Change-Id: Ib61eef9bf9ef29d292b035935c6e1223f673b716
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/ThreadPthread.h
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/ThreadPthread.h
@@ -1,9 +1,13 @@
 #ifndef THREADPTHREAD_H
 #define THREADPTHREAD_H
 
+// For pthread_setname_np
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/prctl.h>
 
 /* Thread functions */
 
@@ -16,6 +20,16 @@ static int JoinThread(ThreadHandle handle, ThreadRet *ret)
 	return pthread_join(handle, ret) == 0;
 }
 
+static int ThreadSetName(ThreadHandle handle, const char *name)
+{
+	//return pthread_setname_np(handle, name) == 0;
+	return prctl(PR_SET_NAME, name, 0, 0, 0);
+}
+
+static ThreadHandle ThreadSelf(void)
+{
+	return pthread_self();
+}
 
 /* Barrier functions */
 
@@ -34,16 +48,6 @@ static int ThreadBarrierWait(ThreadBarrier *barrier)
 	int ret;
 	ret = pthread_barrier_wait(barrier);
 	return ret == 0 || ret == PTHREAD_BARRIER_SERIAL_THREAD;
-}
-
-static int ThreadSetName(ThreadHandle thread, const char *name)
-{
-	return pthread_setname_np(thread, name) == 0;
-}
-
-static ThreadHandle ThreadSelf(void)
-{
-	return pthread_self();
 }
 
 #endif // THREADPTHREAD_H

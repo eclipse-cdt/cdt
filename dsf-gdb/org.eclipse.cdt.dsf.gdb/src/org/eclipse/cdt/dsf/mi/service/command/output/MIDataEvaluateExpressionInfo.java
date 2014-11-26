a57489621e252f4fc77be4a803c47d34cd515532
Vladimir Prus <vladimir@codesourcery.com>
2014-11-26 17:46:33 +0300

    Bug 453321 - Convenient field access for MITuple/MIResultRecord
    Change-Id: Id43f2cb9b52743792fc7f9ce40d16914d8e257b4
    Signed-off-by: Vladimir Prus <vladimir@codesourcery.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIDataEvaluateExpressionInfo.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/output/MIDataEvaluateExpressionInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 QNX Software Systems and others.
+ * Copyright (c) 2000, 2014 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     QNX Software Systems - Initial API and implementation
  *     Wind River Systems   - Modified for new DSF Reference Implementation
  *     Mathias Kunter       - use MIConst.getString which is for human consumption (Bug 307311)
+ *     Vladimir Prus (Mentor Graphics) - use MIResultRecord.getMIValue
  *******************************************************************************/
 
 package org.eclipse.cdt.dsf.mi.service.command.output;
@@ -27,15 +28,9 @@ public class MIDataEvaluateExpressionInfo extends MIInfo {
             MIOutput out = getMIOutput();
             MIResultRecord outr = out.getMIResultRecord();
             if (outr != null) {
-                MIResult[] results =  outr.getMIResults();
-                for (int i = 0; i < results.length; i++) {
-                    String var = results[i].getVariable();
-                    if (var.equals("value")) { //$NON-NLS-1$
-                        MIValue value = results[i].getMIValue();
-                        if (value instanceof MIConst) {
-                            fValue = ((MIConst)value).getString();
-                        }
-                    }
+                MIValue value = outr.getField("value"); //$NON-NLS-1$
+                if (value instanceof MIConst) {
+                    fValue = ((MIConst)value).getString();
                 }
             }
         }

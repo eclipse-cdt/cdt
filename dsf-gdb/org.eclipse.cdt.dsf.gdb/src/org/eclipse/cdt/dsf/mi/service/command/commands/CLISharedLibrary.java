abf0a8e3225cc03db84513f63f88ef355ab34177
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2015-01-15 16:57:45 -0500

    Bug 214398: Implement Load Symbols and Load Symbols for All
    - switched default ui from actions to commands
    - for DSF implemented this in DSF GDB
    - for CDI implemented command wrappers
    
    Change-Id: Id44f42fa9f0f3d7f22ff7dda07c0830262667d9c

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/CLISharedLibrary.java
@@ -0,0 +1,24 @@
+package org.eclipse.cdt.dsf.mi.service.command.commands;
+
+import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlDMContext;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIInfo;
+
+/**
+ * CLI command implement gdb sharedlibary command<br>
+ * sharedlibrary regex<br>
+ * Load shared object library symbols for files matching a Unix regular
+ * expression. As with files loaded automatically, it only loads shared
+ * libraries required by your program for a core file or after typing run. If
+ * regex is omitted all shared libraries required by your program are loaded.
+ * 
+ * @since 4.6
+ */
+public class CLISharedLibrary extends MIInterpreterExecConsole<MIInfo> {
+	public CLISharedLibrary(ICommandControlDMContext ctx) {
+		super(ctx, "sharedlibrary"); //$NON-NLS-1$
+	}
+
+	public CLISharedLibrary(ICommandControlDMContext ctx, String regexName) {
+		super(ctx, "sharedlibrary " + regexName); //$NON-NLS-1$
+	}
+}

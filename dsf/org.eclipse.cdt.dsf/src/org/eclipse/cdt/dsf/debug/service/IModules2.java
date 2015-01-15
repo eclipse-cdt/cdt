8db788ee920ff431b19f35076a2f6a810ce2d017
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2015-01-15 16:57:45 -0500

    Bug 214398: Implement Load Symbols and Load Symbols for All
    - switched default ui from actions to commands
    - for DSF implemented this in DSF GDB
    - for CDI implemented command wrappers
    - created new API to support loading symbols in IModules service
    (IModules2)
    
    Change-Id: Id44f42fa9f0f3d7f22ff7dda07c0830262667d9c

---
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/debug/service/IModules2.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2015 QNX Software System and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Elena Laskavaia (QNX Software System) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.debug.service;
+
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
+import org.eclipse.cdt.dsf.datamodel.IDMEvent;
+
+/**
+ * Extends to IModules interface to supporting loading symbols.
+ * @since 2.6
+ */
+public interface IModules2 extends IModules {
+	
+    /**
+     * Event indicating a change in the symbols of certain modules of the symbol context.
+     */
+    public interface ISymbolsChangedDMEvent extends IDMEvent<ISymbolDMContext> {
+        public IModuleDMContext[] getModules();        
+    }
+
+    /**
+     * Indicates symbols were loaded for some modules.
+     */
+    public interface ISymbolsLoadedDMEvent extends ISymbolsChangedDMEvent {}
+
+    /**
+     * Indicates symbols were unloaded for some modules.
+     */
+    public interface ISymbolsUnloadedDMEvent extends ISymbolsChangedDMEvent {}
+
+	/** 
+	 * Load symbols for all modules of the specified symbol context 
+	 */
+	void loadSymbolsForAllModules(ISymbolDMContext symCtx, RequestMonitor rm);
+	
+	/**
+	 *  Load symbols for the specified module 
+	 */
+	void loadSymbols(IModuleDMContext dmc, RequestMonitor rm);
+}

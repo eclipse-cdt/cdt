7e9fa043c62f9e8695def55be085097331122515
Teodor Madan <teodor.madan@freescale.com>
2015-01-28 17:42:26 +0200

    Bug 458650 - NPE when creating a "C/C++ Remote Application" launch config
    Change-Id: I72f48aa4fa7b46e0d5ba5e5e8f6e4877269ea7f5
    Signed-off-by: Teodor Madan <teodor.madan@freescale.com>

--- launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/ui/CAbstractMainTab.java
+++ launch/org.eclipse.cdt.launch/src/org/eclipse/cdt/launch/ui/CAbstractMainTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011  Nokia Corporation and others.
+ * Copyright (c) 2010, 2015  Nokia Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -610,7 +610,8 @@ public abstract class CAbstractMainTab extends CLaunchConfigurationTab {
 	 * @since 7.2
 	 */
 	protected void updateComboTooltip() {
-		if (fBuildConfigCombo != null) {
+		// fBuildConfigCombo might not be loaded when controls are created
+		if (fBuildConfigCombo != null && fBuildConfigCombo.getSelectionIndex() > 0) {
 			String configId = (String) fBuildConfigCombo.getData(Integer.toString(fBuildConfigCombo.getSelectionIndex()));
 			String tooltip = EMPTY_STRING;
 			if (configId.equals(AUTO_CONFIG)) {

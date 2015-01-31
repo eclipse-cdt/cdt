5f3575c3896792453bdacd2092014b8eb266a165
Simon Marchi <simon.marchi@polymtl.ca>
2015-01-30 23:26:14 -0500

    Bug 437001 - [RFC] Support display as array with dynamic lengths
    The goal is to be able to specify a variable or an expression as the
    length of the array when using "Display as array". This can be useful
    when debugging a C program where an array is passed to a function with
    the length as an argument beside it.
    
    It doesn't work completely right now. It works to specify an expression,
    and the initial length corresponds to the result of the expression, but
    the length is not updated when the result of the expression. The
    interface keeps displaying the initial number of elements.
    
    Change-Id: I0ca5e25a74807aed4ffd69c95a23e86f1460d86d
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/model/ICastToArray2.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/core/model/ICastToArray2.java
@@ -1,36 +1,29 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 QNX Software Systems and others.
+ * Copyright (c) 2015 Ericsson.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     QNX Software Systems - Initial API and implementation
+ *     Simon Marchi (Ericsson) - Add castToArray with length expression.
  *******************************************************************************/
 package org.eclipse.cdt.debug.core.model;
 
 import org.eclipse.debug.core.DebugException;
 
 /**
- * Provides the ability to present a variable as an array of the same type.
+ * @since 7.6
  */
-public interface ICastToArray extends ICastToType {
-
-	/**
-	 * Returns whether this element can be currently casted to array.
-	 * 
-	 * @return whether this element can be currently casted to array
-	 */
-	boolean canCastToArray();
-
+public interface ICastToArray2 extends ICastToArray {
 	/**
 	 * Performs the casting. The element is transformed to the array of the same type.
 	 *  
 	 * @param startIndex the index of the first element of the array. 0 means that 
 	 * the original element is the first member of the array. 
-	 * @param length the array size
+	 * @param length an expression representing the array size
 	 * @throws DebugException
+	 * @since 7.6
 	 */
-	void castToArray( int startIndex, int length ) throws DebugException;
+	void castToArray( int startIndex, String lengthExpr ) throws DebugException;
 }

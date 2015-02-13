002e422dac613ff2bebc7bb5e51d43abb47db44b
Marc Khouzam <marc.khouzam@ericsson.com>
2015-02-13 16:14:09 -0500

    Remove unused private method
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/Query.java
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/Query.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 Wind River Systems and others.
+ * Copyright (c) 2006, 2015 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -83,10 +83,6 @@ abstract public class Query<V> extends DsfRunnable
             } 
             fExecuted = true;
             return true;
-        }
-        
-        public synchronized boolean isExecuted() {
-            return fExecuted;
         }
     };
     

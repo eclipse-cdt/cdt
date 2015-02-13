cba4006e97d6af71709fea2ae48520f305ab7b74
Marc Khouzam <marc.khouzam@ericsson.com>
2015-02-13 16:09:37 -0500

    Remove unnecessary SuppressWarnings
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/RequestMonitor.java
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/concurrent/RequestMonitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 Wind River Systems and others.
+ * Copyright (c) 2006, 2015 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -124,7 +124,6 @@ public class RequestMonitor extends DsfExecutable {
 	 * <p>
 	 * This field is set only when tracing is enabled.
 	 */
-	@SuppressWarnings("unused")
 	private String fMonitorBacktrace;
 
 	/**

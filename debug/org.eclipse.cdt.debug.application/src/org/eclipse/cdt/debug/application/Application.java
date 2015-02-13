daeedc08afa82283de6db21eb3ff352a13ebb8df
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-02-13 16:37:45 -0500

    Bug 460167 - RCP for the Stand-alone debugger
    This patch creates a product so that the stand-alone can be downloaded
    without
    the rest of the C/C++ EPP. It also makes it easier to use because the
    user only
    has to launch the executable, just like a regular Eclipse instead of
    finding
    the script.
    
    To try the RCP:
    
    mvn clean package -Pbuild-standalone-debugger-rcp
    
    The result (tar.gz) is available under
    debug/org.eclipse.cdt.debug.application.product/target/products
    Once extracted, it can be started just like the normal script:
    ./cdtdebug -e myexecutable
    
    Change-Id: Ifb849af8a8f2ec03abcae57cf43d57cde2333759
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- debug/org.eclipse.cdt.debug.application/src/org/eclipse/cdt/debug/application/Application.java
+++ debug/org.eclipse.cdt.debug.application/src/org/eclipse/cdt/debug/application/Application.java
@@ -10,8 +10,17 @@
  *******************************************************************************/
 package org.eclipse.cdt.debug.application;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.MessageFormat;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -20,6 +29,10 @@ import org.eclipse.ui.PlatformUI;
  * This class controls all aspects of the application's execution
  */
 public class Application implements IApplication {
+	
+	public static final String WORKSPACE_NAME = "workspace-cdtdebug"; //$NON-NLS-1$
+
+	private Location fInstanceLoc = null;
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
@@ -28,6 +41,10 @@ public class Application implements IApplication {
 	public Object start(IApplicationContext context) throws Exception {
 		Display display = PlatformUI.createDisplay();
 		try {
+			if (!setupWorkspaceLocation(display)) {
+				return IApplication.EXIT_OK;
+			}
+			
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART)
 				return IApplication.EXIT_RESTART;
@@ -37,6 +54,46 @@ public class Application implements IApplication {
 			display.dispose();
 		}
 		
+	}
+	
+	private boolean setupWorkspaceLocation(Display display) throws IOException {
+        // fetch the Location that we will be modifying
+        fInstanceLoc = Platform.getInstanceLocation();
+
+        // -data @noDefault in <applName>.ini allows us to set the workspace here.
+        // If the user wants to change the location then he has to change
+        // @noDefault to a specific location or remove -data @noDefault for
+        // default location
+        if (!fInstanceLoc.allowsDefault() && !fInstanceLoc.isSet()) {
+            File workspaceRoot = new Path(System.getProperty("user.home")).toFile(); //$NON-NLS-1$
+
+            if (!workspaceRoot.exists()) {
+                MessageDialog.openError(display.getActiveShell(),
+                        Messages.Application_WorkspaceCreationError,
+                        MessageFormat.format(Messages.Application_WorkspaceRootNotExistError, new Object[] { workspaceRoot }));
+                return false;
+            }
+
+            if (!workspaceRoot.canWrite()) {
+                MessageDialog.openError(display.getActiveShell(),
+                        Messages.Application_WorkspaceCreationError,
+                        MessageFormat.format(Messages.Application_WorkspaceRootPermissionError, new Object[] { workspaceRoot }));
+                return false;
+            }
+
+            String workspace = workspaceRoot.getAbsolutePath() + File.separator + WORKSPACE_NAME;
+            // set location to workspace
+            fInstanceLoc.set(new URL("file", null, workspace), false); //$NON-NLS-1$
+        }
+
+        if (!fInstanceLoc.lock()) {
+            MessageDialog.openError(display.getActiveShell(),
+                    Messages.Application_WorkspaceCreationError,
+                    MessageFormat.format(Messages.Application_WorkspaceInUseError, new Object[] { fInstanceLoc.getURL().getPath() }));
+            return false;
+        }
+		
+        return true;
 	}
 
 	/* (non-Javadoc)
@@ -48,6 +105,7 @@ public class Application implements IApplication {
 			return;
 		final IWorkbench workbench = PlatformUI.getWorkbench();
 		final Display display = workbench.getDisplay();
+		fInstanceLoc.release();
 		display.syncExec(new Runnable() {
 			@Override
 			public void run() {

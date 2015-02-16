275feb68745a7eaf27d4010d4875abdd158cbc33
Doug Schaefer <dschaefer@qnx.com>
2015-02-16 12:19:18 -0500

    Bug 459971 New SerialPort support. First step is Mac library.
    Java classes are defined and a pretty simple native library for
    Mac is there. Tested with Arduino so I know reading works. This
    implementation should work for Linux as well. Windows is going to
    be the hard one.
    
    Not checking in the binaries yet until I get more testing with them.
    
    Also remove ppc native libraries for Mac since that hasn't been
    supported in a long time.
    
    Change-Id: If4ffbc6e73a7656a47c2f45b875be0842c482b05

---
+++ core/org.eclipse.cdt.core.native/src/org/eclipse/cdt/utils/serial/ByteSize.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2015 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     QNX Software Systems - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.utils.serial;
+
+public enum ByteSize {
+
+	B5(5),
+	B6(6),
+	B7(7),
+	B8(8);
+	
+	private final int size;
+	
+	private ByteSize(int size) {
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+}

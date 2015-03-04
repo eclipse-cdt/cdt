841d82e0ba94e2341eb11b2e7f9282f7ffc8b06e
Alvaro Sanchez-Leon <alvsan09@gmail.com>
2015-03-04 12:41:26 -0500

    Bug 345872 - Updated to keep compatibility with earlier versions than Eclipse 4.5
    Change-Id: I20f1981ee4b1a1bf395040ea0d22e2ac37259e7d

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CEditor.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CEditor.java
@@ -374,7 +374,6 @@ public class CEditor extends TextEditor implements ICEditor, ISelectionChangedLi
 
 		// This method is called when the Platform version is 4.5 or higher.
 		// @Override
-		@Override
 		protected IFormattingContext createFormattingContext(int selectionOffset, int selectionLength) {
 			return createFormattingContext(selectionOffset, selectionLength, true);
 		}

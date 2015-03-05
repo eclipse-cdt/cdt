f3457c7a55b271335fd5ad491d2ecbcd16890c2f
Marc Khouzam <marc.khouzam@ericsson.com>
2015-03-05 13:38:43 -0500

    Typo
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/launch/DefaultDsfSelectionPolicy.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/ui/viewmodel/launch/DefaultDsfSelectionPolicy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 Wind River Systems, Inc. and others.
+ * Copyright (c) 2008, 2015 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,7 @@ import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeSelection;
 
 /**
- * Default DSF selection policy implementation modelled after platform version
+ * Default DSF selection policy implementation modeled after platform version
  * (<code>DefaultSelectionPolicy</code>).
  * @since 1.1
  */

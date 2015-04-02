eclipse-cdt/cdt

commit d5cb7731dbb432e6bb8fefc9bf75c54ff1cea89e

Bug 457493 - Add support for window terminal size and signals.
Change-Id: Iceb1786796c43c70584d6e01824ffe56e9bed255
Signed-off-by: Greg Watson <g.watson@computer.org>

bundles/org.eclipse.remote.jsch.core/src/org/eclipse/remote/internal/jsch/core/JSchConnectionProxyFactory.java
@@ -24,16 +24,13 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.remote.core.IRemoteConnection;
-import org.eclipse.remote.core.IRemoteProcess;
 import org.eclipse.remote.core.IRemoteProcessService;
+import org.eclipse.remote.core.IRemoteProcess;
 import org.eclipse.remote.core.IRemoteServicesManager;
 import org.eclipse.remote.core.exception.RemoteConnectionException;
 import org.eclipse.remote.internal.jsch.core.messages.Messages;
 
 import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Proxy;
 import com.jcraft.jsch.SocketFactory;
 
@@ -49,12 +46,13 @@ public class JSchConnectionProxyFactory {
 		private String command;
 		private IRemoteProcess process;
 		private JSchConnection connection;
-		private IProgressMonitor monitor;
+		private final IProgressMonitor monitor;
 		private boolean connectCalled = false;
 
 		private CommandProxy(JSchConnection connection, String command, IProgressMonitor monitor) {
-			if (command == null || monitor == null)
+			if (command == null || monitor == null) {
 				throw new IllegalArgumentException();
+			}
 			this.command = command;
 			this.connection = connection;
 			this.monitor = monitor;
@@ -62,7 +60,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#close()
 		 */
 		@Override
@@ -72,12 +70,11 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#connect(com.jcraft.jsch.SocketFactory, java.lang.String, int, int)
 		 */
 		@Override
-		public void connect(SocketFactory socket_factory, String host,
-				int port, int timeout) throws IOException {
+		public void connect(SocketFactory socket_factory, String host, int port, int timeout) throws IOException {
 			assert !connectCalled : "connect should only be called once"; //$NON-NLS-1$
 			try {
 				if (timeout == 0) {
@@ -88,11 +85,14 @@ public class JSchConnectionProxyFactory {
 				SubMonitor subMon = SubMonitor.convert(monitor, waitSteps * 2);
 				final SubMonitor childMon = subMon.newChild(waitSteps);
 
+				if (connection == null) {
+					IRemoteServicesManager manager = Activator.getService(IRemoteServicesManager.class);
+					connection = (JSchConnection) manager.getLocalConnectionType().getConnections().get(0);
+				}
+
 				// Open connection if it isn't already opened
 				try {
-					if (connection != null) {
-						connection.openMinimal(childMon);
-					}
+					connection.openMinimal(childMon);
 				} catch (RemoteConnectionException e) {
 					throw new IOException(e);
 				}
@@ -102,22 +102,11 @@ public class JSchConnectionProxyFactory {
 				command = command.replace("%h", host); //$NON-NLS-1$
 				command = command.replace("%p", Integer.toString(port)); //$NON-NLS-1$
 
-				if (connection != null) {
-					// The process-builder adds unnecessary extra commands (cd, export, ..) and might not work for restricted shells
-					try {
-						ChannelExec exec = connection.getExecChannel();
-						exec.setCommand(command);
-						exec.connect();
-						process = new JSchProcess(exec, false);
-					} catch (JSchException | RemoteConnectionException e) {
-						throw new IOException(e);
-					}
-				} else {
-					List<String> cmd = new ArgumentParser(command).getTokenList();
-					IRemoteServicesManager manager = Activator.getService(IRemoteServicesManager.class);
-					IRemoteConnection connection = manager.getLocalConnectionType().getConnections().get(0); 
-					process = connection.getService(IRemoteProcessService.class).getProcessBuilder(cmd).start();
-				}
+				List<String> cmd = new ArgumentParser(command).getTokenList();
+				JSchProcessBuilder processBuilder = (JSchProcessBuilder) connection.getRemoteConnection()
+						.getService(IRemoteProcessService.class).getProcessBuilder(cmd);
+				processBuilder.setPreamble(false);
+				process = processBuilder.start();
 
 				// Wait on command to produce stdout output
 				long endTime = System.currentTimeMillis() + timeout;
@@ -154,8 +143,8 @@ public class JSchConnectionProxyFactory {
 					} else if (bCanceled) {
 						cause = Messages.JSchConnectionProxyFactory_wasCanceled;
 					}
-					throw new IOException(MessageFormat.format(Messages.JSchConnectionProxyFactory_ProxyCommandFailed,
-							command, cause, msg));
+					throw new IOException(MessageFormat.format(Messages.JSchConnectionProxyFactory_ProxyCommandFailed, command,
+							cause, msg));
 				}
 
 				// Dump the stderr to log
@@ -166,8 +155,7 @@ public class JSchConnectionProxyFactory {
 						String line;
 						try {
 							while ((line = bufferedReader.readLine()) != null) {
-								log.log(new Status(IStatus.INFO, Activator.getUniqueIdentifier(),
-										IStatus.OK, line, null));
+								log.log(new Status(IStatus.INFO, Activator.getUniqueIdentifier(), IStatus.OK, line, null));
 							}
 						} catch (IOException e) {
 							Activator.log(e);
@@ -181,7 +169,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getInputStream()
 		 */
 		@Override
@@ -191,7 +179,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getOutputStream()
 		 */
 		@Override
@@ -201,7 +189,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getSocket()
 		 */
 		@Override
@@ -212,20 +200,21 @@ public class JSchConnectionProxyFactory {
 
 	private static class SSHForwardProxy implements Proxy {
 		private Channel channel;
-		private JSchConnection connection;
-		private IProgressMonitor monitor;
+		private final JSchConnection connection;
+		private final IProgressMonitor monitor;
 		private boolean connectCalled = false;
 
 		private SSHForwardProxy(JSchConnection proxyConnection, IProgressMonitor monitor) {
-			if (proxyConnection == null || monitor == null)
+			if (proxyConnection == null || monitor == null) {
 				throw new IllegalArgumentException();
+			}
 			this.connection = proxyConnection;
 			this.monitor = monitor;
 		}
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#close()
 		 */
 		@Override
@@ -235,12 +224,11 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#connect(com.jcraft.jsch.SocketFactory, java.lang.String, int, int)
 		 */
 		@Override
-		public void connect(SocketFactory socket_factory, String host, int port,
-				int timeout) throws Exception {
+		public void connect(SocketFactory socket_factory, String host, int port, int timeout) throws Exception {
 			assert !connectCalled : "connect should only be called once"; //$NON-NLS-1$
 			try {
 				if (!connection.hasOpenSession()) {
@@ -258,7 +246,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getInputStream()
 		 */
 		@Override
@@ -273,7 +261,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getOutputStream()
 		 */
 		@Override
@@ -288,7 +276,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 *
+		 * 
 		 * @see com.jcraft.jsch.Proxy#getSocket()
 		 */
 		@Override

bundles/org.eclipse.remote.jsch.core/src/org/eclipse/remote/internal/jsch/core/JSchProcessBuilder.java
@@ -24,10 +24,12 @@ import java.util.Set;
 
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.remote.core.AbstractRemoteProcessBuilder;
+import org.eclipse.remote.core.IRemoteConnection;
 import org.eclipse.remote.core.IRemoteFileService;
 import org.eclipse.remote.core.IRemoteProcess;
 import org.eclipse.remote.core.exception.RemoteConnectionException;
 import org.eclipse.remote.internal.core.RemoteDebugOptions;
+import org.eclipse.remote.internal.core.RemoteProcess;
 import org.eclipse.remote.internal.jsch.core.messages.Messages;
 
 import com.jcraft.jsch.ChannelExec;
@@ -39,11 +41,13 @@ public class JSchProcessBuilder extends AbstractRemoteProcessBuilder {
 	private final Map<String, String> fRemoteEnv = new HashMap<String, String>();
 	private final Set<Character> charSet = new HashSet<Character>();
 
-	private Map<String, String> fNewRemoteEnv = null;
+	private ChannelExec fChannel;
+	private Map<String, String> fNewRemoteEnv;
+	private boolean fPreamble = true;
 
-	public JSchProcessBuilder(JSchConnection connection, List<String> command) {
-		super(command);
-		fConnection = connection;
+	public JSchProcessBuilder(IRemoteConnection connection, List<String> command) {
+		super(connection, command);
+		fConnection = connection.getService(JSchConnection.class);
 		fRemoteEnv.putAll(fConnection.getEnv());
 
 		// Create set of characters not to escape
@@ -56,7 +60,7 @@ public class JSchProcessBuilder extends AbstractRemoteProcessBuilder {
 		}
 	}
 
-	public JSchProcessBuilder(JSchConnection connection, String... command) {
+	public JSchProcessBuilder(IRemoteConnection connection, String... command) {
 		this(connection, Arrays.asList(command));
 	}
 
@@ -151,14 +155,14 @@ public class JSchProcessBuilder extends AbstractRemoteProcessBuilder {
 		}
 
 		try {
-			ChannelExec exec = fConnection.getExecChannel();
+			fChannel = fConnection.getExecChannel();
 			String command = buildCommand(remoteCmd, env, clearEnv);
-			exec.setCommand(command);
-			exec.setPty((flags & ALLOCATE_PTY) == ALLOCATE_PTY);
-			exec.setXForwarding((flags & FORWARD_X11) == FORWARD_X11);
-			exec.connect();
+			fChannel.setCommand(command);
+			fChannel.setPty((flags & ALLOCATE_PTY) == ALLOCATE_PTY);
+			fChannel.setXForwarding((flags & FORWARD_X11) == FORWARD_X11);
+			fChannel.connect();
 			RemoteDebugOptions.trace(RemoteDebugOptions.DEBUG_REMOTE_COMMANDS, "executing command: " + command); //$NON-NLS-1$
-			return new JSchProcess(exec, redirectErrorStream());
+			return new RemoteProcess(getRemoteConnection(), this);
 		} catch (RemoteConnectionException e) {
 			throw new IOException(e.getMessage());
 		} catch (JSchException e) {
@@ -166,24 +170,34 @@ public class JSchProcessBuilder extends AbstractRemoteProcessBuilder {
 		}
 	}
 
+	public ChannelExec getChannel() {
+		return fChannel;
+	}
+
+	public void setPreamble(boolean enable) {
+		fPreamble = enable;
+	}
+
 	private String buildCommand(String cmd, List<String> environment, boolean clearEnv) {
 		StringBuffer sb = new StringBuffer();
-		if (directory() != null) {
-			sb.append("cd " + charEscapify(directory().toURI().getPath(), charSet) + " && "); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-		if (clearEnv) {
-			sb.append("env -i"); //$NON-NLS-1$
-			for (String env : environment) {
-				sb.append(" \"" + env + "\""); //$NON-NLS-1$ //$NON-NLS-2$
+		if (fPreamble) {
+			if (directory() != null) {
+				sb.append("cd " + charEscapify(directory().toURI().getPath(), charSet) + " && "); //$NON-NLS-1$ //$NON-NLS-2$
 			}
-			sb.append(" "); //$NON-NLS-1$
-		} else {
-			for (String env : environment) {
-				sb.append("export \"" + env + "\"; "); //$NON-NLS-1$ //$NON-NLS-2$
+			if (clearEnv) {
+				sb.append("env -i"); //$NON-NLS-1$
+				for (String env : environment) {
+					sb.append(" \"" + env + "\""); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+				sb.append(" "); //$NON-NLS-1$
+			} else {
+				for (String env : environment) {
+					sb.append("export \"" + env + "\"; "); //$NON-NLS-1$ //$NON-NLS-2$
+				}
 			}
 		}
 		sb.append(cmd);
-		if (fConnection.useLoginShell()) {
+		if (fPreamble && fConnection.useLoginShell()) {
 			sb.insert(0, "/bin/bash -l -c '"); //$NON-NLS-1$
 			sb.append("'"); //$NON-NLS-1$
 		}

bundles/org.eclipse.remote.core/plugin.xml
@@ -33,6 +33,16 @@
             factory="org.eclipse.remote.internal.core.services.local.LocalProcessService$Factory"
             service="org.eclipse.remote.core.IRemoteProcessService">
       </connectionService>
+      <processService
+            connectionTypeId="org.eclipse.remote.LocalServices"
+            factory="org.eclipse.remote.internal.core.services.local.LocalProcess$Factory"
+            service="org.eclipse.remote.core.IRemoteProcessControlService">
+      </processService>
+      <processService
+            connectionTypeId="org.eclipse.remote.LocalServices"
+            factory="org.eclipse.remote.internal.core.services.local.LocalProcess$Factory"
+            service="org.eclipse.remote.internal.core.services.local.LocalProcess">
+      </processService>
    </extension>
    <extension
          point="org.eclipse.core.runtime.adapters">

bundles/org.eclipse.remote.core/schema/remoteServices.exsd
@@ -21,6 +21,7 @@
             <element ref="connectionType" minOccurs="0" maxOccurs="unbounded"/>
             <element ref="connectionTypeService" minOccurs="0" maxOccurs="unbounded"/>
             <element ref="connectionService" minOccurs="0" maxOccurs="unbounded"/>
+            <element ref="processService" minOccurs="0" maxOccurs="unbounded"/>
          </sequence>
          <attribute name="point" type="string" use="required">
             <annotation>
@@ -167,6 +168,46 @@
                </documentation>
                <appInfo>
                   <meta.attribute kind="java" basedOn=":org.eclipse.remote.core.IRemoteConnection$Service$Factory"/>
+               </appInfo>
+            </annotation>
+         </attribute>
+      </complexType>
+   </element>
+
+   <element name="processService">
+      <annotation>
+         <documentation>
+            This is a service that implements the given service interface for processes of a given connection type.
+         </documentation>
+      </annotation>
+      <complexType>
+         <attribute name="connectionTypeId" type="string" use="required">
+            <annotation>
+               <documentation>
+                  The connection type for the connections that this service applies to.
+               </documentation>
+               <appInfo>
+                  <meta.attribute kind="identifier" basedOn="org.eclipse.remote.core.remoteServices/connectionType/@id"/>
+               </appInfo>
+            </annotation>
+         </attribute>
+         <attribute name="service" type="string" use="required">
+            <annotation>
+               <documentation>
+                  The interface class that this service implements. The service is found by calling the getService() method on the IRemoteProcess object with this interface class as the parameter.
+               </documentation>
+               <appInfo>
+                  <meta.attribute kind="java"/>
+               </appInfo>
+            </annotation>
+         </attribute>
+         <attribute name="factory" type="string" use="required">
+            <annotation>
+               <documentation>
+                  The factory class used to instantiate the service.
+               </documentation>
+               <appInfo>
+                  <meta.attribute kind="java" basedOn=":org.eclipse.remote.core.IRemoteProcess$Service$Factory"/>
                </appInfo>
             </annotation>
          </attribute>

bundles/org.eclipse.remote.core/src/org/eclipse/remote/core/AbstractRemoteProcess.java
@@ -1,89 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * IBM Corporation - Initial API and implementation
- *******************************************************************************/
-package org.eclipse.remote.core;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
-/**
- * Abstract base class for remote processes. Implementors can use this class to provide a default remote process implementation.
- */
-public abstract class AbstractRemoteProcess extends Process implements IRemoteProcess {
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#destroy()
-	 */
-	@Override
-	public void destroy() {
-		// Nothing
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#exitValue()
-	 */
-	@Override
-	public int exitValue() {
-		return 0;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#getErrorStream()
-	 */
-	@Override
-	public InputStream getErrorStream() {
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#getInputStream()
-	 */
-	@Override
-	public InputStream getInputStream() {
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#getOutputStream()
-	 */
-	@Override
-	public OutputStream getOutputStream() {
-		return null;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Process#waitFor()
-	 */
-	@Override
-	public int waitFor() throws InterruptedException {
-		return 0;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.remote.core.IRemoteProcess#isCompleted()
-	 */
-	@Override
-	public boolean isCompleted() {
-		return true;
-	}
-}

bundles/org.eclipse.remote.core/src/org/eclipse/remote/core/AbstractRemoteProcessBuilder.java
@@ -25,15 +25,24 @@ import org.eclipse.core.filesystem.IFileStore;
  */
 public abstract class AbstractRemoteProcessBuilder implements IRemoteProcessBuilder {
 	private List<String> fCommandArgs;
-	private IFileStore fRemoteDir = null;
-	private boolean fRedirectErrorStream = false;
+	private IFileStore fRemoteDir;
+	private boolean fRedirectErrorStream;
 
-	public AbstractRemoteProcessBuilder(List<String> command) {
+	private final IRemoteConnection fConnection;
+
+	/**
+	 * @since 2.0
+	 */
+	public AbstractRemoteProcessBuilder(IRemoteConnection connection, List<String> command) {
 		fCommandArgs = command;
+		fConnection = connection;
 	}
 
-	public AbstractRemoteProcessBuilder(String... command) {
-		this(Arrays.asList(command));
+	/**
+	 * @since 2.0
+	 */
+	public AbstractRemoteProcessBuilder(IRemoteConnection connection, String... command) {
+		this(connection, Arrays.asList(command));
 	}
 
 	/*
@@ -172,5 +181,18 @@ public abstract class AbstractRemoteProcessBuilder implements IRemoteProcessBuil
 			res.append(" "); //$NON-NLS-1$
 		}
 		return res.toString();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.remote.core.IRemoteProcessBuilder#getRemoteConnection()
+	 */
+	/**
+	 * @since 2.0
+	 */
+	@Override
+	public IRemoteConnection getRemoteConnection() {
+		return fConnection;
 	}
 }

bundles/org.eclipse.remote.core/src/org/eclipse/remote/core/IRemoteConnectionType.java
@@ -111,6 +111,15 @@ public interface IRemoteConnectionType {
 	<T extends IRemoteConnection.Service> boolean hasConnectionService(Class<T> service);
 
 	/**
+	 * Do processes created by this connection type support the given service.
+	 * 
+	 * @param service
+	 *            the service to be tested
+	 * @return true if processes created by this connection type support this service
+	 */
+	<T extends IRemoteProcess.Service> boolean hasProcessService(Class<T> service);
+
+	/**
 	 * Gets the remote connection corresponding to the supplied name.
 	 * 
 	 * @param name

commit f65b1837eddd213ad1f9f9699c6c8ba63520a177

Fix local command proxy
Was broken by d5cb7731dbb

Change-Id: I26ec14d832150d20dfefa19b614299a495897bf5

bundles/org.eclipse.remote.jsch.core/src/org/eclipse/remote/internal/jsch/core/JSchConnectionProxyFactory.java
@@ -24,8 +24,8 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.remote.core.IRemoteProcessService;
 import org.eclipse.remote.core.IRemoteProcess;
+import org.eclipse.remote.core.IRemoteProcessService;
 import org.eclipse.remote.core.IRemoteServicesManager;
 import org.eclipse.remote.core.exception.RemoteConnectionException;
 import org.eclipse.remote.internal.jsch.core.messages.Messages;
@@ -60,7 +60,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#close()
 		 */
 		@Override
@@ -70,106 +70,106 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#connect(com.jcraft.jsch.SocketFactory, java.lang.String, int, int)
 		 */
 		@Override
 		public void connect(SocketFactory socket_factory, String host, int port, int timeout) throws IOException {
 			assert !connectCalled : "connect should only be called once"; //$NON-NLS-1$
-			try {
-				if (timeout == 0) {
-					timeout = 10000; // default to 10s
-				}
-				final int waitTime = 50;
-				final int waitSteps = timeout / waitTime;
-				SubMonitor subMon = SubMonitor.convert(monitor, waitSteps * 2);
-				final SubMonitor childMon = subMon.newChild(waitSteps);
+			connectCalled = true;
 
-				if (connection == null) {
-					IRemoteServicesManager manager = Activator.getService(IRemoteServicesManager.class);
-					connection = (JSchConnection) manager.getLocalConnectionType().getConnections().get(0);
-				}
+			if (timeout == 0) {
+				timeout = 10000; // default to 10s
+			}
+			final int waitTime = 50;
+			final int waitSteps = timeout / waitTime;
+			SubMonitor subMon = SubMonitor.convert(monitor, waitSteps * 2);
+			final SubMonitor childMon = subMon.newChild(waitSteps);
 
+			if (connection != null) {
 				// Open connection if it isn't already opened
 				try {
 					connection.openMinimal(childMon);
 				} catch (RemoteConnectionException e) {
 					throw new IOException(e);
 				}
-				subMon.setWorkRemaining(waitSteps);
+			}
+			subMon.setWorkRemaining(waitSteps);
 
-				// Start command
-				command = command.replace("%h", host); //$NON-NLS-1$
-				command = command.replace("%p", Integer.toString(port)); //$NON-NLS-1$
+			// Start command
+			command = command.replace("%h", host); //$NON-NLS-1$
+			command = command.replace("%p", Integer.toString(port)); //$NON-NLS-1$
 
-				List<String> cmd = new ArgumentParser(command).getTokenList();
-				JSchProcessBuilder processBuilder = (JSchProcessBuilder) connection.getRemoteConnection()
-						.getService(IRemoteProcessService.class).getProcessBuilder(cmd);
+			List<String> cmd = new ArgumentParser(command).getTokenList();
+
+			if (connection != null) {
+				JSchProcessBuilder processBuilder = (JSchProcessBuilder) connection.getProcessBuilder(cmd);
 				processBuilder.setPreamble(false);
 				process = processBuilder.start();
+			} else {
+				process = Activator.getService(IRemoteServicesManager.class).getLocalConnectionType().getConnections().get(0).
+						getService(IRemoteProcessService.class).getProcessBuilder(cmd).start();
+			}
 
-				// Wait on command to produce stdout output
-				long endTime = System.currentTimeMillis() + timeout;
-				boolean bOutputAvailable, bProcessComplete, bTimedOut, bCanceled;
-				do {
-					try {
-						Thread.sleep(waitTime);
-						subMon.worked(1);
-					} catch (InterruptedException e) {
-						/* ignore */
-					}
-					bOutputAvailable = (getInputStream().available() != 0);
-					bProcessComplete = process.isCompleted();
-					bTimedOut = System.currentTimeMillis() > endTime;
-					bCanceled = subMon.isCanceled();
-				} while (!bOutputAvailable && !bProcessComplete && !bTimedOut && !bCanceled);
+			// Wait on command to produce stdout output
+			long endTime = System.currentTimeMillis() + timeout;
+			boolean bOutputAvailable, bProcessComplete, bTimedOut, bCanceled;
+			do {
+				try {
+					Thread.sleep(waitTime);
+					subMon.worked(1);
+				} catch (InterruptedException e) {
+					/* ignore */
+				}
+				bOutputAvailable = (getInputStream().available() != 0);
+				bProcessComplete = process.isCompleted();
+				bTimedOut = System.currentTimeMillis() > endTime;
+				bCanceled = subMon.isCanceled();
+			} while (!bOutputAvailable && !bProcessComplete && !bTimedOut && !bCanceled);
 
-				// If no output was produced before process died, throw an exception with the stderr output
-				final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-				if (getInputStream().available() == 0 || process.isCompleted()) {
-					String msg = ""; //$NON-NLS-1$
-					while (bufferedReader.ready()) {
-						msg += (char) bufferedReader.read();
-					}
-					msg = msg.trim();
+			// If no output was produced before process died, throw an exception with the stderr output
+			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			if (getInputStream().available() == 0 || process.isCompleted()) {
+				String msg = ""; //$NON-NLS-1$
+				while (bufferedReader.ready()) {
+					msg += (char) bufferedReader.read();
+				}
+				msg = msg.trim();
 
-					if (!process.isCompleted()) {
-						process.destroy();
-					}
-
-					String cause = Messages.JSchConnectionProxyFactory_failed;
-					if (bTimedOut) {
-						cause = Messages.JSchConnectionProxyFactory_timedOut;
-					} else if (bCanceled) {
-						cause = Messages.JSchConnectionProxyFactory_wasCanceled;
-					}
-					throw new IOException(MessageFormat.format(Messages.JSchConnectionProxyFactory_ProxyCommandFailed, command,
-							cause, msg));
+				if (!process.isCompleted()) {
+					process.destroy();
 				}
 
-				// Dump the stderr to log
-				new Thread() {
-					@Override
-					public void run() {
-						final ILog log = Activator.getDefault().getLog();
-						String line;
-						try {
-							while ((line = bufferedReader.readLine()) != null) {
-								log.log(new Status(IStatus.INFO, Activator.getUniqueIdentifier(), IStatus.OK, line, null));
-							}
-						} catch (IOException e) {
-							Activator.log(e);
-						}
-					};
-				}.start();
-			} finally {
-				connectCalled = true;
+				String cause = Messages.JSchConnectionProxyFactory_failed;
+				if (bTimedOut) {
+					cause = Messages.JSchConnectionProxyFactory_timedOut;
+				} else if (bCanceled) {
+					cause = Messages.JSchConnectionProxyFactory_wasCanceled;
+				}
+				throw new IOException(MessageFormat.format(Messages.JSchConnectionProxyFactory_ProxyCommandFailed, command,
+						cause, msg));
 			}
+
+			// Dump the stderr to log
+			new Thread() {
+				@Override
+				public void run() {
+					final ILog log = Activator.getDefault().getLog();
+					String line;
+					try {
+						while ((line = bufferedReader.readLine()) != null) {
+							log.log(new Status(IStatus.INFO, Activator.getUniqueIdentifier(), IStatus.OK, line, null));
+						}
+					} catch (IOException e) {
+						Activator.log(e);
+					}
+				};
+			}.start();
 		}
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getInputStream()
 		 */
 		@Override
@@ -179,7 +179,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getOutputStream()
 		 */
 		@Override
@@ -189,7 +189,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getSocket()
 		 */
 		@Override
@@ -214,7 +214,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#close()
 		 */
 		@Override
@@ -224,7 +224,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#connect(com.jcraft.jsch.SocketFactory, java.lang.String, int, int)
 		 */
 		@Override
@@ -246,7 +246,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getInputStream()
 		 */
 		@Override
@@ -261,7 +261,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getOutputStream()
 		 */
 		@Override
@@ -276,7 +276,7 @@ public class JSchConnectionProxyFactory {
 
 		/*
 		 * (non-Javadoc)
-		 * 
+		 *
 		 * @see com.jcraft.jsch.Proxy#getSocket()
 		 */
 		@Override

bundles/org.eclipse.remote.jsch.core/src/org/eclipse/remote/internal/jsch/core/JSchProcessBuilder.java
@@ -91,7 +91,7 @@ public class JSchProcessBuilder extends AbstractRemoteProcessBuilder {
 
 	@Override
 	public IRemoteProcess start(int flags) throws IOException {
-		if (!fConnection.isOpen()) {
+		if (!fConnection.hasOpenSession()) {
 			throw new IOException(Messages.JSchProcessBuilder_Connection_is_not_open);
 		}
 
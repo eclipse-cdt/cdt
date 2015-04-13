9cc312e1fef3e8db9e818a6002eeca25b587a5bd
Marc Dumais <marc.dumais@ericsson.com>
2015-04-13 11:02:14 -0400

    Bug 462353 - [visualizer] Add support for persistent List<T> and Map<String,T> parameters in PersistentSettingsManager
    Change-Id: I77fc411e7484e41a9b66bf388a4120bc51a78549

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.multicorevisualizer.ui.tests/src/org/eclipse/cdt/dsf/gdb/multicorevisualizer/ui/test/AllTests.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Ericsson AB and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Marc Dumais (Ericsson) - Initial Implementation
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.gdb.multicorevisualizer.ui.test;
+
+import org.eclipse.cdt.dsf.gdb.multicorevisualizer.ui.test.PersistentSettingsManagerTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+
+// Add additional test case classes below
+@SuiteClasses({
+    PersistentSettingsManagerTest.class,
+})	
+public class AllTests {
+	// Often overriding BeforeClass method here
+}

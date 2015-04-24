99a53e782b1b90a45387dd5725ca4cae90441061
Jeff Johnston <jjohnstn@redhat.com>
2015-04-24 19:38:08 -0400

    Add Docker launching support
    - use the new docker tooling plug-ins to launch and debug
      CDT C/C++ applications in docker containers
    
    Change-Id: I30689255a3443ce6d49f937f5e2506d86452915b

---
+++ launch/org.eclipse.cdt.docker.launcher/src/org/eclipse/cdt/internal/docker/launcher/PreferenceConstants.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - Initial Contribution
+ *******************************************************************************/
+package org.eclipse.cdt.internal.docker.launcher;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String DEFAULT_IMAGE = "defaultImageForLaunch"; //$NON-NLS-1$
+	public static final String KEEP_CONTAINER_AFTER_LAUNCH = "keepContainerAfterLaunch"; //$NON-NLS-1$
+
+}

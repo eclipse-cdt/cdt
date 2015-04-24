fd11365bf3b7f40376b63a092f3b0a25441929d9
Simon Marchi <simon.marchi@polymtl.ca>
2015-04-24 13:36:27 -0400

    [dsf-gdb] debug tests: Use line tags where possible
    The goal is to eliminate direct source line number references in tests,
    so that modifying test cases is easier.
    
    Change-Id: I9a4964478e9486bff137fb9aec199cee144c2af4
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/MIBreakpointsTest.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/MIBreakpointsTest.java
@@ -134,13 +134,24 @@ public class MIBreakpointsTest extends BaseParametrizedTestCase	 {
 	protected final String EXPRESSION_TAG = MIBreakpoints.EXPRESSION;
 	protected final String READ_TAG = MIBreakpoints.READ;
 	protected final String WRITE_TAG = MIBreakpoints.WRITE;
+
 	// Target application 'special' locations
-	protected final int LINE_NUMBER_1 = 20;
-	protected final int LINE_NUMBER_2 = 21;
-	protected final int LINE_NUMBER_3 = 27;
-	protected final int LINE_NUMBER_4 = 36;
-	protected final int LINE_NUMBER_5 = 49;
-	protected final int LINE_NUMBER_6 = 50;
+	private static final String[] LINE_TAGS = new String[] {
+		"LINE_NUMBER_1",
+		"LINE_NUMBER_2",
+		"LINE_NUMBER_3",
+		"LINE_NUMBER_4",
+		"LINE_NUMBER_5",
+		"LINE_NUMBER_6",
+	};
+
+	private int LINE_NUMBER_1;
+	private int LINE_NUMBER_2;
+	private int LINE_NUMBER_3;
+	private int LINE_NUMBER_4;
+	private int LINE_NUMBER_5;
+	private int LINE_NUMBER_6;
+
 	protected final String FUNCTION = "zeroBlocks";
 	protected final String SIGNED_FUNCTION = "zeroBlocks(int)";
 	protected final String NO_CONDITION = "";
@@ -200,6 +211,16 @@ public class MIBreakpointsTest extends BaseParametrizedTestCase	 {
 		IContainerDMContext containerDmc = SyncUtil.getContainerContext();
 		fBreakpointsDmc = DMContexts.getAncestorOfType(containerDmc, IBreakpointsTargetDMContext.class);
 		assert (fBreakpointsDmc != null);
+
+		// Look up line tags in source file.
+		resolveLineTagLocations(SOURCE_NAME, LINE_TAGS);
+
+		LINE_NUMBER_1 = getLineForTag("LINE_NUMBER_1");
+		LINE_NUMBER_2 = getLineForTag("LINE_NUMBER_2");
+		LINE_NUMBER_3 = getLineForTag("LINE_NUMBER_3");
+		LINE_NUMBER_4 = getLineForTag("LINE_NUMBER_4");
+		LINE_NUMBER_5 = getLineForTag("LINE_NUMBER_5");
+		LINE_NUMBER_6 = getLineForTag("LINE_NUMBER_6");
 	}
 
 	@Override

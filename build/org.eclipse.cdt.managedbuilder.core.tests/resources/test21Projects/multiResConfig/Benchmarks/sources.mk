8341a624df165a2a2009805c88b75989a79d949a
Marc Khouzam <marc.khouzam@ericsson.com>
2015-05-06 15:42:15 -0400

    Change output to fix failing tests.
    Change-Id: I3331649cf516adbe3b453b11f1ed6733a007ce4d

--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test21Projects/multiResConfig/Benchmarks/sources.mk
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test21Projects/multiResConfig/Benchmarks/sources.mk
@@ -19,8 +19,8 @@ C_UPPER_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-source2/source21 \
-source2 \
-source1 \
 . \
+source1 \
+source2 \
+source2/source21 \
 
--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/test with spaces/Benchmarks/sub folder with spaces/foo with spaces.d
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/test with spaces/Benchmarks/sub folder with spaces/foo with spaces.d
@@ -1,3 +1,2 @@
-sub\ folder\ with\ spaces/foo\ with\ spaces.d \
-  sub\ folder\ with\ spaces/foo\ with\ spaces.o:  \
+sub\ folder\ with\ spaces/foo\ with\ spaces.d: \
  ../sub\ folder\ with\ spaces/foo\ with\ spaces.c

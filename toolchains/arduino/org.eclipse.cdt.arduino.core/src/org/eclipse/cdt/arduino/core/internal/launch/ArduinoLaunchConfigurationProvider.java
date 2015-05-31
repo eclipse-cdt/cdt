4134d5ee0d6d4d849744a7a9dad8bad5fa4010e8
Doug Schaefer <dschaefer@qnx.com>
2015-05-30 22:51:14 -0400

    Adapt Arduino plug-ins to final LaunchBar API.
    Change-Id: Ib5d7738ad137566b7cf6f9b8c177f72c747b2d2b

--- toolchains/arduino/org.eclipse.cdt.arduino.core/src/org/eclipse/cdt/arduino/core/internal/launch/ArduinoLaunchConfigurationProvider.java
+++ toolchains/arduino/org.eclipse.cdt.arduino.core/src/org/eclipse/cdt/arduino/core/internal/launch/ArduinoLaunchConfigurationProvider.java
@@ -10,16 +10,20 @@
  *******************************************************************************/
 package org.eclipse.cdt.arduino.core.internal.launch;
 
+import org.eclipse.cdt.arduino.core.internal.Activator;
 import org.eclipse.cdt.arduino.core.internal.ArduinoProjectNature;
 import org.eclipse.cdt.arduino.core.internal.remote.ArduinoRemoteConnection;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.launchbar.core.ILaunchDescriptor;
 import org.eclipse.launchbar.core.ProjectPerTargetLaunchConfigProvider;
 import org.eclipse.remote.core.IRemoteConnection;
+import org.eclipse.remote.core.IRemoteConnectionType;
+import org.eclipse.remote.core.IRemoteServicesManager;
 
 public class ArduinoLaunchConfigurationProvider extends ProjectPerTargetLaunchConfigProvider {
 
@@ -36,7 +40,7 @@ public class ArduinoLaunchConfigurationProvider extends ProjectPerTargetLaunchCo
 			return false;
 		}
 
-		if (!target.getConnectionType().getId().equals(ArduinoRemoteConnection.TYPE_ID)) {
+		if (target != null && !target.getConnectionType().getId().equals(ArduinoRemoteConnection.TYPE_ID)) {
 			return false;
 		}
 
@@ -49,7 +53,25 @@ public class ArduinoLaunchConfigurationProvider extends ProjectPerTargetLaunchCo
 	protected void populateLaunchConfiguration(ILaunchDescriptor descriptor, IRemoteConnection target,
 			ILaunchConfigurationWorkingCopy workingCopy) throws CoreException {
 		super.populateLaunchConfiguration(descriptor, target, workingCopy);
-		workingCopy.setAttribute(ArduinoLaunchConfigurationDelegate.CONNECTION_NAME, target.getName());
+		if (target != null) {
+			workingCopy.setAttribute(ArduinoLaunchConfigurationDelegate.CONNECTION_NAME, target.getName());
+		}
+	}
+
+	@Override
+	protected IRemoteConnection getLaunchTarget(ILaunchConfiguration configuration) throws CoreException {
+		String name = configuration.getAttribute(ArduinoLaunchConfigurationDelegate.CONNECTION_NAME, ""); //$NON-NLS-1$
+		if (name.isEmpty()) {
+			return null;
+		}
+		IRemoteServicesManager manager = Activator.getService(IRemoteServicesManager.class);
+		IRemoteConnectionType type = manager.getConnectionType(ArduinoRemoteConnection.TYPE_ID);
+		return type.getConnection(name);
+	}
+
+	@Override
+	protected boolean providesForNullTarget() {
+		return true;
 	}
 
 }

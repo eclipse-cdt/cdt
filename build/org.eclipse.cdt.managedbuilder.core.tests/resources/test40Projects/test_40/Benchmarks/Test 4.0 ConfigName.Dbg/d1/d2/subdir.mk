654fabae006d766fd38729e629588fe5c88b6190
ocagdas <ocagdas@yahoo.com>
2015-06-03 11:32:36 +0100

    Bugzilla 468417 The '.d' files that are generated by CDT, contain 'file_name.d: file_name.cpp file_name.h' type of target prerequisite relations.
    However, to get benefit of the dependency mechanism, this should be
    something like 'file_name.o: file_name.cpp file_name.h'.
    
    Therefore, we need to change ''options[3] = "-MT\"$(@:%.o=%.d)\"";'' as
    'options[3] = "-MT\"$(@)\"";'
    
    Also update the test cases.
    
    Change-Id: Iee829a8564e1b134905ee06a8a4b76a1e3b30ecc
    Signed-off-by: ocagdas <ocagdas@yahoo.com>

--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test40Projects/test_40/Benchmarks/Test 4.0 ConfigName.Dbg/d1/d2/subdir.mk
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test40Projects/test_40/Benchmarks/Test 4.0 ConfigName.Dbg/d1/d2/subdir.mk
@@ -27,7 +27,7 @@ d1/d2/e.o: ../d1/d2/e.cpp
 d1/d2/%.o: ../d1/d2/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Test 4.0 ToolName.compiler.gnu.cpp'
-	g++ -Id2_rel/path -I../d2_proj/rel/path -I/d2_abs/path -Ic:/d2_abs/path -Id1_rel/path -I../d1_proj/rel/path -I/d1_abs/path -Ic:/d1_abs/path -Irel/path -I../proj/rel/path -I/abs/path -Ic:/abs/path -I"${WorkspaceDirPath}/test_40/dir1/dir2/dir3" -I"${WorkspaceDirPath}/test_40" -I"D:\docs\incs" -I"D:\d1_docs\incs" -I"D:\d2_docs\incs" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -Id2_rel/path -I../d2_proj/rel/path -I/d2_abs/path -Ic:/d2_abs/path -Id1_rel/path -I../d1_proj/rel/path -I/d1_abs/path -Ic:/d1_abs/path -Irel/path -I../proj/rel/path -I/abs/path -Ic:/abs/path -I"${WorkspaceDirPath}/test_40/dir1/dir2/dir3" -I"${WorkspaceDirPath}/test_40" -I"D:\docs\incs" -I"D:\d1_docs\incs" -I"D:\d2_docs\incs" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

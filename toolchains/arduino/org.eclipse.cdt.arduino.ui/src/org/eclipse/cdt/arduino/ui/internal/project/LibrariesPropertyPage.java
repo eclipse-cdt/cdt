b9cc82f6debe17f28ddca1a9cb0ded12c800d08a
Doug Schaefer <dschaefer@qnx.com>
2015-08-22 22:26:30 -0400

    Start of Arduino Library support.
    Includes metamodel from index file and project property page to
    select. Nothing behind the selection though.
    
    Change-Id: Ifdc9075beba4002b1481576d07ed2d8d44e5c2da

---
+++ toolchains/arduino/org.eclipse.cdt.arduino.ui/src/org/eclipse/cdt/arduino/ui/internal/project/LibrariesPropertyPage.java
@@ -0,0 +1,149 @@
+package org.eclipse.cdt.arduino.ui.internal.project;
+
+import org.eclipse.cdt.arduino.core.internal.board.ArduinoLibrary;
+import org.eclipse.cdt.arduino.core.internal.board.ArduinoManager;
+import org.eclipse.cdt.arduino.core.internal.board.LibraryIndex;
+import org.eclipse.cdt.arduino.ui.internal.Activator;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.BaseLabelProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.ui.dialogs.FilteredTree;
+import org.eclipse.ui.dialogs.PatternFilter;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class LibrariesPropertyPage extends PropertyPage {
+
+	private static class ContentProvider implements ITreeContentProvider {
+		private LibraryIndex index;
+
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			index = (LibraryIndex) newInput;
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			if (element instanceof LibraryIndex) {
+				return !index.getCategories().isEmpty();
+			} else if (element instanceof String) { // category
+				return !index.getLibraries((String) element).isEmpty();
+			} else if (element instanceof ArduinoLibrary) {
+				return false;
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			if (element instanceof ArduinoLibrary) {
+				return ((ArduinoLibrary) element).getCategory();
+			} else if (element instanceof String) {
+				return index;
+			} else {
+				return null;
+			}
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			return ((LibraryIndex) inputElement).getCategories().toArray(new String[0]);
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			if (parentElement instanceof String) {
+				return index.getLibraries((String) parentElement).toArray(new ArduinoLibrary[0]);
+			} else {
+				return null;
+			}
+		}
+	}
+
+	private static class LabelProvider extends BaseLabelProvider implements ITableLabelProvider {
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		@Override
+		public String getColumnText(Object element, int columnIndex) {
+			if (element instanceof String) {
+				return columnIndex == 0 ? (String) element : null;
+			} else if (element instanceof ArduinoLibrary) {
+				switch (columnIndex) {
+				case 0:
+					return ((ArduinoLibrary) element).getName();
+				case 1:
+					return ((ArduinoLibrary) element).getSentence();
+				default:
+					return null;
+				}
+			} else {
+				return null;
+			}
+		}
+
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite comp = new Composite(parent, SWT.NULL);
+		comp.setLayout(new GridLayout());
+
+		FilteredTree filteredTree = new FilteredTree(comp, SWT.CHECK | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL,
+				new PatternFilter() {
+					@Override
+					protected boolean isLeafMatch(Viewer viewer, Object element) {
+						if (element instanceof String) {
+							return wordMatches((String) element);
+						} else if (element instanceof ArduinoLibrary) {
+							ArduinoLibrary lib = (ArduinoLibrary) element;
+							return wordMatches(lib.getName()) || wordMatches(lib.getSentence())
+									|| wordMatches(lib.getParagraph());
+						} else {
+							return false;
+						}
+					}
+				}, true);
+		filteredTree.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		TreeViewer viewer = filteredTree.getViewer();
+
+		Tree tree = viewer.getTree();
+		tree.setHeaderVisible(true);
+		TreeColumn column1 = new TreeColumn(tree, SWT.LEFT);
+		column1.setText("Name");
+		column1.setWidth(200);
+		TreeColumn column2 = new TreeColumn(tree, SWT.LEFT);
+		column2.setText("Description");
+		column2.setWidth(200);
+
+		viewer.setContentProvider(new ContentProvider());
+		viewer.setLabelProvider(new LabelProvider());
+
+		try {
+			viewer.setInput(ArduinoManager.instance.getLibraryIndex());
+		} catch (CoreException e) {
+			Activator.log(e);
+		}
+
+		return comp;
+	}
+
+}

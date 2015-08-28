537f3cdf3bd98469ef0c12b7fccd4273119b8a3e
Tim Martin <tim@asymptotic.co.uk>
2015-08-28 18:44:21 +0100

    Bug 377046. Fixed a bogus codan "no return" warning
    Change-Id: I68784cef1cad944a1fd47b343f84c0bfd3837481
    Signed-off-by: Tim Martin <tim@asymptotic.co.uk>

--- codan/org.eclipse.cdt.codan.core.cxx/src/org/eclipse/cdt/codan/core/cxx/internal/model/cfg/ControlFlowGraphBuilder.java
+++ codan/org.eclipse.cdt.codan.core.cxx/src/org/eclipse/cdt/codan/core/cxx/internal/model/cfg/ControlFlowGraphBuilder.java
@@ -560,10 +560,10 @@ public class ControlFlowGraphBuilder {
 		if (prev instanceof IDecisionNode) {
 			if (node instanceof IBranchNode) {
 				IDecisionNode decisionNode = (IDecisionNode) prev;
-				if (isConstant(decisionNode, 1) && ((IBranchNode) node).getLabel().equals(IBranchNode.ELSE)) {
+				if (isConstant(decisionNode, true) && ((IBranchNode) node).getLabel().equals(IBranchNode.ELSE)) {
 					dead.add(node);
 					return;
-				} else if (isConstant(decisionNode, 0) && ((IBranchNode) node).getLabel().equals(IBranchNode.THEN)) {
+				} else if (isConstant(decisionNode, false) && ((IBranchNode) node).getLabel().equals(IBranchNode.THEN)) {
 					dead.add(node);
 					return;
 				}
@@ -577,7 +577,7 @@ public class ControlFlowGraphBuilder {
 			((AbstractBasicBlock) node).addIncoming(prev);
 	}
 
-	private boolean isConstant(IDecisionNode node, long testvalue) {
+	private boolean isConstant(IDecisionNode node, boolean testvalue) {
 		if (node instanceof ICfgData) {
 			IASTNode ast = (IASTNode) ((ICfgData) node).getData();
 			if (ast instanceof IASTExpression) {
@@ -585,7 +585,11 @@ public class ControlFlowGraphBuilder {
 				Long numericalValue = dvalue.numericalValue();
 				if (numericalValue == null)
 					return false;
-				return numericalValue == testvalue;
+				return (numericalValue != 0) == testvalue;
+			} else if (ast == null) {
+				// Empty decision nodes are regarded as true. This gives the correct
+				// interpretation of the empty condition in for(;;){...}
+				return testvalue;
 			}
 		}
 		return false;

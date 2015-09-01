a2e0c459bdf77ce07ba4ea4a6c67f09e2e2d3a2f
Doug Schaefer <dschaefer@qnx.com>
2015-08-31 21:35:05 -0400

    Add description text for board and library install pages.
    Change-Id: I6354ab5cc54e16df3ffe0d27fae0eec4158d9546

--- toolchains/arduino/org.eclipse.cdt.arduino.ui/src/org/eclipse/cdt/arduino/ui/internal/project/LibrariesPropertyPage.java
+++ toolchains/arduino/org.eclipse.cdt.arduino.ui/src/org/eclipse/cdt/arduino/ui/internal/project/LibrariesPropertyPage.java
@@ -8,6 +8,7 @@ import org.eclipse.cdt.arduino.core.internal.board.ArduinoLibrary;
 import org.eclipse.cdt.arduino.core.internal.board.ArduinoManager;
 import org.eclipse.cdt.arduino.core.internal.board.LibraryIndex;
 import org.eclipse.cdt.arduino.ui.internal.Activator;
+import org.eclipse.cdt.arduino.ui.internal.Messages;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.BaseLabelProvider;
@@ -21,6 +22,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
@@ -115,6 +117,13 @@ public class LibrariesPropertyPage extends PropertyPage {
 		Composite comp = new Composite(parent, SWT.NULL);
 		comp.setLayout(new GridLayout());
 
+		Text desc = new Text(comp, SWT.READ_ONLY | SWT.WRAP);
+		GridData layoutData = new GridData(SWT.LEFT, SWT.FILL, true, false);
+		layoutData.widthHint = 500;
+		desc.setLayoutData(layoutData);
+		desc.setBackground(parent.getBackground());
+		desc.setText(Messages.LibrariesPropertyPage_desc);
+
 		filteredTree = new FilteredTree(comp, SWT.CHECK | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL,
 				new PatternFilter() {
 					@Override
@@ -135,7 +144,7 @@ public class LibrariesPropertyPage extends PropertyPage {
 				return new ContainerCheckedTreeViewer(parent, style);
 			}
 		};
-		filteredTree.setLayoutData(new GridData(GridData.FILL_BOTH));
+		filteredTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
 		ContainerCheckedTreeViewer viewer = (ContainerCheckedTreeViewer) filteredTree.getViewer();
 

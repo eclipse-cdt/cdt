23c784bcfee985f2f814033b6d5711cab5c80d8d
Marc Khouzam <marc.khouzam@ericsson.com>
2015-09-02 10:47:27 -0400

    Bug 476432 - GDB version comparison fails for GDB 7.10
    Change-Id: I51366e45deabda29a1a5b00166039bad14a146f8
    Signed-off-by: Marc Khouzam <marc.khouzam@ericsson.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.tests/src/org/eclipse/cdt/dsf/gdb/tests/LaunchUtilsTest.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.tests/src/org/eclipse/cdt/dsf/gdb/tests/LaunchUtilsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2014 Ericsson and others.
+ * Copyright (c) 2010, 2015 Ericsson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,16 +13,31 @@ package org.eclipse.cdt.dsf.gdb.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.cdt.dsf.gdb.launching.LaunchUtils;
+import org.eclipse.cdt.dsf.gdb.service.GdbDebugServicesFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LaunchUtilsTest {
 	
+	private class Versions {
+		private String version1;
+		private String version2;
+		private int expectedResult;
+		
+		public Versions(String v1, String v2, int result) {
+			version1 = v1;
+			version2 = v2;
+			expectedResult = result;
+		}
+	}
+
     @Before
     public void init() {
     }
@@ -32,8 +47,8 @@ public class LaunchUtilsTest {
     }
 	
     @Test
-	public void tesetGDBVersionFromText() {
-    	Map<String, String> versions = new HashMap<String, String>(10);
+	public void tesGDBVersionFromText() {
+    	Map<String, String> versions = new HashMap<>(10);
 
     	versions.put("GNU gdb 6.8.50.20080730", "6.8.50.20080730");
     	versions.put("GNU gdb (GDB) 6.8.50.20080730-cvs", "6.8.50.20080730");
@@ -59,6 +74,120 @@ public class LaunchUtilsTest {
 
     	for (String key : versions.keySet()) {
     		assertEquals("From \"" + key + "\"", versions.get(key), LaunchUtils.getGDBVersionFromText(key));
+    	}
+    }
+    
+    /**
+     * Verify that GDB version comparison is done properly.
+     */
+    @Test
+    public void testGDBVersionComparison() {
+    	List<Versions> versions = new ArrayList<>(10);
+
+    	versions.add(new Versions("7",		"6",		 1));
+    	versions.add(new Versions("7",		"6.1",		 1));
+    	versions.add(new Versions("7",		"6.1.1",	 1));
+    	versions.add(new Versions("7",		"7",		 0));
+    	versions.add(new Versions("7",		"7.0",		 0));
+    	versions.add(new Versions("7",		"7.0.0",	 0));
+    	versions.add(new Versions("7",		"7.1",		-1));
+    	versions.add(new Versions("7",		"7.1.1",	-1));
+    	versions.add(new Versions("7",		"8",		-1));
+    	versions.add(new Versions("7",		"8.0",		-1));
+    	versions.add(new Versions("7",		"8.1",		-1));
+    	versions.add(new Versions("7",		"8.1.1",	-1));
+    	versions.add(new Versions("7",		"10",		-1));
+    	versions.add(new Versions("7",		"10.0",		-1));
+    	versions.add(new Versions("7",		"10.1",		-1));
+    	versions.add(new Versions("7",		"10.1.1", 	-1));
+    	versions.add(new Versions("7",		"70",	 	-1));
+    	versions.add(new Versions("7",		"70.1",	 	-1));
+    	versions.add(new Versions("7",		"70.1.1", 	-1));
+    	versions.add(new Versions("7",		"72",	 	-1));
+    	versions.add(new Versions("7",		"72.1",	 	-1));
+    	versions.add(new Versions("7",		"72.1.1", 	-1));
+
+    	versions.add(new Versions("7.3",	"6",	  	 1));
+    	versions.add(new Versions("7.3",	"6.4",	  	 1));
+    	versions.add(new Versions("7.3",	"6.4.4", 	 1));
+    	versions.add(new Versions("7.3",	"7",	  	 1));
+    	versions.add(new Versions("7.3",	"7.0",	  	 1));
+    	versions.add(new Versions("7.3",	"7.3",	  	 0));
+    	versions.add(new Versions("7.3",	"7.3.0", 	 0));
+    	versions.add(new Versions("7.0",	"7.0",  	 0));
+    	versions.add(new Versions("7.3",	"7.3.3",	-1));
+    	versions.add(new Versions("7.3",	"7.30", 	-1));
+    	versions.add(new Versions("7.3",	"7.30.3",	-1));
+    	versions.add(new Versions("7.3",	"8",	 	-1));
+    	versions.add(new Versions("7.3",	"8.0",	 	-1));
+    	versions.add(new Versions("7.3",	"8.1",	 	-1));
+    	versions.add(new Versions("7.3",	"8.1.1",	-1));
+    	versions.add(new Versions("7.3",	"8.4",	 	-1));
+    	versions.add(new Versions("7.3",	"8.4.4",	-1));
+    	versions.add(new Versions("7.3",	"10",	 	-1));
+    	versions.add(new Versions("7.3",	"10.0", 	-1));
+    	versions.add(new Versions("7.3",	"10.1", 	-1));
+    	versions.add(new Versions("7.3",	"10.1.1",	-1));
+    	versions.add(new Versions("7.3",	"10.4", 	-1));
+    	versions.add(new Versions("7.3",	"10.4.4",	-1));
+    	versions.add(new Versions("7.3",	"70",	 	-1));
+    	versions.add(new Versions("7.3",	"70.1", 	-1));
+    	versions.add(new Versions("7.3",	"70.1.1",	-1));
+    	versions.add(new Versions("7.3",	"72",	 	-1));
+    	versions.add(new Versions("7.3",	"72.1", 	-1));
+    	versions.add(new Versions("7.3",	"72.1.1",	-1));
+
+    	versions.add(new Versions("7.5.4",	"6",	  	 1));
+    	versions.add(new Versions("7.5.4",	"6.4",  	 1));
+    	versions.add(new Versions("7.5.4",	"6.4.4", 	 1));
+    	versions.add(new Versions("7.5.4",	"6.5",	 	 1));
+    	versions.add(new Versions("7.5.4",	"6.5.4", 	 1));
+    	versions.add(new Versions("7.5.4",	"7",	  	 1));
+    	versions.add(new Versions("7.5.4",	"7.0",	  	 1));
+    	versions.add(new Versions("7.5.4",	"7.3",	  	 1));
+    	versions.add(new Versions("7.5.4",	"7.3.0", 	 1));
+    	versions.add(new Versions("7.5.4",	"7.5",	 	 1));
+    	versions.add(new Versions("7.5.4",	"7.5.3",	 1));
+    	versions.add(new Versions("7.5.4",	"7.5.4",	 0));
+    	versions.add(new Versions("7.0.0",	"7.0.0", 	 0));
+    	versions.add(new Versions("7.5.4",	"7.7",	 	-1));
+    	versions.add(new Versions("7.5.4",	"7.7.6",	-1));
+    	versions.add(new Versions("7.5.4",	"7.50", 	-1));
+    	versions.add(new Versions("7.5.4",	"7.50.3", 	-1));
+    	versions.add(new Versions("7.5.4",	"7.50.4",	-1));
+    	versions.add(new Versions("7.5.4",	"8",	 	-1));
+    	versions.add(new Versions("7.5.4",	"8.0",	 	-1));
+    	versions.add(new Versions("7.5.4",	"8.1",	 	-1));
+    	versions.add(new Versions("7.5.4",	"8.1.1",	-1));
+    	versions.add(new Versions("7.5.4",	"8.5",	 	-1));
+    	versions.add(new Versions("7.5.4",	"8.5.4",	-1));
+    	versions.add(new Versions("7.5.4",	"10",	 	-1));
+    	versions.add(new Versions("7.5.4",	"10.0", 	-1));
+    	versions.add(new Versions("7.5.4",	"10.1", 	-1));
+    	versions.add(new Versions("7.5.4",	"10.1.1",	-1));
+    	versions.add(new Versions("7.5.4",	"10.5", 	-1));
+    	versions.add(new Versions("7.5.4",	"10.5.4",	-1));
+    	versions.add(new Versions("7.5.4",	"10.7.4",	-1));
+    	versions.add(new Versions("7.5.4",	"7.10",		-1));
+    	versions.add(new Versions("7.5.4",	"7.10.1",	-1));
+    	versions.add(new Versions("7.5.4",	"7.10.5",	-1));
+    	versions.add(new Versions("7.5.4",	"72",		-1));
+    	versions.add(new Versions("7.5.4",	"72.1",		-1));
+    	versions.add(new Versions("7.5.4",	"72.1.1",	-1));
+    	
+    	versions.add(new Versions("6.8.51",	"6.8.50.20080730",	1));
+    	versions.add(new Versions("6.9.4",	"6.8.50.20080730",	1));
+    	versions.add(new Versions("6.8.50.20080730", "6.8.50.20080730",	0));
+    	versions.add(new Versions("6.5.4",	"6.8.50.20080730",	-1));
+    	versions.add(new Versions("6.8.50",	"6.8.50.20080730",	-1));
+
+    	for (Versions v : versions) {
+    		assertEquals("Comparing " + v.version1 + " and " + v.version2,
+    				v.expectedResult,
+    				GdbDebugServicesFactory.compareVersions(v.version1, v.version2));
+    		assertEquals("Comparing " + v.version2 + " and " + v.version1,
+    				-v.expectedResult,
+    		        GdbDebugServicesFactory.compareVersions(v.version2, v.version1));
     	}
     }
 }

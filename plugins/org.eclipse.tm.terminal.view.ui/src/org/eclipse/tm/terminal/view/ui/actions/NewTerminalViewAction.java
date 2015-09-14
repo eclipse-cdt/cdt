1a1bcf774bf3709fbbabd9ab2b50c6f1f5492b0c
Uwe Stieber <uwe.stieber@windriver.com>
2015-09-14 08:06:44 +0200

    Improve "New Terminal View" handling.
    - Add a command to open the new terminal view. Allows to associate a
    short cut
    - Open the "Open Terminal" dialog on creating a new terminal view

--- plugins/org.eclipse.tm.terminal.view.ui/src/org/eclipse/tm/terminal/view/ui/actions/NewTerminalViewAction.java
+++ plugins/org.eclipse.tm.terminal.view.ui/src/org/eclipse/tm/terminal/view/ui/actions/NewTerminalViewAction.java
@@ -9,14 +9,21 @@
  *******************************************************************************/
 package org.eclipse.tm.terminal.view.ui.actions;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.tm.internal.terminal.control.actions.AbstractTerminalAction;
 import org.eclipse.tm.terminal.view.ui.activator.UIPlugin;
 import org.eclipse.tm.terminal.view.ui.interfaces.ITerminalsView;
-import org.eclipse.tm.terminal.view.ui.interfaces.IUIConstants;
 import org.eclipse.tm.terminal.view.ui.interfaces.ImageConsts;
-import org.eclipse.tm.terminal.view.ui.manager.ConsoleManager;
 import org.eclipse.tm.terminal.view.ui.nls.Messages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 
 /**
  * Opens a new terminal view with a new secondary view ID.
@@ -44,10 +51,27 @@ public class NewTerminalViewAction extends AbstractTerminalAction {
 	/* (non-Javadoc)
 	 * @see org.eclipse.tm.internal.terminal.control.actions.AbstractTerminalAction#run()
 	 */
-	@Override
+	@SuppressWarnings("cast")
+    @Override
 	public void run() {
-		String secondaryId = ConsoleManager.getInstance().getNextTerminalSecondaryId(IUIConstants.ID);
-		ConsoleManager.getInstance().showConsoleView(IUIConstants.ID, secondaryId);
+		ICommandService service = (ICommandService)PlatformUI.getWorkbench().getService(ICommandService.class);
+		Command command = service != null ? service.getCommand("org.eclipse.tm.terminal.view.ui.command.newview") : null; //$NON-NLS-1$
+		if (command != null && command.isDefined() && command.isEnabled()) {
+			try {
+				ParameterizedCommand pCmd = ParameterizedCommand.generateCommand(command, null);
+				Assert.isNotNull(pCmd);
+				IHandlerService handlerSvc = (IHandlerService)PlatformUI.getWorkbench().getService(IHandlerService.class);
+				Assert.isNotNull(handlerSvc);
+				handlerSvc.executeCommandInContext(pCmd, null, handlerSvc.getCurrentState());
+			} catch (Exception e) {
+				// If the platform is in debug mode, we print the exception to the log view
+				if (Platform.inDebugMode()) {
+					IStatus status = new Status(IStatus.ERROR, UIPlugin.getUniqueIdentifier(),
+												Messages.AbstractTriggerCommandHandler_error_executionFailed, e);
+					UIPlugin.getDefault().getLog().log(status);
+				}
+			}
+		}
 	}
 
 }

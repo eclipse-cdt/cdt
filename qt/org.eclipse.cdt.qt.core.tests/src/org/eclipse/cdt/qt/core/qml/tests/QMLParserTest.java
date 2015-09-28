dff3c83fdc5804905fd2b249e2328f53a7fb5f6a
Doug Schaefer <dschaefer@qnx.com>
2015-09-28 16:33:36 -0400

    Move QML stuff to qt.core plug-in. Add deps for ANTLR, FreeMarker.
    Moves to the cdt 4.6 platform where the new ANTLR and Freemarker deps
    will be. Also properly names the qt test plug-ins.
    
    Change-Id: I026873583c6e6ca7602cb983ab04f99acd700a4b

---
+++ qt/org.eclipse.cdt.qt.core.tests/src/org/eclipse/cdt/qt/core/qml/tests/QMLParserTest.java
@@ -0,0 +1,97 @@
+package org.eclipse.cdt.qt.core.qml.tests;
+
+import static org.junit.Assert.fail;
+
+import java.util.BitSet;
+
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.eclipse.cdt.internal.qt.core.qml.parser.QMLLexer;
+import org.eclipse.cdt.internal.qt.core.qml.parser.QMLListener;
+import org.eclipse.cdt.internal.qt.core.qml.parser.QMLParser;
+import org.eclipse.cdt.internal.qt.core.qml.parser.QMLParser.QmlProgramContext;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class QMLParserTest extends AbstractParserTest {
+
+	public void runParser(String code, QMLListener listener) throws Exception {
+		ANTLRInputStream input = new ANTLRInputStream(code);
+		QMLLexer lexer = new QMLLexer(input);
+		lexer.addErrorListener(new ANTLRErrorListener() {
+
+			@Override
+			public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
+					int charPositionInLine, String msg, RecognitionException e) {
+				fail(msg);
+			}
+
+			@Override
+			public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+					int prediction, ATNConfigSet configs) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+					BitSet conflictingAlts, ATNConfigSet configs) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
+					BitSet ambigAlts, ATNConfigSet configs) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		QMLParser parser = new QMLParser(tokens);
+		parser.addParseListener(listener);
+		parser.addErrorListener(new ANTLRErrorListener() {
+			@Override
+			public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
+					int charPositionInLine, String msg, RecognitionException e) {
+				fail(msg);
+			}
+
+			@Override
+			public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+					int prediction, ATNConfigSet configs) {
+			}
+
+			@Override
+			public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
+					BitSet conflictingAlts, ATNConfigSet configs) {
+			}
+
+			@Override
+			public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
+					BitSet ambigAlts, ATNConfigSet configs) {
+			}
+		});
+		parser.qmlProgram();
+	}
+
+	// testCode
+	@Test
+	@Ignore
+	public void testCodeExtract() throws Exception {
+		// see if you can use TestSourceReader out of the cdt.core.tests plugin
+		runParser(extract(), new AbstractQMLListener() {
+			@Override
+			public void exitQmlProgram(QmlProgramContext ctx) {
+
+			}
+		});
+	}
+
+}

f372228c3ee443c99de8b4f23730d64ba070829e
Marc Dumais <marc.dumais@ericsson.com>
2015-10-05 08:46:39 -0400

    bug 479039 - Move VisualizerVirtualBoundsGraphicObjectTest to org.eclipse.cdt.dsf.gdb.multicoreVisualizer.ui.tests
    Change-Id: Iceed10c7458aceee2560d6066ec052f85f2d9086

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.tests/src/org/eclipse/cdt/dsf/gdb/tests/AllTests.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.tests/src/org/eclipse/cdt/dsf/gdb/tests/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Ericsson AB and others.
+ * Copyright (c) 2014, 2015 Ericsson AB and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -27,7 +27,6 @@ import org.junit.runners.Suite.SuiteClasses;
     LaunchUtilsTest.class,
     MIStringHandlerTests.class,
     ProcStatParserTest.class,
-    VisualizerVirtualBoundsGraphicObjectTest.class,
 })	
 public class AllTests {
 	// Often overriding BeforeClass method here

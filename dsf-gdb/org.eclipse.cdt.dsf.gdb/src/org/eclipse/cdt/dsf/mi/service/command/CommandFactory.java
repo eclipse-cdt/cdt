e6ffa883ad58af2a05962bf7d373de690330d4f1
Marc Khouzam <marc.khouzam@ericsson.com>
2015-10-09 16:40:54 -0400

    Remove deprecated methods.
    The methods
    MIDataListRegisterValues.MIDataListRegisterValues(IMIExecutionDMContext,
    int) and
    MIDataListRegisterValues.MIDataListRegisterValues(IMIExecutionDMContext,
    int, int[]) have been deprecated for over two years and have been
    removed.  Callers should instead the methods of the same name but
    passing in an IFrameDMContext instead of an IMIExecutionDMContext.
    
    Following that change, the methods
    CommandFactory.MIDataListRegisterValues(IMIExecutionDMContext, int) and
    CommandFactory.MIDataListRegisterValues(IMIExecutionDMContext, int,
    int[]) have also been removed, and their corresponding methods using
    IFrameDMContext should be used instead.
    
    Change-Id: Ifa518b9a113dadb9d3dc44d803d8c3819afe810c

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/CommandFactory.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/CommandFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 QNX Software Systems and others.
+ * Copyright (c) 2000, 2016 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -466,16 +466,6 @@ public class CommandFactory {
 		return new MIDataListRegisterNames(ctx, regnos);
 	}
 
-	@Deprecated
-	public ICommand<MIDataListRegisterValuesInfo> createMIDataListRegisterValues(IMIExecutionDMContext ctx, int fmt) {
-		return new MIDataListRegisterValues(ctx, fmt);
-	}
-
-	@Deprecated
-	public ICommand<MIDataListRegisterValuesInfo> createMIDataListRegisterValues(IMIExecutionDMContext ctx, int fmt, int [] regnos) {
-		return new MIDataListRegisterValues(ctx, fmt, regnos);
-	}
-	
 	/**
 	 * @since 4.3
 	 */

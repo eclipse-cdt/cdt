b185265614b14fc703f91cd3fe31d7b78211d18f
Marc Khouzam <marc.khouzam@ericsson.com>
2015-10-13 10:46:49 -0400

    Remove deprecated constructors.
    The constructors MIFileExecAndSymbols(ICommandControlDMContext) and
    MIFileExecAndSymbols(ICommandControlDMContext, String) where removed.
    MIFileExecAndSymbols(IMIContainerDMContext) and
    MIFileExecAndSymbols(IMIContainerDMContext, String) should be used
    instead.
    
    Change-Id: I1c54599f930430c9640833420256691d01184e76

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/MIFileExecAndSymbols.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/MIFileExecAndSymbols.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Ericsson and others.
+ * Copyright (c) 2008, 2015 Ericsson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@
 
 package org.eclipse.cdt.dsf.mi.service.command.commands;
 
-import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlDMContext;
 import org.eclipse.cdt.dsf.mi.service.IMIContainerDMContext;
 import org.eclipse.cdt.dsf.mi.service.command.output.MIInfo;
 
@@ -27,26 +26,6 @@ import org.eclipse.cdt.dsf.mi.service.command.output.MIInfo;
  */
 public class MIFileExecAndSymbols extends MICommand<MIInfo>
 {
-    /**
-     * @since 1.1
-     * @deprecated Replaced with MIFileExecAndSymbols(IMIContainerDMContext, String)
-     * since this command is container-specific
-     */
-	@Deprecated
-    public MIFileExecAndSymbols(ICommandControlDMContext dmc, String file) {
-        super(dmc, "-file-exec-and-symbols", null, new String[] {file}); //$NON-NLS-1$
-    }
-   
-    /**
-     * @since 1.1
-     * @deprecated Replaced with  MIFileExecAndSymbols(IMIContainerDMContext)
-     * since this command is container-specific
-     */
-	@Deprecated
-    public MIFileExecAndSymbols(ICommandControlDMContext dmc) {
-        super(dmc, "-file-exec-and-symbols"); //$NON-NLS-1$
-    }
-    
     /** @since 4.0 */
     public MIFileExecAndSymbols(IMIContainerDMContext dmc) {
     	this(dmc, null);

971f286375da1979d90cfd266bf8ae60f5ce12ee
Marc Khouzam <marc.khouzam@ericsson.com>
2015-10-21 12:57:26 -0400

    Grouping feature
    Delta stuff works a little.
    Grouping service allows to group as expected (views must be refreshed
    manually sometimes).
    
    Ungroup does not work yet.
    Other services no longer need to be modified as we are using the same
    IDMContext for each debug view element.  This has the limitation that
    all elements must have the same children, so we cannot show a process
    with a subset of its threads.  But that may be ok, instead we could show
    a label in the thread node about its parent process.
    
    Change-Id: I0d3a6aa037d0a1fce3795dda3daba7d63f042bb8

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/service/GDBRunControl_7_0_NS.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/service/GDBRunControl_7_0_NS.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 Wind River Systems and others.
+ * Copyright (c) 2006, 2015 Wind River Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,6 +53,7 @@ import org.eclipse.cdt.dsf.debug.service.IBreakpoints.IBreakpointsTargetDMContex
 import org.eclipse.cdt.dsf.debug.service.IBreakpointsExtension.IBreakpointHitDMEvent;
 import org.eclipse.cdt.dsf.debug.service.ICachingService;
 import org.eclipse.cdt.dsf.debug.service.IMultiRunControl;
+import org.eclipse.cdt.dsf.debug.service.IProcesses;
 import org.eclipse.cdt.dsf.debug.service.IProcesses.IProcessDMContext;
 import org.eclipse.cdt.dsf.debug.service.IProcesses.IThreadDMContext;
 import org.eclipse.cdt.dsf.debug.service.IRunControl;
@@ -68,6 +69,7 @@ import org.eclipse.cdt.dsf.gdb.internal.GdbPlugin;
 import org.eclipse.cdt.dsf.gdb.internal.service.command.events.MITracepointSelectedEvent;
 import org.eclipse.cdt.dsf.gdb.internal.service.control.StepIntoSelectionActiveOperation;
 import org.eclipse.cdt.dsf.gdb.internal.service.control.StepIntoSelectionUtils;
+import org.eclipse.cdt.dsf.gdb.launching.IGdbGrouping;
 import org.eclipse.cdt.dsf.gdb.service.IGDBTraceControl.ITraceRecordSelectedChangedDMEvent;
 import org.eclipse.cdt.dsf.mi.service.IMIBreakpointPathAdjuster;
 import org.eclipse.cdt.dsf.mi.service.IMICommandControl;
@@ -1108,9 +1110,17 @@ public class GDBRunControl_7_0_NS extends AbstractDsfService implements IMIRunCo
 
 	@Override
 	public void getExecutionContexts(final IContainerDMContext containerDmc, final DataRequestMonitor<IExecutionDMContext[]> rm) {
-        IMIProcesses procService = getServicesTracker().getService(IMIProcesses.class);
+		// user groups support 
+		IGdbGrouping groupService = getServicesTracker().getService(IGdbGrouping.class);
+		if (groupService != null) {
+			groupService.getExecutionContexts(containerDmc, rm);
+			return;
+		}
+		// end user group support
+
+		IProcesses procService = getServicesTracker().getService(IProcesses.class);
 		procService.getProcessesBeingDebugged(
-				containerDmc,
+				containerDmc != null ? containerDmc : fConnection.getContext(),
 				new DataRequestMonitor<IDMContext[]>(getExecutor(), rm) {
 					@Override
 					protected void handleSuccess() {

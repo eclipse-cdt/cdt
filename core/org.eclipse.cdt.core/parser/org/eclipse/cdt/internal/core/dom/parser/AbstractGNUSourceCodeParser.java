3098d1679b375f5aade47509f0090aa400396058
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2015-10-28 18:01:03 -0700

    Bug 472950 - Fixed an infinite loop in content assist inside a body of a lambda.
    Change-Id: I5194571ba8a88e67e9cc897be463c5e833700000

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/AbstractGNUSourceCodeParser.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/AbstractGNUSourceCodeParser.java
@@ -2674,6 +2674,9 @@ public abstract class AbstractGNUSourceCodeParser implements ISourceCodeParser {
 		while (true) {
 			final int lt1= LT(1);
 
+			if (lt1 == IToken.tEOC)
+				throwBacktrack(LA(1));
+
 			// Ignore passages inside braces (such as for a statement-expression),
 			// as they can basically contain tokens of any kind.
 			if (lt1 == IToken.tLBRACE) {
@@ -2686,7 +2689,7 @@ public abstract class AbstractGNUSourceCodeParser implements ISourceCodeParser {
 				continue;
 			}
 
-			if (lt1 == IToken.tEOC || lt1 == terminator)
+			if (lt1 == terminator)
 				throwBacktrack(LA(1));
 
 			consume();

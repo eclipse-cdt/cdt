ad47ea3e3d35ab40d55a5d55731976588a6749dc
Doug Schaefer <dschaefer@qnx.com>
2015-11-01 19:14:49 -0500

    Bug 480577 - Add an empty tab group for Arduino launch configs
    This prevents the error in the launch config dialog so you can delete
    orphaned configs.
    
    Change-Id: I9c33547a506b32260f4a258ec1911f32fd363df2

---
+++ toolchains/arduino/org.eclipse.cdt.arduino.ui/src/org/eclipse/cdt/arduino/ui/internal/launch/ArduinoLaunchConfigurationTabGroup.java
@@ -0,0 +1,14 @@
+package org.eclipse.cdt.arduino.ui.internal.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class ArduinoLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		setTabs(new ILaunchConfigurationTab[0]);
+	}
+
+}

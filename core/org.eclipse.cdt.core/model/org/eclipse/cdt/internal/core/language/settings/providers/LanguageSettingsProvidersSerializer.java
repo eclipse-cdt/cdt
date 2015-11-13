c22a3c538f4e5ac16136c0ebc41022571615a011
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2015-11-12 22:45:41 -0500

    Bug 481753 - LanguageSettingsProvidersSerializer does not "Check Out" shared language.settings.xml file before writing to it
    Change-Id: Ic256e95f17f070c763bdaef8b63583bd915c7eb0
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/language/settings/providers/LanguageSettingsProvidersSerializer.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/language/settings/providers/LanguageSettingsProvidersSerializer.java
@@ -47,6 +47,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
@@ -879,6 +880,7 @@ public class LanguageSettingsProvidersSerializer {
 						fileStorePrj.delete(true, null);
 					}
 				} else {
+					project.getWorkspace().validateEdit(new IFile[] { fileStorePrj }, IWorkspace.VALIDATE_PROMPT);
 					IContainer folder = fileStorePrj.getParent();
 					if (folder instanceof IFolder && !folder.exists()) {
 						((IFolder) folder).create(true, true, null);

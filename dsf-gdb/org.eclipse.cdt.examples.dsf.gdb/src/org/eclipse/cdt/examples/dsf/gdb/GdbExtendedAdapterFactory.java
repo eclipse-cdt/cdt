e137b129c39d61ac34e094a411885dbf89444f35
Jonah Graham <jonah@kichwacoders.com>
2015-11-29 21:10:47 +0000

    Bug 483234: Add a command example to the example plug-in
    A new command, ShowVersionHandler, has been added to the plug-in to
    demonstrate all the pieces that need to be put in place to connect
    a command to a DSF session.
    
    Change-Id: Iefde0ba144c12ce2e580da7717b390258c0d3675
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- dsf-gdb/org.eclipse.cdt.examples.dsf.gdb/src/org/eclipse/cdt/examples/dsf/gdb/GdbExtendedAdapterFactory.java
+++ dsf-gdb/org.eclipse.cdt.examples.dsf.gdb/src/org/eclipse/cdt/examples/dsf/gdb/GdbExtendedAdapterFactory.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Marc Khouzam (Ericsson) - initial API and implementation
  *******************************************************************************/
@@ -14,13 +14,23 @@ import org.eclipse.cdt.dsf.concurrent.ThreadSafe;
 import org.eclipse.cdt.dsf.gdb.internal.ui.GdbAdapterFactory;
 import org.eclipse.cdt.dsf.gdb.internal.ui.GdbSessionAdapters;
 import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.cdt.examples.dsf.gdb.commands.IShowVersionHandler;
 import org.eclipse.debug.core.ILaunch;
 
 @SuppressWarnings("restriction")
 @ThreadSafe
 public class GdbExtendedAdapterFactory extends GdbAdapterFactory {
-    @Override
-    protected GdbSessionAdapters createGdbSessionAdapters(ILaunch launch, DsfSession session) {
-    	return new GdbExtendedSessionAdapters(launch, session, getAdapterList());
-    }
+	@Override
+	public Class<?>[] getAdapterList() {
+		Class<?>[] list = super.getAdapterList();
+		Class<?>[] extendedList = new Class<?>[list.length + 1];
+		System.arraycopy(list, 0, extendedList, 0, list.length);
+		extendedList[list.length] = IShowVersionHandler.class;
+		return extendedList;
+	}
+
+	@Override
+	protected GdbSessionAdapters createGdbSessionAdapters(ILaunch launch, DsfSession session) {
+		return new GdbExtendedSessionAdapters(launch, session, getAdapterList());
+	}
 }

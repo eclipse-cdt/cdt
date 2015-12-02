425a8430612bfa8c1e664be8116424405556b0dd
Alexander Kurtakov <akurtako@redhat.com>
2015-12-03 00:51:25 +0200

    autotools: Saner handlers.
    Old IActions were just wrapped in handlers which created insane amount
    of interactions. This is one idea better by moving the implementations
    in the handlers themselves.
    
    Change-Id: Ieb7fc465c8a23af09cb6a3d809f157289c65d2ae
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/AutoconfHandler.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/AutoconfHandler.java
@@ -10,13 +10,46 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.autotools.ui.actions;
 
+import java.util.ArrayList;
+
+import org.eclipse.cdt.internal.autotools.core.AutotoolsPropertyConstants;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.swt.widgets.Shell;
 
 public class AutoconfHandler extends AbstractAutotoolsHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) {
-		return execute(event, new InvokeAutoconfAction());
+		return execute1(event);
+	}
+
+	private final static String DEFAULT_COMMAND = "autoconf"; //$NON-NLS-1$
+
+	@Override
+	public void run(Shell activeShell) {
+		IContainer container = getSelectedContainer();
+		if (container == null)
+			return;
+
+		IPath execDir = getExecDir(container);
+
+		IProject project = container.getProject();
+		String autoconfCommand = null;
+		try {
+			autoconfCommand = project.getPersistentProperty(AutotoolsPropertyConstants.AUTOCONF_TOOL);
+		} catch (CoreException e) {
+			// do nothing
+		}
+
+		// If unset for the project, default to system path
+		if (autoconfCommand == null) {
+			autoconfCommand = DEFAULT_COMMAND;
+		}
+		executeConsoleCommand(DEFAULT_COMMAND, autoconfCommand, new ArrayList<>(), execDir);
 	}
 
 }

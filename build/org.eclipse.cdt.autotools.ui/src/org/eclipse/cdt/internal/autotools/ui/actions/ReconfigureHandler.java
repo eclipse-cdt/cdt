a9b8749ca3094849c1fbe4a091003a08dbb78fd0
Alexander Kurtakov <akurtako@redhat.com>
2015-12-03 00:51:25 +0200

    autotools: Saner handlers.
    Old IActions were just wrapped in handlers which created insane amount
    of interactions. This is one idea better by moving the implementations
    in the handlers themselves.
    
    Change-Id: Ieb7fc465c8a23af09cb6a3d809f157289c65d2ae
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/ReconfigureHandler.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/actions/ReconfigureHandler.java
@@ -10,7 +10,23 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.autotools.ui.actions;
 
+import org.eclipse.cdt.internal.autotools.core.AutotoolsNewMakeGenerator;
+import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
+import org.eclipse.cdt.ui.CUIPlugin;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * @author Jeff Johnston
@@ -20,7 +36,47 @@ public class ReconfigureHandler extends AbstractAutotoolsHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) {
-		return execute(event, new ReconfigureAction());
+		return execute1(event);
+	}
+
+	@Override
+	public void run(Shell activeShell) {
+		IContainer container = getSelectedContainer();
+		if (container == null)
+			return;
+
+		// We need to use a workspace root scheduling rule because adding
+		// MakeTargets
+		// may end up saving the project description which runs under a
+		// workspace root rule.
+		final ISchedulingRule rule = ResourcesPlugin.getWorkspace().getRoot();
+
+		Job backgroundJob = new Job("Reconfigure Action") { //$NON-NLS-1$
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					ResourcesPlugin.getWorkspace().run((IWorkspaceRunnable) monitor1 -> {
+						IProject project = getSelectedContainer().getProject();
+						AutotoolsNewMakeGenerator m = new AutotoolsNewMakeGenerator();
+						IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project);
+						CUIPlugin.getDefault().startGlobalConsole();
+						m.initialize(project, info, monitor1);
+						try {
+							m.reconfigure();
+						} catch (CoreException e) {
+							// do nothing for now
+						}
+					}, rule, IWorkspace.AVOID_UPDATE, monitor);
+				} catch (CoreException e) {
+					return e.getStatus();
+				}
+				IStatus returnStatus = Status.OK_STATUS;
+				return returnStatus;
+			}
+		};
+
+		backgroundJob.setRule(rule);
+		backgroundJob.schedule();
 	}
 
 }

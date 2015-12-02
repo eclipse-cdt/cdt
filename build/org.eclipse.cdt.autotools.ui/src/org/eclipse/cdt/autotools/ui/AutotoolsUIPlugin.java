3dd076a1c28f969be6792a7a743109446a8ece12
Alexander Kurtakov <akurtako@redhat.com>
2015-12-02 11:57:07 +0200

    autotools: Sanitize activators.
    Reduce coupling by changing UI bundle to use UI activator and drop UI
    getters (e.g. workbench, window, shell) from the core bundle.
    Also remove other unused methods.
    
    Change-Id: Ida510d5a0e991c08332a998aefaba5843743172a
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/autotools/ui/AutotoolsUIPlugin.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/autotools/ui/AutotoolsUIPlugin.java
@@ -11,7 +11,6 @@
 package org.eclipse.cdt.autotools.ui;
 
 import java.lang.reflect.InvocationTargetException;
-import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -21,9 +20,6 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -88,17 +84,6 @@ public class AutotoolsUIPlugin extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns active shell.
-	 */
-	public static Shell getActiveWorkbenchShell() {
-		IWorkbenchWindow window = getDefault().getWorkbench().getActiveWorkbenchWindow();
-		if (window != null) {
-			return window.getShell();
-		}
-		return null;
-	}
-
-	/**
 	 * Returns the string from the plugin's resource bundle,
 	 * or 'key' if not found.
 	 * 
@@ -115,68 +100,18 @@ public class AutotoolsUIPlugin extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 * 
-	 * @param key the message key
-	 * @param args an array of substituition strings
-	 * @return the resource bundle message
-	 */
-	public static String getFormattedString(String key, String[] args) {
-		return MessageFormat.format(getResourceString(key), (Object[])args);
-	}
-
-	/**
 	 * Returns the plugin's resource bundle,
 	 */
 	public ResourceBundle getResourceBundle() {
 		return resourceBundle;
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
-	
 	public static void log(IStatus status) {
 		ResourcesPlugin.getPlugin().getLog().log(status);
 	}
 
 	public static void logErrorMessage(String message) {
 		log(new Status(IStatus.ERROR, getUniqueIdentifier(), IStatus.ERROR, message, null));
-	}
-
-	public static void logException(Throwable e, final String title, String message) {
-		if (e instanceof InvocationTargetException) {
-			e = ((InvocationTargetException) e).getTargetException();
-		}
-		IStatus status = null;
-		if (e instanceof CoreException)
-			status = ((CoreException) e).getStatus();
-		else {
-			if (message == null)
-				message = e.getMessage();
-			if (message == null)
-				message = e.toString();
-			status = new Status(IStatus.ERROR, getUniqueIdentifier(), IStatus.OK, message, e);
-		}
-		ResourcesPlugin.getPlugin().getLog().log(status);
-		Display display;
-		display = Display.getCurrent();
-		if (display == null)
-			display = Display.getDefault();
-		final IStatus fstatus = status;
-		display.asyncExec(() -> ErrorDialog.openError(null, title, null, fstatus));
-	}
-
-	public static void logException(Throwable e) {
-		logException(e, null, null);
 	}
 
 	public static void log(Throwable e) {
@@ -190,38 +125,6 @@ public class AutotoolsUIPlugin extends AbstractUIPlugin {
 		log(status);
 	}
 
-	/**
-	* Utility method with conventions
-	*/
-	public static void errorDialog(Shell shell, String title, String message, IStatus s) {
-		log(s);
-		// if the 'message' resource string and the IStatus' message are the same,
-		// don't show both in the dialog
-		if (s != null && message.equals(s.getMessage())) {
-			message = null;
-		}
-		ErrorDialog.openError(shell, title, message, s);
-	}
-
-	/**
-	* Utility method with conventions
-	*/
-	public static void errorDialog(Shell shell, String title, String message, Throwable t) {
-		log(t);
-		IStatus status;
-		if (t instanceof CoreException) {
-			status = ((CoreException) t).getStatus();
-			// if the 'message' resource string and the IStatus' message are the same,
-			// don't show both in the dialog
-			if (status != null && message.equals(status.getMessage())) {
-				message = null;
-			}
-		} else {
-			status = new Status(IStatus.ERROR, AutotoolsUIPlugin.getUniqueIdentifier(), -1, "Internal Error: ", t); //$NON-NLS-1$	
-		}
-		ErrorDialog.openError(shell, title, message, status);
-	}
-	
 	/**
 	 * Returns the workspace instance.
 	 */
@@ -243,6 +146,17 @@ public class AutotoolsUIPlugin extends AbstractUIPlugin {
 		IWorkbenchWindow window= getActiveWorkbenchWindow();
 		if (window != null) {
 			return window.getActivePage();
+		}
+		return null;
+	}
+
+	/**
+	 * Returns active shell.
+	 */
+	public static Shell getActiveWorkbenchShell() {
+		IWorkbenchWindow window = getActiveWorkbenchWindow();
+		if (window != null) {
+			return window.getShell();
 		}
 		return null;
 	}

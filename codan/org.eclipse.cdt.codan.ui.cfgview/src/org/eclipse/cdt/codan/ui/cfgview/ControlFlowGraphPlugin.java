91bbe7769d519549b90cdc0fac7c1f4ea182c62c
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2015-12-17 10:22:25 -0500

    cleanup of ControlFlowGraph view
    Change-Id: Ic6be0eedc1e66867efda2b437181da081b2234c0

--- codan/org.eclipse.cdt.codan.ui.cfgview/src/org/eclipse/cdt/codan/ui/cfgview/ControlFlowGraphPlugin.java
+++ codan/org.eclipse.cdt.codan.ui.cfgview/src/org/eclipse/cdt/codan/ui/cfgview/ControlFlowGraphPlugin.java
@@ -1,5 +1,8 @@
 package org.eclipse.cdt.codan.ui.cfgview;
 
+import org.eclipse.cdt.codan.core.CodanCorePlugin;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
@@ -23,7 +26,7 @@ public class ControlFlowGraphPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
 	 * )
@@ -36,7 +39,7 @@ public class ControlFlowGraphPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
 	 * )
@@ -49,7 +52,7 @@ public class ControlFlowGraphPlugin extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 * 
+	 *
 	 * @return the shared instance
 	 */
 	public static ControlFlowGraphPlugin getDefault() {
@@ -59,7 +62,7 @@ public class ControlFlowGraphPlugin extends AbstractUIPlugin {
 	/**
 	 * Returns an image descriptor for the image file at the given
 	 * plug-in relative path
-	 * 
+	 *
 	 * @param path the path
 	 * @return the image descriptor
 	 */
@@ -82,5 +85,46 @@ public class ControlFlowGraphPlugin extends AbstractUIPlugin {
 			image = registry.get(key);
 		}
 		return image;
+	}
+
+	/**
+	 * Logs the specified status with this plug-in's log.
+	 *
+	 * @param status
+	 *        status to log
+	 */
+	public static void log(IStatus status) {
+		getDefault().getLog().log(status);
+	}
+
+	/**
+	 * Logs an internal error with the specified throwable
+	 *
+	 * @param e the exception to be logged
+	 * @noreference This method is not intended to be referenced by clients.
+	 */
+	public static void log(Throwable e) {
+		log("Internal Error", e); //$NON-NLS-1$
+	}
+
+	/**
+	 * Logs an internal error with the specified message and throwable
+	 *
+	 * @param message the error message to log
+	 * @param e the exception to be logged
+	 * @noreference This method is not intended to be referenced by clients.
+	 */
+	public static void log(String message, Throwable e) {
+		log(new Status(IStatus.ERROR, CodanCorePlugin.PLUGIN_ID, 1, message, e));
+	}
+
+	/**
+	 * Logs an internal error with the specified message.
+	 *
+	 * @param message the error message to log
+	 * @noreference This method is not intended to be referenced by clients.
+	 */
+	public static void log(String message) {
+		log(new Status(IStatus.ERROR, PLUGIN_ID, 1, message, null));
 	}
 }

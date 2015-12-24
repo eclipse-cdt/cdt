350e9e67ef956344a80c045b320b8e0ff8a1bccd
Marc Khouzam <marc.khouzam@ericsson.com>
2015-12-23 23:51:01 -0500

    Fix copyright of all CDT plugins using the copyright tool from platform.
    This commit does not add missing copyrights, just updates the date on
    the existing ones.
    
    Change-Id: I646f5afd533a1fcc539bdf2e0686b22f406ecf65

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/properties/AutotoolsToolPropertyOptionPage.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/properties/AutotoolsToolPropertyOptionPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Red Hat Inc.
+ * Copyright (c) 2009, 2015 Red Hat Inc.
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html

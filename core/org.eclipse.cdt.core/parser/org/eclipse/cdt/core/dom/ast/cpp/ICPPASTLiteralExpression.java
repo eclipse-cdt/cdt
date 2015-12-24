752b0a27516ee448effeef297d83a623c171b061
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2015-12-24 10:27:28 -0800

    Revert "Bug 484618 - Implicit names for calls to user-defined literal operators"
    This reverts commit 7059bfb0ead4096a133c58e972a07946a8bea383.
    
    Change-Id: I99c73e4655a29b1e06e8b709451f939175b80031

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTLiteralExpression.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTLiteralExpression.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2015 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast.cpp;
 
-import org.eclipse.cdt.core.dom.ast.IASTImplicitNameOwner;
 import org.eclipse.cdt.core.dom.ast.IASTLiteralExpression;
 
 /**
@@ -19,8 +18,7 @@ import org.eclipse.cdt.core.dom.ast.IASTLiteralExpression;
  * @noextend This interface is not intended to be extended by clients.
  * @noimplement This interface is not intended to be implemented by clients.
  */
-public interface ICPPASTLiteralExpression extends IASTLiteralExpression, ICPPASTExpression, 
-		IASTImplicitNameOwner {
+public interface ICPPASTLiteralExpression extends IASTLiteralExpression, ICPPASTExpression {
 	/**
 	 * <code>lk_this</code> represents the 'this' keyword.
 	 */

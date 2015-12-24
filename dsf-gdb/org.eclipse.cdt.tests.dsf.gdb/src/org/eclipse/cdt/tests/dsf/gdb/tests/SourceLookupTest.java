59859eaaa9a71394b5f019f25626bb3605eb9e68
Jonah Graham <jonah@kichwacoders.com>
2015-12-24 19:18:07 +0000

    Bug 472765: [source lookup] support gdb "set substitute-path from to"
    Add support for gdb's "set substitute-path from to" by adding a new
    service called GDBSourceLookup that uses GDB to perform the path mapping
    when enabled.
    
    The new GDBSourceLookup service uses the new GdbSourceLookupDirector to
    get the set of paths that need mapping using the GDB "set
    substitute-path" and the director resolves compilation path differently
    so at to leave the GDB backend to handle the local path to compile path
    resolution.
    
    The MappingSourceContainer has been enhanced to allow a per mapping
    container override of the new behaviour. This is a fallback as the
    default behaviour when using GDB is to use "set substitute-path".
    
    The MappingSourceContainerDialog exposes the new option in
    MappingSourceContainer as a checkbox at the bottom of the path mapping
    dialog in a backend agnostic way.
    
    The new code is tested in methods called "sourceSubstitute*" and
    mirror the same tests for the now non-backend handled version
    which are "sourceMapping*". Note that
    doMappingAndLaunch/doSubstituteAndLaunch have been updated to explicitly
    check or uncheck the setIsMappingWithBackendEnabled setting
    
    Change-Id: I122d7c597cd461d8e38c4f82522ccfdf9e51a5ba
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/SourceLookupTest.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/SourceLookupTest.java
@@ -31,9 +31,17 @@ import org.eclipse.cdt.debug.core.model.ICBreakpointType;
 import org.eclipse.cdt.debug.core.model.ICLineBreakpoint;
 import org.eclipse.cdt.debug.core.sourcelookup.MappingSourceContainer;
 import org.eclipse.cdt.debug.internal.core.sourcelookup.MapEntrySourceContainer;
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.concurrent.Query;
+import org.eclipse.cdt.dsf.debug.service.IRunControl.StepType;
 import org.eclipse.cdt.dsf.debug.service.IStack.IFrameDMContext;
 import org.eclipse.cdt.dsf.debug.service.IStack.IFrameDMData;
 import org.eclipse.cdt.dsf.debug.sourcelookup.DsfSourceLookupDirector;
+import org.eclipse.cdt.dsf.gdb.service.command.IGDBControl;
+import org.eclipse.cdt.dsf.mi.service.command.CommandFactory;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIInfo;
+import org.eclipse.cdt.dsf.service.DsfServicesTracker;
+import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.cdt.tests.dsf.gdb.framework.AsyncCompletionWaitor;
 import org.eclipse.cdt.tests.dsf.gdb.framework.BackgroundRunner;
 import org.eclipse.cdt.tests.dsf.gdb.framework.BaseTestCase;
@@ -131,6 +139,9 @@ public class SourceLookupTest extends BaseTestCase {
 		}
 	};
 
+	private IGDBControl fCommandControl;
+	private CommandFactory fCommandFactory;
+
 	@Override
 	public void doBeforeTest() throws Exception {
 		IBreakpointManager manager = DebugPlugin.getDefault().getBreakpointManager();
@@ -157,6 +168,20 @@ public class SourceLookupTest extends BaseTestCase {
 	public void doLaunch(String programName) throws Exception {
 		setLaunchAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_NAME, programName);
 		super.doLaunch();
+
+		// Get a reference to the breakpoint service
+		final DsfSession session = getGDBLaunch().getSession();
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				DsfServicesTracker tracker = new DsfServicesTracker(TestsPlugin.getBundleContext(), session.getId());
+				fCommandControl = tracker.getService(IGDBControl.class);
+				fCommandFactory = fCommandControl.getCommandFactory();
+				tracker.dispose();
+			}
+		};
+		session.getExecutor().submit(runnable).get();
 	}
 
 	@Override
@@ -273,11 +298,10 @@ public class SourceLookupTest extends BaseTestCase {
 	 * Set default source locators and a path mapping
 	 * {@link MappingSourceContainer} from BUILD_ABSPATH -> SOURCE_ABSPATH and
 	 * do the launch
-	 * 
-	 * @return
 	 */
 	protected void doMappingAndLaunch(String programName) throws CoreException, Exception {
 		MappingSourceContainer mapContainer = new MappingSourceContainer("Mappings");
+		mapContainer.setIsMappingWithBackendEnabled(false);
 		mapContainer.addMapEntry(fMapEntrySourceContainer);
 		setSourceContainer(mapContainer);
 		doLaunch(EXEC_PATH + programName);
@@ -446,6 +470,236 @@ public class SourceLookupTest extends BaseTestCase {
 		sourceLocator.setSourceContainers(newContainers);
 
 		assertSourceFoundByDirectorOnly();
+	}
+
+	/**
+	 * Set default source locators and a path mapping
+	 * {@link MappingSourceContainer} from BUILD_ABSPATH -> SOURCE_ABSPATH and
+	 * do the launch
+	 */
+	protected void doSubstituteAndLaunch(String programName) throws CoreException, Exception {
+		MappingSourceContainer mapContainer = new MappingSourceContainer("Mappings");
+		// Explicit for the test, but true is the default
+		mapContainer.setIsMappingWithBackendEnabled(true);
+		mapContainer.addMapEntry(fMapEntrySourceContainer);
+		setSourceContainer(mapContainer);
+		doLaunch(EXEC_PATH + programName);
+	}
+
+	/**
+	 * With mapping test that GDB does not locate the file, but the source
+	 * lookup director and the source lookup service do find the file.
+	 */
+	protected void sourceSubstitute(String programName) throws Throwable {
+		doSubstituteAndLaunch(programName);
+		assertSourceFound();
+	}
+
+	/**
+	 * With mapping test breakpoints can be inserted.
+	 */
+	protected void sourceSubstituteBreakpoints(String programName) throws Throwable {
+		doSubstituteAndLaunch(programName);
+
+		// insert breakpoint in source file
+		fBreakpointInstalledWait.waitReset();
+		ICLineBreakpoint bp = CDIDebugModel.createLineBreakpoint(
+				new Path(SOURCE_ABSPATH).append(SOURCE_NAME).toOSString(), ResourcesPlugin.getWorkspace().getRoot(),
+				ICBreakpointType.REGULAR, SOURCE_LINE, true, 0, "", true);
+		// The delay here is based on:
+		// 1) The installation of the breakpoint takes some time
+		// 2) The notification of the IBreakpoint change needs the autobuild
+		// to run, and it has up to a 1 second delay (depending on how
+		// long since it last ran). See
+		// org.eclipse.core.internal.events.AutoBuildJob.computeScheduleDelay()
+		fBreakpointInstalledWait.waitUntilDone(TestsPlugin.massageTimeout(2000));
+		assertTrue("Breakpoint failed to install", bp.isInstalled());
+	}
+
+	/**
+	 * Test source mappings with executable built with an Absolute and Canonical
+	 * build path
+	 */
+	@Test
+	public void sourceSubstituteAC() throws Throwable {
+		sourceSubstitute(EXEC_AC_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with an Absolute and
+	 * Non-canonical build path
+	 */
+	@Test
+	@Ignore("Not supported because GDB does not handle non-canonical paths. See Bug 477057")
+	public void sourceSubstituteAN() throws Throwable {
+		sourceSubstitute(EXEC_AN_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with a Relative and Canonical
+	 * build path
+	 */
+	@Test
+	public void sourceSubstituteRC() throws Throwable {
+		sourceSubstitute(EXEC_RC_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with a Relative and
+	 * Non-canonical build path
+	 */
+	@Test
+	@Ignore("Not supported because GDB does not handle non-canonical paths. See Bug 477057")
+	public void sourceSubstituteRN() throws Throwable {
+		sourceSubstitute(EXEC_RN_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with an Absolute and Canonical
+	 * build path
+	 */
+	@Test
+	public void sourceSubstituteBreakpointsAC() throws Throwable {
+		sourceSubstituteBreakpoints(EXEC_AC_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with an Absolute and
+	 * Non-canonical build path
+	 */
+	@Test
+	@Ignore("Not supported because GDB does not handle non-canonical paths. See Bug 477057")
+	public void sourceSubstituteBreakpointsAN() throws Throwable {
+		sourceSubstituteBreakpoints(EXEC_AN_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with a Relative and Canonical
+	 * build path
+	 */
+	@Test
+	public void sourceSubstituteBreakpointsRC() throws Throwable {
+		sourceSubstituteBreakpoints(EXEC_RC_NAME);
+	}
+
+	/**
+	 * Test source mappings with executable built with a Relative and
+	 * Non-canonical build path
+	 */
+	@Test
+	@Ignore("Not supported because GDB does not handle non-canonical paths. See Bug 477057")
+	public void sourceSubstituteBreakpointsRN() throws Throwable {
+		sourceSubstituteBreakpoints(EXEC_RN_NAME);
+	}
+
+	/**
+	 * Change directory to the binary (aka EXEC_PATH)
+	 */
+	protected void doCdToBinDir() throws Exception {
+		Query<MIInfo> query = new Query<MIInfo>() {
+			@Override
+			protected void execute(DataRequestMonitor<MIInfo> rm) {
+				fCommandControl.queueCommand(fCommandFactory.createMIEnvironmentCD(fCommandControl.getContext(),
+						new File(EXEC_PATH).getAbsolutePath()), rm);
+			}
+		};
+
+		fCommandControl.getExecutor().execute(query);
+		query.get();
+	}
+
+	/**
+	 * Test that if the user changes the source mappings in the middle of a
+	 * debug session (e.g. with CSourceNotFoundEditor) that the lookups are
+	 * updated.
+	 */
+	@Test
+	public void sourceSubstituteChanges() throws Throwable {
+		doSubstituteAndLaunch(EXEC_AC_NAME);
+
+		DsfSourceLookupDirector sourceLocator = (DsfSourceLookupDirector) getGDBLaunch().getSourceLocator();
+		MapEntrySourceContainer incorrectMapEntry = new MapEntrySourceContainer(
+				new Path(BUILD_ABSPATH + "/incorrectsubpath"), new Path(SOURCE_ABSPATH));
+
+		assertSourceFound();
+
+		// Change the source mappings
+		ISourceContainer[] containers = sourceLocator.getSourceContainers();
+		MappingSourceContainer mappingSourceContainer = (MappingSourceContainer) containers[1];
+		mappingSourceContainer.removeMapEntry(fMapEntrySourceContainer);
+		mappingSourceContainer.addMapEntry(incorrectMapEntry);
+		sourceLocator.setSourceContainers(containers);
+
+		/*
+		 * GDB (pre 7.0) changes the current directory when the above source is
+		 * found as a result GDB is able to find the source. To make sure that
+		 * GDB is really doing a substitution rather than looking in current
+		 * directory, change the current directory. Without this, the
+		 * assertSourceNotFound fails because GDB unexpectedly finds the source
+		 * (for the wrong reason).
+		 */
+		doCdToBinDir();
+
+		/*
+		 * TODO: remove need for the sleep + step!
+		 * 
+		 * While the Source Substitute path code listens for changes on source
+		 * containers, it does not invalidate the caches, doing the step manages
+		 * that. The sleep is a temporary hack to ensure that the
+		 * setSourceContainers has completed.
+		 */
+		Thread.sleep(1000);
+		SyncUtil.step(StepType.STEP_INTO);
+
+		assertSourceNotFound();
+
+		// Change the source mappings back
+		containers = sourceLocator.getSourceContainers();
+		mappingSourceContainer = (MappingSourceContainer) containers[1];
+		mappingSourceContainer.removeMapEntry(incorrectMapEntry);
+		mappingSourceContainer.addMapEntry(fMapEntrySourceContainer);
+		sourceLocator.setSourceContainers(containers);
+
+		/*
+		 * TODO: same reason as above TODO
+		 */
+		Thread.sleep(1000);
+		SyncUtil.step(StepType.STEP_INTO);
+
+		assertSourceFound();
+	}
+
+	/**
+	 * Test that if the user changes the source mappings in the middle of a
+	 * debug session (e.g. with CSourceNotFoundEditor) that the lookups are
+	 * updated.
+	 * 
+	 * This version is for a new source mapping where there wasn't one
+	 * previously.
+	 */
+	@Test
+	public void sourceSubstituteAdded() throws Throwable {
+		doLaunch(EXEC_PATH + EXEC_RC_NAME);
+
+		assertSourceNotFound();
+
+		// Change the source mappings back
+		DsfSourceLookupDirector sourceLocator = (DsfSourceLookupDirector) getGDBLaunch().getSourceLocator();
+		ISourceContainer[] containers = sourceLocator.getSourceContainers();
+		MappingSourceContainer mappingSourceContainer = new MappingSourceContainer("Mappings");
+		mappingSourceContainer.addMapEntry(fMapEntrySourceContainer);
+		ISourceContainer[] newContainers = new ISourceContainer[containers.length + 1];
+		System.arraycopy(containers, 0, newContainers, 0, containers.length);
+		newContainers[newContainers.length - 1] = mappingSourceContainer;
+		sourceLocator.setSourceContainers(newContainers);
+
+		/*
+		 * TODO: same reason as above TODO in sourceSubstituteChanges
+		 */
+		Thread.sleep(1000);
+		SyncUtil.step(StepType.STEP_INTO);
+
+		assertSourceFound();
 	}
 
 	/**

eab00875bdf94581c645a577c18678e04abd7fb3
Marc Khouzam <marc.khouzam@ericsson.com>
2015-12-23 23:51:01 -0500

    Fixing copyright files using the copyright tool from platform.
    Change-Id: I646f5afd533a1fcc539bdf2e0686b22f406ecf65

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/ITranslationUnitHolder.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/core/model/ITranslationUnitHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Zeligsoft (2009) Limited.
+ * Copyright (c) 2013 Zeligsoft (2014) Limited.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

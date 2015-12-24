1614f8fe3f61f4e525042cc51fcf1ab691a5c85c
Marc Khouzam <marc.khouzam@ericsson.com>
2015-12-23 23:51:01 -0500

    Fix copyright of all CDT plugins using the copyright tool from platform.
    This commit does not add missing copyrights, just updates the date on
    the existing ones.
    
    Change-Id: I646f5afd533a1fcc539bdf2e0686b22f406ecf65

--- codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/AssignmentToItselfChecker.java
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/AssignmentToItselfChecker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Severin Gehwolf 
+ * Copyright (c) 2010, 2013 Severin Gehwolf 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at

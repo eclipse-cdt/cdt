3508addc404bedcdba0f8ba659a4a0e411c3ef58
Nathan Ridge <zeratul976@hotmail.com>
2015-12-25 02:38:55 -0500

    Bug 416748 - Improve semantic highlighting for alias templates
    Change-Id: Ic0d0110133a4732cace07d19e995e7c51b5538e3
    Signed-off-by: Nathan Ridge <zeratul976@hotmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/SemanticHighlightingTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/SemanticHighlightingTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -438,6 +438,21 @@ public class SemanticHighlightingTest extends TestCase {
     //	struct S { };                                    //$class
     //	alignas(S) int x;                                //$class,globalVariable
     public void testHighlightingInsideAlignmentSpecifier_451082() throws Exception {
+    	makeAssertions();
+    }
+    
+	//	template<typename T, typename U>                 //$templateParameter,templateParameter
+	//	struct Pair {};                                  //$class
+	//
+	//	template<typename T>                             //$templateParameter
+	//	using PairIntX = Pair<int, T>;                   //$typedef,class,templateParameter
+	//
+    //	struct Waldo {};                                 //$class
+    //
+	//	int main() {                                     //$functionDeclaration
+	//		PairIntX<Waldo> pair;                        //$typedef,class,localVariableDeclaration
+	//	}
+    public void testAliasTemplates_416748() throws Exception {
     	makeAssertions();
     }
 }

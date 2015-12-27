fabae6680e73c1ef811f2b60170ad1db7cdbcd04
Jonah Graham <jonah@kichwacoders.com>
2015-12-27 17:57:07 +0000

    Bug 472765: use gdb's "set substitute-path from to"
    Add support for gdb's "set substitute-path from to" by adding a new
    service called GDBSourceLookup that uses GDB to perform the path mapping.
    
    The new GDBSourceLookup service uses the new GdbSourceLookupDirector to
    get the set of paths that need mapping using the GDB "set
    substitute-path" and the director resolves compilation path differently
    so at to leave the GDB backend to handle the local path to compile path
    resolution.
    
    The MappingSourceContainer has been enhanced to allow a per mapping
    container override of the new behaviour. This is a fallback as the
    default behaviour when using GDB is to use "set substitute-path".
    
    The MappingSourceContainerDialog exposes the new option in
    MappingSourceContainer as a checkbox at the bottom of the path mapping
    dialog in a backend agnostic way.
    
    The new code is tested in methods called "sourceSubstitute*" and
    mirror the same tests for the now non-backend handled version
    which are "sourceMapping*". Note that
    doMappingAndLaunch/doSubstituteAndLaunch have been updated to explicitly
    check or uncheck the setIsMappingWithBackendEnabled setting
    
    Change-Id: I122d7c597cd461d8e38c4f82522ccfdf9e51a5ba
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/MappingSourceContainerType.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/MappingSourceContainerType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2012 QNX Software Systems and others.
+ * Copyright (c) 2004, 2015 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,6 +30,7 @@ public class MappingSourceContainerType extends AbstractSourceContainerTypeDeleg
 	private final static String ELEMENT_MAPPING = "mapping"; //$NON-NLS-1$
 	private final static String ELEMENT_MAP_ENTRY = "mapEntry"; //$NON-NLS-1$
 	private final static String ATTR_NAME = "name"; //$NON-NLS-1$
+	private final static String ATTR_BACKEND_ENABLED = "backend_enabled"; //$NON-NLS-1$
 	private final static String ATTR_MEMENTO = "memento"; //$NON-NLS-1$
 
 	/* (non-Javadoc)
@@ -44,6 +45,8 @@ public class MappingSourceContainerType extends AbstractSourceContainerTypeDeleg
 				String name = element.getAttribute(ATTR_NAME);
 				if (name == null) 
 					name = ""; //$NON-NLS-1$
+				String backendEnabled = element.getAttribute(ATTR_BACKEND_ENABLED);
+				boolean enabled = backendEnabled == null || Boolean.parseBoolean(backendEnabled);
 				List<MapEntrySourceContainer> entries = new ArrayList<MapEntrySourceContainer>();
 				Node childNode = element.getFirstChild();
 				while (childNode != null) {
@@ -62,6 +65,7 @@ public class MappingSourceContainerType extends AbstractSourceContainerTypeDeleg
 					childNode = childNode.getNextSibling();
 				}
 				MappingSourceContainer container = new MappingSourceContainer(name);
+				container.setIsMappingWithBackendEnabled(enabled);
 				for (MapEntrySourceContainer entry : entries) {
 					container.addMapEntry(entry);
 				}
@@ -81,6 +85,8 @@ public class MappingSourceContainerType extends AbstractSourceContainerTypeDeleg
 		Document document = newDocument();
 		Element element = document.createElement(ELEMENT_MAPPING);
 		element.setAttribute(ATTR_NAME, container.getName());
+		boolean backendEnabled = ((MappingSourceContainer)container).isMappingWithBackendEnabled();
+		element.setAttribute(ATTR_BACKEND_ENABLED, String.valueOf(backendEnabled));
 		ISourceContainer[] entries = ((MappingSourceContainer)container).getSourceContainers();
 		for (int i = 0; i < entries.length; ++i) {
 			Element child = document.createElement(ELEMENT_MAP_ENTRY);

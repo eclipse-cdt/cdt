74ba37587417fa263a185fc70ca48bbfec1589f2
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-24 17:03:02 -0500

    Update copyright dates
    Change-Id: I94ac38c37657e270f62db9bbd2859c2c6de40e5f

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/ICPPInternalVariable.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/ICPPInternalVariable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Institute for Software, HSR Hochschule fuer Technik
+ * Copyright (c) 2015, 2016 Institute for Software, HSR Hochschule fuer Technik
  * Rapperswil, University of applied sciences.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0

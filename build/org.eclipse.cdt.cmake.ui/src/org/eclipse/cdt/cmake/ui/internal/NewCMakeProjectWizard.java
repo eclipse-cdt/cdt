766f66c0ab8520dc90a3066e34d1f22094b56786
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-23 23:13:49 -0500

    Missing copyright header.
    Copyright assigned to company of the committer
    who did the first commit of the file.
    
    Change-Id: Ia133694018c798f9558258810982f5276737a0b0

--- build/org.eclipse.cdt.cmake.ui/src/org/eclipse/cdt/cmake/ui/internal/NewCMakeProjectWizard.java
+++ build/org.eclipse.cdt.cmake.ui/src/org/eclipse/cdt/cmake/ui/internal/NewCMakeProjectWizard.java
@@ -1,6 +1,10 @@
-/**
- * The activator class controls the plug-in life cycle
- */
+/*******************************************************************************
+ * Copyright (c) 2015 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.cdt.cmake.ui.internal;
 
 import java.lang.reflect.InvocationTargetException;

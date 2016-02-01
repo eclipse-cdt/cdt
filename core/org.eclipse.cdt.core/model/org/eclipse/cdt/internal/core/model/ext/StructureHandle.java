23dc890167b486c8d5b7d2677be77872329ed2b9
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2016-02-01 11:41:09 -0800

    Cosmetics.

--- core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/ext/StructureHandle.java
+++ core/org.eclipse.cdt.core/model/org/eclipse/cdt/internal/core/model/ext/StructureHandle.java
@@ -6,9 +6,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Markus Schorn - initial API and implementation
+ *     Markus Schorn - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.cdt.internal.core.model.ext;
 
 import org.eclipse.cdt.core.dom.ast.ICompositeType;
@@ -27,7 +26,7 @@ public class StructureHandle extends CElementHandle implements IStructure {
 	}
 
 	private static int convertKey(int astKey) {
-		switch(astKey) {
+		switch (astKey) {
 		case ICompositeType.k_struct:
 			return ICElement.C_STRUCT;
 		case ICompositeType.k_union:

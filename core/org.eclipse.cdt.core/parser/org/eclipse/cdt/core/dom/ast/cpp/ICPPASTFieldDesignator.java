d085d1d63336477305d0140d6bfcf07146369891
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2016-02-08 15:25:23 -0800

    Adjusted copyright.

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTFieldDesignator.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/core/dom/ast/cpp/ICPPASTFieldDesignator.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBM Corporation and others.
+ * Copyright (c) 2015 Google, Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Sergey Prigogin (Google) - Initial API and implementation
+ *     Sergey Prigogin (Google) - Initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.core.dom.ast.cpp;
 

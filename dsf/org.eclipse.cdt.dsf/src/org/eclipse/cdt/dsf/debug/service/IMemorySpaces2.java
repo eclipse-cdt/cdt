e559a907b7efb8d117bc39b6415ef45240f6d384
Alvaro Sanchez-Leon <alvsan09@gmail.com>
2016-02-22 21:42:48 +0100

    Bug 489501 - [memory] Add API to resolve the default memory space id
    When using memory spaces, a back end may sometimes qualify an address
    to specify the memory space this address belongs to,
    however to minimize impacts the memory space may not always be present
    e.g. as it will use it to refer to the default memory space.
    
    However DSF-GDB has no means to know what is the default memory space
    to apply to e.g. and address resulting from an expression.
    
    This change introduces a new API to request the memory service for the
    default memory space.
    
    Change-Id: Icea87543b8529756aee92c6d2af95fb936f5fb9c

--- dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/debug/service/IMemorySpaces2.java
+++ dsf/org.eclipse.cdt.dsf/src/org/eclipse/cdt/dsf/debug/service/IMemorySpaces2.java
@@ -28,4 +28,19 @@ public interface IMemorySpaces2 extends IMemorySpaces {
 	 */
 	public void decodeExpression(IDMContext context, String expression, DataRequestMonitor<DecodeResult> rm);
 
+	/**
+	 * Provides the default memory space to be used in the given context.
+	 * 
+	 * @param ctx
+	 *            a context which might <i>contain</i> one or more memory spaces. Contexts that may be <i>associated</i>
+	 *            with a memory space should not be passed in. E.g., an expression might be associated with a memory
+	 *            space, but it does not contain memory spaces, and is thus not an appropriate context for this method.
+	 * @param rm
+	 *            the asynchronous data request monitor. Returns a memory space ID. Never null, but may be empty.
+	 * @since 2.7
+	 */
+	default void getDefaultMemorySpace(IDMContext context, final DataRequestMonitor<String> rm) {
+	    rm.setData(""); //$NON-NLS-1$
+	    rm.done();
+	}
 }

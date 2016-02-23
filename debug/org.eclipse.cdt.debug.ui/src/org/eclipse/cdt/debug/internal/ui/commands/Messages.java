aff727504476ed8f75e16ceeccb84066d3352504
Marc Khouzam <marc.khouzam@ericsson.com>
2016-02-23 09:52:37 -0500

    Bug 478740: Missing externalized strings
    Change-Id: Ia343d2c055e167fe093586aca32a209b4a8a1c11

---
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/commands/Messages.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+  * Copyright (c) 2016 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.cdt.debug.internal.ui.commands;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	public static String ReverseDebugging_Error;
+	public static String ReverseDebugging_UndefinedTraceMethod;
+	public static String ReverseDebugging_HardwareTracingNotAvailable;
+	public static String ReverseDebugging_DoNotShowAgain;
+	public static String ReverseDebugging_ToggleHardwareTrace;
+	public static String ReverseDebugging_ToggleSoftwareTrace;
+	public static String ReverseDebugging_ToggleReverseDebugging;
+	
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(Messages.class.getName(), Messages.class);
+	}
+
+	private Messages() {
+	}
+}

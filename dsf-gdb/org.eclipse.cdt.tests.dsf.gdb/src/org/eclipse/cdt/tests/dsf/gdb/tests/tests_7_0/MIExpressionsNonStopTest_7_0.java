28cdca10f8fef5f7e05cfae96d511fe96bd9abf7
Marc Khouzam <marc.khouzam@ericsson.com>
2016-02-24 16:33:18 -0500

    Remove unnecessary setting of GDB version for non-stop tests.
    Non-stop tests inherit from their all-stop counterpart which already
    sets the GDB version.
    
    Change-Id: I7444372971a9a74536745279fddac32842c46c03

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_7_0/MIExpressionsNonStopTest_7_0.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_7_0/MIExpressionsNonStopTest_7_0.java
@@ -12,7 +12,6 @@ package org.eclipse.cdt.tests.dsf.gdb.tests.tests_7_0;
 
 import org.eclipse.cdt.dsf.gdb.IGDBLaunchConfigurationConstants;
 import org.eclipse.cdt.tests.dsf.gdb.framework.BackgroundRunner;
-import org.eclipse.cdt.tests.dsf.gdb.tests.ITestConstants;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -25,11 +24,6 @@ public class MIExpressionsNonStopTest_7_0 extends MIExpressionsTest_7_0 {
 		Assume.assumeTrue(supportsNonStop());
 	}
 
-	@Override
-	protected void setGdbVersion() {
-		setGdbProgramNamesLaunchAttributes(ITestConstants.SUFFIX_GDB_7_0);
-	}
-	
 	@Override
 	protected void setLaunchAttributes() {
 		super.setLaunchAttributes();

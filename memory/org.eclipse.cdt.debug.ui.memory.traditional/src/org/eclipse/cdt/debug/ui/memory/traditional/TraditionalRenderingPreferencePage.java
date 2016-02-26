5d83f2be428e5e25565a1fe93c319b87eacad076
Marc Dumais <marc.dumais@ericsson.com>
2016-02-26 09:38:21 -0500

    bug 488469 - [traditional memory rendering] add preference for coloring background independently for different memory spaces
    Change-Id: If61d909ef66b9c11aebe0863b787af465121dd59

--- memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/TraditionalRenderingPreferencePage.java
+++ memory/org.eclipse.cdt.debug.ui.memory.traditional/src/org/eclipse/cdt/debug/ui/memory/traditional/TraditionalRenderingPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2013 Wind River Systems, Inc. and others.
+ * Copyright (c) 2006, 2016 Wind River Systems, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -76,6 +76,15 @@ public class TraditionalRenderingPreferencePage
 		
 		addField(new ColorFieldEditor(TraditionalRenderingPreferenceConstants.MEM_COLOR_BACKGROUND, 
 				TraditionalRenderingMessages.getString("TraditionalRenderingPreferencePage_BackgroundColor"), getFieldEditorParent())); //$NON-NLS-1$
+		
+		// are there known memory spaces? If so make their background color configurable
+		MemorySpacePreferencesUtil util = new MemorySpacePreferencesUtil();
+		String[] memSpaceKeys = util.getMemorySpaceKeys();
+		String[] memSpacesLabels = util.getMemorySpacesLabels(); 
+		
+		for (int i = 0; i < memSpaceKeys.length; i++) {
+		  addField(new ColorFieldEditor(memSpaceKeys[i], memSpacesLabels[i], getFieldEditorParent()));
+		}
 
 		addField(new ColorAndEffectFieldEditor(TraditionalRenderingPreferenceConstants.MEM_COLOR_CHANGED,
 				TraditionalRenderingPreferenceConstants.MEM_COLOR_CHANGED_BOLD,

8dbcc1f90a53a830e6f7e8b03f737f6463955d40
raddepal <ravitheja.addepally@intel.com>
2016-03-01 16:04:36 +0100

    Bug 488660 - Reverse toggle now refreshes on console changes with GDB 7.10
    The patch implements the event handler which
    queries the trace method and triggers the necessary
    update in the command handler.
    
    Change-Id: I9c1827deb56dce099b7f455e942d1b6735a80643
    Signed-off-by: raddepal <ravitheja.addepally@intel.com>
    Signed-off-by: ravitheja.addepally <ravitheja.addepally@intel.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/CommandFactory.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/CommandFactory.java
@@ -238,6 +238,7 @@ import org.eclipse.cdt.dsf.mi.service.command.output.MIVarSetFormatInfo;
 import org.eclipse.cdt.dsf.mi.service.command.output.MIVarShowAttributesInfo;
 import org.eclipse.cdt.dsf.mi.service.command.output.MIVarShowFormatInfo;
 import org.eclipse.cdt.dsf.mi.service.command.output.MIVarUpdateInfo;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIinfoRecord;
 import org.eclipse.cdt.debug.core.model.IChangeReverseMethodHandler.ReverseTraceMethod;
 
 /**
@@ -320,6 +321,11 @@ public class CommandFactory {
 	/** @since 5.0*/
 	public ICommand<MIInfo> createCLIRecord(ICommandControlDMContext ctx, ReverseTraceMethod traceMethod) {
 		return new CLIRecord(ctx, traceMethod);
+	}
+
+	/** @since 5.0*/
+	public ICommand<MIinfoRecord> createCLIInfoRecord(ICommandControlDMContext ctx) {
+		return new CLIInfoRecord(ctx);
 	}
 
 	/** @since 4.1 */

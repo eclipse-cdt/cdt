c297330c9cdcf0c8ba32768476c2a96fc0b9a32c
William Riley <william.riley@renesas.com>
2016-03-08 13:39:56 -0500

    Bug 476797 - Port Expand annotation hover from JDT
    Ported the JDT expand annotation hover code into CDT & adapted.
    
    Added preference to control in equivalent location to JDT. Default is
    disabled for now.
    
    Changed behaviour from JDT version - 
     * Breakpoint annotations & add breakpoint option displayed last
     * Tooltips displayed with no delay after expanded hover is displayed
     * Reverted fix for bug 165533 due to issue where single click on 1st
       item in expanded hover also triggers single click on top item in
       ruler if mouse within ruler area (Same as JDT)
    
    Known issues -
     * Double click on ruler column sometimes triggers single click on 1st
       item in expanded hover if user is too slow (Same as JDT)
    
    Change-Id: I87c2f8efd04ea5084b056241a04758a368e2ca55
    Signed-off-by: William Riley <william.riley@renesas.com>

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CDocumentProvider.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/CDocumentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2015 QNX Software Systems and others.
+ * Copyright (c) 2002, 2016 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -107,7 +107,7 @@ public class CDocumentProvider extends TextFileDocumentProvider {
 	/**
 	 * Annotation representing an {@code IProblem}.
 	 */
-	static protected class ProblemAnnotation extends Annotation implements ICAnnotation {
+	public static class ProblemAnnotation extends Annotation implements ICAnnotation {
 		private static final String INDEXER_ANNOTATION_TYPE= "org.eclipse.cdt.ui.indexmarker"; //$NON-NLS-1$
 
 		private final ITranslationUnit fTranslationUnit;

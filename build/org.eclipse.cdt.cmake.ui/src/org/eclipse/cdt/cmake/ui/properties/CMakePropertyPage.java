53332c93f35c32fa103e81f7f22a6550f1473cfe
Jesper Eskilson <jesper.eskilson@iar.com>
2016-03-08 20:20:09 -0500

    Added property page for CMake projects
    Currently only contains a button for launching the CMake GUI
    (cmake-qt-gui), but we might want to put other things in here as well.
    
    
    Change-Id: Ia89ad409eaad17a170cf1e2f1cd4fb31a7d678e2
    Signed-off-by: Jesper Eskilson <jesper.eskilson@iar.com>

---
+++ build/org.eclipse.cdt.cmake.ui/src/org/eclipse/cdt/cmake/ui/properties/CMakePropertyPage.java
@@ -0,0 +1,59 @@
+package org.eclipse.cdt.cmake.ui.properties;
+
+import java.io.IOException;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+/**
+ * Property page for CMake projects. The only thing we have here at the moment is a button
+ * to launch the CMake GUI configurator (cmake-qt-gui).
+ * 
+ * We assume that the build directory is in project/build/configname, which is where
+ * the CMake project wizard puts it. We also assume that "cmake-gui" is in the user's 
+ * PATH.
+ * 
+ * @author jesperes
+ */
+public class CMakePropertyPage extends PropertyPage {
+
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setLayout(layout);
+		GridData data = new GridData(GridData.FILL);
+		data.grabExcessHorizontalSpace = true;
+		composite.setLayoutData(data);
+
+		Button b = new Button(composite, SWT.NONE);
+		b.setText(Messages.CMakePropertyPage_LaunchCMakeGui);
+		b.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				IProject project = (IProject) getElement();
+				try {
+					String configName = project.getActiveBuildConfig().getName();
+					String sourceDir = project.getLocation().toOSString();
+					String buildDir = project.getLocation().append("build").append(configName).toOSString(); //$NON-NLS-1$
+					
+					Runtime.getRuntime().exec(new String[] { "cmake-gui", "-H" + sourceDir, "-B" + buildDir }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				} catch (CoreException | IOException e1) {
+					MessageDialog.openError(parent.getShell(), Messages.CMakePropertyPage_FailedToStartCMakeGui_Title, 
+							Messages.CMakePropertyPage_FailedToStartCMakeGui_Body + e1.getMessage());
+				}
+			}
+		});
+		
+		return composite;
+	}
+}

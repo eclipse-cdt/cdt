47dd2899896c86b99176a3de61cda3c6bdc5fa29
Jonah Graham <jonah@kichwacoders.com>
2016-03-16 00:11:04 +0000

    Bug 472765: use Strings for backend path in Map Entries
    Preserve the user supplied backend path as a string so that it matches
    what GDB (backend) later reports as a file name to resolve.
    
    Condensed history:
    
    Traditionally in source mapping container, the backend path was stored
    as an IPath and that was used to compare against the debug information
    being returned by GDB. This worked well when the platform compiled for
    matched the platform run on, but when there was Windows/Linux together
    that did not work. So as part of Bug 393791 a workaround was done to try
    and use hand crafted MapEntrySourceContainer.createPath() instead of
    Path constructor to preserve some of the differences. This solution had
    its limitations too as UNC paths still could not be represented, See
    open Bug 386675.
    
    Change-Id: I4c8f62114a2c6019dc9d07ce446ac424be8b230c
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_6_8/SourceLookupTest_6_8.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/tests_6_8/SourceLookupTest_6_8.java
@@ -12,7 +12,9 @@ package org.eclipse.cdt.tests.dsf.gdb.tests.tests_6_8;
 
 import org.eclipse.cdt.tests.dsf.gdb.framework.BackgroundRunner;
 import org.eclipse.cdt.tests.dsf.gdb.tests.ITestConstants;
+import org.eclipse.cdt.tests.dsf.gdb.tests.tests_6_6.SourceLookupTest_6_6;
 import org.eclipse.cdt.tests.dsf.gdb.tests.tests_6_7.SourceLookupTest_6_7;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(BackgroundRunner.class)
@@ -20,5 +22,23 @@ public class SourceLookupTest_6_8 extends SourceLookupTest_6_7 {
 	@Override
 	protected void setGdbVersion() {
 		setGdbProgramNamesLaunchAttributes(ITestConstants.SUFFIX_GDB_6_8);
+	}
+
+	/**
+	 * @see SourceLookupTest_6_6#sourceSubstituteAN()
+	 */
+	@Test
+	@Override
+	public void sourceSubstituteAN() throws Throwable {
+		super.sourceSubstituteAN();
+	}
+
+	/**
+	 * @see SourceLookupTest_6_6#sourceSubstituteAN()
+	 */
+	@Test
+	@Override
+	public void sourceSubstituteBreakpointsAN() throws Throwable {
+		super.sourceSubstituteAN();
 	}
 }

628389071558c43c52b666995e1eaa5c4aa67a8f
Jonah Graham <jonah@kichwacoders.com>
2016-03-16 20:41:33 +0000

    Bug 472765: Use gdb's "set substitute-path from to"
    Add support for gdb's "set substitute-path from to" by adding a new
    service called GDBSourceLookup that uses GDB to perform the path
    mapping.
    
    The new GDBSourceLookup service uses the new GdbSourceLookupDirector to
    get the set of paths that need mapping using GDB's "set
    substitute-path" and the director resolves compilation path differently
    so as to leave the GDB backend to handle the local path to compile path
    resolution.
    
    The MappingSourceContainer has been enhanced to allow a per mapping
    container override of the new behaviour. This is a fallback as the
    default behaviour when using GDB is to use "set substitute-path".
    
    The MappingSourceContainerDialog exposes the new option in
    MappingSourceContainer as a checkbox at the bottom of the path mapping
    dialog in a backend agnostic way.
    
    The new code is tested in methods called "sourceSubstitute*" and
    mirror the same tests for the now non-backend handled version
    which are "sourceMapping*". Note that
    doMappingAndLaunch/doSubstituteAndLaunch have been updated to explicitly
    check or uncheck the setIsMappingWithBackendEnabled setting
    
    Change-Id: I122d7c597cd461d8e38c4f82522ccfdf9e51a5ba
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/service/IGDBSourceLookup.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jonah Graham (Kichwa Coders) - initial API and implementation to Add support for gdb's "set substitute-path" (Bug 472765)
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.gdb.service;
+
+import org.eclipse.cdt.debug.core.sourcelookup.MappingSourceContainer;
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
+import org.eclipse.cdt.dsf.debug.service.ISourceLookup;
+
+/**
+ * Extension to the {@link ISourceLookup} service that allows GDB backend to
+ * handle the path mapping source container type -
+ * {@link MappingSourceContainer} - on the GDB side using GDB's
+ * "set substitute-path" mechanism.
+ * 
+ * @since 5.0
+ */
+public interface IGDBSourceLookup extends ISourceLookup {
+
+	/**
+	 * Initialise the source substitutions on the GDB backend (aka do the
+	 * initial "set substitute-path"s)
+	 * 
+	 * @param sourceLookupCtx
+	 * @param rm
+	 */
+	void initializeSourceSubstitutions(ISourceLookupDMContext sourceLookupCtx, RequestMonitor rm);
+
+	/**
+	 * Update the source substitutions on the GDB backend (aka modify the
+	 * "set substitute-path"s)
+	 * 
+	 * @param sourceLookupCtx
+	 * @param rm
+	 *            with the result set to True if a change was made
+	 */
+	void sourceContainersChanged(ISourceLookupDMContext sourceLookupCtx, DataRequestMonitor<Boolean> rm);
+}

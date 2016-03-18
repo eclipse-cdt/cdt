00fa4e79eccec937bcd94713950d7ff51082682d
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2016-03-17 20:56:48 -0400

    Flattening rest of gdb tests
    Change-Id: I352545c2ea3d76154c8079f8efa9aa5d4a3e0032

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/AllRemoteSuites.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/AllRemoteSuites.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 Ericsson and others.
+ * Copyright (c) 2016 QNX Software System and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Ericsson			  - Initial Implementation
+ *     Elena Laskavaia (QNX Software System) - initial API and implementation
  *******************************************************************************/
 package org.eclipse.cdt.tests.dsf.gdb.tests;
 
@@ -17,13 +17,13 @@ import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
 
 /**
- * This suite runs tests for all gdb versions (it will ignore setting of "cdt.tests.dsf.gdb.versions", if you
+ * This suite runs remote tests for all gdb versions (it will ignore setting of "cdt.tests.dsf.gdb.versions", if you
  * want run tests controlled by this var run {@link AutomatedSuite}
  */
 @RunWith(AllTests.class)
-public class AllSuites {
+public class AllRemoteSuites {
 	public static junit.framework.Test suite() {
-		String gdbVersions = String.join(",", ITestConstants.ALL_KNOWN_VERSIONS);
+		String gdbVersions = "gdbserver."+String.join(",gdbserver.", ITestConstants.ALL_KNOWN_VERSIONS);
 		System.setProperty("cdt.tests.dsf.gdb.versions", gdbVersions);
 		TestSuite suite = new TestSuite();
 		suite.addTest(new JUnit4TestAdapter(SuiteGdb.class));

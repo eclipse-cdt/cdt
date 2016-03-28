929d8dfb458403dc776e19c84828e8acaa1f693e
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2016-03-28 13:02:32 -0700

    Consolidated similar tests.
    Change-Id: If8161fdd23956f3cb61660f45d1b1407bb102835

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/CharArrayUtilsTest.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/CharArrayUtilsTest.java
@@ -11,7 +11,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.core.parser.tests;
 
-import org.eclipse.cdt.core.parser.util.CharArrayObjectMap;
 import org.eclipse.cdt.core.parser.util.CharArrayUtils;
 
 import junit.framework.TestCase;
@@ -21,32 +20,11 @@ import junit.framework.TestCase;
  */
 public class CharArrayUtilsTest extends TestCase {
 
-	public void testMapAdd() {
-		CharArrayObjectMap map = new CharArrayObjectMap(4);
-		char[] key1 = "key1".toCharArray();
-		Object value1 = new Integer(43);
-		map.put(key1, value1);
-		
-		char[] key2 = "key1".toCharArray();
-		Object value2 = map.get(key2);
-		assertEquals(value1, value2);
-		
-		for (int i = 0; i < 5; ++i) {
-			map.put(("ikey" + i).toCharArray(), new Integer(i));
-		}
-		
-		Object ivalue1 = map.get("ikey1".toCharArray());
-		assertEquals(ivalue1, new Integer(1));
-		
-		Object ivalue4 = map.get("ikey4".toCharArray());
-		assertEquals(ivalue4, new Integer(4));
-	}
-	
 	public void testEquals_Bug289852() {
 		assertTrue(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 3, "abc".toCharArray(), false));
 		assertFalse(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 4, "abcd".toCharArray(), false));
 		assertTrue(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 2, "ab".toCharArray(), false));
-		
+
 		assertTrue(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 3, "ABC".toCharArray(), true));
 		assertFalse(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 4, "abcd".toCharArray(), true));
 		assertTrue(CharArrayUtils.equals("pre_abc".toCharArray(), 4, 2, "AB".toCharArray(), true));
@@ -74,5 +52,14 @@ public class CharArrayUtilsTest extends TestCase {
 		assertEquals(6, CharArrayUtils.lastIndexOf("".toCharArray(), "123456".toCharArray()));
 		assertEquals(4, CharArrayUtils.lastIndexOf("56".toCharArray(), "123456".toCharArray()));
 		assertEquals(-1, CharArrayUtils.lastIndexOf("123".toCharArray(), "".toCharArray()));
+
+		char[] buffer = "A::B::C".toCharArray();
+
+        assertEquals(CharArrayUtils.lastIndexOf("::".toCharArray(), buffer), 4);
+        assertTrue(CharArrayUtils.equals(CharArrayUtils.lastSegment(buffer, "::".toCharArray()), "C".toCharArray()));
+
+        buffer = "A::B::C:foo".toCharArray();
+        assertEquals(CharArrayUtils.lastIndexOf("::".toCharArray(), buffer), 4);
+        assertTrue(CharArrayUtils.equals(CharArrayUtils.lastSegment(buffer, "::".toCharArray()), "C:foo".toCharArray()));
 	}
 }

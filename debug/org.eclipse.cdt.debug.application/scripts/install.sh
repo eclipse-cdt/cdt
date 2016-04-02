75c2dcebf9f5b0c6cf120136784ace05189f140c
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2016-04-02 18:04:58 -0400

    Stand-alone debugger for Mac OS X
    This commits adds support for the stand-alone debugger on Mac OS X
    for both the RCP (product) and the script. The script had to be
    modified slightly because of the different installation layout on Mac
    and also the use of non-standard arguments to the 'find' command.
    
    Change-Id: I24e4fec98803689dbaadb1dcfa651e431d4188a7
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- debug/org.eclipse.cdt.debug.application/scripts/install.sh
+++ debug/org.eclipse.cdt.debug.application/scripts/install.sh
@@ -15,18 +15,24 @@
 
 SCRIPT_DIR=`dirname $0`
 
+IS_MAC=0
+
 if [ ! -f "$SCRIPT_DIR/../../../eclipse" ]; then
-  echo "$0: error: eclipse executable not found in expected location"
-  echo " "
-  echo "This can occur if you are running this script from your local .eclipse directory"
-  echo "which would mean you are running a shared instance of the Eclipse platform for"
-  echo "your distro and have installed the Stand-alone Debugger from an eclipse.org"
-  echo "download repo.  Downloading the Stand-alone Debugger feature on top of a distro"
-  echo "version of Eclipse Debugger is not supported.  If you are using a distro version"
-  echo "of the Eclipse platform, you should not use this script.  Instead, install the"
-  echo "corresponding Eclipse CDT package for your distro (e.g. eclipse-cdt package)"
-  echo "which will install the Stand-alone Debugger for you."
-  exit 1
+  if [ ! -f "$SCRIPT_DIR/../../../../MacOS/eclipse" ]; then
+    echo "$0: error: eclipse executable not found in expected location"
+    echo " "
+    echo "This can occur if you are running this script from your local .eclipse directory"
+    echo "which would mean you are running a shared instance of the Eclipse platform for"
+    echo "your distro and have installed the Stand-alone Debugger from an eclipse.org"
+    echo "download repo.  Downloading the Stand-alone Debugger feature on top of a distro"
+    echo "version of Eclipse Debugger is not supported.  If you are using a distro version"
+    echo "of the Eclipse platform, you should not use this script.  Instead, install the"
+    echo "corresponding Eclipse CDT package for your distro (e.g. eclipse-cdt package)"
+    echo "which will install the Stand-alone Debugger for you."
+    exit 1
+  else
+    IS_MAC=1
+  fi
 fi
 
 if [ ! -d "$HOME/cdtdebugger" ]; then
@@ -37,7 +43,12 @@ cp "$SCRIPT_DIR/dev.properties" "$HOME/cdtdebugger"
 cp "$SCRIPT_DIR/cdtdebug.sh" "$HOME/cdtdebugger"
 chmod +x "$HOME/cdtdebugger/cdtdebug.sh"
 
-ECLIPSE_HOME=$(cd "$SCRIPT_DIR/../../.." && pwd)
+if [ $IS_MAC -eq 0 ]; then
+  ECLIPSE_HOME=$(cd "$SCRIPT_DIR/../../.." && pwd)
+else
+  ECLIPSE_HOME=$(cd "$SCRIPT_DIR/../../../../MacOS" && pwd)
+fi
+
 # Replace the entire line with tag @#@# by the actual location of the eclipse installation
 sed -i -e "s,^.*@#@#.*$,ECLIPSE_HOME=$ECLIPSE_HOME," "$HOME/cdtdebugger/cdtdebug.sh"
 echo "Installation complete"

ee46a667548324e806af5ed076acb150f24d6493
Nathan Ridge <zeratul976@hotmail.com>
2016-04-19 01:37:05 -0400

    Bug 491834 - Avoid certain kinds of infinte recursion in HeuristicResolver
    This is done by tracking the set of lookups of names in primary template
    scopes performed during a heuristic resolution, and short-circuiting a
    resoluton if the same lookup is attempted twice.
    
    Change-Id: I512cdc9493d1ac91b1f77603d816a33312d4be00

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/SemanticHighlightingTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/SemanticHighlightingTest.java
@@ -546,4 +546,21 @@ public class SemanticHighlightingTest extends TestCase {
     public void testOverloadedOperatorInMacroExpansion_371839() throws Exception {
     	makeAssertions();
     }
+    
+    //	template<unsigned... _Indexes>                   //$templateParameter
+    //	struct _Index_tuple {                            //$class
+    //		typedef _Index_tuple<_Indexes..., sizeof...(_Indexes)> __next;  //$class,templateParameter,templateParameter,typedef
+    //	};
+	//	template<unsigned _Num>                          //$templateParameter
+	//	struct _Build_index_tuple {                      //$class
+	//		typedef typename _Build_index_tuple<_Num - 1>::__type::__next __type;  //$class,templateParameter,class,class,typedef
+	//	};
+	//
+	//	template<>
+	//	struct _Build_index_tuple<0> {                   //$class
+	//		typedef _Index_tuple<> __type;               //$class,typedef
+	//	};
+    public void testRecursion_491834() throws Exception {
+    	makeAssertions();
+    }
 }

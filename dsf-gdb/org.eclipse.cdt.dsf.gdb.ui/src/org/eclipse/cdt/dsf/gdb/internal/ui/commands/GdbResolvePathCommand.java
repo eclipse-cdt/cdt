898fac9cd09b0c918b60e90f436211ab50f65d5d
Stefan Sprenger <stefan@sprenger.software>
2016-05-23 09:38:32 +0200

    Bug 491514 - Source Lookup better heuristics
    Change-Id: Iebad1c375ddcad0f399f97de0ecb970cef3c4731
    Signed-off-by: Stefan Sprenger <stefan@sprenger.software>

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/commands/GdbResolvePathCommand.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2016 Stefan Sprenger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * 
+ * Bug 491514: Command to handle path mapping via wizard
+ * 
+ * Contributors:
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.gdb.internal.ui.commands;
+
+import java.util.List;
+
+import org.eclipse.cdt.debug.core.model.IResolveHandler;
+import org.eclipse.cdt.dsf.concurrent.DsfExecutor;
+import org.eclipse.cdt.dsf.gdb.actions.IResolve;
+import org.eclipse.cdt.dsf.gdb.internal.ui.GdbUIPlugin;
+import org.eclipse.cdt.dsf.service.DsfServicesTracker;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.debug.core.ILaunch;
+
+public class GdbResolvePathCommand implements IResolveHandler, IResolve {
+
+	private final ILaunch fLaunch;
+	private final DsfExecutor fExecutor;
+    private final DsfServicesTracker fTracker;
+    
+    public GdbResolvePathCommand(DsfSession session, ILaunch launch) {
+    	fLaunch = launch;
+        fExecutor = session.getExecutor();
+        fTracker = new DsfServicesTracker(GdbUIPlugin.getBundleContext(), session.getId());
+    }    
+	
+	@Override
+	public void resolve(List<String> fileList) {
+		// TODO DIALOG
+		System.out.println("RESOLVE FILELLIST: "+fileList);
+	}
+
+}
+

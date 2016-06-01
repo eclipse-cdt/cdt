6c98ec48d505579a71129452cb361a237d88918c
Nathan Ridge <zeratul976@hotmail.com>
2016-06-01 03:26:02 -0400

    Bug 495095 - Fix incorrect tests
    Change-Id: I90e211b9bbf55245bc15bc6239555267e8d88f92

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist2/CPPParameterGuessingTests.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist2/CPPParameterGuessingTests.java
@@ -91,8 +91,7 @@ public class CPPParameterGuessingTests extends AbstractContentAssistTest {
 	//		tFunc<aClass> (
 	public void testTemplateFunction() throws Exception {
 		Map<String, String[][]> resultsMap = new HashMap<>();
-		resultsMap.put("tFunc<aClass> (x, y)", new String[][] { { "x" },
-				{ "y" } });
+		resultsMap.put("tFunc<aClass> (x, y)", new String[][] { { "aTypeObj" }, { "bTypeObj" } });
 		assertParametersGuesses(resultsMap);
 	}
 
@@ -135,7 +134,7 @@ public class CPPParameterGuessingTests extends AbstractContentAssistTest {
 		Map<String, String[][]> resultsMap = new HashMap<>();
 		resultsMap.put("aMethod(c)", new String[][] { { "charX", "charY", "inCall" } });
 		resultsMap.put("aMethod(c, x)", new String[][] {
-				{ "charX", "charY"}, { "inCall" } });
+				{ "charX", "charY", "inCall"}, { "charX", "charY", "inCall" } });
 		assertParametersGuesses(resultsMap);
 	}
 }

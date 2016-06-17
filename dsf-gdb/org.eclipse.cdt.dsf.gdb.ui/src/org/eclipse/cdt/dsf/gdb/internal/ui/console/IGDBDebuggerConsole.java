cf6e2b4888c7b3400b8eea1b7ea5717827e8dd63
Marc Dumais <marc.dumais@ericsson.com>
2016-06-17 12:58:48 -0400

    bug 498782 - [debug] synchronize selection between the DV and GDB
    This adds a new service, GDBFocusSynchronizer, that helps keep the
    internal GDB selection and the Debug View selection synchronized.
    
    Change-Id: I021b3f65d61e82f6971bdb9232369b6fdf58ea5b

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/console/IGDBDebuggerConsole.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.cdt.dsf.gdb.internal.ui.console;
+
+import org.eclipse.cdt.debug.ui.debuggerconsole.IDebuggerConsole;
+import org.eclipse.cdt.dsf.gdb.internal.service.IGDBFocusSynchronizer;
+import org.eclipse.cdt.dsf.gdb.internal.ui.GdbUIPlugin;
+import org.eclipse.cdt.dsf.gdb.launching.GdbLaunch;
+import org.eclipse.cdt.dsf.service.DsfServicesTracker;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.ui.DebugUITools;
+
+/**
+ * GDB specifics to IDebuggerConsole e.g. default implementations
+ */
+public interface IGDBDebuggerConsole extends IDebuggerConsole {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.cdt.debug.ui.debuggerconsole.IDebuggerConsole#consoleSelected()
+	 */
+	@Override
+	public default void consoleSelected() {
+		DsfSession session = ((GdbLaunch)getLaunch()).getSession();
+		if (!session.isActive()) {return;}
+
+		// only trigger the DV selection if the current selection is in
+		// a different session
+		IAdaptable context = DebugUITools.getDebugContext();
+		if (context != null) {
+			ILaunch selectedLaunch =  context.getAdapter(ILaunch.class);
+			if (getLaunch().equals(selectedLaunch)) {
+				return;
+			}
+		}
+
+		session.getExecutor().execute(new Runnable() {
+			@Override
+			public void run() {
+				DsfServicesTracker tracker = new DsfServicesTracker(GdbUIPlugin.getBundleContext(), session.getId());
+				IGDBFocusSynchronizer gdbSync = tracker.getService(IGDBFocusSynchronizer.class);
+				tracker.dispose();
+				if (gdbSync != null) {
+					gdbSync.sessionSelected();
+				}
+			}
+		});
+	}
+}

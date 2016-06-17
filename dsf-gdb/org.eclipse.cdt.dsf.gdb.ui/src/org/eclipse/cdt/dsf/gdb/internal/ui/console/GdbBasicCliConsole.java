9e8409447b15f6d0a0ff14e97939696c421e31ae
Marc Dumais <marc.dumais@ericsson.com>
2016-06-17 12:58:48 -0400

    bug 498782 - [debug] improve synchronization between the DV and GDB
    This add a new service, GDBSynchronizer, that helps keep the internal
    GDB selection and the Debug View selection synchronized.
    
    Change-Id: I021b3f65d61e82f6971bdb9232369b6fdf58ea5b

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/console/GdbBasicCliConsole.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/console/GdbBasicCliConsole.java
@@ -228,4 +228,9 @@ public class GdbBasicCliConsole extends IOConsole implements IDebuggerConsole, I
             return Status.OK_STATUS;
         }
     }
+
+	@Override
+	public void consoleSelected() {
+		// nothing to do
+	}
 }

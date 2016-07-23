4289aa7b0d8961230ccad7192eab1014e81dc26b
Jonah Graham <jonah@kichwacoders.com>
2016-07-23 13:54:38 +0100

    Bug 494650: Refactor BaseTestCase to allow multiple launches per test
    Bug 494650 has an issues when multiple launches are terminated, at
    present the test infrastructure makes it very difficult to launch
    multiple launches within one test. This commit refactors the base test
    case to enable launching additional tests with doLaunchInner.
    
    Change-Id: I501edf4e485c304b0a00c18f1d5e3813011a0491
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

---
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/GDBTestTest.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jonah Graham (Kichwa Coders) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.tests.dsf.gdb.tests;
+
+import static org.junit.Assert.assertFalse;
+
+import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
+import org.eclipse.cdt.dsf.gdb.launching.GdbLaunch;
+import org.eclipse.cdt.tests.dsf.gdb.framework.BaseParametrizedTestCase;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * These are basic tests to demonstrate that the test infrastructure works as
+ * expected. This class can also be used as a starting point for additional test
+ * cases as this class tries to stay simple.
+ */
+@RunWith(Parameterized.class)
+public class GDBTestTest extends BaseParametrizedTestCase {
+	private static final String EXEC_NAME = "MultiThread.exe";
+
+	@Override
+	protected void setLaunchAttributes() {
+		super.setLaunchAttributes();
+		setLaunchAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_NAME, EXEC_PATH + EXEC_NAME);
+	}
+
+	/**
+	 * Test that we can launch, as the launch and terminate is all handled in
+	 * before/after test, this test looks pretty minimal.
+	 */
+	@Test
+	public void testLaunch() {
+		assertFalse("Launch should be running", getGDBLaunch().isTerminated());
+	}
+	
+	/**
+	 * Test that test infrastructure allows multiple launches on same launch config. 
+	 */
+	@Test
+	public void testMultipleLaunch() throws Exception {
+		Assume.assumeFalse("Test framework only supports multiple launches for non-remote", remote);
+		
+		// get the launch that was created automatically
+		GdbLaunch autoLaunched = getGDBLaunch();
+		
+		autoLaunched.terminate();
+		waitUntil("Launch did not terminate", () -> autoLaunched.isTerminated());
+		
+		// launch an additional launch
+		GdbLaunch secondLaunch = doLaunchInner();
+		assertFalse("Second launch should be running", secondLaunch.isTerminated());
+		
+		secondLaunch.terminate();
+		waitUntil("Second launch did not terminate", () -> secondLaunch.isTerminated());
+	}
+}

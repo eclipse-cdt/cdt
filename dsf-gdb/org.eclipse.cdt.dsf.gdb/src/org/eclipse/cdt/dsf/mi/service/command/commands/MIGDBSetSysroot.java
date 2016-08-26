a4ef3983e82d44bde4855469edf5b16f81849127
Vincent Guignot <vincent.guignot@ingenico.com>
2016-08-26 15:06:35 +0200

    Bug 497693/ Sysroot with spaces
    If the sysroot path contains a space, CDT adds doubles quote to escape
    the space (in MIStandardParameterAdjustable class) . But Gdb client (7.5
    and 7.11) doesn’t understand the double quotes path.
    This patch do not add double quotes when the path contains spaces.
    Add test case.
    
    Change-Id: I8c54fa625ce1fb1f1b9249361ca07060b56ba90c
    Signed-off-by: Vincent Guignot <vincent.guignot@ingenico.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/MIGDBSetSysroot.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/MIGDBSetSysroot.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009  Ericsson and others.
+ * Copyright (c) 2008, 2016  Ericsson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,25 +7,26 @@
  *
  * Contributors:
  *     Ericsson - Initial API and implementation
+ *     Ingenico	- Sysroot with spaces (Bug 497693)
  *******************************************************************************/
 package org.eclipse.cdt.dsf.mi.service.command.commands;
 
 import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlDMContext;
 
 /**
- * 
+ *
  *     -gdb-set sysroot PATH
  * @since 1.1
- * 
+ *
  */
-public class MIGDBSetSysroot extends MIGDBSet 
+public class MIGDBSetSysroot extends MIGDBSet
 {
 	public MIGDBSetSysroot(ICommandControlDMContext ctx, String path) {
-		super(ctx, new String[] {"sysroot", path});//$NON-NLS-1$
+		super(ctx, new String[] {"sysroot", path}, x-> new MINoChangeAdjustable(x));//$NON-NLS-1$
 	}
-	
+
 	// Using /dev/null is the recommended way to disable sysroot
 	public MIGDBSetSysroot(ICommandControlDMContext ctx) {
-		this(ctx, "/dev/null"); //$NON-NLS-1$ 
+		this(ctx, "/dev/null"); //$NON-NLS-1$
 	}
 }

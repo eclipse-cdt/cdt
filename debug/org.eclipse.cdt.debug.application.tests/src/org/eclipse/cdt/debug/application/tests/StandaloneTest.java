f692515981bd5015dc08beaa6160e75871f90384
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2016-09-14 17:55:44 -0400

    Try to force GTK2 for SWTBot failing test
    Change-Id: I1907d458981a574c0d46b1237e3a8f539e769b8d
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- debug/org.eclipse.cdt.debug.application.tests/src/org/eclipse/cdt/debug/application/tests/StandaloneTest.java
+++ debug/org.eclipse.cdt.debug.application.tests/src/org/eclipse/cdt/debug/application/tests/StandaloneTest.java
@@ -10,16 +10,31 @@
  *******************************************************************************/
 package org.eclipse.cdt.debug.application.tests;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.results.Result;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+import org.eclipse.swtbot.swt.finder.waits.WaitForObjectCondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.AfterClass;
 
@@ -33,6 +48,76 @@ public abstract class StandaloneTest {
 	protected static SWTBotView projectExplorer;
 	private static final Logger fLogger = Logger.getRootLogger();
 
+    static class WaitForShell extends WaitForObjectCondition<Shell> {
+
+        private Shell[] fShells;
+
+        WaitForShell(Matcher<Shell> matcher) {
+            super(matcher);
+        }
+
+        @Override
+        public String getFailureMessage() {
+            String shellTitles = UIThreadRunnable.syncExec(new Result<String>() {
+                @Override
+                public String run() {
+                    return String.join(", ", Arrays.asList(fShells).stream().map(s -> "\"" + s.hashCode() + ": " + s.getText() + "\"").collect(Collectors.toList()));
+                }
+            });
+            captureScreenshot("screenshots/fail.png");
+            return "Could not find shell matching: " + matcher + ". Found shell with titles: " + shellTitles; //$NON-NLS-1$
+        }
+
+        @Override
+        protected List<Shell> findMatches() {
+            fShells = findShells();
+            ArrayList<Shell> matchingShells = new ArrayList<>();
+            for (Shell shell : fShells) {
+                if (!shell.isDisposed() && matcher.matches(shell)) {
+                    matchingShells.add(shell);
+                }
+            }
+            return matchingShells;
+        }
+
+        /**
+         * Subclasses may override to find other shells.
+         */
+        Shell[] findShells() {
+            return bot.getFinder().getShells();
+        }
+
+    }
+    
+    private static void printJobs() {
+        Job[] jobs = Job.getJobManager().find(null);
+        for (Job job : jobs) {
+            System.err.println(job.toString() + " state: " + jobStateToString(job.getState())); //$NON-NLS-1$
+            Thread thread = job.getThread();
+            if (thread != null) {
+                for (StackTraceElement stractTraceElement : thread.getStackTrace()) {
+                    System.err.println("  " + stractTraceElement); //$NON-NLS-1$
+                }
+            }
+            System.err.println();
+        }
+    }
+    
+    private static String jobStateToString(int jobState) {
+        switch (jobState) {
+        case Job.RUNNING:
+            return "RUNNING"; //$NON-NLS-1$
+        case Job.WAITING:
+            return "WAITING"; //$NON-NLS-1$
+        case Job.SLEEPING:
+            return "SLEEPING"; //$NON-NLS-1$
+        case Job.NONE:
+            return "NONE"; //$NON-NLS-1$
+        default:
+            return "UNKNOWN"; //$NON-NLS-1$
+        }
+    }
+	
 	public static void init(String projectName) throws Exception {
 		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
 		SWTBotPreferences.TIMEOUT = 20000;
@@ -40,20 +125,37 @@ public abstract class StandaloneTest {
 		fLogger.removeAllAppenders();
 		fLogger.addAppender(new ConsoleAppender(new SimpleLayout(), ConsoleAppender.SYSTEM_OUT));
 
+		SWTBotUtils.initialize();
+
 		bot = new SWTBot();
 		Utilities.getDefault().buildProject(projectName);
 		final IPath executablePath = Utilities.getDefault().getProjectPath(projectName).append("a.out"); //$NON-NLS-1$
 		bot.waitUntil(new WaitForFileCondition(executablePath));
 
-		bot.waitUntil(Conditions.shellIsActive(DEBUG_NEW_EXECUTABLE_TITLE));
+		captureScreenshot("screenshots/before.png");
+		bot.waitUntil(new WaitForShell(WidgetMatcherFactory.withText(DEBUG_NEW_EXECUTABLE_TITLE)));
 		SWTBotShell executableShell = bot.shell(DEBUG_NEW_EXECUTABLE_TITLE);
 		executableShell.setFocus();
+		bot.waitUntil(Conditions.shellIsActive(DEBUG_NEW_EXECUTABLE_TITLE));
 
 		executableShell.bot().textWithLabel("Binary: ").typeText(executablePath.toOSString());
 		executableShell.bot().button("OK").click();
 
-		bot.waitUntil(Conditions.shellIsActive(C_C_STAND_ALONE_DEBUGGER_TITLE));
+		bot.waitUntil(new WaitForShell(WidgetMatcherFactory.withText(C_C_STAND_ALONE_DEBUGGER_TITLE)));
 		mainShell = bot.shell(C_C_STAND_ALONE_DEBUGGER_TITLE);
+		mainShell.setFocus();
+		bot.waitUntil(Conditions.shellIsActive(C_C_STAND_ALONE_DEBUGGER_TITLE));
+	}
+
+	static void captureScreenshot(String pathname) {
+		UIThreadRunnable.syncExec(() ->  {
+			try {
+				ImageHelper.grabImage(Display.getDefault().getBounds()).writePng(new File(pathname));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 	}
 
 	@After

dd07fd4b6b8c69ffbd5b60e5de28b85a0a94137f
Jonah Graham <jonah@kichwacoders.com>
2016-10-13 14:29:49 +0100

    Bug 505868: Split clean command when cleaning lots of files
    This change overcomes the Cannot run program "rm": Command line
    too long error when there are hundreds to thousands of files.
    
    This change only applies to the interal builder.
    
    (This contains the tests only, next patch has the fix too.)
    
    Change-Id: Idc32067e27d76e3b438b2b1a07376859c7c8d1e4

---
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/ManagedBuildClean.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 20116 Kichwa Coders Ltd and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jonah Graham (Kichwa Coders) - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.managedbuilder.core.tests;
+
+import java.util.Collection;
+
+import org.eclipse.cdt.managedbuilder.testplugin.AbstractBuilderTest;
+import org.eclipse.cdt.managedbuilder.testplugin.ManagedBuildTestHelper;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+public class ManagedBuildClean extends AbstractBuilderTest {
+	private static final String PROJ_PATH = "testCleanProjects";
+	private IProject fInternalBuilderProject;
+	private IProject fExternalBuilderProject;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		IWorkspaceDescription wsDescription = ResourcesPlugin.getWorkspace().getDescription();
+		wsDescription.setAutoBuilding(false);
+		ResourcesPlugin.getWorkspace().setDescription(wsDescription);
+		assertNotNull("Cannot create testCleanInternal project",
+				fInternalBuilderProject = ManagedBuildTestHelper.loadProject("testCleanInternal", PROJ_PATH));
+		assertNotNull("Cannot create testCleanExternal project",
+				fExternalBuilderProject = ManagedBuildTestHelper.loadProject("testCleanExternal", PROJ_PATH));
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		ManagedBuildTestHelper.removeProject(fInternalBuilderProject.getName());
+	}
+
+	public void testCleanInternal() throws Exception {
+		helperTestClean(fInternalBuilderProject, false);
+	}
+
+	public void testCleanExternal() throws Exception {
+		helperTestClean(fExternalBuilderProject, true);
+	}
+
+	private void helperTestClean(IProject project, boolean externalBuilder) throws CoreException {
+
+		// do a build and ensure files are present
+		project.build(IncrementalProjectBuilder.FULL_BUILD, null);
+		Collection<IResource> resources = getProjectBuildExeResources(project.getName(), "Debug",
+				"src/" + project.getName(), externalBuilder);
+		for (IResource resource : resources) {
+			assertTrue("Resource not found: " + resource, resource.exists());
+		}
+
+		// do a clean and make sure files are gone
+		project.build(IncrementalProjectBuilder.CLEAN_BUILD, null);
+		for (IResource resource : resources) {
+			if (!(resource instanceof IFile)) {
+				// Only files are removed by clean, not folders
+				continue;
+			}
+			if (externalBuilder
+					&& (resource.getName().endsWith(".mk") || resource.getName().equals("makefile"))) {
+				// makefiles are not removed when cleaning
+				continue;
+			}
+			assertFalse("Resource not deleted: " + resource, resource.exists());
+		}
+	}
+
+}

a534bbee4f47fc77dba4d3471a0e8ad2566b3b45
jantje <eclipse@baeyens.it>
2016-10-21 02:10:32 +0200

    Bug 422377: Use correct build variable when multiple outputTypes
    When you have multiple output types the buildvariable is always the 
    build variable of the primary output type.
    This changes makes sure the build variable of the "current" output type
    is used
    
    
    Change-Id: I51b998a851b44a544191f68c3e0746947cfd1ed1
    Signed-off-by: jantje <eclipse@baeyens.it>
    
    # Conflicts:
    #	build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
    
    
    Change-Id: Ibb147637b1c32104fe18a675859a803e9fab8b4c
    Signed-off-by: jantje <eclipse@baeyens.it>

--- build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
@@ -3092,7 +3092,12 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 		IOutputType[] outTypes = tool.getOutputTypes();
 		if (outTypes != null && outTypes.length > 0) {
 			for (IOutputType type : outTypes) {
-				boolean primaryOutput = type.getPrimaryOutput();
+				/*tool.getPrimaryOutputType does not make sense because there can be multiple 
+				primary output types
+				So the code below should actually be
+				boolean primaryOutput = tool.getPrimaryOutputTypes().contains(type));
+				*/
+				boolean primaryOutput = (type == tool.getPrimaryOutputType());
 				//if (primaryOutput && ignorePrimary) continue;
 				String outputPrefix = type.getOutputPrefix();
 
@@ -3257,7 +3262,9 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 					inPaths[0] = sourceLocation;
 					IPath[] outPaths = nameProvider.getOutputNames(tool, inPaths);
 					if (outPaths != null) {
-						for (int j=0; j<outPaths.length; j++) {
+						usedOutType=type;
+						for (int j = 0; j < outPaths.length; j++) {
+
 							IPath outPath = outPaths[j];
 							String outputName = outPaths[j].toString();
 

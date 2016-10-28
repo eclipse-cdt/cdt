a87ee7a4d5d827bc6109f5a19cb4eaafb57366e0
Marc Khouzam <marc.khouzam@ericsson.com>
2016-10-28 15:57:22 -0400

    Don't force the GDB console to be shown when created.
    When a new launch is triggered and a new GDB console created, it was
    shown automatically.  However, if there was a current debug session
    where the selection was a stack frame, our sticky policy
    (DefaultDsfSelectionPolicy) prevents the new session from being
    selected, so the new GDB console shown didn't match the selection in the
    DV.
    
    With this patch, when a new GDB console is created, we don't show it
    automatically, but let other logic figure out if it should be shown or
    not.
    
    Change-Id: If826677a6fb0f925f6b397a13f61fc75fc892748

--- debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/views/debuggerconsole/DebuggerConsoleView.java
+++ debug/org.eclipse.cdt.debug.ui/src/org/eclipse/cdt/debug/internal/ui/views/debuggerconsole/DebuggerConsoleView.java
@@ -211,7 +211,13 @@ implements IConsoleView, IDebuggerConsoleView, IConsoleListener, IPropertyChange
 								DebuggerConsoleWorkbenchPart part = new DebuggerConsoleWorkbenchPart(registered, getSite());
 								fConsoleToPart.put(registered, part);
 								fPartToConsole.put(part, registered);
+								// Must call partActivated() to create the page
+								// However, this will also show the page, which is not
+								// what we want.  Therefore, let's force the previous
+								// page to be shown again right after.
+								IDebuggerConsole previouslyShown = getCurrentConsole();
 								partActivated(part);
+								display(previouslyShown);
 								break;
 							}
 						}

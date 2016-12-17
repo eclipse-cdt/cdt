0b6dfff78c9a3b1065600732f66363f850e0ae69
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2016-12-16 21:11:44 -0400

    Revert "Added stack trace to diagnostic messages."
    This reverts commit 5f1c234b774f648a1131d7e8541bc62152ed74b3.
    
    Change-Id: Ic821628d828ca3b9eeed1a723f97b48a17767419

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/ASTInternal.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/ASTInternal.java
@@ -53,8 +53,8 @@ public class ASTInternal {
 		if (binding instanceof ICInternalBinding) {
 			return ((ICInternalBinding) binding).getDefinition();
 		}
-		CCorePlugin.log(new Exception("ASTInternal.getDefinitionOfBinding() called for binding of type " +  //$NON-NLS-1$ 
-				binding.getClass().getName()));
+		CCorePlugin.log("ASTInternal.getDefinitionOfBinding() called for binding of type " +  //$NON-NLS-1$ 
+				binding.getClass().getName());
 		assert false;
 		return null;
 	}

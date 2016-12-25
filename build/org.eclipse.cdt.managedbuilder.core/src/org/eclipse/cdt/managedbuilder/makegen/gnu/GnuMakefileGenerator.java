1f2355f07a60140d3885094e5fc9c6a03afc7981
Morten Kristiansen <millibit@gmail.com>
2016-12-25 15:58:07 +0100

    [Bug 340300] Parallel build using generated makefile doesn't manage dependencies
    Pre-build will always run in parallel to compilation of source files unless each and every source file depends on pre-build. Also, when parallel build was enabled, the "Make build target" under "Workbench Build Behavior" was ignored. Instead of doing make -j all, Eclipse ran make -j pre-build main-build. While the intentions are good, make will attempt to build pre-build and main-build in parallel for previous stated reasons.
    
    This patch changes two things:
    
    1. Eclipse will consistently respect the "Workbench Build Behavior" for both single- and multi-process build.
    2. The generated makefile is changed to guarantee pre-build is run first.
       Changed from
    
           all: pre-build main-build
    
       to
    
           all:
               $(MAKE) pre-build
               $(MAKE) main-build
    
    Change-Id: Icf3a1057ee3b3cc8a04a433820492a4f469e17dd
    Signed-off-by: Morten Kristiansen <millibit@gmail.com>

--- build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu/GnuMakefileGenerator.java
@@ -1349,7 +1349,9 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 		}
 */
         // If a prebuild step exists, redefine the all target to be
-		// all: {pre-build} main-build
+		// all: 
+		//     $(MAKE) pre-build
+		//	   $(MAKE) main-build
 		// and then reset the "traditional" all target to main-build
 		// This will allow something meaningful to happen if the generated
 		// makefile is
@@ -1361,17 +1363,14 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 			// Add the comment for the "All" target
 			buffer.append(COMMENT_SYMBOL).append(WHITESPACE).append(ManagedMakeMessages.getResourceString(ALL_TARGET)).append(NEWLINE);
 
-			buffer.append(defaultTarget).append(WHITESPACE);
-			buffer.append(PREBUILD).append(WHITESPACE);
-
-			// Reset defaultTarget for now and for subsequent use, below
-			defaultTarget = MAINBUILD;
-			buffer.append(defaultTarget);
-
+			// Invoke make multiple times to ensure pre-build is executed before main-build
+			buffer.append(defaultTarget).append(NEWLINE);
+			buffer.append(TAB).append("$(MAKE) ").append(PREBUILD).append(NEWLINE); //$NON-NLS-1$
+			buffer.append(TAB).append("$(MAKE) ").append(MAINBUILD).append(NEWLINE); //$NON-NLS-1$		
+			buffer.append(NEWLINE);
+			
 			// Update the defaultTarget, main-build, by adding a colon, which is
-			// needed below
-			defaultTarget = defaultTarget.concat(COLON);
-			buffer.append(NEWLINE).append(NEWLINE);
+			defaultTarget = MAINBUILD.concat(COLON);
 
 			// Add the comment for the "main-build" target
 			buffer.append(COMMENT_SYMBOL).append(WHITESPACE).append(ManagedMakeMessages.getResourceString(MAINBUILD_TARGET)).append(NEWLINE);

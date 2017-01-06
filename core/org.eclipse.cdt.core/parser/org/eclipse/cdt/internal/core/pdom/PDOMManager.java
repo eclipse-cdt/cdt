eab632b42c1674999614dcd9be3971a9fcc82448
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2017-01-06 13:46:38 -0800

    Bug 510054 - Unrecoverable index file corruption
    Change-Id: Iba386baf2c62f10044c4ffc1f399823a94aacf84

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/PDOMManager.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/pdom/PDOMManager.java
@@ -384,8 +384,8 @@ public class PDOMManager implements IWritableIndexManager, IListener {
 			WritablePDOM pdom;
 			try {
 				pdom= new WritablePDOM(dbFile, new PDOMProjectIndexLocationConverter(project), getLinkageFactories());
-			} catch (CoreException e) {
-				CCorePlugin.log("Failed to open C/C++ index " + dbFile.getAbsolutePath() //$NON-NLS-1$
+			} catch (CoreException | RuntimeException e) {
+				CCorePlugin.log("Failed to open C/C++ index file " + dbFile.getAbsolutePath() //$NON-NLS-1$
 						+ " - rebuilding the index", e); //$NON-NLS-1$
 				dbFile.delete();
 				fromScratch= true;

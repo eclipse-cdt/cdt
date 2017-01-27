f1c812e1fef20477feec493ff38f9d755885c99c
Alvaro Sanchez-Leon <alvsan09@gmail.com>
2017-01-27 13:39:12 -0500

    Bug 511243 - Unit test validating run control program-interrupt using CLI or MI
    The tests validate the use of CLI (e.g. Ctrl-C) or MI (i.e.
    -exec-interrupt) to interrupt the target program.
    
    The MI command is used when the target is running in async mode,
    which is mandatory when using the Full CLI console.
    
    At the time of writing async mode is nut supported for Windows and
    MAC, therefore these tests rely on a new mechanism to override specific
    DSF-GDB services.
    
    Change-Id: Ie4ab30f07640d112fff9aec2b4f348a27bfe188b

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/launching/TestLaunchDelegate.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/launching/TestLaunchDelegate.java
@@ -11,7 +11,11 @@
 package org.eclipse.cdt.tests.dsf.gdb.launching; 
 
 import org.eclipse.cdt.dsf.concurrent.ThreadSafe;
+import org.eclipse.cdt.dsf.debug.service.IDsfDebugServicesFactory;
 import org.eclipse.cdt.dsf.gdb.launching.GdbLaunchDelegate;
+import org.eclipse.cdt.dsf.gdb.service.GdbDebugServicesFactory;
+import org.eclipse.cdt.tests.dsf.gdb.framework.BaseTestCase;
+import org.eclipse.cdt.tests.dsf.gdb.framework.ServiceFactoriesManager;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -24,6 +28,9 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 @ThreadSafe
 public class TestLaunchDelegate extends GdbLaunchDelegate
 {
+	// A Test Debug Services factory that may be provided for the associated test case
+	private GdbDebugServicesFactory fTestServicesFactory;
+
 	@Override
 	protected IProject[] getBuildOrder(ILaunchConfiguration configuration,
 			String mode) throws CoreException {
@@ -43,6 +50,8 @@ public class TestLaunchDelegate extends GdbLaunchDelegate
 
     @Override
     public boolean preLaunchCheck(ILaunchConfiguration config, String mode, IProgressMonitor monitor) throws CoreException {
+    	// Make sure we start the launch with no pre-defined services factory
+    	fTestServicesFactory = null;
     	// Don't override the base method to allow it to set the GdbProcessFactory
     	// which LaunchConfigurationAndRestartTest.testExitCodeSet() depends on
     	return super.preLaunchCheck(config, mode, monitor);
@@ -50,6 +59,12 @@ public class TestLaunchDelegate extends GdbLaunchDelegate
     
     @Override
 	public boolean finalLaunchCheck(ILaunchConfiguration configuration, String mode, IProgressMonitor monitor) throws CoreException {
+    	// Check if this test has registered a services factory for this launch
+    	String servicesFactoryId = configuration.getAttribute(ServiceFactoriesManager.DEBUG_SERVICES_FACTORY_KEY, "");
+    	if (servicesFactoryId.length() > 0) {
+    		// A services factory has been registered, so lets resolve it
+    		fTestServicesFactory = BaseTestCase.getServiceFactoriesManager().removeTestServicesFactory(servicesFactoryId);
+    	}
         return true;
     }
     
@@ -62,4 +77,14 @@ public class TestLaunchDelegate extends GdbLaunchDelegate
     	return super.checkBinaryDetails(config); 
     }
 
+	@Override
+	protected IDsfDebugServicesFactory newServiceFactory(ILaunchConfiguration config, String version) {
+		if (fTestServicesFactory != null) {
+			// A services factory has been provided for this test case
+			return fTestServicesFactory;
+		}
+
+		// Use the original services factory
+		return super.newServiceFactory(config, version);
+	}
 }

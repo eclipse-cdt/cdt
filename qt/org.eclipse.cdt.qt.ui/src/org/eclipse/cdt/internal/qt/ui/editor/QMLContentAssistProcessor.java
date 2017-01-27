56d950b863dd1555a2526f5915d3453ee6d539ff
Doug Schaefer <dschaefer@blackberry.com>
2017-01-27 15:12:01 -0500

    Turn off QML content assist for now.
    It's throwing an exception in acorn.js. We need to revisit this anyway
    in light of the LSP.
    
    Change-Id: Ia6fd9d7bdd5d5133711947d37c412996ba23f19a

--- qt/org.eclipse.cdt.qt.ui/src/org/eclipse/cdt/internal/qt/ui/editor/QMLContentAssistProcessor.java
+++ qt/org.eclipse.cdt.qt.ui/src/org/eclipse/cdt/internal/qt/ui/editor/QMLContentAssistProcessor.java
@@ -7,23 +7,15 @@
  *******************************************************************************/
 package org.eclipse.cdt.internal.qt.ui.editor;
 
-import java.io.File;
-import java.util.Collection;
-
-import javax.script.ScriptException;
-
 import org.eclipse.cdt.internal.qt.ui.Activator;
 import org.eclipse.cdt.qt.core.IQMLAnalyzer;
-import org.eclipse.cdt.qt.core.QMLTernCompletion;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.jface.text.contentassist.IContextInformationValidator;
-import org.eclipse.ui.IFileEditorInput;
 
 public class QMLContentAssistProcessor implements IContentAssistProcessor {
 	private static final IContextInformation[] NO_CONTEXTS = {};
@@ -38,6 +30,7 @@ public class QMLContentAssistProcessor implements IContentAssistProcessor {
 
 	@Override
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
+		/*
 		IDocument document = viewer.getDocument();
 		String prefix = lastWord(document, offset);
 		// Save the file
@@ -65,6 +58,8 @@ public class QMLContentAssistProcessor implements IContentAssistProcessor {
 		} catch (NoSuchMethodException | ScriptException e) {
 			Activator.log(e);
 		}
+		*/
+
 		return NO_COMPLETIONS;
 	}
 

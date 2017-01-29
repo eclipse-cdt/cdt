e832c49faf46d9014e2953b50bd6d16b4ff68532
Nathan Ridge <zeratul976@hotmail.com>
2017-01-29 17:36:11 -0500

    Bug 510706 - Follow-up to ensure a UniqueType's contribution to a signature is unique
    Change-Id: Iceaf946c073455f10ffd8d95013442da91309be3

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/SignatureBuilder.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/SignatureBuilder.java
@@ -81,7 +81,9 @@ class SignatureBuilder implements ITypeMarshalBuffer {
 			// a need to write it to the index, but it can appear in a signature
 			// during partial ordering of function templates.
 			appendSeparator();
-			fBuffer.append("<UniqueType>");  //$NON-NLS-1$
+			fBuffer.append("Unique(@");  //$NON-NLS-1$
+			fBuffer.append(Integer.toHexString(System.identityHashCode(type)));
+			fBuffer.append(')');
 		} else {
 			assert false : "Cannot serialize " + ASTTypeUtil.getType(type) + " (" + type.getClass().getName() + ")"; //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
 			putShort(UNSTORABLE_TYPE);

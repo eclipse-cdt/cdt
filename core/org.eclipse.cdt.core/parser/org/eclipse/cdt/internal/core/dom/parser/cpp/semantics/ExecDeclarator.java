1f7b0136347d8813ccee9a33c830e39ef3a64c2c
Sergey Prigogin <eclipse.sprigogin@gmail.com>
2017-02-02 20:17:56 -0800

    Bug 511606 - ClassCastException: CPPTypedefSpecialization cannot be cast to IArrayType
    Change-Id: Iac14f1a84323e2a0fee01cb23fb1ce33ee27757c

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/ExecDeclarator.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/ExecDeclarator.java
@@ -109,7 +109,7 @@ public final class ExecDeclarator implements ICPPExecution {
 		} else if (isArrayType(nestedType) && !isCStringType(nestedType)) {
 			if (computedInitializerEval instanceof EvalInitList) {
 				IValue value = CompositeValue.create((EvalInitList) computedInitializerEval, 
-						(IArrayType) type, context.getPoint());
+						(IArrayType) nestedType, context.getPoint());
 				return new EvalFixed(type, computedInitializerEval.getValueCategory(context.getPoint()), value);
 			} else {
 				// TODO(sprigogin): Should something else be done here?

1cc8b974f0d508d8725010399d647175a7d9381e
Marc Dumais <marc.dumais@ericsson.com>
2017-02-17 14:23:47 -0400

    Revert "Bug 512303 - [tests] job debug-tests-master-all-gdb-versions has issues"
    Other test suites have similar issues with GDB < 7.5, so let's look for another way.
    
    This reverts commit ad845e6c3a9359ed1f4a0fa5cc61a64e43e6ffe1.
    
    Change-Id: I34d3aade4e8b10744767dd6eab1874be047685f7

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/MIExpressionsTest.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/MIExpressionsTest.java
@@ -93,11 +93,6 @@ public class MIExpressionsTest extends BaseParametrizedTestCase {
     public void doBeforeTest() throws Exception {
     	super.doBeforeTest();
 
-    	// Disabling this suite for GDB < 7.5. There are issues when the test 
-    	// binaries are compiled with gcc > 4.8, which uses DWARF version 4.
-    	// see bug 512303
-    	assumeGdbVersionAtLeast(ITestConstants.SUFFIX_GDB_7_5);
-    	
     	fSession = getGDBLaunch().getSession();
         Runnable runnable = new Runnable() {
             @Override

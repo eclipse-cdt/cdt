e263da241e37eacd4a20f1d1c85bfd278d5b2b42
Nathan Ridge <zeratul976@hotmail.com>
2017-03-05 03:49:06 -0500

    Bug 512932 - Delegating constructor in class template partial specialization
    Change-Id: Id4cb38d4857af7335a4aa294a6fe149e1a4f2acd

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/CPPSemantics.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/CPPSemantics.java
@@ -404,8 +404,7 @@ public class CPPSemantics {
 				&& !(binding instanceof ICPPTemplateParameter)
 				&& !(lookupName instanceof ICPPASTTemplateId)) {
 			ASTNodeProperty prop = lookupName.getPropertyInParent();
-			if (prop != ICPPASTConstructorChainInitializer.MEMBER_ID &&
-					prop != ICPPASTTemplateId.TEMPLATE_NAME && !lookupName.isQualified()) {
+			if (prop != ICPPASTTemplateId.TEMPLATE_NAME && !lookupName.isQualified()) {
 				// You cannot use a class template name outside of the class template scope,
 				// mark it as a problem.
 				IBinding user= CPPTemplates.isUsedInClassTemplateScope((ICPPClassTemplate) binding, lookupName);

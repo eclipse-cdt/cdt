2b302b96881dfca0c66671948848b6ff23d4f28d
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2017-03-16 21:48:44 -0400

    lldb: Remove -break-insert hack for absolute path for lldb 4.0
    Change-Id: I098e088874d95d9f9cd08e315d059dc00b2c809c
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- llvm/org.eclipse.cdt.llvm.dsf.lldb.core/src/org/eclipse/cdt/llvm/dsf/lldb/core/internal/service/LLDBBreakpoints.java
+++ llvm/org.eclipse.cdt.llvm.dsf.lldb.core/src/org/eclipse/cdt/llvm/dsf/lldb/core/internal/service/LLDBBreakpoints.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import org.eclipse.cdt.dsf.debug.service.IBreakpoints;
 import org.eclipse.cdt.dsf.gdb.service.GDBBreakpoints_7_4;
 import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.cdt.llvm.dsf.lldb.core.internal.LLDBTrait;
 import org.eclipse.core.runtime.Path;
 
 /**
@@ -35,25 +36,32 @@ public class LLDBBreakpoints extends GDBBreakpoints_7_4 {
 
 	@Override
 	protected String formatLocation(Map<String, Object> attributes) {
-		// FIXME: ***Big hack*** lldb-mi's -breakpoint-insert doesn't handle
-		// locations that look like absolute paths (leading /). This will have
-		// to be fixed upstream because the work-around is not ideal: we only
-		// use the last segment to insert the breakpoint. This is not good if
-		// there are two files of the same name in the inferior.
-		// See https://llvm.org/bugs/show_bug.cgi?id=28709
-		String location = super.formatLocation(attributes);
-		return adjustDebuggerPath(location);
+		if (LLDBTrait.BROKEN_BREAKPOINT_INSERT_FULL_PATH_LLVM_BUG_28709.isTraitOf(getSession())) {
+			// FIXME: ***Big hack*** lldb-mi's -breakpoint-insert doesn't handle
+			// locations that look like absolute paths (leading /). This will have
+			// to be fixed upstream because the work-around is not ideal: we only
+			// use the last segment to insert the breakpoint. This is not good if
+			// there are two files of the same name in the inferior.
+			// See https://llvm.org/bugs/show_bug.cgi?id=28709
+			String location = super.formatLocation(attributes);
+			return adjustDebuggerPath(location);
+		}
+
+		return super.formatLocation(attributes);
 	}
 
 	@Override
 	public String adjustDebuggerPath(String originalPath) {
-		// FIXME: See also #formatLocation for hack explanation. This one needs
-		// to be overridden for moveToLine, runToLine and stepIntoSelection
-		// (Once this hack is removed, they should be tested).
-		Path path = new Path(originalPath);
-		if (path.isAbsolute()) {
-			return path.lastSegment();
+		if (LLDBTrait.BROKEN_BREAKPOINT_INSERT_FULL_PATH_LLVM_BUG_28709.isTraitOf(getSession())) {
+			// FIXME: See also #formatLocation for hack explanation. This one needs
+			// to be overridden for moveToLine, runToLine and stepIntoSelection
+			// (Once this hack is removed, they should be tested).
+			Path path = new Path(originalPath);
+			if (path.isAbsolute()) {
+				return path.lastSegment();
+			}
 		}
+
 		return super.adjustDebuggerPath(originalPath);
 	}
 }

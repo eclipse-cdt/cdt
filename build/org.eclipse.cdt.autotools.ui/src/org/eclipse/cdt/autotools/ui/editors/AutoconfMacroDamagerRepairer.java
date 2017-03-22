5d7f0d31af08278563f742c037c68c2a596b0237
Alexander Kurtakov <akurtako@redhat.com>
2017-03-22 11:58:24 +0200

    autotools: automake/autoconf generic editor.
    Reorganize code and expose things for generic editor.
    So far presentation reconciler only is ported.
    Allows to right-click/Open with/Generic editor on e.g. Makefile.am and
    see it in the generic editor. It is not as feature rich as the
    specialized editors we have now but over time it should provide all the
    features of them with significantly less code.
    
    Change-Id: I72324b94ce08ab762979804914adbb1982c88198
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/autotools/ui/editors/AutoconfMacroDamagerRepairer.java
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/autotools/ui/editors/AutoconfMacroDamagerRepairer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2015 Red Hat, Inc.
+ * Copyright (c) 2007, 2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,46 +10,22 @@
  *******************************************************************************/
 package org.eclipse.cdt.autotools.ui.editors;
 
-import org.eclipse.jface.text.DocumentEvent;
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITypedRegion;
-import org.eclipse.jface.text.TextPresentation;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.ITokenScanner;
 
 public class AutoconfMacroDamagerRepairer extends DefaultDamagerRepairer {
 	
-	public final static String UNMATCHED_RIGHT_PARENTHESIS = "UnmatchedRightParenthesis"; //$NON-NLS-1$
-	public final static String UNMATCHED_LEFT_PARENTHESIS = "UnmatchedLeftParenthesis"; //$NON-NLS-1$
-	public final static String UNMATCHED_RIGHT_QUOTE = "UnmatchedRightQuote"; //$NON-NLS-1$
-	public final static String UNMATCHED_LEFT_QUOTE = "UnmatchedLeftQuote"; //$NON-NLS-1$
-	
 	/**
-	 * Creates a damager/repairer that uses the given scanner. The scanner may not be <code>null</code>
-	 * and is assumed to return only token that carry text attributes.
+	 * Creates a damager/repairer that uses the given scanner. The scanner may
+	 * not be <code>null</code> and is assumed to return only token that carry
+	 * text attributes.
 	 *
-	 * @param scanner the token scanner to be used, may not be <code>null</code>
+	 * @param scanner
+	 *            the token scanner to be used, may not be <code>null</code>
 	 */
 	public AutoconfMacroDamagerRepairer(ITokenScanner scanner) {
 		super(scanner);
 	}
 
-	@Override
-	public IRegion getDamageRegion(ITypedRegion partition, DocumentEvent e, boolean documentPartitioningChanged) {
-		// In the case of a partition with multiline rules, we will punt to
-		// reparse the entire partition because we don't know if the line being
-		// edited is in the middle of an area covered by a multiline rule.  In
-		// such a case, we need to back up and find the start sequence of the
-		// rule.  It is easiest to just reparse the whole partition.
-		return partition;
-	}
 	
-	@Override
-	public void createPresentation(TextPresentation presentation, ITypedRegion region) {
-
-//		int offset = region.getOffset();
-//		int length = region.getLength();
-		
-		super.createPresentation(presentation, region);
-	}
 }

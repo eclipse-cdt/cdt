3de06d4f0e2e8120699e8b1749d893b8c08cd121
Alexander Kurtakov <akurtako@redhat.com>
2017-03-22 11:58:24 +0200

    autotools: automake/autoconf generic editor.
    Reorganize code and expose things for generic editor.
    So far presentation reconciler only is ported.
    Allows to right-click/Open with/Generic editor on e.g. Makefile.am and
    see it in the generic editor. It is not as feature rich as the
    specialized editors we have now but over time it should provide all the
    features of them with significantly less code.
    
    Change-Id: I72324b94ce08ab762979804914adbb1982c88198
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

---
+++ build/org.eclipse.cdt.autotools.ui/src/org/eclipse/cdt/internal/autotools/ui/editors/autoconf/AutoconfPresentationReconciler.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.internal.autotools.ui.editors.autoconf;
+
+import org.eclipse.cdt.autotools.ui.editors.AutoconfCodeScanner;
+import org.eclipse.cdt.autotools.ui.editors.AutoconfMacroCodeScanner;
+import org.eclipse.cdt.autotools.ui.editors.AutoconfMacroDamagerRepairer;
+import org.eclipse.cdt.autotools.ui.editors.AutoconfPartitionScanner;
+import org.eclipse.cdt.autotools.ui.editors.MultilineRuleDamagerRepairer;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.presentation.PresentationReconciler;
+import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
+
+public class AutoconfPresentationReconciler extends PresentationReconciler {
+
+	public AutoconfPresentationReconciler() {
+		DefaultDamagerRepairer dr = new AutoconfMacroDamagerRepairer(new AutoconfMacroCodeScanner());
+		setDamager(dr, AutoconfPartitionScanner.AUTOCONF_MACRO);
+		setRepairer(dr, AutoconfPartitionScanner.AUTOCONF_MACRO);
+
+		dr = new DefaultDamagerRepairer(new AutoconfCodeScanner());
+		setDamager(dr, AutoconfPartitionScanner.AUTOCONF_COMMENT);
+		setRepairer(dr, AutoconfPartitionScanner.AUTOCONF_COMMENT);
+
+		dr = new MultilineRuleDamagerRepairer(new AutoconfCodeScanner());
+		setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
+		setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
+	}
+
+}

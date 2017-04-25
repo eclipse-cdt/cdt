a79753496813f5cbf133afe8f4206d5a5db01f4d
Jonah Graham <jonah@kichwacoders.com>
2017-04-25 12:08:22 +0100

    Bug 515756: Allow extending classes to provide getFilterExtension
    This affects the synchronizer as it now should defer to the manager
    to get the filter extension, instead of trying to load it directly.
    
    Change-Id: I0e646f115ba089453c56f2cc356a4e0a99a1a3b7
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIBreakpointsSynchronizer.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/MIBreakpointsSynchronizer.java
@@ -32,7 +32,6 @@ import org.eclipse.cdt.debug.core.CDIDebugModel;
 import org.eclipse.cdt.debug.core.breakpointactions.BreakpointActionManager;
 import org.eclipse.cdt.debug.core.model.ICAddressBreakpoint;
 import org.eclipse.cdt.debug.core.model.ICBreakpoint;
-import org.eclipse.cdt.debug.core.model.ICBreakpointExtension;
 import org.eclipse.cdt.debug.core.model.ICBreakpointType;
 import org.eclipse.cdt.debug.core.model.ICDynamicPrintf;
 import org.eclipse.cdt.debug.core.model.ICFunctionBreakpoint;
@@ -335,9 +334,7 @@ public class MIBreakpointsSynchronizer extends AbstractDsfService implements IMI
 							try {
 								String threadId = data.getThreadId();
 								if (!threadId.equals("0")) { //$NON-NLS-1$
-									IDsfBreakpointExtension bpExtension = 
-										(IDsfBreakpointExtension)((ICBreakpoint)plBpt).getExtension(
-											MIBreakpointsManager.GDB_DEBUG_MODEL_ID, ICBreakpointExtension.class);
+									IDsfBreakpointExtension bpExtension = fBreakpointsManager.getFilterExtension((ICBreakpoint)plBpt);
 									
 									IMIProcesses processes = getServicesTracker().getService(IMIProcesses.class);
 									if (processes == null) {
@@ -506,8 +503,8 @@ public class MIBreakpointsSynchronizer extends AbstractDsfService implements IMI
 			if (procDmc == null) {
 				return;
 			}
-			IDsfBreakpointExtension bpExtension = (IDsfBreakpointExtension)plBpt.getExtension(
-					MIBreakpointsManager.GDB_DEBUG_MODEL_ID, ICBreakpointExtension.class);
+			IDsfBreakpointExtension bpExtension = fBreakpointsManager.getFilterExtension(plBpt);
+
 			IExecutionDMContext[] execDMCs = bpExtension.getThreadFilters(contDMC);
 			if (execDMCs == null) {
 				execDMCs = new IExecutionDMContext[0];

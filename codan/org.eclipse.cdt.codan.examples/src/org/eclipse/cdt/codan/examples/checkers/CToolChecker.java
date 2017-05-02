689de3a1405dbbd66d08dbba5ded868b22ea85bd
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2017-05-02 11:44:59 -0400

    Fixed copyright
    Change-Id: I801f28d73afeba8432283eb9b5e8de3fa9b47a32
    Signed-off-by: Alena Laskavaia <elaskavaia.cdt@gmail.com>

--- codan/org.eclipse.cdt.codan.examples/src/org/eclipse/cdt/codan/examples/checkers/CToolChecker.java
+++ codan/org.eclipse.cdt.codan.examples/src/org/eclipse/cdt/codan/examples/checkers/CToolChecker.java
@@ -1,40 +1,21 @@
 /*******************************************************************************
- * Copyright (c) Mar 4, 2015 QNX Software Systems. All Rights Reserved.
+ * Copyright (c) 2009,2015 QNX Software Systems
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * You must obtain a written license from and pay applicable license fees to QNX
- * Software Systems before you may reproduce, modify or distribute this software,
- * or any work that includes all or part of this software.   Free development
- * licenses are available for evaluation and non-commercial purposes.  For more
- * information visit [http://licensing.qnx.com] or email licensing@qnx.com.
- *
- * This file may contain contributions from others.  Please review this entire
- * file for other proprietary rights or license notices, as well as the QNX
- * Development Suite License Guide at [http://licensing.qnx.com/license-guide/]
- * for other information.
+ * Contributors:
+ *    QNX Software Systems (Alena Laskavaia)  - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.cdt.codan.examples.checkers;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.cdt.codan.core.cxx.externaltool.InvocationFailure;
-import org.eclipse.cdt.codan.core.cxx.internal.externaltool.ExternalToolInvoker;
-import org.eclipse.cdt.codan.core.cxx.model.AbstractCElementChecker;
-import org.eclipse.cdt.codan.core.cxx.model.CodanMarkerGenerator;
-import org.eclipse.cdt.codan.core.model.CheckerLaunchMode;
-import org.eclipse.cdt.codan.core.model.IProblemWorkingCopy;
 import org.eclipse.cdt.codan.examples.Activator;
-import org.eclipse.cdt.core.ErrorParserManager;
-import org.eclipse.cdt.core.IConsoleParser;
-import org.eclipse.cdt.core.IMarkerGenerator;
-import org.eclipse.cdt.core.model.ITranslationUnit;
-import org.eclipse.cdt.core.parser.IScannerInfo;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
 
 /**
  * @author elaskavaia
@@ -49,7 +30,7 @@ public class CToolChecker extends AbstractCElementChecker {
 	public void initPreferences(IProblemWorkingCopy problem) {
 		getTopLevelPreference(problem); // initialize
 		getLaunchModePreference(problem).enableInLaunchModes(
-				CheckerLaunchMode.RUN_ON_FILE_SAVE, 
+				CheckerLaunchMode.RUN_ON_FILE_SAVE,
 				CheckerLaunchMode.RUN_ON_DEMAND);
 	}
 
@@ -77,12 +58,15 @@ public class CToolChecker extends AbstractCElementChecker {
 		List<String> res = new ArrayList<>();
 		for (String macro : symbols.keySet()) {
 			if (macro.startsWith("_"))
+			 {
 				continue; // likely embedded macro
+			}
 			String value = symbols.get(macro);
-			if (value.isEmpty())
+			if (value.isEmpty()) {
 				res.add("-D" + macro);
-			else
+			} else {
 				res.add("-D" + macro + "=" + value);
+			}
 		}
 		for (String inc : scannerInfo.getIncludePaths()) {
 			res.add("-I" + inc);

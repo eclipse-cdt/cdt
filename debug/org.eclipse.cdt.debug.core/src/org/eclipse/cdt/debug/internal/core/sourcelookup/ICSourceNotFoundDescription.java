3a84b3b137d56d38bc58ea3b2441e4c0f791f5ff
Yannick Mayeur <yannick.mayeur@gmail.com>
2017-05-04 10:51:21 +0100

    Bug 515296: Changed the message when only Adress
    When the Source Not Found Editor opens with only an address, the message is
    changed, to confuse the user less.
    We know when this is the case through a global boolean "isAdress" in the
    CSourceNotFoundEditor class.
    
    Change-Id: I1dcc9fae80d20975b00d2d356469ddda8c2d8d2b
    Signed-off-by: Yannick Mayeur <yannick.mayeur@gmail.com>
    Also-by: Pierre Sachot <sachot.pierre@laposte.net>

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/ICSourceNotFoundDescription.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/sourcelookup/ICSourceNotFoundDescription.java
@@ -28,4 +28,14 @@ public interface ICSourceNotFoundDescription {
 	 */
 	String getDescription();
 
+	/**
+	 * Return true if the debug element only is an address, false if not.
+	 * This is used by the editor to know wich type of message he should use.
+	 * 
+	 * @return a boolean that is true if the debug element only is an address
+	 */
+	default boolean isAddressOnly() {
+		return false;
+	}
+
 }

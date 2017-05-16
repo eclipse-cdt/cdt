a2e80aca03fe6e5ef21e7ec843b6cb0989bf1580
Felix Morgner <fmorgner@hsr.ch>
2017-05-16 14:44:53 +0200

    Bug 495842: Add a quick fix for codan problems that ignores them by adding a @suppress comment
    Change-Id: I760d2d435010f219bdb94273a6450e4163fe472e
    Signed-off-by: Felix Morgner <fmorgner@hsr.ch>

---
+++ codan/org.eclipse.cdt.codan.core.tests/src/org/eclipse/cdt/codan/internal/checkers/ui/quickfix/QuickFixSuppressProblemTest.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Institute for Software.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Felix Morgner <fmorgner@hsr.ch> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.codan.internal.checkers.ui.quickfix;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import org.eclipse.cdt.codan.ui.AbstractCodanCMarkerResolution;
+import org.eclipse.cdt.ui.PreferenceConstants;
+
+public class QuickFixSuppressProblemTest extends QuickFixTestCase {
+	@SuppressWarnings("restriction")
+	@Override
+	protected AbstractCodanCMarkerResolution createQuickFix() {
+		return new QuickFixSuppressProblem();
+	}
+
+	//struct s {};
+	//void func() {
+	//	try {
+	//	} catch (s e) {
+	//	}
+	//}
+	public void testCPPMarkerOnNode_495842() throws Exception {
+		loadcode(getAboveComment(), true);
+		String result = runQuickFixOneFile();
+		assertContainedIn("} catch (s e) { // @suppress(\"Catching by reference is recommended\")", result);
+	}
+
+	//void func() {
+	//	int n = 42;
+	//
+	//	switch (n) {
+	//	case 1:
+	//		n = 32;
+	//	default:
+	//		break;
+	//	}
+	//}
+	public void testCPPMarkerNotOnNode_495842() throws Exception {
+		loadcode(getAboveComment(), true);
+		String result = runQuickFixOneFile();
+		assertContainedIn("n = 32; // @suppress(\"No break at end of case\")", result);
+	}
+
+	//int func() { }
+	public void testCMarker_495842() throws Exception {
+		loadcode(getAboveComment(), false);
+		String result = runQuickFixOneFile();
+		assertContainedIn("int func() { } // @suppress(\"No return\")", result);
+	}
+
+	//int func() { }
+	public void testMarkerOnLastLineNoNewline_495842() throws Exception {
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter(loadcode("", false)))) {
+			writer.write(getAboveComment().trim());
+		}
+		PreferenceConstants.getPreferenceStore().setValue(PreferenceConstants.ENSURE_NEWLINE_AT_EOF, false);
+		indexFiles();
+		String result = runQuickFixOneFile();
+		PreferenceConstants.getPreferenceStore().setValue(PreferenceConstants.ENSURE_NEWLINE_AT_EOF, true);
+		assertContainedIn("int func() { } // @suppress(\"No return\")", result);
+	}
+}

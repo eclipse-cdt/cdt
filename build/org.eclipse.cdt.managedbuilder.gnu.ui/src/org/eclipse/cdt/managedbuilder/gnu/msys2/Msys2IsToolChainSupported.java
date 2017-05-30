e723f8fa8b9f3f7bda05f2275f09a2bcd0306402
Yannick Mayeur <yannick.mayeur@gmail.com>
2017-05-30 15:06:48 +0100

    duplicated files for the new toolchain
    Change-Id: I6c6158f27926938d9a14227d64485dd9b70dcc03
    Signed-off-by: Yannick Mayeur <yannick.mayeur@gmail.com>

---
+++ build/org.eclipse.cdt.managedbuilder.gnu.ui/src/org/eclipse/cdt/managedbuilder/gnu/msys2/Msys2IsToolChainSupported.java
@@ -0,0 +1,35 @@
+/**********************************************************************
+ * Copyright (c) 2007, 2013 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Doug Schaefer (QNX Software Systems) - Initial API and implementation
+ *     Andrew Gvozdev                       - Ability to use different MinGW versions in different cfg
+ **********************************************************************/
+
+package org.eclipse.cdt.managedbuilder.gnu.msys2;
+
+import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
+import org.eclipse.cdt.internal.core.MinGW;
+import org.eclipse.cdt.managedbuilder.core.IManagedIsToolChainSupported;
+import org.eclipse.cdt.managedbuilder.core.IToolChain;
+import org.eclipse.cdt.managedbuilder.internal.envvar.EnvironmentVariableManagerToolChain;
+import org.osgi.framework.Version;
+
+/**
+ * @noextend This class is not intended to be subclassed by clients.
+ */
+public class Msys2IsToolChainSupported implements IManagedIsToolChainSupported {
+	private static final String ENV_PATH = "PATH"; //$NON-NLS-1$
+
+	@Override
+	public boolean isSupported(IToolChain toolChain, Version version, String instance) {
+		IEnvironmentVariable var = new EnvironmentVariableManagerToolChain(toolChain).getVariable(ENV_PATH, true);
+		String envPath = var != null ? var.getValue() : null;
+		return MinGW.isAvailable(envPath);
+	}
+
+}

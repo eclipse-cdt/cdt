7237b6287a638ba791890e6e0489e1618e192488
Mustafa Yücel <mustafa.yuecel@siemens.com>
2017-06-29 22:54:53 +0200

    Bug 520340: enable CLI to CDT's code formatter
    example usage:
    eclipse -nosplash -application org.eclipse.cdt.core.CodeFormatter -consoleLog \
            -verbose -config .settings/org.eclipse.cdt.core.prefs src/
    
    imported CodeFormatterApplication from eclipse.jdt.core:
    org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/CodeFormatterApplication.java
    revision 823698366b88fe7e6c1438813d7dd191ff9aa71f
    org.eclipse.jdt.core/formatter/org/eclipse/jdt/core/formatter/messages.properties
    revision c2646812d22906feca88c2f52911ca7c4eae2299
    
    Change-Id: Id934c8958c66f562bd2402dcbd8b7c60e39cc7a5
    Signed-off-by: Mustafa Yücel <mustafa.yuecel@siemens.com>

---
+++ core/org.eclipse.cdt.core/src/org/eclipse/cdt/core/formatter/Messages.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *  Copyright (c) 2005, 2017 IBM Corporation and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *     Mustafa Yuecel - initial implementation - adapted from org.eclipse.jdt.core
+ *******************************************************************************/
+package org.eclipse.cdt.core.formatter;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @since 6.3
+ */
+public class Messages extends NLS {
+	public static String CommandLineConfigFile;
+	public static String CommandLineDone;
+	public static String CommandLineErrorConfig;
+	public static String CommandLineErrorFileTryFullPath;
+	public static String CommandLineErrorFile;
+	public static String CommandLineErrorFileDir;
+	public static String CommandLineErrorQuietVerbose;
+	public static String CommandLineFormatting;
+	public static String CommandLineStart;
+	public static String CommandLineUsage;
+	public static String ConfigFileNotFoundErrorTryFullPath;
+	public static String ConfigFileReadingError;
+	public static String FormatProblem;
+	public static String CaughtException;
+	public static String ExceptionSkip;
+
+	static {
+		NLS.initializeMessages(Messages.class.getName(), Messages.class);
+	}
+
+	private Messages() {
+	}
+}

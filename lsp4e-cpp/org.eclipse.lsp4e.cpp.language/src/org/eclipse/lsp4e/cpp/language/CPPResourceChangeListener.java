1202579b07981b68c92b18492e575f3212fe7f54
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2017-09-23 21:44:05 -0400

    lsp4e-cpp: Generate LSP FileEvents from ResourceChangeEvents
    FileEvents are useful for indexing and lsp4e (so far) does not send
    them. It is not clear whether or not this will be implemented in lsp4e.
    In the mean time, lsp4e-cpp can have its own mechanism to generate
    the events.
    
    Change-Id: I09aac4fda7755260f47b73fd683ca6d2ad317f81
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

---
+++ lsp4e-cpp/org.eclipse.lsp4e.cpp.language/src/org/eclipse/lsp4e/cpp/language/CPPResourceChangeListener.java
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Ericsson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.lsp4e.cpp.language;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.lsp4e.LanguageServersRegistry;
+import org.eclipse.lsp4e.LanguageServersRegistry.LanguageServerDefinition;
+import org.eclipse.lsp4e.LanguageServiceAccessor;
+import org.eclipse.lsp4e.ProjectSpecificLanguageServerWrapper;
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.FileChangeType;
+import org.eclipse.lsp4j.FileEvent;
+
+/**
+ * A resource listener used to generate FileEvents, as part of the LSP. This
+ * only listens to Added, Changed, Removed event on a specific project that as a
+ * C/C++ language server started.
+ */
+@SuppressWarnings("restriction")
+final class CPPResourceChangeListener implements IResourceChangeListener {
+	private final IProject fProject;
+
+	CPPResourceChangeListener(IProject project) {
+		fProject = project;
+	}
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		LanguageServerDefinition definition = LanguageServersRegistry.getInstance().getDefinition(CPPLanguageServer.ID);
+		ProjectSpecificLanguageServerWrapper wrapper = getLanguageSeverWrapper(definition);
+		if (event.getType() != IResourceChangeEvent.POST_CHANGE || !isRelevantDelta(event.getDelta())
+				|| wrapper == null) {
+			return;
+		}
+
+		sendFileEvents(wrapper, createFileEventsFromResourceEvent(event));
+	}
+
+	private static void sendFileEvents(ProjectSpecificLanguageServerWrapper wrapper, List<FileEvent> fileEvents) {
+		if (!fileEvents.isEmpty()) {
+			DidChangeWatchedFilesParams params = new DidChangeWatchedFilesParams(fileEvents);
+			wrapper.getServer().getWorkspaceService().didChangeWatchedFiles(params);
+		}
+	}
+
+	private static List<FileEvent> createFileEventsFromResourceEvent(IResourceChangeEvent event) {
+		List<FileEvent> fileEvents = new ArrayList<>();
+		try {
+			event.getDelta().accept((delta) -> {
+				if (delta.getResource() instanceof IFile && isRelevantDelta(delta)) {
+					FileEvent fileEvent = createFileEventFromDelta(delta);
+					if (fileEvent != null) {
+						fileEvents.add(fileEvent);
+					}
+				}
+				return true;
+			}, false);
+		} catch (CoreException e) {
+			// Do nothing
+		}
+		return fileEvents;
+	}
+
+	private ProjectSpecificLanguageServerWrapper getLanguageSeverWrapper(LanguageServerDefinition definition) {
+		try {
+			return LanguageServiceAccessor.getLSWrapperForConnection(fProject, definition, false);
+		} catch (IOException e) {
+			// Do nothing
+			return null;
+		}
+	}
+
+	private static boolean isRelevantDelta(IResourceDelta delta) {
+		int kind = delta.getKind();
+		return kind == IResourceDelta.ADDED || kind == IResourceDelta.CHANGED || kind == IResourceDelta.REMOVED;
+	}
+
+	private static FileEvent createFileEventFromDelta(IResourceDelta delta) {
+		URI locationURI = delta.getResource().getLocationURI();
+		if (locationURI == null) {
+			return null;
+		}
+
+		FileChangeType changeType = null;
+		if (delta.getKind() == IResourceDelta.ADDED) {
+			changeType = FileChangeType.Created;
+		} else if (delta.getKind() == IResourceDelta.CHANGED) {
+			changeType = FileChangeType.Changed;
+		} else if (delta.getKind() == IResourceDelta.REMOVED) {
+			changeType = FileChangeType.Deleted;
+		} else {
+			throw new IllegalStateException("Unsupported resource delta kind: " + delta.getKind()); //$NON-NLS-1$
+		}
+
+		return new FileEvent(locationURI.toString(), changeType);
+	}
+}

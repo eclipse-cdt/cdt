0e5dc114da285a786c52fcbfdd59aea82643cabb
Chin Huat Ang <chin.huat.ang@intel.com>
2017-10-24 02:16:05 +0800

    Bug 525591: autotools: Initial core build implementation
    Add core, UI and feature plugins for the new autotools project which
    uses template wizard and core build.
    
    The template wizard currently supports generating a simple autotools
    project as described using a template manifest. 
    
    The project generator sets new autotools nature associated with new
    autotools build config provider.
    
    The generated autotools project has been tested to compile fine with gcc
    (x86_64-linux-gnu-gcc) toolchain.
    
    Change-Id: I8349999d551bb693725a246340cf46266150fadc
    Signed-off-by: Chin Huat Ang <chin.huat.ang@intel.com>

---
+++ build/org.eclipse.cdt.core.autotools.core/templates/autotools/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Hello from autotools!\n");
+	return 0;
+
+}
---
+++ build/org.eclipse.cdt.core.autotools.ui/src/org/eclipse/cdt/core/autotools/ui/internal/NewAutotoolsProjectWizard.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Intel Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Intel Corporation - initial API and implementation
+ *******************************************************************************/ 
+
+package org.eclipse.cdt.core.autotools.ui.internal;
+
+import org.eclipse.cdt.core.autotools.core.AutotoolsProjectGenerator;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.tools.templates.core.IGenerator;
+import org.eclipse.tools.templates.ui.TemplateWizard;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+public class NewAutotoolsProjectWizard extends TemplateWizard {
+
+	private WizardNewProjectCreationPage mainPage;
+
+	@Override
+	public void addPages() {
+		mainPage = new WizardNewProjectCreationPage("basicNewProjectPage") { //$NON-NLS-1$
+			@Override
+			public void createControl(Composite parent) {
+				super.createControl(parent);
+				createWorkingSetGroup((Composite) getControl(), getSelection(),
+						new String[] { "org.eclipse.ui.resourceWorkingSetPage" }); //$NON-NLS-1$
+				Dialog.applyDialogFont(getControl());
+			}
+		};
+		mainPage.setTitle(Messages.NewAutotoolsProjectWizard_Title);
+		mainPage.setDescription(Messages.NewAutotoolsProjectWizard_Description);
+		this.addPage(mainPage);
+	}
+
+	@Override
+	protected IGenerator getGenerator() {
+		AutotoolsProjectGenerator generator = new AutotoolsProjectGenerator("templates/autotools/manifest.xml"); //$NON-NLS-1$
+		generator.setProjectName(mainPage.getProjectName());
+		if (!mainPage.useDefaults()) {
+			generator.setLocationURI(mainPage.getLocationURI());
+		}
+		return generator;
+	}
+
+}

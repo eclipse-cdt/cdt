64fdcadef8e722b0d38dae2a4718d9343b19ac4f
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2017-12-21 09:15:21 +0100

    Bug 529023 - Cannot set build.proj.ref.configs.enabled via customization
    The setting build.proj.ref.configs.enabled=true ensures CDT projects are
    not rebuilt if non-related projects are changed. This setting cannot be
    changed with product or command line customization, hindering products
    which require this behavior.
    
    The reason for this is that ACBuilder, which is asked for the value of
    the preference, communicates with the instance scope of preferences.
    Product and command line customization, however, are on default scope
    level.
    
    With this change, ACBuilder will also ask the default scope when
    retrieving the value of the preference.
    
    Change-Id: I09e895ea2a05b677e36fac9eb28f24f1f3bc2877
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ core/org.eclipse.cdt.core.tests/misc/org/eclipse/cdt/core/preferences/tests/TestScopeOfBuildConfigResourceChangesPreference.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ *  Copyright (c) 2017 Simeon Andreev and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *     Simeon Andreev - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.core.preferences.tests;
+
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.CCorePreferenceConstants;
+import org.eclipse.cdt.core.resources.ACBuilder;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for Bug 529023: Cannot set build.proj.ref.configs.enabled via
+ * plugin_customization.ini.
+ *
+ * @author Simeon Andreev
+ */
+public class TestScopeOfBuildConfigResourceChangesPreference extends TestCase {
+
+	private static final String PREFERENCE_NAME = CCorePreferenceConstants.PREF_BUILD_CONFIGS_RESOURCE_CHANGES;
+
+	private boolean oldInstanceScopeValue;
+	private boolean oldDefaultScopeValue;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		oldInstanceScopeValue = ACBuilder.buildConfigResourceChanges();
+		IEclipsePreferences defaultScopePreferences = defaultScopePreferences();
+		oldDefaultScopeValue = defaultScopePreferences.getBoolean(PREFERENCE_NAME, oldInstanceScopeValue);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		ACBuilder.setBuildConfigResourceChanges(oldInstanceScopeValue);
+		IEclipsePreferences defaultScopePreferences = defaultScopePreferences();
+		defaultScopePreferences.putBoolean(PREFERENCE_NAME, oldDefaultScopeValue);
+
+		super.tearDown();
+	}
+
+	private static IEclipsePreferences defaultScopePreferences() {
+		return DefaultScope.INSTANCE.getNode(CCorePlugin.PLUGIN_ID);
+	}
+
+	/**
+	 * Validates that {@link ACBuilder#buildConfigResourceChanges()} also takes
+	 * {@link DefaultScope} into account.
+	 */
+	public void testSettingPreferenceViaDefaultScope() throws Exception {
+		IEclipsePreferences defaultScopePreferences = defaultScopePreferences();
+		defaultScopePreferences.putBoolean(PREFERENCE_NAME, true);
+
+		boolean buildConfigResourceChanges = ACBuilder.buildConfigResourceChanges();
+		assertTrue("unable to set preference \"" + PREFERENCE_NAME + "\" via default scope",
+				buildConfigResourceChanges);
+	}
+}

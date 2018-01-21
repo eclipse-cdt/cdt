573af6187b2f89d4d2b6a843ad93e366736503ef
John Dallaway <john@dallaway.org.uk>
2018-01-21 16:35:50 +0000

    Bug 494059: Add OpenOCD extensions
    Change-Id: I0a2820f40c641a85aac11e8d72b6aca65f599b6e
    Signed-off-by: John Dallaway <john@dallaway.org.uk>

---
+++ jtag/org.eclipse.cdt.debug.gdbjtag.core/src/org/eclipse/cdt/debug/gdbjtag/core/jtagdevice/OpenOCDPipe.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2018 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     QNX Software Systems - Initial API and implementation
+ *     Andy Jin - Hardware debugging UI improvements, bug 229946
+ *     John Dallaway - OpenOCD extensions, bug 494059
+ *******************************************************************************/
+package org.eclipse.cdt.debug.gdbjtag.core.jtagdevice;
+
+import java.util.Collection;
+
+
+/**
+ * @since 9.2
+ */
+public class OpenOCDPipe extends DefaultGDBJtagConnectionImpl {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.cdt.debug.gdbjtag.core.jtagdevice.DefaultGDBJtagConnectionImpl#doDelay(int, java.util.Collection)
+	 */
+	@Override
+	public void doDelay(int delay, Collection <String>commands) {
+		addCmd(commands, "monitor sleep " + String.valueOf(delay * 1000)); //$NON-NLS-1$
+	}
+
+}

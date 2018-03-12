e2e6ad691df7d25af21e4787bc73a3d6decffe24
Jonah Graham <jonah@kichwacoders.com>
2018-03-12 12:13:01 +0000

    [releng] Updated officially supported GDB versions
    Change-Id: I22eb86b34659ff54223a81dec4f8341b63b5675b

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/ITestConstants.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/ITestConstants.java
@@ -45,19 +45,19 @@ public class ITestConstants {
 			ITestConstants.SUFFIX_GDB_7_7,
 			ITestConstants.SUFFIX_GDB_7_6,
 			ITestConstants.SUFFIX_GDB_7_5,
+	};
+
+	public static String[] ALL_UNSUPPORTED_VERSIONS = new String[] {
 			ITestConstants.SUFFIX_GDB_7_4,
 			ITestConstants.SUFFIX_GDB_7_3,
 			ITestConstants.SUFFIX_GDB_7_2,
 			ITestConstants.SUFFIX_GDB_7_1,
-	};
-	
-	public static String[] ALL_UNSUPPORTED_VERSIONS = new String[] {
 			ITestConstants.SUFFIX_GDB_7_0,
 			ITestConstants.SUFFIX_GDB_6_8,
 			ITestConstants.SUFFIX_GDB_6_7,
 			ITestConstants.SUFFIX_GDB_6_6,
 	};
-	
+
 	public static String[] ALL_KNOWN_VERSIONS;
 	static {
 		// Initialize all known version based on the other arrays to avoid code duplication
@@ -65,7 +65,7 @@ public class ITestConstants {
 		System.arraycopy(ALL_SUPPORTED_VERSIONS, 0, ALL_KNOWN_VERSIONS, 0, ALL_SUPPORTED_VERSIONS.length);
 		System.arraycopy(ALL_UNSUPPORTED_VERSIONS, 0, ALL_KNOWN_VERSIONS, ALL_SUPPORTED_VERSIONS.length, ALL_UNSUPPORTED_VERSIONS.length);
 	};
-	
+
 	// Attribute that allows a test to request not to start gdbserver even if the session is a remote one
 	public static final String LAUNCH_GDB_SERVER = TestsPlugin.PLUGIN_ID + ".launchGdbServer";
 }

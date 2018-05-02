bbe501b5237daf44cd9f0138d1e4ca03cadd1fbd
Baha El Kassaby <baha.elkassaby@gmail.com>
2018-05-02 21:52:39 +0200

    Bug 530443: Add support for "info sources" MI equivalent
    *Implementation of -file-list-exec-source-files MI command.
    *Add new Debug Sources view with tree-like structure
    
    Change-Id: I6e734799712c059c8e53aa882777dfebd85aa0d5
    Also-by: Jonah Graham <jonah@kichwacoders.com>
    Signed-off-by: Baha El Kassaby <baha.elkassaby@gmail.com>

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/debugsources/IDebugSourcesImagesConst.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Baha El-Kassaby - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.dsf.gdb.internal.ui.debugsources;
+
+/**
+ * 
+ */
+public interface IDebugSourcesImagesConst {
+
+	public static final String IMG_COLLAPSE_DEBUG_SOURCES = "icons/full/dlcl16/collapse_all.gif"; //$NON-NLS-1$
+
+	public static final String IMG_EXPAND_DEBUG_SOURCES = "icons/full/dlcl16/expand_all.gif"; //$NON-NLS-1$
+
+}

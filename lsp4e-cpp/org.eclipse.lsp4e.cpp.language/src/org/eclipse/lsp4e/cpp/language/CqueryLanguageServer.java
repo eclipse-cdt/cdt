b8a85da06f3d325d6b0f34e10048fc98ccde11ea
Manish Khurana <mkmanishkhurana98@gmail.com>
2018-06-08 15:10:41 +0530

    Bug 535632. Set a cache directory for CQuery LS
    Change-Id: I80b88f48207b5091fe1c634c730a8fd6983f40b9
    Signed-off-by: Manish Khurana <mkmanishkhurana98@gmail.com>

---
+++ lsp4e-cpp/org.eclipse.lsp4e.cpp.language/src/org/eclipse/lsp4e/cpp/language/CqueryLanguageServer.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Manish Khurana , Nathan Ridge and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.lsp4e.cpp.language;
+
+import java.net.URI;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import com.google.gson.JsonObject;
+
+public class CqueryLanguageServer implements ICPPLanguageServer {
+
+	@Override
+	public Object getLSSpecificInitializationOptions(Object defaultInitOptions,URI rootPath) {
+		IPath cacheDirectory = Path.fromOSString(rootPath.getPath()).append(".cquery/cquery_index"); //$NON-NLS-1$
+		JsonObject result = (defaultInitOptions instanceof JsonObject) ? (JsonObject) defaultInitOptions : new JsonObject();
+		result.addProperty("cacheDirectory", cacheDirectory.toString()); //$NON-NLS-1$
+		return result;
+	}
+
+}

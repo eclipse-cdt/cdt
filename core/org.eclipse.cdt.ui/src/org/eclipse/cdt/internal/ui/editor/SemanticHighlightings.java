b64e6844d533b21cb7437320b7dcaf25d63adcc5
Bassem Girgis <brgirgis@gmail.com>
2018-06-22 14:36:16 -0500

    Bug 536201 Add enum classes highlights
    Change-Id: I6b16efbbb268f9fdc4a884e96c30192dd64c5db1
    Signed-off-by: Bassem Girgis <brgirgis@gmail.com>

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/SemanticHighlightings.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/editor/SemanticHighlightings.java
@@ -76,6 +76,7 @@ import org.eclipse.cdt.core.dom.ast.cpp.ICPPNamespace;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPReferenceType;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateNonTypeParameter;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPTemplateParameter;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPEnumeration;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPUsingDeclaration;
 import org.eclipse.cdt.core.dom.ast.cpp.SemanticQueries;
 import org.eclipse.cdt.core.index.IIndex;
@@ -175,6 +176,11 @@ public class SemanticHighlightings {
 	 * A named preference part that controls the highlighting of enums.
 	 */
 	public static final String ENUM= "enum"; //$NON-NLS-1$
+
+	/**
+	 * A named preference part that controls the highlighting of enum classes.
+	 */
+	public static final String ENUM_CLASS= "enumClass"; //$NON-NLS-1$
 
 	/**
 	 * A named preference part that controls the highlighting of macro references.
@@ -1113,6 +1119,10 @@ public class SemanticHighlightings {
 					return false;
 				}
 				IBinding binding= token.getBinding();
+				if (binding instanceof ICPPEnumeration) {
+					IType fType = ((ICPPEnumeration) binding).getFixedType();
+					return fType == null;
+				}
 				if (binding instanceof IEnumeration) {
 					return true;
 				}
@@ -1120,6 +1130,57 @@ public class SemanticHighlightings {
 					if (heuristicallyResolvesToEnumeration((ICPPUnknownBinding) binding)) {
 						return true;
 					}
+				}
+			}
+			return false;
+		}
+	}
+
+	/**
+	 * Semantic highlighting for enum classes.
+	 */
+	private static final class EnumClassHighlighting extends SemanticHighlightingWithOwnPreference {
+		@Override
+		public String getPreferenceKey() {
+			return ENUM_CLASS;
+		}
+
+		@Override
+		public RGB getDefaultDefaultTextColor() {
+			return new RGB(0, 80, 50);
+		}
+
+		@Override
+		public boolean isBoldByDefault() {
+			return false;
+		}
+
+		@Override
+		public boolean isItalicByDefault() {
+			return false;
+		}
+
+		@Override
+		public boolean isEnabledByDefault() {
+			return true;
+		}
+
+		@Override
+		public String getDisplayName() {
+			return CEditorMessages.SemanticHighlighting_enumClasses;
+		}
+
+		@Override
+		public boolean consumes(ISemanticToken token) {
+			IASTNode node= token.getNode();
+			if (node instanceof IASTName) {
+				if (node instanceof ICPPASTQualifiedName) {
+					return false;
+				}
+				IBinding binding= token.getBinding();
+				if (binding instanceof ICPPEnumeration) {
+					IType fType = ((ICPPEnumeration) binding).getFixedType();
+					return fType != null;
 				}
 			}
 			return false;
@@ -2012,6 +2073,7 @@ public class SemanticHighlightings {
 		highlightings.put(new Key(240), new ContextSensitiveKeywordHighlighting());
 		highlightings.put(new Key(250), new VariablePassedByNonconstRefHighlighting());
 		highlightings.put(new Key(260), new ContextSensitiveUDLHighlighting());
+		highlightings.put(new Key(270), new EnumClassHighlighting());
 	}
 
 	private static final String ExtensionPoint = "semanticHighlighting"; //$NON-NLS-1$

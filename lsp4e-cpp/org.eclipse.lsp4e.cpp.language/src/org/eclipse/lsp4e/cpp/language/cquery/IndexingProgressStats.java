495553e355a866dcf2ea1ffb82b05ae843d90e76
Manish Khurana <mkmanishkhurana98@gmail.com>
2018-06-25 12:16:13 +0530

    Bug 535629. Add empty implementation for unsupported CQuery notifications
    Change-Id: Ic1c91acd00f6d3aca64c82d4abffd8c41396b15c
    Signed-off-by: Manish Khurana <mkmanishkhurana98@gmail.com>

---
+++ lsp4e-cpp/org.eclipse.lsp4e.cpp.language/src/org/eclipse/lsp4e/cpp/language/cquery/IndexingProgressStats.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Manish Khurana , Nathan Ridge and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.lsp4e.cpp.language.cquery;
+
+public class IndexingProgressStats {
+	private int indexRequestCount;
+	private int doIdMapCount;
+	private int loadPreviousIndexCount;
+	private int onIdMappedCount;
+	private int onIndexedCount;
+	private int activeThreads;
+
+	public IndexingProgressStats(int indexRequestCount, int doIdMapCount, int loadPreviousIndexCount, int onIdMappedCount,
+			int onIndexedCount, int activeThreads) {
+		this.indexRequestCount = indexRequestCount;
+		this.doIdMapCount = doIdMapCount;
+		this.loadPreviousIndexCount = loadPreviousIndexCount;
+		this.onIdMappedCount = onIdMappedCount;
+		this.onIndexedCount = onIndexedCount;
+		this.activeThreads = activeThreads;
+	}
+
+	public int getIndexRequestCount() {
+		return indexRequestCount;
+	}
+
+	public int getDoIdMapCount() {
+		return doIdMapCount;
+	}
+
+	public int getLoadPreviousIndexCount() {
+		return loadPreviousIndexCount;
+	}
+
+	public int getOnIdMappedCount() {
+		return onIdMappedCount;
+	}
+
+	public int getOnIndexedCount() {
+		return onIndexedCount;
+	}
+
+	public int getActiveThreads() {
+		return activeThreads;
+	}
+}

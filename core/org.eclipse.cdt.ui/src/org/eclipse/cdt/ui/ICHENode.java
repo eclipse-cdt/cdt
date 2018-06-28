78a0de9e7cd511adb49549d27a3f9f9b85183851
Lidia Popescu <lidia.popescu@windriver.com>
2018-06-28 20:49:56 +0300

    Bug 536255 - Extension point for open call hierarchy view
    Adding a new extension point that makes possible to extend content from
    the Open Call Hierarchy View by adding a new node at the top of the
    tree.
    
    Change-Id: I9ac79896a4e8fffc9ed51cdb1be3c70f30d117c4
    Signed-off-by: Lidia Popescu <lidia.popescu@windriver.com>

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/ICHENode.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Lidia Popescu - [536255] initial API and implementation. Extension point for open call hierarchy view
+ *******************************************************************************/
+package org.eclipse.cdt.ui;
+
+
+import org.eclipse.cdt.core.model.ICElement;
+
+/**
+ * The Call Hierarchy Extension Node
+ * @since 6.4
+ */
+public interface ICHENode {
+
+	ICElement getRepresentedDeclaration();
+}

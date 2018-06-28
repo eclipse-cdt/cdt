4b8274b9a1a80231ae4d4f848693da79b3241157
Jeff Johnston <jjohnstn@redhat.com>
2018-06-28 15:25:11 -0400

    Bug 536317 - Deadlock at start with launchbar and docker tooling enabled
    - fix ContainerTargetTypeProvider changeEvent() to start a Job and
      return immediately so it will not cause a DockerConnection
      to be held in multi-threading
    - move the DockerConnectionManager addConnectionListener call
      to end of init() method so the fetching of images won't cause
      a notification event to occur
    - at end of init(), call CBuildConfigurationManager.recheckConfigs()
      to make sure any disabled configuration due to a missing
      IDockerConnection is now put in the configs master list and
      removed from the noconfigs list
    - make similar changes to ContainerGCCToolChainProvider
    
    Change-Id: Idc120d613b99ec365522f5e7bf5da82d1b362425
    (cherry picked from commit 2bcd06f097d1cc846baf8164705ff349843b8080)

--- launch/org.eclipse.cdt.docker.launcher/src/org/eclipse/cdt/internal/docker/launcher/ui/launchbar/ContainerGCCToolChainProvider.java
+++ launch/org.eclipse.cdt.docker.launcher/src/org/eclipse/cdt/internal/docker/launcher/ui/launchbar/ContainerGCCToolChainProvider.java
@@ -25,7 +25,11 @@ import org.eclipse.cdt.docker.launcher.ContainerTargetTypeProvider;
 import org.eclipse.cdt.docker.launcher.DockerLaunchUIPlugin;
 import org.eclipse.cdt.docker.launcher.IContainerLaunchTarget;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.launchbar.core.target.ILaunchTarget;
 import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
@@ -58,8 +62,6 @@ public class ContainerGCCToolChainProvider
 		this.toolChainManager = manager;
 		IDockerConnection[] connections = DockerConnectionManager.getInstance()
 				.getConnections();
-		DockerConnectionManager.getInstance()
-				.addConnectionManagerListener(this);
 		Map<String, IDockerConnection> connectionMap = new HashMap<>();
 		for (IDockerConnection connection : connections) {
 			connectionMap.put(connection.getUri(), connection);
@@ -80,6 +82,7 @@ public class ContainerGCCToolChainProvider
 					// following can be used for naming build configurations
 					properties.put(CONTAINER_LINUX_CONFIG_ID,
 							image.repoTags().get(0).replace(':', '_'));
+					// .replace('/', '_'));
 
 					ContainerGCCToolChain toolChain = new ContainerGCCToolChain(
 							"gcc-img-" + image.id().substring(0, 19), //$NON-NLS-1$
@@ -90,73 +93,100 @@ public class ContainerGCCToolChainProvider
 			}
 		}
 
+		// add a Docker Connection listener to handle enablement/disablement of
+		// Connections
+		DockerConnectionManager.getInstance()
+				.addConnectionManagerListener(this);
+
+		// call the recheckConfigs method in case any disabled targets are now
+		// ok
+		ICBuildConfigurationManager mgr = CCorePlugin
+				.getService(ICBuildConfigurationManager.class);
+		ICBuildConfigurationManager2 cbuildmanager = (ICBuildConfigurationManager2) mgr;
+		cbuildmanager.recheckConfigs();
+
 	}
 
 	@Override
 	public synchronized void changeEvent(IDockerConnection connection,
 			int type) {
-		ICBuildConfigurationManager mgr = CCorePlugin
-				.getService(ICBuildConfigurationManager.class);
-		ICBuildConfigurationManager2 manager = (ICBuildConfigurationManager2) mgr;
 
-		if (type == IDockerConnectionManagerListener.ADD_EVENT
-				|| type == IDockerConnectionManagerListener.ENABLE_EVENT) {
-			List<IDockerImage> images = connection.getImages();
-			for (IDockerImage image : images) {
-				if (!image.isDangling() && !image.isIntermediateImage()) {
+		final ContainerGCCToolChainProvider provider = this;
 
-					Map<String, String> properties = new HashMap<>();
+		Job checkConfigs = new Job("Check configs") { //$NON-NLS-1$
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
 
-					properties.put(ILaunchTarget.ATTR_OS,
-							ContainerTargetTypeProvider.CONTAINER_LINUX);
-					properties.put(ILaunchTarget.ATTR_ARCH,
-							Platform.getOSArch());
-					properties.put(IContainerLaunchTarget.ATTR_CONNECTION_URI,
-							connection.getUri());
-					properties.put(IContainerLaunchTarget.ATTR_IMAGE_ID,
-							image.repoTags().get(0));
-					// following can be used for naming build configurations
-					properties.put(CONTAINER_LINUX_CONFIG_ID,
-							image.repoTags().get(0).replace(':', '_'));
+				ICBuildConfigurationManager mgr = CCorePlugin
+						.getService(ICBuildConfigurationManager.class);
+				ICBuildConfigurationManager2 manager = (ICBuildConfigurationManager2) mgr;
+
+				if (type == IDockerConnectionManagerListener.ADD_EVENT
+						|| type == IDockerConnectionManagerListener.ENABLE_EVENT) {
+					List<IDockerImage> images = connection.getImages();
+					for (IDockerImage image : images) {
+						if (!image.isDangling()
+								&& !image.isIntermediateImage()) {
+
+							Map<String, String> properties = new HashMap<>();
+
+							properties.put(ILaunchTarget.ATTR_OS,
+									ContainerTargetTypeProvider.CONTAINER_LINUX);
+							properties.put(ILaunchTarget.ATTR_ARCH,
+									Platform.getOSArch());
+							properties.put(
+									IContainerLaunchTarget.ATTR_CONNECTION_URI,
+									connection.getUri());
+							properties.put(IContainerLaunchTarget.ATTR_IMAGE_ID,
+									image.repoTags().get(0));
+							// following can be used for naming build
+							// configurations
+							properties.put(CONTAINER_LINUX_CONFIG_ID,
+									image.repoTags().get(0).replace(':', '_'));
+							// .replace('/', '_'));
 
 
-					Collection<IToolChain> toolChains;
-					try {
-						toolChains = toolChainManager
-								.getToolChainsMatching(properties);
-						if (toolChains.isEmpty()) {
-							ContainerGCCToolChain toolChain = new ContainerGCCToolChain(
-									"gcc-img-" + image.id().substring(0, 19), //$NON-NLS-1$
-									this, properties, null);
-							toolChainManager.addToolChain(toolChain);
+							Collection<IToolChain> toolChains;
+							try {
+								toolChains = toolChainManager
+										.getToolChainsMatching(properties);
+								if (toolChains.isEmpty()) {
+									ContainerGCCToolChain toolChain = new ContainerGCCToolChain(
+											"gcc-img-" + image.id().substring(0, //$NON-NLS-1$
+													19), provider, properties, null);
+									toolChainManager.addToolChain(toolChain);
+								}
+							} catch (CoreException e) {
+								DockerLaunchUIPlugin.log(e);
+							}
 						}
-					} catch (CoreException e) {
-						DockerLaunchUIPlugin.log(e);
+					}
+					// Re-evaluate config list in case a build config was marked
+					// invalid and is now enabled
+					manager.recheckConfigs();
+				} else if (type == IDockerConnectionManagerListener.REMOVE_EVENT
+						|| type == IDockerConnectionManagerListener.DISABLE_EVENT) {
+					try {
+						String connectionURI = connection.getUri();
+						Collection<IToolChain> toolChains = toolChainManager
+								.getAllToolChains();
+						for (IToolChain toolChain : toolChains) {
+							String uri = toolChain.getProperty(
+									IContainerLaunchTarget.ATTR_CONNECTION_URI);
+							if (connectionURI.equals(uri)) {
+								toolChainManager.removeToolChain(toolChain);
+							}
+						}
+					} catch (CoreException e1) {
+						// TODO Auto-generated catch block
+						DockerLaunchUIPlugin.log(e1);
 					}
 				}
+				return Status.OK_STATUS;
 			}
-			// Re-evaluate config list in case a build config was marked
-			// invalid and is now enabled
-			manager.recheckConfigs();
-		} else if (type == IDockerConnectionManagerListener.REMOVE_EVENT
-				|| type == IDockerConnectionManagerListener.DISABLE_EVENT) {
-			try {
-				String connectionURI = connection.getUri();
-				Collection<IToolChain> toolChains = toolChainManager
-						.getAllToolChains();
-				for (IToolChain toolChain : toolChains) {
-					String uri = toolChain.getProperty(
-							IContainerLaunchTarget.ATTR_CONNECTION_URI);
-					if (connectionURI.equals(uri)) {
-						toolChainManager.removeToolChain(toolChain);
-					}
-				}
-			} catch (CoreException e1) {
-				// TODO Auto-generated catch block
-				DockerLaunchUIPlugin.log(e1);
-			}
-		}
-
+		};
+		checkConfigs.setUser(true);
+		checkConfigs.schedule();
 	}
 
 }

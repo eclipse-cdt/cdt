ea8d975e4a10ce9cdebbca099d14a36078377ad0
Baha El Kassaby <baha.elkassaby@gmail.com>
2018-07-07 03:10:15 +0200

    Bug 530443: Info Sources clean-up
    *Add missing licence and comments
    *Open non existing files in CSourceNotFound editor
    *populate the tree in a job and check for file existence in a job
    *Remove unnecessary code
    
    Change-Id: I040b538c044ebe007ccb912d2897e5bf4707bd81
    Signed-off-by: Baha El Kassaby <baha.elkassaby@gmail.com>
    Also-by: Jonah Graham <jonah@kichwacoders.com>

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/debugsources/DebugSourcesViewRefresh.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/debugsources/DebugSourcesViewRefresh.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Jonah Graham- Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.dsf.gdb.internal.ui.debugsources;
 
 import org.eclipse.debug.ui.DebugUITools;
@@ -65,7 +75,7 @@ public class DebugSourcesViewRefresh implements IViewActionDelegate, IDebugConte
 
 	@Override
 	public void run(IAction action) {
-		throw new UnsupportedOperationException("call runWithEvent instead");
+		throw new UnsupportedOperationException("call runWithEvent instead"); //$NON-NLS-1$
 	}
 
 	@Override

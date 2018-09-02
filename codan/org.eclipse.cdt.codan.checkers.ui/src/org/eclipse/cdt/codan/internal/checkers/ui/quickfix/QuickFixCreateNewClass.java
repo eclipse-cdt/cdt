4592a0774f5200d8ff2e024a1a47ffc7f4ff1c29
Nathan Ridge <zeratul976@hotmail.com>
2018-09-01 23:58:17 -0400

    Bug 538266 - NPE in QuickFixCreateNewClass.isApplicable()
    Change-Id: If20b46da95f58e93ba46ea6f4386e841609f4159

--- codan/org.eclipse.cdt.codan.checkers.ui/src/org/eclipse/cdt/codan/internal/checkers/ui/quickfix/QuickFixCreateNewClass.java
+++ codan/org.eclipse.cdt.codan.checkers.ui/src/org/eclipse/cdt/codan/internal/checkers/ui/quickfix/QuickFixCreateNewClass.java
@@ -20,6 +20,7 @@ import org.eclipse.cdt.codan.internal.checkers.ui.CheckersUiActivator;
 import org.eclipse.cdt.codan.ui.AbstractCodanCMarkerResolution;
 import org.eclipse.cdt.core.dom.ILinkage;
 import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.ILanguage;
 import org.eclipse.cdt.core.model.ITranslationUnit;
 import org.eclipse.cdt.internal.corext.util.CModelUtil;
 import org.eclipse.cdt.internal.ui.wizards.NewClassCreationWizard;
@@ -67,7 +68,8 @@ public class QuickFixCreateNewClass extends AbstractCodanCMarkerResolution imple
 	public boolean isApplicable(IMarker marker) {
 		return getTranslationUnitViaEditorOrWorkspace(marker).map(tu -> {
 			try {
-				return tu.getLanguage().getLinkageID() != ILinkage.C_LINKAGE_ID;
+				ILanguage language = tu.getLanguage(); 
+				return language != null && language.getLinkageID() != ILinkage.C_LINKAGE_ID;
 			} catch (CoreException e) {
 				return true;
 			}

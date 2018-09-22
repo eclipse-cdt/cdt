b031c502e0021fd9285a59298bd16d4e9019f633
Marc-Andre Laperle <malaperle@gmail.com>
2018-09-22 00:08:58 -0400

    Bug 486036 - Toggle Function Definition does not work when comment is involved
    Change-Id: I77a2c2fe0607ce098da4a4e7679fc3999ea7ebbf
    Signed-off-by: Marc-Andre Laperle <malaperle@gmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/refactoring/togglefunction/ToggleRefactoringTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/refactoring/togglefunction/ToggleRefactoringTest.java
@@ -3648,4 +3648,22 @@ public class ToggleRefactoringTest extends RefactoringTestBase {
 	public void testToggleWithLambdaExpression_518271() throws Exception {
 		assertRefactoringSuccess();
 	}
+
+	//Test.cpp
+	//class MyClass {
+	//	void MyMethod();
+	//};
+	//
+	//void MyClass::/*$*/MyMethod/*$$*/() {
+	//// A comment
+	//}
+	//====================
+	//class MyClass {
+	//	void MyMethod() {
+	//		// A comment
+	//	}
+	//};
+	public void testToggleWithCommentsInSameFile_486036() throws Exception {
+		assertRefactoringSuccess();
+	}
 }

6a33dd10bcafc61b45bb4e777f564493f3f0a510
Felix Morgner <fmorgner@hsr.ch>
2018-11-14 10:26:59 +0100

    541239: [C++17] Implement parser support for deduction guides
    Change-Id: I1683583af981d276b346ba8470a9044cfd8921cd
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=541239
    Signed-off-by: Felix Morgner <fmorgner@hsr.ch>

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPASTAmbiguousTemplateArgument.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/CPPASTAmbiguousTemplateArgument.java
@@ -47,6 +47,7 @@ public class CPPASTAmbiguousTemplateArgument extends ASTAmbiguousNode implements
 		fNodes = new ArrayList<>(2);
 		for (IASTNode node : nodes) {
 			if (node instanceof IASTTypeId || node instanceof IASTExpression) {
+				node.setParent(this);
 				fNodes.add(node);
 			} else {
 				Assert.isLegal(false, node == null ? "null" : node.getClass().getName()); //$NON-NLS-1$

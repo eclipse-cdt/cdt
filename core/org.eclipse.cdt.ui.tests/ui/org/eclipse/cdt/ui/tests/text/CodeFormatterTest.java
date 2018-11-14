59c6ec4e0c1b29828882121ab1bb245769b5d918
Felix Morgner <fmorgner@hsr.ch>
2018-11-14 10:26:59 +0100

    541239: [C++17] Implement parser support for deduction guides
    Change-Id: I1683583af981d276b346ba8470a9044cfd8921cd
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=541239
    Signed-off-by: Felix Morgner <fmorgner@hsr.ch>
    Also-by: Marco Stornelli <marco.stornelli@gmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
@@ -4734,4 +4734,41 @@ public class CodeFormatterTest extends BaseUITestCase {
 	public void testFormatterOnOff_Bug559669() throws Exception {
 		assertFormatterResult();
 	}
+
+	//template<class T>
+	//struct U;
+	//U()->U<int>;
+
+	//template<class T>
+	//struct U;
+	//U() -> U<int>;
+	public void testDeductionGuide() throws Exception {
+		assertFormatterResult();
+	}
+
+	//template<class T>
+	//struct U;
+	//template<class C>
+	//U(C)->U<C>;
+
+	//template<class T>
+	//struct U;
+	//template<class C>
+	//U(C) -> U<C>;
+	public void testDeductionGuideTemplate() throws Exception {
+		assertFormatterResult();
+	}
+
+	//template<class T>
+	//struct U;
+	//template<class C>
+	//explicit U(C)->U<C>;
+
+	//template<class T>
+	//struct U;
+	//template<class C>
+	//explicit U(C) -> U<C>;
+	public void testDeductionGuideExplicit() throws Exception {
+		assertFormatterResult();
+	}
 }

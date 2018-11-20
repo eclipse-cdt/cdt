b836eb6ae189d6d0c47e26298807ac3ffbff8f50
Jonah Graham <jonah@kichwacoders.com>
2018-11-20 11:39:12 +0000

    Bug 540371: normalize files before applying EPLv2
    Change-Id: I8bfd427bba1df18579d6d5e58ad1e5d704485050

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist2/TemplateProposalTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/contentassist2/TemplateProposalTest.java
@@ -1,8 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Marc-Andre Laperle and others. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2012, 2014 Marc-Andre Laperle and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors: 
  *     Marc-Andre Laperle - initial API and implementation

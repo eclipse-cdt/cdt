4b833cd56f7db57c7dc786485d0fc70824b18515
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-09 10:22:10 +0100

    Bug 303870 - Add override virtual methods functionality
    Added overridemethods package
    
    Change-Id: I73a8f0a396336acf7d3bbc8988e629da510ae781
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>
    Signed-off-by: pmarek <pavel.akira.marek@gmail.com>

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/refactoring/overridemethods/OverrideMethodsAction.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Pavel Marek
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *      Pavel Marek - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.internal.ui.refactoring.overridemethods;
+
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.IWorkingCopy;
+import org.eclipse.cdt.ui.refactoring.actions.RefactoringAction;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.ui.IEditorPart;
+
+/**
+ * @noextend This class is not intended to be subclassed by clients.
+ */
+public class OverrideMethodsAction extends RefactoringAction implements ISelectionChangedListener {
+
+	public OverrideMethodsAction() {
+		super(Messages.OverrideMethods_label);
+	}
+
+	public OverrideMethodsAction(IEditorPart editor) {
+		this();
+		setEditor(editor);
+	}
+
+	@Override
+	public void run(IShellProvider shellProvider, IWorkingCopy wc, ITextSelection selection) {
+		if (wc.getResource() != null) {
+			new OverrideMethodsRefactoringRunner(wc, selection, shellProvider, wc.getCProject()).run();
+		}
+	}
+
+	@Override
+	public void run(IShellProvider shellProvider, ICElement elem) {
+	}
+
+	@Override
+	public void updateSelection(ICElement elem) {
+		super.updateSelection(elem);
+		setEnabled(false);
+	}
+
+	public void update(ISelection selection) {
+
+	}
+
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+
+	}
+}

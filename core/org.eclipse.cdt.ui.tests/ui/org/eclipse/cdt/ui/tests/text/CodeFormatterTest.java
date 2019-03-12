d34d2f3a5ddbfe75ab11f81e41cfb8be889eb89d
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-12 18:36:09 +0100

    Bug 449394 - Fix constructor init list formatting
    Init list was always on new lines regardless user input about
    new line policy or wrapping policy.
    
    Change-Id: I1ac85611129c23e89d9b299ca87ced314ad542f3
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
@@ -1731,8 +1731,7 @@ public class CodeFormatterTest extends BaseUITestCase {
 
 	//class Point {
 	//public:
-	//    Point(int x, int y) :
-	//            x(x), y(y) {
+	//    Point(int x, int y) : x(x), y(y) {
 	//    }
 	//
 	//private:

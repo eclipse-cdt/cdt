2084df2b54a942ce0ba748919f63906eb1d38a83
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-13 18:06:38 +0100

    Bug 376395 - Fix const methods with never join wrapped lines
    When at the same time never join wrapped lines and next line
    for opening function brace were selected, two tabs were added.
    
    Change-Id: I90f606bf7f8b7bb7e83f3ae85d7cbb9cd0146b97
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
@@ -4018,4 +4018,20 @@ public class CodeFormatterTest extends BaseUITestCase {
 	public void testBoolInStructWithMacro_Bug397710() throws Exception {
 		assertFormatterResult();
 	}
+
+	//int foo(int arg1) const
+	//{
+	//	return 2;
+	//}
+
+	//int foo(int arg1) const
+	//{
+	//	return 2;
+	//}
+	public void testConstMethodsWithNeverJoin_Bug376395() throws Exception {
+		fOptions.put(DefaultCodeFormatterConstants.FORMATTER_JOIN_WRAPPED_LINES, false);
+		fOptions.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_METHOD_DECLARATION,
+				DefaultCodeFormatterConstants.NEXT_LINE);
+		assertFormatterResult();
+	}
 }

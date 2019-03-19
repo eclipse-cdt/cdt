f2635eed74b108675d48c215e7ebc5d20773910b
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-19 20:34:27 +0100

    Bug 545562 - Fix return checker check for template functions
    Change-Id: Ie5e3d4560cb3784f6c8393290d64794db56294d7
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>

--- codan/org.eclipse.cdt.codan.core.tests/src/org/eclipse/cdt/codan/core/internal/checkers/ReturnCheckerTest.java
+++ codan/org.eclipse.cdt.codan.core.tests/src/org/eclipse/cdt/codan/core/internal/checkers/ReturnCheckerTest.java
@@ -504,4 +504,15 @@ public class ReturnCheckerTest extends CheckerTestCase {
 	public void testFunctionWithAttribute_519105() throws Exception {
 		checkSampleAboveCpp();
 	}
+
+	//	template <typename T>
+	//	[[noreturn]] void foo(const T& e) {
+	//		throw e;
+	//	}
+	//	int bar() {
+	//		foo<int>(5);
+	//	}
+	public void testTemplateFunctionNoReturn() throws Exception {
+		checkSampleAboveCpp();
+	}
 }

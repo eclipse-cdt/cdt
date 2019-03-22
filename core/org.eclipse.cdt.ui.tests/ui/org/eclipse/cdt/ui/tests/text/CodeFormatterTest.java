fb9ae6720893002261ebd151a518d0ee157c46de
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-22 19:18:04 +0100

    Bug 500000 - Added proper formatting for lambda expressions
    Change-Id: Ia530b00f3710b74d1749978c9c5d23a2d55646f0
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/text/CodeFormatterTest.java
@@ -3903,4 +3903,37 @@ public class CodeFormatterTest extends BaseUITestCase {
 	public void testFormmatterWithMacroFuncStyle_Bug475349() throws Exception {
 		assertFormatterResult();
 	}
+	
+	//int main() {
+	//	auto f = []() {
+	//
+	//		for(int c1 = 0; c1 < 3; c1 ++) {
+	//			for(int i = 0; i < 5; i ++) {
+	//				for(int c2 = 0; c2 < 3; c2 ++) {
+	//					// keep me here
+	//				}
+	//			}
+	//		}
+	//	};
+	//	f();
+	//	return 0;
+	//}
+
+	//int main() {
+	//	auto f = []() {
+	//
+	//		for (int c1 = 0; c1 < 3; c1++) {
+	//			for (int i = 0; i < 5; i++) {
+	//				for (int c2 = 0; c2 < 3; c2++) {
+	//					// keep me here
+	//				}
+	//			}
+	//		}
+	//	};
+	//	f();
+	//	return 0;
+	//}
+	public void testAlignmentOfLambda_Bug500000() throws Exception {
+		assertFormatterResult();
+	}
 }

265e3fc23c4d5d0ebb4b4ad125ffbf527854d8e6
Jeff Johnston <jjohnstn@redhat.com>
2019-03-26 17:34:16 -0400

    Bug 545819 - Meson properties dialog doesn't work with meson 0.50.1
    - fix meson properties page to use meson setup -h
    - reenable meson ui tests
    - fix CMake New Project test to wait for shell open/closes
    - fix regression in EvalUnaryTypeID that is made visible by
      CMake New project test
    
    Change-Id: Id64d4df6d9764d0c84553b5c4f73fa44149bda19

--- build/org.eclipse.cdt.meson.ui.tests/src/org/eclipse/cdt/internal/meson/ui/tests/NewMesonProjectTest.java
+++ build/org.eclipse.cdt.meson.ui.tests/src/org/eclipse/cdt/internal/meson/ui/tests/NewMesonProjectTest.java
@@ -43,13 +43,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @SuppressWarnings("nls")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Ignore
 public class NewMesonProjectTest {
 
 	private static SWTWorkbenchBot bot;
@@ -185,10 +183,10 @@ public class NewMesonProjectTest {
 		assertEquals("Build dir: " + projectPath + "/build/default", lines[5]);
 		assertEquals("Build type: native build", lines[6]);
 		assertEquals("Project name: MesonTestProj", lines[7]);
-		assertTrue(lines[8].startsWith("Native C compiler: cc"));
-		assertEquals("Build targets in project: 1", lines[11]);
-		assertTrue(lines[lines.length - 3].startsWith("[1/2] cc  -IMesonTestProj@exe"));
-		assertTrue(lines[lines.length - 2].startsWith("[2/2] cc  -o MesonTestProj"));
+		assertTrue(lines[lines.length - 3].startsWith("[1/2] cc"));
+		assertTrue(lines[lines.length - 3].contains("-IMesonTestProj@exe"));
+		assertTrue(lines[lines.length - 2].startsWith("[2/2] cc"));
+		assertTrue(lines[lines.length - 2].contains("-o MesonTestProj"));
 
 		int i = 0;
 		while (i < 10 && !lines[lines.length - 1].startsWith("Build complete")) {

89e0f92d152ab8ba768633c4b653307227cf5f67
Marco Stornelli <marco.stornelli@gmail.com>
2019-03-31 12:22:55 +0200

    Bug 545977 - Added checker for variable initialization
    Checker checks for multiple declarations on one line and
    static variables defined in header files.
    
    Change-Id: Ibc9670ee129e9bdd3ea58ac5409493fd99c4a234
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>

---
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/VariableInitializationChecker.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Marco Stornelli
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.codan.internal.checkers;
+
+import org.eclipse.cdt.codan.checkers.CodanCheckersActivator;
+import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.DOMException;
+import org.eclipse.cdt.core.dom.ast.EScopeKind;
+import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
+import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTDeclarationStatement;
+import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTForStatement;
+import org.eclipse.cdt.core.dom.ast.IASTIfStatement;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTSimpleDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTSwitchStatement;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IBinding;
+import org.eclipse.cdt.core.dom.ast.IScope;
+import org.eclipse.cdt.core.dom.ast.IVariable;
+
+public class VariableInitializationChecker extends AbstractIndexAstChecker {
+	public static final String STATIC_VAR_ID = "org.eclipse.cdt.codan.internal.checkers.StaticVariableInHeaderProblem"; //$NON-NLS-1$
+	public static final String VAR_MULTI_DEC_ID = "org.eclipse.cdt.codan.internal.checkers.MultipleDeclarationsProblem"; //$NON-NLS-1$
+
+	@Override
+	public void processAst(IASTTranslationUnit ast) {
+		ast.accept(new ASTVisitor() {
+			{
+				shouldVisitDeclarations = true;
+			}
+
+			@Override
+			public int visit(IASTDeclaration declaration) {
+				if (declaration instanceof IASTSimpleDeclaration) {
+					IASTSimpleDeclaration simple = (IASTSimpleDeclaration) declaration;
+					IASTDeclarator decls[] = ((IASTSimpleDeclaration) declaration).getDeclarators();
+					if (simple.getDeclSpecifier().getStorageClass() == IASTDeclSpecifier.sc_static
+							&& declaration.getTranslationUnit().isHeaderUnit()) {
+						for (IASTDeclarator d : decls) {
+							IBinding binding = d.getName().resolveBinding();
+							if (binding != null && binding instanceof IVariable) {
+								try {
+									IScope scope = binding.getScope();
+									if (scope == null)
+										return PROCESS_CONTINUE;
+									if (scope.getKind() == EScopeKind.eGlobal
+											|| scope.getKind() == EScopeKind.eNamespace)
+										reportProblem(STATIC_VAR_ID, declaration, d.getName());
+								} catch (DOMException e) {
+									CodanCheckersActivator.log(e);
+								}
+							}
+						}
+					}
+					if (declaration.getParent() instanceof IASTDeclarationStatement) {
+						IASTDeclarationStatement declStatement = (IASTDeclarationStatement) declaration.getParent();
+						IASTNode node = declStatement.getParent();
+						if (node instanceof IASTForStatement || node instanceof IASTIfStatement
+								|| node instanceof IASTSwitchStatement) {
+							//we allow multiple declarations for if/switch initializers and for statements
+							return PROCESS_CONTINUE;
+						}
+					}
+					if (decls.length == 0)
+						return PROCESS_CONTINUE;
+					if (decls.length != 1)
+						reportProblem(VAR_MULTI_DEC_ID, declaration);
+				}
+				return PROCESS_CONTINUE;
+			}
+		});
+	}
+}
---
+++ codan/org.eclipse.cdt.codan.core.tests/src/org/eclipse/cdt/codan/core/internal/checkers/VariablesCheckerTest.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Marco Stornelli
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.codan.core.internal.checkers;
+
+import org.eclipse.cdt.codan.core.tests.CheckerTestCase;
+import org.eclipse.cdt.codan.internal.checkers.VariableInitializationChecker;
+
+/**
+ * Test for {@link VariableInitializationChecker} class
+ */
+public class VariablesCheckerTest extends CheckerTestCase {
+
+	public static final String STATIC_ID = VariableInitializationChecker.STATIC_VAR_ID;
+	public static final String MULTI_ID = VariableInitializationChecker.VAR_MULTI_DEC_ID;
+
+	private boolean header;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		header = true;
+		enableProblems(STATIC_ID, MULTI_ID);
+	}
+
+	@Override
+	public boolean isCpp() {
+		return true;
+	}
+
+	@Override
+	public boolean isHeader() {
+		return header;
+	}
+
+	//static int a = 0;
+	public void testStaticVarInHeader() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(1, STATIC_ID);
+	}
+
+	//static int a = 0;
+	public void testStaticVarInCpp() throws Exception {
+		header = false;
+		loadCodeAndRun(getAboveComment());
+		checkNoErrorsOfKind(STATIC_ID);
+	}
+
+	//void foo() {
+	//    int p = 0, u = 0;
+	//}
+	public void testMultiInitVar() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(2, MULTI_ID);
+	}
+
+	//void foo() {
+	//    int p = 0;
+	//    int u = 0;
+	//}
+	public void testCleanVar() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrorsOfKind(MULTI_ID);
+		checkNoErrorsOfKind(STATIC_ID);
+	}
+
+	//void foo() {
+	//		for (int i = 0, j = 1; i < 10; ++i, ++j) {
+	//		}
+	//}
+	public void testFor() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrorsOfKind(MULTI_ID);
+		checkNoErrorsOfKind(STATIC_ID);
+	}
+
+	//void foo() {
+	//		if (int i = 0, i < 0) {
+	//		}
+	//}
+	public void testIf() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrorsOfKind(MULTI_ID);
+		checkNoErrorsOfKind(STATIC_ID);
+	}
+
+	//void foo() {
+	//		switch (int i = 0, i) {
+	//		default:
+	//			break;
+	//		}
+	//}
+	public void testSwitch() throws Exception {
+		loadCodeAndRun(getAboveComment());
+		checkNoErrorsOfKind(MULTI_ID);
+		checkNoErrorsOfKind(STATIC_ID);
+	}
+}

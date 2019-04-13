e153fa920d41caf4456943e9950f79866215dcb2
Jeff Johnston <jjohnstn@redhat.com>
2019-04-12 23:13:36 -0400

    Bug 546390 - Add Container build support to new Core Autotools
    - add support for Container builds to AutotoolsBuildConfiguration,
      AutotoolsBuildConfigurationProvider
    - fix core Autotools templates to set up the default source and
      binary to be the same as the project name
    
    Change-Id: Ib262c1f6ddd65b136be2d2d3d25601226ddd289f

--- build/org.eclipse.cdt.core.autotools.core/src/org/eclipse/cdt/core/autotools/core/AutotoolsBuildConfiguration.java
+++ build/org.eclipse.cdt.core.autotools.core/src/org/eclipse/cdt/core/autotools/core/AutotoolsBuildConfiguration.java
@@ -16,14 +16,20 @@ package org.eclipse.cdt.core.autotools.core;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.cdt.core.ConsoleOutputStream;
+import org.eclipse.cdt.core.ErrorParserManager;
+import org.eclipse.cdt.core.IConsoleParser;
 import org.eclipse.cdt.core.autotools.core.internal.Activator;
 import org.eclipse.cdt.core.build.CBuildConfiguration;
 import org.eclipse.cdt.core.build.IToolChain;
+import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
+import org.eclipse.cdt.core.model.ICModelMarker;
 import org.eclipse.cdt.core.resources.IConsole;
 import org.eclipse.core.resources.IBuildConfiguration;
 import org.eclipse.core.resources.IProject;
@@ -48,6 +54,11 @@ public class AutotoolsBuildConfiguration extends CBuildConfiguration {
 		super(config, name, toolChain, "run"); // TODO: why "run" //$NON-NLS-1$
 	}
 
+	public AutotoolsBuildConfiguration(IBuildConfiguration config, String name, IToolChain toolChain,
+			String launchMode) {
+		super(config, name, toolChain, launchMode);
+	}
+
 	@Override
 	public IProject[] build(int kind, Map<String, String> args, IConsole console, IProgressMonitor monitor)
 			throws CoreException {
@@ -55,15 +66,15 @@ public class AutotoolsBuildConfiguration extends CBuildConfiguration {
 		IProject project = getProject();
 
 		execute(Arrays.asList(new String[] { "autoreconf", "--install" }), project.getLocation(), console, monitor); //$NON-NLS-1$ //$NON-NLS-2$
-		execute(Arrays.asList(new String[] { "./configure" }), project.getLocation(), console, monitor); //$NON-NLS-1$
-		execute(Arrays.asList(new String[] { "make" }), project.getLocation(), console, monitor); //$NON-NLS-1$
+		executeRemote(Arrays.asList(new String[] { "./configure" }), project.getLocation(), console, monitor); //$NON-NLS-1$
+		executeRemote(Arrays.asList(new String[] { "make" }), project.getLocation(), console, monitor); //$NON-NLS-1$
 
 		return new IProject[] { project };
 	}
 
 	@Override
 	public void clean(IConsole console, IProgressMonitor monitor) throws CoreException {
-		execute(Arrays.asList(new String[] { "make", "clean" }), getProject().getLocation(), console, monitor); //$NON-NLS-1$ //$NON-NLS-2$
+		executeRemote(Arrays.asList(new String[] { "make", "clean" }), getProject().getLocation(), console, monitor); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	protected void execute(List<String> command, IPath dir, IConsole console, IProgressMonitor monitor)
@@ -97,10 +108,60 @@ public class AutotoolsBuildConfiguration extends CBuildConfiguration {
 			Process process = builder.start();
 			watchProcess(process, console);
 		} catch (IOException e) {
-			throw new CoreException(Activator.errorStatus("Error executing: " + String.join(" ", command), e)); //$NON-NLS-2$
+			throw new CoreException(Activator.errorStatus("Error executing: " + String.join(" ", command), e)); //$NON-NLS-1$ //$NON-NLS-2$
 		}
 
 		getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+	}
+
+	protected void executeRemote(List<String> command, IPath dir, IConsole console, IProgressMonitor monitor)
+			throws CoreException {
+
+		IProject project = getProject();
+
+		try {
+			project.deleteMarkers(ICModelMarker.C_MODEL_PROBLEM_MARKER, false, IResource.DEPTH_INFINITE);
+
+			ConsoleOutputStream outStream = console.getOutputStream();
+
+			Path buildDir = getBuildDirectory();
+
+			String cmd = command.get(0);
+
+			if (cmd.startsWith(".")) { //$NON-NLS-1$
+				Path cmdPath = Paths.get(dir.toString(), cmd);
+				if (cmdPath.toFile().exists()) {
+					command.set(0, cmdPath.toAbsolutePath().toString());
+				}
+			}
+
+			outStream.write("Building in: " + buildDir.toString() + "\n"); //$NON-NLS-1$ //$NON-NLS-2$
+
+			try (ErrorParserManager epm = new ErrorParserManager(project, getBuildDirectoryURI(), this,
+					getToolChain().getErrorParserIds())) {
+				epm.setOutputStream(console.getOutputStream());
+
+				IEnvironmentVariable[] env = new IEnvironmentVariable[0];
+
+				org.eclipse.core.runtime.Path workingDir = new org.eclipse.core.runtime.Path(
+						getBuildDirectory().toString());
+
+				Process p = startBuildProcess(command, env, workingDir, console, monitor);
+				if (p == null) {
+					console.getErrorStream().write(String.format("Error executing: {0}", String.join(" ", command))); //$NON-NLS-1$ //$NON-NLS-2$
+					throw new CoreException(
+							Activator.errorStatus("Error executing: " + String.join(" ", command), null)); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+
+				watchProcess(p, new IConsoleParser[] { epm });
+			}
+
+			project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+
+		} catch (IOException e) {
+			throw new CoreException(Activator.errorStatus("Error executing: " + String.join(" ", command), e)); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
 	}
 
 }

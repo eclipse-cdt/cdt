b7bd96d37145e5ff45f2777e659e8a651582c74a
Jonah Graham <jonah@kichwacoders.com>
2020-01-07 15:07:12 -0500

    Bug 551817: Normalize case of (c) to match Eclipse Handbook
    Change-Id: I46404d20d20c385bf8efaaada66e18eea4d4e163

--- build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/TestPathConverter3.java
+++ build/org.eclipse.cdt.managedbuilder.core.tests/tests/org/eclipse/cdt/managedbuilder/core/tests/TestPathConverter3.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2006 Siemens AG.
+ * Copyright (c) 2006 Siemens AG.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0

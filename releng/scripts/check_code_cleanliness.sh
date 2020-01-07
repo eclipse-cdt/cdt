d1a15ef4737adbed626b7b5fdc5164ae76641a05
Jonah Graham <jonah@kichwacoders.com>
2020-01-07 06:36:50 -0500

    Bug 558827: Add cleanliness check for features
    Change-Id: Id25d340506210206dcd8f0ce93f2e01a041d4e1e

--- releng/scripts/check_code_cleanliness.sh
+++ releng/scripts/check_code_cleanliness.sh
@@ -3,6 +3,12 @@
 set -e
 
 ##
+# Check the features are all branded
+##
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+${DIR}/check_features.sh
+
+##
 # The script is shared by all the CDT projects. When running
 # local edits, you can, for example, do:
 # SCRIPT_URL=file:///scratch/eclipse/src/cdt/cdt-infra/scripts/check_code_cleanliness.sh ./check_code_cleanliness.sh

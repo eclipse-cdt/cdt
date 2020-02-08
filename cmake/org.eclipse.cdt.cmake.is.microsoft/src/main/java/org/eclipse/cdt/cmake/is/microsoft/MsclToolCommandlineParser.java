a0e896c20af5858f345bca7d4f7647b99f37abd5
Martin Weber <fifteenknots505@gmail.com>
2020-02-08 15:01:33 +0100

    Bug 559674: initial import from cmake4eclipse
    Change-Id: I3b9b5b1079ca8692ed0989baab281a1b79987267
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

---
+++ cmake/org.eclipse.cdt.cmake.is.microsoft/src/main/java/org/eclipse/cdt/cmake/is/microsoft/MsclToolCommandlineParser.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.cdt.cmake.is.microsoft;
+
+import org.eclipse.cdt.cmake.is.core.Arglets.IncludePathGeneric;
+import org.eclipse.cdt.cmake.is.core.Arglets.MacroDefineGeneric;
+import org.eclipse.cdt.cmake.is.core.Arglets.MacroUndefineGeneric;
+import org.eclipse.cdt.cmake.is.core.Arglets.NameOptionMatcher;
+import org.eclipse.cdt.cmake.is.core.Arglets.NameValueOptionMatcher;
+import org.eclipse.cdt.cmake.is.core.DefaultToolCommandlineParser;
+import org.eclipse.cdt.cmake.is.core.IArglet;
+import org.eclipse.cdt.cmake.is.core.IToolCommandlineParser;
+import org.eclipse.cdt.cmake.is.core.ResponseFileArglets;
+import org.eclipse.core.runtime.IPath;
+
+/**
+ * An {@link IToolCommandlineParser} for the microsoft C and C++ compiler (cl).
+ *
+ * @author Martin Weber
+ */
+class MsclToolCommandlineParser extends DefaultToolCommandlineParser {
+
+	private static final IArglet[] arglets = { new IncludePath_C_CL(), new MacroDefine_C_CL(),
+			new MacroUndefine_C_CL() };
+
+	public MsclToolCommandlineParser() {
+		super(null, new ResponseFileArglets.At(), null, arglets);
+	}
+
+	/**
+	 * Overridden to get the language ID from the file name extension.
+	 */
+	@Override
+	public String getLanguageId(String sourceFileExtension) {
+		return super.determineLanguageId(sourceFileExtension).orElse(null);
+	}
+
+	////////////////////////////////////////////////////////////////////
+	/** matches a macro name, with optional macro parameter list */
+	private static final String REGEX_MACRO_NAME = "([\\w$]+)(?:\\([\\w$, ]*?\\))?";
+	/**
+	 * matches a macro name, skipping leading whitespace. Name in matcher group 1
+	 */
+	private static final String REGEX_MACRO_NAME_SKIP_LEADING_WS = "\\s*" + REGEX_MACRO_NAME;
+	/** matches an include path with quoted directory. Name in matcher group 2 */
+	private static final String REGEX_INCLUDEPATH_QUOTED_DIR = "\\s*([\"'])(.+?)\\1";
+	/**
+	 * matches an include path with unquoted directory. Name in matcher group 1
+	 */
+	private static final String REGEX_INCLUDEPATH_UNQUOTED_DIR = "\\s*([^\\s]+)";
+
+	/**
+	 * A tool argument parser capable to parse a cl (Microsoft c compiler)
+	 * compatible C-compiler include path argument: {@code /Ipath}.
+	 */
+	public static class IncludePath_C_CL extends IncludePathGeneric implements IArglet {
+		private static final NameOptionMatcher[] optionMatchers = {
+				/* quoted directory */
+				new NameOptionMatcher("[-/]I" + REGEX_INCLUDEPATH_QUOTED_DIR, 2),
+				/* unquoted directory */
+				new NameOptionMatcher("[-/]I" + REGEX_INCLUDEPATH_UNQUOTED_DIR, 1), };
+
+		/*-
+		 * @see org.eclipse.cdt.cmake.is.IArglet#processArgs(java.lang.String)
+		 */
+		@Override
+		public int processArgument(IParseContext parseContext, IPath cwd, String argsLine) {
+			return processArgument(parseContext, cwd, argsLine, optionMatchers);
+		}
+	}
+
+	/**
+	 * A tool argument parser capable to parse a cl (Microsoft c compiler)
+	 * compatible C-compiler macro definition argument: {@code /DNAME=value}.
+	 */
+	public static class MacroDefine_C_CL extends MacroDefineGeneric implements IArglet {
+
+		private static final NameValueOptionMatcher[] optionMatchers = {
+				/* quoted value, whitespace in value, w/ macro arglist */
+				new NameValueOptionMatcher("[-/]D" + REGEX_MACRO_NAME_SKIP_LEADING_WS + "((?:=)([\"'])(.+?)\\4)", 1, 5),
+				/* w/ macro arglist */
+				new NameValueOptionMatcher("[-/]D" + REGEX_MACRO_NAME_SKIP_LEADING_WS + "((?:=)(\\S+))?", 1, 3),
+				/* quoted name, whitespace in value, w/ macro arglist */
+				new NameValueOptionMatcher("[-/]D" + REGEX_MACRO_NAME_SKIP_LEADING_WS + "((?:=)(.+?))?\\1", 2, 5),
+				/* w/ macro arglist, shell escapes \' and \" in value */
+				new NameValueOptionMatcher("[-/]D" + REGEX_MACRO_NAME_SKIP_LEADING_WS + "(?:=)((\\\\([\"']))(.*?)\\2)",
+						1, 2), };
+
+		/*-
+		 * @see org.eclipse.cdt.cmake.is.IArglet#processArgs(java.lang.String)
+		 */
+		@Override
+		public int processArgument(IParseContext parseContext, IPath cwd, String argsLine) {
+			return processArgument(parseContext, argsLine, optionMatchers);
+		}
+	}
+
+	/**
+	 * A tool argument parser capable to parse a cl (Microsoft c compiler)
+	 * compatible C-compiler macro cancel argument: {@code /UNAME}.
+	 */
+	public static class MacroUndefine_C_CL extends MacroUndefineGeneric implements IArglet {
+
+		private static final NameOptionMatcher optionMatcher = new NameOptionMatcher(
+				"[-/]U" + REGEX_MACRO_NAME_SKIP_LEADING_WS, 1);
+
+		/*-
+		 * @see org.eclipse.cdt.cmake.is.IArglet#processArgument(java.util.List, java.lang.String)
+		 */
+		@Override
+		public int processArgument(IParseContext parseContext, IPath cwd, String argsLine) {
+			return processArgument(parseContext, argsLine, optionMatcher);
+		}
+	}
+}

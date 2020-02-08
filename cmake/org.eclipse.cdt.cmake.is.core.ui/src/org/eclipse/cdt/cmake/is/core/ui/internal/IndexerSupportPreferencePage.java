2359724b52867d80c43254593c5d86ac96bec249
Martin Weber <fifteenknots505@gmail.com>
2020-02-08 15:01:33 +0100

    Bug 559674: initial import from cmake4eclipse
    Change-Id: I3b9b5b1079ca8692ed0989baab281a1b79987267
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

---
+++ cmake/org.eclipse.cdt.cmake.is.core.ui/src/org/eclipse/cdt/cmake/is/core/ui/internal/IndexerSupportPreferencePage.java
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Martin Weber.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.cmake.is.core.ui.internal;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.eclipse.cdt.cmake.is.core.ui.CMakeISPlugin;
+import org.eclipse.cdt.cmake.is.core.ui.PreferenceConstants;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * Preference page for indexer support.
+ */
+public class IndexerSupportPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	private Text pattern;
+	private Button btnVersionsEnabled;
+	private Button btnWithConsole;
+
+	public IndexerSupportPreferencePage() {
+		super();
+		setPreferenceStore(CMakeISPlugin.getDefault().getPreferenceStore());
+		setDescription("Configure how macros and include paths get extracted from the compile_commands.json file");
+	}
+
+	/**
+	 * Creates the field editors. Field editors are abstractions of the common GUI
+	 * blocks needed to manipulate various types of preferences. Each field editor
+	 * knows how to save and restore itself.
+	 */
+	@Override
+	protected Control createContents(Composite parent) {
+		final Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+		GridDataFactory.swtDefaults().applyTo(composite);
+
+		final Group gr = createGroup(composite, SWT.FILL, 1, "For compilers with version in name", 2);
+		btnVersionsEnabled = createCheckbox(gr, SWT.BEGINNING, 2, "&Also try with version suffix");
+		btnVersionsEnabled.setToolTipText("Can recognize gcc-12.9.2, clang++-7.5.4, ...");
+		IPreferenceStore preferenceStore = getPreferenceStore();
+		btnVersionsEnabled.setSelection(preferenceStore.getBoolean(PreferenceConstants.P_PATTERN_ENABLED));
+		{
+			Label label = new Label(gr, SWT.NONE);
+			label.setText("&Suffix pattern:");
+			GridDataFactory.defaultsFor(label).applyTo(label);
+		}
+
+		pattern = new Text(gr, SWT.SINGLE | SWT.BORDER);
+		GridDataFactory.defaultsFor(pattern).applyTo(pattern);
+		pattern.setToolTipText("Specify a Java regular expression pattern here");
+		pattern.setEnabled(btnVersionsEnabled.getSelection());
+		pattern.setText(preferenceStore.getString(PreferenceConstants.P_PATTERN));
+		pattern.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				final String text = ((Text) e.widget).getText();
+				//				provider.setVersionPattern(text);
+			}
+		});
+		pattern.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (btnVersionsEnabled.getSelection()) {
+					final String text = pattern.getText();
+					try {
+						Pattern.compile(text);
+						setErrorMessage(null);
+					} catch (PatternSyntaxException ex) {
+						String msg = String.format("Suffix pattern regular expression: %1$s in '%2$s' at index %3$d",
+								ex.getDescription(), ex.getPattern(), ex.getIndex());
+						setErrorMessage(msg);
+					}
+				}
+			}
+		});
+
+		// to adjust sensitivity...
+		btnVersionsEnabled.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				boolean selected = ((Button) event.widget).getSelection();
+				pattern.setEnabled(selected);
+				if (!selected) {
+					setErrorMessage(null);
+				}
+			}
+		});
+
+		btnWithConsole = createCheckbox(composite, SWT.BEGINNING, 1,
+				"&Show output of compiler built-in detection in a console in the Console View");
+		btnWithConsole.setSelection(preferenceStore.getBoolean(PreferenceConstants.P_WITH_CONSOLE));
+
+		return composite;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	@Override
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected void performDefaults() {
+		IPreferenceStore preferenceStore = getPreferenceStore();
+		btnVersionsEnabled.setSelection(preferenceStore.getDefaultBoolean(PreferenceConstants.P_PATTERN_ENABLED));
+		pattern.setText(preferenceStore.getDefaultString(PreferenceConstants.P_PATTERN));
+		btnWithConsole.setSelection(preferenceStore.getDefaultBoolean(PreferenceConstants.P_WITH_CONSOLE));
+		setErrorMessage(null);
+		super.performDefaults();
+	}
+
+	@Override
+	public boolean performOk() {
+		IPreferenceStore preferenceStore = getPreferenceStore();
+		preferenceStore.setValue(PreferenceConstants.P_PATTERN_ENABLED, btnVersionsEnabled.getSelection());
+		preferenceStore.setValue(PreferenceConstants.P_PATTERN, pattern.getText());
+		preferenceStore.setValue(PreferenceConstants.P_WITH_CONSOLE, btnWithConsole.getSelection());
+		return true;
+	}
+
+	/**
+	 * Creates a check-box button.
+	 *
+	 * @param parent
+	 * @param horizontalAlignment how control will be positioned horizontally within
+	 *                            a cell of the parent's grid layout, one of:
+	 *                            SWT.BEGINNING (or SWT.LEFT), SWT.CENTER, SWT.END
+	 *                            (or SWT.RIGHT), or SWT.FILL
+	 * @param horizontalSpan      number of column cells in the parent's grid layout
+	 *                            that the control will take up.
+	 * @param text                text to display on the check-box
+	 */
+	private static Button createCheckbox(Composite parent, int horizontalAlignment, int horizontalSpan, String text) {
+		Button b = new Button(parent, SWT.CHECK);
+		b.setText(text);
+		GridDataFactory.defaultsFor(b).align(horizontalAlignment, SWT.CENTER).span(horizontalSpan, 1).grab(true, false)
+				.applyTo(b);
+		return b;
+	}
+
+	/**
+	 * Creates a group with a grid layout.
+	 *
+	 * @param parent
+	 * @param horizontalAlignment how control will be positioned horizontally within
+	 *                            a cell of the parent's grid layout, one of:
+	 *                            SWT.BEGINNING (or SWT.LEFT), SWT.CENTER, SWT.END
+	 *                            (or SWT.RIGHT), or SWT.FILL
+	 * @param horizontalSpan      number of column cells in the parent's grid layout
+	 *                            that the control will take up.
+	 * @param text                title text to display on the group
+	 * @param numColumns          the number of columns in the grid inside the group
+	 */
+	private static Group createGroup(Composite parent, int horizontalAlignment, int horizontalSpan, String text,
+			int numColumns) {
+		Group gr = new Group(parent, SWT.NONE);
+		gr.setLayout(new GridLayout(numColumns, false));
+		gr.setText(text);
+		GridDataFactory.defaultsFor(gr).align(horizontalAlignment, SWT.CENTER).span(horizontalSpan, 1).grab(true, false)
+				.applyTo(gr);
+		return gr;
+	}
+}
---
+++ cmake/org.eclipse.cdt.cmake.is.nvidia/src/main/java/org/eclipse/cdt/cmake/is/nvidia/NvccToolDetectionParticipant.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2019-2020 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.cdt.cmake.is.nvidia;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.cdt.cmake.is.core.Arglets;
+import org.eclipse.cdt.cmake.is.core.DefaultToolCommandlineParser;
+import org.eclipse.cdt.cmake.is.core.DefaultToolDetectionParticipant;
+import org.eclipse.cdt.cmake.is.core.IArglet;
+import org.eclipse.cdt.cmake.is.core.ResponseFileArglets;
+
+/**
+ * CUDA: nvcc compilers (POSIX compatible).
+ *
+ * @author Martin Weber
+ */
+public class NvccToolDetectionParticipant extends DefaultToolDetectionParticipant {
+	static final String COM_NVIDIA_CUDA_LANGUAGE_ID = "com.nvidia.cuda.toolchain.language.cuda.cu";
+
+	public NvccToolDetectionParticipant() {
+		super("nvcc", true, "exe", new ToolCommandlineParser());
+	}
+
+	private static class ToolCommandlineParser extends DefaultToolCommandlineParser {
+
+		private static final IArglet[] arglets = { new Arglets.IncludePath_C_POSIX(), new Arglets.MacroDefine_C_POSIX(),
+				new Arglets.MacroUndefine_C_POSIX(), new NvccSystemIncludePathArglet(),
+				new Arglets.SystemIncludePath_C(), new NvccLangStdArglet() };
+
+		private ToolCommandlineParser() {
+			super(COM_NVIDIA_CUDA_LANGUAGE_ID, new ResponseFileArglets.At(), new NvccBuiltinDetectionBehavior(),
+					arglets);
+		}
+
+		@Override
+		public Set<String> getCustomLanguageIds() {
+			return Collections.singleton(COM_NVIDIA_CUDA_LANGUAGE_ID);
+		}
+	}
+}

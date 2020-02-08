243db99b3f07caf74a4ba9fefd81a4a3904f469d
Martin Weber <fifteenknots505@gmail.com>
2020-02-08 15:01:33 +0100

    Bug 559674: Reformatted and cleaned up
    Using the files in o.e.c/releng/scripts
    
    Change-Id: I3b9b5b1079ca8692ed0989baab281a1b79987267
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

---
+++ cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/internal/builtins/MaybeGccBuiltinDetectionBehavior.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.cdt.cmake.is.core.internal.builtins;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.cdt.cmake.is.core.builtins.GccOutputProcessor;
+import org.eclipse.cdt.cmake.is.core.builtins.IBuiltinsDetectionBehavior;
+import org.eclipse.cdt.cmake.is.core.builtins.IBuiltinsOutputProcessor;
+
+/**
+ * The {link IBuiltinsDetectionBehavior} for the GNU C and GNU C++ compiler
+ * (includes clang). This implementation assumes that the 'cc' command (which is
+ * the same as any POSIX compliant compiler) actually is a GNU compiler.
+ *
+ * @author Martin Weber
+ */
+public class MaybeGccBuiltinDetectionBehavior implements IBuiltinsDetectionBehavior {
+	// -E -P -dM for macros, -Wp,-v for include paths
+	private final List<String> enablingArgs = Arrays.asList("-E", "-P", "-dM", "-Wp,-v");
+
+	@Override
+	public List<String> getBuiltinsOutputEnablingArgs() {
+		return enablingArgs;
+	}
+
+	@Override
+	public IBuiltinsOutputProcessor createCompilerOutputProcessor() {
+		return new GccOutputProcessor();
+	}
+
+	@Override
+	public boolean suppressErrormessage() {
+		// Assume 'cc' is a GNU compiler: do not report an error, if the compiler
+		// actually is
+		// a POSIX compiler that does not understand the arguments that enable built-in
+		// detection
+		return true;
+	}
+
+	@Override
+	public String getInputFileExtension(String languageId) {
+		if (languageId.equals("org.eclipse.cdt.core.gcc")) {
+			return "c";
+		}
+		if (languageId.equals("org.eclipse.cdt.core.g++")) {
+			return "cpp";
+		}
+		return null;
+	}
+}

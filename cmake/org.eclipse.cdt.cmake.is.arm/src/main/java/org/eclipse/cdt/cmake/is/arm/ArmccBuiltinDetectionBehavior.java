27702e418745dcc94f674cb50cf37040a0e1e44b
Martin Weber <fifteenknots505@gmail.com>
2020-02-08 15:01:33 +0100

    Bug 559674: initial import from cmake4eclipse
    Change-Id: I3b9b5b1079ca8692ed0989baab281a1b79987267
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

---
+++ cmake/org.eclipse.cdt.cmake.is.arm/src/main/java/org/eclipse/cdt/cmake/is/arm/ArmccBuiltinDetectionBehavior.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.cdt.cmake.is.arm;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.cdt.cmake.is.core.builtins.GccOutputProcessor;
+import org.eclipse.cdt.cmake.is.core.builtins.IBuiltinsDetectionBehavior;
+import org.eclipse.cdt.cmake.is.core.builtins.IBuiltinsOutputProcessor;
+
+/**
+ * The {link IBuiltinsDetectionBehavior} for the ARM C or C++ version 5
+ * compiler.
+ *
+ * @author Martin Weber
+ */
+class ArmccBuiltinDetectionBehavior implements IBuiltinsDetectionBehavior {
+	// --list_macros for macros
+	private final List<String> enablingArgs = Arrays.asList("--list_macros");
+
+	@Override
+	public List<String> getBuiltinsOutputEnablingArgs() {
+		return enablingArgs;
+	}
+
+	@Override
+	public IBuiltinsOutputProcessor createCompilerOutputProcessor() {
+		return new GccOutputProcessor();
+	}
+
+	@Override
+	public boolean suppressErrormessage() {
+		// report an error, if the compiler does not understand the arguments that
+		// enable built-in detection
+		return false;
+	}
+
+	@Override
+	public String getInputFileExtension(String languageId) {
+		if (languageId.equals("org.eclipse.cdt.core.gcc")) {
+			return "c";
+		}
+		if (languageId.equals("org.eclipse.cdt.core.g++")) {
+			return "cpp";
+		}
+		return null;
+	}
+}
---
+++ cmake/org.eclipse.cdt.cmake.is.core.tests/src/main/java/org/eclipse/cdt/cmake/is/core/internal/ParserLookupResultTest.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2016-2017 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.cmake.is.core.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.cdt.cmake.is.core.language.settings.providers.CompileCommandsJsonParser;
+import org.junit.Test;
+
+/**
+ * @author weber
+ *
+ */
+public class ParserLookupResultTest {
+
+	CompileCommandsJsonParser testee = new CompileCommandsJsonParser();
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.cdt.cmake.is.core.internal.ParserDetection#determineDetector(String, String,boolean)}
+	 */
+	@Test
+	public void testCanParse() {
+		String compiler = "/bin/c++";
+		String args = "-DQT_CORE_LIB -I/home/self/shared/qt5-project/build/Debug"
+				+ " -isystem /home/self/Qt5.9.1/5.9.1/gcc_64/include/QtWidgets"
+				+ " -g -fPIC -std=gnu++11 -o CMakeFiles/foo.dir/foo_automoc.cpp.o"
+				+ " -c /home/self/shared/qt5-project/build/Debug/foo_automoc.cpp";
+		String cmd = compiler + " " + args;
+		ParserDetection.ParserDetectionResult result = ParserDetection.determineDetector(cmd, null, false);
+		assertNotNull(result);
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.cdt.cmake.is.core.internal.ParserDetection.ParserDetectionResult#getReducedCommandLine()}.
+	 */
+	@Test
+	public void testGetReducedCommandLine() {
+		String compiler = "/bin/c++";
+		String args = "-DQT_CORE_LIB -I/home/self/shared/qt5-project/build/Debug"
+				+ " -isystem /home/self/Qt5.9.1/5.9.1/gcc_64/include/QtWidgets"
+				+ " -g -fPIC -std=gnu++11 -o CMakeFiles/foo.dir/foo_automoc.cpp.o"
+				+ " -c /home/self/shared/qt5-project/build/Debug/foo_automoc.cpp";
+		String cmd = compiler + " " + args;
+		ParserDetection.ParserDetectionResult result = ParserDetection.determineDetector(cmd, null, false);
+		assertNotNull(result);
+		// verify that we got a C++ parser
+		assertEquals("C++", "org.eclipse.cdt.core.g++",
+				result.getDetectorWithMethod().getToolDetectionParticipant().getParser().getLanguageId("cpp"));
+		assertEquals("reducedCommandLine", args, result.getReducedCommandLine());
+
+		// test without leading path
+		compiler = "c++";
+		cmd = compiler + " " + args;
+		result = ParserDetection.determineDetector(cmd, null, false);
+		assertNotNull(result);
+		// verify that we got a C++ parser
+		assertEquals("C++", "org.eclipse.cdt.core.g++",
+				result.getDetectorWithMethod().getToolDetectionParticipant().getParser().getLanguageId("cpp"));
+		assertEquals("reducedCommandLine", args, result.getReducedCommandLine());
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.eclipse.cdt.cmake.is.core.internal.ParserDetection.ParserDetectionResult#getCommandLine()}.
+	 */
+	@Test
+	public void testGetCommandLine() {
+		String compiler = "/bin/c++";
+		String args = "-DQT_CORE_LIB -I/home/self/shared/qt5-project/build/Debug"
+				+ " -isystem /home/self/Qt5.9.1/5.9.1/gcc_64/include/QtWidgets"
+				+ " -g -fPIC -std=gnu++11 -o CMakeFiles/foo.dir/foo_automoc.cpp.o"
+				+ " -c /home/self/shared/qt5-project/build/Debug/foo_automoc.cpp";
+		String cmd = compiler + " " + args;
+		ParserDetection.ParserDetectionResult result = ParserDetection.determineDetector(cmd, null, false);
+		assertNotNull(result);
+		// verify that we got a C++ parser
+		assertEquals("C++", "org.eclipse.cdt.core.g++",
+				result.getDetectorWithMethod().getToolDetectionParticipant().getParser().getLanguageId("cpp"));
+		assertEquals("command", compiler, result.getCommandLine().getCommand());
+		assertEquals("args", args, result.getCommandLine().getArguments());
+
+		// test without leading path
+		compiler = "c++";
+		cmd = compiler + " " + args;
+		result = ParserDetection.determineDetector(cmd, null, false);
+		assertNotNull(result);
+		// verify that we got a C++ parser
+		assertEquals("C++", "org.eclipse.cdt.core.g++",
+				result.getDetectorWithMethod().getToolDetectionParticipant().getParser().getLanguageId("cpp"));
+		assertEquals("command", compiler, result.getCommandLine().getCommand());
+		assertEquals("args", args, result.getCommandLine().getArguments());
+	}
+}

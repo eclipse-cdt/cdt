2b62a5f9793c13de55f16637329bea1d322451c3
Martin Weber <fifteenknots505@gmail.com>
2020-03-26 21:19:37 +0100

    Bug 559674: Migrating from a ILanguageSettingsProvider implementation to IIndexerInfoConsumer (1)
    Change-Id: I0903f75c7cb4fbd4f3119ae22500b66b2f15794d
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/IArglet.java
+++ cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/IArglet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2019 Martin Weber.
+ * Copyright (c) 2015-2020 Martin Weber.
  *
  * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
  * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
@@ -8,7 +8,6 @@
  *******************************************************************************/
 package org.eclipse.cdt.cmake.is.core;
 
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
 import org.eclipse.core.runtime.IPath;
 
 /**
@@ -23,37 +22,30 @@ public interface IArglet {
 	 * Parses the next command-line argument and extracts all detected
 	 * LanguageSettings objects.
 	 *
-	 * @param parseContext the buffer that receives the new {@code LanguageSetting}
-	 *                     entries
-	 * @param cwd          the current working directory of the compiler at its
-	 *                     invocation
-	 * @param argsLine     the arguments passed to the tool, as they appear in the
-	 *                     build output. Implementers may safely assume that the
-	 *                     specified value does not contain leading whitespace
-	 *                     characters, but trailing WS may occur.
+	 * @param resultCollector the buffer that receives the parsed command-line
+	 *                        arguments
+	 * @param cwd             the current working directory of the compiler at its
+	 *                        invocation
+	 * @param argsLine        the arguments passed to the tool, as they appear in
+	 *                        the build output. Implementers may safely assume that
+	 *                        the specified value does not contain leading
+	 *                        whitespace characters, but trailing WS may occur.
 	 * @return the number of characters from {@code argsLine} that has been
 	 *         processed. Return a value of {@code zero} or less, if this tool
 	 *         argument parser cannot process the first argument from the input.
 	 */
-	int processArgument(IParseContext parseContext, IPath cwd, String argsLine);
+	int processArgument(IArgumentCollector resultCollector, IPath cwd, String argsLine);
 
 	/**
 	 * Gathers the results of argument parsing.
 	 *
 	 * @author Martin Weber
 	 */
-	interface IParseContext {
+	interface IArgumentCollector extends IRawIndexerInfoCollector {
 		/**
-		 * Adds a language setting to the result.
-		 *
-		 * @param entry
-		 */
-		void addSettingEntry(ICLanguageSettingEntry entry);
-
-		/**
-		 * Adds a compiler argument that affects built-in detection to the result. For
-		 * the GNU compilers, these are options like {@code --sysroot} and options that
-		 * specify the language's standard ({@code -std=c++17}.
+		 * Adds a compiler argument that affects built-in detection. For the GNU
+		 * compilers, these are options like {@code --sysroot} and options that specify
+		 * the language's standard ({@code -std=c++17}.
 		 */
 		void addBuiltinDetectionArgument(String argument);
 	}

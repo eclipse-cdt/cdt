e97f5d8af49273c1ade34e645892767cc9ce6a97
Martin Weber <fifteenknots505@gmail.com>
2020-03-26 21:19:37 +0100

    Bug 559674: Migrating from a ILanguageSettingsProvider implementation to IIndexerInfoConsumer (1)
    This is piece one of change https://git.eclipse.org/r/c/159828 being split into pieces.
    Splitting is done for code reviewing reasons.
    
    Change-Id: I0903f75c7cb4fbd4f3119ae22500b66b2f15794d
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/IToolCommandlineParser.java
+++ cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/IToolCommandlineParser.java
@@ -10,10 +10,8 @@ package org.eclipse.cdt.cmake.is.core;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.eclipse.cdt.cmake.is.core.builtins.IBuiltinsDetectionBehavior;
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
 import org.eclipse.core.runtime.IPath;
 
 /**
@@ -35,44 +33,12 @@ public interface IToolCommandlineParser {
 	public IResult processArgs(IPath cwd, String args);
 
 	/**
-	 * Gets the language ID of the language that the tool compiles. If the tool is
-	 * able to compile for multiple programming languages, the specified
-	 * {@code sourceFileExtension} may be used to compute the Language ID.
-	 * <p>
-	 * NOTE: CDT expects "org.eclipse.cdt.core.gcc" for the C language and
-	 * "org.eclipse.cdt.core.g++" for the C++ language, so one of that IDs should be
-	 * returned here. (Some extension to CDT may recognize different language IDs,
-	 * such as "com.nvidia.cuda.toolchain.language.cuda.cu".)
-	 * </p>
-	 *
-	 * @param sourceFileExtension the extension of the source file name
-	 *
-	 * @return a valid language ID, or {@code null}. If {@code null} or an invalid
-	 *         language ID, the results of argument processing will be ignored by
-	 *         CDT.
-	 */
-	public String getLanguageId(String sourceFileExtension);
-
-	/**
-	 * Gets the custom language IDs that the tool compiles. Some CDT based IDEs use
-	 * language IDs of their own, for example
-	 * "com.nvidia.cuda.toolchain.language.cuda.cu" for CUDA. A custom language ID
-	 * is an ID other than CDT's default IDs for the C language
-	 * ("org.eclipse.cdt.core.gcc") and the C++ ("org.eclipse.cdt.core.g++")
-	 * language,
-	 *
-	 * @return the custom language IDs or an empty set if the tool does not compile
-	 *         for languages other than C and C++
-	 */
-	public Set<String> getCustomLanguageIds();
-
-	/**
 	 * Gets the {@code IBuiltinsDetectionBehavior} which specifies how built-in
 	 * compiler macros and include path detection is handled for a specific
 	 * compiler.
 	 *
-	 * @return the {@code IBuiltinsDetectionBehavior} or an empty {@code Oprional} if the
-	 *         compiler does not support built-in detection
+	 * @return the {@code IBuiltinsDetectionBehavior} or an empty {@code Optional}
+	 *         if the compiler does not support built-in detection
 	 */
 	public Optional<IBuiltinsDetectionBehavior> getIBuiltinsDetectionBehavior();
 
@@ -83,14 +49,7 @@ public interface IToolCommandlineParser {
 	 *
 	 * @see IToolCommandlineParser#processArgs(IPath, String)
 	 */
-	interface IResult {
-		/**
-		 * Gets the language setting entries produced during processing.
-		 *
-		 * @return the language setting entries
-		 */
-		List<ICLanguageSettingEntry> getSettingEntries();
-
+	interface IResult extends IRawIndexerInfo {
 		/**
 		 * Gets the compiler arguments from the command-line that affect built-in
 		 * detection. For the GNU compilers, these are options like {@code --sysroot}

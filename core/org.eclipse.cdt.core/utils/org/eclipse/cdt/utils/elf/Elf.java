6c2633a067152808476034d711481942ec73d751
Matthew Khouzam <matthew.khouzam@ericsson.com>
2020-04-02 18:29:17 -0400

    Convert fields into variables in Elf
    The fields are not used outside of the loadSymbols section,
    and they are written before being read, so they should be safely
    inlineable.
    
    Change-Id: I68388227074d80ace283395b48c88c59700dd9da
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/elf/Elf.java
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/elf/Elf.java
@@ -53,10 +53,8 @@ public class Elf {
 	private Symbol[] symbols;
 	/** .symtab section */
 	private Symbol[] symbolsTable;
-	private Section symbolsTableSection;
 	/** .dynSym section */
 	private Symbol[] dynamicSymbols;
-	private Section dynamicSymbolSection;
 	private boolean areSectionsMapped; // Have sections been mapped? Used to clean up properly in Elf.Dispose.
 
 	protected String EMPTY_STRING = ""; //$NON-NLS-1$
@@ -1130,13 +1128,14 @@ public class Elf {
 	}
 
 	public void loadSymbols() throws IOException {
+		Section symbolsTableSection = null;
+		Section dynamicSymbolSection = null;
 		if (symbols == null) {
 			Section section[] = getSections(Section.SHT_SYMTAB);
 			if (section.length > 0) {
 				symbolsTableSection = section[0];
 				symbolsTable = loadSymbolsBySection(section[0]);
 			} else {
-				symbolsTableSection = null;
 				symbolsTable = new Symbol[0];
 			}
 
@@ -1145,7 +1144,6 @@ public class Elf {
 				dynamicSymbolSection = section[0];
 				dynamicSymbols = loadSymbolsBySection(section[0]);
 			} else {
-				dynamicSymbolSection = null;
 				dynamicSymbols = new Symbol[0];
 			}
 

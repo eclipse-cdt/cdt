a4233e9eb64b00d8e15002253fa61265f38f2861
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-04-16 21:28:56 +0300

    Bug 562164 - Add JUnit tests for IMemoryImporter implementations
    The following changes are done to make the implementation headless and
    testable:
    * Reworked FileImport to implement base flow
    * Added ImportRequest to aggregate operation configuration
    * Extracted PlainTextImport extends FileImport
    * Extracted RawBinaryImport extends FileImport
    * Extracted SRecordImport extends FileImport
    * Extracted FileImportJob
    * Added ScrollMemory to interact with UI
    * Added WriteMemoryBlock to isolate from UI class BufferedMemoryWriter
    * Reworked related i18n
    
    Change-Id: Ibd68c0e345e9455f5f8a2e1be673ddac5eb9a9b1
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ memory/org.eclipse.cdt.debug.core.memory/src/org/eclipse/cdt/debug/internal/core/memory/transport/FileImportJob.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alexander Fedorov (ArSysOp) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.debug.internal.core.memory.transport;
+
+import org.eclipse.cdt.debug.core.memory.transport.FileImport;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+/**
+ * Runs {@link FileImport} operation as a {@link Job}
+ *
+ */
+public final class FileImportJob extends Job {
+
+	private final FileImport<?> fileImport;
+
+	public FileImportJob(String name, FileImport<?> fileImport) {
+		super(name);
+		this.fileImport = fileImport;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		try {
+			fileImport.run(monitor);
+		} catch (CoreException e) {
+			return e.getStatus();
+		}
+		return Status.OK_STATUS;
+	}
+
+}

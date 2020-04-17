79ba5ef27be1e0f8b2695badbf1ca2e5e89479cd
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-04-17 11:27:00 +0300

    Bug 562247 - Add CDT Debug Core Memory bundle
    Added "org.eclipse.cdt.debug.core.memory" bundle with a stub abstract
    class to be improved in the future commits
    
    Change-Id: I14136379d8e595b7af36084cff8711d1d50a0a28
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ memory/org.eclipse.cdt.debug.core.memory/src/org/eclipse/cdt/debug/core/memory/transport/MemoryImport.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2020 ArSysOp and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Alexander Fedorov (ArSysOp) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.debug.core.memory.transport;
+
+import org.eclipse.core.runtime.ICoreRunnable;
+
+/**
+ * Imports memory information from a given source
+ *
+ * @since 0.1
+ */
+public abstract class MemoryImport implements ICoreRunnable {
+	/* WIP: here we will place basic logic to read the data and notify consumer interactively
+	private final Consumer<BigInteger> scroll;
+	*/
+}

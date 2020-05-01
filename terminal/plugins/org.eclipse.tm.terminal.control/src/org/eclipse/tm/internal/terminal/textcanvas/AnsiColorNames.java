af8271605dabf96aa856cd66b9c75eb3ce9e6b08
Jonah Graham <jonah@kichwacoders.com>
2020-05-01 16:58:02 -0400

    Bug 549697: Extract standard colour names
    The inconsistency of lower case and upper case in the internal
    representation of the colour names made it harder than it should be
    to find where colours are used in the code.
    
    Note that the SytleMap creates lower and uppercase entries for each
    colour.
    
    Change-Id: I16b4ebe4a97adecd01f835319aa0f0d175ae3bb6

---
+++ terminal/plugins/org.eclipse.tm.terminal.control/src/org/eclipse/tm/internal/terminal/textcanvas/AnsiColorNames.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2020 Wind River Systems, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Jonah Graham - extracted colour names from StyleMap to be reused and cross referenced in VT100Emulator
+ *******************************************************************************/
+package org.eclipse.tm.internal.terminal.textcanvas;
+
+public class AnsiColorNames {
+	public static final String BLACK = "black"; //$NON-NLS-1$
+	public static final String WHITE = "white"; //$NON-NLS-1$
+	public static final String WHITE_FOREGROUND = "white_foreground"; //$NON-NLS-1$
+	public static final String GRAY = "gray"; //$NON-NLS-1$
+	public static final String MAGENTA = "magenta"; //$NON-NLS-1$
+	public static final String CYAN = "cyan"; //$NON-NLS-1$
+	public static final String YELLOW = "yellow"; //$NON-NLS-1$
+	public static final String BLUE = "blue"; //$NON-NLS-1$
+	public static final String GREEN = "green"; //$NON-NLS-1$
+	public static final String RED = "red"; //$NON-NLS-1$
+}

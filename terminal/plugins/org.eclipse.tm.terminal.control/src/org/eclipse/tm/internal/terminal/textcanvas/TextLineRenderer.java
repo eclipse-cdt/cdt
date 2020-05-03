3201a07fc3cfd98364131197dad7948f9b4693ae
Jonah Graham <jonah@kichwacoders.com>
2020-05-03 14:39:28 -0400

    Bug 549697: Mark update font methods in Terminal as deprecated
    There are two ways of specifying fonts in the terminal,
    one via font definitions, and one via the font directly. The
    method via the font directly was already deprecated in the public
    API, this commit follows that deprecation chain through to
    make it easier to identify which code paths are not likely
    relevant anymore.
    
    These methods could simply be removed as part of the major version
    bump.
    
    Change-Id: I05d746c429c4e6c3ced5ee7bb7a6172e0a255c2d

--- terminal/plugins/org.eclipse.tm.terminal.control/src/org/eclipse/tm/internal/terminal/textcanvas/TextLineRenderer.java
+++ terminal/plugins/org.eclipse.tm.terminal.control/src/org/eclipse/tm/internal/terminal/textcanvas/TextLineRenderer.java
@@ -169,6 +169,10 @@ public class TextLineRenderer implements ILinelRenderer {
 		return fModel.getTerminalText();
 	}
 
+	/**
+	 * @deprecated Use {@link #updateFont(String)}
+	 */
+	@Deprecated
 	@Override
 	public void onFontChange() {
 		fStyleMap.updateFont();

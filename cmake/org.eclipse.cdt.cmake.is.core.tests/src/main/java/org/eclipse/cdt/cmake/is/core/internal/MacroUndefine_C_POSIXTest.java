02b7fc3b5b37414057eaf29dc14b609495872773
Martin Weber <fifteenknots505@gmail.com>
2020-05-07 21:29:23 +0200

    Bug 559674: Migrating from a ILanguageSettingsProvider implementation to IIndexerInfoConsumer (4/6)
    Adjust core tests to new implementation.
    
    This is piece four of change https://git.eclipse.org/r/c/159828 being split into pieces.
    Splitting is done for code reviewing reasons.
    
    Change-Id: I925fd5e56cbf6bf3e6d14322c738c0ff412b8b1d
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- cmake/org.eclipse.cdt.cmake.is.core.tests/src/main/java/org/eclipse/cdt/cmake/is/core/internal/MacroUndefine_C_POSIXTest.java
+++ cmake/org.eclipse.cdt.cmake.is.core.tests/src/main/java/org/eclipse/cdt/cmake/is/core/internal/MacroUndefine_C_POSIXTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Martin Weber.
+ * Copyright (c) 2015-2020 Martin Weber.
  *
  * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
  * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
@@ -11,9 +11,7 @@ package org.eclipse.cdt.cmake.is.core.internal;
 import static org.junit.Assert.assertEquals;
 
 import org.eclipse.cdt.cmake.is.core.Arglets.MacroUndefine_C_POSIX;
-import org.eclipse.cdt.cmake.is.core.IArglet.IParseContext;
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
-import org.eclipse.cdt.core.settings.model.ICSettingEntry;
+import org.eclipse.cdt.cmake.is.core.IArglet.IArgumentCollector;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.junit.Before;
@@ -36,35 +34,26 @@ public class MacroUndefine_C_POSIXTest {
 
 	/**
 	 * Test method for
-	 * {@link MacroUndefine_C_POSIX#processArgument(IParseContext, IPath, java.lang.String)}
+	 * {@link MacroUndefine_C_POSIX#processArgument(IArgumentCollector, IPath, java.lang.String)}
 	 * .
 	 */
 	@Test
 	public final void testProcessArgument() {
 		final String more = " -g -MMD -MT CMakeFiles/execut1.dir/util1.c.o -MF \"CMakeFiles/execut1.dir/util1.c.o.d\""
 				+ " -o CMakeFiles/execut1.dir/util1.c.o -c /testprojects/C-subsrc/src/src-sub/main1.c";
-		ParseContext entries;
-		ICLanguageSettingEntry parsed;
+		ParseContext result;
 		final IPath cwd = new Path("");
 
 		String name = "FOO";
 		// -UFOO
-		entries = new ParseContext();
-		assertEquals(2 + name.length(), testee.processArgument(entries, cwd, "-U" + name + more));
-		assertEquals("#entries", 1, entries.getSettingEntries().size());
-		parsed = entries.getSettingEntries().get(0);
-		assertEquals("kind", ICSettingEntry.MACRO, parsed.getKind());
-		assertEquals("kind", ICSettingEntry.UNDEFINED, (parsed.getFlags() & ICSettingEntry.UNDEFINED));
-		assertEquals("name", name, parsed.getName());
-		assertEquals("value", "", parsed.getValue());
+		result = new ParseContext();
+		assertEquals(2 + name.length(), testee.processArgument(result, cwd, "-U" + name + more));
+		assertEquals("#entries", 1, result.getUndefines().size());
+		assertEquals("name", name, result.getUndefines().get(0));
 		// -U FOO
-		entries = new ParseContext();
-		assertEquals(2 + 2 + name.length(), testee.processArgument(entries, cwd, "-U  " + name + more));
-		assertEquals("#entries", 1, entries.getSettingEntries().size());
-		parsed = entries.getSettingEntries().get(0);
-		assertEquals("kind", ICSettingEntry.MACRO, parsed.getKind());
-		assertEquals("kind", ICSettingEntry.UNDEFINED, (parsed.getFlags() & ICSettingEntry.UNDEFINED));
-		assertEquals("name", name, parsed.getName());
-		assertEquals("value", "", parsed.getValue());
+		result = new ParseContext();
+		assertEquals(2 + 2 + name.length(), testee.processArgument(result, cwd, "-U  " + name + more));
+		assertEquals("#entries", 1, result.getUndefines().size());
+		assertEquals("name", name, result.getUndefines().get(0));
 	}
 }

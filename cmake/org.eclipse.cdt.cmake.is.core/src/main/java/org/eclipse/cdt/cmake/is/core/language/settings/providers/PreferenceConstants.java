4a0fcfb4c7791ee6c20c6ee4375d76e998c8ed64
Martin Weber <fifteenknots505@gmail.com>
2020-05-07 22:11:56 +0200

    Bug 559674: Migrating from a ILanguageSettingsProvider implementation to IIndexerInfoConsumer (6/6)
    Move preference page to new UI plugin.
    
    This is piece six of change https://git.eclipse.org/r/c/159828 being split into pieces.
    Splitting is done for code reviewing reasons.
    
    Change-Id: I9db0f3053ef9524a5b79a0fc9b8198ff241acb1a
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/language/settings/providers/PreferenceConstants.java
+++ cmake/org.eclipse.cdt.cmake.is.core/src/main/java/org/eclipse/cdt/cmake/is/core/language/settings/providers/PreferenceConstants.java
@@ -8,7 +8,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
-package org.eclipse.cdt.cmake.is.core.ui;
+package org.eclipse.cdt.cmake.is.core.language.settings.providers;
 
 /**
  * Constant definitions for plug-in preferences
@@ -16,9 +16,9 @@ package org.eclipse.cdt.cmake.is.core.ui;
 public class PreferenceConstants {
 
 	/** preferences key for version suffix regex pattern */
-	public static final String P_PATTERN = "vPattern";
+	public static final String P_PATTERN = "vPattern"; //$NON-NLS-1$
 	/** preferences key for version suffix regex pattern enabled */
-	public static final String P_PATTERN_ENABLED = "vPatternEnabled";
+	public static final String P_PATTERN_ENABLED = "vPatternEnabled"; //$NON-NLS-1$
 	/** preferences key for built-ins detection output showing a console */
-	public static final String P_WITH_CONSOLE = "console";
+	public static final String P_WITH_CONSOLE = "console"; //$NON-NLS-1$
 }

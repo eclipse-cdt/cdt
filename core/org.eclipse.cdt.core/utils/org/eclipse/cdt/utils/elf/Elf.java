01f87f03d8f14f9dbb32356a28a1b00070c2f0fc
Aijun Shi <Aijun.Shi@windriver.com>
2020-06-16 12:50:21 +0800

    Bug 564232 - Add cpu risc for RISC-V need to support.
    Change-Id: Ib62ed14f3c0dbe4375da41dba7209b27e034c53e
    Signed-off-by: Aijun Shi <Aijun.Shi@windriver.com>

--- core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/elf/Elf.java
+++ core/org.eclipse.cdt.core/utils/org/eclipse/cdt/utils/elf/Elf.java
@@ -158,6 +158,8 @@ public class Elf {
 		/** @since 6.0 */
 		public static final int EM_AARCH64 = 183;
 
+		public static final int EM_RISCV = 243;
+
 		public static final int EM_NIOS = 0xFEBB;
 		public static final int EM_CYGNUS_POWERPC = 0x9025;
 		public static final int EM_CYGNUS_M32R = 0x9041;
@@ -907,6 +909,9 @@ public class Elf {
 			break;
 		case Elf.ELFhdr.EM_RS08:
 			attrib.cpu = "rs08"; //$NON-NLS-1$
+			break;
+		case Elf.ELFhdr.EM_RISCV:
+			attrib.cpu = "riscv"; //$NON-NLS-1$
 			break;
 		case Elf.ELFhdr.EM_NONE:
 		default:

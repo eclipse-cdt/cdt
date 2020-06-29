9043b4b51140672de4dc899e1d5848d91b6149ae
Torbjörn Svensson <azoff@svenskalinuxforeningen.se>
2020-06-29 18:59:20 +0200

    Corrected plugin id to org.eclipse.cdt.msw.build
    Signed-off-by: Torbjörn Svensson <azoff@svenskalinuxforeningen.se>
    Change-Id: I85f94d31163caa07a522c17fc73bbd7504230b0a

--- windows/org.eclipse.cdt.msw.build/src/org/eclipse/cdt/msw/build/Activator.java
+++ windows/org.eclipse.cdt.msw.build/src/org/eclipse/cdt/msw/build/Activator.java
@@ -19,7 +19,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "net.sf.wascana.msvc.build";
+	public static final String PLUGIN_ID = "org.eclipse.cdt.msw.build"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;

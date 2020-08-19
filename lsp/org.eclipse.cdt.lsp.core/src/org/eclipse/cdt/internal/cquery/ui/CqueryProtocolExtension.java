6c358a8cdff10c7f03f3a1f41b7e903fd99ab457
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-08-19 22:32:03 +0300

    Bug 558664 - Cquery integration should be extracted from LSP Core
    Part 7:
    Extract CQuery protocol extension to its package and declare it as a
    LanguageProtocolExtension component.
    Remove custom "clientImpl" from LSP extension point, as we can use
    standard one.
    Declare DelegatingLauncherBuilder to collect and register language
    extensions for the preferred language server.
    
    Change-Id: I88be0456bbb53d24a6a0df2578c145649268b028
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- lsp/org.eclipse.cdt.lsp.core/src/org/eclipse/cdt/internal/cquery/ui/CqueryProtocolExtension.java
+++ lsp/org.eclipse.cdt.lsp.core/src/org/eclipse/cdt/internal/cquery/ui/CqueryProtocolExtension.java
@@ -8,31 +8,35 @@
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 
-package org.eclipse.cdt.lsp.core;
+package org.eclipse.cdt.internal.cquery.ui;
 
 import org.eclipse.cdt.cquery.CqueryInactiveRegions;
 import org.eclipse.cdt.cquery.CquerySemanticHighlights;
 import org.eclipse.cdt.cquery.IndexingProgressStats;
 import org.eclipse.cdt.internal.cquery.CqueryMessages;
-import org.eclipse.cdt.internal.cquery.ui.PublishSemanticHighlighting;
+import org.eclipse.cdt.lsp.LanguageProtocolExtension;
 import org.eclipse.cdt.lsp.internal.core.ShowStatus;
 import org.eclipse.cdt.lsp.internal.text.SetInactiveRegions;
 import org.eclipse.cdt.lsp.internal.ui.StatusLineMessage;
-import org.eclipse.lsp4e.LanguageClientImpl;
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
+import org.osgi.service.component.annotations.Component;
 
-//FIXME: AF: currently this extension is cquery-specific and it should be contributed from cquery-specific part
-@SuppressWarnings("restriction")
-public class Server2ClientProtocolExtension extends LanguageClientImpl {
+@Component
+public class CqueryProtocolExtension implements LanguageProtocolExtension {
 
 	private final ShowStatus progress;
 	private final SetInactiveRegions inactive;
 	private final PublishSemanticHighlighting highlighting;
 
-	public Server2ClientProtocolExtension() {
+	public CqueryProtocolExtension() {
 		this.progress = new ShowStatus(() -> CqueryMessages.CqueryLanguageServer_label, new StatusLineMessage());
 		this.inactive = new SetInactiveRegions();
 		this.highlighting = new PublishSemanticHighlighting();
+	}
+
+	@Override
+	public String targetIdentifier() {
+		return "cquery"; //$NON-NLS-1$
 	}
 
 	@JsonNotification("$cquery/progress")

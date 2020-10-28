b735bd8aa3d001cd54547ce6ab40f052b183caea
Martin Weber <fifteenknots505@gmail.com>
2020-10-28 21:36:37 +0100

    Bug 567488: Use command-line options to pass to cmake from ICMakeProperties object
    Change-Id: I4d6383ce240e6f2b8d7079f281ef7c2e56ea93a6
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

---
+++ cmake/org.eclipse.cdt.cmake.core/src/org/eclipse/cdt/cmake/core/internal/CMakePropertiesController.java
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Martin Weber.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.cdt.cmake.core.internal;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+
+import org.eclipse.cdt.cmake.core.internal.properties.CMakePropertiesBean;
+import org.eclipse.cdt.cmake.core.properties.CMakeGenerator;
+import org.eclipse.cdt.cmake.core.properties.ICMakeProperties;
+import org.eclipse.cdt.cmake.core.properties.ICMakePropertiesController;
+
+/**
+ * A {@code ICMakePropertiesController} that monitors modifications to the project properties that force
+ * us to delete file CMakeCache.txt to avoid complaints by cmake.
+ * @author Martin Weber
+ */
+class CMakePropertiesController implements ICMakePropertiesController {
+
+	private final Runnable cmakeCacheDirtyMarker;
+
+	private String cacheFile;
+	private List<String> extraArguments;
+	private CMakeGenerator generatorLinux;
+	private List<String> extraArgumentsLinux;
+	private CMakeGenerator generatorWindows;
+	private List<String> extraArgumentsWindows;
+	private String buildType;
+
+	/** Creates a new CMakePropertiesController object.
+	 *
+	 * @param cmakeCacheDirtyMarker
+	 * 		the object to notify when modifications to the project properties force
+	 * us to delete file CMakeCache.txt to avoid complaints by cmake
+	 */
+	CMakePropertiesController(Runnable cmakeCacheDirtyMarker) {
+		this.cmakeCacheDirtyMarker = Objects.requireNonNull(cmakeCacheDirtyMarker);
+	}
+
+	@Override
+	public ICMakeProperties load() {
+		// TODO implement load()
+		CMakePropertiesBean props = new CMakePropertiesBean();
+
+		setupModifyDetection(props);
+		return props;
+	}
+
+	@Override
+	public void save(ICMakeProperties properties) {
+		// detect whether changes force us to delete file CMakeCache.txt to avoid complaints by cmake
+		if (isPropertyModified(properties) || isExtraArgumentsPropertyModified(properties)) {
+			cmakeCacheDirtyMarker.run(); // mark cmake cache file for removal
+		}
+		// TODO implement save()
+		setupModifyDetection(properties);
+	}
+
+	/** Gets whether changes in one of the basic properties force us to delete file CMakeCache.txt
+	 * to avoid complaints by cmake.
+	 */
+	private boolean isPropertyModified(ICMakeProperties properties) {
+		return !Objects.equals(buildType, properties.getBuildType())
+				|| !Objects.equals(cacheFile, properties.getCacheFile())
+				|| !Objects.equals(generatorLinux, properties.getLinuxOverrides().getGenerator())
+				|| !Objects.equals(generatorWindows, properties.getWindowsOverrides().getGenerator());
+	}
+
+	/** Gets whether changes in one of the extra arguments properties force us to delete file CMakeCache.txt
+	 * to avoid complaints by cmake.
+	 */
+	private boolean isExtraArgumentsPropertyModified(ICMakeProperties properties) {
+		return extraArgumentsChange(extraArguments, properties.getExtraArguments())
+				|| extraArgumentsChange(extraArgumentsLinux, properties.getLinuxOverrides().getExtraArguments())
+				|| extraArgumentsChange(extraArgumentsWindows, properties.getWindowsOverrides().getExtraArguments());
+	}
+
+	/** Sets up detection of modifications that force us to delete file CMakeCache.txt to avoid complaints by cmake
+	 */
+	private void setupModifyDetection(ICMakeProperties properties) {
+		buildType = properties.getBuildType();
+		cacheFile = properties.getCacheFile();
+		extraArguments = properties.getExtraArguments();
+		generatorLinux = properties.getLinuxOverrides().getGenerator();
+		extraArgumentsLinux = properties.getLinuxOverrides().getExtraArguments();
+		generatorWindows = properties.getWindowsOverrides().getGenerator();
+		extraArgumentsWindows = properties.getWindowsOverrides().getExtraArguments();
+	}
+
+	private boolean extraArgumentsChange(List<String> args1, List<String> args2) {
+		String wanted = "CMAKE_TOOLCHAIN_FILE"; //$NON-NLS-1$
+		// extract the last arguments that contain String wanted..
+		Predicate<? super String> predContains = a -> a.contains(wanted);
+		BinaryOperator<String> keepLast = (first, second) -> second;
+		String a1 = args1.stream().filter(predContains).reduce(keepLast).orElse(null);
+		String a2 = args2.stream().filter(predContains).reduce(keepLast).orElse(null);
+		if (!Objects.equals(a1, a2)) {
+			return true;
+		}
+		return false;
+	}
+}

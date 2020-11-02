b0d25c22ebd6970dc75ac3a47adef810222e7a50
John Dallaway <john@dallaway.org.uk>
2020-11-02 17:53:49 +0000

    Bug 568454: Rework GDB Hardware "Main" tab to extend CMainTab2
    Change-Id: I0b7be555a43f66cead397e93b58689da1c94dcd4

--- jtag/org.eclipse.cdt.debug.gdbjtag.ui/src/org/eclipse/cdt/debug/gdbjtag/internal/ui/GDBJtagDSFCMainTab.java
+++ jtag/org.eclipse.cdt.debug.gdbjtag.ui/src/org/eclipse/cdt/debug/gdbjtag/internal/ui/GDBJtagDSFCMainTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 - 2015 QNX Software Systems and others.
+ * Copyright (c) 2007 - 2020 QNX Software Systems and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,18 +10,22 @@
  *
  * Contributors:
  *     QNX Software Systems - Initial implementation
+ *     John Dallaway - Extend CMainTab2, bug 568454
  *******************************************************************************/
 package org.eclipse.cdt.debug.gdbjtag.internal.ui;
 
-import org.eclipse.cdt.dsf.gdb.internal.ui.launching.CMainTab;
+import org.eclipse.cdt.launch.ui.CMainTab2;
 
-/**
- * @since 7.0
- */
-@SuppressWarnings("deprecation")
-public class GDBJtagDSFCMainTab extends CMainTab {
+public class GDBJtagDSFCMainTab extends CMainTab2 {
 
 	public GDBJtagDSFCMainTab() {
-		super(CMainTab.INCLUDE_BUILD_SETTINGS);
+		super(CMainTab2.INCLUDE_BUILD_SETTINGS);
 	}
+
+	@Override
+	public String getId() {
+		// return historic ID for compatibility with existing consumers
+		return "org.eclipse.cdt.dsf.gdb.launch.mainTab"; //$NON-NLS-1$
+	}
+
 }

437e9636d7f2e87040b223732d0a25f3c340e371
Martin Weber <fifteenknots505@gmail.com>
2020-11-03 21:58:54 +0100

    Bug 567488: Use snakeyaml to persist command-line options to pass to cmake
    Change-Id: Ia6b60865f663aecae74d6d571bc9d213bf7cd36b
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- cmake/org.eclipse.cdt.cmake.core/src/org/eclipse/cdt/cmake/core/properties/IGeneralProperties.java
+++ cmake/org.eclipse.cdt.cmake.core/src/org/eclipse/cdt/cmake/core/properties/IGeneralProperties.java
@@ -16,6 +16,8 @@ package org.eclipse.cdt.cmake.core.properties;
  *
  * @author Martin Weber
  *
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
  */
 public interface IGeneralProperties {
 

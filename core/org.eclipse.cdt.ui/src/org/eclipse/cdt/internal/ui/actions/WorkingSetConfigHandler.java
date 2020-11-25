58de25c727f31c8640d14bd37e2fee132b7c8fe9
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2020-11-25 12:46:13 +0100

    Bug 383348 - Replace actions with invalid menu paths in CDT UI
    This change adjusts replaces the following actions (part of action set
    org.eclipse.cdt.ui.buildConfigActionSet) with commands and menus:
    
    * org.eclipse.cdt.ui.manageConfigsAction2
    * org.eclipse.cdt.ui.buildConfigMenuAction
    * org.eclipse.cdt.ui.wsselection
    
    This is done to avoid menu extension errors on perspective
    customization, due to problematic code in CustomizePerspectiveDialog. In
    particular the customize perspective dialog will populate main menu
    submenus only after going over actions; this causes the menu paths of
    the actions above to be detected as invalid, despite actually being
    valid and functional.
    
    The original action classes are deprecated and marked for removal. The
    respective classes are defined in non-internal packages; removal would
    be considered API breakage and so must be done in a major version bump.
    
    Change-Id: I31517697689772395b7e1868ef4cab07ad946085
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/actions/WorkingSetConfigHandler.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Simeon Andreev and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Simeon Andreev - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.internal.ui.actions;
+
+import org.eclipse.cdt.internal.ui.workingsets.WorkingSetConfigurationDialog;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.IWorkingSetManager;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class WorkingSetConfigHandler extends AbstractHandler {
+
+	public static final String COMMAND_ID = "org.eclipse.cdt.ui.menu.manage.configs.command"; //$NON-NLS-1$
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		boolean enabled = hasNonEmptyWorksets();
+		if (enabled) {
+			IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+			new WorkingSetConfigurationDialog(window.getShell()).open();
+		}
+		return null;
+	}
+
+	private boolean hasNonEmptyWorksets() {
+		IWorkingSetManager workingSetManager = PlatformUI.getWorkbench().getWorkingSetManager();
+		IWorkingSet[] workingSets = workingSetManager.getWorkingSets();
+		if (workingSets != null) {
+			for (IWorkingSet workingSet : workingSets) {
+				if (!workingSet.isEmpty()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+}
---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/workingsets/HasManagedCdtProjectSelection.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Simeon Andreev and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Simeon Andreev - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.internal.ui.workingsets;
+
+import java.util.List;
+
+import org.eclipse.cdt.ui.newui.ManageConfigSelector;
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IProject;
+
+/**
+ * Property tester for the enablement of the command handled by {@link org.eclipse.cdt.internal.ui.actions.ManageConfigsHandler}.
+ * Will evaluate {@code true} if the current selection is a managed CDT project, contains a managed CDT project, or belongs to a managed CDT project.
+ */
+public class HasManagedCdtProjectSelection extends PropertyTester {
+
+	private static final String PROPERTY_HAS_MANAGED_CDT_PROJECT_SELECTION = "hasManagedCdtProjectSelection"; //$NON-NLS-1$
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if (PROPERTY_HAS_MANAGED_CDT_PROJECT_SELECTION.equals(property)) {
+			boolean hasNonEmptyWorksets = hasManagedCdtProjectSelection(receiver);
+			return hasNonEmptyWorksets;
+		}
+		return false;
+	}
+
+	private static boolean hasManagedCdtProjectSelection(Object receiver) {
+		if (receiver instanceof List) {
+			List<?> selection = (List<?>) receiver;
+			if (!selection.isEmpty()) {
+				IProject[] obs = ManageConfigSelector.getProjects(selection.toArray());
+				return ManageConfigSelector.getManager(obs) != null;
+			}
+		}
+		return false;
+	}
+
+}

a3b6deddfadbd9e8c86ce2a39a3e990f7f2d3303
Jonah Graham <jonah@kichwacoders.com>
2021-04-04 21:17:15 -0400

    Bug 572582: Increase minimum supported version of GDB to 7.12
    Change-Id: Ifb7cd77c9fae4b9d1b82e0aa8a2cea206a4298b7

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/ITestConstants.java
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/src/org/eclipse/cdt/tests/dsf/gdb/tests/ITestConstants.java
@@ -45,14 +45,14 @@ public class ITestConstants {
 			// add new versions here
 			ITestConstants.SUFFIX_GDB_10, ITestConstants.SUFFIX_GDB_9, ITestConstants.SUFFIX_GDB_8_3,
 			ITestConstants.SUFFIX_GDB_8_2, ITestConstants.SUFFIX_GDB_8_1, ITestConstants.SUFFIX_GDB_8_0,
-			ITestConstants.SUFFIX_GDB_7_12, ITestConstants.SUFFIX_GDB_7_11, ITestConstants.SUFFIX_GDB_7_10,
-			ITestConstants.SUFFIX_GDB_7_9, ITestConstants.SUFFIX_GDB_7_8, ITestConstants.SUFFIX_GDB_7_7,
-			ITestConstants.SUFFIX_GDB_7_6, ITestConstants.SUFFIX_GDB_7_5, };
+			ITestConstants.SUFFIX_GDB_7_12, };
 
-	public static String[] ALL_UNSUPPORTED_VERSIONS = new String[] { ITestConstants.SUFFIX_GDB_7_4,
-			ITestConstants.SUFFIX_GDB_7_3, ITestConstants.SUFFIX_GDB_7_2, ITestConstants.SUFFIX_GDB_7_1,
-			ITestConstants.SUFFIX_GDB_7_0, ITestConstants.SUFFIX_GDB_6_8, ITestConstants.SUFFIX_GDB_6_7,
-			ITestConstants.SUFFIX_GDB_6_6, };
+	public static String[] ALL_UNSUPPORTED_VERSIONS = new String[] { ITestConstants.SUFFIX_GDB_7_11,
+			ITestConstants.SUFFIX_GDB_7_10, ITestConstants.SUFFIX_GDB_7_9, ITestConstants.SUFFIX_GDB_7_8,
+			ITestConstants.SUFFIX_GDB_7_7, ITestConstants.SUFFIX_GDB_7_6, ITestConstants.SUFFIX_GDB_7_5,
+			ITestConstants.SUFFIX_GDB_7_4, ITestConstants.SUFFIX_GDB_7_3, ITestConstants.SUFFIX_GDB_7_2,
+			ITestConstants.SUFFIX_GDB_7_1, ITestConstants.SUFFIX_GDB_7_0, ITestConstants.SUFFIX_GDB_6_8,
+			ITestConstants.SUFFIX_GDB_6_7, ITestConstants.SUFFIX_GDB_6_6, };
 
 	public static String[] ALL_KNOWN_VERSIONS;
 	static {

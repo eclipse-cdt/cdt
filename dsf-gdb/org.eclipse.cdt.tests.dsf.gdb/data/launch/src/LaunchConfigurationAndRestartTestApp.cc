7e03467055b4803f4e9f9f086cb6447d8370f4f0
Jonah Graham <jonah@kichwacoders.com>
2021-04-09 10:02:51 -0400

    Bug 572742: Fix reverse debugging enable at startup
    This fixes and reenables a bunch of reverse debugging tests
    
    Change-Id: Ib44a60c06c4c94b2dfca4631794f960ed99a0465

--- dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/LaunchConfigurationAndRestartTestApp.cc
+++ dsf-gdb/org.eclipse.cdt.tests.dsf.gdb/data/launch/src/LaunchConfigurationAndRestartTestApp.cc
@@ -23,11 +23,15 @@ int envTest() {
 }
 
 int main (int argc, char *argv[])
-{
-    int dummy = 1; // FIRST_LINE_IN_MAIN
-    envTest(); // main_init
+{    int dummy = 1; // FIRST_LINE_IN_MAIN (make the line of code the same as opening brace to account for different gdb/gcc combinations)
+	int var = 1; // GDB has incomplete support for reverse debugging - so for the purpose of tests reverse debug these simple assignment statements
+	var = 2;
+	var = 3;
+	var = 4; // three_steps_back_from_b_stopAtOther
+	var = 5;
+	stopAtOther(); // main_init
     reverseTest(); // tests assume that every line between first and last
-    stopAtOther(); // is steppable, so no blank lines allowed.
+    envTest(); // is steppable, so no blank lines allowed.
     return 36; // LAST_LINE_IN_MAIN
     // Return special value to allow
     // testing exit code feature

766d6fec6a60ea4e18dd9569859c9fda7bece78f
Marc Ernst <marc.ernst@intel.com>
2021-04-20 09:34:23 +0200

    Bug 572880: show opcodes in disassembly view as byte sequence
    Change-Id: Ib7ad37968ab23ff22cbb1fc110e85b47ffc37ab8

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/OpcodeRulerColumn.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/OpcodeRulerColumn.java
@@ -13,12 +13,13 @@
  *******************************************************************************/
 package org.eclipse.cdt.dsf.debug.internal.ui.disassembly;
 
+import java.util.StringJoiner;
+
 import org.eclipse.cdt.debug.internal.ui.disassembly.dsf.AddressRangePosition;
 import org.eclipse.cdt.debug.internal.ui.disassembly.dsf.DisassemblyPosition;
 import org.eclipse.cdt.dsf.debug.internal.ui.disassembly.model.DisassemblyDocument;
 import org.eclipse.cdt.dsf.debug.internal.ui.disassembly.preferences.DisassemblyPreferenceConstants;
 import org.eclipse.cdt.dsf.debug.internal.ui.disassembly.provisional.DisassemblyRulerColumn;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.util.PropertyChangeEvent;
 
@@ -30,10 +31,8 @@ public class OpcodeRulerColumn extends DisassemblyRulerColumn {
 	public static final String ID = "org.eclipse.cdt.dsf.ui.disassemblyColumn.opcode"; //$NON-NLS-1$
 
 	/** Maximum width of column (in characters) */
-	private static final int MAXWIDTH = 20;
-
-	private int fRadix;
-	private String fRadixPrefix;
+	/** 15 bytes plus separator */
+	private static final int MAXWIDTH = 44;
 
 	/**
 	 * Default constructor.
@@ -42,22 +41,6 @@ public class OpcodeRulerColumn extends DisassemblyRulerColumn {
 		super();
 		setBackground(getColor(DisassemblyPreferenceConstants.RULER_BACKGROUND_COLOR));
 		setForeground(getColor(DisassemblyPreferenceConstants.CODE_BYTES_COLOR));
-		setRadix(getPreferenceStore().getInt(DisassemblyPreferenceConstants.OPCODE_RADIX));
-	}
-
-	public void setRadix(int radix) {
-		fRadix = radix;
-		setShowRadixPrefix();
-	}
-
-	public void setShowRadixPrefix() {
-		if (fRadix == 16) {
-			fRadixPrefix = "0x"; //$NON-NLS-1$
-		} else if (fRadix == 8) {
-			fRadixPrefix = "0"; //$NON-NLS-1$
-		} else {
-			fRadixPrefix = null;
-		}
 	}
 
 	/*
@@ -73,25 +56,9 @@ public class OpcodeRulerColumn extends DisassemblyRulerColumn {
 				AddressRangePosition pos = doc.getDisassemblyPosition(offset);
 				if (pos instanceof DisassemblyPosition && pos.length > 0 && pos.offset == offset && pos.fValid) {
 					DisassemblyPosition disassPos = (DisassemblyPosition) pos;
-					if (disassPos.fOpcodes != null) {
+					if (disassPos.fOpcode != null) {
 						// Format the output.
-						String str = disassPos.fOpcodes.toString(fRadix);
-						int prefixLength = 0;
-
-						if (fRadixPrefix != null)
-							prefixLength = fRadixPrefix.length();
-
-						StringBuilder buf = new StringBuilder(nChars);
-
-						if (prefixLength != 0)
-							buf.append(fRadixPrefix);
-
-						for (int i = str.length() + prefixLength; i < nChars; ++i)
-							buf.append('0');
-						buf.append(str);
-						if (buf.length() > nChars)
-							buf.delete(nChars, buf.length());
-						return buf.toString();
+						return getOpcodeString(disassPos.fOpcode);
 					}
 				} else if (pos != null && !pos.fValid) {
 					return DOTS.substring(0, nChars);
@@ -103,23 +70,30 @@ public class OpcodeRulerColumn extends DisassemblyRulerColumn {
 		return ""; //$NON-NLS-1$
 	}
 
+	protected String getOpcodeString(Byte[] opcode) {
+		if (opcode.length == 0) {
+			return "??"; //$NON-NLS-1$
+		}
+		StringJoiner opcodeStringJoiner = new StringJoiner(" "); //$NON-NLS-1$
+		for (int i = 0; i < opcode.length; i++) {
+			opcodeStringJoiner.add(String.format("%02x", //$NON-NLS-1$
+					opcode[i].intValue() & 0xff));
+		}
+		return opcodeStringJoiner.toString();
+	}
+
 	@Override
 	protected int computeNumberOfCharacters() {
 		DisassemblyDocument doc = (DisassemblyDocument) getParentRuler().getTextViewer().getDocument();
-		return Math.min(MAXWIDTH, doc.getMaxOpcodeLength(fRadix));
+		return Math.min(MAXWIDTH, doc.getMaxOpcodeLength());
 	}
 
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
 		String property = event.getProperty();
-		IPreferenceStore store = getPreferenceStore();
 		boolean needRedraw = false;
 		if (DisassemblyPreferenceConstants.CODE_BYTES_COLOR.equals(property)) {
 			setForeground(getColor(property));
-			needRedraw = true;
-		} else if (DisassemblyPreferenceConstants.OPCODE_RADIX.equals(property)) {
-			setRadix(store.getInt(property));
-			layout(false);
 			needRedraw = true;
 		} else if (DisassemblyPreferenceConstants.RULER_BACKGROUND_COLOR.equals(property)) {
 			setBackground(getColor(property));

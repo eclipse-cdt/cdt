cdae47f5ba916001dbc09edc2746869d6a6a9812
Michael Uhl <Michael.Uhl@NashTech.Com>
2021-05-25 19:03:31 +0200

    Bug 573764 - Ambiguous conversion on numeric type
    Change-Id: Ia7ffd1169a867f7e05de36f5e278b4141af55b62
    Signed-off-by: Michael Uhl <Michael.Uhl@NashTech.Com>

--- core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/Cost.java
+++ core/org.eclipse.cdt.core/parser/org/eclipse/cdt/internal/core/dom/parser/cpp/semantics/Cost.java
@@ -20,6 +20,7 @@ package org.eclipse.cdt.internal.core.dom.parser.cpp.semantics;
 import org.eclipse.cdt.core.dom.ast.IArrayType;
 import org.eclipse.cdt.core.dom.ast.IBasicType.Kind;
 import org.eclipse.cdt.core.dom.ast.IEnumeration;
+import org.eclipse.cdt.core.dom.ast.IPointerType;
 import org.eclipse.cdt.core.dom.ast.IType;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPBasicType;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPEnumeration;
@@ -282,6 +283,12 @@ public class Cost {
 				return -1;
 			if ((other.fQualificationAdjustments & qdiff) == 0)
 				return 1;
+		}
+
+		if (other.target instanceof IPointerType && !(this.target instanceof IPointerType)) {
+			return 1;
+		} else if (!(other.target instanceof IPointerType) && this.target instanceof IPointerType) {
+			return -1;
 		}
 
 		return 0;

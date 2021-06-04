d07b3c43c3332721c5490bc1dd3b976e9610e13d
vinodappu <vinod.appu@ashling.com>
2021-06-04 14:55:02 +0530

    GDB global variables view support
    Change-Id: I64892010858d28333fe60aded99822c120d58f73

---
+++ dsf-gdb/com.ashling.riscfree.globalvariable.view/src/com/ashling/riscfree/globalvariable/view/dsf/Messages.java
@@ -0,0 +1,19 @@
+package com.ashling.riscfree.globalvariable.view.dsf;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.ashling.riscfree.globalvariable.view.dsf.messages"; //$NON-NLS-1$
+	public static String GlobalVariableService_0;
+	public static String GlobalVariableService_1;
+	public static String GlobalVariableService_2;
+	public static String GlobalVariableService_3;
+	public static String GlobalVariableService_4;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}

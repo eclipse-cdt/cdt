d49362a5268921e56df4e245dc6ea6f9652c69d9
Jonah Graham <jonah@kichwacoders.com>
2021-06-06 21:12:49 -0400

    Add missing copyright header.
    Change-Id: If005b907dd902acd8647dfb5932bcfbbfa0ae0a3

--- launchbar/org.eclipse.launchbar.core/src/org/eclipse/launchbar/core/target/TargetStatus.java
+++ launchbar/org.eclipse.launchbar.core/src/org/eclipse/launchbar/core/target/TargetStatus.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 QNX Software Systems and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package org.eclipse.launchbar.core.target;
 
 import org.eclipse.launchbar.core.internal.Messages;

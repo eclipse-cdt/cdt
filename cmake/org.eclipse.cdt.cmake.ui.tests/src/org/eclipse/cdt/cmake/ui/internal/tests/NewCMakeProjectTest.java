1b8575fc98350a3535ae256c6502357eb466200e
Mat Booth <mat.booth@gmail.com>
2021-07-31 15:39:24 +0100

    Bug 575145 - Importing CMake/Meson Projects
    Add API to o.e.tools.templates.ui that hooks into the smart import
    feature and allows easy implementation of configurators for various
    CDT project types.
    
    Project types that have a IGenerator may use this API to offer smart
    import functionality by registering their implementation using the
    org.eclipse.ui.ide.projectConfigurator extension point.
    
    This change includes project import implementations for Meson and
    CMake project types.
    
    For these project types users can use the normal project import
    workflow for their existing non-Eclipse CMake projects instead of
    using the New Project Wizard. As an additional benefit, users
    can now also import more than one project at a time, even nested
    projects.
    
    Change also includes SWTBot tests to exercise the feature.
    
    Signed-off-by: Mat Booth <mat.booth@gmail.com>
    Change-Id: I96589e86bee561aa200a4a4487549305765d6409

--- cmake/org.eclipse.cdt.cmake.ui.tests/src/org/eclipse/cdt/cmake/ui/internal/tests/NewCMakeProjectTest.java
+++ cmake/org.eclipse.cdt.cmake.ui.tests/src/org/eclipse/cdt/cmake/ui/internal/tests/NewCMakeProjectTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 QNX Software Systems and others.
+ * Copyright (c) 2017, 2021 QNX Software Systems and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -11,7 +11,16 @@
 package org.eclipse.cdt.cmake.ui.internal.tests;
 
 import static org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory.withPartName;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.cdt.cmake.core.CMakeNature;
 import org.eclipse.cdt.core.CCorePlugin;
@@ -20,44 +29,58 @@ import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotPerspective;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTableItem;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.MethodName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.io.TempDir;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
-@SuppressWarnings("nls")
+@TestMethodOrder(MethodName.class)
 public class NewCMakeProjectTest {
 
 	private static SWTWorkbenchBot bot;
 
-	@BeforeClass
+	@TempDir
+	public static Path TEMP_DIR;
+
+	@BeforeAll
 	public static void beforeClass() {
 		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
 		SWTBotPreferences.TIMEOUT = 10000;
 		bot = new SWTWorkbenchBot();
 	}
 
-	@Before
+	@BeforeEach
 	public void before() {
 		bot.resetWorkbench();
 
 		for (SWTBotView view : bot.views(withPartName("Welcome"))) {
 			view.close();
 		}
+		SWTBotPerspective perspective = bot.perspectiveById("org.eclipse.cdt.ui.CPerspective");
+		perspective.activate();
+		bot.shell().activate();
 	}
 
-	@Test(timeout = 60000)
-	public void createCMakeProject() throws Exception {
-		// open C++ perspective
-		if (!"C/C++".equals(bot.activePerspective().getLabel())) {
-			bot.perspectiveByLabel("C/C++").activate();
-		}
+	@Test
+	@Timeout(value = 2, unit = TimeUnit.MINUTES)
+	void createCMakeProject() throws Exception {
 
 		// Activate C/C++ wizard
 		bot.menu("File").menu("New").menu("C/C++ Project").click();
@@ -86,22 +109,102 @@ public class NewCMakeProjectTest {
 		bot.button("Finish").click();
 		bot.waitUntil(Conditions.shellCloses(newProjectShell));
 
+		verifyProjectInExplorer(projectName);
+	}
+
+	@Test
+	@Timeout(value = 2, unit = TimeUnit.MINUTES)
+	void importSimpleCMakeProject() throws Exception {
+		importCMakeProject("SimpleProject", 1);
+	}
+
+	@Test
+	@Timeout(value = 2, unit = TimeUnit.MINUTES)
+	void importNestedCMakeProject() throws Exception {
+		importCMakeProject("NestedProject", 3);
+	}
+
+	private void importCMakeProject(String projectName, int expectedProjects) throws Exception {
+		// Copy test project out of the bundle to a temp location first
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+		URL url = FileLocator.toFileURL(
+				FileLocator.find(bundle, new org.eclipse.core.runtime.Path("projects/" + projectName), null));
+		copyDir(Paths.get(url.getPath()), TEMP_DIR.resolve(projectName));
+
+		// Activate import wizard
+		bot.menu("File").menu("Import...").click();
+		bot.shell("Import").activate();
+
+		// Open the smart import wizard
+		SWTBotTree wizTree = bot.tree();
+		SWTBotTreeItem generalItem = wizTree.getTreeItem("General").expand();
+		generalItem.getNode("Projects from Folder or Archive").doubleClick();
+
+		// Select path that contains projects to import and check the project type detection works
+		bot.comboBox().setText(TEMP_DIR.resolve(projectName).toString());
+		SWTBotTree projectProposalTree = bot.tree();
+		assertEquals(expectedProjects, projectProposalTree.getAllItems().length);
+		for (SWTBotTreeItem item : projectProposalTree.getAllItems()) {
+			assertEquals("CMake Project", item.cell(1));
+		}
+
+		// Import the project
+		SWTBotShell wizShell = bot.activeShell();
+		bot.button("Finish").click();
+		bot.waitUntil(Conditions.shellCloses(wizShell));
+
+		verifyProjectInExplorer(projectName);
+	}
+
+	private void verifyProjectInExplorer(String projectName) throws Exception {
 		// Make sure it shows up in Project Explorer
 		SWTBotView explorer = bot.viewByPartName("Project Explorer");
 		explorer.show();
 		explorer.setFocus();
-		bot.tree().getTreeItem(projectName);
+		explorer.bot().tree().getTreeItem(projectName);
 
-		// Make sure the project indexer completes. At that point we're stable.
+		// Tests can be unstable if we are too quick, so make sure the project indexer completes
+		// and project natures have been assigned before continuing with post-creation verification.
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-		ICProject cproject = CoreModel.getDefault().create(project);
-		IIndexManager indexManager = CCorePlugin.getIndexManager();
-		while (!indexManager.isProjectContentSynced(cproject)) {
-			Thread.sleep(1000);
-		}
+		bot.waitUntil(new DefaultCondition() {
+
+			@Override
+			public boolean test() throws Exception {
+				int natures = project.getDescription().getNatureIds().length;
+				ICProject cproject = CoreModel.getDefault().create(project);
+				IIndexManager indexManager = CCorePlugin.getIndexManager();
+				return natures > 0 && indexManager.isProjectContentSynced(cproject);
+			}
+
+			@Override
+			public String getFailureMessage() {
+				return "Indexer never finished or natures never assigned for project " + project.getName();
+			}
+		});
 
 		// Make sure it has the right nature
 		assertTrue(project.hasNature(CMakeNature.ID));
+
+		// Ensure CMakeLists exists
+		assertTrue(project.getFile("CMakeLists.txt").exists());
 	}
 
+	/**
+	 * Utility to perform a depth-first copy of a directory tree.
+	 */
+	private static void copyDir(Path src, Path dest) throws IOException {
+		Files.walk(src).forEach(a -> {
+			if (!a.equals(src)) {
+				Path b = dest.resolve(a.subpath(src.getNameCount(), a.getNameCount()));
+				try {
+					if (!Files.isDirectory(a)) {
+						Files.createDirectories(b.getParent());
+						Files.copy(a, b);
+					}
+				} catch (IOException e) {
+					fail(e.getMessage());
+				}
+			}
+		});
+	}
 }

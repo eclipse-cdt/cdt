eclipse-cdt/cdt

commit 15c29ebf2cfd2b97a67ac837cc80f8ed468a0493

Bug 505882: Generate clean target per subdir.mk
To avoid errors like "Command line too long" when removing build
artifacts, split the clean command on multiple lines. In order to
properly handle whitespace etc, generate a new clean target in each
subdir.mk with the explicit list of files to remove in that directory.
The command should be splited on a sane length, 6000 is used in the
internal builder, so reuse that limit here.

Contributed by STMicroelectronics

Change-Id: I843e1ac896076d279173c421210b72f4a325296f
Signed-off-by: Torbjörn Svensson <torbjorn.svensson@st.com>

build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/test30_2/Benchmarks/makefile
@@ -61,7 +61,7 @@ new.log: $(TAROUT) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(TAROUT)$(TERMINAL__DUMMY_OUTPUT__OUTPUTS)$(LOGFILE) test30_2.tmp
+	-$(RM) new.log new.tar test30_2.tmp
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu2/GnuMakefileGenerator.java
@@ -27,6 +27,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,6 +36,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Vector;
@@ -344,6 +346,15 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 	private static final String POSTBUILD = "post-build"; //$NON-NLS-1$
 	private static final String SECONDARY_OUTPUTS = "secondary-outputs"; //$NON-NLS-1$
 
+	/**
+	 * On Windows XP and above, the maximum command line length is 8191, on Linux it is at least 131072, but
+	 * that includes the environment. We want to limit the invocation of a single command to this number of
+	 * characters, and we want to ensure that the number isn't so low as to slow down operation.
+	 *
+	 * Doing each rm in its own command would be very slow, especially on Windows.
+	 */
+	private static final int MAX_CLEAN_LENGTH = 6000;
+
 	// Enumerations
 	public static final int PROJECT_RELATIVE = 1, PROJECT_SUBDIR_RELATIVE = 2, ABSOLUTE = 3;
 
@@ -388,7 +399,7 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 	private final HashMap<String, List<IPath>> buildOutVars = new HashMap<>();
 	//  Map of dependency file build variable names to a List of GnuDependencyGroupInfo objects
 	private final HashMap<String, GnuDependencyGroupInfo> buildDepVars = new HashMap<>();
-	private final LinkedHashMap<String, String> topBuildOutVars = new LinkedHashMap<>();
+	private final Map<String, Set<String>> topBuildOutVars = new LinkedHashMap<>();
 	// Dependency file variables
 	//	private Vector dependencyMakefiles;		//  IPath's - relative to the top build directory or absolute
 
@@ -1008,7 +1019,7 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 		IFile modMakefile = createFile(moduleOutputDir.append(MODFILE_NAME));
 		StringBuffer makeBuf = new StringBuffer();
 		makeBuf.append(addFragmentMakefileHeader());
-		makeBuf.append(addSources(module));
+		makeBuf.append(addSources(module, toCleanTarget(moduleRelativePath)));
 
 		// Save the files
 		save(makeBuf, modMakefile);
@@ -1627,11 +1638,8 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 
 		// Always add a clean target
 		buffer.append("clean:").append(NEWLINE); //$NON-NLS-1$
-		buffer.append(TAB).append("-$(RM)").append(WHITESPACE); //$NON-NLS-1$
-		for (Entry<String, List<IPath>> entry : buildOutVars.entrySet()) {
-			String macroName = entry.getKey();
-			buffer.append("$(").append(macroName).append(')'); //$NON-NLS-1$
-		}
+
+		Set<String> filesToClean = new HashSet<>();
 		String outputPrefix = EMPTY_STRING;
 		if (targetTool != null) {
 			outputPrefix = targetTool.getOutputPrefix();
@@ -1640,11 +1648,42 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 		if (buildTargetExt.length() > 0) {
 			completeBuildTargetName = completeBuildTargetName + DOT + buildTargetExt;
 		}
-		if (completeBuildTargetName.contains(" ")) { //$NON-NLS-1$
-			buffer.append(WHITESPACE).append('"').append(completeBuildTargetName).append('"');
-		} else {
-			buffer.append(WHITESPACE).append(completeBuildTargetName);
+		completeBuildTargetName = ensurePathIsGNUMakeTargetRuleCompatibleSyntax(completeBuildTargetName);
+		filesToClean.add(completeBuildTargetName);
+
+		Map<String, Set<String>> map = getTopBuildOutputVars();
+		for (String macroName : outputVarsAdditionsList) {
+			Set<String> set = map.getOrDefault(macroName, Collections.emptySet());
+			filesToClean.addAll(set);
 		}
+
+		if (!filesToClean.isEmpty()) {
+			StringBuffer rmLineBuffer = new StringBuffer();
+			rmLineBuffer.append(TAB).append("-$(RM)"); //$NON-NLS-1$
+
+			// Convert the set to an ordered list. Without this "unneeded" sorting, the unit tests will fail
+			List<String> filesToCleanOrdered = new ArrayList<>(filesToClean);
+			filesToCleanOrdered.sort((p1, p2) -> p1.toString().compareTo(p2.toString()));
+
+			for (String path : filesToCleanOrdered) {
+				// Bug 417228, ilg@livius.net & freidin.alex@gmail.com
+				path = ensurePathIsGNUMakeTargetRuleCompatibleSyntax(path);
+
+				// There is a max length for a command line, wrap to multiple invocations if needed.
+				if (rmLineBuffer.length() + path.length() > MAX_CLEAN_LENGTH) {
+					// Terminate this RM line
+					buffer.append(rmLineBuffer).append(NEWLINE);
+
+					// Start a new RM line
+					rmLineBuffer = new StringBuffer();
+					rmLineBuffer.append(TAB).append("-$(RM)"); //$NON-NLS-1$
+				}
+				rmLineBuffer.append(WHITESPACE).append(path);
+			}
+
+			buffer.append(rmLineBuffer);
+		}
+
 		buffer.append(NEWLINE);
 		buffer.append(TAB).append(DASH).append(AT).append(ECHO_BLANK_LINE).append(NEWLINE);
 
@@ -2014,7 +2053,7 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 	 * @param module  project resource directory/subdirectory
 	 * @return StringBuffer  generated text for the fragement makefile
 	 */
-	protected StringBuffer addSources(IContainer module) throws CoreException {
+	protected StringBuffer addSources(IContainer module, String cleanTarget) throws CoreException {
 		// Calculate the new directory relative to the build output
 		IPath moduleRelativePath = module.getProjectRelativePath();
 		String relativePath = moduleRelativePath.toString();
@@ -2047,6 +2086,7 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 
 		IResourceInfo rcInfo;
 		IFolder folder = project.getFolder(computeTopBuildDir(config.getName()));
+		Set<IPath> filesToClean = new HashSet<>();
 
 		for (IResource resource : resources) {
 			if (resource.getType() == IResource.FILE) {
@@ -2058,16 +2098,50 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 				//				if( (rcInfo.isExcluded()) )
 				//					continue;
 				addFragmentMakefileEntriesForSource(buildVarToRuleStringMap, ruleBuffer, folder, relativePath, resource,
-						getPathForResource(resource), rcInfo, null, false);
+						getPathForResource(resource), rcInfo, null, false, filesToClean);
 			}
 		}
 
 		// Write out the macro addition entries to the buffer
 		buffer.append(writeAdditionMacros(buildVarToRuleStringMap));
-		return buffer.append(ruleBuffer).append(NEWLINE);
+		buffer.append(ruleBuffer).append(NEWLINE);
+
+		if (cleanTarget != null && !filesToClean.isEmpty()) {
+			buffer.append("clean: " + cleanTarget).append(NEWLINE).append(NEWLINE); //$NON-NLS-1$
+			buffer.append(cleanTarget + COLON).append(NEWLINE);
+
+			StringBuffer rmLineBuffer = new StringBuffer();
+			rmLineBuffer.append(TAB).append("-$(RM)"); //$NON-NLS-1$
+
+			// Convert the set to an ordered list. Without this "unneeded" sorting, the unit tests will fail
+			List<IPath> filesToCleanOrdered = new ArrayList<>(filesToClean);
+			filesToCleanOrdered.sort((p1, p2) -> p1.toString().compareTo(p2.toString()));
+
+			for (IPath fileToClean : filesToCleanOrdered) {
+				String path = escapeWhitespaces(
+						(fileToClean.isAbsolute() || fileToClean.segment(0).equals(".") ? EMPTY_STRING : "./") //$NON-NLS-1$ //$NON-NLS-2$
+								+ fileToClean.toString());
+
+				// There is a max length for a command line, wrap to multiple invocations if needed.
+				if (rmLineBuffer.length() + path.length() > MAX_CLEAN_LENGTH) {
+					// Terminate this RM line
+					buffer.append(rmLineBuffer).append(NEWLINE);
+
+					// Start a new RM line
+					rmLineBuffer = new StringBuffer();
+					rmLineBuffer.append(TAB).append("-$(RM)"); //$NON-NLS-1$
+				}
+				rmLineBuffer.append(WHITESPACE).append(path);
+			}
+
+			buffer.append(rmLineBuffer).append(NEWLINE).append(NEWLINE);
+			buffer.append(".PHONY: ").append(cleanTarget).append(NEWLINE).append(NEWLINE); //$NON-NLS-1$
+		}
+
+		return buffer;
 	}
 
-	/* (non-Javadoc
+	/**
 	 * Adds the entries for a particular source file to the fragment makefile
 	 *
 	 * @param buildVarToRuleStringMap  map of build variable names to the list of files assigned to the variable
@@ -2083,7 +2157,7 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 	 */
 	protected void addFragmentMakefileEntriesForSource(LinkedHashMap<String, String> buildVarToRuleStringMap,
 			StringBuffer ruleBuffer, IFolder folder, String relativePath, IResource resource, IPath sourceLocation,
-			IResourceInfo rcInfo, String varName, boolean generatedSource) {
+			IResourceInfo rcInfo, String varName, boolean generatedSource, Set<IPath> filesToClean) {
 
 		//  Determine which tool, if any, builds files with this extension
 		String ext = sourceLocation.getFileExtension();
@@ -2213,8 +2287,11 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 						nextRcInfo = rcInfo;
 					}
 					addFragmentMakefileEntriesForSource(buildVarToRuleStringMap, ruleBuffer, folder, relativePath,
-							generateOutputResource, generatedOutput, nextRcInfo, buildVariable, true);
+							generateOutputResource, generatedOutput, nextRcInfo, buildVariable, true, filesToClean);
 				}
+
+				filesToClean.addAll(generatedDepFiles);
+				filesToClean.addAll(generatedOutputs);
 			}
 		} else {
 			//  If this is a secondary input, add it to build vars
@@ -3860,24 +3937,15 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 
 	/**
 	 * Adds file(s) to an entry in a map of macro names to entries.
-	 * File additions look like:
-	 * 	example.c, \
 	 */
-	public void addMacroAdditionFiles(HashMap<String, String> map, String macroName, Vector<String> filenames) {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append(map.get(macroName));
-		for (int i = 0; i < filenames.size(); i++) {
-			String filename = filenames.get(i);
-			if (filename.length() > 0) {
+	public void addMacroAdditionFiles(Map<String, Set<String>> map, String macroName, Vector<String> filenames) {
+		Set<String> set = map.get(macroName);
 
-				// Bug 417288, ilg@livius.net & freidin.alex@gmail.com
-				filename = ensurePathIsGNUMakeTargetRuleCompatibleSyntax(filename);
-
-				buffer.append(filename).append(WHITESPACE).append(LINEBREAK);
+		for (String filename : filenames) {
+			if (!filename.isEmpty()) {
+				set.add(filename);
 			}
 		}
-		// re-insert string in the map
-		map.put(macroName, buffer.toString());
 	}
 
 	/**
@@ -3914,20 +3982,25 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 	/**
 	 * Write all macro addition entries in a map to the buffer
 	 */
-	protected StringBuffer writeTopAdditionMacros(List<String> varList, HashMap<String, String> varMap) {
+	protected StringBuffer writeTopAdditionMacros(List<String> varList, Map<String, Set<String>> varMap) {
 		StringBuffer buffer = new StringBuffer();
 		// Add the comment
 		buffer.append(COMMENT_SYMBOL).append(WHITESPACE).append(ManagedMakeMessages.getResourceString(MOD_VARS))
 				.append(NEWLINE);
 
-		for (int i = 0; i < varList.size(); i++) {
-			String addition = varMap.get(varList.get(i));
-			StringBuffer currentBuffer = new StringBuffer();
-			currentBuffer.append(addition);
-			currentBuffer.append(NEWLINE);
+		for (String macroName : varList) {
+			Set<String> files = varMap.get(macroName);
 
-			// append the contents of the buffer to the master buffer for the whole file
-			buffer.append(currentBuffer);
+			buffer.append(macroName).append(WHITESPACE).append(MACRO_ADDITION_PREFIX_SUFFIX);
+
+			for (String filename : files) {
+				// Bug 417228, ilg@livius.net & freidin.alex@gmail.com
+				filename = ensurePathIsGNUMakeTargetRuleCompatibleSyntax(filename);
+
+				buffer.append(filename).append(WHITESPACE).append(LINEBREAK);
+			}
+
+			buffer.append(NEWLINE);
 		}
 		return buffer.append(NEWLINE);
 	}
@@ -3992,16 +4065,14 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 				}
 
 				//  Initialize the build output variable to file additions map
-				LinkedHashMap<String, String> map = getTopBuildOutputVars();
+				Map<String, Set<String>> map = getTopBuildOutputVars();
 				Set<Entry<String, List<IPath>>> set = buildOutVars.entrySet();
 				for (Entry<String, List<IPath>> entry : set) {
 					String macroName = entry.getKey();
 
 					// for projects with specific setting on folders/files do
 					// not clear the macro value on subsequent passes
-					if (!map.containsKey(macroName)) {
-						addMacroAdditionPrefix(map, macroName, "", false); //$NON-NLS-1$
-					}
+					map.putIfAbsent(macroName, new HashSet<>());
 				}
 
 				// Set of input extensions for which macros have been created so far
@@ -4146,10 +4217,8 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 
 	/**
 	 * Returns the map of build variables used in the top makefile to list of files
-	 *
-	 * @return HashMap
 	 */
-	public LinkedHashMap<String, String> getTopBuildOutputVars() {
+	public Map<String, Set<String>> getTopBuildOutputVars() {
 		return topBuildOutVars;
 	}
 
@@ -4787,4 +4856,24 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 		return root;
 	}
 
+	private String toCleanTarget(IPath path) {
+		final BitSet allowedCodePoints = new BitSet(128);
+		allowedCodePoints.set("A".codePointAt(0), "Z".codePointAt(0) + 1); //$NON-NLS-1$ //$NON-NLS-2$
+		allowedCodePoints.set("a".codePointAt(0), "z".codePointAt(0) + 1); //$NON-NLS-1$ //$NON-NLS-2$
+		allowedCodePoints.set("0".codePointAt(0), "9".codePointAt(0) + 1); //$NON-NLS-1$ //$NON-NLS-2$
+		allowedCodePoints.set("_".codePointAt(0)); //$NON-NLS-1$
+
+		StringBuilder sb = new StringBuilder("clean-"); //$NON-NLS-1$
+		(path.isEmpty() ? DOT : path.toString()).codePoints().forEach(c -> {
+			if (allowedCodePoints.get(c)) {
+				sb.append(Character.toChars(c));
+			} else {
+				sb.append("-"); //$NON-NLS-1$
+				sb.append(Long.toHexString(c));
+				sb.append("-"); //$NON-NLS-1$
+			}
+		});
+
+		return sb.toString();
+	}
 }

build/org.eclipse.cdt.managedbuilder.core.tests/resources/depCalcProjects/test1DepCalc2/Benchmarks/makefile
@@ -50,7 +50,7 @@ test1DepCalc2: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(OBJS)$(C_DEPS)$(EXECUTABLES) test1DepCalc2
+	-$(RM) test1DepCalc2
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/depCalcProjects/test1DepCalc3/Benchmarks/makefile
@@ -50,7 +50,7 @@ test1DepCalc3: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(OBJS)$(C_DEPS)$(EXECUTABLES) test1DepCalc3
+	-$(RM) test1DepCalc3
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/depCalcProjects/test1DepCalcPreBuild/Benchmarks/makefile
@@ -50,7 +50,7 @@ test1DepCalcPreBuild: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_D
 
 # Other Targets
 clean:
-	-$(RM) $(OBJS)$(C_DEPS)$(EXECUTABLES) test1DepCalcPreBuild
+	-$(RM) test1DepCalcPreBuild
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/oldTypeProjects/1.2/Benchmarks/makefile
@@ -63,7 +63,7 @@ test12.exe: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(C++_DEPS)$(OBJS)$(C_DEPS)$(CC_DEPS)$(CPP_DEPS)$(EXECUTABLES)$(CXX_DEPS)$(C_UPPER_DEPS) test12.exe
+	-$(RM) test12.exe
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/oldTypeProjects/2.0/Benchmarks/makefile
@@ -63,7 +63,7 @@ test20.exe: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(OBJS)$(C++_DEPS)$(EXECUTABLES)$(CC_DEPS)$(C_DEPS)$(CPP_DEPS)$(CXX_DEPS)$(C_UPPER_DEPS) test20.exe
+	-$(RM) test20.exe
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/oldTypeProjects/2.1/Benchmarks/makefile
@@ -49,7 +49,7 @@ MBS2dot1.exe: $(OBJS) $(USER_OBJS) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 
 # Other Targets
 clean:
-	-$(RM) $(OBJS)$(C_DEPS)$(EXECUTABLES) MBS2dot1.exe
+	-$(RM) MBS2dot1.exe
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

commit 32a6ad1f4d444218050b3b6b5a841641125fd170

Bug 575702: Nondeterministic makefile ordering (prelude)
This fixes a small and rarely hit in practice regression regarding
nondeterministic ordering in makefile output introduced in 15c29eb (bug
505882).

Multiple values for a build output variable were previously output in
definition order. Now they are generated by iterating over a HashSet,
which does not have a deterministic iteration order. Use a TreeSet for
output in a consistently sorted order instead.

Output variables with multiple values are rare (most tools generate one
output file of a particular type) and this case was not exercised in the
existing unit tests. Modify one such that it does (even if in a somewhat
contrived situation). Of note, ${OUTPUT} cannot be used in the
commandLinePattern for such a tool because it squashes the multiple
values into a single quoted file name - misuse ${FLAGS} instead.

Change-Id: Iaa28525c21d74c87dd4d72a03e334ed0e5576727
Signed-off-by: Christian Walther <walther@indel.ch>

build/org.eclipse.cdt.managedbuilder.core.tests/plugin.xml
@@ -3613,6 +3613,42 @@
 			            outputs="tmp">
 			         </outputType>
 			      </tool>
+			      <tool
+			      		name="touch"
+			      		command="touch"
+			      		commandLinePattern="${COMMAND} -r ${INPUTS} ${FLAGS}"
+			      		id="test30_2.touch">
+					 <optionCategory
+						  name="Options"
+						  id="test30_2.touch.category">
+					 </optionCategory>
+					 <option
+						  name="Touched Files"
+						  category="test30_2.touch.category"
+						  command="&quot;${value}&quot;"
+						  id="test30_2.touch.filenames"
+						  valueType="stringList">
+						<listOptionValue value="these"/>
+						<listOptionValue value="file names"/>
+						<listOptionValue value="are"/>
+						<listOptionValue value="not"/>
+						<listOptionValue value="in"/>
+						<listOptionValue value="alphabetical"/>
+						<listOptionValue value="order"/>
+					 </option>
+			         <inputType
+			            id="test30_2.touch.input1"
+			            buildVariable="LOGFILE"
+			            sources="log">
+			         </inputType>
+			         <outputType
+			            id="test30_2.touch.output"
+			            name="Touched files"
+			            multipleOfType="true"
+			            buildVariable="TOUCHEDFILES"
+			            option="test30_2.touch.filenames">
+			         </outputType>
+			      </tool>
 			  </toolChain>
          </configuration>
       </projectType>                 

build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/test30_2/Benchmarks/makefile
@@ -32,6 +32,15 @@ new.tar \
 LOGFILE += \
 new.log \
 
+TOUCHEDFILES += \
+alphabetical \
+are \
+file\ names \
+in \
+not \
+order \
+these \
+
 
 # All Target
 all: main-build
@@ -59,9 +68,15 @@ new.log: $(TAROUT) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
 	@echo 'Finished building: $@'
 	@echo ' '
 
+these file\ names are not in alphabetical order: $(LOGFILE) makefile objects.mk $(OPTIONAL_TOOL_DEPS)
+	@echo 'Invoking: touch'
+	touch -r $(LOGFILE) "these" "file names" "are" "not" "in" "alphabetical" "order"
+	@echo 'Finished building: $@'
+	@echo ' '
+
 # Other Targets
 clean:
-	-$(RM) new.log new.tar test30_2.tmp
+	-$(RM) alphabetical are file\ names in new.log new.tar not order test30_2.tmp these
 	-@echo ' '
 
 .PHONY: all clean dependents main-build

build/org.eclipse.cdt.managedbuilder.core.tests/resources/test30Projects/test30_2/Benchmarks/sources.mk
@@ -14,6 +14,7 @@ CC_SRCS :=
 TAROUT := 
 TERMINAL__DUMMY_OUTPUT__OUTPUTS := 
 LOGFILE := 
+TOUCHEDFILES := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \

build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/makegen/gnu2/GnuMakefileGenerator.java
@@ -39,6 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -4035,7 +4036,8 @@ public class GnuMakefileGenerator implements IManagedBuilderMakefileGenerator2 {
 
 					// for projects with specific setting on folders/files do
 					// not clear the macro value on subsequent passes
-					map.putIfAbsent(macroName, new HashSet<>());
+					// use TreeSet for deterministically sorted output
+					map.putIfAbsent(macroName, new TreeSet<>());
 				}
 
 				// Set of input extensions for which macros have been created so far
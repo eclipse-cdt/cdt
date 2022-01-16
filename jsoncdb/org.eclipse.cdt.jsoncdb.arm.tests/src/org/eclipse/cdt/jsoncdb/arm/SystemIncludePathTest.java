66f344abf608c9a16a60152d014f76ee5ccf5b4d
Martin Weber <fifteenknots505@gmail.com>
2022-01-16 17:44:32 +0100

    Move class o.e.cdt.jsoncdb.core.participant.Arglets.SystemIncludePath_armcc to plugin org.eclipse.cdt.jsoncdb.arm
    Eliminates a TODO.
    
    Change-Id: I00b30ce14b0218947e7a93b95cd8f6e66ae2b650
    Signed-off-by: Martin Weber <fifteenknots505@gmail.com>

--- jsoncdb/org.eclipse.cdt.jsoncdb.arm.tests/src/org/eclipse/cdt/jsoncdb/arm/SystemIncludePathTest.java
+++ jsoncdb/org.eclipse.cdt.jsoncdb.arm.tests/src/org/eclipse/cdt/jsoncdb/arm/SystemIncludePathTest.java
@@ -6,12 +6,12 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
-package org.eclipse.cdt.jsoncdb.core.internal;
+package org.eclipse.cdt.jsoncdb.arm;
 
 import static org.junit.Assert.assertEquals;
 
-import org.eclipse.cdt.jsoncdb.core.participant.Arglets.SystemIncludePath_armcc;
-import org.eclipse.cdt.core.settings.model.ICLanguageSettingEntry;
+import org.eclipse.cdt.jsoncdb.core.participant.DefaultToolCommandlineParser;
+import org.eclipse.cdt.jsoncdb.core.participant.IToolCommandlineParser.IResult;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.junit.Before;
@@ -20,54 +20,50 @@ import org.junit.Test;
 /**
  * @author Martin Weber
  */
-public class SystemIncludePath_armcc_Test {
+public class SystemIncludePathTest {
 
-	private SystemIncludePath_armcc testee;
+	private ArmccToolDetectionParticipant.SystemIncludePath_armcc testee;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		testee = new SystemIncludePath_armcc();
+		testee = new ArmccToolDetectionParticipant.SystemIncludePath_armcc();
 	}
 
 	@Test
 	public final void testProcessArgument() {
+		DefaultToolCommandlineParser tcp = new DefaultToolCommandlineParser(null, null, testee);
+
 		final String more = " -g "
 				+ " -o CMakeFiles/execut1.dir/util1.c.o -c /testprojects/C-subsrc/src/src-sub/main1.c";
-		ParseContext result;
-		ICLanguageSettingEntry parsed;
 		final IPath cwd = new Path("");
 
 		String name = "/an/Include/Path";
 
+		IResult result;
 		// -J/an/Include/Path
-		result = new ParseContext();
-		assertEquals(2 + name.length(), testee.processArgument(result, cwd, "-J" + name + more));
+		result = tcp.processArgs(cwd, "-J" + name + more);
 		assertEquals("#entries", 1, result.getSystemIncludePaths().size());
 		assertEquals("name", name, result.getSystemIncludePaths().get(0));
 		// -J'/an/Include/Path'
-		result = new ParseContext();
-		assertEquals(2 + name.length() + 2, testee.processArgument(result, cwd, "-J" + "'" + name + "'" + more));
+		result = tcp.processArgs(cwd, "-J" + "'" + name + "'" + more);
 		assertEquals("#entries", 1, result.getSystemIncludePaths().size());
 		assertEquals("name", name, result.getSystemIncludePaths().get(0));
 		// -J"/an/Include/Path"
-		result = new ParseContext();
-		assertEquals(2 + name.length() + 2, testee.processArgument(result, cwd, "-J" + "\"" + name + "\"" + more));
+		result = tcp.processArgs(cwd, "-J" + "\"" + name + "\"" + more);
 		assertEquals("#entries", 1, result.getSystemIncludePaths().size());
 		assertEquals("name", name, result.getSystemIncludePaths().get(0));
 
 		name = (new Path("A:an\\In CLU  de/Pat h")).toOSString();
 		// -J"A:an\In CLU de/Pat h"
-		result = new ParseContext();
-		assertEquals(2 + name.length() + 2, testee.processArgument(result, cwd, "-J" + "\"" + name + "\"" + more));
+		result = tcp.processArgs(cwd, "-J" + "\"" + name + "\"" + more);
 		assertEquals("#entries", 1, result.getSystemIncludePaths().size());
 		assertEquals("name", name, result.getSystemIncludePaths().get(0));
 
 		// -J'A:an\In CLU de/Pat h'
-		result = new ParseContext();
-		assertEquals(2 + name.length() + 2, testee.processArgument(result, cwd, "-J" + "'" + name + "'" + more));
+		result = tcp.processArgs(cwd, "-J" + "\"" + name + "\"" + more);
 		assertEquals("#entries", 1, result.getSystemIncludePaths().size());
 		assertEquals("name", name, result.getSystemIncludePaths().get(0));
 	}

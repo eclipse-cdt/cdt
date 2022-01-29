c9cb18af0ff4b5bfb6c2efcb439f1549e3c59101
Torbjörn Svensson <torbjorn.svensson@st.com>
2022-01-29 17:08:37 +0100

    Bug 578400: Refresh view on ICommandControlRefreshAllDMEvent
    Contributed by STMicroelectronics
    
    Also-By: Jonah Graham <jonah@kichwacoders.com>
    Signed-off-by: Torbjörn Svensson <torbjorn.svensson@st.com>
    Change-Id: I879c09e90056798ddf3b6da44d1399afd7139600

--- dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/DisassemblyBackendDsf.java
+++ dsf/org.eclipse.cdt.dsf.ui/src/org/eclipse/cdt/dsf/debug/internal/ui/disassembly/DisassemblyBackendDsf.java
@@ -73,6 +73,7 @@ import org.eclipse.cdt.dsf.debug.service.ISourceLookup.ISourceLookupDMContext;
 import org.eclipse.cdt.dsf.debug.service.IStack;
 import org.eclipse.cdt.dsf.debug.service.IStack.IFrameDMContext;
 import org.eclipse.cdt.dsf.debug.service.IStack.IFrameDMData;
+import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlRefreshAllDMEvent;
 import org.eclipse.cdt.dsf.internal.ui.DsfUIPlugin;
 import org.eclipse.cdt.dsf.service.DsfServiceEventHandler;
 import org.eclipse.cdt.dsf.service.DsfServicesTracker;
@@ -459,6 +460,15 @@ public class DisassemblyBackendDsf extends AbstractDisassemblyBackend implements
 			return fServicesTracker.getService(serviceClass);
 		}
 		return null;
+	}
+
+	@DsfServiceEventHandler
+	public void handleEvent(ICommandControlRefreshAllDMEvent event) {
+		if (fTargetContext == null) {
+			return;
+		}
+
+		fCallback.asyncExec(() -> fCallback.refresh());
 	}
 
 	@DsfServiceEventHandler

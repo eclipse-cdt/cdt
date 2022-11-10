fa14e99ff8548ed94fbf9d0a3575752aee90501d
John Dallaway <john@dallaway.org.uk>
2022-11-10 19:37:21 +0000

    Workaround EASE 0.9.0 headless startup issue (#156)
    We register org.eclipse.cdt.codan.internal.ui.cxx.Startup with EASE to
    avoid triggering bug 580960.

--- releng/scripts/do_project_settings.sh
+++ releng/scripts/do_project_settings.sh
@@ -103,6 +103,12 @@ git ls-files  -- \*\*/.project ":!$COREPROJECT/.project" | while read i ; do
                 '-es@compilers.p.unknown-identifier=0@compilers.p.unknown-identifier=1@' \
                 $d/.settings/org.eclipse.pde.prefs
         fi
+        if echo $i | grep 'org.eclipse.cdt.codan.ui.cxx' > /dev/null; then
+            # Special case, see comment in org.eclipse.cdt.codan.ui.cxx/plugin.xml
+            sed -i \
+                '-es@compilers.p.unresolved-ex-points=0@compilers.p.unresolved-ex-points=1@' \
+                $d/.settings/org.eclipse.pde.prefs
+        fi
     else
         rm -f $d/.settings/org.eclipse.pde*.prefs
     fi

09951ee03924735b468d58c7a7e3d674273c0a61
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2022-12-08 02:54:52 +0800

    1

--- core/org.eclipse.cdt.core.native/src/org/eclipse/cdt/utils/pty/PTY2Util.java
+++ core/org.eclipse.cdt.core.native/src/org/eclipse/cdt/utils/pty/PTY2Util.java
@@ -1,3 +1,12 @@
+/********************************************************************************
+ * Copyright (c) 2022 徐持恒 Xu Chiheng
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
 package org.eclipse.cdt.utils.pty;
 
 import java.io.File;

f495358a251c98f3925f7b0cf743840ecd7bbd4d
Gesa HENTSCHKE <Gesa.HENTSCHKE@bachmann.info>
2023-08-02 17:48:09 +0200

    [#484] Add extension point for custom icons of C/C++ source files
    - Add missing license header
    
    fixes #484

--- core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/ICFileImageDescriptor.java
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/ui/ICFileImageDescriptor.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   See git history
+ *******************************************************************************/
 package org.eclipse.cdt.ui;
 
 import org.eclipse.core.resources.IProject;

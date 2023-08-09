e725b889518c7625268160f68bf8111fc86b9c51
Jonah Graham <jonah@kichwacoders.com>
2023-08-09 16:40:56 -0400

    Fix test cases that recently regressed
    Recently new extensions were added to CDT for C++ modules in #422
    At the time some test failures were not noticed. This change patches
    those failing tests.
    
    Fixes #454

--- build/org.eclipse.cdt.managedbuilder.core.tests/resources/test40Projects/test_40/Benchmarks/dbg 2/sources.mk
+++ build/org.eclipse.cdt.managedbuilder.core.tests/resources/test40Projects/test_40/Benchmarks/dbg 2/sources.mk
@@ -3,18 +3,24 @@
 ################################################################################
 
 ASM_SRCS := 
+C++M_SRCS := 
 C++_SRCS := 
+CCM_SRCS := 
 CC_SRCS := 
 CPP_SRCS := 
+CXXM_SRCS := 
 CXX_SRCS := 
 C_SRCS := 
 C_UPPER_SRCS := 
 OBJ_SRCS := 
 O_SRCS := 
 S_UPPER_SRCS := 
+C++M_DEPS := 
 C++_DEPS := 
+CCM_DEPS := 
 CC_DEPS := 
 CPP_DEPS := 
+CXXM_DEPS := 
 CXX_DEPS := 
 C_DEPS := 
 C_UPPER_DEPS := 

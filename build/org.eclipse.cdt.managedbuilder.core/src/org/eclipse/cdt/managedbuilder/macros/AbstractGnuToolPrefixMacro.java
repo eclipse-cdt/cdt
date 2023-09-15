8062151c301d8b057c7957eaf4be0e6a84e85e6c
John Dallaway <john@dallaway.org.uk>
2023-09-15 18:01:17 +0100

    Add AbstractGnuToolPrefixMacro copyright notice

--- build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/macros/AbstractGnuToolPrefixMacro.java
+++ build/org.eclipse.cdt.managedbuilder.core/src/org/eclipse/cdt/managedbuilder/macros/AbstractGnuToolPrefixMacro.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2023 John Dallaway and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * John Dallaway - Initial API and implementation (#361)
+ *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.macros;
 
 import org.eclipse.cdt.core.cdtvariables.CdtVariableException;

285bdb3509a53d8c809fe021d71bdb4506d1f95b
Michaelll123 <3120205503@bit.edu.cn>
2024-04-07 15:21:16 +0800

    Improving the Names Recommended by Extract Local Variable Refactoring. Addressing Issue 745.

---
+++ core/org.eclipse.cdt.ui/src/org/eclipse/cdt/internal/ui/refactoring/extractlocalvariable/AbortSearchException.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2025 Fabrice TIERCELIN and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Taiming Wang - Initial implementation copied from JDT and extended for CDT
+ *******************************************************************************/
+package org.eclipse.cdt.internal.ui.refactoring.extractlocalvariable;
+
+public class AbortSearchException extends RuntimeException {
+	private static final long serialVersionUID = 8809979732051907351L;
+}

ca7ce9971cbae654e3274a621485736c2db8dca2
Erwin Waterlander <erwin.waterlander@intel.com>
2024-06-12 10:20:38 +0100

    Support arguments and working directory in launching Core Build projects for Local
    Added the CArgumentsTab to the Core Build launch configuration for the
    Local target. This enables users to set command line arguments and
    specify a working directory.

--- debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/launch/CoreBuildLocalRunLaunchDelegate.java
+++ debug/org.eclipse.cdt.debug.core/src/org/eclipse/cdt/debug/internal/core/launch/CoreBuildLocalRunLaunchDelegate.java
@@ -10,18 +10,25 @@
  *******************************************************************************/
 package org.eclipse.cdt.debug.internal.core.launch;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.cdt.core.build.ICBuildConfiguration;
 import org.eclipse.cdt.core.model.IBinary;
 import org.eclipse.cdt.debug.core.CDebugCorePlugin;
+import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
 import org.eclipse.cdt.debug.core.launch.CoreBuildLaunchConfigDelegate;
 import org.eclipse.cdt.debug.internal.core.InternalDebugCoreMessages;
+import org.eclipse.cdt.utils.CommandLineUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.variables.VariablesPlugin;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -38,7 +45,24 @@ public class CoreBuildLocalRunLaunchDelegate extends CoreBuildLaunchConfigDelega
 		IBinary exeFile = getBinary(buildConfig);
 
 		try {
-			ProcessBuilder builder = new ProcessBuilder(Paths.get(exeFile.getLocationURI()).toString());
+			String args = configuration.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, ""); //$NON-NLS-1$
+			if (args.length() != 0) {
+				args = VariablesPlugin.getDefault().getStringVariableManager().performStringSubstitution(args);
+			}
+
+			String[] arguments = CommandLineUtil.argumentsToArray(args);
+			List<String> command = new ArrayList<>(1 + arguments.length);
+			command.add(Paths.get(exeFile.getLocationURI()).toString());
+			command.addAll(Arrays.asList(arguments));
+
+			ProcessBuilder builder = new ProcessBuilder(command);
+
+			String workingDirectory = configuration
+					.getAttribute(ICDTLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY, ""); //$NON-NLS-1$
+			if (!workingDirectory.isBlank()) {
+				builder.directory(new File(workingDirectory));
+			}
+
 			buildConfig.setBuildEnvironment(builder.environment());
 			Process process = builder.start();
 			DebugPlugin.newProcess(launch, process, exeFile.getPath().lastSegment());

c1d2386e3639a3d39b4fce5a93fb1bc6756f3300
Jonah Graham <jonah@kichwacoders.com>
2025-01-22 11:01:11 -0500

    Provide an example of extending CMake project type in Core Build
    In issue #1000 a lot of the work is on exposing a better API to ISVs.
    This new project serves these purposes:
    
    - Ensure the exposed API works in practice
    - Example for ISVs to use if they want to extend CMake
    - Provide an in-tree example of the API in use, so future editors of
      CDT have better chance of understanding *why* items are done as
      they are.

---
+++ cmake/org.eclipse.cdt.cmake.example/src/org/eclipse/cdt/cmake/example/ExtendedCMakeBuildConfigurationProvider.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2025 QNX Software Systems and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.cmake.example;
+
+import org.eclipse.cdt.cmake.core.CMakeBuildConfiguration;
+import org.eclipse.cdt.cmake.core.CMakeBuildConfigurationProvider;
+import org.eclipse.cdt.cmake.core.ICMakeToolChainFile;
+import org.eclipse.cdt.core.build.IToolChain;
+import org.eclipse.core.resources.IBuildConfiguration;
+import org.eclipse.core.runtime.CoreException;
+
+public class ExtendedCMakeBuildConfigurationProvider extends CMakeBuildConfigurationProvider {
+	@SuppressWarnings("hiding")
+	public static final String ID = "org.eclipse.cdt.cmake.example.provider.extended"; //$NON-NLS-1$
+
+	@Override
+	public String getId() {
+		return ID;
+	}
+
+	@Override
+	protected CMakeBuildConfiguration createCMakeBuildConfiguration(IBuildConfiguration config, String name)
+			throws CoreException {
+		return new ExtendedCMakeBuildConfiguration(config, name);
+	}
+
+	@Override
+	protected CMakeBuildConfiguration createCMakeBuildConfiguration(IBuildConfiguration config, String name,
+			IToolChain toolChain) {
+		return new ExtendedCMakeBuildConfiguration(config, name, toolChain);
+	}
+
+	@Override
+	protected CMakeBuildConfiguration createCMakeBuildConfiguration(IBuildConfiguration config, String name,
+			IToolChain toolChain, ICMakeToolChainFile toolChainFile, String launchMode) {
+		return new ExtendedCMakeBuildConfiguration(config, name, toolChain, toolChainFile, launchMode);
+	}
+
+}
---
+++ cmake/org.eclipse.cdt.cmake.example/src/org/eclipse/cdt/cmake/example/ExtendedCMakeNature.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2025 QNX Software Systems and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.cdt.cmake.example;
+
+import org.eclipse.cdt.cmake.core.CMakeNature;
+
+public class ExtendedCMakeNature extends CMakeNature {
+
+	@SuppressWarnings("hiding")
+	public static final String ID = "org.eclipse.cdt.cmake.example.extendedCmakeNature"; //$NON-NLS-1$
+}

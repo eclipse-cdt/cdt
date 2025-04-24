8b22be8683776e3e8d68c322b727e5997692c980
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-04-24 10:19:01 +0200

    Allow debugging without modify the source
    Currently one needs to modify and recompile the source to enable
    debugging, this is replaced by a system property that can be set.

--- core/org.eclipse.cdt.core.native/src/org/eclipse/cdt/utils/spawner/ProcessFactory.java
+++ core/org.eclipse.cdt.core.native/src/org/eclipse/cdt/utils/spawner/ProcessFactory.java
@@ -36,6 +36,8 @@ import org.eclipse.core.runtime.Platform;
  */
 public class ProcessFactory {
 
+	private static final boolean DEBUG = Boolean.getBoolean("org.eclipse.cdt.utils.spawner.debug"); //$NON-NLS-1$
+
 	static private ProcessFactory instance;
 	private boolean hasSpawner;
 	private Runtime runtime;
@@ -255,9 +257,9 @@ public class ProcessFactory {
 		}
 
 		public Process start() throws IOException {
-			// Uncomment the next line, set a breakpoint in the last line of debug() method,
-			// when the breakpoint is triggered, inspect the sb variable to see detailed info on what is being launched.
-			// debug();
+			if (DEBUG) {
+				debug();
+			}
 			Process p;
 			if (hasSpawner) {
 				if (use_pty) {

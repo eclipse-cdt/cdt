193d6281da371af95b1f885c425c3c7650c02037
John Dallaway <john@dallaway.org.uk>
2025-06-12 17:38:11 +0100

    Implement LLDB memory service
    We use CLI to read address size, replacing MI expression evaluation
    which is not supported by LLDB-MI outside the context of a running
    process.
    
    We accommodate the LLDB response format when reading addressable size.
    
    We assume little-endian since CLI 'show endian' is not supported.

---
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/mi/service/command/commands/CLIAddressSize.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2025 John Dallaway and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     John Dallaway - Initial implementation (#1191)
+ *******************************************************************************/
+
+package org.eclipse.cdt.dsf.mi.service.command.commands;
+
+import org.eclipse.cdt.dsf.debug.service.IMemory.IMemoryDMContext;
+import org.eclipse.cdt.dsf.mi.service.command.output.CLIAddressSizeInfo;
+import org.eclipse.cdt.dsf.mi.service.command.output.MIOutput;
+
+/**
+ * CLI command to report address size where the MI expression evaluator is not available
+ *
+ * @since 7.2
+ */
+public class CLIAddressSize extends MIInterpreterExecConsole<CLIAddressSizeInfo> {
+
+	private static final String COMMAND = "p/x sizeof(void*)"; //$NON-NLS-1$
+
+	public CLIAddressSize(IMemoryDMContext ctx) {
+		super(ctx, COMMAND);
+	}
+
+	@Override
+	public CLIAddressSizeInfo getResult(MIOutput miResult) {
+		return new CLIAddressSizeInfo(miResult);
+	}
+}

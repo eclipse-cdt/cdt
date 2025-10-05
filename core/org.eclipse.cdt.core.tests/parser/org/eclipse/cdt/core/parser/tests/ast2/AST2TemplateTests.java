43207c2176c4c361494c8837ce13094ba05934d7
Igor V. Kovalenko <igor.v.kovalenko@gmail.com>
2025-10-05 21:24:33 +0300

    Test execution of compound statement with static_assert declaration statement

--- core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/ast2/AST2TemplateTests.java
+++ core/org.eclipse.cdt.core.tests/parser/org/eclipse/cdt/core/parser/tests/ast2/AST2TemplateTests.java
@@ -11942,4 +11942,45 @@ public class AST2TemplateTests extends AST2CPPTestBase {
 		CPPVariable valueIfTrue = helper.assertNonProblem("value_if_true");
 		assertEquals(IntegralValue.STATIC_ASSERT_FAILED_ERROR, valueIfTrue.getInitialValue());
 	}
+
+	//	constexpr bool asserts_true() {
+	//	    static_assert(true);
+	//	    return true;
+	//	}
+	//
+	//	constexpr bool calls_asserts_true() {
+	//	    asserts_true();
+	//	    return true;
+	//	}
+	//
+	//	constexpr bool asserts_false() {
+	//	    static_assert(false);
+	//	    return true;
+	//	}
+	//
+	//	constexpr bool calls_asserts_false() {
+	//	    asserts_false();
+	//	    return true;
+	//	}
+	//
+	//	static constexpr auto value_asserts_true = asserts_true();
+	//	static constexpr auto value_calls_asserts_true = calls_asserts_true();
+	//	static constexpr auto value_asserts_false = asserts_false();
+	//	static constexpr auto value_calls_asserts_false = calls_asserts_false();
+	public void testStaticAssertInCompoundStatement() throws Exception {
+		BindingAssertionHelper helper = getAssertionHelper();
+
+		// initial value must be available and equal 1but it's value is wrong because static_assert() is not supported (yet)
+		helper.assertVariableValue("value_asserts_true", 1);
+		helper.assertVariableValue("value_calls_asserts_true", 1);
+
+		// initial value must be available, and should be problem value
+		CPPVariable valueAssertsFalse = helper.assertNonProblem("value_asserts_false");
+		assertEquals(IntegralValue.STATIC_ASSERT_FAILED_ERROR, valueAssertsFalse.getInitialValue());
+		//assertEquals(IntegralValue.ERROR, valueAssertsFalse.getInitialValue());
+
+		// initial value must be available, and should be problem value
+		CPPVariable valueCallsAssertsFalse = helper.assertNonProblem("value_calls_asserts_false");
+		assertEquals(IntegralValue.STATIC_ASSERT_FAILED_ERROR, valueCallsAssertsFalse.getInitialValue());
+	}
 }
